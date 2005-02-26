53ff556a0370da3d7f61829cf1ccdf59fef3ab46
Robin Fernandes <rewbs@users.sourceforge.net>
2005-02-26 18:52:19 +0000

    v 1.17RC0 (revision 1.13.2.5)    . <rewbs> Fixed ridiculous mono ITI loading bug.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT_1_17RC/wild@88 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mptrack.cpp
+++ mptrack/Mptrack.cpp
@@ -1295,7 +1295,7 @@ BOOL CAboutDlg::OnInitDialog()
 	m_bmp.LoadBitmap(MAKEINTRESOURCE(IDB_MPTRACK));
 	wsprintf(s, "Build Date: %s", gszBuildDate);
 	SetDlgItemText(IDC_TEXT1, s);
-	wsprintf(s, "%s version %X.%02XRC0 (revision 1.13.2.4)",		//remove 'a' when ready for wide usage.
+	wsprintf(s, "%s version %X.%02XRC0 (revision 1.13.2.5)",		//remove 'a' when ready for wide usage.
 				MAINFRAME_TITLE,
 				(MPTRACK_VERSION>>24)&0xFF,
 				(MPTRACK_VERSION>>16)&0xFF,
