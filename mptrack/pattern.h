18156a1e977e2f912915cea55ba55f5f840e39fe
Ahti Leppänen <relabsoluness@users.sourceforge.net>
2009-01-10 11:10:49 +0000

    / Pattern: The keyboard split 'feature' when entering chords is removed (bug 2790). . Pattern: Undo should now work with chords.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@244 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/pattern.h
+++ mptrack/pattern.h
@@ -8,6 +8,8 @@ using std::vector;
 
 class CPatternContainer;
 
+typedef MODCOMMAND* PatternRow;
+
 
 //============
 class CPattern
@@ -38,6 +40,10 @@ public:
 
 	// Return true if modcommand can be accessed from given row, false otherwise.
 	bool IsValidRow(const ROWINDEX iRow) const {return (iRow < GetNumRows());}
+
+	// Return PatternRow object which has operator[] defined so that MODCOMMAND
+	// at (iRow, iChn) can be accessed with GetRow(iRow)[iChn].
+	PatternRow GetRow(const ROWINDEX iRow) {return GetpModCommand(iRow, 0);}
 
 	CHANNELINDEX GetNumChannels() const;
 
