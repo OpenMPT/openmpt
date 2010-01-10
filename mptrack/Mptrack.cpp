181ab7b16188111c0b9500896b52fdf477e81539
Johannes Schultz <saga-games@users.sourceforge.net>
2010-01-10 21:54:41 +0000

    [Fix] Sample editor: Using "resize sample" created an invalid undo point which could lead to broken samples and even crashes. [Mod] Updated the "what's new" dialog a bit (removed leading spaces, as they look ugly in Vista/Win7 messageboxes anyway (those have a slightly different format, so every line was too long)) [Mod] Changed history... (Ok, I just changed history.txt)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@464 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mptrack.cpp
+++ mptrack/Mptrack.cpp
@@ -228,26 +228,25 @@ static void ShowChangesDialog()
 //-----------------------------
 {
 	const char* const firstOpenMessage = "OpenMPT build " MPT_VERSION_STR ".\n\n"
-				"Some changes since version 1.17.02.54:\n\n"
-				"    [New] Pattern tab: New paste modes: overflow, push forward and flood.\n"
-				"    [Mod] Pattern tab: Keyboard split is now accessible from keyshortcut or menu.\n"
-				"    [Imp] Pattern tab: Numerous improvements including better effect descriptions in note properties.\n"
-				"    [New] Sequence editor: Can now handle order selections including delete/copy/cut/paste functionality.\n"
-				"    [New] Sequence editor: Can render selected patterns to wave directly from orderlist.\n"
-				"    [New] Envelope editor: Numerous improvements including ability to insert and remove points easily.\n"
-				"    [New] Sample tab: sample undo, sample draw, resize sample, DC offset removal, batch export, better loop...\n"
-				"            ...point handling when deleting selections. Play sample from given position with Ctrl + left mouse button.\n"
-				"    [New] MPTM: New parameter controls for controlling plug params from pattern.\n"
-				"    [New] MPTM: Can have envelope points up to 240 and multiple sequences.\n"
-				"    [New] MIDI mapping: Editing a plug param in its editor while holding shift key will now open MIDI mapping dialog.\n"
-				"    [New] MIDI mapping: Can now record MIDI mapping changes to pattern.\n"
-				"    [Imp] MOD/S3M/XM/IT: Numerous improvements to load, save and playback compatibility.\n"
-				"    [New] Input: Can import RIFF AM, RIFF AMFF, J2B, PSM16, IMF, GDM and SCL files. Improved PSM import.\n"
-				"    [New] Setup: New default directories: plugins and plugin presets, sharable colour schemes\n"
-				"    [New] Cleanup: New option compo cleanup, rearrange samples is back, can merge sequences.\n"
-				"    [Mod] Misc: Program settings are now by default stored in %APPDATA%\\OpenMPT\n"
-				"\n"
-				"    and many more. See history.txt for more details.";
+		"Some changes since version 1.17.02.54:\n\n"
+		"[New] Pattern tab: New paste modes: overflow, push forward and flood.\n"
+		"[Mod] Pattern tab: Keyboard split is now accessible from key shortcut or menu.\n"
+		"[Imp] Pattern tab: Numerous improvements including better effect descriptions in note properties.\n"
+		"[New] Sequence editor: Can now handle order selections including delete/copy/cut/paste functionality.\n"
+		"[New] Sequence editor: Can render selected patterns to wave directly from orderlist.\n"
+		"[New] Envelope editor: Numerous improvements including ability to insert and remove points easily and editing envelopes with the keyboard.\n"
+		"[New] Sample tab: sample undo, sample draw, resize sample, DC offset removal, batch export, better loop point handling when deleting selections. Play sample from given position with Ctrl + left mouse button.\n"
+		"[New] MPTM: New parameter controls for controlling plug params from pattern.\n"
+		"[New] MPTM: Can have envelope points up to 240 and multiple sequences.\n"
+		"[New] MIDI mapping: Editing a plug param in its editor while holding shift key will now open MIDI mapping dialog.\n"
+		"[New] MIDI mapping: Can now record MIDI mapping changes to pattern.\n"
+		"[Imp] MOD/S3M/XM/IT: Numerous improvements to load, save and playback compatibility.\n"
+		"[New] Input: Can import J2B (RIFF AM / RIFF AMFF), PSM16, IMF, GDM and SCL files. Improved PSM import.\n"
+		"[New] Setup: New default directories: plugins and plugin presets, sharable colour schemes\n"
+		"[New] Cleanup: New option compo cleanup, rearrange samples is back, can merge sequences.\n"
+		"[Mod] Misc: Program settings are now by default stored in %APPDATA%\\OpenMPT\n"
+		"\n"
+		"and many more. See history.txt for more details.";
 			
 	CMainFrame::GetMainFrame()->MessageBox(firstOpenMessage, "OpenMPT " MPT_VERSION_STR, MB_ICONINFORMATION);
 }
@@ -3279,7 +3278,7 @@ LRESULT CTrackApp::ProcessWndProcException(CException* e, const MSG* pMsg)
  * - load: true: load dialog. false: save dialog.
  * - defaultExtension: dialog should use this as the default extension for the file(s)
  * - defaultFilename: dialog should use this as the default filename
- * - extFilter: list of possible extensions. format: "description|extensions|description|extensions|..."
+ * - extFilter: list of possible extensions. format: "description|extensions|...|description|extensions||"
  * - workingDirectory: default directory of the dialog
  * - allowMultiSelect: allow the user to select multiple files? (will be ignored if load == false)
  * - filterIndex: pointer to a variable holding the index of the last extension filter used.
