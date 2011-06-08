6901ea8debe05daa9cb57d7d6f782200c9decfa8
Johannes Schultz <saga-games@users.sourceforge.net>
2011-06-08 22:15:59 +0000

    [Mod] Returning to _mkgmtime, since the new code doesn't seem to play well with DST settings (using it only in VS2003 now, where it is required because _mkgmtime is not defined)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@895 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/misc_util.cpp
+++ mptrack/misc_util.cpp
@@ -62,11 +62,14 @@ std::basic_string<TCHAR> Util::sdTime::GetDateTimeStr()
 
 time_t Util::sdTime::MakeGmTime(tm& timeUtc)
 {
+#if MSVC_VER_2003
+	// VC++ 2003 doesn't have _mkgmtime
+	// This does not seem to work properly with DST time zones sometimes - if that's of any concern for you, please upgrade your compiler :)
 	TIME_ZONE_INFORMATION tzi;
 	GetTimeZoneInformation(&tzi);
-	const time_t timeUtcTimeT = mktime(&timeUtc) - 60 * tzi.Bias;
-#if (_MSC_VER >= MSVC_VER_2005) && defined(_DEBUG)
-	ASSERT(timeUtcTimeT < 0 || timeUtcTimeT == _mkgmtime(&timeUtc));
+	const time_t timeUtcTimeT = mktime(&timeUtc) - tzi.Bias * 60;
+#else
+	const time_t timeUtcTimeT = _mkgmtime(&timeUtc);
 #endif
 	return timeUtcTimeT;
 }
