d288619a8a0de4499043be03fe9c0566a4bcac23
Johannes Schultz <saga-games@users.sourceforge.net>
2011-08-22 18:21:40 +0000

    [Mod] Project settings: Changed exception handling style (bloats EXE a bit, but catches exceptions in VST plugins again) [Mod] If an unhandled exception is encountered, a minidump is created. [Ref] Moved ACM code to separate file. [Mod] OpenMPT: Version is now 1.20.00.11
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@993 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ mptrack/ACMConvert.h
@@ -0,0 +1,69 @@
+/*
+ *
+ * ACMConvert.h
+ * ------------
+ * Purpose: Header for ACM and MPEG Layer-3 conversion
+ * Notes  : (currently none)
+ * Authors: Olivier Lapicque
+ *          OpenMPT Devs
+ *
+ */
+
+
+#pragma once
+#ifndef ACMCONVERT_H
+#define ACMCONVERT_H
+
+/////////////////////////////////////////////////////////////////////////////
+// ACM Functions (for dynamic linking)
+
+typedef VOID (ACMAPI *PFNACMMETRICS)(HACMOBJ, UINT, LPVOID);
+typedef MMRESULT (ACMAPI *PFNACMFORMATENUM)(HACMDRIVER, LPACMFORMATDETAILSA, ACMFORMATENUMCBA, DWORD dwInstance, DWORD fdwEnum);
+typedef MMRESULT (ACMAPI *PFNACMDRIVEROPEN)(LPHACMDRIVER, HACMDRIVERID, DWORD);
+typedef MMRESULT (ACMAPI *PFNACMDRIVERCLOSE)(HACMDRIVER, DWORD);
+typedef MMRESULT (ACMAPI *PFNACMSTREAMOPEN)(LPHACMSTREAM, HACMDRIVER, LPWAVEFORMATEX, LPWAVEFORMATEX, LPWAVEFILTER, DWORD, DWORD, DWORD);
+typedef MMRESULT (ACMAPI *PFNACMSTREAMCLOSE)(HACMSTREAM, DWORD);
+typedef MMRESULT (ACMAPI *PFNACMSTREAMSIZE)(HACMSTREAM, DWORD, LPDWORD, DWORD);
+typedef MMRESULT (ACMAPI *PFNACMSTREAMCONVERT)(HACMSTREAM, LPACMSTREAMHEADER, DWORD);
+typedef MMRESULT (ACMAPI *PFNACMDRIVERDETAILS)(HACMDRIVERID, LPACMDRIVERDETAILS, DWORD);
+
+
+//==============
+class ACMConvert
+//==============
+{
+protected:
+	HINSTANCE m_hACMInst;
+	HINSTANCE m_hBladeEnc, m_hLameEnc;
+	PFNACMFORMATENUM m_pfnAcmFormatEnum;
+	static BOOL layer3Present;
+
+public:
+	BOOL InitializeACM(BOOL bNoAcm = FALSE);
+	BOOL UninitializeACM();
+	static void AcmExceptionHandler();
+	MMRESULT AcmFormatEnum(HACMDRIVER had, LPACMFORMATDETAILSA pafd, ACMFORMATENUMCBA fnCallback, DWORD dwInstance, DWORD fdwEnum);
+	MMRESULT AcmDriverOpen(LPHACMDRIVER, HACMDRIVERID, DWORD);
+	MMRESULT AcmDriverDetails(HACMDRIVERID hadid, LPACMDRIVERDETAILS padd, DWORD fdwDetails);
+	MMRESULT AcmDriverClose(HACMDRIVER, DWORD);
+	MMRESULT AcmStreamOpen(LPHACMSTREAM, HACMDRIVER, LPWAVEFORMATEX, LPWAVEFORMATEX, LPWAVEFILTER pwfltr, DWORD dwCallback, DWORD dwInstance, DWORD fdwOpen);
+	MMRESULT AcmStreamClose(HACMSTREAM, DWORD);
+	MMRESULT AcmStreamSize(HACMSTREAM has, DWORD cbInput, LPDWORD pdwOutputBytes, DWORD fdwSize);
+	MMRESULT AcmStreamPrepareHeader(HACMSTREAM has, LPACMSTREAMHEADER pash, DWORD fdwPrepare);
+	MMRESULT AcmStreamUnprepareHeader(HACMSTREAM has, LPACMSTREAMHEADER pash, DWORD fdwUnprepare);
+	MMRESULT AcmStreamConvert(HACMSTREAM has, LPACMSTREAMHEADER pash, DWORD fdwConvert);
+	BOOL IsLayer3Present() const { return layer3Present; };
+
+	ACMConvert()
+	{
+		m_hBladeEnc = NULL;
+		m_hLameEnc = NULL;
+		m_hACMInst = NULL;
+	}
+
+protected:
+	static BOOL CALLBACK AcmFormatEnumCB(HACMDRIVERID, LPACMFORMATDETAILS, DWORD, DWORD);
+
+};
+
+#endif // ACMCONVERT_H
