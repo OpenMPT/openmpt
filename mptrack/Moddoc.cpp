7e6dc2b2e673497a607147e6ec5cb9d0efe172c1
Johannes Schultz <saga-games@users.sourceforge.net>
2011-09-23 23:12:06 +0000

    [Fix] Setting pattern names was slightly broken, so that CString::IsEmpty() checks "failed". [Mod] Renamed X Param to Parameter Extension
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@1052 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Moddoc.cpp
+++ mptrack/Moddoc.cpp
@@ -2294,7 +2294,7 @@ const MPTEFFECTINFO gFXInfo[] =
 	{CMD_DELAYCUT,		0x00,0x00,	0,	MOD_TYPE_MPT,	"Note Delay and Cut"},
 // -> CODE#0010
 // -> DESC="add extended parameter mechanism to pattern effects"
-	{CMD_XPARAM,		0x00,0x00,	0,	MOD_TYPE_XMITMPT,	"X param"},
+	{CMD_XPARAM,		0x00,0x00,	0,	MOD_TYPE_XMITMPT,	"Parameter Extension"},
 // -! NEW_FEATURE#0010
 	{CMD_NOTESLIDEUP,	0x00,0x00,	0,	MOD_TYPE_NONE,	"Note Slide Up"}, // .IMF effect
 	{CMD_NOTESLIDEDOWN,	0x00,0x00,	0,	MOD_TYPE_NONE,	"Note Slide Down"}, // .IMF effect
