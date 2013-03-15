b90e69886f8a1b110668a4bd314d2f0dfeaa5c31
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-03-15 22:09:13 +0000

    [Var] Remove unused NO_PACKING macro, remove HAVE_DOT_NET feature macro which was always set and remove all other macro tests for MSVC compilers older than 2008.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@1572 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/misc_util.cpp
+++ common/misc_util.cpp
@@ -74,15 +74,6 @@ std::basic_string<TCHAR> Util::sdTime::GetDateTimeStr()
 
 time_t Util::sdTime::MakeGmTime(tm& timeUtc)
 {
-#if (_MSC_VER < MSVC_VER_2005)
-	// VC++ 2003 doesn't have _mkgmtime
-	// This does not seem to work properly with DST time zones sometimes - if that's of any concern for you, please upgrade your compiler :)
-	TIME_ZONE_INFORMATION tzi;
-	GetTimeZoneInformation(&tzi);
-	const time_t timeUtcTimeT = mktime(&timeUtc) - tzi.Bias * 60;
-#else
-	const time_t timeUtcTimeT = _mkgmtime(&timeUtc);
-#endif
-	return timeUtcTimeT;
+	return _mkgmtime(&timeUtc);
 }
 
