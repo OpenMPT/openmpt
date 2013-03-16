OpenMPT/openmpt

commit dd9e1c128d86c7b9ec3c48ec4133e89c10b19618

[Ref] Introduced new string class to allow easier replacement of CString-objects in soundlib and made some related changes.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@1571 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Snd_defs.h
@@ -381,3 +381,92 @@ enum VibratoType
 	VIB_RAMP_DOWN,
 	VIB_RANDOM
 };
+
+namespace mpt
+{
+	// Quick and incomplete string class to be used as a replacement for
+	// CString and std::string in soundlib.
+	class String : public std::string
+	{
+	public:
+		typedef std::string BaseClass;
+		typedef char CharT;
+
+		String() {}
+		String(const CharT* psz) : BaseClass(psz) {}
+		String(const std::string& other) : BaseClass(other) {}
+
+		// Move constructors and move assignments.
+	#if (_MSC_VER >= MSVC_VER_2010)
+		String(BaseClass&& str) : BaseClass(std::move(str)) {}
+		String(String&& other) : BaseClass(std::move(static_cast<BaseClass&>(other))) {}
+		String& operator=(BaseClass&& str) {BaseClass::operator=(std::move(str)); return *this;}
+		String& operator=(String&& other) {BaseClass::operator=(std::move(static_cast<BaseClass&>(other))); return *this;}
+	#endif
+
+		String& operator=(const CharT* psz) {BaseClass::operator=(psz); return *this;}
+
+	#ifdef MODPLUG_TRACKER
+		String(const CString& str) {(*this) = str.GetString();}
+		String& operator=(const CString& str) {return operator=(str.GetString());}
+	#endif // MODPLUG_TRACKER
+
+		// To allow easy assignment to CString in GUI side.
+		operator const CharT*() const {return c_str();}
+
+		// Clears string.
+		void Empty() {clear();}
+
+		// Tests whether string is empty.
+		bool IsEmpty() const {return empty();}
+
+		// Compares this and other string.
+		bool Compare(const CharT* psz) const {return ((*this) == psz);}
+
+		// Equivalent to Empty(); Append(psz, nCount);
+		void SetString(const CharT* psz, const size_t nCount)
+		{
+			Empty();
+			Append(psz, nCount);
+		}
+
+		// Append string to this.
+		void Append(const CharT* psz) {append(psz);}
+
+		// Appends given string to this. Stops after 'nCount' chars if null terminator 
+		// hasn't been encountered before that.
+		void Append(const CharT* psz, const size_t nCount) {append(psz, nCount);}
+
+		// Appends single character to string.
+		void AppendChar(const CharT c) {append(1, c);}
+
+		// Difference between Append and AppendChars is that latter can be used to add 
+		// potentially non-null terminated char array.
+		template <size_t N>
+		void AppendChars(const CharT (&arr)[N])
+		{
+			append(arr, arr + N);
+		}
+
+		// Reserves store for the string without resizing.
+		void Reserve(size_t nSize) {reserve(nSize);}
+
+		// Formats this string, like CString::Format.
+		void Format(const CharT* pszFormat, ...)
+		{
+			va_list argList;
+			va_start( argList, pszFormat );
+
+			// Count the needed array size.
+			const size_t nCount = _vscprintf(pszFormat, argList); // null character not included.
+			resize(nCount + 1); // + 1 is for null terminator.
+
+			// Hack: directly modify the std::string's string.
+			// In C++11 std::string is guaranteed to be contiguous.
+			const int nCount2 = vsprintf_s(&*begin(), size(), pszFormat, argList);
+			resize(nCount2); // Removes the null character that vsprintf_s adds.
+
+			va_end( argList );
+		}
+	};
+}

common/misc_util.cpp
@@ -44,8 +44,8 @@ LPCCH LoadResource(LPCTSTR lpName, LPCTSTR lpType, LPCCH& pData, size_t& nSize, 
 
 
 // Returns WinAPI error message corresponding to error code returned by GetLastError().
-CString GetErrorMessage(DWORD nErrorCode)
-//---------------------------------------
+std::string GetErrorMessage(DWORD nErrorCode)
+//-------------------------------------------
 {
 	LPVOID lpMsgBuf;
 
@@ -57,7 +57,7 @@ CString GetErrorMessage(DWORD nErrorCode)
 					0,
 					NULL );
 
-	CString msg = (LPTSTR)lpMsgBuf;
+	std::string msg = (LPTSTR)lpMsgBuf;
 	LocalFree(lpMsgBuf);
 
 	return msg;

common/misc_util.h
@@ -141,7 +141,7 @@ inline void LimitMax(T& val, const C upperLimit)
  
 #ifdef MODPLUG_TRACKER
 LPCCH LoadResource(LPCTSTR lpName, LPCTSTR lpType, LPCCH& pData, size_t& nSize, HGLOBAL& hglob);
-CString GetErrorMessage(DWORD nErrorCode);
+std::string GetErrorMessage(DWORD nErrorCode);
 #endif // MODPLUG_TRACKER
 
 namespace utilImpl

mptrack/MIDIMapping.cpp
@@ -14,16 +14,15 @@
 #include "Mainfrm.h"
 
 
-CString CMIDIMappingDirective::ToString() const
-//---------------------------------------------
+mpt::String CMIDIMappingDirective::ToString() const
+//-------------------------------------------------
 {
-	CString str; str.Preallocate(20);
+	mpt::String str;
 	char flags[4] = "000";
 	if(m_Active) flags[0] = '1';
 	if(m_CaptureMIDI) flags[1] = '1';
 	if(m_AllowPatternEdit) flags[2] = '1';
 	str.Format("%s:%d:%x:%d:%d:%d", flags, (int)GetChannel(), (int)GetEvent(), (int)GetController(), (int)m_PluginIndex, m_Parameter);
-	str.Trim();
 	return str;
 }
 

mptrack/MIDIMapping.h
@@ -14,6 +14,8 @@
 #include <algorithm>
 using std::vector;
 
+namespace mpt { class String; }
+
 
 //=========================
 class CMIDIMappingDirective
@@ -57,7 +59,7 @@ public:
 
 	bool operator==(const CMIDIMappingDirective& d) const {return memcmp(this, &d, sizeof(CMIDIMappingDirective)) == 0;}
 
-	CString ToString() const;
+	mpt::String ToString() const;
 
 	BYTE GetChnEvent() const {return m_ChnEvent;}
 

mptrack/Vstplug.cpp
@@ -201,7 +201,7 @@ void CVstPluginManager::LoadPlugin(const char *pluginPath, AEffect *&effect, HIN
 			if(error != ERROR_MOD_NOT_FOUND)	// "File not found errors" are annoying.
 			{
 				TCHAR szBuf[256];
-				wsprintf(szBuf, "Warning: encountered problem when loading plugin dll. Error %d: %s", error, (LPCTSTR)GetErrorMessage(error));
+				wsprintf(szBuf, "Warning: encountered problem when loading plugin dll. Error %d: %s", error, GetErrorMessage(error).c_str());
 				Reporting::Error(szBuf, "DEBUG: Error when loading plugin dll");
 			}
 #endif //_DEBUG
@@ -3545,7 +3545,7 @@ AEffect *DmoToVst(VSTPluginLib *pLib)
 #endif // NO_VST
 
 
-CString SNDMIXPLUGIN::GetParamName(PlugParamIndex index) const
+mpt::String SNDMIXPLUGIN::GetParamName(PlugParamIndex index) const
 //------------------------------------------------------------
 {
 	CVstPlugin *vstPlug = dynamic_cast<CVstPlugin *>(pMixPlugin);
@@ -3554,6 +3554,6 @@ CString SNDMIXPLUGIN::GetParamName(PlugParamIndex index) const
 		return vstPlug->GetParamName(index);
 	} else
 	{
-		return CString();
+		return mpt::String();
 	}
 }

soundlib/Load_xm.cpp
@@ -557,7 +557,7 @@ bool CSoundFile::SaveXM(LPCSTR lpszFileName, bool compatibilityExport)
 	\
 	if((len > uint16_max - (UINT)x) && GetpModDoc()) /*Reaching the limits of file format?*/ \
 	{ \
- 		CString str; str.Format("%s (%s %u)\n", str_tooMuchPatternData, str_pattern, pat); \
+ 		mpt::String str; str.Format("%s (%s %u)\n", str_tooMuchPatternData, str_pattern, pat); \
 		GetpModDoc()->AddToLog(str); \
 		break; \
 	}

soundlib/Message.cpp
@@ -194,10 +194,10 @@ bool CSoundFile::ReadFixedLineLengthMessage(const BYTE *data, const size_t lengt
 // [in]  lineEnding: line ending formatting of the text in memory.
 // [in]  pTextConverter: Pointer to a callback function which can be used to post-process the written characters, if necessary (nullptr otherwise).
 // [out] returns formatted song message.
-CString CSoundFile::GetSongMessage(const enmLineEndings lineEnding, void (*pTextConverter)(char &)) const
+mpt::String CSoundFile::GetSongMessage(const enmLineEndings lineEnding, void (*pTextConverter)(char &)) const
 //-------------------------------------------------------------------------------------------------------
 {
-	CString comments;
+	mpt::String comments;
 
 	if(m_lpszSongComments == nullptr)
 	{
@@ -205,7 +205,7 @@ CString CSoundFile::GetSongMessage(const enmLineEndings lineEnding, void (*pText
 	}
 
 	const size_t len = strlen(m_lpszSongComments);
-	comments.Preallocate(len);
+	comments.Reserve(len);
 
 	for(size_t i = 0; i < len; i++)
 	{

commit 18a51bd254ade071a8ecd01a5c444e07b6396074

[Fix] Fixed a bug in setting pattern name (bug introduced in r1571).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@1582 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Snd_defs.h
@@ -420,8 +420,8 @@ namespace mpt
 		// Tests whether string is empty.
 		bool IsEmpty() const {return empty();}
 
-		// Compares this and other string.
-		bool Compare(const CharT* psz) const {return ((*this) == psz);}
+		// See std::string::compare.
+		int Compare(const CharT* psz) const {return compare(psz);}
 
 		// Equivalent to Empty(); Append(psz, nCount);
 		void SetString(const CharT* psz, const size_t nCount)