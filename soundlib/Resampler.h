b21c89699c089f4fef9a4dbfa9726979bfb61e0d
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-04-09 18:13:33 +0000

    [Ref] Only regenerate resampler tables when the settings actually changed.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/manx/nonglobal-mixer@1800 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Resampler.h
+++ soundlib/Resampler.h
@@ -32,6 +32,11 @@ public:
 		gdWFIRCutoff = 0.97;
 		gbWFIRType = 7; //WFIR_KAISER4T;
 	}
+	bool operator == (const CResamplerSettings &cmp) const
+	{
+		return SrcMode == cmp.SrcMode && gdWFIRCutoff == cmp.gdWFIRCutoff && gbWFIRType == cmp.gbWFIRType;
+	}
+	bool operator != (const CResamplerSettings &cmp) const { return !(*this == cmp); }
 };
 
 
@@ -45,10 +50,12 @@ public:
 	short int gKaiserSinc[SINC_PHASES*8];		// Upsampling
 	short int gDownsample13x[SINC_PHASES*8];	// Downsample 1.333x
 	short int gDownsample2x[SINC_PHASES*8];		// Downsample 2x
+private:
+	CResamplerSettings m_OldSettings;
 public:
-	CResampler() {}
+	CResampler() { InitializeTables(true); }
 	~CResampler() {}
-	void InitializeTables();
+	void InitializeTables(bool force=false);
 	bool IsHQ() const { return m_Settings.SrcMode >= SRCMODE_SPLINE && m_Settings.SrcMode < SRCMODE_DEFAULT; }
 	bool IsUltraHQ() const { return m_Settings.SrcMode >= SRCMODE_POLYPHASE && m_Settings.SrcMode < SRCMODE_DEFAULT; }
 	bool Is(ResamplingMode cmp) const { return m_Settings.SrcMode == cmp; }
