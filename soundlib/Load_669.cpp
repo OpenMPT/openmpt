OpenMPT/openmpt

commit 263b47575d448221f49af2bec236cecf1a231f4b

[Ref] Add compile-time assertions for the size of all on-disk structures. [Ref] Tag all on-disk structures with PACKED attribute, and use #pragma pack only when NEEDS_PRAGMA_PACK is defined for the current compiler in typedefs.h .
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@1895 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_669.cpp
@@ -13,9 +13,11 @@
 #include "stdafx.h"
 #include "Loaders.h"
 
+#ifdef NEEDS_PRAGMA_PACK
 #pragma pack(push, 1)
+#endif
 
-struct _669FileHeader
+struct PACKED _669FileHeader
 {
 	enum MagicBytes
 	{
@@ -42,7 +44,7 @@ struct _669FileHeader
 STATIC_ASSERT(sizeof(_669FileHeader) == 497);
 
 
-struct _669Sample
+struct PACKED _669Sample
 {
 	char   filename[13];
 	uint32 length;
@@ -80,7 +82,9 @@ struct _669Sample
 
 STATIC_ASSERT(sizeof(_669Sample) == 25);
 
+#ifdef USER_PRAGMA_PACK
 #pragma pack(pop)
+#endif
 
 
 bool CSoundFile::Read669(FileReader &file)

common/typedefs.h
@@ -33,8 +33,8 @@
 #endif
 
 #if defined(_MSC_VER)
-#define USE_PRAGMA_PACK
 #define PACKED __declspec(align(1))
+#define NEEDS_PRAGMA_PACK
 #elif defined(__GNUC__)
 #define PACKED __attribute__((packed))) __attribute__((aligned(1))))
 #endif

mptrack/TrackerSettings.h
@@ -118,12 +118,19 @@ enum
 
 
 // EQ
-struct EQPreset
+#ifdef NEEDS_PRAGMA_PACK
+#pragma pack(push, 1)
+#endif
+struct PACKED EQPreset
 {
 	char szName[12];
 	UINT Gains[MAX_EQ_BANDS];
 	UINT Freqs[MAX_EQ_BANDS];
 };
+#ifdef NEEDS_PRAGMA_PACK
+#pragma pack(pop)
+#endif
+STATIC_ASSERT(sizeof(EQPreset) == 60);
 
 
 // Chords

mptrack/mod2midi.cpp
@@ -15,9 +15,11 @@
 #include "mod2midi.h"
 #include "Wav.h"
 
+#ifdef NEEDS_PRAGMA_PACK
 #pragma pack(push, 1)
+#endif
 
-typedef struct _RMIDDATACHUNK
+typedef struct PACKED _RMIDDATACHUNK
 {
 	DWORD id_RIFF;	// "RIFF"
 	DWORD filelen;
@@ -26,7 +28,9 @@ typedef struct _RMIDDATACHUNK
 	DWORD datalen;
 } RMIDDATACHUNK, *PRMIDDATACHUNK;
 
-typedef struct _MTHDCHUNK	// (big endian)
+STATIC_ASSERT(sizeof(RMIDDATACHUNK) == 20);
+
+typedef struct PACKED _MTHDCHUNK	// (big endian)
 {
 	DWORD id;		// "MThd" = 0x6468544D
 	DWORD len;		// 6
@@ -35,11 +39,19 @@ typedef struct _MTHDCHUNK	// (big endian)
 	WORD wDivision;	// PPQN
 } MTHDCHUNK, *PMTHDCHUNK;
 
-typedef struct _MTRKCHUNK	// (big endian)
+STATIC_ASSERT(sizeof(MTHDCHUNK) == 14);
+
+typedef struct PACKED _MTRKCHUNK	// (big endian)
 {
 	DWORD id;		// "MTrk" = 0x6B72544D
 	DWORD len;
 } MTRKCHUNK, *PMTRKCHUNK;
+
+STATIC_ASSERT(sizeof(MTRKCHUNK) == 8);
+
+#ifdef NEEDS_PRAGMA_PACK
+#pragma pack(pop)
+#endif
 
 typedef struct _DYNMIDITRACK
 {
@@ -55,8 +67,6 @@ typedef struct _DYNMIDITRACK
 	void Write(const void *, unsigned long nBytes);
 	void WriteLen(unsigned long len);
 } DYNMIDITRACK, *PDYNMIDITRACK;
-
-#pragma pack(pop)
 
 
 void DYNMIDITRACK::Write(const void *pBuffer, unsigned long nBytes)

mptrack/tagging.h
@@ -18,9 +18,11 @@ using std::string;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // ID3v2.4 Tags
 
+#ifdef NEEDS_PRAGMA_PACK
 #pragma pack(push, 1)
+#endif
 
-struct ID3v2Header
+struct PACKED ID3v2Header
 {
 	uint8 signature[3];
 	uint8 version[2];
@@ -29,7 +31,9 @@ struct ID3v2Header
 	// Total: 10 bytes
 };
 
-struct ID3v2Frame
+STATIC_ASSERT(sizeof(ID3v2Header) == 10);
+
+struct PACKED ID3v2Frame
 {
 	uint32 frameid;
 	uint32 size;
@@ -37,7 +41,11 @@ struct ID3v2Frame
 	// Total: 10 bytes
 };
 
+STATIC_ASSERT(sizeof(ID3v2Frame) == 10);
+
+#ifdef NEEDS_PRAGMA_PACK
 #pragma pack(pop)
+#endif
 
 // we will add some padding bytes to our id3v2 tag (extending tags will be easier this way)
 #define ID3v2_PADDING 512

sounddsp/EQ.cpp
@@ -43,24 +43,24 @@ static const UINT gEqLinearToDB[33] =
 };
 
 
-static const REAL f2ic = (REAL)(1 << 28);
-static const REAL i2fc = (REAL)(1.0 / (1 << 28));
+static const float32 f2ic = (float32)(1 << 28);
+static const float32 i2fc = (float32)(1.0 / (1 << 28));
 
 static const EQBANDSTRUCT gEQDefaults[MAX_EQ_BANDS*2] =
 {
 	// Default: Flat EQ
-	{0,0,0,0,0, 0,0,0,0, 1,   120, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,   600, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,  1200, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,  3000, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,  6000, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1, 10000, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,   120, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,   600, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,  1200, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,  3000, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1,  6000, FALSE},
-	{0,0,0,0,0, 0,0,0,0, 1, 10000, FALSE},
+	{0,0,0,0,0, 0,0,0,0, 1,   120, false},
+	{0,0,0,0,0, 0,0,0,0, 1,   600, false},
+	{0,0,0,0,0, 0,0,0,0, 1,  1200, false},
+	{0,0,0,0,0, 0,0,0,0, 1,  3000, false},
+	{0,0,0,0,0, 0,0,0,0, 1,  6000, false},
+	{0,0,0,0,0, 0,0,0,0, 1, 10000, false},
+	{0,0,0,0,0, 0,0,0,0, 1,   120, false},
+	{0,0,0,0,0, 0,0,0,0, 1,   600, false},
+	{0,0,0,0,0, 0,0,0,0, 1,  1200, false},
+	{0,0,0,0,0, 0,0,0,0, 1,  3000, false},
+	{0,0,0,0,0, 0,0,0,0, 1,  6000, false},
+	{0,0,0,0,0, 0,0,0,0, 1, 10000, false},
 };
 
 #ifdef ENABLE_X86
@@ -77,8 +77,8 @@ static const EQBANDSTRUCT gEQDefaults[MAX_EQ_BANDS*2] =
 #define PBS_Y1	DWORD PTR [eax+28]
 #define PBS_Y2	DWORD PTR [eax+32]
 
-static void EQFilter(EQBANDSTRUCT *pbs, REAL *pbuffer, UINT nCount)
-//-------------------------------------------------------------------------
+static void EQFilter(EQBANDSTRUCT *pbs, float32 *pbuffer, UINT nCount)
+//--------------------------------------------------------------------
 {
 	_asm {
 	mov eax, pbs		// eax = pbs
@@ -124,8 +124,8 @@ EQ_Loop:
 }
 
 
-static void AMD_StereoEQ(EQBANDSTRUCT *pbl, EQBANDSTRUCT *pbr, REAL *pbuffer, UINT nCount)
-//----------------------------------------------------------------------------------------
+static void AMD_StereoEQ(EQBANDSTRUCT *pbl, EQBANDSTRUCT *pbr, float32 *pbuffer, UINT nCount)
+//-------------------------------------------------------------------------------------------
 {
 #ifdef ENABLE_3DNOW
 	float tmp[16];
@@ -208,8 +208,8 @@ mainloop:
 }
 
 
-static void SSE_StereoEQ(EQBANDSTRUCT *pbl, EQBANDSTRUCT *pbr, REAL *pbuffer, UINT nCount)
-//----------------------------------------------------------------------------------------
+static void SSE_StereoEQ(EQBANDSTRUCT *pbl, EQBANDSTRUCT *pbr, float32 *pbuffer, UINT nCount)
+//-------------------------------------------------------------------------------------------
 {
 #ifdef ENABLE_SSE
 	static const float gk1 = 1.0f;
@@ -306,13 +306,13 @@ done:;
 
 #else
 
-static void EQFilter(EQBANDSTRUCT *pbs, REAL *pbuffer, UINT nCount)
-//-----------------------------------------------------------------
+static void EQFilter(EQBANDSTRUCT *pbs, float32 *pbuffer, UINT nCount)
+//--------------------------------------------------------------------
 {
 	for (UINT i=0; i<nCount; i++)
 	{
-		REAL x = pbuffer[i];
-		REAL y = pbs->a1 * pbs->x1 + pbs->a2 * pbs->x2 + pbs->a0 * x + pbs->b1 * pbs->y1 + pbs->b2 * pbs->y2;
+		float32 x = pbuffer[i];
+		float32 y = pbs->a1 * pbs->x1 + pbs->a2 * pbs->x2 + pbs->a0 * x + pbs->b1 * pbs->y1 + pbs->b2 * pbs->y2;
 		pbs->x2 = pbs->x1;
 		pbs->y2 = pbs->y1;
 		pbs->x1 = x;
@@ -407,6 +407,10 @@ void CEQ::ProcessStereo(int *pbuffer, float *MixFloatBuffer, UINT nCount, CSound
 CEQ::CEQ()
 //--------
 {
+	#if defined(ENABLE_MMX) || defined(ENABLE_SSE)
+		ALWAYS_ASSERT(((uintptr_t)&(gEQ[0])) % 4 == 0);
+		ALWAYS_ASSERT(((uintptr_t)&(gEQ[1])) % 4 == 0);
+	#endif
 	memcpy(gEQ, gEQDefaults, sizeof(gEQ));
 }
 
@@ -414,12 +418,12 @@ CEQ::CEQ()
 void CEQ::Initialize(BOOL bReset, DWORD MixingFreq)
 //-------------------------------------------------
 {
-	REAL fMixingFreq = (REAL)MixingFreq;
+	float32 fMixingFreq = (float32)MixingFreq;
 	// Gain = 0.5 (-6dB) .. 2 (+6dB)
 	for (UINT band=0; band<MAX_EQ_BANDS*2; band++) if (gEQ[band].bEnable)
 	{
-		REAL k, k2, r, f;
-		REAL v0, v1;
+		float32 k, k2, r, f;
+		float32 v0, v1;
 		BOOL b = bReset;
 
 		f = gEQ[band].CenterFrequency / fMixingFreq;
@@ -428,7 +432,7 @@ void CEQ::Initialize(BOOL bReset, DWORD MixingFreq)
 		// k = tan(PI*f);
 		k = f * 3.141592654f;
 		k = k + k*f;
-//		if (k > (REAL)0.707) k = (REAL)0.707;
+//		if (k > (float32)0.707) k = (float32)0.707;
 		k2 = k*k;
 		v0 = gEQ[band].Gain;
 		v1 = 1;
@@ -445,31 +449,31 @@ void CEQ::Initialize(BOOL bReset, DWORD MixingFreq)
 		if (r != gEQ[band].a0)
 		{
 			gEQ[band].a0 = r;
-			b = TRUE;
+			b = true;
 		}
 		r = 2 * (k2 - 1) / (1 + v1*k + k2);
 		if (r != gEQ[band].a1)
 		{
 			gEQ[band].a1 = r;
-			b = TRUE;
+			b = true;
 		}
 		r = (1 - v0*k + k2) / (1 + v1*k + k2);
 		if (r != gEQ[band].a2)
 		{
 			gEQ[band].a2 = r;
-			b = TRUE;
+			b = true;
 		}
 		r = - 2 * (k2 - 1) / (1 + v1*k + k2);
 		if (r != gEQ[band].b1)
 		{
 			gEQ[band].b1 = r;
-			b = TRUE;
+			b = true;
 		}
 		r = - (1 - v1*k + k2) / (1 + v1*k + k2);
 		if (r != gEQ[band].b2)
 		{
 			gEQ[band].b2 = r;
-			b = TRUE;
+			b = true;
 		}
 		if (b)
 		{
@@ -498,13 +502,13 @@ void CEQ::SetEQGains(const UINT *pGains, UINT nGains, const UINT *pFreqs, BOOL b
 {
 	for (UINT i=0; i<MAX_EQ_BANDS; i++)
 	{
-		REAL g, f = 0;
+		float32 g, f = 0;
 		if (i < nGains)
 		{
 			UINT n = pGains[i];
 			if (n > 32) n = 32;
-			g = ((REAL)gEqLinearToDB[n]) / 64.0f;
-			if (pFreqs) f = (REAL)(int)pFreqs[i];
+			g = ((float32)gEqLinearToDB[n]) / 64.0f;
+			if (pFreqs) f = (float32)(int)pFreqs[i];
 		} else
 		{
 			g = 1;
@@ -515,12 +519,12 @@ void CEQ::SetEQGains(const UINT *pGains, UINT nGains, const UINT *pFreqs, BOOL b
 		gEQ[i+MAX_EQ_BANDS].CenterFrequency = f;
 		if (f > 20.0f)
 		{
-			gEQ[i].bEnable = TRUE;
-			gEQ[i+MAX_EQ_BANDS].bEnable = TRUE;
+			gEQ[i].bEnable = true;
+			gEQ[i+MAX_EQ_BANDS].bEnable = true;
 		} else
 		{
-			gEQ[i].bEnable = FALSE;
-			gEQ[i+MAX_EQ_BANDS].bEnable = FALSE;
+			gEQ[i].bEnable = false;
+			gEQ[i+MAX_EQ_BANDS].bEnable = false;
 		}
 	}
 	Initialize(bReset, MixingFreq);

sounddsp/EQ.h
@@ -15,19 +15,23 @@
 #include "../soundlib/SoundFilePlayConfig.h"
 
 
-#define REAL			float
 #define MAX_EQ_BANDS	6
 
-#pragma pack(push, 4)
-typedef struct _EQBANDSTRUCT
+typedef struct ALIGN(4) _EQBANDSTRUCT
 {
-	REAL a0, a1, a2, b1, b2;
-	REAL x1, x2, y1, y2;
-	REAL Gain, CenterFrequency;
-	BOOL bEnable;
-} EQBANDSTRUCT, *PEQBANDSTRUCT;
-#pragma pack(pop)
-
+	float32 a0;
+	float32 a1;
+	float32 a2;
+	float32 b1;
+	float32 b2;
+	float32 x1;
+	float32 x2;
+	float32 y1;
+	float32 y2;
+	float32 Gain;
+	float32 CenterFrequency;
+	bool bEnable;
+} EQBANDSTRUCT;
 
 //=======
 class CEQ

soundlib/Dlsbank.cpp
@@ -108,7 +108,9 @@
 #define ART_DEFAULTPAN		MAKE_ART	(CONN_SRC_NONE,	CONN_SRC_NONE,	CONN_DST_PAN)
 
 
+#ifdef NEEDS_PRAGMA_PACK
 #pragma pack(push, 1)
+#endif
 
 //////////////////////////////////////////////////////////
 // DLS IFF Chunk IDs
@@ -134,47 +136,59 @@
 //////////////////////////////////////////////////////////
 // DLS Structures definitions
 
-typedef struct IFFCHUNK
+typedef struct PACKED IFFCHUNK
 {
 	DWORD id;
 	DWORD len;
 } IFFCHUNK, *LPIFFCHUNK;
 
-typedef struct RIFFCHUNKID
+STATIC_ASSERT(sizeof(IFFCHUNK) == 8);
+
+typedef struct PACKED RIFFCHUNKID
 {
 	DWORD id_RIFF;
 	DWORD riff_len;
 	DWORD id_DLS;
 } RIFFCHUNKID;
 
-typedef struct LISTCHUNK
+STATIC_ASSERT(sizeof(RIFFCHUNKID) == 12);
+
+typedef struct PACKED LISTCHUNK
 {
 	DWORD id;
 	DWORD len;
 	DWORD listid;
 } LISTCHUNK;
 
-typedef struct DLSRGNRANGE
+STATIC_ASSERT(sizeof(LISTCHUNK) == 12);
+
+typedef struct PACKED DLSRGNRANGE
 {
 	WORD usLow;
 	WORD usHigh;
 } DLSRGNRANGE;
 
-typedef struct COLHCHUNK
+STATIC_ASSERT(sizeof(DLSRGNRANGE) == 4);
+
+typedef struct PACKED COLHCHUNK
 {
 	DWORD id;
 	DWORD len;
 	DWORD ulInstruments;
 } COLHCHUNK;
 
-typedef struct VERSCHUNK
+STATIC_ASSERT(sizeof(COLHCHUNK) == 12);
+
+typedef struct PACKED VERSCHUNK
 {
 	DWORD id;
 	DWORD len;
 	WORD version[4];
 } VERSCHUNK;
 
-typedef struct PTBLCHUNK
+STATIC_ASSERT(sizeof(VERSCHUNK) == 16);
+
+typedef struct PACKED PTBLCHUNK
 {
 	DWORD id;
 	DWORD len;
@@ -183,7 +197,9 @@ typedef struct PTBLCHUNK
 	DWORD ulOffsets[1];
 } PTBLCHUNK;
 
-typedef struct INSHCHUNK
+STATIC_ASSERT(sizeof(PTBLCHUNK) == 20);
+
+typedef struct PACKED INSHCHUNK
 {
 	DWORD id;
 	DWORD len;
@@ -192,7 +208,9 @@ typedef struct INSHCHUNK
 	DWORD ulInstrument;
 } INSHCHUNK;
 
-typedef struct RGNHCHUNK
+STATIC_ASSERT(sizeof(INSHCHUNK) == 20);
+
+typedef struct PACKED RGNHCHUNK
 {
 	DWORD id;
 	DWORD len;
@@ -202,7 +220,9 @@ typedef struct RGNHCHUNK
 	WORD usKeyGroup;
 } RGNHCHUNK;
 
-typedef struct WLNKCHUNK
+STATIC_ASSERT(sizeof(RGNHCHUNK) == 20);
+
+typedef struct PACKED WLNKCHUNK
 {
 	DWORD id;
 	DWORD len;
@@ -212,7 +232,9 @@ typedef struct WLNKCHUNK
 	DWORD ulTableIndex;
 } WLNKCHUNK;
 
-typedef struct ART1CHUNK
+STATIC_ASSERT(sizeof(WLNKCHUNK) == 20);
+
+typedef struct PACKED ART1CHUNK
 {
 	DWORD id;
 	DWORD len;
@@ -220,7 +242,9 @@ typedef struct ART1CHUNK
 	DWORD cConnectionBlocks;
 } ART1CHUNK;
 
-typedef struct CONNECTIONBLOCK
+STATIC_ASSERT(sizeof(ART1CHUNK) == 16);
+
+typedef struct PACKED CONNECTIONBLOCK
 {
 	WORD usSource;
 	WORD usControl;
@@ -229,25 +253,31 @@ typedef struct CONNECTIONBLOCK
 	LONG lScale;
 } CONNECTIONBLOCK;
 
-typedef struct WSMPCHUNK
+STATIC_ASSERT(sizeof(CONNECTIONBLOCK) == 12);
+
+typedef struct PACKED WSMPCHUNK
 {
 	DWORD id;
 	DWORD len;
 	DWORD cbSize;
 	WORD usUnityNote;
-	signed short sFineTune;
+	SHORT sFineTune;
 	LONG lAttenuation;
 	DWORD fulOptions;
 	DWORD cSampleLoops;
 } WSMPCHUNK;
 
-typedef struct WSMPSAMPLELOOP
+STATIC_ASSERT(sizeof(WSMPCHUNK) == 28);
+
+typedef struct PACKED WSMPSAMPLELOOP
 {
 	DWORD cbSize;
 	DWORD ulLoopType;
 	DWORD ulLoopStart;
 	DWORD ulLoopLength;
 } WSMPSAMPLELOOP;
+
+STATIC_ASSERT(sizeof(WSMPSAMPLELOOP) == 16);
 
 
 /////////////////////////////////////////////////////////////////////
@@ -285,7 +315,7 @@ typedef struct WSMPSAMPLELOOP
 /////////////////////////////////////////////////////////////////////
 // SF2 Structures Definitions
 
-typedef struct SFPRESETHEADER
+typedef struct PACKED SFPRESETHEADER
 {
 	CHAR achPresetName[20];
 	WORD wPreset;
@@ -296,37 +326,49 @@ typedef struct SFPRESETHEADER
 	DWORD dwMorphology;
 } SFPRESETHEADER;
 
-typedef struct SFPRESETBAG
+STATIC_ASSERT(sizeof(SFPRESETHEADER) == 38);
+
+typedef struct PACKED SFPRESETBAG
 {
 	WORD wGenNdx;
 	WORD wModNdx;
 } SFPRESETBAG;
 
-typedef struct SFGENLIST
+STATIC_ASSERT(sizeof(SFPRESETBAG) == 4);
+
+typedef struct PACKED SFGENLIST
 {
 	WORD sfGenOper;
 	WORD genAmount;
 } SFGENLIST;
 
-typedef struct SFINST
+STATIC_ASSERT(sizeof(SFGENLIST) == 4);
+
+typedef struct PACKED SFINST
 {
 	CHAR achInstName[20];
 	WORD wInstBagNdx;
 } SFINST;
 
-typedef struct SFINSTBAG
+STATIC_ASSERT(sizeof(SFINST) == 22);
+
+typedef struct PACKED SFINSTBAG
 {
 	WORD wGenNdx;
 	WORD wModNdx;
 } SFINSTBAG;
 
-typedef struct SFINSTGENLIST
+STATIC_ASSERT(sizeof(SFINSTBAG) == 4);
+
+typedef struct PACKED SFINSTGENLIST
 {
 	WORD sfGenOper;
 	WORD genAmount;
 } SFINSTGENLIST;
 
-typedef struct SFSAMPLE
+STATIC_ASSERT(sizeof(SFINSTGENLIST) == 4);
+
+typedef struct PACKED SFSAMPLE
 {
 	CHAR achSampleName[20];
 	DWORD dwStart;
@@ -339,6 +381,15 @@ typedef struct SFSAMPLE
 	WORD wSampleLink;
 	WORD sfSampleType;
 } SFSAMPLE;
+
+STATIC_ASSERT(sizeof(SFSAMPLE) == 46);
+
+// End of structures definitions
+/////////////////////////////////////////////////////////////////////
+
+#ifdef NEEDS_PRAGMA_PACK
+#pragma pack(pop)
+#endif
 
 
 typedef struct SF2LOADERINFO
@@ -355,11 +406,6 @@ typedef struct SF2LOADERINFO
 	SFINSTGENLIST *pInstGens;
 } SF2LOADERINFO;
 
-
-// End of structures definitions
-/////////////////////////////////////////////////////////////////////
-
-#pragma pack(pop)
 
 /////////////////////////////////////////////////////////////////////
 // Unit conversion

commit 062985d1a6c330430fade36b2bc795fc2abd31fb

[Fix] Fix typo introduced in r1895.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@1896 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_669.cpp
@@ -82,7 +82,7 @@ struct PACKED _669Sample
 
 STATIC_ASSERT(sizeof(_669Sample) == 25);
 
-#ifdef USER_PRAGMA_PACK
+#ifdef NEEDS_PRAGMA_PACK
 #pragma pack(pop)
 #endif
 