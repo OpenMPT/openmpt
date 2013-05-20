92cefb006880c086901b9a493e8933c4a7a98dd9
Johannes Schultz <saga-games@users.sourceforge.net>
2013-05-20 17:10:12 +0000

    [Fix] Compile error.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2150 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/View_smp.cpp
+++ mptrack/View_smp.cpp
@@ -2246,7 +2246,7 @@ void CViewSample::PlayNote(UINT note, const uint32 nStartPos)
 
 			CSoundFile &sndFile = pModDoc->GetrSoundFile();
 			ModSample &sample = sndFile.GetSample(m_nSample);
-			uint32 freq = sndFile.GetFreqFromPeriod(pSndFile->GetPeriodFromNote(note + (pSndFile->GetType() == MOD_TYPE_XM ? sample.RelativeTone : 0), sample.nFineTune, sample.nC5Speed), sample.nC5Speed, 0);
+			uint32 freq = sndFile.GetFreqFromPeriod(sndFile.GetPeriodFromNote(note + (sndFile.GetType() == MOD_TYPE_XM ? sample.RelativeTone : 0), sample.nFineTune, sample.nC5Speed), sample.nC5Speed, 0);
 
 			CHAR s[32];
 			wsprintf(s, "%s (%d.%d Hz)", szDefaultNoteNames[note - 1], freq >> FREQ_FRACBITS, Util::muldiv(freq & ((1 << FREQ_FRACBITS) - 1), 100, 1 << FREQ_FRACBITS));
