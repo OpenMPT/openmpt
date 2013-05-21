bc7bd5a11d97cd79bb0d35114ff768bd0bc5b93f
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-05-21 14:46:11 +0000

    [Mod] Change unit of MASTERGAIN render_param in libopenmpt from decibel to millibel. [Ref] Clarify render_param names in libeopenmpt API. [Ref] Cleanup libopenmpt_settings. Remove libopenmpt_settings_dialog.*. [Mod] Properly CamelCase registry keys for in_openmpt and xmp-openmpt. [Fix] class openmpt::exception_message has to be exported from shared libraries so that type_info lookup does not crash.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2160 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_modplug.c
+++ libopenmpt/libopenmpt_modplug.c
@@ -329,14 +329,14 @@ LIBOPENMPT_MODPLUG_API unsigned int ModPlug_GetMasterVolume(ModPlugFile* file)
 	int32_t val;
 	if(!file) return 0;
 	val = 0;
-	if(!openmpt_module_get_render_param(file->mod,OPENMPT_MODULE_RENDER_MASTERGAIN_DB,&val)) return 128;
-	return (unsigned int)(128.0*pow(10.0,val*0.05));
+	if(!openmpt_module_get_render_param(file->mod,OPENMPT_MODULE_RENDER_MASTERGAIN_MILLIBEL,&val)) return 128;
+	return (unsigned int)(128.0*pow(10.0,val*0.0005));
 }
 
 LIBOPENMPT_MODPLUG_API void ModPlug_SetMasterVolume(ModPlugFile* file,unsigned int cvol)
 {
 	if(!file) return;
-	openmpt_module_set_render_param(file->mod,OPENMPT_MODULE_RENDER_MASTERGAIN_DB,(int32_t)(20.0*log10(cvol/128.0)));
+	openmpt_module_set_render_param(file->mod,OPENMPT_MODULE_RENDER_MASTERGAIN_MILLIBEL,(int32_t)(2000.0*log10(cvol/128.0)));
 }
 
 LIBOPENMPT_MODPLUG_API int ModPlug_GetCurrentSpeed(ModPlugFile* file)
