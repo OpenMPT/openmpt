OpenMPT/openmpt

commit a1b5ec7a027a57fb7470ea16cd8e48365865327c

[Ref] Rename mpt::String to mpt::string. [Ref] Shrink mpt::string to the needed bare minimum (operator const char*()). Move Format function out of the class and make it return a std::string. [Ref] When not building MODPLUG_TRACKER, mpt::string can now be a simple typedef of std::string. [Ref] Move function in mptString.h into namespace mpt::String. [Ref] Reduce mpt::string usage all over the place down to just 2 usage sites. Those 2 would require adding a ton of .c_str() in mptrack/ to remove. Leave that for later.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2162 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptString.cpp
@@ -14,25 +14,22 @@
 #include <cstdarg>
 
 
-namespace mpt {
+namespace mpt { namespace String {
 
-// Formats this string, like CString::Format.
-void String::Format(const CharT* pszFormat, ...)
+
+std::string Format(const char *format, ...)
 {
 	#if MPT_COMPILER_MSVC
 		va_list argList;
-		va_start( argList, pszFormat );
+		va_start(argList, format);
 
 		// Count the needed array size.
-		const size_t nCount = _vscprintf(pszFormat, argList); // null character not included.
-		resize(nCount + 1); // + 1 is for null terminator.
+		const size_t nCount = _vscprintf(format, argList); // null character not included.
+		std::vector<char> buf(nCount + 1); // + 1 is for null terminator.
+		vsprintf_s(&(buf[0]), buf.size(), format, argList);
 
-		// Hack: directly modify the std::string's string.
-		// In C++11 std::string is guaranteed to be contiguous.
-		const int nCount2 = vsprintf_s(&*begin(), size(), pszFormat, argList);
-		resize(nCount2); // Removes the null character that vsprintf_s adds.
-
-		va_end( argList );
+		va_end(argList);
+		return &(buf[0]);
 	#else
 		va_list argList;
 		va_start(argList, pszFormat);
@@ -42,34 +39,9 @@ void String::Format(const CharT* pszFormat, ...)
 		va_start(argList, pszFormat);
 		vsnprintf(&(temp[0]), size + 1, pszFormat, argList);
 		va_end(argList);
-		assign(&(temp[0]));
+		return &(temp[0]);
 	#endif
 }
 
 
-// Remove whitespaces at start of string
-void String::LTrim()
-{
-	size_type pos = find_first_not_of(" \n\r\t");
-	if(pos != npos)
-		erase(begin(), begin() + pos);
-}
-
-
-// Remove whitespaces at end of string
-void String::RTrim()
-{
-	size_type pos = find_last_not_of(" \n\r\t");
-	if(pos != npos)
-		erase(begin() + pos + 1, end());
-}
-
-
-// Remove whitespaces at start and end of string
-void String::Trim()
-{
-	LTrim();
-	RTrim();
-}
-
-} // namespace mpt
+} } // namespace mpt::String

common/mptString.h
@@ -23,71 +23,68 @@
 #define MPT_PRINTF_FUNC(formatstringindex,varargsindex)
 #endif
 
+
 namespace mpt
 {
-	// Quick and incomplete string class to be used as a replacement for
-	// CString and std::string in soundlib.
-	class String : public std::string
+
+
+#ifdef MODPLUG_TRACKER
+	// std::string compatible class that is convertible to const char* (and thus CString).
+	// This eases interfacing CSoundFile from MFC code in some cases.
+	// The goal is to remove this sometime in the future.
+	class string : public std::string
 	{
 	public:
-		typedef std::string BaseClass;
-		typedef char CharT;
-
-		String() {}
-		String(const CharT* psz) : BaseClass(psz) {}
-		String(const std::string& other) : BaseClass(other) {}
-
-		// Move constructors and move assignments.
-	#if defined(MPT_COMPILER_HAS_RVALUE_REF)
-		String(BaseClass&& str) : BaseClass(std::move(str)) {}
-		String(String&& other) : BaseClass(std::move(static_cast<BaseClass&>(other))) {}
-		String& operator=(BaseClass&& str) {BaseClass::operator=(std::move(str)); return *this;}
-		String& operator=(String&& other) {BaseClass::operator=(std::move(static_cast<BaseClass&>(other))); return *this;}
-	#endif
-
-		String& operator=(const CharT* psz) {BaseClass::operator=(psz); return *this;}
-
-	#ifdef _MFC_VER
-		String(const CString& str) {(*this) = str.GetString();}
-		String& operator=(const CString& str) {return operator=(str.GetString());}
-	#endif // _MFC_VER
-
-		// To allow easy assignment to CString in GUI side.
-		operator const CharT*() const {return c_str();}
-
-		// Set the string to psz, copy at most nCount characters
-		void SetString(const CharT* psz, const size_t nCount)
-		{
-			clear();
-			Append(psz, nCount);
-		}
-
-		// Appends given string to this. Stops after 'nCount' chars if null terminator 
-		// hasn't been encountered before that.
-		void Append(const CharT* psz, const size_t nCount) {append(psz, nCount);}
-
-		// Difference between Append and AppendChars is that latter can be used to add 
-		// potentially non-null terminated char array.
-		template <size_t N>
-		void AppendChars(const CharT (&arr)[N])
-		{
-			append(arr, arr + N);
-		}
-
-		// Formats this string, like CString::Format.
-		void MPT_PRINTF_FUNC(2,3) Format(const CharT* pszFormat, ...);
-
-		// Remove whitespaces at start of string
-		void LTrim();
-		// Remove whitespaces at end of string
-		void RTrim();
-		// Remove whitespaces at start and end of string
-		void Trim();
+		string() {}
+		string(const char *psz) : std::string(psz) {}
+		string(const std::string &other) : std::string(other) {}
+		string& operator=(const char *psz) { std::string::operator=(psz); return *this; }
+		operator const char *() const { return c_str(); } // To allow easy assignment to CString in GUI side.
 	};
+#else
+	typedef std::string string;
+#endif
+
+
+namespace String
+{
+
+
+std::string MPT_PRINTF_FUNC(1,2) Format(const char * format, ...);
+
+
+// Remove whitespace at start of string
+static inline std::string LTrim(std::string str, const std::string &whitespace = " \n\r\t")
+{
+	std::string::size_type pos = str.find_first_not_of(whitespace);
+	if(pos != std::string::npos)
+	{
+		str.erase(str.begin(), str.begin() + pos);
+	}
+	return str;
 }
 
 
-inline std::string string_replace(std::string str, const std::string &oldStr, const std::string &newStr)
+// Remove whitespace at end of string
+static inline std::string RTrim(std::string str, const std::string &whitespace = " \n\r\t")
+{
+	std::string::size_type pos = str.find_last_not_of(whitespace);
+	if(pos != std::string::npos)
+	{
+		str.erase(str.begin() + pos + 1, str.end());
+	}
+	return str;
+}
+
+
+// Remove whitespace at start and end of string
+static inline std::string Trim(std::string str, const std::string &whitespace = " \n\r\t")
+{
+	return RTrim(LTrim(str, whitespace), whitespace);
+}
+
+
+static inline std::string Replace(std::string str, const std::string &oldStr, const std::string &newStr)
 {
 	std::size_t pos = 0;
 	while((pos = str.find(oldStr, pos)) != std::string::npos)
@@ -97,6 +94,12 @@ inline std::string string_replace(std::string str, const std::string &oldStr, co
 	}
 	return str;
 }
+
+
+} // namespace String
+
+
+} // namespace mpt
 
 
 static inline int mpt_strnicmp(const char *a, const char *b, size_t count)

common/version.cpp
@@ -87,30 +87,29 @@ std::string GetOpenMPTVersionStr()
 	return std::string("OpenMPT ") + std::string(MPT_VERSION_STR);
 }
 
-VersionNum ToNum(const char *const s)
+VersionNum ToNum(const std::string &s_)
 {
+	const char *s = s_.c_str();
 	int v1, v2, v3, v4; 
 	sscanf(s, "%x.%x.%x.%x", &v1, &v2, &v3, &v4);
 	return ((v1 << 24) |  (v2 << 16) | (v3 << 8) | v4);
 }
 
-mpt::String ToStr(const VersionNum v)
+std::string ToStr(const VersionNum v)
 {
-	mpt::String strVersion;
 	if(v == 0)
 	{
 		// Unknown version
-		strVersion = "Unknown";
+		return "Unknown";
 	} else if((v & 0xFFFF) == 0)
 	{
 		// Only parts of the version number are known (e.g. when reading the version from the IT or S3M file header)
-		strVersion.Format("%X.%02X", (v >> 24) & 0xFF, (v >> 16) & 0xFF);
+		return mpt::String::Format("%X.%02X", (v >> 24) & 0xFF, (v >> 16) & 0xFF);
 	} else
 	{
 		// Full version info available
-		strVersion.Format("%X.%02X.%02X.%02X", (v >> 24) & 0xFF, (v >> 16) & 0xFF, (v >> 8) & 0xFF, (v) & 0xFF);
+		return mpt::String::Format("%X.%02X.%02X.%02X", (v >> 24) & 0xFF, (v >> 16) & 0xFF, (v >> 8) & 0xFF, (v) & 0xFF);
 	}
-	return strVersion;
 }
 
 VersionNum RemoveBuildNumber(const VersionNum num)

common/version.h
@@ -32,10 +32,10 @@ namespace MptVersion
 	std::string GetOpenMPTVersionStr(); // e.g. "OpenMPT 1.17.02.08"
 
 	// Returns numerical version value from given version string.
-	VersionNum ToNum(const char *const s);
+	VersionNum ToNum(const std::string &s);
 
 	// Returns version string from given numerical version value.
-	mpt::String ToStr(const VersionNum v);
+	std::string ToStr(const VersionNum v);
 
 	// Return a version without build number (the last number in the version).
 	// The current versioning scheme uses this number only for test builds, and it should be 00 for official builds,

mptrack/Ctrl_pat.cpp
@@ -1097,7 +1097,7 @@ void CCtrlPatterns::OnSequenceNameChanged()
 	GetDlgItemText(IDC_EDIT_SEQUENCE_NAME, str);
 	if (str != m_sndFile.Order.m_sName)
 	{
-		m_sndFile.Order.m_sName = str;
+		m_sndFile.Order.m_sName = str.GetString();
 		m_modDoc.SetModified();
 		m_modDoc.UpdateAllViews(NULL, (m_sndFile.Order.GetCurrentSequenceIndex() << HINT_SHIFT_SEQUENCE) | HINT_SEQNAMES, this);
 	}

mptrack/Draw_pat.cpp
@@ -1644,7 +1644,7 @@ void CViewPattern::UpdateIndicator()
 						if(m->instr > 0 && m->instr <= MAX_MIXPLUGINS)
 						{
 							CHAR sztmp[128] = "";
-							StringFixer::CopyN(sztmp, pSndFile->m_MixPlugins[m->instr - 1].GetParamName(m->GetValueVolCol()));
+							StringFixer::CopyN(sztmp, pSndFile->m_MixPlugins[m->instr - 1].GetParamName(m->GetValueVolCol()).c_str());
 							if (sztmp[0]) wsprintf(s, "%d: %s", m->GetValueVolCol(), sztmp);
 						}
 					} else

mptrack/MIDIMapping.cpp
@@ -17,13 +17,11 @@
 std::string CMIDIMappingDirective::ToString() const
 //-------------------------------------------------
 {
-	mpt::String str;
 	char flags[4] = "000";
 	if(m_Active) flags[0] = '1';
 	if(m_CaptureMIDI) flags[1] = '1';
 	if(m_AllowPatternEdit) flags[2] = '1';
-	str.Format("%s:%d:%x:%d:%d:%d", flags, (int)GetChannel(), (int)GetEvent(), (int)GetController(), (int)m_PluginIndex, m_Parameter);
-	return str;
+	return mpt::String::Format("%s:%d:%x:%d:%d:%d", flags, (int)GetChannel(), (int)GetEvent(), (int)GetController(), (int)m_PluginIndex, m_Parameter);
 }
 
 

mptrack/MIDIMappingDialog.cpp
@@ -349,7 +349,7 @@ CString CMIDIMappingDialog::CreateListString(const CMIDIMappingDirective& s)
 		str.AppendChar('.');
 
 		//Param name
-		str.Insert(80, m_rSndFile.m_MixPlugins[plugindex-1].GetParamName(s.GetParamIndex()));
+		str.Insert(80, m_rSndFile.m_MixPlugins[plugindex-1].GetParamName(s.GetParamIndex()).c_str());
 	}
 	else
 		str.Insert(55, "No plugin");

commit 04761015f650ff80683e1504e0c8440be713fadc

[Fix] mpt::String::Format broke for non MSVC in r2162. Fix it.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2163 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptString.cpp
@@ -32,12 +32,12 @@ std::string Format(const char *format, ...)
 		return &(buf[0]);
 	#else
 		va_list argList;
-		va_start(argList, pszFormat);
-		int size = vsnprintf(NULL, 0, pszFormat, argList); // get required size, requires c99 compliant vsnprintf which msvc does not have
+		va_start(argList, format);
+		int size = vsnprintf(NULL, 0, format, argList); // get required size, requires c99 compliant vsnprintf which msvc does not have
 		va_end(argList);
 		std::vector<char> temp(size + 1);
-		va_start(argList, pszFormat);
-		vsnprintf(&(temp[0]), size + 1, pszFormat, argList);
+		va_start(argList, format);
+		vsnprintf(&(temp[0]), size + 1, format, argList);
 		va_end(argList);
 		return &(temp[0]);
 	#endif