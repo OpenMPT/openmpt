OpenMPT/openmpt

commit a0e39f40854f585c24f1cbc7e1e66309b386ea22

[Ref] Various fixes regarding interactions of ENABLE_ASM, ENABLE_X86 and NO_REVERB build settings. [Ref] Also disable more code if !ENABLE_ASM. [Fix] Make EQ compile in all cases.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2177 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndfile.cpp
@@ -402,7 +402,11 @@ CSoundFile::CSoundFile() :
 #endif
 //----------------------
 {
+#ifdef ENABLE_ASM
 	gdwSysInfo = GetSysInfo();
+#else
+	gdwSysInfo = 0;
+#endif
 	MemsetZero(MixSoundBuffer);
 	MemsetZero(MixRearBuffer);
 #ifndef NO_REVERB
@@ -887,7 +891,9 @@ void CSoundFile::FreeSample(void *p)
 void CSoundFile::SetDspEffects(DWORD DSPMask)
 //-------------------------------------------
 {
+#ifdef ENABLE_ASM
 	if(!(GetSysInfo() & PROCSUPPORT_MMX)) DSPMask &= ~SNDDSP_REVERB;
+#endif
 	m_MixerSettings.DSPMask = DSPMask;
 	InitPlayer(FALSE);
 }

common/BuildSettings.h
@@ -150,6 +150,14 @@
 
 
 
+// fixing stuff up
+
+#if !defined(ENABLE_MMX) && !defined(NO_REVERB)
+#define NO_REVERB // reverb requires mmx
+#endif
+
+
+
 #if MPT_COMPILER_MSVC
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 #endif

mptrack/Mpdlgs.cpp
@@ -125,10 +125,11 @@ BOOL COptionsSoundcard::OnInitDialog()
 
 	CPropertyPage::OnInitDialog();
 	if(TrackerSettings::Instance().m_MixerSettings.MixerFlags & SNDMIX_SOFTPANNING) CheckDlgButton(IDC_CHECK2, MF_CHECKED);
-	if(TrackerSettings::Instance().m_MixerSettings.MixerFlags & SNDMIX_ENABLEMMX) CheckDlgButton(IDC_CHECK3, MF_CHECKED);
 	if(m_SoundDeviceFlags & SNDDEV_OPTIONS_EXCLUSIVE) CheckDlgButton(IDC_CHECK4, MF_CHECKED);
 	if(m_SoundDeviceFlags & SNDDEV_OPTIONS_BOOSTTHREADPRIORITY) CheckDlgButton(IDC_CHECK5, MF_CHECKED);
 	// Multimedia extensions
+#ifdef ENABLE_ASM
+	if(TrackerSettings::Instance().m_MixerSettings.MixerFlags & SNDMIX_ENABLEMMX) CheckDlgButton(IDC_CHECK3, MF_CHECKED);
 	::EnableWindow(::GetDlgItem(m_hWnd, IDC_CHECK3), (CSoundFile::GetSysInfo() & PROCSUPPORT_MMX) ? TRUE : FALSE);
 	if(CSoundFile::GetSysInfo() & PROCSUPPORT_SSE)
 	{
@@ -137,6 +138,9 @@ BOOL COptionsSoundcard::OnInitDialog()
 	{
 		SetDlgItemText(IDC_CHECK3, _T("Enable 3DNow! acceleration"));
 	}
+#else
+	::ShowWindow(::GetDlgItem(m_hWnd, IDC_CHECK3), SW_HIDE);
+#endif
 
 	// Sampling Rate
 	UpdateSampleRates(m_nSoundDevice);
@@ -441,8 +445,10 @@ BOOL COptionsSoundcard::OnSetActive()
 void COptionsSoundcard::OnOK()
 //----------------------------
 {
-	if(IsDlgButtonChecked(IDC_CHECK2)) TrackerSettings::Instance().m_MixerSettings.MixerFlags |= SNDMIX_SOFTPANNING; else TrackerSettings::Instance().m_MixerSettings.MixerFlags &= ~SNDMIX_SOFTPANNING;
+#ifdef ENABLE_ASM
 	if(IsDlgButtonChecked(IDC_CHECK3)) TrackerSettings::Instance().m_MixerSettings.MixerFlags |= SNDMIX_ENABLEMMX; else TrackerSettings::Instance().m_MixerSettings.MixerFlags &= ~SNDMIX_ENABLEMMX;
+#endif
+	if(IsDlgButtonChecked(IDC_CHECK2)) TrackerSettings::Instance().m_MixerSettings.MixerFlags |= SNDMIX_SOFTPANNING; else TrackerSettings::Instance().m_MixerSettings.MixerFlags &= ~SNDMIX_SOFTPANNING;
 	m_SoundDeviceFlags = 0;
 	if(IsDlgButtonChecked(IDC_CHECK4)) m_SoundDeviceFlags |= SNDDEV_OPTIONS_EXCLUSIVE;
 	if(IsDlgButtonChecked(IDC_CHECK5)) m_SoundDeviceFlags |= SNDDEV_OPTIONS_BOOSTTHREADPRIORITY;
@@ -787,7 +793,9 @@ void COptionsPlayer::OnOK()
 	if (IsDlgButtonChecked(IDC_CHECK4)) dwQuality |= SNDDSP_SURROUND;
 	if (IsDlgButtonChecked(IDC_CHECK5)) dwQuality |= SNDDSP_NOISEREDUCTION;
 #endif
+#ifndef NO_REVERB
 	if (IsDlgButtonChecked(IDC_CHECK6)) dwQuality |= SNDDSP_REVERB;
+#endif
 	dwSrcMode = m_CbnResampling.GetCurSel();
 
 #ifndef NO_DSP

mptrack/Mptrack.cpp
@@ -867,6 +867,8 @@ BOOL CTrackApp::InitInstance()
 	AddDocTemplate(m_pModTemplate);
 
 	// Initialize Audio
+#ifdef ENABLE_ASM
+	// rough heuristic to select less cpu consuming defaults for old CPUs
 	DWORD sysinfo = CSoundFile::GetSysInfo();
 	if(sysinfo & PROCSUPPORT_MMX)
 	{
@@ -877,6 +879,10 @@ BOOL CTrackApp::InitInstance()
 	{
 		TrackerSettings::Instance().m_ResamplerSettings.SrcMode = SRCMODE_POLYPHASE;
 	}
+#else
+	// just use a sane default
+	TrackerSettings::Instance().m_ResamplerSettings.SrcMode = SRCMODE_POLYPHASE;
+#endif
 	// Load Midi Library
 	if (m_szConfigFileName[0]) ImportMidiConfig(m_szConfigFileName);
 

mptrack/View_smp.cpp
@@ -673,7 +673,9 @@ void CViewSample::DrawSampleData2(HDC hdc, int ymed, int cx, int cy, int len, in
 	int32 y0 = 0, xmax, poshi;
 	uint64 posincr, posfrac;
 
+#ifdef ENABLE_ASM
 	DWORD sysinfo = CSoundFile::GetSysInfo();
+#endif
 
 	if (len <= 0) return;
 	smplsize = (uFlags & CHN_16BIT) ? 2 : 1;

sounddsp/EQ.cpp
@@ -17,14 +17,22 @@
 #define EQ_BANDWIDTH	2.0
 #define EQ_ZERO			0.000001
 
+extern void C_StereoMixToFloat(const int *pSrc, float *pOut1, float *pOut2, UINT nCount, const float _i2fc);
+extern void C_FloatToStereoMix(const float *pIn1, const float *pIn2, int *pOut, UINT nCount, const float _f2ic);
+extern void C_MonoMixToFloat(const int *pSrc, float *pOut, UINT nCount, const float _i2fc);
+extern void C_FloatToMonoMix(const float *pIn, int *pOut, UINT nCount, const float _f2ic);
+
+#ifdef ENABLE_X86
 extern void X86_StereoMixToFloat(const int *pSrc, float *pOut1, float *pOut2, UINT nCount, const float _i2fc);
 extern void X86_FloatToStereoMix(const float *pIn1, const float *pIn2, int *pOut, UINT nCount, const float _f2ic);
 extern void X86_MonoMixToFloat(const int *pSrc, float *pOut, UINT nCount, const float _i2fc);
 extern void X86_FloatToMonoMix(const float *pIn, int *pOut, UINT nCount, const float _f2ic);
+#endif
 
 #ifdef ENABLE_SSE
 extern void SSE_MonoMixToFloat(const int *pSrc, float *pOut, UINT nCount, const float _i2fc);
 #endif
+
 #ifdef ENABLE_3DNOW
 extern void AMD_MonoMixToFloat(const int *pSrc, float *pOut, UINT nCount, const float _i2fc);
 extern void AMD_FloatToMonoMix(const float *pIn, int *pOut, UINT nCount, const float _f2ic);
@@ -325,19 +333,27 @@ static void EQFilter(EQBANDSTRUCT *pbs, float32 *pbuffer, UINT nCount)
 
 
 void CEQ::ProcessMono(int *pbuffer, float *MixFloatBuffer, UINT nCount, CSoundFilePlayConfig &config)
-//----------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------
 {
+#ifdef ENABLE_X86
 	X86_MonoMixToFloat(pbuffer, MixFloatBuffer, nCount, config.getIntToFloat());
+#else
+	C_MonoMixToFloat(pbuffer, MixFloatBuffer, nCount, config.getIntToFloat());
+#endif
 	for (UINT b=0; b<MAX_EQ_BANDS; b++)
 	{
 		if ((gEQ[b].bEnable) && (gEQ[b].Gain != 1.0f)) EQFilter(&gEQ[b], MixFloatBuffer, nCount);
 	}
+#ifdef ENABLE_X86
 	X86_FloatToMonoMix(MixFloatBuffer, pbuffer, nCount, config.getFloatToInt());
+#else
+	C_FloatToMonoMix(MixFloatBuffer, pbuffer, nCount, config.getFloatToInt());
+#endif
 }
 
 
 void CEQ::ProcessStereo(int *pbuffer, float *MixFloatBuffer, UINT nCount, CSoundFilePlayConfig &config, DWORD SoundSetupFlags, DWORD SysInfoFlags)
-//-------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------
 {
 
 #ifdef ENABLE_SSE
@@ -388,7 +404,15 @@ void CEQ::ProcessStereo(int *pbuffer, float *MixFloatBuffer, UINT nCount, CSound
 #endif // ENABLE_3DNOW
 
 	{	
+
+		UNREFERENCED_PARAMETER(SoundSetupFlags);
+		UNREFERENCED_PARAMETER(SysInfoFlags);
+
+#ifdef ENABLE_X86
 		X86_StereoMixToFloat(pbuffer, MixFloatBuffer, MixFloatBuffer+MIXBUFFERSIZE, nCount, config.getIntToFloat());
+#else
+		C_StereoMixToFloat(pbuffer, MixFloatBuffer, MixFloatBuffer+MIXBUFFERSIZE, nCount, config.getIntToFloat());
+#endif
 		
 		for (UINT bl=0; bl<MAX_EQ_BANDS; bl++)
 		{
@@ -399,7 +423,12 @@ void CEQ::ProcessStereo(int *pbuffer, float *MixFloatBuffer, UINT nCount, CSound
 			if ((gEQ[br].bEnable) && (gEQ[br].Gain != 1.0f)) EQFilter(&gEQ[br], MixFloatBuffer+MIXBUFFERSIZE, nCount);
 		}
 
+#ifdef ENABLE_X86
 		X86_FloatToStereoMix(MixFloatBuffer, MixFloatBuffer+MIXBUFFERSIZE, pbuffer, nCount, config.getFloatToInt());
+#else
+		C_FloatToStereoMix(MixFloatBuffer, MixFloatBuffer+MIXBUFFERSIZE, pbuffer, nCount, config.getFloatToInt());
+#endif
+
 	}
 }
 

soundlib/Fastmix.cpp
@@ -1754,6 +1754,7 @@ void CSoundFile::ProcessPlugins(UINT nCount)
 VOID CSoundFile::StereoMixToFloat(const int *pSrc, float *pOut1, float *pOut2, UINT nCount)
 //-----------------------------------------------------------------------------------------
 {
+#ifdef ENABLE_ASM
 	if(m_MixerSettings.MixerFlags & SNDMIX_ENABLEMMX)
 	{
 #ifdef ENABLE_SSE
@@ -1771,17 +1772,19 @@ VOID CSoundFile::StereoMixToFloat(const int *pSrc, float *pOut1, float *pOut2, U
 		}
 #endif // ENABLE_3DNOW
 	}
+#endif // ENABLE_ASM
 #ifdef ENABLE_X86
 	X86_StereoMixToFloat(pSrc, pOut1, pOut2, nCount, m_PlayConfig.getIntToFloat());
-#else
+#else // !ENABLE_X86
 	C_StereoMixToFloat(pSrc, pOut1, pOut2, nCount, m_PlayConfig.getIntToFloat());
-#endif
+#endif // ENABLE_X86
 }
 
 
 VOID CSoundFile::FloatToStereoMix(const float *pIn1, const float *pIn2, int *pOut, UINT nCount)
 //---------------------------------------------------------------------------------------------
 {
+#ifdef ENABLE_ASM
 	if(m_MixerSettings.MixerFlags & SNDMIX_ENABLEMMX)
 	{
 #ifdef ENABLE_3DNOW
@@ -1792,11 +1795,12 @@ VOID CSoundFile::FloatToStereoMix(const float *pIn1, const float *pIn2, int *pOu
 		}
 #endif // ENABLE_3DNOW
 	}
+#endif // ENABLE_ASM
 #ifdef ENABLE_X86
 	X86_FloatToStereoMix(pIn1, pIn2, pOut, nCount, m_PlayConfig.getFloatToInt());
-#else
+#else // !ENABLE_X86
 	C_FloatToStereoMix(pIn1, pIn2, pOut, nCount, m_PlayConfig.getFloatToInt());
-#endif
+#endif // ENABLE_X86
 }
 
 

soundlib/Mmx_mix.cpp
@@ -24,6 +24,7 @@
 #include "Sndfile.h"
 
 
+#ifdef ENABLE_ASM
 DWORD CSoundFile::GetSysInfo()
 //----------------------------
 {
@@ -83,6 +84,7 @@ DWORD CSoundFile::GetSysInfo()
 	return 0;
 #endif
 }
+#endif
 
 
 ////////////////////////////////////////////////////////////////////////////////////

commit 1df523dc623674593ef2347a94bf9e361f5cae2f

[Fix] Compilation of libopenmpt broke in r2177. Fix it.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2178 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndfile.cpp
@@ -892,7 +892,9 @@ void CSoundFile::SetDspEffects(DWORD DSPMask)
 //-------------------------------------------
 {
 #ifdef ENABLE_ASM
+#ifndef NO_REVERB
 	if(!(GetSysInfo() & PROCSUPPORT_MMX)) DSPMask &= ~SNDDSP_REVERB;
+#endif
 #endif
 	m_MixerSettings.DSPMask = DSPMask;
 	InitPlayer(FALSE);