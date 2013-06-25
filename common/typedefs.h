c738c4660b32abf80beb8b5717df0ce955c5d2f1
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-06-25 19:09:04 +0000

    [Fix] With VS2008, int8 should be a signed __int8 instead of a __int8 which is identical to char. This broke compilation when SwapBytes overloads for these types were added in r2271.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2406 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/typedefs.h
+++ common/typedefs.h
@@ -212,10 +212,10 @@ void AlwaysAssertHandler(const char *file, int line, const char *function, const
 
 #if MPT_COMPILER_MSVC && MPT_MSVC_BEFORE(2010,0)
 
-typedef __int8 int8;
-typedef __int16 int16;
-typedef __int32 int32;
-typedef __int64 int64;
+typedef signed __int8 int8;
+typedef signed __int16 int16;
+typedef signed __int32 int32;
+typedef signed __int64 int64;
 typedef unsigned __int8 uint8;
 typedef unsigned __int16 uint16;
 typedef unsigned __int32 uint32;
