fa7d10eb5a4f049e1875813d3c7efa7fe5213569
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-07-31 17:05:28 +0000

    [Fix] In CopySample<>, correctly calculate sampleSize and thereby avoid potentially reading beyond sourceSize.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2563 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/SampleFormatConverters.h
+++ soundlib/SampleFormatConverters.h
@@ -477,7 +477,7 @@ struct NormalizationChain
 
 // Copy a sample data buffer.
 // targetBuffer: Buffer in which the sample should be copied into.
-// numSamples: Number of samples of size T that should be copied. targetBuffer is expected to be able to hold "numSampels * incTarget" samples.
+// numSamples: Number of samples of size T that should be copied. targetBuffer is expected to be able to hold "numSamples * incTarget" samples.
 // incTarget: Number of samples by which the target data pointer is increased each time.
 // sourceBuffer: Buffer from which the samples should be read.
 // sourceSize: Size of source buffer, in bytes.
@@ -489,7 +489,7 @@ template <typename SampleConversion>
 size_t CopySample(typename SampleConversion::output_t *outBuf, size_t numSamples, size_t incTarget, const typename SampleConversion::input_t *inBuf, size_t sourceSize, size_t incSource, SampleConversion conv = SampleConversion())
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 {
-	const size_t sampleSize = incSource * SampleConversion::input_inc;
+	const size_t sampleSize = incSource * SampleConversion::input_inc * sizeof(typename SampleConversion::input_t);
 	LimitMax(numSamples, sourceSize / sampleSize);
 	const size_t copySize = numSamples * sampleSize;
 
