f5c33a7f6236cbe4c511d44e180ac5b2dce34417
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-09-05 14:36:36 +0000

    [Ref] libopenmpt: Remove "charset" ctl again. We always return UTF-8 strings.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@2636 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_impl.cpp
+++ libopenmpt/libopenmpt_impl.cpp
@@ -848,25 +848,17 @@ std::string module_impl::highlight_pattern_row_channel( std::int32_t p, std::int
 
 std::vector<std::string> module_impl::get_ctls() const {
 	std::vector<std::string> retval;
-	retval.push_back( "charset" );
 	return retval;
 }
 std::string module_impl::ctl_get( const std::string & ctl ) const {
 	if ( ctl == "" ) {
 		throw openmpt::exception("unknown ctl");
-	} else if ( ctl == "charset" ) {
-		/*
-		return m_sndFile->GetCharset().second;
-		*/
-		return "UTF-8";
 	}
 	throw openmpt::exception("unknown ctl");
 }
 void module_impl::ctl_set( const std::string & ctl, const std::string & value ) {
 	if ( ctl == "" ) {
 		throw openmpt::exception("unknown ctl: " + ctl + " := " + value);
-	} else if ( ctl == "charset" ) {
-		throw openmpt::exception("ctl is read-only: " + ctl);
 	}
 	throw openmpt::exception("unknown ctl: " + ctl + " := " + value);
 }
