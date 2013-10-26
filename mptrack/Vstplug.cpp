4096534a3f56c7543e77bd69ada1477d666ea699
Johannes Schultz <saga-games@users.sourceforge.net>
2013-10-26 23:20:12 +0000

    [Fix] Fix compilation of debug builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3054 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Vstplug.cpp
+++ mptrack/Vstplug.cpp
@@ -33,8 +33,6 @@
 #include "../zlib/zlib.h"			//For CRC32 calculation (to detect plugins with same UID)
 #endif // VST_USE_ALTERNATIVE_MAGIC
 
-#define new DEBUG_NEW
-
 #ifndef NO_VST
 
 char CVstPluginManager::s_szHostProductString[64] = "OpenMPT";
