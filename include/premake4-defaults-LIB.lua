12b9482f1088d85bae6f8090867e003c5c4f4d36
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-11-06 18:45:16 +0000

    [Ref] Reorganize premake files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3105 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/premake4-defaults-LIB.lua
+++ include/premake4-defaults-LIB.lua
@@ -1,23 +1,23 @@
 
   configuration { "Debug", "x32" }
-   targetdir "../bin/Win32-Debug"
+   targetdir "../build/lib/x32/Debug"
   
   configuration { "Release", "x32" }
-   targetdir "../bin/Win32"
+   targetdir "../build/lib/x32/Release"
   
   configuration { "ReleaseNoLTCG", "x32" }
-   targetdir "../bin/Win32"
+   targetdir "../build/lib/x32/ReleaseNoLTCG"
   
   configuration { "Debug", "x64" }
-   targetdir "../bin/x64-Debug"
+   targetdir "../build/lib/x64/Debug"
   
   configuration { "Release", "x64" }
-   targetdir "../bin/x64"
+   targetdir "../build/lib/x64/Release"
   
   configuration { "ReleaseNoLTCG", "x64" }
-   targetdir "../bin/x64"
+   targetdir "../build/lib/x64/ReleaseNoLTCG"
   
   configuration "*"
-   kind "SharedLib"
-
+   kind "StaticLib"
+   
   dofile "premake4-defaults.lua"
--- include/premake4-defaults.lua
+++ include/premake4-defaults.lua
@@ -1,25 +1,4 @@
 
-  configuration { "Debug", "x32" }
-   targetdir "../build/lib/x32/Debug"
-  
-  configuration { "Release", "x32" }
-   targetdir "../build/lib/x32/Release"
-  
-  configuration { "ReleaseNoLTCG", "x32" }
-   targetdir "../build/lib/x32/ReleaseNoLTCG"
-  
-  configuration { "Debug", "x64" }
-   targetdir "../build/lib/x64/Debug"
-  
-  configuration { "Release", "x64" }
-   targetdir "../build/lib/x64/Release"
-  
-  configuration { "ReleaseNoLTCG", "x64" }
-   targetdir "../build/lib/x64/ReleaseNoLTCG"
-  
-  configuration "*"
-   kind "StaticLib"
-   
   configuration "Debug"
    defines { "DEBUG" }
    defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_WARNINGS" }
