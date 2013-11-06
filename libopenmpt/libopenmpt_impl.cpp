e4a74b4b99d6e313fc58811ead5b92fc8593449d
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-11-06 21:27:32 +0000

    [Ref] libopenmpt: Merge libopenmpt_version.cpp into libopenmpt_impl.cpp and kill it.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3109 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_impl.cpp
+++ libopenmpt/libopenmpt_impl.cpp
@@ -17,18 +17,82 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <sstream>
 
 #include <cmath>
 #include <cstdlib>
 #include <cstring>
 
+#include "common/version.h"
 #include "soundlib/Sndfile.h"
 #include "soundlib/AudioReadTarget.h"
 #include "soundlib/FileReader.h"
 
 namespace openmpt {
 
-log_interface::log_interface() {
+namespace version {
+
+std::uint32_t get_library_version() {
+	return OPENMPT_API_VERSION | ( MptVersion::GetRevision() & 0xffff );
+}
+
+std::uint32_t get_core_version() {
+	return MptVersion::num;
+}
+
+static std::string get_library_version_string() {
+	std::ostringstream str;
+	std::uint32_t version = get_library_version();
+	if ( ( version & 0xffff ) == 0 ) {
+		str << ((version>>24) & 0xff) << "." << ((version>>16) & 0xff);
+	} else {
+		str << ((version>>24) & 0xff) << "." << ((version>>16) & 0xff) << "." << ((version>>0) & 0xffff);
+	}
+	if ( MptVersion::IsDirty() ) {
+		str << ".2-modified";
+	} else if ( MptVersion::HasMixedRevisions() ) {
+		str << ".1-modified";
+	}
+	return str.str();
+}
+
+static std::string get_core_version_string() {
+	return MptVersion::GetVersionStringExtended();
+}
+
+static std::string get_build_string() {
+	return MptVersion::GetBuildDateString();
+}
+
+static std::string get_credits_string() {
+	return mpt::String::Convert( MptVersion::GetFullCreditsString(), mpt::CharsetISO8859_1, mpt::CharsetUTF8 );
+}
+
+static std::string get_contact_string() {
+	return mpt::String::Convert( MptVersion::GetContactString(), mpt::CharsetISO8859_1, mpt::CharsetUTF8 );
+}
+
+std::string get_string( const std::string & key ) {
+	if ( key == "" ) {
+		return std::string();
+	} else if ( key == string::library_version ) {
+		return get_library_version_string();
+	} else if ( key == string::core_version ) {
+		return get_core_version_string();
+	} else if ( key == string::build ) {
+		return get_build_string();
+	} else if ( key == string::credits ) {
+		return get_credits_string();
+	} else if ( key == string::contact ) {
+		return get_contact_string();
+	} else {
+		return std::string();
+	}
+}
+
+} // namespace version
+
+ log_interface::log_interface() {
 	return;
 }
 log_interface::~log_interface() {
