OpenMPT/openmpt

commit 0308407a5a8d65656e9a53fa44d20c53c546c1a8

[Ref] Where applicable, use TrackerDirectories directly instead of indirection through TrackerSettings.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3124 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndfile.cpp
@@ -1721,7 +1721,7 @@ bool CSoundFile::LoadStaticTunings()
 	}
 
 	// Load local tunings.
-	s_pTuningsSharedLocal->SetSavefilePath(mpt::String::Format("%slocal_tunings%s", TrackerSettings::Instance().GetDefaultDirectory(DIR_TUNING), CTuningCollection::s_FileExtension));
+	s_pTuningsSharedLocal->SetSavefilePath(mpt::String::Format("%slocal_tunings%s", TrackerDirectories::Instance().GetDefaultDirectory(DIR_TUNING), CTuningCollection::s_FileExtension));
 	s_pTuningsSharedLocal->Deserialize();
 
 	// Enabling adding/removing of tunings for standard collection

mptrack/Ctrl_ins.cpp
@@ -1439,7 +1439,7 @@ BOOL CCtrlInstruments::OpenInstrument(LPCSTR lpszFileName)
 		{
 			TCHAR szName[_MAX_FNAME], szExt[_MAX_EXT];
 			_tsplitpath(lpszFileName, nullptr, nullptr, szName, szExt);
-			TrackerSettings::Instance().SetWorkingDirectory(lpszFileName, DIR_INSTRUMENTS, true);
+			TrackerDirectories::Instance().SetWorkingDirectory(lpszFileName, DIR_INSTRUMENTS, true);
 	
 			if (!pIns->name[0] && m_sndFile.GetModSpecifications().instrNameLengthMax > 0)
 			{
@@ -1689,11 +1689,11 @@ void CCtrlInstruments::OnInstrumentOpen()
 			"GF1 Patches (*.pat)|*.pat|"
 			"Impulse Tracker Instruments (*.iti)|*.iti|"
 			"All Files (*.*)|*.*||")
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_INSTRUMENTS))
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_INSTRUMENTS))
 		.FilterIndex(&nLastIndex);
 	if(!dlg.Show()) return;
 
-	TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_INSTRUMENTS, true);
+	TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_INSTRUMENTS, true);
 
 	const FileDialog::PathList &files = dlg.GetFilenames();
 	for(size_t counter = 0; counter < files.size(); counter++)
@@ -1747,7 +1747,7 @@ void CCtrlInstruments::OnInstrumentSave()
 		: "Impulse Tracker Instruments (*.iti)|*.iti|"
 		"Compressed Impulse Tracker Instruments (*.iti)|*.iti|"
 		"FastTracker II Instruments (*.xi)|*.xi||")
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_INSTRUMENTS))
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_INSTRUMENTS))
 		.FilterIndex(&index);
 	if(!dlg.Show()) return;
 	
@@ -1772,7 +1772,7 @@ void CCtrlInstruments::OnInstrumentSave()
 		strcpy(szFileName, drive);
 		strcat(szFileName, path);
 		
-		TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_INSTRUMENTS);
+		TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_INSTRUMENTS);
 
 // -> CODE#0023
 // -> DESC="IT project files (.itp)"

mptrack/Ctrl_smp.cpp
@@ -819,7 +819,7 @@ bool CCtrlSamples::OpenSample(LPCSTR lpszFileName)
 	if (bOk)
 	{
 		ModSample &sample = m_sndFile.GetSample(m_nSample);
-		TrackerSettings::Instance().SetWorkingDirectory(lpszFileName, DIR_SAMPLES, true);
+		TrackerDirectories::Instance().SetWorkingDirectory(lpszFileName, DIR_SAMPLES, true);
 		if (!sample.filename[0])
 		{
 			CHAR szName[_MAX_PATH], szExt[_MAX_EXT];
@@ -961,11 +961,11 @@ void CCtrlSamples::OnSampleOpen()
 			"AIFF Files (*.aiff;*.8svx)|*.aif;*.aiff;*.8sv;*.8svx;*.svx|"
 			"Raw Samples (*.raw,*.snd,*.pcm)|*.raw;*.snd;*.pcm|"
 			"All Files (*.*)|*.*||")
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_SAMPLES))
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_SAMPLES))
 		.FilterIndex(&nLastIndex);
 	if(!dlg.Show()) return;
 
-	TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_SAMPLES, true);
+	TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_SAMPLES, true);
 
 	const FileDialog::PathList &files = dlg.GetFilenames();
 	for(size_t counter = 0; counter < files.size(); counter++)
@@ -1041,7 +1041,7 @@ void CCtrlSamples::OnSampleSave()
 		.ExtensionFilter("Wave File (*.wav)|*.wav|"
 			"FLAC File (*.flac)|*.flac|"
 			"RAW Audio (*.raw)|*.raw||")
-			.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_SAMPLES))
+			.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_SAMPLES))
 			.FilterIndex(&filter);
 	if(!dlg.Show()) return;
 
@@ -1095,7 +1095,7 @@ void CCtrlSamples::OnSampleSave()
 		ErrorBox(IDS_ERR_SAVESMP, this);
 	} else
 	{
-		TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_SAMPLES, true);
+		TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_SAMPLES, true);
 	}
 	SwitchToView();
 }

mptrack/MainFrm.cpp
@@ -1604,11 +1604,11 @@ BOOL CMainFrame::SetupDirectories(LPCTSTR szModDir, LPCTSTR szSampleDir, LPCTSTR
 //---------------------------------------------------------------------------------------------------------------------------------
 {
 	// will also set working directory
-	TrackerSettings::Instance().SetDefaultDirectory(szModDir, DIR_MODS);
-	TrackerSettings::Instance().SetDefaultDirectory(szSampleDir, DIR_SAMPLES);
-	TrackerSettings::Instance().SetDefaultDirectory(szInstrDir, DIR_INSTRUMENTS);
-	TrackerSettings::Instance().SetDefaultDirectory(szVstDir, DIR_PLUGINS);
-	TrackerSettings::Instance().SetDefaultDirectory(szPresetDir, DIR_PLUGINPRESETS);
+	TrackerDirectories::Instance().SetDefaultDirectory(szModDir, DIR_MODS);
+	TrackerDirectories::Instance().SetDefaultDirectory(szSampleDir, DIR_SAMPLES);
+	TrackerDirectories::Instance().SetDefaultDirectory(szInstrDir, DIR_INSTRUMENTS);
+	TrackerDirectories::Instance().SetDefaultDirectory(szVstDir, DIR_PLUGINS);
+	TrackerDirectories::Instance().SetDefaultDirectory(szPresetDir, DIR_PLUGINPRESETS);
 	return TRUE;
 }
 

mptrack/Moddoc.cpp
@@ -607,10 +607,10 @@ BOOL CModDoc::DoSave(LPCSTR lpszPathName, BOOL)
 			.DefaultExtension(defaultExtension)
 			.DefaultFilename(s)
 			.ExtensionFilter(ModTypeToFilter(m_SndFile))
-			.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_MODS));
+			.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_MODS));
 		if(!dlg.Show()) return FALSE;
 
-		TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_MODS, true);
+		TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_MODS, true);
 
 		strcpy(s, dlg.GetFirstFile().c_str());
 		_splitpath(s, drive, path, fname, fext);
@@ -1667,11 +1667,11 @@ void CModDoc::OnFileWaveConvert(ORDERINDEX nMinOrder, ORDERINDEX nMaxOrder, cons
 		.DefaultExtension(extension)
 		.DefaultFilename(fname)
 		.ExtensionFilter(encFactory->GetTraits().fileDescription + " (*." + extension + ")|*." + extension + "||")
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_EXPORT));
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_EXPORT));
 	if(!dlg.Show()) return;
 
 	// will set default dir here because there's no setup option for export dir yet (feel free to add one...)
-	TrackerSettings::Instance().SetDefaultDirectory(dlg.GetWorkingDirectory().c_str(), DIR_EXPORT, true);
+	TrackerDirectories::Instance().SetDefaultDirectory(dlg.GetWorkingDirectory().c_str(), DIR_EXPORT, true);
 
 	char drive[_MAX_DRIVE], dir[_MAX_DIR], name[_MAX_FNAME], ext[_MAX_EXT];
 	_splitpath(dlg.GetFirstFile().c_str(), drive, dir, name, ext);
@@ -1945,7 +1945,7 @@ void CModDoc::OnFileCompatibilitySave()
 		.DefaultExtension(ext)
 		.DefaultFilename(filename)
 		.ExtensionFilter(pattern)
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_MODS));
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_MODS));
 	if(!dlg.Show()) return;
 
 	ScopedLogCapturer logcapturer(*this);
@@ -2872,7 +2872,7 @@ void CModDoc::OnSaveTemplateModule()
 //----------------------------------
 {
 	// Create template folder if doesn't exist already.
-	const LPCTSTR pszTemplateFolder = TrackerSettings::Instance().GetDefaultDirectory(DIR_TEMPLATE_FILES_USER);
+	const LPCTSTR pszTemplateFolder = TrackerDirectories::Instance().GetDefaultDirectory(DIR_TEMPLATE_FILES_USER);
 	if (!PathIsDirectory(pszTemplateFolder))
 	{
 		if (!CreateDirectory(pszTemplateFolder, nullptr))

mptrack/Moptions.cpp
@@ -607,11 +607,11 @@ BOOL COptionsGeneral::OnInitDialog()
 	m_CheckList.SetCurSel(0);
 	OnOptionSelChanged();
 
-	SetDlgItemText(IDC_OPTIONS_DIR_MODS,		TrackerSettings::Instance().GetDefaultDirectory(DIR_MODS));
-	SetDlgItemText(IDC_OPTIONS_DIR_SAMPS,		TrackerSettings::Instance().GetDefaultDirectory(DIR_SAMPLES));
-	SetDlgItemText(IDC_OPTIONS_DIR_INSTS,		TrackerSettings::Instance().GetDefaultDirectory(DIR_INSTRUMENTS));
-	SetDlgItemText(IDC_OPTIONS_DIR_VSTS,		TrackerSettings::Instance().GetDefaultDirectory(DIR_PLUGINS));
-	SetDlgItemText(IDC_OPTIONS_DIR_VSTPRESETS,	TrackerSettings::Instance().GetDefaultDirectory(DIR_PLUGINPRESETS));
+	SetDlgItemText(IDC_OPTIONS_DIR_MODS,		TrackerDirectories::Instance().GetDefaultDirectory(DIR_MODS));
+	SetDlgItemText(IDC_OPTIONS_DIR_SAMPS,		TrackerDirectories::Instance().GetDefaultDirectory(DIR_SAMPLES));
+	SetDlgItemText(IDC_OPTIONS_DIR_INSTS,		TrackerDirectories::Instance().GetDefaultDirectory(DIR_INSTRUMENTS));
+	SetDlgItemText(IDC_OPTIONS_DIR_VSTS,		TrackerDirectories::Instance().GetDefaultDirectory(DIR_PLUGINS));
+	SetDlgItemText(IDC_OPTIONS_DIR_VSTPRESETS,	TrackerDirectories::Instance().GetDefaultDirectory(DIR_PLUGINPRESETS));
 
 	return TRUE;
 }

mptrack/Mptrack.cpp
@@ -1143,11 +1143,11 @@ void CTrackApp::OnFileOpen()
 			"Wave Files (*.wav)|*.wav|"
 			"Midi Files (*.mid,*.rmi)|*.mid;*.rmi;*.smf|"
 			"All Files (*.*)|*.*||")
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_MODS))
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_MODS))
 		.FilterIndex(&nFilterIndex);
 	if(!dlg.Show()) return;
 
-	TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_MODS, true);
+	TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_MODS, true);
 
 	const FileDialog::PathList &files = dlg.GetFilenames();
 	for(size_t counter = 0; counter < files.size(); counter++)

mptrack/SelectPluginDialog.cpp
@@ -458,10 +458,10 @@ void CSelectPluginDlg::OnAddPlugin()
 		.AllowMultiSelect()
 		.DefaultExtension("dll")
 		.ExtensionFilter("VST Plugins (*.dll)|*.dll||")
-		.WorkingDirectory(TrackerSettings::Instance().GetWorkingDirectory(DIR_PLUGINS));
+		.WorkingDirectory(TrackerDirectories::Instance().GetWorkingDirectory(DIR_PLUGINS));
 	if(!dlg.Show()) return;
 
-	TrackerSettings::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_PLUGINS, true);
+	TrackerDirectories::Instance().SetWorkingDirectory(dlg.GetWorkingDirectory().c_str(), DIR_PLUGINS, true);
 
 	CVstPluginManager *pManager = theApp.GetPluginManager();
 	bool bOk = false;

commit 0d156ddcd3f5c0b99596ddba8eb61519e0aab56e

[Fix] Fix crash in CSoundFile::LoadStaticTunings (broken since r3124 ).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3138 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndfile.cpp
@@ -1721,7 +1721,11 @@ bool CSoundFile::LoadStaticTunings()
 	}
 
 	// Load local tunings.
-	s_pTuningsSharedLocal->SetSavefilePath(mpt::String::Format("%slocal_tunings%s", TrackerDirectories::Instance().GetDefaultDirectory(DIR_TUNING), CTuningCollection::s_FileExtension));
+	s_pTuningsSharedLocal->SetSavefilePath((
+		TrackerDirectories::Instance().GetDefaultDirectory(DIR_TUNING)
+		+ mpt::PathString::FromUTF8("local_tunings")
+		+ mpt::PathString::FromLocale(CTuningCollection::s_FileExtension)
+		).ToLocale());
 	s_pTuningsSharedLocal->Deserialize();
 
 	// Enabling adding/removing of tunings for standard collection