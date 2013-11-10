f960dac35edaaf292cf9801d183680f541a7c8cf
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-11-10 19:10:25 +0000

    [Ref] Split mpt::PathString into its own header file.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3184 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ common/mptPathString.h
@@ -0,0 +1,196 @@
+/*
+ * mptPathString.h
+ * ---------------
+ * Purpose: Wrapper class around the platform-native representation of path names. Should be the only type that is used to store path names.
+ * Notes  : Currently none.
+ * Authors: OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
+
+
+#pragma once
+
+//#define MPT_DEPRECATED_PATH
+#define MPT_DEPRECATED_PATH MPT_DEPRECATED
+
+namespace mpt
+{
+
+#if defined(WIN32)
+typedef std::wstring RawPathString;
+#else // !WIN32
+typedef std::string RawPathString;
+#endif // WIN32
+
+class PathString
+{
+private:
+	RawPathString path;
+private:
+	PathString(const RawPathString & path)
+		: path(path)
+	{
+		return;
+	}
+public:
+	PathString()
+	{
+		return;
+	}
+	PathString(const PathString & other)
+		: path(other.path)
+	{
+		return;
+	}
+	PathString & assign(const PathString & other)
+	{
+		path = other.path;
+		return *this;
+	}
+	PathString & operator = (const PathString & other)
+	{
+		return assign(other);
+	}
+	PathString & append(const PathString & other)
+	{
+		path.append(other.path);
+		return *this;
+	}
+	PathString & operator += (const PathString & other)
+	{
+		return append(other);
+	}
+	friend PathString operator + (const PathString & a, const PathString & b)
+	{
+		return PathString(a).append(b);
+	}
+	friend bool operator == (const PathString & a, const PathString & b)
+	{
+		return a.AsNative() == b.AsNative();
+	}
+	friend bool operator != (const PathString & a, const PathString & b)
+	{
+		return a.AsNative() != b.AsNative();
+	}
+	bool empty() const { return path.empty(); }
+	const RawPathString &NativeRef() const
+	{
+		return path;
+	}
+
+#if defined(WIN32)
+	static int CompareNoCase(const PathString & a, const PathString & b)
+	{
+		return lstrcmpiW(a.ToWide().c_str(), b.ToWide().c_str());
+	}
+#endif
+
+public:
+
+#if defined(MODPLUG_TRACKER)
+
+	void SplitPath(PathString *drive, PathString *dir, PathString *fname, PathString *ext) const;
+	PathString GetDrive() const;
+	PathString GetDir() const;
+	PathString GetPath() const;
+	PathString GetFileName() const;
+	PathString GetFileExt() const;
+
+	bool HasTrailingSlash() const
+	{
+		if(empty())
+			return false;
+#if defined(WIN32)
+		if(path[path.length()-1] == L'\\' || path[path.length()-1] == L'/')
+			return true;
+#else
+		if(path[path.length()-1] == '/')
+			return true;
+#endif
+		return false;
+	}
+
+#endif // MODPLUG_TRACKER
+
+public:
+
+#if defined(WIN32)
+
+	// conversions
+	MPT_DEPRECATED_PATH std::string ToLocale() const { return mpt::String::Encode(path, mpt::CharsetLocale); }
+	std::string ToUTF8() const { return mpt::String::Encode(path, mpt::CharsetUTF8); }
+	std::wstring ToWide() const { return path; }
+	MPT_DEPRECATED_PATH static PathString FromLocale(const std::string &path) { return PathString(mpt::String::Decode(path, mpt::CharsetLocale)); }
+	static PathString FromUTF8(const std::string &path) { return PathString(mpt::String::Decode(path, mpt::CharsetUTF8)); }
+	static PathString FromWide(const std::wstring &path) { return PathString(path); }
+	RawPathString AsNative() const { return path; }
+	static PathString FromNative(const RawPathString &path) { return PathString(path); }
+#if defined(_MFC_VER)
+	// CString TCHAR, so this is CHAR or WCHAR, depending on UNICODE
+	MPT_DEPRECATED_PATH CString ToCString() const { return mpt::String::ToCString(path); }
+	MPT_DEPRECATED_PATH static PathString FromCString(const CString &path) { return PathString(mpt::String::FromCString(path)); }
+#endif
+
+#else // !WIN32
+
+	// conversions
+	std::string ToLocale() const { return path; }
+	std::string ToUTF8() const { return mpt::String::Convert(path, mpt::CharsetLocale, mpt::CharsetUTF8); }
+	std::wstring ToWide() const { return mpt::String::Decode(path, mpt::CharsetLocale); }
+	static PathString FromLocale(const std::string &path) { return PathString(path); }
+	static PathString FromUTF8(const std::string &path) { return PathString(mpt::String::Convert(path, mpt::CharsetUTF8, mpt::CharsetLocale)); }
+	static PathString FromWide(const std::wstring &path) { return PathString(mpt::String::Encode(path, mpt::CharsetLocale)); }
+	RawPathString AsNative() const { return path; }
+	static PathString FromNative(const RawPathString &path) { return PathString(path); }
+
+#endif // WIN32
+
+};
+
+} // namespace mpt
+
+#if defined(WIN32)
+
+#define MPT_PATHSTRING(x) mpt::PathString::FromNative( L ## x )
+
+#else // !WIN32
+
+#define MPT_PATHSTRING(x) mpt::PathString::FromNative( x )
+
+#endif // WIN32
+
+FILE * mpt_fopen(const mpt::PathString &filename, const char *mode);
+FILE * mpt_fopen(const mpt::PathString &filename, const wchar_t *mode);
+
+#if defined(MODPLUG_TRACKER)
+
+// Sanitize a filename (remove special chars)
+void SanitizeFilename(mpt::PathString &filename);
+
+void SanitizeFilename(char *beg, char *end);
+void SanitizeFilename(wchar_t *beg, wchar_t *end);
+
+void SanitizeFilename(std::string &str);
+void SanitizeFilename(std::wstring &str);
+
+template <std::size_t size>
+void SanitizeFilename(char (&buffer)[size])
+//-----------------------------------------
+{
+	STATIC_ASSERT(size > 0);
+	SanitizeFilename(buffer, buffer + size);
+}
+
+template <std::size_t size>
+void SanitizeFilename(wchar_t (&buffer)[size])
+//--------------------------------------------
+{
+	STATIC_ASSERT(size > 0);
+	SanitizeFilename(buffer, buffer + size);
+}
+
+#if defined(_MFC_VER)
+MPT_DEPRECATED_PATH void SanitizeFilename(CString &str);
+#endif
+
+#endif // MODPLUG_TRACKER
