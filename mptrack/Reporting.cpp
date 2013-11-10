2aa157c2620853c9d7721210e195364ce0e358db
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-11-10 12:48:22 +0000

    [Ref] Add wide string overloads in Reporting.h. This allows proper displaying of filenames in message boxes.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3168 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Reporting.cpp
+++ mptrack/Reporting.cpp
@@ -38,6 +38,31 @@ UINT Reporting::ShowNotification(const char *text, const char *caption, UINT fla
 }
 
 
+UINT Reporting::ShowNotification(const std::wstring &text, const std::wstring &caption, UINT flags, const CWnd *parent)
+//---------------------------------------------------------------------------------------------------------------------
+{
+	CMainFrame *pMainFrm = CMainFrame::GetMainFrame();
+
+	if(pMainFrm != nullptr && pMainFrm->GetInputHandler() != nullptr)
+	{
+		pMainFrm->GetInputHandler()->Bypass(true);
+	}
+
+	if(parent == nullptr)
+	{
+		parent = pMainFrm;
+	}
+	UINT result = ::MessageBoxW((parent ? parent->m_hWnd : NULL), text.c_str(), caption.c_str(), flags);
+
+	if(pMainFrm != nullptr && pMainFrm->GetInputHandler() != nullptr)
+	{
+		pMainFrm->GetInputHandler()->Bypass(false);
+	}
+
+	return result;
+}
+
+
 void Reporting::Notification(const char *text, const CWnd *parent)
 //----------------------------------------------------------------
 {
@@ -127,6 +152,108 @@ RetryAnswer Reporting::RetryCancel(const char *text, const CWnd *parent)
 
 RetryAnswer Reporting::RetryCancel(const char *text, const char *caption, const CWnd *parent)
 //-------------------------------------------------------------------------------------------
+{
+	UINT result = ShowNotification(text, caption, MB_RETRYCANCEL, parent);
+	switch(result)
+	{
+	case IDRETRY:
+		return rtyRetry;
+	default:
+	case IDCANCEL:
+		return rtyCancel;
+	}
+}
+
+
+void Reporting::Notification(const std::wstring &text, const CWnd *parent)
+//------------------------------------------------------------------------
+{
+	Notification(text, MAINFRAME_TITLEW, parent);
+}
+
+
+void Reporting::Notification(const std::wstring &text, const std::wstring &caption, const CWnd *parent)
+//-----------------------------------------------------------------------------------------------------
+{
+	ShowNotification(text, caption, MB_OK, parent);
+}
+
+
+void Reporting::Information(const std::wstring &text, const CWnd *parent)
+//-----------------------------------------------------------------------
+{
+	Information(text, MAINFRAME_TITLEW, parent);
+}
+
+
+void Reporting::Information(const std::wstring &text, const std::wstring &caption, const CWnd *parent)
+//----------------------------------------------------------------------------------------------------
+{
+	ShowNotification(text, caption, MB_OK | MB_ICONINFORMATION, parent);
+}
+
+
+void Reporting::Warning(const std::wstring &text, const CWnd *parent)
+//-----------------------------------------------------------
+{
+	Warning(text, MAINFRAME_TITLEW L" - Warning", parent);
+}
+
+
+void Reporting::Warning(const std::wstring &text, const std::wstring &caption, const CWnd *parent)
+//------------------------------------------------------------------------------------------------
+{
+	ShowNotification(text, caption, MB_OK | MB_ICONWARNING, parent);
+}
+
+
+void Reporting::Error(const std::wstring &text, const CWnd *parent)
+//---------------------------------------------------------
+{
+	Error(text, MAINFRAME_TITLEW L" - Error", parent);
+}
+
+
+void Reporting::Error(const std::wstring &text, const std::wstring &caption, const CWnd *parent)
+//----------------------------------------------------------------------------------------------
+{
+	ShowNotification(text, caption, MB_OK | MB_ICONERROR, parent);
+}
+
+
+ConfirmAnswer Reporting::Confirm(const std::wstring &text, bool showCancel, bool defaultNo, const CWnd *parent)
+//-------------------------------------------------------------------------------------------------------------
+{
+	return Confirm(text, MAINFRAME_TITLEW L" - Confirmation", showCancel, defaultNo, parent);
+}
+
+
+ConfirmAnswer Reporting::Confirm(const std::wstring &text, const std::wstring &caption, bool showCancel, bool defaultNo, const CWnd *parent)
+//------------------------------------------------------------------------------------------------------------------------------------------
+{
+	UINT result = ShowNotification(text, caption, (showCancel ? MB_YESNOCANCEL : MB_YESNO) | MB_ICONQUESTION | (defaultNo ? MB_DEFBUTTON2 : 0), parent);
+	switch(result)
+	{
+	case IDYES:
+		return cnfYes;
+	case IDNO:
+		return cnfNo;
+	default:
+	case IDCANCEL:
+		return cnfCancel;
+	}
+}
+
+
+RetryAnswer Reporting::RetryCancel(const std::wstring &text, const CWnd *parent)
+//------------------------------------------------------------------------------
+{
+	return RetryCancel(text, MAINFRAME_TITLEW, parent);
+}
+
+
+RetryAnswer Reporting::RetryCancel(const std::wstring &text, const std::wstring &caption, const CWnd *parent)
+//-----------------------------------------------------------------------------------------------------------
 {
 	UINT result = ShowNotification(text, caption, MB_RETRYCANCEL, parent);
 	switch(result)
