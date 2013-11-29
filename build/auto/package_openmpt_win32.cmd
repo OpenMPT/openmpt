10ad3a8eea492dc326b44029b60ab181ce0f6681
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-11-29 16:15:32 +0000

    [Int] build: Add win64 automated build and package scripts. [Int] build: Add MIDI i/O plugin to automated builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3331 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/package_openmpt_win32.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+set GOT_REVISION=%1%
+set MY_DIR=%CD%
+
+
+
+cd bin\Win32 || goto error
+del /f /q openmpt-win32.tar
+del /f /q openmpt-win32-r%GOT_REVISION%.7z
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-r%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_i16.dll "MIDI Input Output.dll" || goto error
+"C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win32.tar openmpt-win32-r%GOT_REVISION%.7z || goto error
+del /f /q openmpt-win32-r%GOT_REVISION%.7z
+cd ..\.. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
---
+++ build/auto/vs2010_openmpt_win64.cmd
@@ -0,0 +1,39 @@
+@echo off
+
+set MY_DIR=%CD%
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" goto msvc64bit
+if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" goto msvc32bit
+
+goto error
+
+:msvc32bit
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+goto compile
+
+:msvc64bit
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+goto compile
+
+:compile
+
+
+
+xcopy /e /y /c ..\..\externals\*.* include
+
+cd mptrack || goto error
+devenv MPTRACK_10.sln /clean "Release|x64" || goto error
+devenv MPTRACK_10.sln /build "Release|x64" || goto error
+cd .. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
