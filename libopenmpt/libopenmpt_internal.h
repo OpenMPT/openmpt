b22e923cae25c0b2854e4643b62e45f788871287
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-11-29 02:53:15 +0000

    [Ref] libopenmpt: Remove NO_WINAMP, NO_XMPLAY and NO_LIBMODPLUG from BuildSettings.h .
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3330 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_internal.h
+++ libopenmpt/libopenmpt_internal.h
@@ -25,10 +25,7 @@
 #ifdef __cplusplus
 #if defined(LIBOPENMPT_BUILD_DLL) || defined(LIBOPENMPT_USE_DLL)
 #if defined(_MSC_VER) && !defined(_DLL)
-#if defined(NO_LIBMODPLUG) && defined(NO_WINAMP) && defined(NO_XMPLAY)
-/* do not warn if building libmodplug emulation or winamp plugin or xmplay plugin */
-#pragma message( "libopenmpt C++ interface is disabled if libopenmpt is built as a DLL and the runtime is statically linked. This is not supported by microsoft and cannot possibly work. Ever." )
-#endif
+/* #pragma message( "libopenmpt C++ interface is disabled if libopenmpt is built as a DLL and the runtime is statically linked. This is not supported by microsoft and cannot possibly work. Ever." ) */
 #undef LIBOPENMPT_CXX_API
 #define LIBOPENMPT_CXX_API LIBOPENMPT_API_HELPER_LOCAL
 #endif
