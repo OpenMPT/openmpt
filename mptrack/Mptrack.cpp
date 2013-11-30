193a9e9e22d49474fecd1aa0ec1c6bf979644149
Johannes Schultz <saga-games@users.sourceforge.net>
2013-11-30 18:13:47 +0000

    [Mod] New high-resolution application icon by Nobuyuki
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3340 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mptrack.cpp
+++ mptrack/Mptrack.cpp
@@ -809,7 +809,7 @@ BOOL CTrackApp::InitInstance()
 
 	// Register document templates
 	m_pModTemplate = new CModDocTemplate(
-		IDR_MODULETYPE,
+		IDR_MAINFRAME,
 		RUNTIME_CLASS(CModDoc),
 		RUNTIME_CLASS(CChildFrame), // custom MDI child frame
 		RUNTIME_CLASS(CModControlView));
