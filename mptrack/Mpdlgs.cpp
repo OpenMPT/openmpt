4c88fa4297768e06af7b8ae55fa1a5e2e6a84994
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-12-02 13:36:24 +0000

    [Mod] sounddev: Only show current/total buffers for devices that actually use > 2 fixed-size buffers.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3346 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mpdlgs.cpp
+++ mptrack/Mpdlgs.cpp
@@ -627,8 +627,13 @@ void COptionsSoundcard::UpdateStatistics()
 		const double currentUpdateInterval = pMainFrm->gpSoundDevice->GetCurrentUpdateInterval();
 		const uint32 samplerate = pMainFrm->gpSoundDevice->GetSettings().Samplerate;
 		std::string s;
-		s += mpt::String::Print("Buffer: %1%%\r\n", (bufferAttributes.Latency > 0.0) ? Util::Round<int64>(currentLatency / bufferAttributes.Latency * 100.0) : 0);
-		s += mpt::String::Print("Buffers: %1 (current: %2)\r\n", bufferAttributes.NumBuffers, (currentUpdateInterval > 0.0) ? Util::Round<int64>(bufferAttributes.Latency / currentUpdateInterval) : 0);
+		if(bufferAttributes.NumBuffers > 2)
+		{
+			s += mpt::String::Print("Buffer: %1%% (%2/%3)\r\n", (bufferAttributes.Latency > 0.0) ? Util::Round<int64>(currentLatency / bufferAttributes.Latency * 100.0) : 0, (currentUpdateInterval > 0.0) ? Util::Round<int64>(bufferAttributes.Latency / currentUpdateInterval) : 0, bufferAttributes.NumBuffers);
+		} else
+		{
+			s += mpt::String::Print("Buffer: %1%%\r\n", (bufferAttributes.Latency > 0.0) ? Util::Round<int64>(currentLatency / bufferAttributes.Latency * 100.0) : 0);
+		}
 		s += mpt::String::Print("Latency: %1 ms (current: %2 ms, %3 frames)\r\n", mpt::Format("%4.1f").ToString(bufferAttributes.Latency * 1000.0), mpt::Format("%4.1f").ToString(currentLatency * 1000.0), Util::Round<int64>(currentLatency * samplerate));
 		s += mpt::String::Print("Update Interval: %1 ms (current: %2 ms, %3 frames)\r\n", mpt::Format("%4.1f").ToString(bufferAttributes.UpdateInterval * 1000.0), mpt::Format("%4.1f").ToString(currentUpdateInterval * 1000.0), Util::Round<int64>(currentUpdateInterval * samplerate));
 		m_EditStatistics.SetWindowText(s.c_str());
