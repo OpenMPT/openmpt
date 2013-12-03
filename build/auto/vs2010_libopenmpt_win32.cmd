816493726408dd31584b4d5418307774d5e31ba9
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-12-03 15:04:13 +0000

    [Ref] libopenmpt: Rename libopenmpt_foobar2000 to foo_openmpt.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3357 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/vs2010_libopenmpt_win32.cmd
+++ build/auto/vs2010_libopenmpt_win32.cmd
@@ -23,7 +23,7 @@ xcopy /e /y /c ..\..\externals\*.* include
 
 cd libopenmpt || goto error
  devenv libopenmpt.sln /clean "Release|Win32" || goto error
- devenv libopenmpt_foobar2000.sln /clean "Release|Win32" || goto error
+ devenv foo_openmpt.sln /clean "Release|Win32" || goto error
 cd .. || goto error
 cd openmpt123 || goto error
  devenv openmpt123.sln /clean "Release|Win32" || goto error
@@ -31,7 +31,7 @@ cd .. || goto error
 
 cd libopenmpt || goto error
  devenv libopenmpt.sln /build "Release|Win32" || goto error
- devenv libopenmpt_foobar2000.sln /build "Release|Win32" || goto error
+ devenv foo_openmpt.sln /build "Release|Win32" || goto error
 cd .. || goto error
 cd openmpt123 || goto error
  devenv openmpt123.sln /build "Release|Win32" || goto error
