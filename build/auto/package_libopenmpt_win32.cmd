661a1a752ba11742616f7073ba2886ab891d793f
Jörn Heusipp <manxorist@users.sourceforge.net>
2013-12-03 15:26:50 +0000

    [Int] build: Add development files to automated libopenmpt win32 and win64 package builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3359 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_libopenmpt_win32.cmd
+++ build/auto/package_libopenmpt_win32.cmd
@@ -8,7 +8,15 @@ set MY_DIR=%CD%
 cd bin\Win32 || goto error
 del /f /q libopenmpt-win32.tar
 del /f /q libopenmpt-win32-r%GOT_REVISION%.7z
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 libopenmpt-win32-r%GOT_REVISION%.7z libopenmpt.dll libmodplug.dll in_openmpt.dll xmp-openmpt.dll foo_openmpt.dll libopenmpt_settings.dll openmpt123.exe || goto error
+mkdir inc lib bin
+mkdir inc\libopenmpt lib\Win32 bin\Win32
+copy /y ..\..\libopenmpt\libopenmpt.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt.hpp inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_config.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_version.h inc\libopenmpt\ || goto error
+copy /y libopenmpt.lib lib\Win32\ || goto error
+copy /y libopenmpt.dll bin\Win32\ || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 libopenmpt-win32-r%GOT_REVISION%.7z libopenmpt.dll libmodplug.dll in_openmpt.dll xmp-openmpt.dll foo_openmpt.dll libopenmpt_settings.dll openmpt123.exe inc\libopenmpt\libopenmpt.h inc\libopenmpt\libopenmpt.hpp inc\libopenmpt\libopenmpt_config.h inc\libopenmpt\libopenmpt_version.h lib\Win32\libopenmpt.lib bin\Win32\libopenmpt.dll || goto error
 "C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-win32.tar libopenmpt-win32-r%GOT_REVISION%.7z || goto error
 del /f /q libopenmpt-win32-r%GOT_REVISION%.7z
 cd ..\.. || goto error
