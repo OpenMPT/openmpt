b9aa670f0a7857f1b72fe85c56d7609348034192
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-01-04 17:41:10 +0000

    [Mod] build: Add LICENSE to binary packages.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3558 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_openmpt_win32.cmd
+++ build/auto/package_openmpt_win32.cmd
@@ -8,7 +8,8 @@ set MY_DIR=%CD%
 cd bin\Win32 || goto error
 del /f /q openmpt-win32.tar
 del /f /q openmpt-win32-r%GOT_REVISION%.7z
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-r%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_i16.dll "MIDI Input Output.dll" || goto error
+copy /y ..\..\LICENSE .\ || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-r%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_i16.dll "MIDI Input Output.dll" LICENSE || goto error
 "C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win32.tar openmpt-win32-r%GOT_REVISION%.7z || goto error
 del /f /q openmpt-win32-r%GOT_REVISION%.7z
 cd ..\.. || goto error
--- build/auto/package_openmpt_win64.cmd
+++ build/auto/package_openmpt_win64.cmd
@@ -8,7 +8,8 @@ set MY_DIR=%CD%
 cd bin\x64 || goto error
 del /f /q openmpt-win64.tar
 del /f /q openmpt-win64-r%GOT_REVISION%.7z
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win64-r%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_i16.dll "MIDI Input Output.dll" || goto error
+copy /y ..\..\LICENSE .\ || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win64-r%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_i16.dll "MIDI Input Output.dll" LICENSE || goto error
 "C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win64.tar openmpt-win64-r%GOT_REVISION%.7z || goto error
 del /f /q openmpt-win64-r%GOT_REVISION%.7z
 cd ..\.. || goto error
