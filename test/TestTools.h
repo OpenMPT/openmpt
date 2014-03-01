5bc232bc0709aedf9005b0b30fdcf1982a118323
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-03-01 16:05:16 +0000

    [Var] Test: Add license headers to TestTools*.* .
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3803 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- test/TestTools.h
+++ test/TestTools.h
@@ -1,3 +1,11 @@
+/*
+ * TestTools.h
+ * -----------
+ * Purpose: Unit test framework.
+ * Notes  : Currently somewhat unreadable :/
+ * Authors: OpenMPT Devs
+ * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
+ */
 
 #pragma once
 
