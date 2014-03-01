3ee8ad5ee0b07be90b37837267a08581827b3aa0
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-03-01 11:03:23 +0000

    [Ref] Test: Split tracker and library test framework tools into separate header files. [Mod] Test: Revert tracker test framework to r3782 .
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3802 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- test/test.cpp
+++ test/test.cpp
@@ -35,10 +35,6 @@
 #include "../common/mptFstream.h"
 #endif // !MODPLUG_TRACKER
 #include <limits>
-#ifndef MODPLUG_TRACKER
-#include <memory>
-#include <iostream>
-#endif // !MODPLUG_TRACKER
 #include <istream>
 #include <ostream>
 #include <sstream>
@@ -50,10 +46,7 @@
 #endif
 #endif
 
-
-
-namespace MptTest
-{
+#include "TestTools.h"
 
 
 
@@ -61,275 +54,15 @@ namespace MptTest
 #undef THIS_FILE
 #endif
 #ifdef _MSC_VER
-#define THIS_FILE "..\\test\\test.cpp" // __FILE__
+static const char * const THIS_FILE = "..\\test\\test.cpp"; // __FILE__
 #else
-#define THIS_FILE "test/test.cpp" // __FILE__
+static const char * const THIS_FILE = "test/test.cpp"; // __FILE__
 #endif
 
 
 
-#ifdef MODPLUG_TRACKER
-
-static noinline void ReportError(const char * file, int line, const char * description, bool abort)
-//-------------------------------------------------------------------------------------------------
+namespace MptTest
 {
-	std::string pos = std::string() + file + "(" + Stringify(line) + "):\n";
-	std::string message = pos + "Test failed: '" + description + "'";
-	if(IsDebuggerPresent())
-	{
-		message += "\n";
-		OutputDebugString(message.c_str());
-	} else
-	{
-		if(abort)
-		{
-			throw std::runtime_error(message);
-		} else
-		{
-			Reporting::Error(message.c_str(), "OpenMPT TestSuite");
-		}
-	}
-}
-
-static noinline void ReportExceptionError(const char * file, int line, const char * description, const char * exception_what)
-//---------------------------------------------------------------------------------------------------------------------------
-{
-	std::string pos = std::string() + file + "(" + Stringify(line) + "):\n";
-	std::string message;
-	if(exception_what)
-	{
-		message = pos + "Test '" + description + "' threw an exception, message:\n" + exception_what;
-	} else
-	{
-		message = pos + "Test '" + description + "' threw an unknown exception.";
-	}
-	if(IsDebuggerPresent())
-	{
-		message += "\n";
-		OutputDebugString(message.c_str());
-	} else
-	{
-		Reporting::Error(message.c_str(), "OpenMPT TestSuite");
-	}
-}
-
-static noinline void ReportException(const char * const file, const int line, const char * const description, bool rethrow)
-//-------------------------------------------------------------------------------------------------------------------------
-{
-	try
-	{
-		throw; // get the exception
-	} catch(std::exception &e)
-	{
-		ReportExceptionError(file, line, description, e.what());
-		if(rethrow)
-		{
-			throw;
-		}
-	} catch(...)
-	{
-		ReportExceptionError(file, line, description, nullptr);
-		if(rethrow)
-		{
-			throw;
-		}
-	}
-}
-
-#if defined(_MSC_VER) && defined(_M_IX86)
-// on x86, break directly using asm break interrupt instead of calling DebugBreak which breaks one stackframe deeper than we want
-#define MyDebugBreak() do { __asm { int 3 }; } while(0)
-#else
-#define MyDebugBreak() DebugBreak()
-#endif
-
-#define ReportExceptionAndBreak(file, line, description, rethrow) do { ReportException(file, line, description, rethrow); if(IsDebuggerPresent()) MyDebugBreak(); } while(0)
-#define ReportErrorAndBreak(file, line, description, progress)    do { ReportError(file, line, description, progress);    if(IsDebuggerPresent()) MyDebugBreak(); } while(0)
-
-#define MULTI_TEST_TRY   try {
-#define MULTI_TEST_START 
-#define MULTI_TEST_END   
-#define MULTI_TEST_CATCH } catch(...) { ReportExceptionAndBreak(THIS_FILE, __LINE__, description, false); }
-#define TEST_TRY         try {
-#define TEST_CATCH       } catch(...) { ReportExceptionAndBreak(file, line, description, true); }
-#define TEST_START()     do { } while(0)
-#define TEST_OK()        do { MPT_UNREFERENCED_PARAMETER(description); } while(0)
-#define TEST_FAIL()      ReportErrorAndBreak(file, line, description, false)
-#define TEST_FAIL_STOP() ReportErrorAndBreak(file, line, description, true)
-
-#else // !MODPLUG_TRACKER
-
-static std::string remove_newlines(std::string str)
-{
-	return mpt::String::Replace(mpt::String::Replace(str, "\n", " "), "\r", " ");
-}
-
-static noinline void show_start(const char * const file, const int line, const char * const description)
-{
-	std::cout << "Test: " << file << "(" << line << "): " << remove_newlines(description) << ": ";
-}
-
-static noinline void MultiTestStart(const char * const file, const int line, const char * const description)
-{
-	show_start(file, line, description);
-	std::cout << "..." << std::endl;
-}
-
-static noinline void show_ok(const char * const file, const int line, const char * const description)
-{
-	MPT_UNREFERENCED_PARAMETER(file);
-	MPT_UNREFERENCED_PARAMETER(line);
-	MPT_UNREFERENCED_PARAMETER(description);
-	std::cout << "PASS" << std::endl;
-}
-
-static noinline void show_fail(const char * const file, const int line, const char * const description, bool exception = false, const char * const exception_text = nullptr)
-{
-	std::cout << "FAIL" << std::endl;
-	std::cout.flush();
-	if(!exception)
-	{
-		std::cerr << "FAIL: " << file << "(" << line << "): " << remove_newlines(description) << std::endl;
-	} else if(!exception_text || (exception_text && std::string(exception_text).empty()))
-	{
-		std::cerr << "FAIL: " << file << "(" << line << "): " << remove_newlines(description) << " EXCEPTION!" << std::endl;
-	} else
-	{
-		std::cerr << "FAIL: " << file << "(" << line << "): " << remove_newlines(description) << " EXCEPTION: " << exception_text << std::endl;
-	}
-	std::cerr.flush();
-}
-
-static int fail_count = 0;
-
-static noinline void CheckFailCountOrThrow()
-{
-	if(fail_count > 0)
-	{
-		throw std::runtime_error("Test failed.");
-	}
-}
-
-static noinline void ReportException(const char * const file, const int line, const char * const description)
-{
-	try
-	{
-		throw; // get the exception
-	} catch(std::exception & e)
-	{
-		show_fail(file, line, description, true, e.what());
-		throw; // rethrow
-	} catch(...)
-	{
-		show_fail(file, line, description, true);
-		throw; // rethrow
-	}
-}
-
-static noinline void TestFail(const char * const file, const int line, const char * const description)
-{
-	show_fail(file, line, description);
-	fail_count++;
-}
-
-static noinline void TestFailStop(const char * const file, const int line, const char * const description)
-{
-	show_fail(file, line, description);
-	fail_count++;
-	throw std::runtime_error(std::string("Test failed: ") + description);
-}
-
-#define MULTI_TEST_TRY   try { \
-                          fail_count = 0;
-#define MULTI_TEST_START MultiTestStart(THIS_FILE, __LINE__, description);
-#define MULTI_TEST_END   show_start(THIS_FILE, __LINE__, description);
-#define MULTI_TEST_CATCH  CheckFailCountOrThrow(); \
-                          show_ok(THIS_FILE, __LINE__, description); \
-                         } catch(...) { \
-                          ReportException(THIS_FILE, __LINE__, description); \
-                         }
-#define TEST_TRY         try {
-#define TEST_CATCH       } catch(...) { \
-                          fail_count++; \
-                          ReportException(file, line, description); \
-                          throw; \
-                         }
-#define TEST_START()     show_start(file, line, description)
-#define TEST_OK()        show_ok(file, line, description)
-#define TEST_FAIL()      TestFail(file, line, description)
-#define TEST_FAIL_STOP() TestFailStop(file, line, description)
-
-#endif // MODPLUG_TRACKER
-
-
-template <typename Tx, typename Ty>
-noinline void VerifyEqualImpl(const Tx &x, const Ty &y, const char *const description, const char *const file, const int line)
-{
-	TEST_TRY
-	TEST_START();
-	if(x != y)
-	{
-		TEST_FAIL();
-	} else
-	{
-		TEST_OK();
-	}
-	TEST_CATCH
-}
-
-
-template <typename Tx, typename Ty>
-noinline void VerifyEqualNonContImpl(const Tx &x, const Ty &y, const char *const description, const char *const file, const int line)
-{
-	TEST_TRY
-	TEST_START();
-	if(x != y)
-	{
-		TEST_FAIL_STOP();
-	} else
-	{
-		TEST_OK();
-	}
-	TEST_CATCH
-}
-
-
-template <typename Tx, typename Ty>
-noinline void VerifyEqualQuietNonContImpl(const Tx &x, const Ty &y, const char *const description, const char *const file, const int line)
-{
-	TEST_TRY
-	if(x != y)
-	{
-		TEST_FAIL_STOP();
-	}
-	TEST_CATCH
-}
-
-
-//Verify that given parameters are 'equal'(show error message if not).
-//The exact meaning of equality is not specified; for now using operator!=.
-//The macro is active in both 'debug' and 'release' build.
-#define VERIFY_EQUAL(x,y)	VerifyEqualImpl( (x) , (y) , #x " == " #y , THIS_FILE, __LINE__)
-
-
-// Like VERIFY_EQUAL, but throws exception if comparison fails.
-#define VERIFY_EQUAL_NONCONT(x,y)	VerifyEqualNonContImpl( (x) , (y) , #x " == " #y , THIS_FILE, __LINE__)
-
-
-// Like VERIFY_EQUAL_NONCONT, but do not show message if test succeeds
-#define VERIFY_EQUAL_QUIET_NONCONT(x,y)	VerifyEqualQuietNonContImpl( (x) , (y) , #x " == " #y , THIS_FILE, __LINE__)
-
-
-#define DO_TEST(func) \
-do{ \
-	const char * description = #func ; \
-	MULTI_TEST_TRY \
-	MULTI_TEST_START \
-	func(); \
-	MULTI_TEST_END \
-	MULTI_TEST_CATCH \
-}while(0)
-
 
 
 
