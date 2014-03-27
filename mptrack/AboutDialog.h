4907e268039049906f6b31cf1a5937e7f2806109
Johannes Schultz <saga-games@users.sourceforge.net>
2014-03-27 12:57:41 +0000

    [Ref] Make mptrack.cpp a tiny bit more lightweight by moving About dialog to AboutDialog.cpp/.h
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@3976 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ mptrack/AboutDialog.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "CreditStatic.h"
+
+namespace PNG { struct Bitmap; }
+
+//===============================
+class CRippleBitmap: public CWnd
+//===============================
+{
+protected:
+	BITMAPINFOHEADER bi;
+	PNG::Bitmap *bitmapSrc, *bitmapTarget;
+	std::vector<int32_t> offset1, offset2;
+	int32_t *frontBuf, *backBuf;
+	DWORD lastFrame;	// Time of last frame
+	bool frame;			// Backbuffer toggle
+	bool damp;			// Ripple damping status
+	bool activity;		// There are actually some ripples
+	bool showMouse;
+
+public:
+	static CRippleBitmap *instance;
+
+	CRippleBitmap();
+	~CRippleBitmap();
+	bool Animate();
+
+protected:
+	virtual void OnPaint();
+	virtual BOOL OnEraseBkgnd(CDC *) { return TRUE; }
+
+	DECLARE_MESSAGE_MAP()
+	void OnMouseMove(UINT nFlags, CPoint point);
+	void OnMouseHover(UINT nFlags, CPoint point) { OnMouseMove(nFlags, point); }
+	void OnMouseLeave();
+};
+
+
+//=============================
+class CAboutDlg: public CDialog
+//=============================
+{
+protected:
+	CRippleBitmap m_bmp;
+	CCreditStatic m_static;
+
+public:
+	static CAboutDlg *instance;
+
+	~CAboutDlg();
+
+	// Implementation
+protected:
+	virtual BOOL OnInitDialog();
+	virtual void OnOK();
+	virtual void OnCancel();
+};
--- mptrack/Mptrack.cpp
+++ mptrack/Mptrack.cpp
@@ -17,7 +17,6 @@
 #include "globals.h"
 #include "Dlsbank.h"
 #include "vstplug.h"
-#include "CreditStatic.h"
 #include "commctrl.h"
 #include "../common/version.h"
 #include "../test/test.h"
@@ -27,6 +26,7 @@
 #include "../common/StringFixer.h"
 #include "ExceptionHandler.h"
 #include "CloseMainDialog.h"
+#include "AboutDialog.h"
 #include "AutoSaver.h"
 #include "FileDialog.h"
 #include "PNG.h"
@@ -1217,299 +1217,13 @@ void CTrackApp::OnFileOpen()
 }
 
 
-/////////////////////////////////////////////////////////////////////////////
-// CAboutDlg dialog used for App About
-
-//===============================
-class CRippleBitmap: public CWnd
-//===============================
-{
-protected:
-	BITMAPINFOHEADER bi;
-	PNG::Bitmap *bitmapSrc, *bitmapTarget;
-	std::vector<int32_t> offset1, offset2;
-	int32_t *frontBuf, *backBuf;
-	DWORD lastFrame;	// Time of last frame
-	bool frame;			// Backbuffer toggle
-	bool damp;			// Ripple damping status
-	bool activity;		// There are actually some ripples
-	bool showMouse;
-
-public:
-	CRippleBitmap();
-	~CRippleBitmap();
-	bool Animate();
-	uint32_t Width() const { return bitmapSrc->width; }
-	uint32_t Height() const { return bitmapSrc->height; }
-
-protected:
-	virtual void OnPaint();
-	virtual BOOL OnEraseBkgnd(CDC *) { return TRUE; }
-
-	DECLARE_MESSAGE_MAP()
-	void OnMouseMove(UINT nFlags, CPoint point);
-	void OnMouseHover(UINT nFlags, CPoint point) { OnMouseMove(nFlags, point); }
-	void OnMouseLeave();
-};
-
-static CRippleBitmap *gpAboutAnimation = nullptr;
-
-BEGIN_MESSAGE_MAP(CRippleBitmap, CWnd)
-	ON_WM_PAINT()
-	ON_WM_ERASEBKGND()
-	ON_WM_MOUSEMOVE()
-	ON_WM_MOUSEHOVER()
-	ON_WM_MOUSELEAVE()
-END_MESSAGE_MAP()
-
-
-CRippleBitmap::CRippleBitmap()
-//----------------------------
-{
-	bitmapSrc = PNG::ReadPNG(MAKEINTRESOURCE(IDB_MPTRACK));
-	bitmapTarget = new PNG::Bitmap(bitmapSrc->width, bitmapSrc->height);
-	offset1.assign(bitmapSrc->GetNumPixels(), 0);
-	offset2.assign(bitmapSrc->GetNumPixels(), 0);
-	frontBuf = &offset2[0];
-	backBuf = &offset1[0];
-	activity = true;
-	lastFrame = 0;
-	frame = false;
-	damp = true;
-	showMouse = true;
-
-	// Pre-fill first and last row of output bitmap, since those won't be touched.
-	const PNG::Pixel *in1 = bitmapSrc->GetPixels(), *in2 = bitmapSrc->GetPixels() + (bitmapSrc->height - 1) * bitmapSrc->width;
-	PNG::Pixel *out1 = bitmapTarget->GetPixels(), *out2 = bitmapTarget->GetPixels() + (bitmapSrc->height - 1) * bitmapSrc->width;
-	for(uint32_t i = 0; i < bitmapSrc->width; i++)
-	{
-		*(out1++) = *(in1++);
-		*(out2++) = *(in2++);
-	}
-
-	MemsetZero(bi);
-	bi.biSize = sizeof(BITMAPINFOHEADER);
-	bi.biWidth = bitmapSrc->width;
-	bi.biHeight = -(int32_t)bitmapSrc->height;
-	bi.biPlanes = 1;
-	bi.biBitCount = 32;
-	bi.biCompression = BI_RGB;
-	bi.biSizeImage = bitmapSrc->width * bitmapSrc->height * 4;
-
-	gpAboutAnimation = this;
-}
-
-
-CRippleBitmap::~CRippleBitmap()
-//-----------------------------
-{
-	delete bitmapSrc;
-	delete bitmapTarget;
-	if (gpAboutAnimation == this) gpAboutAnimation = nullptr;
-}
-
-
-void CRippleBitmap::OnMouseMove(UINT nFlags, CPoint point)
-//--------------------------------------------------------
-{
-	// Initiate ripples at cursor location
-	Limit(point.x, 1, int(bitmapSrc->width) - 2);
-	Limit(point.y, 2, int(bitmapSrc->height) - 3);
-	int32_t *p = backBuf + point.x + point.y * bitmapSrc->width;
-	p[0] += (nFlags & MK_LBUTTON) ? 50 : 150;
-	p[0] += (nFlags & MK_MBUTTON) ? 150 : 0;
-
-	int32_t w = bitmapSrc->width;
-	// Make the initial point of this ripple a bit "fatter".
-	p[-1] += p[0] / 2;     p[1] += p[0] / 2;
-	p[-w] += p[0] / 2;     p[w] += p[0] / 2;
-	p[-w - 1] += p[0] / 4; p[-w + 1] += p[0] / 4;
-	p[w - 1] += p[0] / 4;  p[w + 1] += p[0] / 4;
-
-	damp = !(nFlags & MK_RBUTTON);
-	activity = true;
-
-	TRACKMOUSEEVENT me;
-	me.cbSize = sizeof(TRACKMOUSEEVENT);
-	me.dwFlags = TME_LEAVE | TME_HOVER;
-	me.hwndTrack = m_hWnd;
-	me.dwHoverTime = 1500;
-
-	if(TrackMouseEvent(&me) && showMouse)
-	{
-		ShowCursor(FALSE);
-		showMouse = false;
-	}
-}
-
-
-void CRippleBitmap::OnMouseLeave()
-//--------------------------------
-{
-	if(!showMouse)
-	{
-		ShowCursor(TRUE);
-		showMouse = true;
-	}
-}
-
-
-void CRippleBitmap::OnPaint()
-//---------------------------
-{
-	CPaintDC dc(this);
-
-	SetDIBitsToDevice(dc.m_hDC,
-		0, 0,
-		bitmapTarget->width, bitmapTarget->height, 0, 0,
-		0, bitmapTarget->height,
-		bitmapTarget->GetPixels(), reinterpret_cast<BITMAPINFO *>(&bi), DIB_RGB_COLORS);
-}
-
-
-bool CRippleBitmap::Animate()
-//---------------------------
-{
-	// Were there any pixels being moved in the last frame?
-	if(!activity)
-		return FALSE;
-
-	DWORD now = timeGetTime();
-	if(now - lastFrame < 15)
-		return TRUE;
-	lastFrame = now;
-	activity = false;
-
-	frontBuf = &(frame ? offset2 : offset1)[0];
-	backBuf = &(frame ? offset1 : offset2)[0];
-
-	// Spread the ripples...
-	const int32_t w = bitmapSrc->width, h = bitmapSrc->height;
-	const int32_t numPixels = w * (h - 2);
-	const int32_t *back = backBuf + w;
-	int32_t *front = frontBuf + w;
-	for(int32_t i = numPixels; i != 0; i--, back++, front++)
-	{
-		(*front) = (back[-1] + back[1] + back[w] + back[-w]) / 2 - (*front);
-		if(damp) (*front) -= (*front) >> 5;
-	}
-
-	// ...and compute the final picture.
-	const int32_t *offset = frontBuf + w;
-	const PNG::Pixel *pixelIn = bitmapSrc->GetPixels() + w;
-	PNG::Pixel *pixelOut = bitmapTarget->GetPixels() + w;
-	PNG::Pixel *limitMin = bitmapSrc->GetPixels(), *limitMax = bitmapSrc->GetPixels() + bitmapSrc->GetNumPixels() - 1;
-	for(int32_t i = numPixels; i != 0; i--, pixelIn++, pixelOut++, offset++)
-	{
-		const int32_t xOff = offset[-1] - offset[1];
-		const int32_t yOff = offset[-w] - offset[w];
-		const PNG::Pixel *p = pixelIn + xOff + yOff * w;
-		Limit(p, limitMin, limitMax);
-		// Add a bit of shading depending on how far we're displacing the pixel...
-		pixelOut->r = (uint8_t)Clamp(p->r + (p->r * xOff) / 32, 0, 255);
-		pixelOut->g = (uint8_t)Clamp(p->g + (p->g * xOff) / 32, 0, 255);
-		pixelOut->b = (uint8_t)Clamp(p->b + (p->b * xOff) / 32, 0, 255);
-		// ...and mix it with original picture
-		pixelOut->r = (pixelOut->r + pixelIn->r) / 2u;
-		pixelOut->g = (pixelOut->g + pixelIn->g) / 2u;
-		pixelOut->b = (pixelOut->b + pixelIn->b) / 2u;
-
-		// And now some cheap image smoothing...
-		if(xOff | yOff)
-		{
-			pixelOut[-1].r = (pixelOut->r + pixelOut[-1].r) / 2u;
-			pixelOut[-1].g = (pixelOut->g + pixelOut[-1].g) / 2u;
-			pixelOut[-1].b = (pixelOut->b + pixelOut[-1].b) / 2u;
-			pixelOut[-w].r = (pixelOut->r + pixelOut[-w].r) / 2u;
-			pixelOut[-w].g = (pixelOut->g + pixelOut[-w].g) / 2u;
-			pixelOut[-w].b = (pixelOut->b + pixelOut[-w].b) / 2u;
-			activity = true;	// Also use this to update activity status...
-		}
-	}
-
-	frame = !frame;
-
-	InvalidateRect(NULL, FALSE);
-	UpdateWindow();
-	Sleep(10); 	//give away some CPU
-
-	return true;
-}
-
-
-//=============================
-class CAboutDlg: public CDialog
-//=============================
-{
-protected:
-	CRippleBitmap m_bmp;
-	CCreditStatic m_static;
-
-public:
-	~CAboutDlg();
-
-// Implementation
-protected:
-	virtual BOOL OnInitDialog();
-	virtual void OnOK();
-	virtual void OnCancel();
-};
-
-static CAboutDlg *gpAboutDlg = NULL;
-
-
-CAboutDlg::~CAboutDlg()
-//---------------------
-{
-	gpAboutDlg = NULL;
-}
-
-void CAboutDlg::OnOK()
-//--------------------
-{
-	gpAboutAnimation = NULL;
-	gpAboutDlg = NULL;
-	DestroyWindow();
-	delete this;
-}
-
-
-void CAboutDlg::OnCancel()
-//------------------------
-{
-	OnOK();
-}
-
-
-BOOL CAboutDlg::OnInitDialog()
-//----------------------------
-{
-	CDialog::OnInitDialog();
-
-	m_bmp.SubclassDlgItem(IDC_BITMAP1, this);
-
-	SetDlgItemText(IDC_EDIT2, CString("Build Date: ") + MptVersion::GetBuildDateString().c_str());
-	SetDlgItemText(IDC_EDIT3, CString("OpenMPT ") + MptVersion::GetVersionStringExtended().c_str());
-	m_static.SubclassDlgItem(IDC_CREDITS,this);
-	m_static.SetCredits((mpt::String::Replace(mpt::To(mpt::CharsetLocale, mpt::CharsetUTF8, MptVersion::GetFullCreditsString()), "\n", "|") + "|" + mpt::String::Replace(MptVersion::GetContactString(), "\n", "|" ) + "||||||").c_str());
-	m_static.SetSpeed(DISPLAY_SLOW);
-	m_static.SetColor(BACKGROUND_COLOR, RGB(138, 165, 219)); // Background Colour
-	m_static.SetTransparent(); // Set parts of bitmaps with RGB(192,192,192) transparent
-	m_static.SetGradient(GRADIENT_LEFT_DARK);  // Background goes from blue to black from left to right
-	// m_static.SetBkImage(IDB_BITMAP1); // Background image
-	m_static.StartScrolling();
-	return TRUE;	// return TRUE unless you set the focus to a control
-					// EXCEPTION: OCX Property Pages should return FALSE
-}
-
-
 // App command to run the dialog
 void CTrackApp::OnAppAbout()
 //--------------------------
 {
-	if (gpAboutDlg) return;
-	gpAboutDlg = new CAboutDlg();
-	gpAboutDlg->Create(IDD_ABOUTBOX, m_pMainWnd);
+	if (CAboutDlg::instance) return;
+	CAboutDlg::instance = new CAboutDlg();
+	CAboutDlg::instance->Create(IDD_ABOUTBOX, m_pMainWnd);
 }
 
 
@@ -1662,9 +1376,9 @@ BOOL CTrackApp::OnIdle(LONG lCount)
 			StopSplashScreen();
 		}
 	}
-	if (gpAboutAnimation)
+	if (CRippleBitmap::instance)
 	{
-		if (gpAboutAnimation->Animate()) return TRUE;
+		if (CRippleBitmap::instance->Animate()) return TRUE;
 	}
 
 	// Call plugins idle routine for open editor
@@ -1744,11 +1458,11 @@ void DrawButtonRect(HDC hdc, LPRECT lpRect, LPCSTR lpszText, BOOL bDisabled, BOO
 //-------------------------------------------------------------------------------------------------------
 {
 	RECT rect;
-	HGDIOBJ oldpen = ::SelectObject(hdc, (bPushed) ? CMainFrame::penDarkGray : CMainFrame::penLightGray);
+	HGDIOBJ oldpen = SelectPen(hdc, (bPushed) ? CMainFrame::penDarkGray : CMainFrame::penLightGray);
 	::MoveToEx(hdc, lpRect->left, lpRect->bottom-1, NULL);
 	::LineTo(hdc, lpRect->left, lpRect->top);
 	::LineTo(hdc, lpRect->right-1, lpRect->top);
-	::SelectObject(hdc, (bPushed) ? CMainFrame::penLightGray : CMainFrame::penDarkGray);
+	SelectPen(hdc, (bPushed) ? CMainFrame::penLightGray : CMainFrame::penDarkGray);
 	::LineTo(hdc, lpRect->right-1, lpRect->bottom-1);
 	::LineTo(hdc, lpRect->left, lpRect->bottom-1);
 	rect.left = lpRect->left + 1;
@@ -1756,7 +1470,7 @@ void DrawButtonRect(HDC hdc, LPRECT lpRect, LPCSTR lpszText, BOOL bDisabled, BOO
 	rect.right = lpRect->right - 1;
 	rect.bottom = lpRect->bottom - 1;
 	::FillRect(hdc, &rect, CMainFrame::brushGray);
-	::SelectObject(hdc, oldpen);
+	SelectPen(hdc, oldpen);
 	if ((lpszText) && (lpszText[0]))
 	{
 		if (bPushed)
