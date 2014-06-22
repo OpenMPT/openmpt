e1bcf3725b52ca7040162425cab3abcd0f6f38ce
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-06-22 09:02:07 +0000

    [Ref] mptIO: Silence stupid MSVC warnings. [Ref] mptIO: Add ReadBinaryLE and WriteBinaryLE. [Ref] mptIO: Add ReadBinaryTruncatedLE and WriteBinaryTruncatedLE.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/manx/io-and-endian-fixes@4154 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/mptIO.h
+++ common/mptIO.h
@@ -37,31 +37,31 @@ namespace IO {
 typedef int64 Offset;
 
 STATIC_ASSERT(sizeof(std::streamoff) == 8); // Assert 64bit file support.
-inline bool IsValid(std::ostream & f) { return f; }
-inline bool IsValid(std::istream & f) { return f; }
-inline bool IsValid(std::iostream & f) { return f; }
+inline bool IsValid(std::ostream & f) { f; return !f.fail(); }
+inline bool IsValid(std::istream & f) { f; return !f.fail(); }
+inline bool IsValid(std::iostream & f) { f; return !f.fail(); }
 inline IO::Offset TellRead(std::istream & f) { return f.tellg(); }
 inline IO::Offset TellWrite(std::ostream & f) { return f.tellp(); }
-inline bool SeekBegin(std::ostream & f) { return f.seekp(0); }
-inline bool SeekBegin(std::istream & f) { return f.seekg(0); }
-inline bool SeekBegin(std::iostream & f) { return f.seekg(0) && f.seekp(0); }
-inline bool SeekEnd(std::ostream & f) { return f.seekp(0, std::ios::end); }
-inline bool SeekEnd(std::istream & f) { return f.seekg(0, std::ios::end); }
-inline bool SeekEnd(std::iostream & f) { return f.seekg(0, std::ios::end) && f.seekp(0, std::ios::end); }
-inline bool SeekAbsolute(std::ostream & f, IO::Offset pos) { return f.seekp(pos, std::ios::beg); }
-inline bool SeekAbsolute(std::istream & f, IO::Offset pos) { return f.seekg(pos, std::ios::beg); }
-inline bool SeekAbsolute(std::iostream & f, IO::Offset pos) { return f.seekg(pos, std::ios::beg) && f.seekp(pos, std::ios::beg); }
-inline bool SeekRelative(std::ostream & f, IO::Offset off) { return f.seekp(off, std::ios::cur); }
-inline bool SeekRelative(std::istream & f, IO::Offset off) { return f.seekg(off, std::ios::cur); }
-inline bool SeekRelative(std::iostream & f, IO::Offset off) { return f.seekg(off, std::ios::cur) && f.seekp(off, std::ios::cur); }
+inline bool SeekBegin(std::ostream & f) { f.seekp(0); return !f.fail(); }
+inline bool SeekBegin(std::istream & f) { f.seekg(0); return !f.fail(); }
+inline bool SeekBegin(std::iostream & f) { f.seekg(0); f.seekp(0); return !f.fail(); }
+inline bool SeekEnd(std::ostream & f) { f.seekp(0, std::ios::end); return !f.fail(); }
+inline bool SeekEnd(std::istream & f) { f.seekg(0, std::ios::end); return !f.fail(); }
+inline bool SeekEnd(std::iostream & f) { f.seekg(0, std::ios::end); f.seekp(0, std::ios::end); return !f.fail(); }
+inline bool SeekAbsolute(std::ostream & f, IO::Offset pos) { f.seekp(pos, std::ios::beg); return !f.fail(); }
+inline bool SeekAbsolute(std::istream & f, IO::Offset pos) { f.seekg(pos, std::ios::beg); return !f.fail(); }
+inline bool SeekAbsolute(std::iostream & f, IO::Offset pos) { f.seekg(pos, std::ios::beg); f.seekp(pos, std::ios::beg); return !f.fail(); }
+inline bool SeekRelative(std::ostream & f, IO::Offset off) { f.seekp(off, std::ios::cur); return !f.fail(); }
+inline bool SeekRelative(std::istream & f, IO::Offset off) { f.seekg(off, std::ios::cur); return !f.fail(); }
+inline bool SeekRelative(std::iostream & f, IO::Offset off) { f.seekg(off, std::ios::cur); f.seekp(off, std::ios::cur); return !f.fail(); }
 inline IO::Offset ReadRaw(std::istream & f, uint8 * data, std::size_t size) { return f.read(reinterpret_cast<char *>(data), size) ? f.gcount() : std::streamsize(0); }
 inline IO::Offset ReadRaw(std::istream & f, char * data, std::size_t size) { return f.read(data, size) ? f.gcount() : std::streamsize(0); }
 inline IO::Offset ReadRaw(std::istream & f, void * data, std::size_t size) { return f.read(reinterpret_cast<char *>(data), size) ? f.gcount() : std::streamsize(0); }
-inline bool WriteRaw(std::ostream & f, const uint8 * data, std::size_t size) { return f.write(reinterpret_cast<const char *>(data), size); }
-inline bool WriteRaw(std::ostream & f, const char * data, std::size_t size) { return f.write(data, size); }
-inline bool WriteRaw(std::ostream & f, const void * data, std::size_t size) { return f.write(reinterpret_cast<const char *>(data), size); }
+inline bool WriteRaw(std::ostream & f, const uint8 * data, std::size_t size) { f.write(reinterpret_cast<const char *>(data), size); return !f.fail(); }
+inline bool WriteRaw(std::ostream & f, const char * data, std::size_t size) { f.write(data, size); return !f.fail(); }
+inline bool WriteRaw(std::ostream & f, const void * data, std::size_t size) { f.write(reinterpret_cast<const char *>(data), size); return !f.fail(); }
 inline bool IsEof(std::istream & f) { return f.eof(); }
-inline bool Flush(std::ostream & f) { return f.flush(); }
+inline bool Flush(std::ostream & f) { f.flush(); return !f.fail(); }
 
 inline bool IsValid(FILE* & f) { return f != NULL; }
 #if MPT_COMPILER_MSVC
@@ -98,9 +98,81 @@ inline bool IsEof(FILE * & f) { return feof(f) != 0; }
 inline bool Flush(FILE* & f) { return fflush(f) == 0; }
 
 template <typename Tbinary, typename Tfile>
+inline bool Read(Tfile & f, const Tbinary & v)
+{
+	return IO::ReadRaw(f, mpt::GetRawBytes(v), sizeof(Tbinary)) == sizeof(Tbinary);
+}
+
+template <typename Tbinary, typename Tfile>
 inline bool Write(Tfile & f, const Tbinary & v)
 {
 	return IO::WriteRaw(f, mpt::GetRawBytes(v), sizeof(Tbinary));
+}
+
+template <typename T, typename Tfile>
+inline bool ReadBinaryLE(Tfile & f, T & v)
+{
+	bool result = false;
+	#ifdef HAS_TYPE_TRAITS
+		static_assert(std::is_trivial<T>::value == true, "");
+	#endif
+	uint8 bytes[sizeof(T)];
+	std::memset(bytes, 0, sizeof(T));
+	result = IO::ReadRaw(f, bytes, sizeof(T)) == sizeof(T);
+	#ifdef MPT_PLATFORM_BIG_ENDIAN
+		std::reverse(bytes, bytes + sizeof(T));
+	#endif
+	std::memcpy(&v, bytes, sizeof(T));
+	return result;
+}
+
+template <typename T, typename Tfile>
+inline bool ReadBinaryTruncatedLE(Tfile & f, T & v, std::size_t size)
+{
+	bool result = false;
+	#ifdef HAS_TYPE_TRAITS
+		static_assert(std::is_trivial<T>::value == true, "");
+	#endif
+	uint8 bytes[sizeof(T)];
+	std::memset(bytes, 0, sizeof(T));
+	result = IO::ReadRaw(f, bytes, std::min(size, sizeof(T))) == std::min(size, sizeof(T));
+	#ifdef MPT_PLATFORM_BIG_ENDIAN
+		std::reverse(bytes, bytes + sizeof(T));
+	#endif
+	std::memcpy(&v, bytes, sizeof(T));
+	return result;
+}
+
+template <typename T, typename Tfile>
+inline bool WriteBinaryLE(Tfile & f, const T & v)
+{
+	bool result = false;
+	#ifdef HAS_TYPE_TRAITS
+		static_assert(std::is_trivial<T>::value == true, "");
+	#endif
+	uint8 bytes[sizeof(T)];
+	std::memcpy(bytes, &v, sizeof(T));
+	#ifdef MPT_PLATFORM_BIG_ENDIAN
+		std::reverse(bytes, bytes + sizeof(T));
+	#endif
+	result = IO::WriteRaw(f, bytes, sizeof(T));
+	return result;	
+}
+
+template <typename T, typename Tfile>
+inline bool WriteBinaryTruncatedLE(Tfile & f, const T & v, std::size_t size)
+{
+	bool result = false;
+	#ifdef HAS_TYPE_TRAITS
+		static_assert(std::is_trivial<T>::value == true, "");
+	#endif
+	uint8 bytes[sizeof(T)];
+	std::memcpy(bytes, &v, sizeof(T));
+	#ifdef MPT_PLATFORM_BIG_ENDIAN
+		std::reverse(bytes, bytes + sizeof(T));
+	#endif
+	result = IO::WriteRaw(f, bytes, std::min(size, sizeof(T)));
+	return result;	
 }
 
 template <typename T, typename Tfile>
