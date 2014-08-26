8363b3b54515032d914a59082d9706580e17b429
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-08-26 11:28:22 +0000

    [Fix] openmpt123: NetBSD defines both TIOCGSIZE and TIOCGWINSZ, but struct ttysize does not contain the referenced members. (Patch by wiz, <https://bugs.openmpt.org/view.php?id=580>)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4215 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- openmpt123/openmpt123.hpp
+++ openmpt123/openmpt123.hpp
@@ -336,17 +336,17 @@ struct commandlineflags {
 					terminal_height = tmp;
 				}
 			}
-			#if defined(TIOCGSIZE)
-				struct ttysize ts;
-				if ( ioctl( STDERR_FILENO, TIOCGSIZE, &ts ) >= 0 ) {
-					terminal_width = ts.ts_cols;
-					terminal_height = ts.ts_rows;
-				}
-			#elif defined(TIOCGWINSZ)
+			#if defined(TIOCGWINSZ)
 				struct winsize ts;
 				if ( ioctl( STDERR_FILENO, TIOCGWINSZ, &ts ) >= 0 ) {
 					terminal_width = ts.ws_col;
 					terminal_height = ts.ws_row;
+				}
+			#elif defined(TIOCGSIZE)
+				struct ttysize ts;
+				if ( ioctl( STDERR_FILENO, TIOCGSIZE, &ts ) >= 0 ) {
+					terminal_width = ts.ts_cols;
+					terminal_height = ts.ts_rows;
 				}
 			#endif
 		}
