OpenMPT/openmpt

commit 288dc53c364fe0f672b56b85bcf159f9c988fe17

[Fix] ASIO: If the corresponding soundcard is currently unavailable, some ASIO drivers allow opening the driver and report zero input and output channels in that case. Other driver do not support loading the driver at all in that case. In both cases, OpenMPT will now disallow selecting the driver in the setup dialog and hide it until restartet or the sound devices get rescanned. This avoids confusion in particular for the case where 0 channels get reported, which until now had the chance of corrupting the channel mapping.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4306 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Mpdlgs.cpp
@@ -157,7 +157,21 @@ COptionsSoundcard::COptionsSoundcard(SoundDevice::ID dev)
 	, m_CurrentDeviceDynamicCaps(theApp.GetSoundDevicesManager()->GetDeviceDynamicCaps(dev, TrackerSettings::Instance().GetSampleRates(), CMainFrame::GetMainFrame(), CMainFrame::GetMainFrame()->gpSoundDevice, true))
 	, m_Settings(TrackerSettings::Instance().GetSoundDeviceSettings(dev))
 {
-	return;
+	if(theApp.GetSoundDevicesManager()->IsDeviceUnavailable(dev))
+	{
+		Reporting::Information("Device not availble. Reverting to default device.");
+		// if the device is unavailable, use the default device
+		SoundDevice::ID newdev = theApp.GetSoundDevicesManager()->FindDeviceInfoBestMatch(std::wstring()).id;
+		if(newdev != dev)
+		{
+			Reporting::Information("Device not availble. Reverting to default device.");
+			SetDevice(newdev);
+			UpdateEverything();
+		} else
+		{
+			Reporting::Warning("Device not availble.");
+		}
+	}
 }
 
 
@@ -171,6 +185,20 @@ void COptionsSoundcard::SetDevice(SoundDevice::ID dev, bool forceReload)
 	if(deviceChanged || forceReload)
 	{
 		m_Settings = TrackerSettings::Instance().GetSoundDeviceSettings(dev);
+	}
+	if(theApp.GetSoundDevicesManager()->IsDeviceUnavailable(dev))
+	{
+		// if the device is unavailable, use the default device
+		SoundDevice::ID newdev = theApp.GetSoundDevicesManager()->FindDeviceInfoBestMatch(std::wstring()).id;
+		if(newdev != dev)
+		{
+			Reporting::Information("Device not availble. Reverting to default device.");
+			SetDevice(newdev);
+			UpdateEverything();
+		} else
+		{
+			Reporting::Warning("Device not availble.");
+		}
 	}
 }
 
@@ -312,6 +340,11 @@ void COptionsSoundcard::UpdateEverything()
 					// can be overwritten via [Sound Settings]MorePortaudio=1
 					continue;
 				}
+			}
+
+			if(theApp.GetSoundDevicesManager()->IsDeviceUnavailable(it->id))
+			{
+				continue;
 			}
 
 			{

mptrack/MainFrm.cpp
@@ -836,7 +836,14 @@ bool CMainFrame::audioOpenDevice()
 	SoundDevice::Settings deviceSettings = TrackerSettings::Instance().GetSoundDeviceSettings(deviceID);
 	if(!gpSoundDevice->Open(deviceSettings))
 	{
-		Reporting::Error("Unable to open sound device: Could not open sound device.");
+		if(!gpSoundDevice->IsAvailable())
+		{
+			theApp.GetSoundDevicesManager()->SetDeviceUnavailable(deviceID);
+			Reporting::Error("Unable to open sound device: Device not available.");
+		} else
+		{
+			Reporting::Error("Unable to open sound device: Could not open sound device.");
+		}
 		return false;
 	}
 	SampleFormat actualSampleFormat = gpSoundDevice->GetActualSampleFormat();

sounddev/SoundDevice.cpp
@@ -148,6 +148,8 @@ Base::Base(SoundDevice::ID id, const std::wstring &internalID)
 	, m_InternalID(internalID)
 {
 
+	m_DeviceUnavailableOnOpen = false;
+
 	m_BufferAttributes.Latency = m_Settings.Latency;
 	m_BufferAttributes.UpdateInterval = m_Settings.UpdateInterval;
 	m_BufferAttributes.NumBuffers = 0;
@@ -264,6 +266,7 @@ bool Base::Open(const SoundDevice::Settings &settings)
 		m_Settings.ChannelMapping = SoundDevice::ChannelMapping(m_Settings.Channels);
 	}
 	m_Flags = SoundDevice::Flags();
+	m_DeviceUnavailableOnOpen = false;
 	m_BufferAttributes.Latency = m_Settings.Latency;
 	m_BufferAttributes.UpdateInterval = m_Settings.UpdateInterval;
 	m_BufferAttributes.NumBuffers = 0;
@@ -451,7 +454,9 @@ void Manager::ReEnumerate()
 //-------------------------
 {
 	m_SoundDevices.clear();
+	m_DeviceUnavailable.clear();
 	m_DeviceCaps.clear();
+	m_DeviceDynamicCaps.clear();
 
 #ifndef NO_PORTAUDIO
 	SndDevPortaudioUnnitialize();
@@ -566,7 +571,7 @@ SoundDevice::Info Manager::FindDeviceInfoBestMatch(const std::wstring &identifie
 	}
 	if(identifier.empty())
 	{
-		return m_SoundDevices[0];
+			return *begin();
 	}
 	for(std::vector<SoundDevice::Info>::const_iterator it = begin(); it != end(); ++it)
 	{
@@ -643,6 +648,9 @@ SoundDevice::Caps Manager::GetDeviceCaps(SoundDevice::ID id, SoundDevice::IBase 
 			if(dummy)
 			{
 				m_DeviceCaps[id] = dummy->GetDeviceCaps();
+			} else
+			{
+				SetDeviceUnavailable(id);
 			}
 			delete dummy;
 		}
@@ -659,6 +667,10 @@ SoundDevice::DynamicCaps Manager::GetDeviceDynamicCaps(SoundDevice::ID id, const
 		if(currentSoundDevice && FindDeviceInfo(id).IsValid() && (currentSoundDevice->GetDeviceID() == id) && (currentSoundDevice->GetDeviceInternalID() == FindDeviceInfo(id).internalID))
 		{
 			m_DeviceDynamicCaps[id] = currentSoundDevice->GetDeviceDynamicCaps(baseSampleRates);
+			if(!currentSoundDevice->IsAvailable())
+			{
+				SetDeviceUnavailable(id);
+			}
 		} else
 		{
 			SoundDevice::IBase *dummy = CreateSoundDevice(id);
@@ -666,6 +678,13 @@ SoundDevice::DynamicCaps Manager::GetDeviceDynamicCaps(SoundDevice::ID id, const
 			{
 				dummy->SetMessageReceiver(messageReceiver);
 				m_DeviceDynamicCaps[id] = dummy->GetDeviceDynamicCaps(baseSampleRates);
+				if(!dummy->IsAvailable())
+				{
+					SetDeviceUnavailable(id);
+				}
+			} else
+			{
+				SetDeviceUnavailable(id);
 			}
 			delete dummy;
 		}

sounddev/SoundDevice.h
@@ -438,6 +438,7 @@ public:
 
 	virtual bool IsInited() const = 0;
 	virtual bool IsOpen() const = 0;
+	virtual bool IsAvailable() const = 0;
 	virtual bool IsPlaying() const = 0;
 
 	virtual bool OnIdle() = 0; // return true if any work has been done
@@ -485,6 +486,7 @@ protected:
 
 	SoundDevice::Settings m_Settings;
 	SoundDevice::Flags m_Flags;
+	bool m_DeviceUnavailableOnOpen;
 
 private:
 
@@ -572,6 +574,7 @@ public:
 
 	bool IsInited() const { return m_Caps.Available; }
 	bool IsOpen() const { return IsInited() && InternalIsOpen(); }
+	bool IsAvailable() const { return m_Caps.Available && !m_DeviceUnavailableOnOpen; }
 	bool IsPlaying() const { return m_IsPlaying; }
 
 	virtual bool OnIdle() { return false; }
@@ -648,6 +651,7 @@ class Manager
 {
 private:
 	std::vector<SoundDevice::Info> m_SoundDevices;
+	std::map<SoundDevice::ID, bool> m_DeviceUnavailable;
 	std::map<SoundDevice::ID, SoundDevice::Caps> m_DeviceCaps;
 	std::map<SoundDevice::ID, SoundDevice::DynamicCaps> m_DeviceDynamicCaps;
 
@@ -668,6 +672,9 @@ public:
 	SoundDevice::Info FindDeviceInfoBestMatch(const std::wstring &identifier) const;
 
 	bool OpenDriverSettings(SoundDevice::ID id, SoundDevice::IMessageReceiver *messageReceiver = nullptr, SoundDevice::IBase *currentSoundDevice = nullptr);
+
+	void SetDeviceUnavailable(SoundDevice::ID id) { m_DeviceUnavailable[id] = true; }
+	bool IsDeviceUnavailable(SoundDevice::ID id) { return m_DeviceUnavailable[id]; }
 
 	SoundDevice::Caps GetDeviceCaps(SoundDevice::ID id, SoundDevice::IBase *currentSoundDevice = nullptr);
 	SoundDevice::DynamicCaps GetDeviceDynamicCaps(SoundDevice::ID id, const std::vector<uint32> &baseSampleRates, SoundDevice::IMessageReceiver *messageReceiver = nullptr, SoundDevice::IBase *currentSoundDevice = nullptr, bool update = false);

sounddev/SoundDeviceASIO.cpp
@@ -268,6 +268,11 @@ bool CASIODevice::InternalOpen()
 		long outputChannels = 0;
 		asioCall(getChannels(&inputChannels, &outputChannels));
 		Log(mpt::String::Print("ASIO: getChannels() => inputChannels=%1 outputChannel=%2", inputChannels, outputChannels));
+		if(inputChannels <= 0 && outputChannels <= 0)
+		{
+			m_DeviceUnavailableOnOpen = true;
+			throw ASIOException("Device unavailble.");
+		}
 		if(m_Settings.Channels > outputChannels)
 		{
 			throw ASIOException("Not enough output channels.");
@@ -1350,6 +1355,7 @@ SoundDevice::DynamicCaps CASIODevice::GetDeviceDynamicCaps(const std::vector<uin
 	TemporaryASIODriverOpener opener(*this);
 	if(!IsDriverOpen())
 	{
+		m_DeviceUnavailableOnOpen = true;
 		return caps;
 	}
 
@@ -1386,6 +1392,10 @@ SoundDevice::DynamicCaps CASIODevice::GetDeviceDynamicCaps(const std::vector<uin
 		long inputChannels = 0;
 		long outputChannels = 0;
 		asioCall(getChannels(&inputChannels, &outputChannels));
+		if(!((inputChannels > 0) || (outputChannels > 0)))
+		{
+			m_DeviceUnavailableOnOpen = true;
+		}
 		for(long i = 0; i < outputChannels; ++i)
 		{
 			ASIOChannelInfo channelInfo;
@@ -1408,7 +1418,6 @@ SoundDevice::DynamicCaps CASIODevice::GetDeviceDynamicCaps(const std::vector<uin
 	{
 		// continue
 	}
-
 	return caps;
 }
 

commit 151146c75aae7a4965dc721eb82de650972ed4a8

[Fix] Soundcard settings: Fix crash when opening settings dialog if the current device is unavailable (broke in r4306). [Fix] Soundcard settings: Only initialize soundcard settings page if the page got actually selected.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4320 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Mpdlgs.cpp
@@ -152,26 +152,9 @@ void COptionsSoundcard::DoDataExchange(CDataExchange* pDX)
 COptionsSoundcard::COptionsSoundcard(SoundDevice::ID dev)
 //-------------------------------------------------------
 	: CPropertyPage(IDD_OPTIONS_SOUNDCARD)
-	, m_CurrentDeviceInfo(theApp.GetSoundDevicesManager()->FindDeviceInfo(dev))
-	, m_CurrentDeviceCaps(theApp.GetSoundDevicesManager()->GetDeviceCaps(dev, CMainFrame::GetMainFrame()->gpSoundDevice))
-	, m_CurrentDeviceDynamicCaps(theApp.GetSoundDevicesManager()->GetDeviceDynamicCaps(dev, TrackerSettings::Instance().GetSampleRates(), CMainFrame::GetMainFrame(), CMainFrame::GetMainFrame()->gpSoundDevice, true))
-	, m_Settings(TrackerSettings::Instance().GetSoundDeviceSettings(dev))
+	, m_InitialDevice(dev)
 {
-	if(theApp.GetSoundDevicesManager()->IsDeviceUnavailable(dev))
-	{
-		Reporting::Information("Device not availble. Reverting to default device.");
-		// if the device is unavailable, use the default device
-		SoundDevice::ID newdev = theApp.GetSoundDevicesManager()->FindDeviceInfoBestMatch(std::wstring()).id;
-		if(newdev != dev)
-		{
-			Reporting::Information("Device not availble. Reverting to default device.");
-			SetDevice(newdev);
-			UpdateEverything();
-		} else
-		{
-			Reporting::Warning("Device not availble.");
-		}
-	}
+	return;
 }
 
 
@@ -221,6 +204,7 @@ BOOL COptionsSoundcard::OnInitDialog()
 //------------------------------------
 {
 	CPropertyPage::OnInitDialog();
+	SetDevice(m_InitialDevice, true);
 	UpdateEverything();
 	return TRUE;
 }

mptrack/Mpdlgs.h
@@ -32,6 +32,8 @@ protected:
 	CStatic m_StaticChannelMapping[NUM_CHANNELCOMBOBOXES];
 	CComboBox m_CbnChannelMapping[NUM_CHANNELCOMBOBOXES];
 
+	SoundDevice::ID m_InitialDevice;
+
 	void SetDevice(SoundDevice::ID dev, bool forceReload=false);
 	SoundDevice::Info m_CurrentDeviceInfo;
 	SoundDevice::Caps m_CurrentDeviceCaps;