4a391fcacb0cb6968887f84e199828ac9f7d778e
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-10-13 08:07:03 +0000

    [Ref] build: Split premake files for external libraries into one per library.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4429 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake4.lua
+++ build/premake4.lua
@@ -3,4 +3,13 @@ solution "all"
  configurations { "Debug", "Release", "ReleaseNoLTCG" }
  platforms { "x32", "x64" }
 
- dofile "../include/premake4.lua"
+ dofile "../include/flac.premake4.lua"
+ dofile "../include/lhasa.premake4.lua"
+ dofile "../include/miniz.premake4.lua"
+ dofile "../include/minizip.premake4.lua"
+ dofile "../include/portaudio.premake4.lua"
+ dofile "../include/portmidi.premake4.lua"
+ dofile "../include/smbPitchShift.premake4.lua"
+ dofile "../include/soundtouch.premake4.lua"
+ dofile "../include/UnRAR.premake4.lua"
+ dofile "../include/zlib.premake4.lua"
---
+++ include/lhasa.premake4.lua
@@ -0,0 +1,50 @@
+
+ project "lhasa"
+  uuid "6B11F6A8-B131-4D2B-80EF-5731A9016436"
+  language "C"
+  location "../build/gen"
+  objdir "../build/obj/lhasa"
+  includedirs { "../include/msinttypes/inttypes" }
+  files {
+   "../include/lhasa/lib/crc16.c",
+   "../include/lhasa/lib/ext_header.c",
+   "../include/lhasa/lib/lh1_decoder.c",
+   "../include/lhasa/lib/lh5_decoder.c",
+   "../include/lhasa/lib/lh6_decoder.c",
+   "../include/lhasa/lib/lh7_decoder.c",
+   "../include/lhasa/lib/lha_arch_unix.c",
+   "../include/lhasa/lib/lha_arch_win32.c",
+   "../include/lhasa/lib/lha_basic_reader.c",
+   "../include/lhasa/lib/lha_decoder.c",
+   "../include/lhasa/lib/lha_endian.c",
+   "../include/lhasa/lib/lha_file_header.c",
+   "../include/lhasa/lib/lha_input_stream.c",
+   "../include/lhasa/lib/lha_reader.c",
+   "../include/lhasa/lib/lhx_decoder.c",
+   "../include/lhasa/lib/lz5_decoder.c",
+   "../include/lhasa/lib/lzs_decoder.c",
+   "../include/lhasa/lib/macbinary.c",
+   "../include/lhasa/lib/null_decoder.c",
+   "../include/lhasa/lib/pm1_decoder.c",
+   "../include/lhasa/lib/pm2_decoder.c",
+  }
+  files {
+   "../include/lhasa/lib/crc16.h",
+   "../include/lhasa/lib/ext_header.h",
+   "../include/lhasa/lib/lha_arch.h",
+   "../include/lhasa/lib/lha_basic_reader.h",
+   "../include/lhasa/lib/lha_decoder.h",
+   "../include/lhasa/lib/lha_endian.h",
+   "../include/lhasa/lib/lha_file_header.h",
+   "../include/lhasa/lib/lha_input_stream.h",
+   "../include/lhasa/lib/macbinary.h",
+   "../include/lhasa/lib/public/lha_decoder.h",
+   "../include/lhasa/lib/public/lha_file_header.h",
+   "../include/lhasa/lib/public/lha_input_stream.h",
+   "../include/lhasa/lib/public/lha_reader.h",
+   "../include/lhasa/lib/public/lhasa.h",
+  }
+  configuration "vs2008"
+   includedirs { "../include/msinttypes/stdint" }
+  dofile "../build/premake4-defaults-LIB.lua"
+  dofile "../build/premake4-defaults-static.lua"
