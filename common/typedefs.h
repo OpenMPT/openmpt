6336fb3969577c5aba773e7bbfa197dd61032ed1
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-10-22 17:51:42 +0000

    [Ref] Remove workaround for missing stdint macros on Andriod. This work-around was never actually required but we defined the __STDC_*_MACROS macros too late after a stdint.h had already been dragged in from other headers.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4468 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/typedefs.h
+++ common/typedefs.h
@@ -355,60 +355,9 @@ typedef std::uint64_t uint64;
 
 #endif
 
-#ifdef ANDROID
-
 OPENMPT_NAMESPACE_END
 #include <stdint.h>
 OPENMPT_NAMESPACE_BEGIN
-
-#if 0
-
-// Android NDK appears to provide a different (and incomplete) <stdint.h> when compiling C++.
-// Provide these macros ourselves if they are not defined by here.
-
-#ifndef INT8_MIN
-#define INT8_MIN (-128)
-#endif
-#ifndef INT8_MAX
-#define INT8_MAX (127)
-#endif
-#ifndef UINT8_MAX
-#define UINT8_MAX (255)
-#endif
-
-#ifndef INT16_MIN
-#define INT16_MIN (-32768)
-#endif
-#ifndef INT16_MAX
-#define INT16_MAX (32767)
-#endif
-#ifndef UINT16_MAX
-#define UINT16_MAX (65535)
-#endif
-
-#ifndef INT32_MIN
-#define INT32_MIN (-2147483647-1)
-#endif
-#ifndef INT32_MAX
-#define INT32_MAX (2147483647)
-#endif
-#ifndef UINT32_MAX
-#define UINT32_MAX (4294967295U)
-#endif
-
-#ifndef INT64_MIN
-#define INT64_MIN ((-9223372036854775807ll)-1)
-#endif
-#ifndef INT64_MAX
-#define INT64_MAX ((9223372036854775807ll))
-#endif
-#ifndef UINT64_MAX
-#define UINT64_MAX ((18446744073709551615ull))
-#endif
-
-#endif
-
-#endif // ANDROID
 
 const int8 int8_min     = INT8_MIN;
 const int16 int16_min   = INT16_MIN;
