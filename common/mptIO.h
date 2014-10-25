9a730115fe5eab76be8c21f67bd8f4100f183c86
Johannes Schultz <saga-games@users.sourceforge.net>
2014-10-25 01:51:48 +0000

    [Ref] Instead of checking for MPT_FILEREADER_STD_ISTREAM at every site, use the new mpt::IO::Open to wrap stream vs memory-mapped file open calls for FileReader.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4481 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/mptIO.h
+++ common/mptIO.h
@@ -31,8 +31,11 @@
 
 OPENMPT_NAMESPACE_BEGIN
 
+class FileReader;
 
 namespace mpt {
+
+class PathString;
 
 namespace IO {
 
@@ -47,7 +50,8 @@ inline bool OffsetFits(IO::Offset off)
 	return (static_cast<IO::Offset>(mpt::saturate_cast<Toff>(off)) == off);
 }
 
-
+// Open a file (either stream or memory-mapped, depending on program configuration) and immediately return FileReader for the file
+FileReader Open(const mpt::PathString &filename);
 
 bool IsValid(std::ostream & f);
 bool IsValid(std::istream & f);
@@ -194,7 +198,7 @@ inline bool ReadAdaptiveInt16LE(Tfile & f, uint16 & v)
 	{
 		byte = 0;
 		if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
-		v |= (static_cast<uint16>(byte) << (((i+1)*8) - 1));		
+		v |= (static_cast<uint16>(byte) << (((i+1)*8) - 1));
 	}
 	return result;
 }
@@ -214,7 +218,7 @@ inline bool ReadAdaptiveInt32LE(Tfile & f, uint32 & v)
 	{
 		byte = 0;
 		if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
-		v |= (static_cast<uint32>(byte) << (((i+1)*8) - 2));		
+		v |= (static_cast<uint32>(byte) << (((i+1)*8) - 2));
 	}
 	return result;
 }
@@ -234,7 +238,7 @@ inline bool ReadAdaptiveInt64LE(Tfile & f, uint64 & v)
 	{
 		byte = 0;
 		if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
-		v |= (static_cast<uint64>(byte) << (((i+1)*8) - 2));		
+		v |= (static_cast<uint64>(byte) << (((i+1)*8) - 2));
 	}
 	return result;
 }
@@ -262,7 +266,7 @@ inline bool ReadSizedStringLE(Tfile & f, std::string & str, Tsize maxSize = std:
 		}
 		str.push_back(c);
 	}
-	return true;	
+	return true;
 }
 
 
@@ -549,7 +553,7 @@ public:
 
 };
 
-#endif 
+#endif
 
 
 class FileDataContainerMemory
--- soundlib/Load_itp.cpp
+++ soundlib/Load_itp.cpp
@@ -20,9 +20,6 @@
 #include "ITTools.h"
 #ifdef MODPLUG_TRACKER
 #include "../mptrack/TrackerSettings.h"
-#if !defined(MPT_FILEREADER_STD_ISTREAM)
-#include "../mptrack/MemoryMappedFile.h"
-#endif
 #ifndef MODPLUG_NO_FILESAVE
 #include "../common/mptFstream.h"
 #endif
@@ -263,18 +260,7 @@ bool CSoundFile::ReadITProject(FileReader &file, ModLoadingFlags loadFlags)
 	{
 		if(m_szInstrumentPath[ins].empty())
 			continue;
-#if defined(MPT_FILEREADER_STD_ISTREAM)
-		mpt::ifstream f(m_szInstrumentPath[ins], std::ios_base::binary);
-		if(!f.good())
-			continue;
-		FileReader file(&f, &m_szInstrumentPath[ins]);
-#else
-		CMappedFile f;
-		if(!f.Open(m_szInstrumentPath[ins]))
-			continue;
-		FileReader file = f.GetFile();
-#endif
-
+		FileReader file(mpt::IO::Open(m_szInstrumentPath[ins]));
 		if(file.IsValid())
 			ReadInstrumentFromFile(ins + 1, file, TrackerSettings::Instance().m_MayNormalizeSamplesOnLoad);
 	}
