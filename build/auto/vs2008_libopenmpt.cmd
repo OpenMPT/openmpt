1ded75270abc0ba0cf7a394af90737d83e147d79
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-10-27 08:22:42 +0000

    [New] build: Add automated build script for VS2008 libopenmpt.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4508 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/vs2008_libopenmpt.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+cd ..\..
+
+set MY_DIR=%CD%
+
+call build\auto\setup_vs2008.cmd
+
+call build\auto\prepare_win.cmd
+
+
+
+cd build\vs2008 || goto error
+ devenv libopenmpt.sln /clean "Release|Win32" || goto error
+ devenv libopenmpt.sln /build "Release|Win32" || goto error
+cd ..\.. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
