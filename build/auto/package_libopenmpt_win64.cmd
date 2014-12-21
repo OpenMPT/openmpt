19c68147b20a92bbfc97f243946b733eb1eb53ce
Jörn Heusipp <manxorist@users.sourceforge.net>
2014-12-21 09:59:33 +0000

    [Fix] libopenmpt: libopenmpt_ext.hpp was missing from the Windows binary zip files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4666 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_libopenmpt_win64.cmd
+++ build/auto/package_libopenmpt_win64.cmd
@@ -23,7 +23,7 @@ copy /y libopenmpt.lib lib\x64\ || goto error
 copy /y libopenmpt.dll bin\x64\ || goto error
 copy /y ..\..\LICENSE .\ || goto error
 copy /y ..\..\libopenmpt\dox\changelog.md .\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 libopenmpt-win64-r%GOT_REVISION%.7z libopenmpt.dll libmodplug.dll openmpt123.exe inc\libopenmpt\libopenmpt.h inc\libopenmpt\libopenmpt.hpp inc\libopenmpt\libopenmpt_config.h inc\libopenmpt\libopenmpt_version.h lib\x64\libopenmpt.lib bin\x64\libopenmpt.dll LICENSE changelog.md || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 libopenmpt-win64-r%GOT_REVISION%.7z libopenmpt.dll libmodplug.dll openmpt123.exe inc\libopenmpt\libopenmpt.h inc\libopenmpt\libopenmpt.hpp inc\libopenmpt\libopenmpt_ext.hpp inc\libopenmpt\libopenmpt_config.h inc\libopenmpt\libopenmpt_version.h lib\x64\libopenmpt.lib bin\x64\libopenmpt.dll LICENSE changelog.md || goto error
 "C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-win64.tar libopenmpt-win64-r%GOT_REVISION%.7z || goto error
 del /f /q libopenmpt-win64-r%GOT_REVISION%.7z
 cd ..\.. || goto error
