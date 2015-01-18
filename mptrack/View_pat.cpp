33db91a9cccf5f232bd56b26e6a2a26c5fd63a64
Johannes Schultz <saga-games@users.sourceforge.net>
2015-01-18 02:02:03 +0000

    [Imp] Picking up an instrument in the pattern editor now also chooses it when switching back to the instrument or sample editor (just like the instrument selection in the upper part of the pattern tab)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4723 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/View_pat.cpp
+++ mptrack/View_pat.cpp
@@ -6910,6 +6910,7 @@ void CViewPattern::FindInstrument()
 			if(!m.IsPcNote() && m.instr != 0)
 			{
 				SendCtrlMessage(CTRLMSG_SETCURRENTINSTRUMENT, m.instr);
+				static_cast<CModControlView *>(CWnd::FromHandle(m_hWndCtrl))->InstrumentChanged(m.instr);
 				return;
 			}
 		} while(row-- != 0);
