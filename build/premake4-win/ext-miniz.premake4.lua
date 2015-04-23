d7d86878a5dfc4383d41f3a522ba10ab4cd97f8e
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-04-23 19:42:21 +0000

    [Ref] build: Move premake4 files for external libraries from include/ to build/premake4-win/. Adjust pathnames in premake files accordingly. No changes in generated project files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4977 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/premake4-win/ext-miniz.premake4.lua
@@ -0,0 +1,11 @@
+ 
+ project "miniz"
+  uuid "B5E0C06B-8121-426A-8FFB-4293ECAAE29C"
+  language "C"
+  location "../../build/gen"
+  objdir "../../build/obj/miniz"
+  files {
+   "../../include/miniz/miniz.c",
+  }
+  dofile "../../build/premake4-defaults-LIB.lua"
+  dofile "../../build/premake4-defaults-static.lua"
---
+++ build/premake4-win/ext-soundtouch.premake4.lua
@@ -0,0 +1,50 @@
+ 
+ project "soundtouch"
+  uuid "F5F8F6DE-84CF-4E9D-91EA-D9B5E2AA36CD"
+  language "C++"
+  location "../../build/gen"
+  objdir "../../build/obj/soundtouch"
+  targetname "OpenMPT_SoundTouch_f32"
+  includedirs { "../../include/soundtouch/include" }
+  files {
+   "../../include/soundtouch/include/BPMDetect.h",
+   "../../include/soundtouch/include/FIFOSampleBuffer.h",
+   "../../include/soundtouch/include/FIFOSamplePipe.h",
+   "../../include/soundtouch/include/SoundTouch.h",
+   "../../include/soundtouch/include/STTypes.h",
+  }
+  files {
+   "../../include/soundtouch/source/SoundTouch/AAFilter.cpp",
+   "../../include/soundtouch/source/SoundTouch/BPMDetect.cpp",
+   "../../include/soundtouch/source/SoundTouch/cpu_detect_x86.cpp",
+   "../../include/soundtouch/source/SoundTouch/FIFOSampleBuffer.cpp",
+   "../../include/soundtouch/source/SoundTouch/FIRFilter.cpp",
+   "../../include/soundtouch/source/SoundTouch/InterpolateCubic.cpp",
+   "../../include/soundtouch/source/SoundTouch/InterpolateLinear.cpp",
+   "../../include/soundtouch/source/SoundTouch/InterpolateShannon.cpp",
+   "../../include/soundtouch/source/SoundTouch/mmx_optimized.cpp",
+   "../../include/soundtouch/source/SoundTouch/PeakFinder.cpp",
+   "../../include/soundtouch/source/SoundTouch/RateTransposer.cpp",
+   "../../include/soundtouch/source/SoundTouch/SoundTouch.cpp",
+   "../../include/soundtouch/source/SoundTouch/sse_optimized.cpp",
+   "../../include/soundtouch/source/SoundTouch/TDStretch.cpp",
+  }
+  files {
+   "../../include/soundtouch/source/SoundTouch/AAFilter.h",
+   "../../include/soundtouch/source/SoundTouch/cpu_detect.h",
+   "../../include/soundtouch/source/SoundTouch/FIRFilter.h",
+   "../../include/soundtouch/source/SoundTouch/InterpolateCubic.h",
+   "../../include/soundtouch/source/SoundTouch/InterpolateLinear.h",
+   "../../include/soundtouch/source/SoundTouch/InterpolateShannon.h",
+   "../../include/soundtouch/source/SoundTouch/PeakFinder.h",
+   "../../include/soundtouch/source/SoundTouch/RateTransposer.h",
+   "../../include/soundtouch/source/SoundTouch/TDStretch.h",
+  }
+  files {
+   "../../include/soundtouch/source/SoundTouchDLL/SoundTouchDLL.cpp",
+   "../../include/soundtouch/source/SoundTouchDLL/SoundTouchDLL.h",
+  }
+  defines { "DLL_EXPORTS" }
+  dofile "../../build/premake4-defaults-DLL.lua"
+  dofile "../../build/premake4-defaults-static.lua"
+ 
