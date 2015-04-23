0e00e1ba4555e82e00304d227bf12ac99bc65956
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-04-23 19:42:21 +0000

    [Ref] build: Move premake4 files for external libraries from include/ to build/premake4-win/. Adjust pathnames in premake files accordingly. No changes in generated project files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4977 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/premake4-win/ext-flac.premake4.lua
@@ -0,0 +1,79 @@
+ 
+ project "flac"
+  uuid "E599F5AA-F9A3-46CC-8DB0-C8DEFCEB90C5"
+  language "C"
+  location "../../build/gen"
+  objdir "../../build/obj/flac"
+  includedirs { "../../include/flac/include", "../../include/flac/src/libFLAC/include" }
+  files {
+   "../../include/flac/src/libFLAC/bitmath.c",
+   "../../include/flac/src/libFLAC/bitreader.c",
+   "../../include/flac/src/libFLAC/bitwriter.c",
+   "../../include/flac/src/libFLAC/cpu.c",
+   "../../include/flac/src/libFLAC/crc.c",
+   "../../include/flac/src/libFLAC/fixed.c",
+   "../../include/flac/src/libFLAC/fixed_intrin_sse2.c",
+   "../../include/flac/src/libFLAC/fixed_intrin_ssse3.c",
+   "../../include/flac/src/libFLAC/float.c",
+   "../../include/flac/src/libFLAC/format.c",
+   "../../include/flac/src/libFLAC/lpc.c",
+   "../../include/flac/src/libFLAC/lpc_intrin_avx2.c",
+   "../../include/flac/src/libFLAC/lpc_intrin_sse2.c",
+   "../../include/flac/src/libFLAC/lpc_intrin_sse41.c",
+   "../../include/flac/src/libFLAC/lpc_intrin_sse.c",
+   "../../include/flac/src/libFLAC/md5.c",
+   "../../include/flac/src/libFLAC/memory.c",
+   "../../include/flac/src/libFLAC/metadata_iterators.c",
+   "../../include/flac/src/libFLAC/metadata_object.c",
+   "../../include/flac/src/libFLAC/stream_decoder.c",
+   "../../include/flac/src/libFLAC/stream_encoder.c",
+   "../../include/flac/src/libFLAC/stream_encoder_intrin_avx2.c",
+   "../../include/flac/src/libFLAC/stream_encoder_intrin_sse2.c",
+   "../../include/flac/src/libFLAC/stream_encoder_intrin_ssse3.c",
+   "../../include/flac/src/libFLAC/stream_encoder_framing.c",
+   "../../include/flac/src/libFLAC/window.c",
+  }
+  files {
+   "../../include/flac/src/share/win_utf8_io/win_utf8_io.c",
+  }
+  files {
+   "../../include/flac/src/libFLAC/include/private/all.h",
+   "../../include/flac/src/libFLAC/include/private/bitmath.h",
+   "../../include/flac/src/libFLAC/include/private/bitreader.h",
+   "../../include/flac/src/libFLAC/include/private/bitwriter.h",
+   "../../include/flac/src/libFLAC/include/private/cpu.h",
+   "../../include/flac/src/libFLAC/include/private/crc.h",
+   "../../include/flac/src/libFLAC/include/private/fixed.h",
+   "../../include/flac/src/libFLAC/include/private/float.h",
+   "../../include/flac/src/libFLAC/include/private/format.h",
+   "../../include/flac/src/libFLAC/include/private/lpc.h",
+   "../../include/flac/src/libFLAC/include/private/md5.h",
+   "../../include/flac/src/libFLAC/include/private/memory.h",
+   "../../include/flac/src/libFLAC/include/private/metadata.h",
+   "../../include/flac/src/libFLAC/include/private/stream_encoder.h",
+   "../../include/flac/src/libFLAC/include/private/stream_encoder_framing.h",
+   "../../include/flac/src/libFLAC/include/private/window.h",
+   "../../include/flac/src/libFLAC/include/protected/all.h",
+   "../../include/flac/src/libFLAC/include/protected/stream_decoder.h",
+   "../../include/flac/src/libFLAC/include/protected/stream_encoder.h",
+  }
+  files {
+   "../../include/flac/include/FLAC/all.h",
+   "../../include/flac/include/FLAC/assert.h",
+   "../../include/flac/include/FLAC/callback.h",
+   "../../include/flac/include/FLAC/export.h",
+   "../../include/flac/include/FLAC/format.h",
+   "../../include/flac/include/FLAC/metadata.h",
+   "../../include/flac/include/FLAC/ordinals.h",
+   "../../include/flac/include/FLAC/stream_decoder.h",
+   "../../include/flac/include/FLAC/stream_encoder.h",
+  }
+  buildoptions { "/wd4244", "/wd4267", "/wd4334" }
+  configuration "*Lib"
+   defines { "FLAC__NO_DLL" }
+  configuration "vs2010"
+   defines { "VERSION=\"1.3.1\"" }
+  configuration "vs2008"
+   defines { "VERSION=\\\"1.3.1\\\"" }
+  dofile "../../build/premake4-defaults-LIB.lua"
+  dofile "../../build/premake4-defaults-static.lua"
---
+++ build/premake4-win/ext-portmidi.premake4.lua
@@ -0,0 +1,24 @@
+
+ project "portmidi"
+  uuid "2512E2CA-578A-4F10-9363-4BFC9A5EF960"
+  language "C"
+  location "../../build/gen"
+  objdir "../../build/obj/portmidi"
+  includedirs { "../../include/portmidi/porttime", "../../include/portmidi/pm_common", "../../include/portmidi/pm_win" }
+  files {
+   "../../include/portmidi/porttime/porttime.c",
+   "../../include/portmidi/porttime/ptwinmm.c",
+   "../../include/portmidi/pm_common/pmutil.c",
+   "../../include/portmidi/pm_common/portmidi.c",
+   "../../include/portmidi/pm_win/pmwin.c",
+   "../../include/portmidi/pm_win/pmwinmm.c",
+  }
+  files {
+   "../../include/portmidi/porttime/porttime.h",
+   "../../include/portmidi/pm_common/pminternal.h",
+   "../../include/portmidi/pm_common/pmutil.h",
+   "../../include/portmidi/pm_common/portmidi.h",
+   "../../include/portmidi/pm_win/pmwinmm.h",
+  }
+  dofile "../../build/premake4-defaults-LIB.lua"
+  dofile "../../build/premake4-defaults-static.lua"
