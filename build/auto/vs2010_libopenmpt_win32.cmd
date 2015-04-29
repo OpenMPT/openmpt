1eddc50f4de8db9e67c80d25fbf98d68eb8d16d3
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-04-29 16:32:38 +0000

    [Fix] build: There is no use in trying to build xmp-openmpt or in_openmpt for Win64.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5020 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/vs2010_libopenmpt_win32.cmd
+++ build/auto/vs2010_libopenmpt_win32.cmd
@@ -16,10 +16,14 @@ cd build\vs2010 || goto error
 
  devenv libopenmpt.sln /clean "Release|Win32" || goto error
  devenv openmpt123.sln /clean "Release|Win32" || goto error
+ devenv in_openmpt.sln /clean "Release|Win32" || goto error
+ devenv xmp-openmpt.sln /clean "Release|Win32" || goto error
  devenv foo_openmpt.sln /clean "Release|Win32" || goto error
 
  devenv libopenmpt.sln /build "Release|Win32" || goto error
  devenv openmpt123.sln /build "Release|Win32" || goto error
+ devenv in_openmpt.sln /build "Release|Win32" || goto error
+ devenv xmp-openmpt.sln /build "Release|Win32" || goto error
  devenv foo_openmpt.sln /build "Release|Win32" || goto error
 
 cd ..\.. || goto error
