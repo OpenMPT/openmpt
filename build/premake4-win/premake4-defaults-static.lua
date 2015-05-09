89b3debc2e59eee846464798b93bd7fed01a23b8
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-09 07:52:16 +0000

    [Ref] build: Add support for premake 4.4-beta5.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5055 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake4-win/premake4-defaults-static.lua
+++ build/premake4-win/premake4-defaults-static.lua
@@ -8,10 +8,19 @@
    defines { "NDEBUG" }
    defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_WARNINGS" }
    flags { "Symbols", "Optimize", "FloatFast", "StaticRuntime" }
+if MPT_PREMAKE_VERSION == "4.3" then
    buildoptions { "/MP" }
+elseif MPT_PREMAKE_VERSION == "4.4" then
+   buildoptions { "/GL /MP" }
+   linkoptions { "/LTCG" }
+end
 
   configuration "ReleaseNoLTCG"
    defines { "NDEBUG" }
    defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_WARNINGS" }
    flags { "Optimize", "FloatFast", "StaticRuntime" }
+if MPT_PREMAKE_VERSION == "4.3" then
    buildoptions { "/GL- /MP" }
+elseif MPT_PREMAKE_VERSION == "4.4" then
+   buildoptions { "/MP" }
+end
