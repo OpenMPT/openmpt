2b14e37f40a633f257aedf41ca15aadf0a59fa9d
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-09 09:46:14 +0000

    [Ref] build: Add support for premake 5.0-alpha3.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5057 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/update_premake4_projects.cmd
+++ build/update_premake4_projects.cmd
@@ -6,6 +6,11 @@ cd build
 cd .. || goto err
 
 
+goto premake4
+
+
+:premake4
+
 echo dofile "build/premake4.lua" > premake4.lua || goto err
 
 build\premake4.exe --group=libopenmpt_test vs2008 || goto err
@@ -28,6 +33,37 @@ build\premake4.exe postprocess || goto err
 
 del premake4.lua || goto err
 
+goto premakedone
+
+
+:premake5
+
+echo dofile "build/premake4.lua" > premake5.lua || goto err
+
+build\premake5.exe --group=libopenmpt_test vs2008 || goto err
+build\premake5.exe --group=libopenmpt vs2008 || goto err
+build\premake5.exe --group=openmpt123 vs2008 || goto err
+build\premake5.exe --group=PluginBridge vs2008 || goto err
+build\premake5.exe --group=OpenMPT vs2008 || goto err
+build\premake5.exe --group=all-externals vs2008 || goto err
+
+build\premake5.exe --group=libopenmpt_test vs2010 || goto err
+build\premake5.exe --group=in_openmpt vs2010 || goto err
+build\premake5.exe --group=xmp-openmpt vs2010 || goto err
+build\premake5.exe --group=libopenmpt vs2010 || goto err
+build\premake5.exe --group=openmpt123 vs2010 || goto err
+build\premake5.exe --group=PluginBridge vs2010 || goto err
+build\premake5.exe --group=OpenMPT vs2010 || goto err
+build\premake5.exe --group=all-externals vs2010 || goto err
+
+build\premake5.exe postprocess || goto err
+
+del premake5.lua || goto err
+
+goto premakedone
+
+
+:premakedone
 
 cd %MY_DIR% || goto err
 
