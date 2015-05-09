f6add8423f78f076b4fef55cab2972649b9fb3e7
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-09 11:33:11 +0000

    [Fix] build: Premake5 projects with shared runtime were broken.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5059 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake4-win/premake4-defaults-shared.lua
+++ build/premake4-win/premake4-defaults-shared.lua
@@ -2,25 +2,43 @@
   configuration "Debug"
    defines { "DEBUG" }
    defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_WARNINGS" }
+if MPT_PREMAKE_VERSION == "4.3" then
    flags { "Symbols" }
-  
+elseif MPT_PREMAKE_VERSION == "4.4" then
+   flags { "Symbols" }
+elseif MPT_PREMAKE_VERSION == "5.0" then
+   flags { "Symbols" }
+   optimize "Debug"
+   floatingpoint "Default"
+end
+
   configuration "Release"
    defines { "NDEBUG" }
    defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_WARNINGS" }
-   flags { "Symbols", "Optimize", "FloatFast" }
 if MPT_PREMAKE_VERSION == "4.3" then
+   flags { "Symbols", "Optimize", "FloatFast" }
    buildoptions { "/MP" }
 elseif MPT_PREMAKE_VERSION == "4.4" then
+   flags { "Symbols", "Optimize", "FloatFast" }
    buildoptions { "/GL /MP" }
    linkoptions { "/LTCG" }
+elseif MPT_PREMAKE_VERSION == "5.0" then
+   flags { "Symbols", "LinkTimeOptimization", "MultiProcessorCompile" }
+   optimize "Full"
+   floatingpoint "Fast"
 end
 
   configuration "ReleaseNoLTCG"
    defines { "NDEBUG" }
    defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE", "_CRT_SECURE_NO_DEPRECATE", "_CRT_NONSTDC_NO_WARNINGS" }
-   flags { "Optimize", "FloatFast" }
 if MPT_PREMAKE_VERSION == "4.3" then
+   flags { "Optimize", "FloatFast" }
    buildoptions { "/GL- /MP" }
 elseif MPT_PREMAKE_VERSION == "4.4" then
+   flags { "Optimize", "FloatFast" }
    buildoptions { "/MP" }
+elseif MPT_PREMAKE_VERSION == "5.0" then
+   flags { "MultiProcessorCompile" }
+   optimize "Full"
+   floatingpoint "Fast"
 end
