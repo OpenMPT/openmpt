4a62a90ccd50931e8cad0bb7197348ebee211bd6
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-11 10:23:41 +0000

    [Ref] build: Kill premake 4.3 and 4.4 support.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5077 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/regenerate_vs_projects.cmd
+++ build/regenerate_vs_projects.cmd
@@ -6,38 +6,6 @@ cd build
 cd .. || goto err
 
 
-goto premake5
-
-
-:premake4
-
-echo dofile "build/premake4-win/premake.lua" > premake4.lua || goto err
-
-include\premake\premake4.exe --group=libopenmpt_test vs2008 || goto err
-include\premake\premake4.exe --group=libopenmpt vs2008 || goto err
-include\premake\premake4.exe --group=openmpt123 vs2008 || goto err
-include\premake\premake4.exe --group=PluginBridge vs2008 || goto err
-include\premake\premake4.exe --group=OpenMPT vs2008 || goto err
-include\premake\premake4.exe --group=all-externals vs2008 || goto err
-
-include\premake\premake4.exe --group=libopenmpt_test vs2010 || goto err
-include\premake\premake4.exe --group=in_openmpt vs2010 || goto err
-include\premake\premake4.exe --group=xmp-openmpt vs2010 || goto err
-include\premake\premake4.exe --group=libopenmpt vs2010 || goto err
-include\premake\premake4.exe --group=openmpt123 vs2010 || goto err
-include\premake\premake4.exe --group=PluginBridge vs2010 || goto err
-include\premake\premake4.exe --group=OpenMPT vs2010 || goto err
-include\premake\premake4.exe --group=all-externals vs2010 || goto err
-
-include\premake\premake4.exe postprocess || goto err
-
-del premake4.lua || goto err
-
-goto premakedone
-
-
-:premake5
-
 echo dofile "build/premake4-win/premake.lua" > premake5.lua || goto err
 
 include\premake\premake5.exe --group=libopenmpt_test vs2008 || goto err
@@ -88,10 +56,6 @@ include\premake\premake5.exe postprocess || goto err
 
 del premake5.lua || goto err
 
-goto premakedone
-
-
-:premakedone
 
 cd %MY_DIR% || goto err
 
