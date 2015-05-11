188dbdba387ac8c1a31fd08ff77e715ac4247668
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-11 15:33:04 +0000

    [Mod] build: Add new libopenmpt windows packaging script, that separates player plugins and openmpt123 from the development package. The automated buildbot will build win32 and win64 in one go from now on.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5081 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/package_libopenmpt_win.cmd
@@ -0,0 +1,100 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+cd ..\..
+
+set GOT_REVISION=%1%
+set MY_DIR=%CD%
+
+
+
+cd bin || goto error
+rmdir /s /q libopenmpt-win
+del /f /q libopenmpt-win.tar
+del /f /q libopenmpt-win-r%GOT_REVISION%.7z
+mkdir libopenmpt-win
+cd libopenmpt-win || goto error
+mkdir openmpt123
+mkdir XMPlay
+mkdir Winamp
+mkdir foobar2000
+copy /y ..\..\LICENSE .\LICENSE.txt || goto error
+copy /y ..\..\libopenmpt\dox\changelog.md .\ || goto error
+copy /y ..\..\libopenmpt\doc\xmp-openmpt.txt .\XMPlay\ || goto error
+copy /y ..\..\libopenmpt\doc\in_openmpt.txt .\Winamp\ || goto error
+copy /y ..\..\libopenmpt\doc\foo_openmpt.txt .\foobar2000\ || goto error
+copy /y ..\..\bin\Win32\openmpt123.exe .\openmpt123\ || goto error
+copy /y ..\..\bin\Win32\xmp-openmpt.dll .\XMPlay\ || goto error
+copy /y ..\..\bin\Win32\in_openmpt.dll .\Winamp\ || goto error
+copy /y ..\..\bin\Win32\foo_openmpt.dll .\foobar2000\ || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-win-r%GOT_REVISION%.7z ^
+ LICENSE.txt ^
+ changelog.md ^
+ openmpt123\openmpt123.exe ^
+ XMPlay\xmp-openmpt.txt ^
+ XMPlay\xmp-openmpt.dll ^
+ Winamp\in_openmpt.txt ^
+ Winamp\in_openmpt.dll ^
+ foobar2000\foo_openmpt.txt ^
+ foobar2000\foo_openmpt.dll ^
+ || goto error
+cd .. || goto error
+"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-win.tar libopenmpt-win-r%GOT_REVISION%.7z || goto error
+del /f /q libopenmpt-win-r%GOT_REVISION%.7z
+cd .. || goto error
+
+cd bin || goto error
+rmdir /s /q libopenmpt-dev-vs2010
+del /f /q libopenmpt-dev-vs2010.tar
+del /f /q libopenmpt-dev-vs2010-r%GOT_REVISION%.7z
+mkdir libopenmpt-dev-vs2010
+cd libopenmpt-dev-vs2010 || goto error
+mkdir inc
+mkdir inc\libopenmpt
+mkdir lib
+mkdir lib\x86
+mkdir lib\x86_64
+mkdir bin
+mkdir bin\x86
+mkdir bin\x86_64
+copy /y ..\..\LICENSE .\LICENSE.txt || goto error
+copy /y ..\..\libopenmpt\dox\changelog.md .\changelog.md || goto error
+copy /y ..\..\libopenmpt\libopenmpt.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt.hpp inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_config.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_version.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_ext.hpp inc\libopenmpt\ || goto error
+copy /y ..\..\bin\Win32\libopenmpt.lib lib\x86\ || goto error
+copy /y ..\..\bin\Win32\libopenmpt.dll bin\x86\ || goto error
+copy /y ..\..\bin\x64\libopenmpt.lib lib\x86_64\ || goto error
+copy /y ..\..\bin\x64\libopenmpt.dll bin\x86_64\ || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-dev-vs2010-r%GOT_REVISION%.7z ^
+ LICENSE.txt ^
+ changelog.md ^
+ inc\libopenmpt\libopenmpt.h ^
+ inc\libopenmpt\libopenmpt.hpp ^
+ inc\libopenmpt\libopenmpt_config.h ^
+ inc\libopenmpt\libopenmpt_version.h ^
+ inc\libopenmpt\libopenmpt_ext.hpp ^
+ lib\x86\libopenmpt.lib ^
+ lib\x86_64\libopenmpt.lib ^
+ bin\x86\libopenmpt.dll ^
+ bin\x86_64\libopenmpt.dll ^
+ || goto error
+cd .. || goto error
+"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-dev-vs2010.tar libopenmpt-dev-vs2010-r%GOT_REVISION%.7z || goto error
+del /f /q libopenmpt-dev-vs2010-r%GOT_REVISION%.7z
+cd .. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
