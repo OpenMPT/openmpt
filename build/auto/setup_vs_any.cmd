c914734d825ebf653980990eed898c14f66c0096
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-18 09:35:26 +0000

    [Ref] build: Make packaging scripts independent of buildbot-provided GOT_REVISION parameter. [Ref] build: Make OpenMPT packaging script work with any installed Visual Studio version.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5117 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/setup_vs_any.cmd
@@ -0,0 +1,49 @@
+@echo off
+
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+ call setup_vs2015.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+ call setup_vs2015.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+ call setup_vs2013.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+ call setup_vs2013.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+ call setup_vs2012.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+ call setup_vs2012.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
+ call setup_vs2010.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
+ call setup_vs2010.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
+ call setup_vs2008.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
+ call setup_vs2008.cmd
+ goto vsdone
+)
+
+:vsdone
