545fb4269fda8208821e2ddaf96fa1dd9ed3befc
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-18 21:37:26 +0000

    [Fix] build: Repair openmpt automated packaging. Broke in r5117.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5120 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_vs_any.cmd
+++ build/auto/setup_vs_any.cmd
@@ -2,47 +2,47 @@
 
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
- call setup_vs2015.cmd
+ call %~dp0\setup_vs2015.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
- call setup_vs2015.cmd
+ call %~dp0\setup_vs2015.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
- call setup_vs2013.cmd
+ call %~dp0\setup_vs2013.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
- call setup_vs2013.cmd
+ call %~dp0\setup_vs2013.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
- call setup_vs2012.cmd
+ call %~dp0\setup_vs2012.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
- call setup_vs2012.cmd
+ call %~dp0\setup_vs2012.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
- call setup_vs2010.cmd
+ call %~dp0\setup_vs2010.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
- call setup_vs2010.cmd
+ call %~dp0\setup_vs2010.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
- call setup_vs2008.cmd
+ call %~dp0\setup_vs2008.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
- call setup_vs2008.cmd
+ call %~dp0\setup_vs2008.cmd
  goto vsdone
 )
 

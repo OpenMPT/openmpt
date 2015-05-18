OpenMPT/openmpt

commit 78ee68f650d528069fcd4ca93c28d0fb712f606f

[Ref] build: Make packaging scripts independent of buildbot-provided GOT_REVISION parameter. [Ref] build: Make OpenMPT packaging script work with any installed Visual Studio version.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5117 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/auto/setup_vs_any.cmd
@@ -1,0 +1,49 @@
+@echo off
+
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+ call setup_vs2015.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+ call setup_vs2015.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+ call setup_vs2013.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+ call setup_vs2013.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+ call setup_vs2012.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+ call setup_vs2012.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
+ call setup_vs2010.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
+ call setup_vs2010.cmd
+ goto vsdone
+)
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
+ call setup_vs2008.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
+ call setup_vs2008.cmd
+ goto vsdone
+)
+
+:vsdone

build/auto/package_libopenmpt_win.cmd
@@ -4,15 +4,18 @@ set BATCH_DIR=%~dp0
 cd %BATCH_DIR%
 cd ..\..
 
-set GOT_REVISION=%1%
 set MY_DIR=%CD%
+
+call build\auto\helper_get_svnversion.cmd
+
+set MPT_REVISION=%SVNVERSION%
 
 
 
 cd bin || goto error
 rmdir /s /q libopenmpt-win
 del /f /q libopenmpt-win.tar
-del /f /q libopenmpt-win-r%GOT_REVISION%.7z
+del /f /q libopenmpt-win-r%MPT_REVISION%.7z
 mkdir libopenmpt-win
 cd libopenmpt-win || goto error
 mkdir openmpt123
@@ -28,7 +31,7 @@ copy /y ..\..\bin\Win32\openmpt123.exe .\openmpt123\ || goto error
 copy /y ..\..\bin\Win32\xmp-openmpt.dll .\XMPlay\ || goto error
 copy /y ..\..\bin\Win32\in_openmpt.dll .\Winamp\ || goto error
 copy /y ..\..\bin\Win32\foo_openmpt.dll .\foobar2000\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-win-r%GOT_REVISION%.7z ^
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-win-r%MPT_REVISION%.7z ^
  LICENSE.txt ^
  changelog.md ^
  openmpt123\openmpt123.exe ^
@@ -40,14 +43,14 @@ copy /y ..\..\bin\Win32\foo_openmpt.dll .\foobar2000\ || goto error
  foobar2000\foo_openmpt.dll ^
  || goto error
 cd .. || goto error
-"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-win.tar libopenmpt-win-r%GOT_REVISION%.7z || goto error
-del /f /q libopenmpt-win-r%GOT_REVISION%.7z
+"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-win.tar libopenmpt-win-r%MPT_REVISION%.7z || goto error
+del /f /q libopenmpt-win-r%MPT_REVISION%.7z
 cd .. || goto error
 
 cd bin || goto error
 rmdir /s /q libopenmpt-dev-vs2010
 del /f /q libopenmpt-dev-vs2010.tar
-del /f /q libopenmpt-dev-vs2010-r%GOT_REVISION%.7z
+del /f /q libopenmpt-dev-vs2010-r%MPT_REVISION%.7z
 mkdir libopenmpt-dev-vs2010
 cd libopenmpt-dev-vs2010 || goto error
 mkdir inc
@@ -69,7 +72,7 @@ copy /y ..\..\bin\Win32\libopenmpt.lib lib\x86\ || goto error
 copy /y ..\..\bin\Win32\libopenmpt.dll bin\x86\ || goto error
 copy /y ..\..\bin\x64\libopenmpt.lib lib\x86_64\ || goto error
 copy /y ..\..\bin\x64\libopenmpt.dll bin\x86_64\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-dev-vs2010-r%GOT_REVISION%.7z ^
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-dev-vs2010-r%MPT_REVISION%.7z ^
  LICENSE.txt ^
  changelog.md ^
  inc\libopenmpt\libopenmpt.h ^
@@ -83,8 +86,8 @@ copy /y ..\..\bin\x64\libopenmpt.dll bin\x86_64\ || goto error
  bin\x86_64\libopenmpt.dll ^
  || goto error
 cd .. || goto error
-"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-dev-vs2010.tar libopenmpt-dev-vs2010-r%GOT_REVISION%.7z || goto error
-del /f /q libopenmpt-dev-vs2010-r%GOT_REVISION%.7z
+"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-dev-vs2010.tar libopenmpt-dev-vs2010-r%MPT_REVISION%.7z || goto error
+del /f /q libopenmpt-dev-vs2010-r%MPT_REVISION%.7z
 cd .. || goto error
 
 

build/auto/package_openmpt_win32.cmd
@@ -6,23 +6,23 @@ cd ..\..
 
 set MY_DIR=%CD%
 
-call build\auto\setup_vs2010.cmd
+call build\auto\setup_vs_any.cmd
 
 call build\auto\helper_get_svnversion.cmd
 call build\auto\helper_get_openmpt_version.cmd
 
-set GOT_REVISION=%OPENMPT_VERSION%-r%SVNVERSION%
+set MPT_REVISION=%OPENMPT_VERSION%-r%SVNVERSION%
 
 
 cd bin\Win32 || goto error
 del /f /q openmpt-win32.tar
-del /f /q openmpt-win32-%GOT_REVISION%.7z
+del /f /q openmpt-win32-%MPT_REVISION%.7z
 copy /y ..\..\LICENSE .\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_f32.dll "MIDI Input Output.dll" PluginBridge32.exe PluginBridge64.exe LICENSE || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-%GOT_REVISION%-symbols.7z mptrack.pdb || goto error
-"C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win32.tar openmpt-win32-%GOT_REVISION%.7z openmpt-win32-%GOT_REVISION%-symbols.7z || goto error
-del /f /q openmpt-win32-%GOT_REVISION%.7z
-del /f /q openmpt-win32-%GOT_REVISION%-symbols.7z
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-%MPT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_f32.dll "MIDI Input Output.dll" PluginBridge32.exe PluginBridge64.exe LICENSE || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32-%MPT_REVISION%-symbols.7z mptrack.pdb || goto error
+"C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win32.tar openmpt-win32-%MPT_REVISION%.7z openmpt-win32-%MPT_REVISION%-symbols.7z || goto error
+del /f /q openmpt-win32-%MPT_REVISION%.7z
+del /f /q openmpt-win32-%MPT_REVISION%-symbols.7z
 cd ..\.. || goto error
 
 

build/auto/package_openmpt_win64.cmd
@@ -6,23 +6,23 @@ cd ..\..
 
 set MY_DIR=%CD%
 
-call build\auto\setup_vs2010.cmd
+call build\auto\setup_vs_any.cmd
 
 call build\auto\helper_get_svnversion.cmd
 call build\auto\helper_get_openmpt_version.cmd
 
-set GOT_REVISION=%OPENMPT_VERSION%-r%SVNVERSION%
+set MPT_REVISION=%OPENMPT_VERSION%-r%SVNVERSION%
 
 
 cd bin\x64 || goto error
 del /f /q openmpt-win64.tar
-del /f /q openmpt-win64-%GOT_REVISION%.7z
+del /f /q openmpt-win64-%MPT_REVISION%.7z
 copy /y ..\..\LICENSE .\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win64-%GOT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_f32.dll "MIDI Input Output.dll" PluginBridge32.exe PluginBridge64.exe LICENSE || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win64-%GOT_REVISION%-symbols.7z mptrack.pdb || goto error
-"C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win64.tar openmpt-win64-%GOT_REVISION%.7z openmpt-win64-%GOT_REVISION%-symbols.7z || goto error
-del /f /q openmpt-win64-%GOT_REVISION%.7z
-del /f /q openmpt-win64-%GOT_REVISION%-symbols.7z
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win64-%MPT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_f32.dll "MIDI Input Output.dll" PluginBridge32.exe PluginBridge64.exe LICENSE || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win64-%MPT_REVISION%-symbols.7z mptrack.pdb || goto error
+"C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win64.tar openmpt-win64-%MPT_REVISION%.7z openmpt-win64-%MPT_REVISION%-symbols.7z || goto error
+del /f /q openmpt-win64-%MPT_REVISION%.7z
+del /f /q openmpt-win64-%MPT_REVISION%-symbols.7z
 cd ..\.. || goto error
 
 

commit 545fb4269fda8208821e2ddaf96fa1dd9ed3befc

[Fix] build: Repair openmpt automated packaging. Broke in r5117.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5120 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/auto/setup_vs_any.cmd
@@ -2,47 +2,47 @@
 
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
- call setup_vs2015.cmd
+ call %~dp0\setup_vs2015.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
- call setup_vs2015.cmd
+ call %~dp0\setup_vs2015.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
- call setup_vs2013.cmd
+ call %~dp0\setup_vs2013.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
- call setup_vs2013.cmd
+ call %~dp0\setup_vs2013.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
- call setup_vs2012.cmd
+ call %~dp0\setup_vs2012.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
- call setup_vs2012.cmd
+ call %~dp0\setup_vs2012.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
- call setup_vs2010.cmd
+ call %~dp0\setup_vs2010.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
- call setup_vs2010.cmd
+ call %~dp0\setup_vs2010.cmd
  goto vsdone
 )
 
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
- call setup_vs2008.cmd
+ call %~dp0\setup_vs2008.cmd
  goto vsdone
 )
 if exist "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" (
- call setup_vs2008.cmd
+ call %~dp0\setup_vs2008.cmd
  goto vsdone
 )
 