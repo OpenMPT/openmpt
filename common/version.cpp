4c454052cd6c075181d76979226208c4cfbe1aa8
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-05-29 11:24:15 +0000

    [Var] Add some separating newlines to the credit string to improve readability.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5203 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/version.cpp
+++ common/version.cpp
@@ -392,6 +392,7 @@ std::string GetContactString()
 std::string GetFullCreditsString()
 {
 	return
+		"\n"
 #ifdef MODPLUG_TRACKER
 		"OpenMPT / ModPlug Tracker\n"
 #else
@@ -419,82 +420,100 @@ std::string GetFullCreditsString()
 		"\n"
 		"Konstanty for the XMMS-ModPlug resampling implementation\n"
 		"http://modplug-xmms.sourceforge.net/\n"
+		"\n"
 #ifdef MODPLUG_TRACKER
 		"Stephan M. Bernsee for pitch shifting source code\n"
 		"http://www.dspdimension.com/\n"
+		"\n"
 		"Aleksey Vaneev of Voxengo for r8brain sample rate converter\n"
 		"https://github.com/avaneev/r8brain-free-src\n"
+		"\n"
 		"Olli Parviainen for SoundTouch Library (time stretching)\n"
 		"http://www.surina.net/soundtouch/\n"
+		"\n"
 #endif
 #ifndef NO_VST
 		"Hermann Seib for his example VST Host implementation\n"
 		"http://www.hermannseib.com/english/vsthost.htm\n"
+		"\n"
 #endif
 #ifndef NO_MO3
 		"Ian Luck for UNMO3\n"
 		"http://www.un4seen.com/mo3.html\n"
+		"\n"
 #endif
 		"Ben \"GreaseMonkey\" Russell for IT sample compression code\n"
 		"https://github.com/iamgreaser/it2everything/\n"
+		"\n"
 		"Alexander Chemeris for msinttypes\n"
 		"https://github.com/chemeris/msinttypes\n"
+		"\n"
 #ifndef NO_ZLIB
 		"Jean-loup Gailly and Mark Adler for zlib\n"
 		"http://zlib.net/\n"
+		"\n"
 #endif
 #ifndef NO_MINIZ
 		"Rich Geldreich for miniz\n"
 		"http://code.google.com/p/miniz/\n"
+		"\n"
 #endif
 #ifndef NO_ARCHIVE_SUPPORT
 		"Simon Howard for lhasa\n"
 		"http://fragglet.github.io/lhasa/\n"
+		"\n"
 		"Alexander L. Roshal for UnRAR\n"
 		"http://rarlab.com/\n"
+		"\n"
 #endif
 #ifndef NO_PORTAUDIO
 		"PortAudio contributors\n"
 		"http://www.portaudio.com/\n"
+		"\n"
 #endif
 #ifndef NO_FLAC
 		"Josh Coalson for libFLAC\n"
 		"http://flac.sourceforge.net/\n"
+		"\n"
 #endif
 #ifndef NO_MP3_SAMPLES
 		"The mpg123 project for libmpg123\n"
 		"http://mpg123.de/\n"
+		"\n"
 #endif
 #ifndef NO_OGG
 		"Xiph.Org Foundation for libogg\n"
 		"https://xiph.org/ogg/\n"
+		"\n"
 #endif
 		"Storlek for all the IT compatibility hints and testcases\n"
 		"as well as the IMF, OKT and ULT loaders\n"
 		"http://schismtracker.org/\n"
+		"\n"
 #ifdef MODPLUG_TRACKER
 		"Pel K. Txnder for the scrolling credits control :)\n"
 		"http://tinyurl.com/4yze8\n"
+		"\n"
 		"Nobuyuki for application and file icon\n"
 		"http://twitter.com/nobuyukinyuu\n"
-#endif
 		"\n"
+#endif
 		"The people at ModPlug forums for crucial contribution\n"
-		"in the form of ideas, testing and support; thanks\n"
-		"particularly to:\n"
+		"in the form of ideas, testing and support;\n"
+		"thanks particularly to:\n"
 		"33, 8bitbubsy, Anboi, BooT-SectoR-ViruZ, Bvanoudtshoorn\n"
 		"christofori, Diamond, Ganja, Georg, Goor00, jmkz,\n"
 		"KrazyKatz, LPChip, Nofold, Rakib, Sam Zen\n"
 		"Skaven, Skilletaudio, Snu, Squirrel Havoc, Waxhead\n"
-#ifndef NO_VST
 		"\n"
+#ifndef NO_VST
 		"VST PlugIn Technology by Steinberg Media Technologies GmbH\n"
+		"\n"
 #endif
 #ifndef NO_ASIO
-		"\n"
 		"ASIO Technology by Steinberg Media Technologies GmbH\n"
-#endif
 		"\n"
+#endif
 		;
 
 }
