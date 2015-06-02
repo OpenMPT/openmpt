2424bea8ed8d838fd0e97b1824ff33e7dd6f5f74
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-06-02 15:11:53 +0000

    [Ref] Split out mptStringFormat and mptStringParse.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5240 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/misc_util.h
+++ common/misc_util.h
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include "mptStringParse.h"
 #include "mptCPU.h"
 #include "mptOS.h"
 #include "mptTime.h"
@@ -39,107 +40,6 @@ OPENMPT_NAMESPACE_BEGIN
 #define M_PI_2     1.57079632679489661923
 #endif
 
-
-bool ConvertStrToBool(const std::string &str);
-signed char ConvertStrToSignedChar(const std::string &str);
-unsigned char ConvertStrToUnsignedChar(const std::string &str);
-signed short ConvertStrToSignedShort(const std::string &str);
-unsigned short ConvertStrToUnsignedShort(const std::string &str);
-signed int ConvertStrToSignedInt(const std::string &str);
-unsigned int ConvertStrToUnsignedInt(const std::string &str);
-signed long ConvertStrToSignedLong(const std::string &str);
-unsigned long ConvertStrToUnsignedLong(const std::string &str);
-signed long long ConvertStrToSignedLongLong(const std::string &str);
-unsigned long long ConvertStrToUnsignedLongLong(const std::string &str);
-float ConvertStrToFloat(const std::string &str);
-double ConvertStrToDouble(const std::string &str);
-long double ConvertStrToLongDouble(const std::string &str);
-template<typename T> inline T ConvertStrTo(const std::string &str); // not defined, generates compiler error for non-specialized types
-template<> inline std::string ConvertStrTo(const std::string &str) { return str; }
-template<> inline bool ConvertStrTo(const std::string &str) { return ConvertStrToBool(str); }
-template<> inline signed char ConvertStrTo(const std::string &str) { return ConvertStrToSignedChar(str); }
-template<> inline unsigned char ConvertStrTo(const std::string &str) { return ConvertStrToUnsignedChar(str); }
-template<> inline signed short ConvertStrTo(const std::string &str) { return ConvertStrToSignedShort(str); }
-template<> inline unsigned short ConvertStrTo(const std::string &str) { return ConvertStrToUnsignedShort(str); }
-template<> inline signed int ConvertStrTo(const std::string &str) { return ConvertStrToSignedInt(str); }
-template<> inline unsigned int ConvertStrTo(const std::string &str) { return ConvertStrToUnsignedInt(str); }
-template<> inline signed long ConvertStrTo(const std::string &str) { return ConvertStrToSignedLong(str); }
-template<> inline unsigned long ConvertStrTo(const std::string &str) { return ConvertStrToUnsignedLong(str); }
-template<> inline signed long long ConvertStrTo(const std::string &str) { return ConvertStrToSignedLongLong(str); }
-template<> inline unsigned long long ConvertStrTo(const std::string &str) { return ConvertStrToUnsignedLongLong(str); }
-template<> inline float ConvertStrTo(const std::string &str) { return ConvertStrToFloat(str); }
-template<> inline double ConvertStrTo(const std::string &str) { return ConvertStrToDouble(str); }
-template<> inline long double ConvertStrTo(const std::string &str) { return ConvertStrToLongDouble(str); }
-
-#if MPT_WSTRING_FORMAT
-bool ConvertStrToBool(const std::wstring &str);
-signed char ConvertStrToSignedChar(const std::wstring &str);
-unsigned char ConvertStrToUnsignedChar(const std::wstring &str);
-signed short ConvertStrToSignedShort(const std::wstring &str);
-unsigned short ConvertStrToUnsignedShort(const std::wstring &str);
-signed int ConvertStrToSignedInt(const std::wstring &str);
-unsigned int ConvertStrToUnsignedInt(const std::wstring &str);
-signed long ConvertStrToSignedLong(const std::wstring &str);
-unsigned long ConvertStrToUnsignedLong(const std::wstring &str);
-signed long long ConvertStrToSignedLongLong(const std::wstring &str);
-unsigned long long ConvertStrToUnsignedLongLong(const std::wstring &str);
-float ConvertStrToFloat(const std::wstring &str);
-double ConvertStrToDouble(const std::wstring &str);
-long double ConvertStrToLongDouble(const std::wstring &str);
-template<typename T> inline T ConvertStrTo(const std::wstring &str); // not defined, generates compiler error for non-specialized types
-template<> inline std::wstring ConvertStrTo(const std::wstring &str) { return str; }
-template<> inline bool ConvertStrTo(const std::wstring &str) { return ConvertStrToBool(str); }
-template<> inline signed char ConvertStrTo(const std::wstring &str) { return ConvertStrToSignedChar(str); }
-template<> inline unsigned char ConvertStrTo(const std::wstring &str) { return ConvertStrToUnsignedChar(str); }
-template<> inline signed short ConvertStrTo(const std::wstring &str) { return ConvertStrToSignedShort(str); }
-template<> inline unsigned short ConvertStrTo(const std::wstring &str) { return ConvertStrToUnsignedShort(str); }
-template<> inline signed int ConvertStrTo(const std::wstring &str) { return ConvertStrToSignedInt(str); }
-template<> inline unsigned int ConvertStrTo(const std::wstring &str) { return ConvertStrToUnsignedInt(str); }
-template<> inline signed long ConvertStrTo(const std::wstring &str) { return ConvertStrToSignedLong(str); }
-template<> inline unsigned long ConvertStrTo(const std::wstring &str) { return ConvertStrToUnsignedLong(str); }
-template<> inline signed long long ConvertStrTo(const std::wstring &str) { return ConvertStrToSignedLongLong(str); }
-template<> inline unsigned long long ConvertStrTo(const std::wstring &str) { return ConvertStrToUnsignedLongLong(str); }
-template<> inline float ConvertStrTo(const std::wstring &str) { return ConvertStrToFloat(str); }
-template<> inline double ConvertStrTo(const std::wstring &str) { return ConvertStrToDouble(str); }
-template<> inline long double ConvertStrTo(const std::wstring &str) { return ConvertStrToLongDouble(str); }
-#endif
-
-template<typename T>
-inline T ConvertStrTo(const char *str)
-{
-	if(!str)
-	{
-		return T();
-	}
-	return ConvertStrTo<T>(std::string(str));
-}
-
-#if MPT_WSTRING_FORMAT
-template<typename T>
-inline T ConvertStrTo(const wchar_t *str)
-{
-	if(!str)
-	{
-		return T();
-	}
-	return ConvertStrTo<T>(std::wstring(str));
-}
-#if MPT_USTRING_MODE_UTF8
-template<> inline mpt::ustring ConvertStrTo(const std::wstring &str) { return mpt::ToUnicode(str); }
-#endif
-#endif
-
-#if MPT_USTRING_MODE_UTF8
-template<typename T>
-inline T ConvertStrTo(const mpt::ustring &str)
-{
-	return ConvertStrTo<T>(mpt::ToCharset(mpt::CharsetUTF8, str));
-}
-template<> inline mpt::ustring ConvertStrTo(const mpt::ustring &str) { return str; }
-#if MPT_WSTRING_CONVERT
-template<> inline std::wstring ConvertStrTo(const mpt::ustring &str) { return mpt::ToWide(str); }
-#endif
-#endif
 
 
 namespace mpt { namespace String {
