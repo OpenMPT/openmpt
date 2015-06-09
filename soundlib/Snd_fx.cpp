OpenMPT/openmpt

commit 8e02f769fe0706a4b955cf30beb9188098c8c4de

[New] Plugins: Apply volume swing to instrument plugins. Involves rearranging some code which hopefully won't break anything... [Mod] OpenMPT: Version is now 1.25.00.07
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5148 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Snd_fx.cpp
@@ -1458,6 +1458,52 @@ void CSoundFile::NoteChange(ModChannel *pChn, int note, bool bPorta, bool bReset
 	// Test case: PanReset.it
 	if(IsCompatibleMode(TRK_IMPULSETRACKER)) ApplyInstrumentPanning(pChn, pIns, pSmp);
 
+	if(bResetEnv)
+	{
+		pChn->nVolSwing = pChn->nPanSwing = 0;
+		pChn->nResSwing = pChn->nCutSwing = 0;
+		if(pIns)
+		{
+			// IT Compatiblity: NNA is reset on every note change, not every instrument change (fixes spx-farspacedance.it).
+			if(IsCompatibleMode(TRK_IMPULSETRACKER)) pChn->nNNA = pIns->nNNA;
+
+			if(!pIns->VolEnv.dwFlags[ENV_CARRY]) pChn->VolEnv.Reset();
+			if(!pIns->PanEnv.dwFlags[ENV_CARRY]) pChn->PanEnv.Reset();
+			if(!pIns->PitchEnv.dwFlags[ENV_CARRY]) pChn->PitchEnv.Reset();
+
+			// Volume Swing
+			if(pIns->nVolSwing)
+			{
+				const double delta = 2 * (((double) rand()) / RAND_MAX) - 1;
+				pChn->nVolSwing = static_cast<int32>(std::floor(delta * (IsCompatibleMode(TRK_IMPULSETRACKER) ? pChn->nInsVol : ((pChn->nVolume + 1) / 2)) * pIns->nVolSwing / 100.0));
+			}
+			// Pan Swing
+			if(pIns->nPanSwing)
+			{
+				const double delta = 2 * (((double) rand()) / RAND_MAX) - 1;
+				pChn->nPanSwing = static_cast<int32>(std::floor(delta * (IsCompatibleMode(TRK_IMPULSETRACKER) ? 4 : 1) * pIns->nPanSwing));
+				if(!IsCompatibleMode(TRK_IMPULSETRACKER))
+				{
+					pChn->nRestorePanOnNewNote = pChn->nPan + 1;
+				}
+			}
+			// Cutoff Swing
+			if(pIns->nCutSwing)
+			{
+				int32 d = ((int32)pIns->nCutSwing * (int32)((rand() & 0xFF) - 0x7F)) / 128;
+				pChn->nCutSwing = (int16)((d * pChn->nCutOff + 1) / 128);
+				pChn->nRestoreCutoffOnNewNote = pChn->nCutOff + 1;
+			}
+			// Resonance Swing
+			if(pIns->nResSwing)
+			{
+				int32 d = ((int32)pIns->nResSwing * (int32)((rand() & 0xFF) - 0x7F)) / 128;
+				pChn->nResSwing = (int16)((d * pChn->nResonance + 1) / 128);
+				pChn->nRestoreResonanceOnNewNote = pChn->nResonance + 1;
+			}
+		}
+	}
+
 	if(!pSmp) return;
 	if(period)
 	{
@@ -1591,48 +1637,6 @@ void CSoundFile::NoteChange(ModChannel *pChn, int note, bool bPorta, bool bReset
 
 		if(bResetEnv)
 		{
-			pChn->nVolSwing = pChn->nPanSwing = 0;
-			pChn->nResSwing = pChn->nCutSwing = 0;
-			if(pIns)
-			{
-				// IT Compatiblity: NNA is reset on every note change, not every instrument change (fixes spx-farspacedance.it).
-				if(IsCompatibleMode(TRK_IMPULSETRACKER)) pChn->nNNA = pIns->nNNA;
-
-				if(!pIns->VolEnv.dwFlags[ENV_CARRY]) pChn->VolEnv.Reset();
-				if(!pIns->PanEnv.dwFlags[ENV_CARRY]) pChn->PanEnv.Reset();
-				if(!pIns->PitchEnv.dwFlags[ENV_CARRY]) pChn->PitchEnv.Reset();
-
-				// Volume Swing
-				if(pIns->nVolSwing)
-				{
-					const double delta = 2 * (((double) rand()) / RAND_MAX) - 1;
-					pChn->nVolSwing = static_cast<int32>(std::floor(delta * (IsCompatibleMode(TRK_IMPULSETRACKER) ? pChn->nInsVol : ((pChn->nVolume + 1) / 2)) * pIns->nVolSwing / 100.0));
-				}
-				// Pan Swing
-				if(pIns->nPanSwing)
-				{
-					const double delta = 2 * (((double) rand()) / RAND_MAX) - 1;
-					pChn->nPanSwing = static_cast<int32>(std::floor(delta * (IsCompatibleMode(TRK_IMPULSETRACKER) ? 4 : 1) * pIns->nPanSwing));
-					if(!IsCompatibleMode(TRK_IMPULSETRACKER))
-					{
-						pChn->nRestorePanOnNewNote = pChn->nPan + 1;
-					}
-				}
-				// Cutoff Swing
-				if(pIns->nCutSwing)
-				{
-					int32 d = ((int32)pIns->nCutSwing * (int32)((rand() & 0xFF) - 0x7F)) / 128;
-					pChn->nCutSwing = (int16)((d * pChn->nCutOff + 1) / 128);
-					pChn->nRestoreCutoffOnNewNote = pChn->nCutOff + 1;
-				}
-				// Resonance Swing
-				if(pIns->nResSwing)
-				{
-					int32 d = ((int32)pIns->nResSwing * (int32)((rand() & 0xFF) - 0x7F)) / 128;
-					pChn->nResSwing = (int16)((d * pChn->nResonance + 1) / 128);
-					pChn->nRestoreResonanceOnNewNote = pChn->nResonance + 1;
-				}
-			}
 			pChn->nAutoVibDepth = 0;
 			pChn->nAutoVibPos = 0;
 			// IT Compatibility: Vibrato reset

common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               25
 #define VER_MINOR               00
-#define VER_MINORMINOR          06
+#define VER_MINORMINOR          07
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

soundlib/Sndmix.cpp
@@ -2261,20 +2261,25 @@ void CSoundFile::ProcessMidiOut(CHANNELINDEX nChn)
 	//If new note, determine notevelocity to use.
 	if(note != NOTE_NONE)
 	{
-		uint16 velocity = static_cast<uint16>(4 * defaultVolume);
+		int32 velocity = static_cast<int32>(4 * defaultVolume);
 		switch(pIns->nPluginVelocityHandling)
 		{
 			case PLUGIN_VELOCITYHANDLING_CHANNEL:
-				velocity = static_cast<uint16>(chn.nVolume);
+				velocity = chn.nVolume;
 			break;
 		}
+
+		int32 swing = chn.nVolSwing;
+		if(IsCompatibleMode(TRK_IMPULSETRACKER)) swing *= 4;
+		velocity += swing;
+		Limit(velocity, 0, 256);
 
 		ModCommand::NOTE realNote = note;
 		if(ModCommand::IsNote(note))
 			realNote = pIns->NoteMap[note - NOTE_MIN];
 		// Experimental VST panning
 		//ProcessMIDIMacro(nChn, false, m_MidiCfg.szMidiGlb[MIDIOUT_PAN], 0, nPlugin);
-		pPlugin->MidiCommand(GetBestMidiChannel(nChn), pIns->nMidiProgram, pIns->wMidiBank, realNote, velocity, nChn);
+		pPlugin->MidiCommand(GetBestMidiChannel(nChn), pIns->nMidiProgram, pIns->wMidiBank, realNote, static_cast<uint16>(velocity), nChn);
 	}
 
 

commit 03943b5e9a1c36ecb3792048f8ca3c880df0b44c

[Fix] Various portamento-related things broke in r5148.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5285 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Snd_fx.cpp
@@ -1164,7 +1164,7 @@ void CSoundFile::InstrumentChange(ModChannel *pChn, UINT instr, bool bPorta, boo
 	}
 
 	// Reset envelopes
-	if(bResetEnv)
+	if(bResetEnv && !bPorta)
 	{
 		// Blurb by Storlek (from the SchismTracker code):
 		// Conditions experimentally determined to cause envelope reset in Impulse Tracker: