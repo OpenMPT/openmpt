bc4496762adab6d875cec537456cf2116fdc575f
Jörn Heusipp <manxorist@users.sourceforge.net>
2015-06-14 08:49:52 +0000

    [Imp] build: Add packaging script for automated VS2008 win32 builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5316 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/package_openmpt_win32old.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+cd ..\..
+
+set MY_DIR=%CD%
+
+call build\auto\setup_vs_any.cmd
+
+call build\auto\helper_get_svnversion.cmd
+call build\auto\helper_get_openmpt_version.cmd
+
+set MPT_REVISION=%OPENMPT_VERSION%-r%SVNVERSION%
+
+
+cd bin\Win32 || goto error
+del /f /q openmpt-win32old.tar
+del /f /q openmpt-win32old-%MPT_REVISION%.7z
+copy /y ..\..\LICENSE .\ || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32old-%MPT_REVISION%.7z mptrack.exe OpenMPT_SoundTouch_f32.dll "MIDI Input Output.dll" PluginBridge32.exe PluginBridge64.exe LICENSE || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 openmpt-win32old-%MPT_REVISION%-symbols.7z mptrack.pdb || goto error
+"C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-win32old.tar openmpt-win32old-%MPT_REVISION%.7z openmpt-win32old-%MPT_REVISION%-symbols.7z || goto error
+del /f /q openmpt-win32old-%MPT_REVISION%.7z
+del /f /q openmpt-win32old-%MPT_REVISION%-symbols.7z
+cd ..\.. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
