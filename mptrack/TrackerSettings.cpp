OpenMPT/openmpt

commit c38530211a9c830a06576220e5c3266aee3b53cd

[New] General tab: Can now set song artist and song-specific resampling mode [Reg] General tab: Remove button leading to mixer settings. This was mostly useful for quickly changing the resampling mode, which can now be done directly on the general tab. [New] Hidden setting: Misc.DefaultArtist sets the default artist (will be editable in GUI soon) [Mod] OpenMPT: Version is now 1.25.00.08
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5179 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/TrackerSettings.cpp
@@ -234,6 +234,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 	, ComponentsKeepLoaded(conf, "Components", "KeepLoaded", ComponentManagerSettingsDefault().KeepLoaded())
 	// Default template
 	, defaultTemplateFile(conf, "Paths", "DefaultTemplate", mpt::PathString())
+	, defaultArtist(conf, "Misc", "DefaultArtist", mpt::ToUnicode(mpt::CharsetLocale, std::getenv("USERNAME")))
 	// MRU List
 	, mruListLength(conf, "Misc", "MRUListLength", 10)
 	// Plugins
@@ -560,6 +561,11 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 		commentsFont = FontSetting("Courier New", (m_dwPatternSetup & 0x02) ? 120 : 90);
 		patternFont = FontSetting((m_dwPatternSetup & 0x08) ? PATTERNFONT_SMALL : PATTERNFONT_LARGE, 0);
 		m_dwPatternSetup &= ~(0x08 | 0x02);
+	}
+
+	if(storedVersion < MAKE_VERSION_NUMERIC(1,25,00,08))
+	{
+		glGeneralWindowHeight += 36;
 	}
 
 	// Effects

common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               25
 #define VER_MINOR               00
-#define VER_MINORMINOR          07
+#define VER_MINORMINOR          08
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

mptrack/Ctrl_gen.cpp
@@ -29,10 +29,9 @@ BEGIN_MESSAGE_MAP(CCtrlGeneral, CModControlDlg)
 	ON_WM_VSCROLL()
 	ON_COMMAND(IDC_BUTTON1,					OnTapTempo)
 	ON_COMMAND(IDC_BUTTON_MODTYPE,			OnSongProperties)
-	ON_COMMAND(IDC_EDIT_MODTYPE,			OnSongProperties)
-	ON_COMMAND(IDC_BUTTON_PLAYERPROPS,		OnPlayerProperties)
 	ON_COMMAND(IDC_CHECK_LOOPSONG,			OnLoopSongChanged)
 	ON_EN_CHANGE(IDC_EDIT_SONGTITLE,		OnTitleChanged)
+	ON_EN_CHANGE(IDC_EDIT_ARTIST,			OnArtistChanged)
 	ON_EN_CHANGE(IDC_EDIT_TEMPO,			OnTempoChanged)
 	ON_EN_CHANGE(IDC_EDIT_SPEED,			OnSpeedChanged)
 	ON_EN_CHANGE(IDC_EDIT_GLOBALVOL,		OnGlobalVolChanged)
@@ -41,6 +40,7 @@ BEGIN_MESSAGE_MAP(CCtrlGeneral, CModControlDlg)
 	ON_EN_CHANGE(IDC_EDIT_SAMPLEPA,			OnSamplePAChanged)
 	ON_MESSAGE(WM_MOD_UPDATEPOSITION,		OnUpdatePosition)
 	ON_EN_SETFOCUS(IDC_EDIT_SONGTITLE,		OnEnSetfocusEditSongtitle)
+	ON_CBN_SELCHANGE(IDC_COMBO1,			OnResamplingChanged)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -50,6 +50,7 @@ void CCtrlGeneral::DoDataExchange(CDataExchange* pDX)
 	CModControlDlg::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CCtrlGeneral)
 	DDX_Control(pDX, IDC_EDIT_SONGTITLE,	m_EditTitle);
+	DDX_Control(pDX, IDC_EDIT_ARTIST,		m_EditArtist);
 	DDX_Control(pDX, IDC_EDIT_TEMPO,		m_EditTempo);
 	DDX_Control(pDX, IDC_SPIN_TEMPO,		m_SpinTempo);
 	DDX_Control(pDX, IDC_EDIT_SPEED,		m_EditSpeed);
@@ -69,9 +70,11 @@ void CCtrlGeneral::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_SLIDER_GLOBALVOL,	m_SliderGlobalVol);
 	DDX_Control(pDX, IDC_SLIDER_SAMPLEPREAMP,	m_SliderSamplePreAmp);
 
-	DDX_Control(pDX, IDC_EDIT_MODTYPE,		m_EditModType);
+	DDX_Control(pDX, IDC_BUTTON_MODTYPE,	m_BtnModType);
 	DDX_Control(pDX, IDC_VUMETER_LEFT,		m_VuMeterLeft);
 	DDX_Control(pDX, IDC_VUMETER_RIGHT,		m_VuMeterRight);
+
+	DDX_Control(pDX, IDC_COMBO1,			m_CbnResampling);
 	//}}AFX_DATA_MAP
 }
 
@@ -208,6 +211,32 @@ void CCtrlGeneral::UpdateView(UpdateHint hint, CObject *pHint)
 	if (pHint == this) return;
 	FlagSet<HintType> hintType = hint.GetType();
 	const bool updateAll = hintType[HINT_MODTYPE];
+	if (hintType == HINT_MPTOPTIONS || updateAll)
+	{
+		m_CbnResampling.ResetContent();
+		const struct
+		{
+			const TCHAR *name;
+			ResamplingMode mode;
+		} interpolationTypes[] =
+		{
+			{ _T("No Interpolation"), SRCMODE_NEAREST },
+			{ _T("Linear"), SRCMODE_LINEAR },
+			{ _T("Cubic Spline"), SRCMODE_SPLINE },
+			{ _T("Polyphase"), SRCMODE_POLYPHASE },
+			{ _T("XMMS-ModPlug"), SRCMODE_FIRFILTER },
+		};
+		m_CbnResampling.SetItemData(m_CbnResampling.AddString(_T("Default (") + CString(interpolationTypes[TrackerSettings::Instance().ResamplerMode].name) + _T(")")), SRCMODE_DEFAULT);
+		int selection = 0;
+		for(int i = 0; i < CountOf(interpolationTypes); i++)
+		{
+			if(m_sndFile.m_nResampling == interpolationTypes[i].mode) selection = i + 1;
+			m_CbnResampling.SetItemData(m_CbnResampling.AddString(interpolationTypes[i].name), interpolationTypes[i].mode);
+		}
+	
+		m_CbnResampling.SetCurSel(selection);
+		m_CbnResampling.Invalidate(FALSE);
+	}
 	if (updateAll || (hint.GetCategory() == HINTCAT_SEQUENCE && hintType[HINT_MODSEQUENCE]))
 	{
 		// Set max valid restart position
@@ -218,6 +247,7 @@ void CCtrlGeneral::UpdateView(UpdateHint hint, CObject *pHint)
 		if (!m_bEditsLocked)
 		{
 			m_EditTitle.SetWindowText(m_sndFile.GetTitle().c_str());
+			::SetWindowTextW(m_EditArtist.m_hWnd, mpt::ToWide(m_sndFile.songArtist).c_str());
 			SetDlgItemInt(IDC_EDIT_TEMPO, m_sndFile.m_nDefaultTempo, FALSE);
 			SetDlgItemInt(IDC_EDIT_SPEED, m_sndFile.m_nDefaultSpeed, FALSE);
 			SetDlgItemInt(IDC_EDIT_GLOBALVOL, m_sndFile.m_nDefaultGlobalVolume / GetGlobalVolumeFactor(), FALSE);
@@ -241,6 +271,7 @@ void CCtrlGeneral::UpdateView(UpdateHint hint, CObject *pHint)
 		const BOOL bIsNotMOD = (m_sndFile.GetType() != MOD_TYPE_MOD);
 		const BOOL bIsNotMOD_S3M = ((bIsNotMOD) && (m_sndFile.GetType() != MOD_TYPE_S3M));
 		const BOOL bIsNotMOD_XM = ((bIsNotMOD) && (m_sndFile.GetType() != MOD_TYPE_XM));
+		m_EditArtist.EnableWindow(bIsNotMOD_S3M);
 		m_EditTempo.EnableWindow(bIsNotMOD);
 		m_SpinTempo.EnableWindow(bIsNotMOD);
 		GetDlgItem(IDC_BUTTON1)->EnableWindow(bIsNotMOD);
@@ -274,7 +305,7 @@ void CCtrlGeneral::UpdateView(UpdateHint hint, CObject *pHint)
 		}
 		TCHAR s[256];
 		wsprintf(s, _T("%s, %d channel%s"), modType, m_sndFile.GetNumChannels(), (m_sndFile.GetNumChannels() != 1) ? _T("s") : _T(""));
-		m_EditModType.SetWindowText(s);
+		m_BtnModType.SetWindowText(s);
 	}
 	CheckDlgButton(IDC_CHECK_LOOPSONG,	(TrackerSettings::Instance().gbLoopSong) ? TRUE : FALSE);
 	if (hintType[HINT_MPTOPTIONS])
@@ -363,6 +394,25 @@ void CCtrlGeneral::OnTitleChanged()
 		m_EditTitle.SetModify(FALSE);
 		m_modDoc.SetModified();
 		m_modDoc.UpdateAllViews(nullptr, GeneralHint().General(), this);
+	}
+}
+
+
+void CCtrlGeneral::OnArtistChanged()
+//----------------------------------
+{
+	if (!m_EditArtist.m_hWnd || !m_EditArtist.GetModify()) return;
+
+	CStringW artist;
+	int len = ::GetWindowTextLengthW(m_EditArtist.m_hWnd);
+	::GetWindowTextW(m_EditArtist.m_hWnd, artist.GetBufferSetLength(len), len + 1);
+	artist.ReleaseBuffer();
+	if(artist != m_sndFile.songArtist.c_str())
+	{
+		m_EditArtist.SetModify(FALSE);
+		m_sndFile.songArtist = artist;
+		m_modDoc.SetModified();
+		m_modDoc.UpdateAllViews(NULL, GeneralHint().General(), this);
 	}
 }
 
@@ -523,15 +573,9 @@ void CCtrlGeneral::OnRestartPosChanged()
 	}
 }
 
-void CCtrlGeneral::OnPlayerProperties()
-//-------------------------------------
-{
-	CMainFrame::m_nLastOptionsPage = OPTIONS_PAGE_MIXER;
-	CMainFrame::GetMainFrame()->OnViewOptions();
-}
 
 void CCtrlGeneral::OnSongProperties()
-//----------------------------------
+//-----------------------------------
 {
 	m_modDoc.SongProperties();
 }
@@ -569,15 +613,15 @@ BOOL CCtrlGeneral::GetToolTipText(UINT uId, LPSTR pszText)
 		switch(uId)
 		{
 			case IDC_SLIDER_SAMPLEPREAMP:
-				(displayDBValues) ? setAsDecibels(pszText, m_sndFile.m_nSamplePreAmp, m_sndFile.GetPlayConfig().getNormalSamplePreAmp()) : wsprintf(pszText, moreRecentMixModeNote);
+				(displayDBValues) ? setAsDecibels(pszText, m_sndFile.m_nSamplePreAmp, m_sndFile.GetPlayConfig().getNormalSamplePreAmp()) : strcpy(pszText, moreRecentMixModeNote);
 				return TRUE;
 				break;
 			case IDC_SLIDER_VSTIVOL:
-				(displayDBValues) ? setAsDecibels(pszText, m_sndFile.m_nVSTiVolume, m_sndFile.GetPlayConfig().getNormalVSTiVol()) : wsprintf(pszText, moreRecentMixModeNote);
+				(displayDBValues) ? setAsDecibels(pszText, m_sndFile.m_nVSTiVolume, m_sndFile.GetPlayConfig().getNormalVSTiVol()) : strcpy(pszText, moreRecentMixModeNote);
 				return TRUE;
 				break;
 			case IDC_SLIDER_GLOBALVOL:
-				(displayDBValues) ? setAsDecibels(pszText, m_sndFile.m_PlayState.m_nGlobalVolume, m_sndFile.GetPlayConfig().getNormalGlobalVol()) : wsprintf(pszText, moreRecentMixModeNote);
+				(displayDBValues) ? setAsDecibels(pszText, m_sndFile.m_PlayState.m_nGlobalVolume, m_sndFile.GetPlayConfig().getNormalGlobalVol()) : strcpy(pszText, moreRecentMixModeNote);
 				return TRUE;
 				break;
 		}
@@ -611,6 +655,20 @@ void CCtrlGeneral::OnEnSetfocusEditSongtitle()
 	m_EditTitle.SetLimitText(m_sndFile.GetModSpecifications().modNameLengthMax);
 }
 
+
+void CCtrlGeneral::OnResamplingChanged()
+//--------------------------------------
+{
+	int sel = m_CbnResampling.GetCurSel();
+	if(sel >= 0)
+	{
+		m_sndFile.m_nResampling = static_cast<ResamplingMode>(m_CbnResampling.GetItemData(sel));
+		if(m_sndFile.GetType()  == MOD_TYPE_MPT)
+		{
+			m_modDoc.SetModified();
+		}
+	}
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////

mptrack/Ctrl_gen.h
@@ -62,12 +62,13 @@ private:
 public:
 	bool m_bEditsLocked;
 	//{{AFX_DATA(CCtrlGeneral)
-	CEdit m_EditTitle;
-	CStatic m_EditModType;
+	CEdit m_EditTitle, m_EditArtist;
 	CEdit m_EditTempo, m_EditSpeed, m_EditGlobalVol, m_EditRestartPos,
 		  m_EditSamplePA, m_EditVSTiVol;
+	CButton m_BtnModType;
 	CSpinButtonCtrl m_SpinTempo, m_SpinSpeed, m_SpinGlobalVol, m_SpinRestartPos, 
 				    m_SpinSamplePA, m_SpinVSTiVol;
+	CComboBox m_CbnResampling;
 
 	CSliderCtrl m_SliderTempo, m_SliderSamplePreAmp, m_SliderGlobalVol, m_SliderVSTiVol;
 	CVuMeter m_VuMeterLeft, m_VuMeterRight;
@@ -88,6 +89,7 @@ protected:
 	afx_msg void OnVScroll(UINT, UINT, CScrollBar *);
 	afx_msg void OnTapTempo();
 	afx_msg void OnTitleChanged();
+	afx_msg void OnArtistChanged();
 	afx_msg void OnTempoChanged();
 	afx_msg void OnSpeedChanged();
 	afx_msg void OnGlobalVolChanged();
@@ -95,9 +97,9 @@ protected:
 	afx_msg void OnSamplePAChanged();
 	afx_msg void OnRestartPosChanged();
 	afx_msg void OnSongProperties();
-	afx_msg void OnPlayerProperties();
 	afx_msg void OnLoopSongChanged();
 	afx_msg void OnEnSetfocusEditSongtitle();
+	afx_msg void OnResamplingChanged();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };

mptrack/MPTHacks.cpp
@@ -469,6 +469,16 @@ bool CModDoc::HasMPTHacks(const bool autofix)
 		}
 	}
 
+	if((m_SndFile.GetType() & (MOD_TYPE_XM|MOD_TYPE_IT)) && !m_SndFile.songArtist.empty())
+	{
+		AddToLog("Found artist name");
+		foundHacks = true;
+		if(autofix)
+		{
+			m_SndFile.songArtist.clear();
+		}
+	}
+
 	if(m_SndFile.GetMixLevels() != mixLevels_compatible && m_SndFile.GetMixLevels() != mixLevels_compatible_FT2)
 	{
 		AddToLog("Found incorrect mix levels (only compatible mix levels allowed)");

mptrack/ModConvert.cpp
@@ -571,6 +571,13 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 			CHANGEMODTYPE_WARNING(wGlobalVolumeNotSupported);
 		}
 	}
+
+	// Resampling is only saved in MPTM
+	if(!newTypeIsMPT && m_SndFile.m_nResampling != SRCMODE_DEFAULT)
+	{
+		CHANGEMODTYPE_WARNING(wResamplingMode);
+		m_SndFile.m_nResampling = SRCMODE_DEFAULT;
+	}
 		
 	// Pattern warnings
 	CHAR s[64];
@@ -606,6 +613,7 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 	CHANGEMODTYPE_CHECK(wVolRamp, "Fasttracker 2 compatible super soft volume ramping gets lost when converting XM files to another type.");
 	CHANGEMODTYPE_CHECK(wCompatibilityMode, "Consider enabling the \"compatible playback\" option in the song properties to increase compatiblity with other players.");
 	CHANGEMODTYPE_CHECK(wGlobalVolumeNotSupported, "Default global volume is not supported by the new format.");
+	CHANGEMODTYPE_CHECK(wResamplingMode, "Song-specific resampling mode is not supported by the new format.");
 
 	SetModified();
 	GetPatternUndo().ClearUndo();

mptrack/ModConvert.h
@@ -40,6 +40,7 @@ enum enmWarnings
 	wPitchToTempoLock,
 	wGlobalVolumeNotSupported,
 	wFilterVariation,
+	wResamplingMode,
 	wNumWarnings
 };
 

mptrack/Moddoc.cpp
@@ -772,6 +772,7 @@ BOOL CModDoc::InitializeMod()
 		}
 	}
 	m_SndFile.ResetPlayPos();
+	m_SndFile.songArtist = TrackerSettings::Instance().defaultArtist;
 
 	return TRUE;
 }

commit 3ca974e50cac8ddf7a69193a8311dfa85908831d

[Fix] std::getenv returns nullpointer for unset envvars. Fixes crash on Win98 and older Wine on startup. Broken since r5179.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5338 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/TrackerSettings.cpp
@@ -300,7 +300,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 	, PathUserTemplates(theApp.GetConfigPath() + MPT_PATHSTRING("TemplateModules\\"))
 	// Default template
 	, defaultTemplateFile(conf, "Paths", "DefaultTemplate", mpt::PathString())
-	, defaultArtist(conf, "Misc", "DefaultArtist", mpt::ToUnicode(mpt::CharsetLocale, std::getenv("USERNAME")))
+	, defaultArtist(conf, "Misc", "DefaultArtist", mpt::ToUnicode(mpt::CharsetLocale, std::getenv("USERNAME") ? std::getenv("USERNAME") : std::string()))
 	// MRU List
 	, mruListLength(conf, "Misc", "MRUListLength", 10)
 	// Plugins