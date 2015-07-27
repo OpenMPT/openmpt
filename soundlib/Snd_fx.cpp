ca0aae761d066aed0cef1517ae6cc15712673100
Johannes Schultz <sagamusix@openmpt.org>
2015-07-27 11:48:39 +0000

    [Imp] Improve NNA channel allocation by also taking volume envelopes into account: If all NNA channels are used and are playing at full note volume, but are effectively muted due to their volume envelope, they can now be reused.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5419 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -1753,23 +1753,24 @@ CHANNELINDEX CSoundFile::GetNNAChannel(CHANNELINDEX nChn) const
 //-------------------------------------------------------------
 {
 	const ModChannel *pChn = &m_PlayState.Chn[nChn];
+
 	// Check for empty channel
 	const ModChannel *pi = &m_PlayState.Chn[m_nChannels];
-	for (CHANNELINDEX i=m_nChannels; i<MAX_CHANNELS; i++, pi++) if (!pi->nLength) return i;
+	for (CHANNELINDEX i = m_nChannels; i < MAX_CHANNELS; i++, pi++) if (!pi->nLength) return i;
 	if (!pChn->nFadeOutVol) return 0;
+
 	// All channels are used: check for lowest volume
 	CHANNELINDEX result = 0;
-	uint32 vol = 64*65536;	// 25%
-	uint32 envpos = int32_max;
+	uint32 vol = (1u << (14 + 9)) / 4u;	// 25%
+	uint32 envpos = uint32_max;
 	const ModChannel *pj = &m_PlayState.Chn[m_nChannels];
-	for (CHANNELINDEX j=m_nChannels; j<MAX_CHANNELS; j++, pj++)
+	for (CHANNELINDEX j = m_nChannels; j < MAX_CHANNELS; j++, pj++)
 	{
 		if (!pj->nFadeOutVol) return j;
-		uint32 v = pj->nVolume;
-		if(pj->dwFlags[CHN_NOTEFADE])
-			v = v * pj->nFadeOutVol;
-		else
-			v <<= 16;
+		// Use a combination of real volume [14 bit] (which includes volume envelopes, but also potentially global volume) and note volume [9 bit].
+		// Rationale: We need volume envelopes in case e.g. all NNA channels are playing at full volume but are looping on a 0-volume envelope node.
+		// But if global volume is not applied to master and the global volume temporarily drops to 0, we would kill arbitrary channels. Hence, add the note volume as well.
+		uint32 v = (pj->nRealVolume << 9) | pj->nVolume;
 		if(pj->dwFlags[CHN_LOOP]) v >>= 1;
 		if ((v < vol) || ((v == vol) && (pj->VolEnv.nEnvPosition > envpos)))
 		{
