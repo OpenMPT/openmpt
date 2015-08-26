b66d7abc5b644570c1fbff6fdf647876e41e3176
Jörn Heusipp <manx@openmpt.org>
2015-08-26 09:03:07 +0000

    [Imp] libopenmpt: Add error handling to C API stdout example.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5521 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- examples/libopenmpt_example_c_stdout.c
+++ examples/libopenmpt_example_c_stdout.c
@@ -2,16 +2,13 @@
  * libopenmpt_example_c_stdout.c
  * -----------------------------
  * Purpose: libopenmpt C API simple example
- * Notes  : This simple example does no error cheking at all.
- *          This example writes raw 48000Hz / stereo / 16bit native endian PCM data to stdout.
+ * Notes  : This example writes raw 48000Hz / stereo / 16bit native endian PCM data to stdout.
  * Authors: OpenMPT Devs
  * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
  */
 
 /*
- * Usage example:
- *
- *     bin/libopenmpt_example_c_stdout SOMEMODULE | aplay --file-type raw --format=dat
+ * Usage: libopenmpt_example_c_stdout SOMEMODULE | aplay --file-type raw --format=dat
  */
 
 #include <memory.h>
@@ -28,6 +25,15 @@
 
 #define BUFFERSIZE 480
 #define SAMPLERATE 48000
+
+static void libopenmpt_example_logfunc( const char * message, void * userdata ) {
+	(void)userdata;
+
+	if ( message ) {
+		fprintf( stderr, "%s\n", message );
+	}
+
+}
 
 static ssize_t xwrite( int fd, const void * buffer, size_t size ) {
 	size_t written = 0;
@@ -52,24 +58,77 @@ int wmain( int argc, wchar_t * argv[] ) {
 #else
 int main( int argc, char * argv[] ) {
 #endif
+
+	int result = 0;
 	FILE * file = 0;
 	openmpt_module * mod = 0;
 	size_t count = 0;
-	(void)argc;
+	site_t written = 0;
+
+	if ( argc != 2 ) {
+		fprintf( stderr, "Error: %s\n", "Wrong invocation. Use 'libopenmpt_example_c_stdout SOMEMODULE'." );
+		goto fail;
+	}
+
 #if (defined(_WIN32) || defined(WIN32)) && (defined(_UNICODE) || defined(UNICODE))
+	if ( wcslen( argv[1] ) == 0 ) {
+		fprintf( stderr, "Error: %s\n", "Wrong invocation. Use 'libopenmpt_example_c_stdout SOMEMODULE'." );
+		goto fail;
+	}
 	file = _wfopen( argv[1], L"rb" );
 #else
+	if ( strlen( argv[1] ) == 0 ) {
+		fprintf( stderr, "Error: %s\n", "Wrong invocation. Use 'libopenmpt_example_c_stdout SOMEMODULE'." );
+		goto fail;
+	}
 	file = fopen( argv[1], "rb" );
 #endif
-	mod = openmpt_module_create( openmpt_stream_get_file_callbacks(), file, NULL, NULL, NULL );
-	fclose( file );
+	if ( !file ) {
+		fprintf( stderr, "Error: %s\n", "fopen() failed." );
+		goto fail;
+	}
+
+	mod = openmpt_module_create( openmpt_stream_get_file_callbacks(), file, &libopenmpt_example_logfunc, NULL, NULL );
+	if ( !mod ) {
+		fprintf( stderr, "Error: %s\n", "openmpt_module_create() failed." );
+		goto fail;
+	}
+
 	while ( 1 ) {
+
 		count = openmpt_module_read_interleaved_stereo( mod, SAMPLERATE, BUFFERSIZE, buffer );
 		if ( count == 0 ) {
 			break;
 		}
-		xwrite( STDOUT_FILENO, buffer, count * 2 * sizeof( int16_t ) );
+
+		written = xwrite( STDOUT_FILENO, buffer, count * 2 * sizeof( int16_t ) );
+		if ( written == 0 ) {
+			fprintf( stderr, "Error: %s\n", "write() failed." );
+			goto fail;
+		}
+
 	}
-	openmpt_module_destroy( mod );
-	return 0;
+
+	result = 0;
+
+	goto cleanup;
+
+fail:
+
+	result = 1;
+
+cleanup:
+
+	if ( mod ) {
+		openmpt_module_destroy( mod );
+		mod = 0;
+	}
+
+	if ( file ) {
+		fclose( file );
+		file = 0;
+	}
+
+	return result;
+
 }
