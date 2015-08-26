3f6baa64d178be65e0c8b6a1103ddcfc15a88cbd
Jörn Heusipp <manx@openmpt.org>
2015-08-26 09:06:34 +0000

    [Ref] build: Add clean script for automated builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5523 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+#
+# Dist clean script for libopenmpt.
+#
+# This is meant to be run by the libopenmpt maintainers.
+#
+# WARNING: The script expects the be run from the root of an OpenMPT svn
+#    checkout. It invests no effort in verifying this precondition.
+#
+
+# We want ccache
+export PATH="/usr/lib/ccache:$PATH"
+
+# Clean dist
+make clean-dist
+
+# Clean
+make clean
