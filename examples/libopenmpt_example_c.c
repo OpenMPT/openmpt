b85e80df34cc7d2a461cd797b7ae0c7411d7fd97
Jörn Heusipp <manx@openmpt.org>
2015-08-26 08:51:25 +0000

    [Mod] libopenmpt: Rename libopenmpt_example_c.c to libopenmpt_example_c_unsafe.c. [Mod] libopenmpt: Rename libopenmpt_example_c_safe.c to libopenmpt_example_c.c.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5519 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- examples/libopenmpt_example_c.c
+++ examples/libopenmpt_example_c.c
@@ -1,17 +1,14 @@
 /*
  * libopenmpt_example_c.c
  * ----------------------
- * Purpose: libopenmpt C API simple example
- * Notes  : This simple example does no error cheking at all.
- *          PortAudio is used for sound output.
+ * Purpose: libopenmpt C API example
+ * Notes  : PortAudio is used for sound output.
  * Authors: OpenMPT Devs
  * The OpenMPT source code is released under the BSD license. Read LICENSE for more details.
  */
 
 /*
- * Usage example:
- *
- *     bin/libopenmpt_example_c SOMEMODULE
+ * Usage: libopenmpt_example_c SOMEMODULE
  */
 
 #include <memory.h>
@@ -32,36 +29,130 @@ static int16_t left[BUFFERSIZE];
 static int16_t right[BUFFERSIZE];
 static int16_t * const buffers[2] = { left, right };
 
+static void libopenmpt_example_logfunc( const char * message, void * userdata ) {
+	(void)userdata;
+
+	if ( message ) {
+		fprintf( stderr, "%s\n", message );
+	}
+
+}
+
 #if (defined(_WIN32) || defined(WIN32)) && (defined(_UNICODE) || defined(UNICODE))
 int wmain( int argc, wchar_t * argv[] ) {
 #else
 int main( int argc, char * argv[] ) {
 #endif
+
+	int result = 0;
 	FILE * file = 0;
 	openmpt_module * mod = 0;
 	size_t count = 0;
+	PaError pa_error = paNoError;
+	int pa_initialized = 0;
 	PaStream * stream = 0;
-	(void)argc;
+
+	if ( argc != 2 ) {
+		fprintf( stderr, "Error: %s\n", "Wrong invocation. Use 'libopenmpt_example_c_safe SOMEMODULE'." );
+		goto fail;
+	}
+
 #if (defined(_WIN32) || defined(WIN32)) && (defined(_UNICODE) || defined(UNICODE))
+	if ( wcslen( argv[1] ) == 0 ) {
+		fprintf( stderr, "Error: %s\n", "Wrong invocation. Use 'libopenmpt_example_c_safe SOMEMODULE'." );
+		goto fail;
+	}
 	file = _wfopen( argv[1], L"rb" );
 #else
+	if ( strlen( argv[1] ) == 0 ) {
+		fprintf( stderr, "Error: %s\n", "Wrong invocation. Use 'libopenmpt_example_c_safe SOMEMODULE'." );
+		goto fail;
+	}
 	file = fopen( argv[1], "rb" );
 #endif
-	mod = openmpt_module_create( openmpt_stream_get_file_callbacks(), file, NULL, NULL, NULL );
-	fclose( file );
-	Pa_Initialize();
-	Pa_OpenDefaultStream( &stream, 0, 2, paInt16 | paNonInterleaved, SAMPLERATE, paFramesPerBufferUnspecified, NULL, NULL );
-	Pa_StartStream( stream );
+	if ( !file ) {
+		fprintf( stderr, "Error: %s\n", "fopen() failed." );
+		goto fail;
+	}
+
+	mod = openmpt_module_create( openmpt_stream_get_file_callbacks(), file, &libopenmpt_example_logfunc, NULL, NULL );
+	if ( !mod ) {
+		fprintf( stderr, "Error: %s\n", "openmpt_module_create() failed." );
+		goto fail;
+	}
+
+	pa_error = Pa_Initialize();
+	if ( pa_error != paNoError ) {
+		fprintf( stderr, "Error: %s\n", "Pa_Initialize() failed." );
+		goto fail;
+	}
+	pa_initialized = 1;
+
+	pa_error = Pa_OpenDefaultStream( &stream, 0, 2, paInt16 | paNonInterleaved, SAMPLERATE, paFramesPerBufferUnspecified, NULL, NULL );
+	if ( pa_error != paNoError ) {
+		fprintf( stderr, "Error: %s\n", "Pa_OpenStream() failed." );
+		goto fail;
+	}
+	if ( !stream ) {
+		fprintf( stderr, "Error: %s\n", "Pa_OpenStream() failed." );
+		goto fail;
+	}
+
+	pa_error = Pa_StartStream( stream );
+	if ( pa_error != paNoError ) {
+		fprintf( stderr, "Error: %s\n", "Pa_StartStream() failed." );
+		goto fail;
+	}
+
 	while ( 1 ) {
+
 		count = openmpt_module_read_stereo( mod, SAMPLERATE, BUFFERSIZE, left, right );
 		if ( count == 0 ) {
 			break;
 		}
-		Pa_WriteStream( stream, buffers, (unsigned long)count );
+
+		pa_error = Pa_WriteStream( stream, buffers, (unsigned long)count );
+		if ( pa_error != paNoError ) {
+			fprintf( stderr, "Error: %s\n", "Pa_WriteStream() failed." );
+			goto fail;
+		}
+
 	}
-	Pa_StopStream( stream );
-	Pa_CloseStream( stream );
-	Pa_Terminate();
-	openmpt_module_destroy( mod );
-	return 0;
+
+	result = 0;
+
+	goto cleanup;
+
+fail:
+
+	result = 1;
+
+cleanup:
+
+	if ( stream ) {
+		if ( Pa_IsStreamActive( stream ) == 1 ) {
+			Pa_StopStream( stream );
+		}
+		Pa_CloseStream( stream );
+		stream = 0;
+	}
+
+	if ( pa_initialized ) {
+		Pa_Terminate();
+		pa_initialized = 0;
+	}
+
+	if ( mod ) {
+		openmpt_module_destroy( mod );
+		mod = 0;
+	}
+
+	if ( file ) {
+		fclose( file );
+		file = 0;
+	}
+
+	return result;
+
 }
+
