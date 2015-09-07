7b2c63f8d3c705938b182bca07fa0e874833bb83
Jörn Heusipp <manx@openmpt.org>
2015-09-07 13:19:57 +0000

    [Ref] build: Remove dead code in libopenmpt packaging script.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5567 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_libopenmpt_winold.cmd
+++ build/auto/package_libopenmpt_winold.cmd
@@ -42,49 +42,6 @@ cd .. || goto error
 del /f /q libopenmpt-winold-r%MPT_REVISION%.7z
 cd .. || goto error
 
-cd bin || goto error
-rmdir /s /q libopenmpt-dev-vs2010
-del /f /q libopenmpt-dev-vs2010.tar
-del /f /q libopenmpt-dev-vs2010-r%MPT_REVISION%.7z
-mkdir libopenmpt-dev-vs2010
-cd libopenmpt-dev-vs2010 || goto error
-mkdir inc
-mkdir inc\libopenmpt
-mkdir lib
-mkdir lib\x86
-mkdir lib\x86_64
-mkdir bin
-mkdir bin\x86
-mkdir bin\x86_64
-copy /y ..\..\LICENSE .\LICENSE.txt || goto error
-copy /y ..\..\libopenmpt\dox\changelog.md .\changelog.md || goto error
-copy /y ..\..\libopenmpt\libopenmpt.h inc\libopenmpt\ || goto error
-copy /y ..\..\libopenmpt\libopenmpt.hpp inc\libopenmpt\ || goto error
-copy /y ..\..\libopenmpt\libopenmpt_config.h inc\libopenmpt\ || goto error
-copy /y ..\..\libopenmpt\libopenmpt_version.h inc\libopenmpt\ || goto error
-copy /y ..\..\libopenmpt\libopenmpt_ext.hpp inc\libopenmpt\ || goto error
-copy /y ..\..\bin\Win32\libopenmpt.lib lib\x86\ || goto error
-copy /y ..\..\bin\Win32\libopenmpt.dll bin\x86\ || goto error
-copy /y ..\..\bin\x64\libopenmpt.lib lib\x86_64\ || goto error
-copy /y ..\..\bin\x64\libopenmpt.dll bin\x86_64\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-dev-vs2010-r%MPT_REVISION%.7z ^
- LICENSE.txt ^
- changelog.md ^
- inc\libopenmpt\libopenmpt.h ^
- inc\libopenmpt\libopenmpt.hpp ^
- inc\libopenmpt\libopenmpt_config.h ^
- inc\libopenmpt\libopenmpt_version.h ^
- inc\libopenmpt\libopenmpt_ext.hpp ^
- lib\x86\libopenmpt.lib ^
- lib\x86_64\libopenmpt.lib ^
- bin\x86\libopenmpt.dll ^
- bin\x86_64\libopenmpt.dll ^
- || goto error
-cd .. || goto error
-"C:\Program Files\7-Zip\7z.exe" a -ttar libopenmpt-dev-vs2010.tar libopenmpt-dev-vs2010-r%MPT_REVISION%.7z || goto error
-del /f /q libopenmpt-dev-vs2010-r%MPT_REVISION%.7z
-cd .. || goto error
-
 
 
 goto noerror
