14a9f30efdd16bf3d025537f2e6cecc6fa81e9e5
Jörn Heusipp <manx@openmpt.org>
2015-09-09 07:10:17 +0000

    [Var] build: Add licenses of all third party code to the automatically built binary packages.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5571 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_libopenmpt_winold.cmd
+++ build/auto/package_libopenmpt_winold.cmd
@@ -21,6 +21,28 @@ cd libopenmpt-winold || goto error
 mkdir openmpt123
 mkdir XMPlay
 mkdir Winamp
+rmdir /s /q Licenses
+mkdir Licenses
+rem copy /y ..\..\include\bladeenc\BladeDLL.html                 .\Licenses\License.BladeDLL.html || goto error
+copy /y ..\..\include\flac\COPYING.Xiph                      .\Licenses\License.FLAC.txt || goto error
+rem copy /y ..\..\include\foobar2000sdk\sdk-license.txt          .\Licenses\License.Foobar2000SDK.txt || goto error
+rem copy /y ..\..\include\foobar2000sdk\pfc\pfc-license.txt      .\Licenses\License.Foobar2000SDK-pfc.txt || goto error
+rem copy /y ..\..\include\lhasa\COPYING                          .\Licenses\License.lhasa.txt || goto error
+copy /y ..\..\include\miniz\miniz.c                          .\Licenses\License.miniz.txt || goto error
+copy /y ..\..\include\msinttypes\stdint\stdint.h             .\Licenses\License.msinttypes.txt || goto error
+rem copy /y ..\..\include\ogg\COPYING                            .\Licenses\License.ogg.txt || goto error
+rem copy /y ..\..\include\opus\COPYING                           .\Licenses\License.Opus.txt || goto error
+copy /y ..\..\include\portaudio\LICENSE.txt                  .\Licenses\License.PortAudio.txt || goto error
+copy /y ..\..\include\portaudio\bindings\cpp\COPYING         .\Licenses\License.portaudiocpp.txt || goto error
+rem copy /y ..\..\include\portmidi\license.txt                   .\Licenses\License.PortMIDI.txt || goto error
+copy /y ..\..\include\pugixml\readme.txt                     .\Licenses\License.PugiXML.txt || goto error
+rem copy /y ..\..\include\r8brain\other\License.txt              .\Licenses\License.R8Brain.txt || goto error
+rem copy /y ..\..\include\smbPitchShift\smbPitchShift.cpp        .\Licenses\License.smbPitchShift.txt || goto error
+rem copy /y ..\..\include\soundtouch\COPYING.TXT                 .\Licenses\License.SoundTouch.txt || goto error
+rem copy /y ..\..\include\unrar\license.txt                      .\Licenses\License.UnRAR.txt || goto error
+rem copy /y ..\..\include\vorbis\COPYING                         .\Licenses\License.Vorbis.txt || goto error
+rem copy /y ..\..\include\zlib\README                            .\Licenses\License.zlib.txt || goto error
+rem copy /y ..\..\include\zlib\contrb\minizip\MiniZip64_info.txt .\Licenses\License.minizip.txt || goto error
 copy /y ..\..\LICENSE .\LICENSE.txt || goto error
 copy /y ..\..\libopenmpt\dox\changelog.md .\ || goto error
 copy /y ..\..\libopenmpt\doc\xmp-openmpt.txt .\XMPlay\ || goto error
@@ -30,6 +52,7 @@ copy /y ..\..\bin\Win32\xmp-openmpt.dll .\XMPlay\ || goto error
 copy /y ..\..\bin\Win32\in_openmpt.dll .\Winamp\ || goto error
 "C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\libopenmpt-winold-r%MPT_REVISION%.7z ^
  LICENSE.txt ^
+ Licenses ^
  changelog.md ^
  openmpt123\openmpt123.exe ^
  XMPlay\xmp-openmpt.txt ^
