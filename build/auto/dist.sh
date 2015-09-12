0db53733915398a341f67b94ba552f46477eff64
Jörn Heusipp <manx@openmpt.org>
2015-09-12 12:44:13 +0000

    [Imp] build: In the dist package building script, verify that the public API headers are compilable with all supported C and C++ language standard versions.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5592 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/dist.sh
+++ build/auto/dist.sh
@@ -13,6 +13,24 @@ set -e
 # We want ccache
 export PATH="/usr/lib/ccache:$PATH"
 
+# Check that the API headers are standard compliant
+echo '' > bin/headercheck.c
+echo '#include "libopenmpt/libopenmpt.h"' >> bin/headercheck.c
+echo 'int main() { return 0; }' >> bin/headercheck.c
+cc -std=c89 -pedantic -Wall -Wextra -Wpedantic -Werror -I. bin/headercheck.c -o bin/headercheck.c89
+cc -std=c99 -pedantic -Wall -Wextra -Wpedantic -Werror -I. bin/headercheck.c -o bin/headercheck.c99
+echo '' > bin/headercheck.cpp
+echo '#include "libopenmpt/libopenmpt.hpp"' >> bin/headercheck.cpp
+echo 'int main() { return 0; }' >> bin/headercheck.cpp
+#c++ -std=c++98 -pedantic -Wall -Wextra -Wpedantic -Werror -I. bin/headercheck.cpp -o bin/headercheck.cpp98
+c++ -std=c++11 -pedantic -Wall -Wextra -Wpedantic -Werror -I. bin/headercheck.cpp -o bin/headercheck.cpp11
+rm bin/headercheck.c89
+rm bin/headercheck.c99
+#rm bin/headercheck.cpp98
+rm bin/headercheck.cpp11
+rm bin/headercheck.c
+rm bin/headercheck.cpp
+
 # Clean dist
 make clean-dist
 
