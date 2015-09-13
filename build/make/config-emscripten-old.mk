63c928a4e6470477cbc2c8d188f4edb45b8edd93
Jörn Heusipp <manx@openmpt.org>
2015-09-13 15:23:45 +0000

    [Fix] build: The emscripten build configuration broke for older emscripten versions in r5586. Add back the old configuration as "emscripten-old".
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5608 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/make/config-emscripten-old.mk
@@ -0,0 +1,51 @@
+
+CC  = emcc
+CXX = em++
+LD  = em++
+AR  = emar
+
+CPPFLAGS += 
+CXXFLAGS += -std=c++11 -fPIC -O3 -ffast-math 
+CFLAGS   += -std=c99   -fPIC -O3 -ffast-math -fno-strict-aliasing 
+LDFLAGS  += -O3 -s DISABLE_EXCEPTION_CATCHING=0
+LDLIBS   += 
+ARFLAGS  := rcs
+
+# help older and slower compilers with huge functions
+LDFLAGS += -s OUTLINING_LIMIT=16000
+
+# allow growing heap (might be slower, especially with V8 (as used by Chrome))
+#LDFLAGS += -s ALLOW_MEMORY_GROWTH=1
+# limit memory to 64MB, faster but loading modules bigger than about 16MB will not work
+#LDFLAGS += -s TOTAL_MEMORY=67108864
+
+LDFLAGS += -s ALLOW_MEMORY_GROWTH=1
+
+CXXFLAGS_WARNINGS += -Wmissing-prototypes
+CFLAGS_WARNINGS   += -Wmissing-prototypes
+
+EXESUFFIX=.js
+SOSUFFIX=.js
+RUNPREFIX=nodejs 
+TEST_LDFLAGS= --pre-js build/make/test-pre.js 
+
+DYNLINK=0
+SHARED_LIB=1
+STATIC_LIB=0
+EXAMPLES=1
+OPENMPT123=0
+SHARED_SONAME=0
+
+# Disable the generic compiler optimization flags as emscripten is sufficiently different.
+# Optimization flags are hard-coded for emscripten in this file.
+DEBUG=0
+OPTIMIZE=0
+OPTIMIZE_SIZE=0
+
+NO_ZLIB=1
+NO_PORTAUDIO=1
+NO_PORTAUDIOCPP=1
+NO_SDL=1
+NO_FLAC=1
+NO_WAVPACK=1
+NO_SNDFILE=1
