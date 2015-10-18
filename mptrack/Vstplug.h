OpenMPT/openmpt

commit 75f71371fc15caf7d3c6e0d7ee1a7706d5f16608

[New] Allow the user to enter plugin-specific tags in the plugin selection dialog. The name filter now searches for both plugin names and tags. (patch partially provided by arseniiv, http://forum.openmpt.org/index.php?topic=5410.0) [Mod] OpenMPT: Version is now 1.25.02.08
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5668 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Vstplug.h
@@ -61,21 +61,23 @@ public:
 	CVstPlugin *pPluginsList;		// Pointer to first plugin instance (this instance carries pointers to other instances)
 	mpt::PathString libraryName;	// Display name
 	mpt::PathString dllPath;		// Full path name
+	mpt::ustring tags;				// User tags
 	VstInt32 pluginId1;				// Plugin type (kEffectMagic, kDmoMagic)
 	VstInt32 pluginId2;				// Plugin unique ID
 	PluginCategory category;
-	bool isInstrument;
-	bool useBridge, shareBridgeInstance;
+	bool isInstrument : 1;
+	bool useBridge : 1, shareBridgeInstance : 1;
 protected:
-	mutable uint8 dllBits;
+	mutable uint8 dllBits : 1;
 
 public:
-	VSTPluginLib(const mpt::PathString &dllPath, const mpt::PathString &libraryName)
+	VSTPluginLib(const mpt::PathString &dllPath, const mpt::PathString &libraryName, const mpt::ustring &tags)
 		: pPluginsList(nullptr),
 		libraryName(libraryName), dllPath(dllPath),
 		pluginId1(0), pluginId2(0),
 		category(catUnknown),
 		isInstrument(false), useBridge(false), shareBridgeInstance(true),
+		tags(tags),
 		dllBits(0)
 	{
 	}
@@ -373,7 +375,7 @@ public:
 	void reserve(size_t num) { pluginList.reserve(num); }
 
 	bool IsValidPlugin(const VSTPluginLib *pLib) const;
-	VSTPluginLib *AddPlugin(const mpt::PathString &dllPath, bool fromCache = true, const bool checkFileExistence = false, std::wstring* const errStr = nullptr);
+	VSTPluginLib *AddPlugin(const mpt::PathString &dllPath, const mpt::ustring &tags, bool fromCache = true, const bool checkFileExistence = false, std::wstring* const errStr = nullptr);
 	bool RemovePlugin(VSTPluginLib *);
 	bool CreateMixPlugin(SNDMIXPLUGIN &, CSoundFile &);
 	void OnIdle();
@@ -400,7 +402,7 @@ public:
 
 #else // NO_VST
 public:
-	VSTPluginLib *AddPlugin(const mpt::PathString &, bool = true, const bool = false, std::wstring* const = nullptr) { return 0; }
+	VSTPluginLib *AddPlugin(const mpt::PathString &, const mpt::ustring &, bool = true, const bool = false, std::wstring* const = nullptr) { return 0; }
 
 	const VSTPluginLib **begin() const { return nullptr; }
 	const VSTPluginLib **end() const { return nullptr; }

common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               25
 #define VER_MINOR               02
-#define VER_MINORMINOR          07
+#define VER_MINORMINOR          08
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

mptrack/Mptrack.cpp
@@ -92,7 +92,7 @@ CDocument *CModDocTemplate::OpenDocumentFile(const mpt::PathString &filename, BO
 	if(!mpt::PathString::CompareNoCase(filename.GetFileExt(), MPT_PATHSTRING(".dll")))
 	{
 		CVstPluginManager *pPluginManager = theApp.GetPluginManager();
-		if(pPluginManager && pPluginManager->AddPlugin(filename) != nullptr)
+		if(pPluginManager && pPluginManager->AddPlugin(filename, mpt::ustring()) != nullptr)
 		{
 			return nullptr;
 		}
@@ -1928,7 +1928,7 @@ BOOL CTrackApp::InitializeDXPlugins()
 	for(size_t plug = 0; plug < numPlugins; plug++)
 	{
 		char tmp[32];
-		sprintf(tmp, "Plugin%d", plug);
+		sprintf(tmp, "Plugin%u", plug);
 		mpt::PathString plugPath = theApp.GetSettings().Read<mpt::PathString>("VST Plugins", tmp, MPT_PATHSTRING(""));
 		if(!plugPath.empty())
 		{
@@ -1942,7 +1942,11 @@ BOOL CTrackApp::InitializeDXPlugins()
 					continue;
 				}
 			}
-			m_pPluginManager->AddPlugin(plugPath, true, true, &nonFoundPlugs);
+
+			sprintf(tmp, "Plugin%u.Tags", plug);
+			mpt::ustring plugTags = theApp.GetSettings().Read<mpt::ustring>("VST Plugins", tmp, std::wstring());
+
+			m_pPluginManager->AddPlugin(plugPath, plugTags, true, true, &nonFoundPlugs);
 		}
 
 		if(!dialogShown && GetTickCount() >= scanStart + 2000)
@@ -1986,13 +1990,17 @@ BOOL CTrackApp::UninitializeDXPlugins()
 		if((**pPlug).pluginId1 != kDmoMagic)
 		{
 			char tmp[32];
-			sprintf(tmp, "Plugin%d", plug);
+			sprintf(tmp, "Plugin%u", plug);
 			mpt::PathString plugPath = (**pPlug).dllPath;
 			if(theApp.IsPortableMode())
 			{
 				plugPath = AbsolutePathToRelative(plugPath);
 			}
 			theApp.GetSettings().Write<mpt::PathString>("VST Plugins", tmp, plugPath);
+
+			sprintf(tmp, "Plugin%u.Tags", plug);
+			theApp.GetSettings().Write("VST Plugins", tmp, (**pPlug).tags);
+
 			plug++;
 		}
 	}

mptrack/SelectPluginDialog.cpp
@@ -41,6 +41,7 @@ BEGIN_MESSAGE_MAP(CSelectPluginDlg, CDialog)
 	ON_COMMAND(IDC_CHECK1,			OnSetBridge)
 	ON_COMMAND(IDC_CHECK2,			OnSetBridge)
 	ON_EN_CHANGE(IDC_NAMEFILTER,	OnNameFilterChanged)
+	ON_EN_CHANGE(IDC_PLUGINTAGS,	OnPluginTagsChanged)
 	ON_WM_SIZE()
 	ON_WM_GETMINMAXINFO()
 END_MESSAGE_MAP()
@@ -60,16 +61,12 @@ CSelectPluginDlg::CSelectPluginDlg(CModDoc *pModDoc, PLUGINDEX nPlugSlot, CWnd *
 	: CDialog(IDD_SELECTMIXPLUGIN, parent)
 	, m_pModDoc(pModDoc)
 	, m_nPlugSlot(nPlugSlot)
+	, m_pPlugin(nullptr)
 //-------------------------------------------------------------------------------------
 {
-	m_pPlugin = NULL;
-
-	if(m_pModDoc)
+	if(m_pModDoc && 0 <= m_nPlugSlot && m_nPlugSlot < MAX_MIXPLUGINS)
 	{
-		if(0 <= m_nPlugSlot && m_nPlugSlot < MAX_MIXPLUGINS)
-		{
-			m_pPlugin = &(pModDoc->GetrSoundFile().m_MixPlugins[m_nPlugSlot]);
-		}
+		m_pPlugin = &(pModDoc->GetrSoundFile().m_MixPlugins[m_nPlugSlot]);
 	}
 
 	CMainFrame::GetMainFrame()->GetInputHandler()->Bypass(true);
@@ -344,6 +341,8 @@ void CSelectPluginDlg::UpdatePluginsList(VstInt32 forceSelect /* = 0*/)
 	bool foundCurrentPlug = false;
 
 	const bool nameFilterActive = !m_nameFilter.empty();
+	std::vector<mpt::ustring> currentTags = mpt::String::Split<std::wstring>(m_nameFilter, L" ");
+
 	if(pManager)
 	{
 		bool first = true;
@@ -355,12 +354,31 @@ void CSelectPluginDlg::UpdatePluginsList(VstInt32 forceSelect /* = 0*/)
 			if(nameFilterActive)
 			{
 				// Apply name filter
-				std::wstring displayName = plug.libraryName.ToWide();
-				for(size_t i = 0; i < displayName.length(); i++) displayName[i] = ::towlower(displayName[i]);
-				if(displayName.find(m_nameFilter, 0) == displayName.npos)
+				bool matches = false;
+				// Search in plugin names
 				{
-					continue;
+					std::wstring displayName = plug.libraryName.ToWide();
+					for(size_t i = 0; i < displayName.length(); i++) displayName[i] = ::towlower(displayName[i]);
+					if(displayName.find(m_nameFilter, 0) != displayName.npos)
+					{
+						matches = true;
+					}
 				}
+				// Search in plugin tags
+				if(!matches)
+				{
+					mpt::ustring tags = plug.tags;
+					for(size_t i = 0; i < tags.length(); i++) tags[i] = ::towlower(tags[i]);
+					for(std::vector<mpt::ustring>::const_iterator it = currentTags.begin(); it != currentTags.end(); it++)
+					{
+						if(!it->empty() && tags.find(*it, 0) != tags.npos)
+						{
+							matches = true;
+							break;
+						}
+					}
+				}
+				if(!matches) continue;
 			}
 
 			std::wstring title = plug.libraryName.AsNative();
@@ -476,9 +494,11 @@ void CSelectPluginDlg::OnSelChanged(NMHDR *, LRESULT *result)
 	CVstPluginManager *pManager = theApp.GetPluginManager();
 	VSTPluginLib *pPlug = GetSelectedPlugin();
 	int showBoxes = SW_HIDE;
+	BOOL enableTagsTextBox = FALSE;
 	if (pManager != nullptr && pManager->IsValidPlugin(pPlug))
 	{
 		::SetDlgItemTextW(m_hWnd, IDC_TEXT_CURRENT_VSTPLUG, pPlug->dllPath.ToWide().c_str());
+		::SetDlgItemTextW(m_hWnd, IDC_PLUGINTAGS, pPlug->tags.c_str());
 		if(pPlug->pluginId1 == kEffectMagic)
 		{
 			bool isBridgeAvailable =
@@ -502,13 +522,16 @@ void CSelectPluginDlg::OnSelChanged(NMHDR *, LRESULT *result)
 			m_chkShare.EnableWindow(m_chkBridge.GetCheck() != BST_UNCHECKED);
 
 			showBoxes = SW_SHOW;
+			enableTagsTextBox = TRUE;
 		}
 	} else
 	{
 		SetDlgItemText(IDC_TEXT_CURRENT_VSTPLUG, _T(""));
+		SetDlgItemText(IDC_PLUGINTAGS, "");
 	}
 	m_chkBridge.ShowWindow(showBoxes);
 	m_chkShare.ShowWindow(showBoxes);
+	GetDlgItem(IDC_PLUGINTAGS)->EnableWindow(enableTagsTextBox);
 	if (result) *result = 0;
 }
 
@@ -781,10 +804,14 @@ void CSelectPluginDlg::OnSize(UINT nType, int cx, int cy)
 
 	if (m_treePlugins)
 	{
-		m_treePlugins.MoveWindow(MulDiv(8, dpiX, 96), MulDiv(36, dpiY, 96), cx - MulDiv(109, dpiX, 96), cy - MulDiv(88, dpiY, 96), FALSE);
+		m_treePlugins.MoveWindow(MulDiv(8, dpiX, 96), MulDiv(36, dpiY, 96), cx - MulDiv(109, dpiX, 96), cy - MulDiv(118, dpiY, 96), FALSE);
 
 		GetDlgItem(IDC_STATIC_VSTNAMEFILTER)->MoveWindow(MulDiv(8, dpiX, 96), MulDiv(11, dpiY, 96), MulDiv(40, dpiX, 96), MulDiv(21, dpiY, 96), FALSE);
 		GetDlgItem(IDC_NAMEFILTER)->MoveWindow(MulDiv(40, dpiX, 96), MulDiv(8, dpiY, 96), cx - MulDiv(141, dpiX, 96), MulDiv(21, dpiY, 96), FALSE);
+
+		GetDlgItem(IDC_STATIC_PLUGINTAGS)->MoveWindow(MulDiv(8, dpiX, 96), cy - MulDiv(71, dpiY, 96), MulDiv(40, dpiX, 96), MulDiv(21, dpiY, 96), FALSE);
+		GetDlgItem(IDC_PLUGINTAGS)->MoveWindow(MulDiv(40, dpiX, 96), cy - MulDiv(74, dpiY, 96), cx - MulDiv(141, dpiX, 96), MulDiv(21, dpiY, 96), FALSE);
+
 		GetDlgItem(IDC_TEXT_CURRENT_VSTPLUG)->MoveWindow(MulDiv(8, dpiX, 96), cy - MulDiv(45, dpiY, 96), cx - MulDiv(22, dpiX, 96), MulDiv(20, dpiY, 96), FALSE);
 		m_chkBridge.MoveWindow(MulDiv(8, dpiX, 96), cy - MulDiv(25, dpiY, 96), MulDiv(110, dpiX, 96), MulDiv(20, dpiY, 96), FALSE);
 		m_chkShare.MoveWindow(MulDiv(120, dpiX, 96), cy - MulDiv(25, dpiY, 96), cx - MulDiv(128, dpiX, 96), MulDiv(20, dpiY, 96), FALSE);
@@ -800,6 +827,7 @@ void CSelectPluginDlg::OnSize(UINT nType, int cx, int cy)
 	}
 }
 
+
 void CSelectPluginDlg::OnGetMinMaxInfo(MINMAXINFO* lpMMI)
 //-------------------------------------------------------
 {
@@ -807,6 +835,25 @@ void CSelectPluginDlg::OnGetMinMaxInfo(MINMAXINFO* lpMMI)
 	lpMMI->ptMinTrackSize.y = MulDiv(270, Util::GetDPIy(m_hWnd), 96);
 	CDialog::OnGetMinMaxInfo(lpMMI);
 }
+
+
+void CSelectPluginDlg::OnPluginTagsChanged()
+//------------------------------------------
+{
+	VSTPluginLib *plug = GetSelectedPlugin();
+	if (plug)
+	{
+		HWND hwnd = ::GetDlgItem(m_hWnd, IDC_PLUGINTAGS);
+		int len = ::GetWindowTextLengthW(hwnd);
+		mpt::ustring tags(len, MPT_UCHAR(' '));
+		if(len)
+		{
+			::GetWindowTextW(hwnd, &tags[0], len + 1);
+		}
+		plug->tags = tags;
+	}
+}
+
 
 OPENMPT_NAMESPACE_END
 

mptrack/SelectPluginDialog.h
@@ -69,6 +69,7 @@ protected:
 	afx_msg void OnSelDblClk(NMHDR *pNotifyStruct, LRESULT *result);
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg void OnGetMinMaxInfo(MINMAXINFO* lpMMI);
+	afx_msg void OnPluginTagsChanged();
 };
 
 OPENMPT_NAMESPACE_END

mptrack/mod2midi.cpp
@@ -340,7 +340,7 @@ namespace MidiExport
 	public:
 		Conversion(CSoundFile &sndFile, const InstrMap &instrMap, mpt::ofstream &file)
 			: m_oldInstruments(sndFile.GetNumInstruments())
-			, m_plugFactory(mpt::PathString(), mpt::PathString())
+			, m_plugFactory(mpt::PathString(), mpt::PathString(), mpt::ustring())
 			, m_sndFile(sndFile)
 			, m_file(file)
 			, m_wasInstrumentMode(sndFile.GetNumInstruments() > 0)

mptrack/mptrack.rc
@@ -1577,7 +1577,9 @@ BEGIN
     EDITTEXT        IDC_NAMEFILTER,30,6,150,14,ES_AUTOHSCROLL
     DEFPUSHBUTTON   "&Add to Song",IDOK,187,6,53,14
     PUSHBUTTON      "&Cancel",IDCANCEL,186,24,54,14
-    CONTROL         "Tree1",IDC_TREE1,"SysTreeView32",TVS_DISABLEDRAGDROP | TVS_SHOWSELALWAYS | WS_HSCROLL | WS_TABSTOP,6,24,174,180,WS_EX_CLIENTEDGE
+    CONTROL         "Tree1",IDC_TREE1,"SysTreeView32",TVS_DISABLEDRAGDROP | TVS_SHOWSELALWAYS | WS_HSCROLL | WS_TABSTOP,6,24,174,157,WS_EX_CLIENTEDGE
+    LTEXT           "&Tags:",IDC_STATIC_PLUGINTAGS,7,188,19,8
+    EDITTEXT        IDC_PLUGINTAGS,30,186,150,14,ES_AUTOHSCROLL | WS_DISABLED
     EDITTEXT        IDC_TEXT_CURRENT_VSTPLUG,7,206,235,12,ES_AUTOHSCROLL | ES_READONLY | NOT WS_BORDER
     CONTROL         "Use Plugin &Bridge",IDC_CHECK1,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,6,216,72,12
     CONTROL         "Share Bridge between all &Instances",IDC_CHECK2,"Button",BS_AUTOCHECKBOX | WS_TABSTOP,78,216,129,12

mptrack/resource.h
@@ -909,6 +909,8 @@
 #define IDC_LIST_SAMPLEGEN_PRESETS      2425
 #define IDC_CHECK_UNDO                  2426
 #define IDC_CHK_REMEMBERSETTINGS        2427
+#define IDC_PLUGINTAGS                  2428
+#define IDC_STATIC_PLUGINTAGS           2429
 #define IDC_BTN_CLEAR                   2430
 #define IDC_TOTAL_EDIT_TIME             2431
 #define IDC_EDIT_HISTORY                2432

commit 50fe666f7f3806898f8c2548cbad4e51c70b9995

[Fix] Typo introduced in r5668
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5670 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Vstplug.h
@@ -68,16 +68,16 @@ public:
 	bool isInstrument : 1;
 	bool useBridge : 1, shareBridgeInstance : 1;
 protected:
-	mutable uint8 dllBits : 1;
+	mutable uint8 dllBits;
 
 public:
 	VSTPluginLib(const mpt::PathString &dllPath, const mpt::PathString &libraryName, const mpt::ustring &tags)
 		: pPluginsList(nullptr),
 		libraryName(libraryName), dllPath(dllPath),
+		tags(tags),
 		pluginId1(0), pluginId2(0),
 		category(catUnknown),
 		isInstrument(false), useBridge(false), shareBridgeInstance(true),
-		tags(tags),
 		dllBits(0)
 	{
 	}