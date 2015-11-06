5d5243893f24446da5b26e19685c0f5d467aa5ff
Jörn Heusipp <manx@openmpt.org>
2015-11-06 12:03:28 +0000

    [Fix] Fix typo redefining bswap16. This was meant to be MPT_bswap16, obviously. (http://bugs.openmpt.org/view.php?id=720)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5736 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/Endianness.h
+++ common/Endianness.h
@@ -28,7 +28,7 @@ OPENMPT_NAMESPACE_BEGIN
 
 #if MPT_COMPILER_GCC
 #if MPT_GCC_AT_LEAST(4,8,0)
-#define bswap16 __builtin_bswap16
+#define MPT_bswap16 __builtin_bswap16
 #endif
 #if MPT_GCC_AT_LEAST(4,3,0)
 #define MPT_bswap32 __builtin_bswap32
