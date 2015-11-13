OpenMPT/openmpt

commit 362bb1eda144aa21fc79cfa0729cfb413a8f7958

[Imp] Stream Export: Make ID3v2 padding size configurable via hidden settings [Export]MP3ID3v2MinPadding and [Export]MP3ID3v2PaddingAlignHint. [Mod] Stream Export: Default to 1KiB minimum padding and align the data after padding to a common block sze (4KiB). ~4KiB additional padding does not matter much at all compared to about 1MiB/minute MP3 data. [Mod] Stream Export: Also set minimum padding size for libmp3lame ID3v2 backend.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5641 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/StreamEncoderMP3.cpp
@@ -99,9 +99,6 @@ STATIC_ASSERT(sizeof(ID3v2Frame) == 10);
 #pragma pack(pop)
 #endif
 
-// we will add some padding bytes to our id3v2 tag (extending tags will be easier this way)
-#define ID3v2_PADDING 512
-
 // charset... choose text ending accordingly.
 // $00 = ISO-8859-1. Terminated with $00.
 // $01 = UTF-16 with BOM. Terminated with $00 00.
@@ -193,12 +190,19 @@ void ID3V2Tagger::WriteID3v2Tags(std::ostream &s, const FileTags &tags)
 	WriteID3v2Frame("COMM", mpt::ToCharset(mpt::CharsetUTF8, tags.comments), s);
 
 	// Write Padding
-	for(size_t i = 0; i < ID3v2_PADDING; i++)
+	uint32 totalID3v2SizeWithoutPadding = totalID3v2Size;
+	uint32 paddingSize = StreamEncoderSettings::Instance().MP3ID3v2MinPadding;
+	totalID3v2Size += paddingSize;
+	if(StreamEncoderSettings::Instance().MP3ID3v2PaddingAlignHint > 0)
+	{
+		totalID3v2Size = Util::AlignUp<uint32>(totalID3v2Size, StreamEncoderSettings::Instance().MP3ID3v2PaddingAlignHint);
+		paddingSize = totalID3v2Size - totalID3v2SizeWithoutPadding;
+	}
+	for(size_t i = 0; i < paddingSize; i++)
 	{
 		char c = 0;
 		s.write(&c, 1);
 	}
-	totalID3v2Size += ID3v2_PADDING;
 
 	// Write correct header (update tag size)
 	tHeader.size = intToSynchsafe(totalID3v2Size);
@@ -286,6 +290,7 @@ public:
 	void (CDECL * id3tag_v1_only)  (lame_t gfp);
 	void (CDECL * id3tag_add_v2)   (lame_t gfp);
 	void (CDECL * id3tag_v2_only)  (lame_t gfp);
+	void (CDECL * id3tag_set_pad)  (lame_t gfp, size_t n);
 	void (CDECL * lame_set_write_id3tag_automatic)(lame_global_flags * gfp, int);
 
 	void (CDECL * id3tag_set_title)(lame_t gfp, const char* title);
@@ -394,6 +399,7 @@ private:
 		MPT_COMPONENT_BIND("libmp3lame", id3tag_v1_only);
 		MPT_COMPONENT_BIND("libmp3lame", id3tag_add_v2);
 		MPT_COMPONENT_BIND("libmp3lame", id3tag_v2_only);
+		MPT_COMPONENT_BIND("libmp3lame", id3tag_set_pad);
 		MPT_COMPONENT_BIND("libmp3lame", lame_set_write_id3tag_automatic);
 		MPT_COMPONENT_BIND("libmp3lame", id3tag_set_title);
 		MPT_COMPONENT_BIND("libmp3lame", id3tag_set_artist);
@@ -566,6 +572,7 @@ public:
 			{
 				lame.id3tag_add_v2(gfp);
 				lame.id3tag_v2_only(gfp);
+				lame.id3tag_set_pad(gfp, StreamEncoderSettings::Instance().MP3ID3v2MinPadding);
 			}
 
 		} else

mptrack/StreamEncoder.cpp
@@ -31,6 +31,8 @@ StreamEncoderSettings &StreamEncoderSettings::Instance()
 StreamEncoderSettings::StreamEncoderSettings(SettingsContainer &conf, const mpt::ustring &section)
 //------------------------------------------------------------------------------------------------
 	: FLACCompressionLevel(conf, section, "FLACCompressionLevel", 5)
+	, MP3ID3v2MinPadding(conf, section, "MP3ID3v2MinPadding", 1024)
+	, MP3ID3v2PaddingAlignHint(conf, section, "MP3ID3v2PaddingAlignHint", 4096)
 	, MP3LameQuality(conf, section, "MP3LameQuality", 3)
 	, MP3LameAllowID3v2inCBR(conf, section, "MP3LameAllowID3v2inCBR", false)
 	, MP3ACMFast(conf, section, "MP3ACMFast", false)

mptrack/StreamEncoder.h
@@ -196,6 +196,8 @@ struct StreamEncoderSettings
 //==========================
 {
 	Setting<int32> FLACCompressionLevel;
+	Setting<uint32> MP3ID3v2MinPadding;
+	Setting<uint32> MP3ID3v2PaddingAlignHint;
 	Setting<int32> MP3LameQuality;
 	Setting<bool> MP3LameAllowID3v2inCBR;
 	Setting<bool> MP3ACMFast;

commit 2ffac0ca4da0460a6f4771dcdcb0a6b752834f5c

[Fix] Stream Export: ID3v2: Account for the header size when calculating the padding alignment and write the correct value to the ID3v2 size field in the header. Previious value was too big by 10 bytes causing decoders to skip 10 bytes too many, causing them to not find stream sync at the beginning of the stream, and ultimately skipping valid audio data. (broken since r5641)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5763 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/StreamEncoderMP3.cpp
@@ -206,6 +206,7 @@ void ID3V2Tagger::WriteID3v2Tags(std::ostream &s, const FileTags &tags, ReplayGa
 	tHeader.flags = 0x00; // No flags
 	tHeader.size  = 0; // will be filled later
 	s.write(reinterpret_cast<const char*>(&tHeader), sizeof(tHeader));
+	totalID3v2Size += sizeof(tHeader);
 
 	// Write TIT2 (Title), TCOM / TPE1 (Composer), TALB (Album), TCON (Genre), TYER / TDRC (Date), WXXX (URL), TENC (Encoder), COMM (Comment)
 	WriteID3v2Frame("TIT2", mpt::ToCharset(mpt::CharsetUTF8, tags.title), s);
@@ -246,7 +247,7 @@ void ID3V2Tagger::WriteID3v2Tags(std::ostream &s, const FileTags &tags, ReplayGa
 	}
 
 	// Write correct header (update tag size)
-	tHeader.size = intToSynchsafe(totalID3v2Size);
+	tHeader.size = intToSynchsafe(totalID3v2Size - sizeof(tHeader));
 	s.seekp(fOffset);
 	s.write(reinterpret_cast<const char*>(&tHeader), sizeof(tHeader));
 	s.seekp(totalID3v2Size - sizeof(tHeader), std::ios::cur);