e694f2dd925aea78111e391dbae1ff055c6db08f
Johannes Schultz <sagamusix@openmpt.org>
2015-11-19 23:31:03 +0000

    [New] Add make config for Haiku (provided by jua)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5780 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/make/config-haiku.mk
@@ -0,0 +1,14 @@
+
+CC  = gcc-x86
+CXX = g++-x86
+LD  = g++-x86
+AR  = ar-x86
+
+CPPFLAGS +=
+CXXFLAGS += -std=c++11 -fPIC
+CFLAGS   += -std=c99   -fPIC
+LDFLAGS  +=
+LDLIBS   +=
+ARFLAGS  := rcs
+
+EXESUFFIX=
