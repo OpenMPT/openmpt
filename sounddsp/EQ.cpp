ca968f512a1ac3ce2aade699db1664c6e3912e77
Jörn Heusipp <manx@openmpt.org>
2015-11-27 09:53:42 +0000

    [Ref] DSP/EQ: Assert that the layout of EQBANDSTRUCT matches the layout the inline asm expects.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5799 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- sounddsp/EQ.cpp
+++ sounddsp/EQ.cpp
@@ -14,6 +14,8 @@
 #include "../soundlib/MixerLoops.h"
 #include "../sounddsp/EQ.h"
 
+#include <cstddef>
+
 
 OPENMPT_NAMESPACE_BEGIN
 
@@ -55,14 +57,31 @@ static const EQBANDSTRUCT gEQDefaults[MAX_EQ_BANDS*2] =
 #pragma warning(disable:4100)
 #endif // MPT_COMPILER_MSVC
 
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, a0) == 0);
 #define PBS_A0	DWORD PTR [eax]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, a1) == 4);
 #define PBS_A1	DWORD PTR [eax+4]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, a2) == 8);
 #define PBS_A2	DWORD PTR [eax+8]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, b1) == 12);
 #define PBS_B1	DWORD PTR [eax+12]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, b2) == 16);
 #define PBS_B2	DWORD PTR [eax+16]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, x1) == 20);
 #define PBS_X1	DWORD PTR [eax+20]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, x2) == 24);
 #define PBS_X2	DWORD PTR [eax+24]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, y1) == 28);
 #define PBS_Y1	DWORD PTR [eax+28]
+
+STATIC_ASSERT(offsetof(EQBANDSTRUCT, y2) == 32);
 #define PBS_Y2	DWORD PTR [eax+32]
 
 static void EQFilter(EQBANDSTRUCT *pbs, float32 *pbuffer, UINT nCount)
