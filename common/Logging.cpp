OpenMPT/openmpt

commit c70e8acf90f46abb7f5789d88e20e592fd0a0b90

[Ref] Replace all uses of MPT_FORMAT with mpt::format. [Ref] Remove MPT_FORMAT and thereby all uses of variadic macros.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5868 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/Logging.cpp
@@ -133,7 +133,7 @@ void Logger::SendLogMessage(const Context &context, LogLevel level, const char *
 			}
 			if(s_logfile)
 			{
-				fprintf(s_logfile, mpt::ToCharset(mpt::CharsetUTF8, MPT_UFORMAT("%1+%2 %3(%4): %5 [%6]\n"
+				fprintf(s_logfile, mpt::ToCharset(mpt::CharsetUTF8, mpt::format(MPT_USTRING("%1+%2 %3(%4): %5 [%6]\n"))(
 					, mpt::Date::ANSI::ToString(cur)
 					, mpt::ufmt::dec<6>(diff)
 					, file
@@ -146,7 +146,7 @@ void Logger::SendLogMessage(const Context &context, LogLevel level, const char *
 		}
 		if(mpt::log::DebuggerEnabled)
 		{
-			OutputDebugStringW(mpt::ToWide(MPT_UFORMAT("%1(%2): +%3 %4 [%5]\n"
+			OutputDebugStringW(mpt::ToWide(mpt::format(MPT_USTRING("%1(%2): +%3 %4 [%5]\n"))(
 				, file
 				, line
 				, mpt::ufmt::dec<6>(diff)
@@ -364,7 +364,7 @@ bool Dump(const mpt::PathString &filename)
 			time = mpt::ToCharset(mpt::CharsetUTF8, mpt::Date::ANSI::ToString( ftNow - static_cast<int64>( static_cast<double>(qpcNow.QuadPart - entry.Timestamp) * (10000000.0 / static_cast<double>(qpcFreq.QuadPart) ) ) ) );
 		} else
 		{
-			time = MPT_FORMAT("0x%1", mpt::fmt::hex0<16>(entry.Timestamp));
+			time = mpt::format("0x%1")(mpt::fmt::hex0<16>(entry.Timestamp));
 		}
 		f << time;
 		if(entry.ThreadId == ThreadIdGUI)

common/Logging.h
@@ -47,7 +47,7 @@ Facility is some course grained code section identifier (more coarse grained
 than the current file name probably), useful to do some selective logging.
 
 Logging a more complex message:
-MPT_LOG(LogWarning, "sounddev", MPT_UFORMAT("Some message: foo=%1, bar=0x%2", foo, mpt::ufmt::hex0<8>(bar)));
+MPT_LOG(LogWarning, "sounddev", mpt::format(MPT_USTRING("Some message: foo=%1, bar=0x%2"))(foo, mpt::ufmt::hex0<8>(bar)));
 
 Note that even with full enabled logging and a runtime configurable logging
 level, the runtime overhead of a MPT_LOG(level, facility, text) call is just a

common/mptOS.cpp
@@ -204,7 +204,7 @@ mpt::ustring Version::VersionToString(uint16 version)
 	}
 	if(result.empty())
 	{
-		result = MPT_UFORMAT("0x%1", mpt::ufmt::dec0<4>(version));
+		result = mpt::format(MPT_USTRING("0x%1"))(mpt::ufmt::dec0<4>(version));
 	}
 	return result;
 }
@@ -266,14 +266,14 @@ mpt::ustring Version::GetName() const
 		{
 			if(mpt::Wine::GetVersion().IsValid())
 			{
-				result = MPT_UFORMAT("Wine %1 (%2)"
-					, mpt::Wine::GetVersion().AsString()
+				result = mpt::format(MPT_USTRING("Wine %1 (%2)"))(
+					  mpt::Wine::GetVersion().AsString()
 					, name
 					);
 			} else
 			{
-				result = MPT_UFORMAT("Wine (unknown version: '%1') (%2)"
-					, mpt::ToUnicode(mpt::CharsetUTF8, mpt::Wine::RawGetVersion())
+				result = mpt::format(MPT_USTRING("Wine (unknown version: '%1') (%2)"))(
+					  mpt::ToUnicode(mpt::CharsetUTF8, mpt::Wine::RawGetVersion())
 					, name
 					);
 			}

common/mptStringFormat.h
@@ -935,44 +935,5 @@ message_formatter<typename mpt::String::detail::to_string_type<Tformat>::type> f
 
 } // namespace mpt
 
-#if MPT_WSTRING_FORMAT || MPT_USTRING_MODE_WIDE || (defined(_MFC_VER) && defined(UNICODE))
-#if MPT_COMPILER_HAS_VARIADIC_MACROS
-#define MPT_FORMAT_MACROS_VARIADIC 1
-#else
-#error "Variadic macro support is required by MPT_FORMAT macros and it is unknown if your compiler supports those. Please tune CompilerSettings.h and/or BuildSettings.h and report an OpenMPT bug."
-#endif
-#else
-#define MPT_FORMAT_MACROS_VARIADIC 0
-#endif
-
-#if MPT_FORMAT_MACROS_VARIADIC
-
-#define MPT_FORMAT( f , ... )  (mpt::message_formatter<std::string >(      f )( __VA_ARGS__ ))
-#if MPT_WSTRING_FORMAT
-#define MPT_WFORMAT( f , ... ) (mpt::message_formatter<std::wstring>( L ## f )( __VA_ARGS__ ))
-#endif
-#if MPT_USTRING_MODE_WIDE
-#define MPT_UFORMAT( f , ... ) (mpt::message_formatter<mpt::ustring>( L ## f )( __VA_ARGS__ ))
-#else
-#define MPT_UFORMAT( f , ... ) (mpt::message_formatter<mpt::ustring>(      f )( __VA_ARGS__ ))
-#endif
-#if defined(_MFC_VER)
-#ifdef UNICODE
-#define MPT_TFORMAT( f , ... ) (mpt::message_formatter<CString     >( L ## f )( __VA_ARGS__ ))
-#else
-#define MPT_TFORMAT( f , ... ) (mpt::message_formatter<CString     >(      f )( __VA_ARGS__ ))
-#endif
-#endif
-
-#else // !MPT_FORMAT_MACROS_VARIADIC
-
-#define MPT_FORMAT  mpt::String::Print<std::string>
-#define MPT_UFORMAT mpt::String::Print<mpt::ustring>
-#if defined(_MFC_VER)
-#define MPT_TFORMAT mpt::String::Print<CString>
-#endif
-
-#endif // MPT_FORMAT_MACROS_VARIADIC
-
 
 OPENMPT_NAMESPACE_END

common/version.cpp
@@ -53,11 +53,11 @@ std::string ToStr(const VersionNum v)
 	} else if((v & 0xFFFF) == 0)
 	{
 		// Only parts of the version number are known (e.g. when reading the version from the IT or S3M file header)
-		return MPT_FORMAT("%1.%2", mpt::fmt::HEX((v >> 24) & 0xFF), mpt::fmt::HEX0<2>((v >> 16) & 0xFF));
+		return mpt::format("%1.%2")(mpt::fmt::HEX((v >> 24) & 0xFF), mpt::fmt::HEX0<2>((v >> 16) & 0xFF));
 	} else
 	{
 		// Full version info available
-		return MPT_FORMAT("%1.%2.%3.%4", mpt::fmt::HEX((v >> 24) & 0xFF), mpt::fmt::HEX0<2>((v >> 16) & 0xFF), mpt::fmt::HEX0<2>((v >> 8) & 0xFF), mpt::fmt::HEX0<2>((v) & 0xFF));
+		return mpt::format("%1.%2.%3.%4")(mpt::fmt::HEX((v >> 24) & 0xFF), mpt::fmt::HEX0<2>((v >> 16) & 0xFF), mpt::fmt::HEX0<2>((v >> 8) & 0xFF), mpt::fmt::HEX0<2>((v) & 0xFF));
 	}
 }
 
@@ -251,11 +251,11 @@ std::string GetBuildFeaturesString()
 		#if MPT_COMPILER_GENERIC
 			retval += "*C++11";
 		#elif MPT_COMPILER_MSVC
-			retval += MPT_FORMAT("*MSVC-%1.%2", MPT_COMPILER_MSVC_VERSION / 100, MPT_COMPILER_MSVC_VERSION % 100);
+			retval += mpt::format("*MSVC-%1.%2")(MPT_COMPILER_MSVC_VERSION / 100, MPT_COMPILER_MSVC_VERSION % 100);
 		#elif MPT_COMPILER_GCC
-			retval += MPT_FORMAT("*GCC-%1.%2.%3", MPT_COMPILER_GCC_VERSION / 10000, (MPT_COMPILER_GCC_VERSION / 100) % 100, MPT_COMPILER_GCC_VERSION % 100);
+			retval += mpt::format("*GCC-%1.%2.%3")(MPT_COMPILER_GCC_VERSION / 10000, (MPT_COMPILER_GCC_VERSION / 100) % 100, MPT_COMPILER_GCC_VERSION % 100);
 		#elif MPT_COMPILER_CLANG
-			retval += MPT_FORMAT("*Clang-%1.%2.%3", MPT_COMPILER_CLANG_VERSION / 10000, (MPT_COMPILER_CLANG_VERSION / 100) % 100, MPT_COMPILER_CLANG_VERSION % 100);
+			retval += mpt::format("*Clang-%1.%2.%3")(MPT_COMPILER_CLANG_VERSION / 10000, (MPT_COMPILER_CLANG_VERSION / 100) % 100, MPT_COMPILER_CLANG_VERSION % 100);
 		#else
 			retval += "*unknown";
 		#endif
@@ -306,25 +306,25 @@ std::string GetBuildCompilerString()
 		retval += "*Generic C++11 Compiler";
 	#elif MPT_COMPILER_MSVC
 		#if defined(_MSC_FULL_VER) && defined(_MSC_BUILD) && (_MSC_BUILD > 0)
-			retval += MPT_FORMAT("Microsoft Compiler %1.%2.%3.%4"
-				, _MSC_FULL_VER / 10000000
+			retval += mpt::format("Microsoft Compiler %1.%2.%3.%4")
+				( _MSC_FULL_VER / 10000000
 				, mpt::fmt::dec0<2>((_MSC_FULL_VER / 100000) % 100)
 				, mpt::fmt::dec0<5>(_MSC_FULL_VER % 100000)
 				, mpt::fmt::dec0<2>(_MSC_BUILD)
 				);
 		#elif defined(_MSC_FULL_VER)
-			retval += MPT_FORMAT("Microsoft Compiler %1.%2.%3"
-				, _MSC_FULL_VER / 10000000
+			retval += mpt::format("Microsoft Compiler %1.%2.%3")
+				( _MSC_FULL_VER / 10000000
 				, mpt::fmt::dec0<2>((_MSC_FULL_VER / 100000) % 100)
 				, mpt::fmt::dec0<5>(_MSC_FULL_VER % 100000)
 				);
 		#else
-			retval += MPT_FORMAT("Microsoft Compiler %1.%2", MPT_COMPILER_MSVC_VERSION / 100, MPT_COMPILER_MSVC_VERSION % 100);
+			retval += mpt::format("Microsoft Compiler %1.%2")(MPT_COMPILER_MSVC_VERSION / 100, MPT_COMPILER_MSVC_VERSION % 100);
 		#endif
 	#elif MPT_COMPILER_GCC
-		retval += MPT_FORMAT("GNU Compiler Collection %1.%2.%3", MPT_COMPILER_GCC_VERSION / 10000, (MPT_COMPILER_GCC_VERSION / 100) % 100, MPT_COMPILER_GCC_VERSION % 100);
+		retval += mpt::format("GNU Compiler Collection %1.%2.%3")(MPT_COMPILER_GCC_VERSION / 10000, (MPT_COMPILER_GCC_VERSION / 100) % 100, MPT_COMPILER_GCC_VERSION % 100);
 	#elif MPT_COMPILER_CLANG
-		retval += MPT_FORMAT("Clang %1.%2.%3", MPT_COMPILER_CLANG_VERSION / 10000, (MPT_COMPILER_CLANG_VERSION / 100) % 100, MPT_COMPILER_CLANG_VERSION % 100);
+		retval += mpt::format("Clang %1.%2.%3")(MPT_COMPILER_CLANG_VERSION / 10000, (MPT_COMPILER_CLANG_VERSION / 100) % 100, MPT_COMPILER_CLANG_VERSION % 100);
 	#else
 		retval += "*unknown";
 	#endif
@@ -395,7 +395,7 @@ static std::string GetVersionString(bool verbose)
 	#ifdef MODPLUG_TRACKER
 		if(verbose)
 		{
-			retval += MPT_FORMAT(" %1 bit", sizeof(void*) * 8);
+			retval += mpt::format(" %1 bit")(sizeof(void*) * 8);
 		}
 	#endif
 	if(IsDebugBuild() || IsTestBuild() || IsDirty() || HasMixedRevisions())

mptrack/AboutDialog.cpp
@@ -261,7 +261,7 @@ BOOL CAboutDlg::OnInitDialog()
 	CDialog::OnInitDialog();
 
 	mpt::ustring app;
-	app += MPT_UFORMAT("OpenMPT %1 bit", sizeof(void*) * 8)
+	app += mpt::format(MPT_USTRING("OpenMPT %1 bit"))(sizeof(void*) * 8)
 		+ (MptVersion::IsForOlderWindows() ? MPT_USTRING(" for older Windows") : MPT_USTRING(""))
 		+ MPT_USTRING("\n");
 	app += MPT_USTRING("Version ") + mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetVersionStringSimple()) + MPT_USTRING("\n");
@@ -317,12 +317,12 @@ mpt::ustring CAboutDlg::GetTabText(int tab)
 		case 0:
 			text += MPT_USTRING("OpenMPT - Open ModPlug Tracker") + lf;
 			text += lf;
-			text += MPT_UFORMAT("Version: %1", mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetVersionStringExtended())) + lf;
-			text += MPT_UFORMAT("Source Code URL: %1", mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetUrl())) + lf;
-			text += MPT_UFORMAT("Build Date: %1", mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetBuildDateString())) + lf;
-			text += MPT_UFORMAT("Compiler: %1", mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetBuildCompilerString())) + lf;
-			text += MPT_UFORMAT("Required Windows Kernel Level: %1", mpt::Windows::Version::VersionToString(mpt::Windows::Version::GetMinimumKernelLevel())) + lf;
-			text += MPT_UFORMAT("Required Windows API Level: %1", mpt::Windows::Version::VersionToString(mpt::Windows::Version::GetMinimumAPILevel())) + lf;
+			text += mpt::format(MPT_USTRING("Version: %1"))(mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetVersionStringExtended())) + lf;
+			text += mpt::format(MPT_USTRING("Source Code URL: %1"))(mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetUrl())) + lf;
+			text += mpt::format(MPT_USTRING("Build Date: %1"))(mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetBuildDateString())) + lf;
+			text += mpt::format(MPT_USTRING("Compiler: %1"))(mpt::ToUnicode(mpt::CharsetUTF8, MptVersion::GetBuildCompilerString())) + lf;
+			text += mpt::format(MPT_USTRING("Required Windows Kernel Level: %1"))(mpt::Windows::Version::VersionToString(mpt::Windows::Version::GetMinimumKernelLevel())) + lf;
+			text += mpt::format(MPT_USTRING("Required Windows API Level: %1"))(mpt::Windows::Version::VersionToString(mpt::Windows::Version::GetMinimumAPILevel())) + lf;
 			{
 				text += MPT_USTRING("Required CPU features: ");
 				std::vector<mpt::ustring> features;
@@ -391,8 +391,8 @@ mpt::ustring CAboutDlg::GetTabText(int tab)
 			text += lf;
 			if(GetProcSupport() & PROCSUPPORT_CPUID)
 			{
-				text += MPT_UFORMAT("CPU: %1, Family %2, Model %3, Stepping %4"
-					, mpt::ToUnicode(mpt::CharsetASCII, (std::strlen(ProcVendorID) > 0) ? std::string(ProcVendorID) : std::string("Generic"))
+				text += mpt::format(MPT_USTRING("CPU: %1, Family %2, Model %3, Stepping %4"))
+					( mpt::ToUnicode(mpt::CharsetASCII, (std::strlen(ProcVendorID) > 0) ? std::string(ProcVendorID) : std::string("Generic"))
 					, ProcFamily
 					, ProcModel
 					, ProcStepping
@@ -401,10 +401,10 @@ mpt::ustring CAboutDlg::GetTabText(int tab)
 			{
 				text += MPT_USTRING("Generic without CPUID") + lf;
 			}
-			text += MPT_UFORMAT("Operating System: %1", mpt::Windows::Version::Current().GetName()) + lf;
+			text += mpt::format(MPT_USTRING("Operating System: %1"))(mpt::Windows::Version::Current().GetName()) + lf;
 			text += lf;
-			text += MPT_UFORMAT("OpenMPT Path%2: %1", theApp.GetAppDirPath(), theApp.IsPortableMode() ? MPT_USTRING(" (portable)") : MPT_USTRING("")) + lf;
-			text += MPT_UFORMAT("Settings%2: %1", theApp.GetConfigFileName(), theApp.IsPortableMode() ? MPT_USTRING(" (portable)") : MPT_USTRING("")) + lf;
+			text += mpt::format(MPT_USTRING("OpenMPT Path%2: %1"))(theApp.GetAppDirPath(), theApp.IsPortableMode() ? MPT_USTRING(" (portable)") : MPT_USTRING("")) + lf;
+			text += mpt::format(MPT_USTRING("Settings%2: %1"))(theApp.GetConfigFileName(), theApp.IsPortableMode() ? MPT_USTRING(" (portable)") : MPT_USTRING("")) + lf;
 			break;
 		case 1:
 			{
@@ -446,8 +446,8 @@ mpt::ustring CAboutDlg::GetTabText(int tab)
 							{
 								name = mpt::ToUnicode(mpt::CharsetASCII, info.settingsKey);
 							}
-							text += MPT_UFORMAT("%1: %2"
-								, name
+							text += mpt::format(MPT_USTRING("%1: %2"))
+								( name
 								, info.state == ComponentStateAvailable ? MPT_USTRING("ok") :
 									info.state == ComponentStateUnavailable? MPT_USTRING("missing") :
 									info.state == ComponentStateUnintialized ? MPT_USTRING("not loaded") :
@@ -457,8 +457,8 @@ mpt::ustring CAboutDlg::GetTabText(int tab)
 								);
 							if(info.type != ComponentTypeUnknown)
 							{
-								text += MPT_UFORMAT(" (%1)"
-									, info.type == ComponentTypeBuiltin ? MPT_USTRING("builtin") :
+								text += mpt::format(MPT_USTRING(" (%1)"))
+									( info.type == ComponentTypeBuiltin ? MPT_USTRING("builtin") :
 										info.type == ComponentTypeSystem ? MPT_USTRING("system") :
 										info.type == ComponentTypeSystemInstallable ? MPT_USTRING("system, optional") :
 										info.type == ComponentTypeBundled ? MPT_USTRING("bundled") :

mptrack/UpdateCheck.cpp
@@ -275,9 +275,9 @@ void CUpdateCheck::ShowSuccessGUI(WPARAM wparam, LPARAM lparam)
 	if(result.UpdateAvailable)
 	{
 		if(Reporting::Confirm(
-			MPT_UFORMAT("A new version is available!\nOpenMPT %1 has been released on %2. Would you like to visit %3 for more information?"
-			, result.Version, result.Date, result.URL
-			), MPT_USTRING("OpenMPT Internet Update")) == cnfYes)
+			mpt::format(MPT_USTRING("A new version is available!\nOpenMPT %1 has been released on %2. Would you like to visit %3 for more information?"))
+			 (result.Version, result.Date, result.URL)
+			 , MPT_USTRING("OpenMPT Internet Update")) == cnfYes)
 		{
 			CTrackApp::OpenURL(result.URL);
 		}

sounddev/SoundDeviceManager.cpp
@@ -155,17 +155,17 @@ void Manager::ReEnumerate()
 	}
 	std::stable_sort(m_SoundDevices.begin(), m_SoundDevices.end(), CompareInfo(typePriorities));
 
-	MPT_LOG(LogDebug, "sounddev", MPT_USTRING("Sound Devices enumerated:"));
+	MPT_LOG(LogDebug, "sounddev", mpt::format(MPT_USTRING("Sound Devices enumerated:"))());
 	for(std::size_t i = 0; i < m_SoundDevices.size(); ++i)
 	{
-		MPT_LOG(LogDebug, "sounddev", MPT_UFORMAT(" Identifier : %1", m_SoundDevices[i].GetIdentifier()));
-		MPT_LOG(LogDebug, "sounddev", MPT_UFORMAT("  Type      : %1", m_SoundDevices[i].type));
-		MPT_LOG(LogDebug, "sounddev", MPT_UFORMAT("  InternalID: %1", m_SoundDevices[i].internalID));
-		MPT_LOG(LogDebug, "sounddev", MPT_UFORMAT("  API Name  : %1", m_SoundDevices[i].apiName));
-		MPT_LOG(LogDebug, "sounddev", MPT_UFORMAT("  Name      : %1", m_SoundDevices[i].name));
+		MPT_LOG(LogDebug, "sounddev", mpt::format(MPT_USTRING(" Identifier : %1"))(m_SoundDevices[i].GetIdentifier()));
+		MPT_LOG(LogDebug, "sounddev", mpt::format(MPT_USTRING("  Type      : %1"))(m_SoundDevices[i].type));
+		MPT_LOG(LogDebug, "sounddev", mpt::format(MPT_USTRING("  InternalID: %1"))(m_SoundDevices[i].internalID));
+		MPT_LOG(LogDebug, "sounddev", mpt::format(MPT_USTRING("  API Name  : %1"))(m_SoundDevices[i].apiName));
+		MPT_LOG(LogDebug, "sounddev", mpt::format(MPT_USTRING("  Name      : %1"))(m_SoundDevices[i].name));
 		for(std::map<mpt::ustring, mpt::ustring>::const_iterator extraIt = m_SoundDevices[i].extraData.begin(); extraIt != m_SoundDevices[i].extraData.end(); ++extraIt)
 		{
-			Log(LogDebug, MPT_UFORMAT("  Extra Data: %1 = %2", extraIt->first, extraIt->second));
+			Log(LogDebug, mpt::format(MPT_USTRING("  Extra Data: %1 = %2"))(extraIt->first, extraIt->second));
 		}
 	}
 	

commit 56af42efdff05c9efe471356dceb4b3a589a3ebf

[Fix] Debug builds broke in r5868.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5869 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/Logging.cpp
@@ -133,8 +133,8 @@ void Logger::SendLogMessage(const Context &context, LogLevel level, const char *
 			}
 			if(s_logfile)
 			{
-				fprintf(s_logfile, mpt::ToCharset(mpt::CharsetUTF8, mpt::format(MPT_USTRING("%1+%2 %3(%4): %5 [%6]\n"))(
-					, mpt::Date::ANSI::ToString(cur)
+				fprintf(s_logfile, mpt::ToCharset(mpt::CharsetUTF8, mpt::format(MPT_USTRING("%1+%2 %3(%4): %5 [%6]\n"))
+					( mpt::Date::ANSI::ToString(cur)
 					, mpt::ufmt::dec<6>(diff)
 					, file
 					, line
@@ -146,8 +146,8 @@ void Logger::SendLogMessage(const Context &context, LogLevel level, const char *
 		}
 		if(mpt::log::DebuggerEnabled)
 		{
-			OutputDebugStringW(mpt::ToWide(mpt::format(MPT_USTRING("%1(%2): +%3 %4 [%5]\n"))(
-				, file
+			OutputDebugStringW(mpt::ToWide(mpt::format(MPT_USTRING("%1(%2): +%3 %4 [%5]\n"))
+				( file
 				, line
 				, mpt::ufmt::dec<6>(diff)
 				, message