OpenMPT/openmpt

commit c44ab64d9c0f7a1365f2b45ec4c351cd6d4a5292

[Mod] build: Rework the build system logic to decide whether un4seen unmo3 and libmpg123 dynamic loading support should be included. If either the build is for Windows or libltdl or libdl are available and libmpg123 is not directly linked, libopenmpt will now always try to dynamically load it. If either the build is for Windows or libltdl or libdl are available AND no MP3 or Vorbis decoder is directly linked, support for dynamic loading of un4seen unmo3 will be included. On default Windows builds (where libmpg123 is not linked directly and MediaFoundation cannot be guaranteed to be aviable on Windows Server versions, un4seen unmo3 support is always included in addition to built-in MO3 support with decoding via dynamically loaded libmpg123 or MediaFoundation. [Mod] build: The built-in MO3 decoder now only gets included if at least one MP3 and at least one Vorbis decoder is available (linked drectly or possibly available via dynamic loading support). [Reg] build: Remove the Makefile and Autotools options to enable un4seen unmo3 support. [Doc] libopenmpt: Update the changelog entries accordingly. [Ref] Rename MPT_WITH_UNMO3_DYNBIND to MPT_ENABLE_UNMO3_DYNBIND and MPT_WITH_MPG133_DYNBIND to MPT_ENABLE_MPG123_DYNBIND. [Ref] Rename MPT_BUILTIN_MO3 to MPT_ENABLE_MO3_BUILTIN.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6002 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/BuildSettings.h
@@ -91,11 +91,9 @@
 #endif
 //#define MPT_WITH_MINIZ
 //#define MPT_WITH_MPG123
-#define MPT_WITH_MPG123_DYNBIND
 #define MPT_WITH_OGG
 #define MPT_WITH_STBVORBIS
 //#define MPT_WITH_UNMO3
-#define MPT_WITH_UNMO3_DYNBIND
 //#define MPT_WITH_VORBIS
 //#define MPT_WITH_VORBISFILE
 #define MPT_WITH_ZLIB
@@ -116,13 +114,9 @@
 #endif
 #define MPT_WITH_MINIZ
 //#define MPT_WITH_MPG123
-#define MPT_WITH_MPG123_DYNBIND
 //#define MPT_WITH_OGG
 #define MPT_WITH_STBVORBIS
 //#define MPT_WITH_UNMO3
-#if !(defined(MPT_WITH_MPG123) || defined(MPT_WITH_MEDIAFOUNDATION)) || !((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
-#define MPT_WITH_UNMO3_DYNBIND
-#endif
 //#define MPT_WITH_VORBIS
 //#define MPT_WITH_VORBISFILE
 //#define MPT_WITH_ZLIB
@@ -197,8 +191,14 @@
 // (HACK) Define to build without any plugin support
 //#define NO_PLUGINS
 
+// Enable dynamic loading of libmpg123
+#define MPT_ENABLE_MPG123_DYNBIND
+
+// Enable dynamic loading of un4seen unmo3
+#define MPT_ENABLE_UNMO3_DYNBIND
+
 // Enable built-in MO3 decoder
-//#define MPT_BUILTIN_MO3
+//#define MPT_ENABLE_MO3_BUILTIN
 
 // Do not build libopenmpt C api
 #define NO_LIBOPENMPT_C
@@ -246,8 +246,15 @@
 #define NO_VST
 #define NO_DMO
 #define NO_PLUGINS
-#if ((defined(MPT_WITH_MPG123) || defined(MPT_WITH_MPG123_DYNBIND)) || defined(MPT_WITH_MEDIAFOUNDATION)) && ((defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
-#define MPT_BUILTIN_MO3
+#if MPT_OS_WINDOWS || defined(MPT_WITH_LTDL) || defined(MPT_WIH_DL)
+#define MPT_ENABLE_MPG123_DYNBIND
+//#if !(defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)) || defined(MPT_WITH_MEDIAFOUNDATION)) || !((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
+#if !(defined(MPT_WITH_MPG123)) || !((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
+#define MPT_ENABLE_UNMO3_DYNBIND
+#endif
+#endif // MPT_OS_WINDOWS || defined(MPT_WITH_LTDL) || defined(MPT_WIH_DL)
+#if ((defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)) || defined(MPT_WITH_MEDIAFOUNDATION)) && ((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
+#define MPT_ENABLE_MO3_BUILTIN
 #endif
 //#define NO_LIBOPENMPT_C
 //#define NO_LIBOPENMPT_CXX
@@ -430,19 +437,19 @@
 #define MPT_ENABLE_DYNBIND // Tracker requires dynamic library loading for export codecs
 #endif
 
-#if defined(MPT_WITH_MPG123_DYNBIND) && !defined(MPT_ENABLE_DYNBIND)
+#if defined(MPT_ENABLE_MPG123_DYNBIND) && !defined(MPT_ENABLE_DYNBIND)
 #define MPT_ENABLE_DYNBIND // mpg123 is loaded dynamically
 #endif
 
-#if (defined(MPT_WITH_MPG123) || defined(MPT_WITH_MPG123_DYNBIND)) && !defined(MPT_ENABLE_MP3_SAMPLES)
+#if (defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)) && !defined(MPT_ENABLE_MP3_SAMPLES)
 #define MPT_ENABLE_MP3_SAMPLES
 #endif
 
-#if defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND) || defined(MPT_BUILTIN_MO3)
+#if defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND) || defined(MPT_ENABLE_MO3_BUILTIN)
 #define MPT_ENABLE_MO3
 #endif
 
-#if defined(MPT_WITH_UNMO3_DYNBIND) && !defined(MPT_ENABLE_DYNBIND)
+#if defined(MPT_ENABLE_UNMO3_DYNBIND) && !defined(MPT_ENABLE_DYNBIND)
 #define MPT_ENABLE_DYNBIND // unmo3 is loaded dynamically
 #endif
 

Makefile
@@ -66,8 +66,6 @@
 #  NO_VORBIS=1      Avoid using libvorbis, even if found
 #  NO_VORBISFILE=1  Avoid using libvorbisfile, even if found
 #
-#  USE_UNMO3=1      Support dynamic loading of unmo3 shared library
-#
 #
 # Build flags for openmpt123 (provide on each `make` invocation)
 #
@@ -408,12 +406,6 @@ NO_VORBISFILE:=1
 endif
 endif
 
-ifeq ($(USE_UNMO3),1)
-CPPFLAGS_UNMO3 := -DMPT_WITH_UNMO3_DYNBIND
-LDLIBS_UNMO3   := 
-else
-endif
-
 ifeq ($(NO_SDL2),1)
 else
 #LDLIBS   += -lsdl2
@@ -487,9 +479,9 @@ NO_SNDFILE:=1
 endif
 endif
 
-CPPFLAGS += $(CPPFLAGS_LTDL) $(CPPFLAGS_DL) $(CPPFLAGS_ZLIB) $(CPPFLAGS_MPG123) $(CPPFLAGS_OGG) $(CPPFLAGS_VORBIS) $(CPPFLAGS_VORBISFILE) $(CPPFLAGS_UNMO3)
-LDFLAGS += $(LDFLAGS_LTDL) $(LDFLAGS_DL) $(LDFLAGS_ZLIB) $(LDFLAGS_MPG123) $(LDFLAGS_OGG) $(LDFLAGS_VORBIS) $(LDFLAGS_VORBISFILE) $(LDFLAGS_UNMO3)
-LDLIBS += $(LDLIBS_LTDL) $(LDLIBS_DL) $(LDLIBS_ZLIB) $(LDLIBS_MPG123) $(LDLIBS_OGG) $(LDLIBS_VORBIS) $(LDLIBS_VORBISFILE) $(LDLIBS_UNMO3)
+CPPFLAGS += $(CPPFLAGS_LTDL) $(CPPFLAGS_DL) $(CPPFLAGS_ZLIB) $(CPPFLAGS_MPG123) $(CPPFLAGS_OGG) $(CPPFLAGS_VORBIS) $(CPPFLAGS_VORBISFILE)
+LDFLAGS += $(LDFLAGS_LTDL) $(LDFLAGS_DL) $(LDFLAGS_ZLIB) $(LDFLAGS_MPG123) $(LDFLAGS_OGG) $(LDFLAGS_VORBIS) $(LDFLAGS_VORBISFILE)
+LDLIBS += $(LDLIBS_LTDL) $(LDLIBS_DL) $(LDLIBS_ZLIB) $(LDLIBS_MPG123) $(LDLIBS_OGG) $(LDLIBS_VORBIS) $(LDLIBS_VORBISFILE)
 
 CPPFLAGS_OPENMPT123 += $(CPPFLAGS_SDL2) $(CPPFLAGS_SDL) $(CPPFLAGS_PORTAUDIO) $(CPPFLAGS_FLAC) $(CPPFLAGS_SNDFILE)
 LDFLAGS_OPENMPT123  += $(LDFLAGS_SDL2) $(LDFLAGS_SDL) $(LDFLAGS_PORTAUDIO) $(LDFLAGS_FLAC) $(LDFLAGS_SNDFILE)
@@ -571,10 +563,6 @@ ifeq ($(NO_ZLIB),1)
 LIBOPENMPT_C_SOURCES += include/miniz/miniz.c
 LIBOPENMPTTEST_C_SOURCES += include/miniz/miniz.c
 CPPFLAGS += -DMPT_WITH_MINIZ
-endif
-
-ifeq ($(NO_MPG123),1)
-CPPFLAGS += -DMPT_WITH_MPG123_DYNBIND
 endif
 
 ifeq ($(NO_OGG),1)

build/autotools/Makefile.am
@@ -84,10 +84,10 @@ pkgconfig_DATA += libopenmpt/libopenmpt.pc
 lib_LTLIBRARIES += libopenmpt.la
 libopenmpt_la_LDFLAGS = -version-info 0:16:0
 nobase_include_HEADERS += libopenmpt/libopenmpt.h libopenmpt/libopenmpt.hpp libopenmpt/libopenmpt_version.h libopenmpt/libopenmpt_config.h libopenmpt/libopenmpt_stream_callbacks_fd.h libopenmpt/libopenmpt_stream_callbacks_file.h libopenmpt/libopenmpt_ext.hpp
-libopenmpt_la_CPPFLAGS = -DLIBOPENMPT_BUILD -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS) $(LTDL_CPPFLAGS) $(DL_CPPFLAGS) $(UNMO3_CPPFLAGS)
+libopenmpt_la_CPPFLAGS = -DLIBOPENMPT_BUILD -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS) $(LTDL_CPPFLAGS) $(DL_CPPFLAGS)
 libopenmpt_la_CXXFLAGS = $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS)
 libopenmpt_la_CFLAGS = $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS)
-libopenmpt_la_LIBADD = $(ZLIB_LIBS) $(MPG123_LIBS) $(OGG_LIBS) $(VORBIS_LIBS) $(VORBISFILE_LIBS) $(LTDL_LIBS) $(DL_LIBS) $(UNMO3_LIBS)
+libopenmpt_la_LIBADD = $(ZLIB_LIBS) $(MPG123_LIBS) $(OGG_LIBS) $(VORBIS_LIBS) $(VORBISFILE_LIBS) $(LTDL_LIBS) $(DL_LIBS)
 libopenmpt_la_SOURCES = 
 libopenmpt_la_SOURCES += build/svn_version/svn_version.h
 libopenmpt_la_SOURCES += common/AudioCriticalSection.cpp
@@ -302,10 +302,10 @@ libmodplug_la_SOURCES += libopenmpt/libopenmpt_modplug_cpp.cpp
 endif
 
 check_PROGRAMS += libopenmpttest
-libopenmpttest_CPPFLAGS = -DLIBOPENMPT_BUILD -DLIBOPENMPT_BUILD_TEST -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS) $(LTDL_CPPFLAGS) $(DL_CPPFLAGS) $(UNMO3_CPPFLAGS)
+libopenmpttest_CPPFLAGS = -DLIBOPENMPT_BUILD -DLIBOPENMPT_BUILD_TEST -I$(srcdir)/build/svn_version -I$(srcdir)/ -I$(srcdir)/common $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS) $(LTDL_CPPFLAGS) $(DL_CPPFLAGS)
 libopenmpttest_CXXFLAGS = $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS)
 libopenmpttest_CFLAGS = $(ZLIB_CFLAGS) $(MPG123_CFLAGS) $(OGG_CFLAGS) $(VORBIS_CFLAGS) $(VORBISFILE_CFLAGS)
-libopenmpttest_LDADD = $(ZLIB_LIBS) $(MPG123_LIBS) $(OGG_LIBS) $(VORBIS_LIBS) $(VORBISFILE_LIBS) $(LTDL_LIBS) $(DL_LIBS) $(UNMO3_LIBS)
+libopenmpttest_LDADD = $(ZLIB_LIBS) $(MPG123_LIBS) $(OGG_LIBS) $(VORBIS_LIBS) $(VORBISFILE_LIBS) $(LTDL_LIBS) $(DL_LIBS)
 libopenmpttest_SOURCES = 
 libopenmpttest_SOURCES += libopenmpt/libopenmpt_test.cpp
 libopenmpttest_SOURCES += test/test.cpp

build/autotools/configure.ac
@@ -141,21 +141,6 @@ AC_SUBST([DL_CPPFLAGS])
 AC_SUBST([DL_LIBS])
 
 
-# Optional libopenmpt feature: unmo3
-AC_ARG_ENABLE([unmo3], AS_HELP_STRING([--enable-unmo3], [Enable the use of dynamically loaded libunmo3.]))
-AS_IF([test "x$enable_unmo3" = "xyes"],
- [
-  UNMO3_CPPFLAGS=-DMPT_WITH_UNMO3_DYNBIND
-  UNMO3_LIBS=
- ],
- [
-  UNMO3_CPPFLAGS=
-  UNMO3_LIBS=
- ]
-)
-AC_SUBST([UNMO3_CPPFLAGS])
-AC_SUBST([UNMO3_LIBS])
-
 # Optional openmpt123 and examples dependency
 AC_ARG_WITH([portaudio], AS_HELP_STRING([--without-portaudio], [Disable use of libportaudio.]))
 AS_IF([test "x$with_portaudio" != "xno"],

common/version.cpp
@@ -275,7 +275,7 @@ std::string GetBuildFeaturesString()
 		#else
 			retval += " -INFLATE";
 		#endif
-		#if defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND)
+		#if defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND)
 			retval += " +UNMO3";
 		#else
 			retval += " -UNMO3";
@@ -505,12 +505,12 @@ mpt::ustring GetFullCreditsString()
 		"http://www.hermannseib.com/english/vsthost.htm\n"
 		"\n"
 #endif
-#if defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND)
+#if defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND)
 		"Ian Luck for UNMO3\n"
 		"http://www.un4seen.com/mo3.html\n"
 		"\n"
 #endif
-#ifdef MPT_BUILTIN_MO3
+#ifdef MPT_ENABLE_MO3_BUILTIN
 		"Laurent Cl\xc3\xA9vy for unofficial MO3 documentation and decompression code\n"
 		"https://github.com/lclevy/unmo3\n"
 		"\n"
@@ -549,7 +549,7 @@ mpt::ustring GetFullCreditsString()
 		"https://xiph.org/flac/\n"
 		"\n"
 #endif
-#if defined(MPT_WITH_MPG123) || defined(MPT_WITH_MPG123_DYNBIND)
+#if defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)
 		"The mpg123 project for libmpg123\n"
 		"http://mpg123.de/\n"
 		"\n"

soundlib/Load_mo3.cpp
@@ -2,8 +2,8 @@
  * Load_mo3.cpp
  * ------------
  * Purpose: MO3 module loader.
- * Notes  : OpenMPT has its own built-in decoder (enabled with MPT_BUILTIN_MO3), but can also
- *          make use of the official, closed-source library (unmo3.dll / libunmo3.so).
+ * Notes  : OpenMPT has its own built-in decoder (enabled with MPT_ENABLE_MO3_BUILTIN),
+ *          but can also make use of the official, closed-source library (unmo3.dll / libunmo3.so).
  * Authors: Johannes Schultz / OpenMPT Devs
  *          Based on documentation and the decompression routines from the
  *          open-source UNMO3 project (https://github.com/lclevy/unmo3).
@@ -17,7 +17,7 @@
 #include "Loaders.h"
 #include "../common/ComponentManager.h"
 
-#ifdef MPT_BUILTIN_MO3
+#ifdef MPT_ENABLE_MO3_BUILTIN
 #include "Tables.h"
 #include "../common/version.h"
 
@@ -42,29 +42,29 @@
 #include "SampleFormatConverters.h"
 #endif // MPT_WITH_STBVORBIS
 
-#endif // MPT_BUILTIN_MO3
+#endif // MPT_ENABLE_MO3_BUILTIN
 
-#if defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND)
+#if defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND)
 // unmo3.h
 #if MPT_OS_WINDOWS
 #define UNMO3_API __stdcall
 #else
 #define UNMO3_API
 #endif
-#if !defined(MPT_WITH_UNMO3_DYNBIND)
+#if !defined(MPT_ENABLE_UNMO3_DYNBIND)
 extern "C" {
 OPENMPT_NAMESPACE::uint32 UNMO3_API UNMO3_GetVersion(void);
 void UNMO3_API UNMO3_Free(const void *data);
 OPENMPT_NAMESPACE::int32 UNMO3_API UNMO3_Decode(const void **data, OPENMPT_NAMESPACE::uint32 *len, OPENMPT_NAMESPACE::uint32 flags);
 }
-#endif // !MPT_WITH_UNMO3_DYNBIND
-#endif // MPT_WITH_UNMO3 || MPT_WITH_UNMO3_DYNBIND
+#endif // !MPT_ENABLE_UNMO3_DYNBIND
+#endif // MPT_WITH_UNMO3 || MPT_ENABLE_UNMO3_DYNBIND
 
 
 OPENMPT_NAMESPACE_BEGIN
 
 
-#if defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND)
+#if defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND)
 
 class ComponentUnMO3 : public ComponentLibrary
 {
@@ -87,7 +87,7 @@ public:
 	ComponentUnMO3() : ComponentLibrary(ComponentTypeForeign) { }
 	bool DoInitialize()
 	{
-#if !defined(MPT_WITH_UNMO3_DYNBIND)
+#if !defined(MPT_ENABLE_UNMO3_DYNBIND)
 		UNMO3_GetVersion = &(::UNMO3_GetVersion);
 		UNMO3_Free = &(::UNMO3_Free);
 		UNMO3_Decode_Old = nullptr;
@@ -112,10 +112,10 @@ public:
 };
 MPT_REGISTERED_COMPONENT(ComponentUnMO3, "UnMO3")
 
-#endif // MPT_WITH_UNMO3 || MPT_WITH_UNMO3_DYNBIND
+#endif // MPT_WITH_UNMO3 || MPT_ENABLE_UNMO3_DYNBIND
 
 
-#ifdef MPT_BUILTIN_MO3
+#ifdef MPT_ENABLE_MO3_BUILTIN
 
 #ifdef NEEDS_PRAGMA_PACK
 #pragma pack(push, 1)
@@ -822,7 +822,7 @@ static long VorbisfileFilereaderTell(void *datasource)
 
 
 
-#endif // MPT_BUILTIN_MO3
+#endif // MPT_ENABLE_MO3_BUILTIN
 
 
 
@@ -846,7 +846,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-#if !(defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND)) && !defined(MPT_BUILTIN_MO3)
+#if !(defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND)) && !defined(MPT_ENABLE_MO3_BUILTIN)
 	// As of November 2015, the format revision is 5; Versions > 31 are unlikely to exist in the next few years,
 	// so we will just ignore those if there's no UNMO3 library to tell us if the file is valid or not
 	// (avoid log entry with .MOD files that have a song name starting with "MO3".
@@ -858,7 +858,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 	AddToLog(LogError, MPT_USTRING("The file appears to be a MO3 file, but this OpenMPT build does not support loading MO3 files."));
 	return false;
 
-#elif defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND)
+#elif defined(MPT_WITH_UNMO3) || defined(MPT_ENABLE_UNMO3_DYNBIND)
 	MPT_UNREFERENCED_PARAMETER(version);
 
 	// Try to load unmo3 dynamically.
@@ -898,14 +898,14 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 		}
 	} else
 	{
-#ifndef MPT_BUILTIN_MO3
+#ifndef MPT_ENABLE_MO3_BUILTIN
 		AddToLog(LogError, MPT_USTRING("Loading MO3 file failed because the unmo3 library could not be loaded."));
 		return false;
-#endif // MPT_BUILTIN_MO3
+#endif // MPT_ENABLE_MO3_BUILTIN
 	}
 #endif
 
-#ifdef MPT_BUILTIN_MO3
+#ifdef MPT_ENABLE_MO3_BUILTIN
 
 	if(version > 5)
 	{
@@ -1879,7 +1879,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 	return true;
 #else
 	return false;
-#endif // BUILTIN_MO3
+#endif // MPT_ENABLE_MO3_BUILTIN
 }
 
 

soundlib/SampleFormats.cpp
@@ -2559,9 +2559,9 @@ fail:
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // MP3 Samples
 
-#if defined(MPT_WITH_MPG123) || defined(MPT_WITH_MPG123_DYNBIND)
+#if defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)
 
-#if !defined(MPT_WITH_MPG123) && defined(MPT_WITH_MPG123_DYNBIND)
+#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
 
 	enum mpg123_parms
 	{
@@ -2598,16 +2598,16 @@ fail:
 
 	typedef struct {int foo;} mpg123_handle;
 
-#endif // !MPT_WITH_MPG123 && MPT_WITH_MPG123_DYNBIND
+#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
 
 class ComponentMPG123
-#if !defined(MPT_WITH_MPG123) && defined(MPT_WITH_MPG123_DYNBIND)
+#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
 	: public ComponentLibrary
-#endif // !MPT_WITH_MPG123 && MPT_WITH_MPG123_DYNBIND
+#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
 {
-#if !defined(MPT_WITH_MPG123) && defined(MPT_WITH_MPG123_DYNBIND)
+#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
 	MPT_DECLARE_COMPONENT_MEMBERS
-#endif // !MPT_WITH_MPG123 && MPT_WITH_MPG123_DYNBIND
+#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
 public:
 
 	int (*mpg123_init )(void);
@@ -2680,7 +2680,7 @@ public:
 			mpg123_exit();
 		}
 	}
-#elif defined(MPT_WITH_MPG123_DYNBIND)
+#elif defined(MPT_ENABLE_MPG123_DYNBIND)
 	ComponentMPG123() : ComponentLibrary(ComponentTypeForeign) { }
 	bool DoInitialize()
 	{
@@ -2718,17 +2718,17 @@ public:
 	}
 #endif // MPT_WITH_MPG123
 };
-#if !defined(MPT_WITH_MPG123) && defined(MPT_WITH_MPG123_DYNBIND)
+#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
 MPT_REGISTERED_COMPONENT(ComponentMPG123, "Mpg123")
-#endif // !MPT_WITH_MPG123 && MPT_WITH_MPG123_DYNBIND
+#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
 
-#endif // MPT_WITH_MPG123 || MPT_WITH_MPG123_DYNBIND
+#endif // MPT_WITH_MPG123 || MPT_ENABLE_MPG123_DYNBIND
 
 
 bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Decode)
 //----------------------------------------------------------------------------------
 {
-#if defined(MPT_WITH_MPG123) || defined(MPT_WITH_MPG123_DYNBIND)
+#if defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)
 
 	// Check file for validity, or else mpg123 will happily munch many files that start looking vaguely resemble an MPEG stream mid-file.
 	file.Rewind();
@@ -2781,7 +2781,7 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 	{
 		return false;
 	}
-#elif defined(MPT_WITH_MPG123_DYNBIND)
+#elif defined(MPT_ENABLE_MPG123_DYNBIND)
 	ComponentHandle<ComponentMPG123> mpg123;
 	if(!IsComponentAvailable(mpg123))
 	{
@@ -2857,7 +2857,7 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 	MPT_UNREFERENCED_PARAMETER(sample);
 	MPT_UNREFERENCED_PARAMETER(file);
 	MPT_UNREFERENCED_PARAMETER(mo3Decode);
-#endif // MPT_WITH_MPG123 || MPT_WITH_MPG123_DYNBIND
+#endif // MPT_WITH_MPG123 || MPT_ENABLE_MPG123_DYNBIND
 	return false;
 }
 

soundlib/Tables.cpp
@@ -112,9 +112,9 @@ static const ModContainerInfo modContainerInfo[] =
 	// Container formats
 	{ MOD_CONTAINERTYPE_GDM,   "General Digital Music",    "gdm"   },
 	{ MOD_CONTAINERTYPE_UMX,   "Unreal Music",             "umx"   },
-#if defined(MPT_WITH_UNMO3) || defined(MPT_WITH_UNMO3_DYNBIND) || defined(MPT_BUILTIN_MO3)
+#if defined(MPT_ENABLE_MO3)
 	{ MOD_CONTAINERTYPE_MO3,   "Un4seen MO3",              "mo3"   },
-#endif // MPT_WITH_UNMO3 || MPT_WITH_UNMO3_DYNBIND || MPT_BUILTIN_MO3
+#endif // MPT_ENABLE_MO3
 	{ MOD_CONTAINERTYPE_XPK,   "XPK packed",               "xpk"   },
 	{ MOD_CONTAINERTYPE_PP20,  "PowerPack PP20",           "ppm"   },
 	{ MOD_CONTAINERTYPE_MMCMP, "Music Module Compressor",  "mmcmp" }

commit a28b5270d13043f623ce21a4271559cf9fcf14a6

[Fix] build: Only enable dynamic loading of mpg123 and unmo3 if the library actually isnt linked directly. (broke in r6002)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6003 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/BuildSettings.h
@@ -247,10 +247,14 @@
 #define NO_DMO
 #define NO_PLUGINS
 #if MPT_OS_WINDOWS || defined(MPT_WITH_LTDL) || defined(MPT_WIH_DL)
+#if !defined(MPT_WITH_MPG123)
 #define MPT_ENABLE_MPG123_DYNBIND
+#endif
 //#if !(defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)) || defined(MPT_WITH_MEDIAFOUNDATION)) || !((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
 #if !(defined(MPT_WITH_MPG123)) || !((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))
+#if !defined(MPT_WITH_UNMO3)
 #define MPT_ENABLE_UNMO3_DYNBIND
+#endif
 #endif
 #endif // MPT_OS_WINDOWS || defined(MPT_WITH_LTDL) || defined(MPT_WIH_DL)
 #if ((defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)) || defined(MPT_WITH_MEDIAFOUNDATION)) && ((defined(MPT_WITH_OGG) && defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)) || defined(MPT_WITH_STBVORBIS))