29cd11dea52d99b2ef44da2e098ac14f1ef263cf
Jörn Heusipp <manx@openmpt.org>
2016-04-20 12:27:53 +0000

    [Fix] build: Set "-s PRECISE_F32=1" for emscripten builds. Emscripten otherwise miscompiles std::pow(float,float) and ::powf(float,float) resulting in a 1.5% error. As we do not use floating point extensively in performance critical paths, this should not hurt performance much for us (I did not verify this). Correctness is vastly more important here. Note that both stb_vorbis and minimp3 use pow internally, so they might also be affected by this problem and fixed by this commit. Emscripten bug <https://github.com/kripken/emscripten/issues/4251>.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6276 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/make/config-emscripten-old.mk
+++ build/make/config-emscripten-old.mk
@@ -5,9 +5,9 @@ LD  = em++
 AR  = emar
 
 CPPFLAGS += 
-CXXFLAGS += -std=c++11 -fPIC -O3 -ffast-math 
-CFLAGS   += -std=c99   -fPIC -O3 -ffast-math -fno-strict-aliasing 
-LDFLAGS  += -O3 -s DISABLE_EXCEPTION_CATCHING=0
+CXXFLAGS += -std=c++11 -fPIC -O3 -s PRECISE_F32=1 -ffast-math 
+CFLAGS   += -std=c99   -fPIC -O3 -s PRECISE_F32=1 -ffast-math -fno-strict-aliasing 
+LDFLAGS  += -O3 -s DISABLE_EXCEPTION_CATCHING=0 -s PRECISE_F32=1
 LDLIBS   += 
 ARFLAGS  := rcs
 
