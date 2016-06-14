e3ed6ee7516c5189ac5123d5b12815f151f143c4
Jörn Heusipp <manx@openmpt.org>
2016-06-14 11:53:02 +0000

    [Fix] There are no threads on Emscripten.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6495 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/BuildSettings.h
+++ common/BuildSettings.h
@@ -518,6 +518,10 @@
 #define MPT_ENABLE_THREADSAFE // Always enable thread-safe code in tracker and libopenmpt on windows or with a C++11 compiler (i.e. guard globals with mutexes). (i.e. do not reply on or require pthreads as fallback)
 #endif
 
+#if MPT_OS_EMSCRIPTEN && defined(MPT_ENABLE_THREADSAFE)
+#undef MPT_ENABLE_THREADSAFE // no threading on Emscripten
+#endif
+
 #if defined(MODPLUG_TRACKER) && !defined(MPT_ENABLE_THREAD)
 #define MPT_ENABLE_THREAD // Tracker requires threads
 #endif
