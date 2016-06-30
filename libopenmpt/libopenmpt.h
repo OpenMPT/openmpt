7fbfc9a1cd72296b9406074cd3542cf4a2f4a9ff
Jörn Heusipp <manx@openmpt.org>
2016-06-30 11:44:45 +0000

    [Doc] libopenmpt: Document subsong behaviour in case select_subsong() is never called as implementation defined and subject to change. [Doc] libopenmpt: Refine explanation of threshold values for file header probing.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6558 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt.h
+++ libopenmpt/libopenmpt.h
@@ -282,7 +282,7 @@ LIBOPENMPT_API void openmpt_log_func_silent( const char * message, void * user )
  * \param logfunc Logging function where warning and errors are written.
  * \param user Logging function user context.
  * \return Probability between 0.0 and 1.0.
- * \remarks openmpt_could_open_propability() can return any value between 0.0 and 1.0. Only 0.0 and 1.0 are definitive answers, all values in between are just estimates. In general, any return value >0.0 means that you should try loading the file, and any value below 1.0 means that loading may fail. If you want a threshold in between, use >=0.5.
+ * \remarks openmpt_could_open_propability() can return any value between 0.0 and 1.0. Only 0.0 and 1.0 are definitive answers, all values in between are just estimates. In general, any return value >0.0 means that you should try loading the file, and any value below 1.0 means that loading may fail. If you want a threshold above which you an be reasonably ure that libopenmpt will be able to load the file, use >=0.5. If you see the need for a threshold below which you could reasonably outright reject a file, use <0.25 (Note: Such a threshold for rejecting on the lower end is not recommended, but may be required for better integration into some other framework's probe scoring.).
  * \remarks openmpt_could_open_propability() expects the complete file data to be eventually available to it, even if it is asked to just parse the header. Verification will be unreliable (both false positives and false negatives), if you pretend that the file is just some few bytes of initial data threshold in size. In order to really just access the first bytes of a file, check in your callback functions whether data or seeking is requested beyond your initial data threshold, and in that case, return an error. openmpt_could_open_propability() will treat this as any other I/O error and return 0.0. You must not expect the correct result in this case. You instead must remember that it asked for more data than you currently want to provide to it and treat this situation as if openmpt_could_open_propability() returned 0.5.
  * \sa \ref libopenmpt_c_fileio
  * \sa openmpt_stream_callbacks
@@ -388,6 +388,7 @@ LIBOPENMPT_API void openmpt_module_destroy( openmpt_module * mod );
  * \param subsong Index of the sub-song. -1 plays all sub-songs consecutively.
  * \return 1 on success, 0 on failure.
  * \sa openmpt_module_get_num_subsongs, openmpt_module_get_subsong_names
+ * \remarks Whether subsong -1 (all subsongs consecutively), subsong 0 or some other subsong is selected by default, is an implementation detail and subject to change. If you do not want to care about subsongs, it is recommended to just not call openmpt_module_select_subsong() at all.
  */
 LIBOPENMPT_API int openmpt_module_select_subsong( openmpt_module * mod, int32_t subsong );
 /*! \brief Set Repeat Count
