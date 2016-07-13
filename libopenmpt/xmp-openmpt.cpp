8a0b75f529e29935cdd3b2450c4692f89e2797b0
Jörn Heusipp <manx@openmpt.org>
2016-07-13 07:44:09 +0000

    [Fix] in_openmpt: Fix VS2008 build. [Fix] xmp-openmpt: Fix VS2008 build.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6623 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/xmp-openmpt.cpp
+++ libopenmpt/xmp-openmpt.cpp
@@ -42,6 +42,18 @@
 
 #include "libopenmpt.hpp"
 #include "libopenmpt_ext.hpp"
+#ifdef LIBOPENMPT_QUIRK_NO_CSTDINT
+namespace std {
+typedef ::int8_t   int8_t;
+typedef ::int16_t  int16_t;
+typedef ::int32_t  int32_t;
+typedef ::int64_t  int64_t;
+typedef ::uint8_t  uint8_t; 
+typedef ::uint16_t uint16_t; 
+typedef ::uint32_t uint32_t;
+typedef ::uint64_t uint64_t;
+}
+#endif
 
 #include "libopenmpt_plugin_gui.hpp"
 
