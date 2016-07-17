OpenMPT/openmpt

commit 58c40250261bf9cc7c861cdd3c50f26967580f82

[Fix] XMPlay plugin: Start counting sample, instrument and channel names at 1. [Fix] XMPlay plugin: Only show sample and instrument names on the sample page to be consistent with what XMPlay does.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6514 56274372-70c3-4bfc-bfc3-4c3a0b034d27

libopenmpt/xmp-openmpt.cpp
@@ -1155,7 +1155,7 @@ static void WINAPI openmpt_GetMessage( char * buf ) {
 		clear_xmplay_string( buf );
 		return;
 	}
-	write_xmplay_string( buf, convert_to_native( sanitize_xmplay_multiline_string( string_replace( self->mod->get_metadata("message"), "\n", "\r" ) ) ) );
+	write_xmplay_string( buf, convert_to_native( sanitize_xmplay_multiline_string( string_replace( self->mod->get_metadata("message_raw"), "\n", "\r" ) ) ) );
 }
 
 // Seek to a position (in granularity units)
@@ -1238,7 +1238,7 @@ static DWORD WINAPI openmpt_Process( float * dstbuf, DWORD count ) {
 	return frames_rendered * self->num_channels;
 }
 
-static void add_names( std::ostream & str, const std::string & title, const std::vector<std::string> & names ) {
+static void add_names( std::ostream & str, const std::string & title, const std::vector<std::string> & names, int display_offset ) {
 	if ( names.size() > 0 ) {
 		bool valid = false;
 		for ( std::size_t i = 0; i < names.size(); i++ ) {
@@ -1251,7 +1251,7 @@ static void add_names( std::ostream & str, const std::string & title, const std:
 		}
 		str << title << " names:" << "\r";
 		for ( std::size_t i = 0; i < names.size(); i++ ) {
-			str << std::setfill('0') << std::setw(2) << i << std::setw(0) << "\t" << convert_to_native( names[i] ) << "\r";
+			str << std::setfill('0') << std::setw(2) << i << std::setw(0) << "\t" << convert_to_native( names[display_offset + i] ) << "\r";
 		}
 		str << "\r";
 	}
@@ -1263,11 +1263,11 @@ static void WINAPI openmpt_GetSamples( char * buf ) {
 		return;
 	}
 	std::ostringstream str;
-	add_names( str, "instrument", self->mod->get_instrument_names() );
-	add_names( str, "sample", self->mod->get_sample_names() );
-	add_names( str, "channel", self->mod->get_channel_names() );
-	add_names( str, "order", self->mod->get_order_names() );
-	add_names( str, "pattern", self->mod->get_pattern_names() );
+	add_names( str, "instrument", self->mod->get_instrument_names(), 1 );
+	add_names( str, "sample", self->mod->get_sample_names(), 1 );
+	add_names( str, "channel", self->mod->get_channel_names(), 1 );
+	add_names( str, "order", self->mod->get_order_names(), 0 );
+	add_names( str, "pattern", self->mod->get_pattern_names(), 0 );
 	write_xmplay_string( buf, str.str() );
 }
 

commit 59a9ab27901849d68b4939d131ae929ae187b74d

[Fix] XMPlay Plugin: Sample text view broke in r6514. [Mod] XMPlay Plugin: Report module type in upper-case to be consistent with XMPlay.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6644 56274372-70c3-4bfc-bfc3-4c3a0b034d27

libopenmpt/xmp-openmpt.cpp
@@ -729,7 +729,10 @@ static void write_xmplay_tags( char * tags[8], const openmpt::module & mod ) {
 	write_xmplay_tag( &tags[4], convert_to_native( mod.get_metadata("xmplay-tracknumber") ) ); // todo, libopenmpt does not support that
 	write_xmplay_tag( &tags[5], convert_to_native( mod.get_metadata("xmplay-genre") ) ); // todo, libopenmpt does not support that
 	write_xmplay_tag( &tags[6], convert_to_native( mod.get_metadata("message") ) );
-	write_xmplay_tag( &tags[7], convert_to_native( mod.get_metadata("type") ) );
+	// Be consistent with XMPlay and report file format in upper-case.
+	std::string type = mod.get_metadata("type");
+	std::transform( type.begin(), type.end(), type.begin(), ::toupper );
+	write_xmplay_tag( &tags[7], convert_to_native( type ) );
 }
 
 static void clear_xmlpay_tags( char * tags[8] ) {
@@ -1265,9 +1268,9 @@ static void add_names( std::ostream & str, const std::string & title, const std:
 		if ( !valid ) {
 			return;
 		}
-		str << title << " names:" << "\r";
+		str << title << " Names:" << "\r";
 		for ( std::size_t i = 0; i < names.size(); i++ ) {
-			str << std::setfill('0') << std::setw(2) << i << std::setw(0) << "\t" << convert_to_native( names[display_offset + i] ) << "\r";
+			str << std::setfill('0') << std::setw(2) << (display_offset + i) << std::setw(0) << "\t" << convert_to_native( names[i] ) << "\r";
 		}
 		str << "\r";
 	}
@@ -1279,11 +1282,11 @@ static void WINAPI openmpt_GetSamples( char * buf ) {
 		return;
 	}
 	std::ostringstream str;
-	add_names( str, "instrument", self->mod->get_instrument_names(), 1 );
-	add_names( str, "sample", self->mod->get_sample_names(), 1 );
-	add_names( str, "channel", self->mod->get_channel_names(), 1 );
-	add_names( str, "order", self->mod->get_order_names(), 0 );
-	add_names( str, "pattern", self->mod->get_pattern_names(), 0 );
+	add_names( str, "Instrument", self->mod->get_instrument_names(), 1 );
+	add_names( str, "Sample", self->mod->get_sample_names(), 1 );
+	add_names( str, "Channel", self->mod->get_channel_names(), 1 );
+	add_names( str, "Order", self->mod->get_order_names(), 0 );
+	add_names( str, "Pattern", self->mod->get_pattern_names(), 0 );
 	write_xmplay_string( buf, str.str() );
 }
 