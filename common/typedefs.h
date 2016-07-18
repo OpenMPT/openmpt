4865ecfc70e72f4a57de1e8a93b75c87485854c3
Jörn Heusipp <manx@openmpt.org>
2016-07-18 17:17:05 +0000

    [Ref] Silence warnings with older clang.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6658 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/typedefs.h
+++ common/typedefs.h
@@ -294,6 +294,7 @@ template <typename T, typename T1, typename T2, typename T3, typename T4> inline
 #if MPT_COMPILER_CLANG
 #define MPT_MAYBE_CONSTANT_IF(x) \
   _Pragma("clang diagnostic push") \
+  _Pragma("clang diagnostic ignored \"-Wunknown-pragmas\"") \
   _Pragma("clang diagnostic ignored \"-Wtype-limits\"") \
   _Pragma("clang diagnostic ignored \"-Wtautological-constant-out-of-range-compare\"") \
   if(x) \
