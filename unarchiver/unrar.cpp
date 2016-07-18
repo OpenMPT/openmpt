ff72e74d20c6a05f5c97e64189e5df27b66926d6
Jörn Heusipp <manx@openmpt.org>
2016-07-18 11:17:45 +0000

    [Ref] Unarchiver: Replace MPT_SHARED_PTR with mpt::scoped_ptr.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6652 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- unarchiver/unrar.cpp
+++ unarchiver/unrar.cpp
@@ -109,7 +109,7 @@ CRarArchive::CRarArchive(FileReader &file)
 	}
 	inFile.Rewind();
 
-	diskFile = MPT_SHARED_PTR<OnDiskFileWrapper>(new OnDiskFileWrapper(inFile));
+	diskFile = mpt::scoped_ptr<OnDiskFileWrapper>::initializer(new OnDiskFileWrapper(inFile));
 	if(!diskFile->IsValid())
 	{
 		return;
