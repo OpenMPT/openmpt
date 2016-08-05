7ee6351e5a1fd15d76c843df2c4da16f4b92eab6
Johannes Schultz <sagamusix@openmpt.org>
2016-08-05 11:22:33 +0000

    [Var] unrar: Update to v5.4.4.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6741 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
 #define RARVER_MINOR    40
-#define RARVER_BETA      2
-#define RARVER_DAY      30
-#define RARVER_MONTH     5
+#define RARVER_BETA      4
+#define RARVER_DAY      25
+#define RARVER_MONTH     7
 #define RARVER_YEAR   2016
