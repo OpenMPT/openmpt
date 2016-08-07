213952a9b051b8b38080bf343cc03fbc9bb47bf6
Jörn Heusipp <manx@openmpt.org>
2016-08-07 17:50:15 +0000

    [Mod] OpenMPT: Version is now 1.27.00.01
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6776 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/versionNumber.h
+++ common/versionNumber.h
@@ -17,9 +17,9 @@ OPENMPT_NAMESPACE_BEGIN
 
 //Version definitions. The only thing that needs to be changed when changing version number.
 #define VER_MAJORMAJOR          1
-#define VER_MAJOR               26
-#define VER_MINOR               04
-#define VER_MINORMINOR          00
+#define VER_MAJOR               27
+#define VER_MINOR               00
+#define VER_MINORMINOR          01
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)
