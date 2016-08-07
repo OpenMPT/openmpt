OpenMPT/openmpt

commit 62c71b1f1fc0bddd4e5df529b3d5a3cbbf5f5c0f

[Fix] Avoid non-standard extensions for structure packing. A new type, packed<value_type, endianness> is added to create integers with undefined endianness and any alignment without resorting to non-standard extensions (https://bugs.openmpt.org/view.php?id=572). This makes the code more readable in many cases, is less error-prone and shorter than the previous ConvertEndianness() dance, and should work on more (unknown) platforms. [Ref] Add FileReader::ReadSizedString<packed<?,?>...>, which reads a string with a prepended length field of defined endianness. This unifies the handling of such strings across several loaders.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6783 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptUUID.cpp
@@ -398,14 +398,6 @@ void UUID::MakeRFC4122(uint8 version)
 	Data3 |= static_cast<uint16>(Mm) << 8;
 }
 
-void UUID::ConvertEndianness()
-{
-	SwapBytesBE(Data1);
-	SwapBytesBE(Data2);
-	SwapBytesBE(Data3);
-	SwapBytesBE(Data4);
-}
-
 UUID::UUID()
 	: Data1(0)
 	, Data2(0)

common/mptUUID.h
@@ -90,8 +90,6 @@ private:
 	uint8 Nn() const;
 	void MakeRFC4122(uint8 version);
 public:
-	void ConvertEndianness();
-public:
 #if MPT_OS_WINDOWS && defined(MODPLUG_TRACKER)
 	explicit UUID(::UUID uuid);
 	operator ::UUID () const;

commit 664a49ee6ea2139256a322888b6d690dd835cab5

[Fix] mptUUID: Fix binary representation (broken since r6783).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6789 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptUUID.cpp
@@ -399,21 +399,19 @@ void UUID::MakeRFC4122(uint8 version)
 }
 
 UUID::UUID()
-	: Data1(0)
-	, Data2(0)
-	, Data3(0)
-	, Data4(0)
 {
-	return;
+	Data1 = 0;
+	Data2 = 0;
+	Data3 = 0;
+	Data4 = 0;
 }
 
 UUID::UUID(uint32 Data1, uint16 Data2, uint16 Data3, uint64 Data4)
-	: Data1(Data1)
-	, Data2(Data2)
-	, Data3(Data3)
-	, Data4(Data4)
 {
-	return;
+	this->Data1 = Data1;
+	this->Data2 = Data2;
+	this->Data3 = Data3;
+	this->Data4 = Data4;
 }
 
 bool operator==(const mpt::UUID & a, const mpt::UUID & b)
@@ -464,16 +462,16 @@ UUID UUID::FromString(const mpt::ustring &str)
 mpt::ustring UUID::ToUString() const
 {
 	return mpt::ustring()
-		+ mpt::ufmt::hex0<8>(Data1)
+		+ mpt::ufmt::hex0<8>(GetData1())
 		+ MPT_USTRING("-")
-		+ mpt::ufmt::hex0<4>(Data2)
+		+ mpt::ufmt::hex0<4>(GetData2())
 		+ MPT_USTRING("-")
-		+ mpt::ufmt::hex0<4>(Data3)
+		+ mpt::ufmt::hex0<4>(GetData3())
 		+ MPT_USTRING("-")
-		+ mpt::ufmt::hex0<4>(static_cast<uint16>(Data4 >> 48))
+		+ mpt::ufmt::hex0<4>(static_cast<uint16>(GetData4() >> 48))
 		+ MPT_USTRING("-")
-		+ mpt::ufmt::hex0<4>(static_cast<uint16>(Data4 >> 32))
-		+ mpt::ufmt::hex0<8>(static_cast<uint32>(Data4 >>  0))
+		+ mpt::ufmt::hex0<4>(static_cast<uint16>(GetData4() >> 32))
+		+ mpt::ufmt::hex0<8>(static_cast<uint32>(GetData4() >>  0))
 		;
 }
 

common/mptUUID.h
@@ -11,6 +11,8 @@
 #pragma once
 
 
+#include "Endianness.h"
+
 #if MPT_OS_WINDOWS
 #if defined(MODPLUG_TRACKER) || !defined(NO_DMO)
 #include <guiddef.h>
@@ -68,10 +70,10 @@ namespace mpt {
 struct UUID
 {
 private:
-	uint32 Data1;
-	uint16 Data2;
-	uint16 Data3;
-	uint64 Data4;
+	uint32be Data1;
+	uint16be Data2;
+	uint16be Data3;
+	uint64be Data4;
 public:
 	uint32 GetData1() const;
 	uint16 GetData2() const;