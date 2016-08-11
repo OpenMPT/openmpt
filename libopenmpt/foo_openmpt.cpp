0c55137b387096e9fb7fdba9f0da2802a375b5dc
Jörn Heusipp <manx@openmpt.org>
2016-08-11 11:25:26 +0000

    [Ref] foo_openmpt: Just use the libopenmpt version as openmpt123 version instead of duplicating the numbers.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6851 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/foo_openmpt.cpp
+++ libopenmpt/foo_openmpt.cpp
@@ -15,7 +15,7 @@
 // Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
 // Note that it is possible to declare multiple components within one DLL, but it's strongly recommended to keep only one declaration per DLL.
 // As for 1.1, the version numbers are used by the component update finder to find updates; for that to work, you must have ONLY ONE declaration per DLL. If there are multiple declarations, the component is assumed to be outdated and a version number of "0" is assumed, to overwrite the component with whatever is currently on the site assuming that it comes with proper version numbers.
-DECLARE_COMPONENT_VERSION("OpenMPT component","0.2","libopenmpt based module file player");
+DECLARE_COMPONENT_VERSION("OpenMPT component", OPENMPT_API_VERSION_STRING ,"libopenmpt based module file player");
 
 
 
