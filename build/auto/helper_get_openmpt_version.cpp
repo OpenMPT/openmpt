7895be459805441f9ceee3a54f04b1ee94835bad
Jörn Heusipp <manx@openmpt.org>
2016-08-12 08:14:07 +0000

    [Imp] build: Extend helper_get_openmpt_version.cpp to also provide the libopenmpt version.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6861 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/helper_get_openmpt_version.cpp
+++ build/auto/helper_get_openmpt_version.cpp
@@ -1,12 +1,46 @@
 
 #include <iostream>
+#incluse <string>
+#include <vector>
 
 #define OPENMPT_NAMESPACE_BEGIN
 #define OPENMPT_NAMESPACE_END
-
 #include "common/versionNumber.h"
 
-int main() {
- std::cout << MPT_VERSION_STR << std::endl;
- return 0;
+#include "libopenmpt/libopenmpt_version.h"
+
+int main( int argc, char * argv [] ) {
+	std::vector<std::string> args = std::vector<std::string>( argv, argv + argc );
+	for ( std::vector<std::string>::const_iterator i = args.begin(); i != args.end(); ++i ) {
+		if ( i == args.begin() ) {
+			// skip program name
+			continue;
+		}
+		std::string arg = *i;
+		if ( arg == "" ) {
+			std::cout << "unknown" << std::endl;
+
+		} else ( arg == "openmpt" ) {
+			std::cout << MPT_VERSION_STR << std::endl;
+
+#ifdef OPENMPT_API_VERSION_MAJOR
+		} else ( arg == "libopenmpt-version-major" ) {
+			std::cout << OPENMPT_API_VERSION_MAJOR << std::endl;
+#endif
+
+#ifdef OPENMPT_API_VERSION_MINOR
+		} else ( arg == "libopenmpt-version-minor" ) {
+			std::cout << OPENMPT_API_VERSION_MINOR << std::endl;
+#endif
+
+#ifdef OPENMPT_API_VERSION_STRING
+		} else ( arg == "libopenmpt-version-string" ) {
+			std::cout << OPENMPT_API_VERSION_STRING << std::endl;
+#endif
+
+		} else {
+			std::cout << "unknown" << std::endl;			
+		}
+	}
+	return 0;
 }
