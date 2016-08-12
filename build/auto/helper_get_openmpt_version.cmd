7c3530d03a9b69a036fb1262436e9105a8d13a94
Jörn Heusipp <manx@openmpt.org>
2016-08-12 08:14:07 +0000

    [Imp] build: Extend helper_get_openmpt_version.cpp to also provide the libopenmpt version.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6861 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/helper_get_openmpt_version.cmd
+++ build/auto/helper_get_openmpt_version.cmd
@@ -4,8 +4,20 @@ cd build\auto
 cl /I..\.. /EHsc helper_get_openmpt_version.cpp
 cd ..\..
 
-build\auto\helper_get_openmpt_version.exe > openmpt_version.txt
+build\auto\helper_get_openmpt_version.exe openmpt > openmpt_version.txt
 set /p OPENMPT_VERSION=<openmpt_version.txt
+del /f openmpt_version.txt
+
+build\auto\helper_get_openmpt_version.exe libopenmpt-version-major > openmpt_version.txt
+set /p LIBOPENMPT_VERSION_MAJOR=<openmpt_version.txt
+del /f openmpt_version.txt
+
+build\auto\helper_get_openmpt_version.exe libopenmpt-version-minor > openmpt_version.txt
+set /p LIBOPENMPT_VERSION_MINOR=<openmpt_version.txt
+del /f openmpt_version.txt
+
+build\auto\helper_get_openmpt_version.exe libopenmpt-version-string > openmpt_version.txt
+set /p LIBOPENMPT_VERSION_STRING=<openmpt_version.txt
 del /f openmpt_version.txt
 
 cd build\auto
