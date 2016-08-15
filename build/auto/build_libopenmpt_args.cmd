30511011999b9b08b9fa6fad1025efc3f3ecdd83
Jörn Heusipp <manx@openmpt.org>
2016-08-15 15:35:34 +0000

    [Fix] build: Support building foo_openmpt with VS > 2010.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6884 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/build_libopenmpt_args.cmd
+++ build/auto/build_libopenmpt_args.cmd
@@ -19,9 +19,31 @@ cd "build\%MPT_VS_WITHTARGET%" || goto error
  if "%MPT_VS_ARCH%" == "Win32" (
   devenv in_openmpt.sln /clean "Release|%MPT_VS_ARCH%" || goto error
   devenv xmp-openmpt.sln /clean "Release|%MPT_VS_ARCH%" || goto error
-  if "%MPT_VS_VER%" == "vs2010" (
+
+  if not "%MPT_VS_VER%" == "vs2008" (
+   if not "%MPT_VS_VER%" == "vs2010" (
+ 	  cd include\foobar2000sdk || goto error
+ 	  svn revert -R --non-interactive . || goto error
+    devenv pfc\pfc.vcxproj /Upgrade || goto error
+    devenv foobar2000\SDK\foobar2000_SDK.vcxproj /Upgrade || goto error
+    devenv foobar2000\helpers\foobar2000_sdk_helpers.vcxproj /Upgrade || goto error
+    devenv foobar2000\foobar2000_component_client\foobar2000_component_client.vcxproj /Upgrade || goto error
+ 	  cd ..\.. || goto error
+   )
+  )
+
+  if not "%MPT_VS_VER%" == "vs2008" (
    devenv foo_openmpt.sln /clean "Release|%MPT_VS_ARCH%" || goto error
   )
+
+  if not "%MPT_VS_VER%" == "vs2008" (
+   if not "%MPT_VS_VER%" == "vs2010" (
+    cd include\foobar2000sdk || goto error
+    svn revert -R --non-interactive . || goto error
+ 	  cd ..\.. || goto error
+   )
+  )
+	
  )
  devenv libopenmpt.sln /clean "ReleaseShared|%MPT_VS_ARCH%" || goto error
  
@@ -30,9 +52,31 @@ cd "build\%MPT_VS_WITHTARGET%" || goto error
  if "%MPT_VS_ARCH%" == "Win32" (
   devenv in_openmpt.sln /build "Release|%MPT_VS_ARCH%" || goto error
   devenv xmp-openmpt.sln /build "Release|%MPT_VS_ARCH%" || goto error
-  if "%MPT_VS_VER%" == "vs2010" (
+
+  if not "%MPT_VS_VER%" == "vs2008" (
+   if not "%MPT_VS_VER%" == "vs2010" (
+ 	  cd include\foobar2000sdk || goto error
+ 	  svn revert -R --non-interactive . || goto error
+    devenv pfc\pfc.vcxproj /Upgrade || goto error
+    devenv foobar2000\SDK\foobar2000_SDK.vcxproj /Upgrade || goto error
+    devenv foobar2000\helpers\foobar2000_sdk_helpers.vcxproj /Upgrade || goto error
+    devenv foobar2000\foobar2000_component_client\foobar2000_component_client.vcxproj /Upgrade || goto error
+ 	  cd ..\.. || goto error
+   )
+  )
+
+  if not "%MPT_VS_VER%" == "vs2008" (
    devenv foo_openmpt.sln /build "Release|%MPT_VS_ARCH%" || goto error
   )
+
+  if not "%MPT_VS_VER%" == "vs2008" (
+   if not "%MPT_VS_VER%" == "vs2010" (
+    cd include\foobar2000sdk || goto error
+    svn revert -R --non-interactive . || goto error
+ 	  cd ..\.. || goto error
+   )
+  )
+
  )
  devenv libopenmpt.sln /build "ReleaseShared|%MPT_VS_ARCH%" || goto error
 
