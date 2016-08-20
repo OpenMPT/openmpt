ef41b7b410c09a623942d64b7af1a06235034b87
Jörn Heusipp <manx@openmpt.org>
2016-08-20 11:23:28 +0000

    [Imp] build: Support svn version information in Android.mk passed via make command line arguments. [Imp] build: Provide svn version information via android automated build script.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6937 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/build_libopenmpt_android.sh
+++ build/auto/build_libopenmpt_android.sh
@@ -7,6 +7,17 @@ MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${MY_DIR}"
 cd ../..
 
+SVN_INFO=$( svn info . > /dev/null 2>&1 ; echo $? )
+if [ $SVN_INFO -eq 0 ] ; then
+	MPT_SVNURL="$( svn info --xml | grep '^<url>' | sed 's/<url>//g' | sed 's/<\/url>//g' )"
+	MPT_SVNVERSION="$( svnversion -n . | tr ':' '-' )"
+	MPT_SVNDATE="$( svn info --xml | grep '^<date>' | sed 's/<date>//g' | sed 's/<\/date>//g' )"
+else
+	MPT_SVNURL=
+	MPT_SVNVERSION=
+	MPT_SVNDATE=
+fi
+
 cd build/android_ndk/
 
 rm -f jni
@@ -18,6 +29,5 @@ rm -f Android.mk
 ln -s build/android_ndk/Android.mk Android.mk
 cd ..
 
-ndk-build clean
-ndk-build
-
+ndk-build MPT_SVNURL="$MPT_SVNURL" MPT_SVNVERSION="$MPT_SVNVERSION" MPT_SVNDATE="$MPT_SVNDATE" clean
+ndk-build MPT_SVNURL="$MPT_SVNURL" MPT_SVNVERSION="$MPT_SVNVERSION" MPT_SVNDATE="$MPT_SVNDATE"
