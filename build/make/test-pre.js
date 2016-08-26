d77dc2f8a6206e4f4ca71c9323e270ff0d472a8f
Jörn Heusipp <manx@openmpt.org>
2016-08-26 10:01:22 +0000

    [Fix] build: Mount libopenmpt/ directory for emscripten test suite in order to be able to parse libopenmpt_version.mk.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6979 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/make/test-pre.js
+++ build/make/test-pre.js
@@ -3,5 +3,7 @@ var Module = {
  'preInit': function(text) {
   FS.mkdir('/test');
   FS.mount(NODEFS, {'root': '../test/'}, '/test');
+  FS.mkdir('/libopenmpt');
+  FS.mount(NODEFS, {'root': '../libopenmpt/'}, '/libopenmpt');
  }
 };
