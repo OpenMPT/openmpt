f1a7862c64aaa41abe81e61611aaba7e3d4e6621
Jörn Heusipp <manx@openmpt.org>
2016-08-26 11:46:57 +0000

    [Ref] build: Fix relative path of work aropund for version resource include path.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@6982 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake/mpt-in_openmpt.lua
+++ build/premake/mpt-in_openmpt.lua
@@ -44,7 +44,7 @@
 		resincludedirs {
 			"$(IntDir)/svn_version",
 			"../../build/svn_version",
-			"$(ProjDir)/../build/svn_version",
+			"$(ProjDir)/../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",
