97f23c74dbe66d42623b988db0135b354353287a
Johannes Schultz <sagamusix@openmpt.org>
2016-09-04 23:43:23 +0000

    [Fix] r7034 introduced a couple of heap overflows.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7054 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/View_smp.cpp
+++ mptrack/View_smp.cpp
@@ -2635,9 +2635,9 @@ void CViewSample::OnDropFiles(HDROP hDropInfo)
 	CMainFrame::GetMainFrame()->SetForegroundWindow();
 	for(UINT f = 0; f < nFiles; f++)
 	{
-		UINT size = ::DragQueryFileW(hDropInfo, f, nullptr, 0);
+		UINT size = ::DragQueryFileW(hDropInfo, f, nullptr, 0) + 1;
 		std::vector<WCHAR> fileName(size, L'\0');
-		if(::DragQueryFileW(hDropInfo, f, fileName.data(), size + 1))
+		if(::DragQueryFileW(hDropInfo, f, fileName.data(), size))
 		{
 			const mpt::PathString file = mpt::PathString::FromNative(fileName.data());
 			if(SendCtrlMessage(CTRLMSG_SMP_OPENFILE, (LPARAM)&file) && f < nFiles - 1)
