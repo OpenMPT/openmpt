920365ff9b2c0bc4717a740b310d488c958443c4
Jörn Heusipp <manx@openmpt.org>
2016-09-08 13:10:58 +0000

    [Fix] Disable constexpr in all MSVC versions for now, due to miscompilation of CModSpecification.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7074 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/CompilerDetect.h
+++ common/CompilerDetect.h
@@ -190,7 +190,7 @@
 
 
 // C++11 constexpr
-#if MPT_GCC_BEFORE(4,7,0) || MPT_CLANG_BEFORE(3,1,0) || MPT_MSVC_BEFORE(2015,0)
+#if MPT_GCC_BEFORE(4,7,0) || MPT_CLANG_BEFORE(3,1,0) || MPT_COMPILER_MSVC
 // constexpr is broken in GCC 4.6
 #define MPT_COMPILER_HAS_CONSTEXPR11 0
 #endif
