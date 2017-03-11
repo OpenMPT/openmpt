03b445e63cf65bfbc26fb42955e60072bc453aab
Jörn Heusipp <manx@openmpt.org>
2017-03-11 10:44:37 +0000

    [New] build: Add VS2017 projects files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7767 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/regenerate_vs_projects.sh
+++ build/regenerate_vs_projects.sh
@@ -63,6 +63,18 @@ ${PREMAKE} --group=OpenMPT-VSTi vs2015
 ${PREMAKE} --group=OpenMPT vs2015
 ${PREMAKE} --group=all-externals vs2015
 
+${PREMAKE} --group=libopenmpt_test vs2017
+${PREMAKE} --group=foo_openmpt vs2017
+${PREMAKE} --group=in_openmpt vs2017
+${PREMAKE} --group=xmp-openmpt vs2017
+${PREMAKE} --group=libopenmpt-small vs2017
+${PREMAKE} --group=libopenmpt vs2017
+${PREMAKE} --group=openmpt123 vs2017
+${PREMAKE} --group=PluginBridge vs2017
+${PREMAKE} --group=OpenMPT-VSTi vs2017
+${PREMAKE} --group=OpenMPT vs2017
+${PREMAKE} --group=all-externals vs2017
+
 ${PREMAKE} --group=libopenmpt_test vs2010 --xp
 ${PREMAKE} --group=foo_openmpt vs2010 --xp
 ${PREMAKE} --group=in_openmpt vs2010 --xp
@@ -110,6 +122,18 @@ ${PREMAKE} --group=PluginBridge vs2015 --xp
 ${PREMAKE} --group=OpenMPT-VSTi vs2015 --xp
 ${PREMAKE} --group=OpenMPT vs2015 --xp
 ${PREMAKE} --group=all-externals vs2015 --xp
+
+${PREMAKE} --group=libopenmpt_test vs2017 --xp
+${PREMAKE} --group=foo_openmpt vs2017 --xp
+${PREMAKE} --group=in_openmpt vs2017 --xp
+${PREMAKE} --group=xmp-openmpt vs2017 --xp
+${PREMAKE} --group=libopenmpt-small vs2017 --xp
+${PREMAKE} --group=libopenmpt vs2017 --xp
+${PREMAKE} --group=openmpt123 vs2017 --xp
+${PREMAKE} --group=PluginBridge vs2017 --xp
+${PREMAKE} --group=OpenMPT-VSTi vs2017 --xp
+${PREMAKE} --group=OpenMPT vs2017 --xp
+${PREMAKE} --group=all-externals vs2017 --xp
 
 ${PREMAKE} postprocess
 
