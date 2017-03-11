47326e6507f07ee5b40b15190e048187ad320b8b
Jörn Heusipp <manx@openmpt.org>
2017-03-11 10:44:37 +0000

    [New] build: Add VS2017 projects files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7767 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/regenerate_vs_projects.cmd
+++ build/regenerate_vs_projects.cmd
@@ -63,6 +63,18 @@ rem %PREMAKE% --group=OpenMPT vs2012 || goto err
 %PREMAKE% --group=OpenMPT vs2015 || goto err
 %PREMAKE% --group=all-externals vs2015 || goto err
 
+%PREMAKE% --group=libopenmpt_test vs2017 || goto err
+%PREMAKE% --group=foo_openmpt vs2017 || goto err
+%PREMAKE% --group=in_openmpt vs2017 || goto err
+%PREMAKE% --group=xmp-openmpt vs2017 || goto err
+%PREMAKE% --group=libopenmpt-small vs2017 || goto err
+%PREMAKE% --group=libopenmpt vs2017 || goto err
+%PREMAKE% --group=openmpt123 vs2017 || goto err
+%PREMAKE% --group=PluginBridge vs2017 || goto err
+%PREMAKE% --group=OpenMPT-VSTi vs2017 || goto err
+%PREMAKE% --group=OpenMPT vs2017 || goto err
+%PREMAKE% --group=all-externals vs2017 || goto err
+
 %PREMAKE% --group=libopenmpt_test vs2010 --xp || goto err
 %PREMAKE% --group=foo_openmpt vs2010 --xp || goto err
 %PREMAKE% --group=in_openmpt vs2010 --xp || goto err
@@ -110,6 +122,18 @@ rem %PREMAKE% --group=OpenMPT vs2012 --xp || goto err
 %PREMAKE% --group=OpenMPT-VSTi vs2015 --xp || goto err
 %PREMAKE% --group=OpenMPT vs2015 --xp || goto err
 %PREMAKE% --group=all-externals vs2015 --xp || goto err
+
+%PREMAKE% --group=libopenmpt_test vs2017 --xp || goto err
+%PREMAKE% --group=foo_openmpt vs2017 --xp || goto err
+%PREMAKE% --group=in_openmpt vs2017 --xp || goto err
+%PREMAKE% --group=xmp-openmpt vs2017 --xp || goto err
+%PREMAKE% --group=libopenmpt-small vs2017 --xp || goto err
+%PREMAKE% --group=libopenmpt vs2017 --xp || goto err
+%PREMAKE% --group=openmpt123 vs2017 --xp || goto err
+%PREMAKE% --group=PluginBridge vs2017 --xp || goto err
+%PREMAKE% --group=OpenMPT-VSTi vs2017 --xp || goto err
+%PREMAKE% --group=OpenMPT vs2017 --xp || goto err
+%PREMAKE% --group=all-externals vs2017 --xp || goto err
 
 %PREMAKE% postprocess || goto err
 
