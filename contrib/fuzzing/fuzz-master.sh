95cc7f2f36d98b34376c1f543ce2e7bb297601f1
Johannes Schultz <sagamusix@openmpt.org>
2017-03-20 15:15:35 +0000

    [Imp] Allow Startrekker synthesized instrument code to be fuzzed by simply taking the external data from the fuzzed MOD file itself. [Mod] Update fuzzer dictionary.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7827 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- contrib/fuzzing/fuzz-master.sh
+++ contrib/fuzzing/fuzz-master.sh
@@ -4,12 +4,12 @@ cd "${0%/*}"
 
 # Create tmpfs for storing temporary fuzzing data
 mkdir $FUZZING_TEMPDIR
-sudo mount -t tmpfs -o size=150M none $FUZZING_TEMPDIR
+sudo mount -t tmpfs -o size=200M none $FUZZING_TEMPDIR
 rm -rf $FUZZING_TEMPDIR/bin
 mkdir $FUZZING_TEMPDIR/bin
 cp ../../bin/* $FUZZING_TEMPDIR/bin/
 rm $FUZZING_TEMPDIR/bin/libopenmpt.so
-cp ../../bin/libopenmpt.so.0 $FUZZING_TEMPDIR/bin/libopenmpt.so
-cp ../../bin/libopenmpt.so.0 $FUZZING_TEMPDIR/bin/libopenmpt.so.0
+cp ../../bin/libopenmpt.so.1 $FUZZING_TEMPDIR/bin/libopenmpt.so
+cp ../../bin/libopenmpt.so.1 $FUZZING_TEMPDIR/bin/libopenmpt.so.1
 
 LD_LIBRARY_PATH=$FUZZING_TEMPDIR/bin $AFL_BIN -f $FUZZING_TEMPDIR/infile01 -x all_formats.dict -t $FUZZING_TIMEOUT $FUZZING_INPUT -o $FUZZING_FINDINGS_DIR -M fuzzer01 $FUZZING_TEMPDIR/bin/fuzz $FUZZING_TEMPDIR/infile01
