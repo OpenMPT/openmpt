f02fcd5b515982521e56d9728ecd83dfed493d04
Jörn Heusipp <manx@openmpt.org>
2017-03-22 14:20:31 +0000

    [Imp] build: Generate a meaningful replacement for svnversion for packaging when building from git repository on Windows. Instead of the SVN revision number, the unix timestamp of the latest commit is used.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7844 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/helper_get_svnversion.cmd
+++ build/auto/helper_get_svnversion.cmd
@@ -1,9 +1,32 @@
 @echo off
 
-svnversion > svnversion.txt
-set /p %RAWSVNVERSION=<svnversion.txt
-del /f svnversion.txt
-if "%RAWSVNVERSION%" == "Unversioned directory" set RAWSVNVERSION=unknown
-set SVNVERSION=%RAWSVNVERSION::=-%
+set SVNVERSION=unknown
+
+if "x%SVNVERSION%" == "xunknown" (
+	svnversion > svnversion.txt
+	if errorlevel 1 (
+		del /f svnversion.txt
+		set SVNVERSION=unknown
+	) else (
+		set /p %RAWSVNVERSION=<svnversion.txt
+		del /f svnversion.txt
+		if "%RAWSVNVERSION%" == "Unversioned directory" (
+			set SVNVERSION=unknown
+		) else (
+			set SVNVERSION=r%RAWSVNVERSION::=-%
+		)
+	)
+)
+
+if "x%SVNVERSION%" == "xunknown" (
+	git log --format=format:"%ct" -n 1 > gitversion
+	if errorlevel 1 (
+		set SVNVERSION=unknown
+	) else (
+		set /p %RAWGITVERSION=<gitversion.txt
+		del /f gitversion.txt
+		set SVNVERSION=t%RAWGITVERSION%
+	)
+)
 
 echo %SVNVERSION%
