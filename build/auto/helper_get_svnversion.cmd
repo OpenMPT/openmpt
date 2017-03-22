80572c464b4efa074cdc97f3d9c3d86a60c55d65
Jörn Heusipp <manx@openmpt.org>
2017-03-22 12:00:00 +0000

    [Fix] build: Make helper_get_svnversion.cmd return 'unknown' instead of 'Unversioned directory' (which contains a space) in the error case.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7839 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/helper_get_svnversion.cmd
+++ build/auto/helper_get_svnversion.cmd
@@ -3,6 +3,7 @@
 svnversion > svnversion.txt
 set /p %RAWSVNVERSION=<svnversion.txt
 del /f svnversion.txt
+if "%RAWSVNVERSION%" == "Unversioned directory" set RAWSVNVERSION=unknown
 set SVNVERSION=%RAWSVNVERSION::=-%
 
-rem echo %SVNVERSION%
+echo %SVNVERSION%
