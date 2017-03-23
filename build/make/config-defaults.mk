89405378bb3486eb8cc9a76c0accb3d2fb09273b
Jörn Heusipp <manx@openmpt.org>
2017-03-23 13:08:00 +0000

    [Fix] build: Disable soname on OSX.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7851 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/make/config-defaults.mk
+++ build/make/config-defaults.mk
@@ -6,6 +6,7 @@ ifeq ($(HOST_FLAVOUR),MACOSX)
 include build/make/config-clang.mk
 # Mac OS X overrides
 DYNLINK=0
+SHARED_SONAME=0
 # when using iconv
 #CPPFLAGS += -DMPT_WITH_ICONV
 #LDLIBS += -liconv
