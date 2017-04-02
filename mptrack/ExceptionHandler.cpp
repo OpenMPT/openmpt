ea4373a9b4e98daeed08d5b07ba59ebcbf6db190
Jörn Heusipp <manx@openmpt.org>
2017-04-02 16:19:14 +0000

    [Fix] Crash Handler: Pre-Vista systems do not have GetErrorMode(). Use SetErrorMode(0) instead.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7924 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/ExceptionHandler.cpp
+++ mptrack/ExceptionHandler.cpp
@@ -620,6 +620,7 @@ void ExceptionHandler::Register()
 void ExceptionHandler::ConfigureSystemHandler()
 //---------------------------------------------
 {
+#if (_WIN32_WINNT >= 0x0600)
 	if(delegateToWindowsHandler)
 	{
 		//SetErrorMode(0);
@@ -628,6 +629,15 @@ void ExceptionHandler::ConfigureSystemHandler()
 	{
 		g_OriginalErrorMode = ::SetErrorMode(::GetErrorMode() | SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
 	}
+#else // _WIN32_WINNT < 0x0600
+	if(delegateToWindowsHandler)
+	{
+		g_OriginalErrorMode = ::SetErrorMode(0);
+	} else
+	{
+		g_OriginalErrorMode = ::SetErrorMode(::SetErrorMode(0) | SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX);
+	}
+#endif // _WIN32_WINNT
 }
 
 
