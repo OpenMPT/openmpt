2faba0fc64206c89024b1e902f3924ac79449d60
Jörn Heusipp <manx@openmpt.org>
2017-04-02 14:01:30 +0000

    [Ref] picojson: Silence MSVC warning.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7917 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/wine/NativeSoundDevice.cpp
+++ mptrack/wine/NativeSoundDevice.cpp
@@ -28,7 +28,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if MPT_COMPILER_MSVC
+#pragma warning(push)
+#pragma warning(disable:4706) // assignment within conditional expression
+#endif // MPT_COMPILER_MSVC
 #include "picojson/picojson.h"
+#if MPT_COMPILER_MSVC
+#pragma warning(pop)
+#endif // MPT_COMPILER_MSVC
 
 OPENMPT_NAMESPACE_BEGIN
 
