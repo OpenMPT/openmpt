12215c28265622ee1fcd7f3d54e69e33b64d137a
Jörn Heusipp <manx@openmpt.org>
2017-04-06 18:33:14 +0000

    [Mod] When only verifying the file header, stop after the header and avoid uncompressing the whole file for MMCMP, PP20 and XPK files. This may yield false positives but is more consistent with what we are doing for MO3, UMX and J2B.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7968 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Sndfile.cpp
+++ soundlib/Sndfile.cpp
@@ -43,9 +43,9 @@ OPENMPT_NAMESPACE_BEGIN
 
 
 // Module decompression
-bool UnpackXPK(std::vector<char> &unpackedData, FileReader &file);
-bool UnpackPP20(std::vector<char> &unpackedData, FileReader &file);
-bool UnpackMMCMP(std::vector<char> &unpackedData, FileReader &file);
+bool UnpackXPK(std::vector<char> &unpackedData, FileReader &file, CSoundFile::ModLoadingFlags loadFlags);
+bool UnpackPP20(std::vector<char> &unpackedData, FileReader &file, CSoundFile::ModLoadingFlags loadFlags);
+bool UnpackMMCMP(std::vector<char> &unpackedData, FileReader &file, CSoundFile::ModLoadingFlags loadFlags);
 
 
 mpt::ustring FileHistory::AsISO8601() const
@@ -265,11 +265,15 @@ bool CSoundFile::Create(FileReader file, ModLoadingFlags loadFlags)
 
 			MODCONTAINERTYPE packedContainerType = MOD_CONTAINERTYPE_NONE;
 			std::vector<char> unpackedData;
-			if(packedContainerType == MOD_CONTAINERTYPE_NONE && UnpackXPK(unpackedData, file)) packedContainerType = MOD_CONTAINERTYPE_XPK;
-			if(packedContainerType == MOD_CONTAINERTYPE_NONE && UnpackPP20(unpackedData, file)) packedContainerType = MOD_CONTAINERTYPE_PP20;
-			if(packedContainerType == MOD_CONTAINERTYPE_NONE && UnpackMMCMP(unpackedData, file)) packedContainerType = MOD_CONTAINERTYPE_MMCMP;
+			if(packedContainerType == MOD_CONTAINERTYPE_NONE && UnpackXPK(unpackedData, file, loadFlags)) packedContainerType = MOD_CONTAINERTYPE_XPK;
+			if(packedContainerType == MOD_CONTAINERTYPE_NONE && UnpackPP20(unpackedData, file, loadFlags)) packedContainerType = MOD_CONTAINERTYPE_PP20;
+			if(packedContainerType == MOD_CONTAINERTYPE_NONE && UnpackMMCMP(unpackedData, file, loadFlags)) packedContainerType = MOD_CONTAINERTYPE_MMCMP;
 			if(packedContainerType != MOD_CONTAINERTYPE_NONE)
 			{
+				if(loadFlags == onlyVerifyHeader)
+				{
+					return true;
+				}
 				file = FileReader(mpt::byte_cast<mpt::const_byte_span>(mpt::as_span(unpackedData)));
 			}
 
