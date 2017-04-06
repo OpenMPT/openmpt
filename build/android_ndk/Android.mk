3d78b3ada142c364a48b80e27aa5ee1c33c9ba40
Johannes Schultz <sagamusix@openmpt.org>
2017-04-06 23:08:36 +0000

    [New] Emulate Chorus DMO. [Mod] OpenMPT: Version is now 1.27.00.42
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@7970 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/android_ndk/Android.mk
+++ build/android_ndk/Android.mk
@@ -236,6 +236,7 @@ LOCAL_SRC_FILES += \
 	soundlib/XMTools.cpp \
 	soundlib/plugins/DigiBoosterEcho.cpp \
 	soundlib/plugins/dmo/DMOPlugin.cpp \
+	soundlib/plugins/dmo/Chorus.cpp \
 	soundlib/plugins/dmo/Compressor.cpp \
 	soundlib/plugins/dmo/Distortion.cpp \
 	soundlib/plugins/dmo/Echo.cpp \
