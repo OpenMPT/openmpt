ad804283995be463a06143f55cfefa120f1d6dc2
Jörn Heusipp <manx@openmpt.org>
2017-04-21 12:14:38 +0000

    [Ref] Remove VS2010, VS2012, VS2013 support in automted build scripts.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8013 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_vs_any.cmd
+++ build/auto/setup_vs_any.cmd
@@ -19,31 +19,4 @@ if exist "C:\Program Files\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
  goto vsdone
 )
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
- call %~dp0\setup_vs2013.cmd
- goto vsdone
-)
-if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
- call %~dp0\setup_vs2013.cmd
- goto vsdone
-)
-
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
- call %~dp0\setup_vs2012.cmd
- goto vsdone
-)
-if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
- call %~dp0\setup_vs2012.cmd
- goto vsdone
-)
-
-if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
- call %~dp0\setup_vs2010.cmd
- goto vsdone
-)
-if exist "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" (
- call %~dp0\setup_vs2010.cmd
- goto vsdone
-)
-
 :vsdone
