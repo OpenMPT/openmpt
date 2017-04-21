f44b989553860fd79d7ea11be2c998a9be1a2e9c
Jörn Heusipp <manx@openmpt.org>
2017-04-21 12:13:49 +0000

    [Fix] build: Support VS2017 in setup_vs_any.cmd.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8012 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_vs_any.cmd
+++ build/auto/setup_vs_any.cmd
@@ -1,6 +1,15 @@
 @echo off
 
 
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call %~dp0\setup_vs2017.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call %~dp0\setup_vs2017.cmd
+ goto vsdone
+)
+
 if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
  call %~dp0\setup_vs2015.cmd
  goto vsdone
