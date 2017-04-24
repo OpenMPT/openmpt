1bf3d03e4c61f2219bec9b302df6e4df0d3b7580
Jörn Heusipp <manx@openmpt.org>
2017-04-24 11:23:48 +0000

    [Ref] mpg123: Remove all code to dynamically load libmpg123.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8059 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/SampleFormatMP3.cpp
+++ soundlib/SampleFormatMP3.cpp
@@ -31,10 +31,6 @@ extern "C" {
 }
 #endif // MPT_WITH_MINIMP3
 
-#if defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)
-#define MPT_MPG123_LATEST_KNOWN_API_VERSION 43
-#endif
-
 // mpg123 must be last because of mpg123 largfile support insanity
 #if defined(MPT_WITH_MPG123)
 
@@ -43,30 +39,6 @@ extern "C" {
 #include <sys/types.h>
 
 #include <mpg123.h>
-#if (MPT_COMPILER_MSVC || MPT_COMPILER_MSVCCLANGC2) && defined(MPT_WITH_MPG123)
-// MSVC builds use mpg123.h.in which does not provide the autoconf symbol @API_VERSION@, which in turn causes MPG123_API_VERSION to be bogus.
-#undef MPG123_API_VERSION
-#define MPG123_API_VERSION MPT_MPG123_LATEST_KNOWN_API_VERSION
-#endif
-
-// check for libmpg123 version
-
-#if !defined(MPG123_API_VERSION)
-#error "libmpg123 API version unknown. Assuming too old."
-#else
-
-#if (MPG123_API_VERSION < 29)
-#error "libmpg123 API version too old."
-
-#else // modern
-// OK
-
-#endif // MPG123_API_VERSION
-#endif // MPG123_API_VERSION
-
-#elif defined(MPT_ENABLE_MPG123_DYNBIND)
-
-#include <stddef.h>
 
 #endif
 
@@ -77,7 +49,7 @@ OPENMPT_NAMESPACE_BEGIN
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // MP3 Samples
 
-#if defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)
+#if defined(MPT_WITH_MPG123)
 
 #if MPT_COMPILER_MSVCCLANGC2
 typedef _off_t mpg123_off_t;
@@ -87,104 +59,18 @@ typedef off_t mpg123_off_t;
 
 typedef size_t mpg123_size_t;
 
-#if defined(MPT_WITH_MPG123)
 typedef ssize_t mpg123_ssize_t;
-#elif defined(MPT_ENABLE_MPG123_DYNBIND)
-#if MPT_COMPILER_MSVC || MPT_COMPILER_MSVCCLANGC2
-typedef ptrdiff_t mpg123_ssize_t;
-#else // MPT_COMPILER
-typedef ssize_t mpg123_ssize_t;
-#endif // MPT_COMPILER
-#endif
-
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
-
-	enum mpg123_parms
-	{
-		MPG123_VERBOSE = 0,
-		MPG123_FLAGS,
-		MPG123_ADD_FLAGS,
-		MPG123_FORCE_RATE,
-		MPG123_DOWN_SAMPLE,
-		MPG123_RVA,
-		MPG123_DOWNSPEED,
-		MPG123_UPSPEED,
-		MPG123_START_FRAME,
-		MPG123_DECODE_FRAMES,
-		MPG123_ICY_INTERVAL,
-		MPG123_OUTSCALE,
-		MPG123_TIMEOUT,
-		MPG123_REMOVE_FLAGS,
-		MPG123_RESYNC_LIMIT,
-		MPG123_INDEX_SIZE,
-		MPG123_PREFRAMES,
-		MPG123_FEEDPOOL,
-		MPG123_FEEDBUFFER
-	};
-
-	enum mpg123_param_flags
-	{
-		MPG123_QUIET = 0x20
-	};
-
-	enum mpg123_enc_enum
-	{
-		MPG123_ENC_16 = 0x040, MPG123_ENC_SIGNED = 0x080
-	};
-
-	typedef struct {int foo;} mpg123_handle;
-
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
 
 class ComponentMPG123
-#if defined(MPT_WITH_MPG123) && defined(MPT_BUILD_MSVC_STATIC) && !MPT_OS_WINDOWS_WINRT
+#if defined(MPT_BUILD_MSVC_STATIC) && !MPT_OS_WINDOWS_WINRT
 	: public ComponentBundledDLL
-#elif defined(MPT_WITH_MPG123)
+#else
 	: public ComponentBuiltin
-#elif defined(MPT_ENABLE_MPG123_DYNBIND)
-	: public ComponentLibrary
 #endif
 {
 	MPT_DECLARE_COMPONENT_MEMBERS
 
-private:
-
-#if MPT_OS_WINDOWS
-#if defined(MPT_WITH_MPG123)
-#elif defined(MPT_ENABLE_MPG123_DYNBIND)
-	mpt::Library MSVCRT;
-	mpt::Library SHLWAPI;
-#endif // MPT_WITH_MPG123 || MPT_ENABLE_MPG123_DYNBIND
-#endif // MPT_OS_WINDOWS
-
 public:
-
-	int (*mpg123_init )(void);
-	void (*mpg123_exit )(void);
-	mpg123_handle* (*mpg123_new )(const char*,int*);
-	void (*mpg123_delete )(mpg123_handle*);
-	int (*mpg123_param )(mpg123_handle*, enum mpg123_parms, long, double);
-	int (*mpg123_open_handle )(mpg123_handle*, void*);
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
-	int (*mpg123_open_handle_64 )(mpg123_handle*, void*);
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
-	int (*mpg123_replace_reader_handle)(mpg123_handle*,
-		mpg123_ssize_t(*r_read)(void *, void *, mpg123_size_t),
-		mpg123_off_t(*r_lseek)(void *, mpg123_off_t, int),
-		void(*cleanup)(void *));
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
-	int (*mpg123_replace_reader_handle_64)(mpg123_handle*,
-		mpg123_ssize_t(*r_read)(void *, void *, mpg123_size_t),
-		mpg123_off_t(*r_lseek)(void *, mpg123_off_t, int),
-		void(*cleanup)(void *));
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
-	int (*mpg123_read )(mpg123_handle*, unsigned char*, mpg123_size_t, mpg123_size_t*);
-	int (*mpg123_getformat )(mpg123_handle*, long*, int*, int*);
-	int (*mpg123_scan )(mpg123_handle*);
-	mpg123_off_t (*mpg123_length )(mpg123_handle*);
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND)
-	mpg123_off_t (*mpg123_length_64 )(mpg123_handle*);
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
 
 	static mpg123_ssize_t FileReaderRead(void *fp, void *buf, mpg123_size_t count)
 	{
@@ -204,111 +90,18 @@ public:
 
 public:
 	ComponentMPG123()
-#if defined(MPT_WITH_MPG123) && defined(MPT_BUILD_MSVC_STATIC) && !MPT_OS_WINDOWS_WINRT
+#if defined(MPT_BUILD_MSVC_STATIC) && !MPT_OS_WINDOWS_WINRT
 		: ComponentBundledDLL(MPT_PATHSTRING("openmpt-mpg123"))
-#elif defined(MPT_WITH_MPG123)
+#else
 		: ComponentBuiltin()
-#elif defined(MPT_ENABLE_MPG123_DYNBIND)
-		: ComponentLibrary(ComponentTypeForeign)
 #endif
 	{
 		return;
 	}
 	bool DoInitialize()
 	{
-#if defined(MPT_WITH_MPG123) && defined(MPT_BUILD_MSVC_STATIC)
+#if defined(MPT_BUILD_MSVC_STATIC) && !MPT_OS_WINDOWS_WINRT
 		if(!ComponentBundledDLL::DoInitialize())
-		{
-			return false;
-		}
-#endif
-#if defined(MPT_WITH_MPG123)
-		MPT_GLOBAL_BIND("mpg123", mpg123_init);
-		MPT_GLOBAL_BIND("mpg123", mpg123_exit);
-		MPT_GLOBAL_BIND("mpg123", mpg123_new);
-		MPT_GLOBAL_BIND("mpg123", mpg123_delete);
-		MPT_GLOBAL_BIND("mpg123", mpg123_param);
-		MPT_GLOBAL_BIND("mpg123", mpg123_open_handle);
-		MPT_GLOBAL_BIND("mpg123", mpg123_replace_reader_handle);
-		MPT_GLOBAL_BIND("mpg123", mpg123_read);
-		MPT_GLOBAL_BIND("mpg123", mpg123_getformat);
-		MPT_GLOBAL_BIND("mpg123", mpg123_scan);
-		MPT_GLOBAL_BIND("mpg123", mpg123_length);
-#elif defined(MPT_ENABLE_MPG123_DYNBIND)
-		#if defined(MODPLUG_TRACKER)
-			AddLibrary("mpg123", mpt::LibraryPath::AppFullName(MPT_PATHSTRING("openmpt-mpg123")));
-		#endif // MODPLUG_TRACKER
-		#if MPT_OS_WINDOWS
-			// preload MSVCRT.DLL in order to prevent DLL preloading injection attacks from the current working directory,
-			// because the stock binary packages of libmpg123 link against it
-			MSVCRT = mpt::Library(mpt::LibraryPath::System(MPT_PATHSTRING("MSVCRT")));
-			#if defined(LIBOPENMPT_BUILD)
-				// require successful dependency loading for libopenmpt
-				if(!MSVCRT.IsValid())
-				{
-					return false;
-				}
-			#endif // LIBOPENMPT_BUILD
-			// preload shlwapi.dll for the same reasons
-			if(!SHLWAPI.IsValid()) SHLWAPI = mpt::Library(mpt::LibraryPath::System(MPT_PATHSTRING("shlwapi")));
-			#if defined(LIBOPENMPT_BUILD)
-				// require successful dependency loading for libopenmpt
-				if(!SHLWAPI.IsValid())
-				{
-					return false;
-				}
-			#endif // LIBOPENMPT_BUILD
-		#endif // MPT_OS_WINDOWS
-		#if defined(MODPLUG_TRACKER)
-			AddLibrary("mpg123", mpt::LibraryPath::AppDataFullName(MPT_PATHSTRING("libmpg123-0"), GetComponentPath()));
-			AddLibrary("mpg123", mpt::LibraryPath::AppDataFullName(MPT_PATHSTRING("libmpg123"), GetComponentPath()));
-			AddLibrary("mpg123", mpt::LibraryPath::AppDataFullName(MPT_PATHSTRING("mpg123-0"), GetComponentPath()));
-			AddLibrary("mpg123", mpt::LibraryPath::AppDataFullName(MPT_PATHSTRING("mpg123"), GetComponentPath()));
-			AddLibrary("mpg123", mpt::LibraryPath::AppFullName(MPT_PATHSTRING("libmpg123-0")));
-			AddLibrary("mpg123", mpt::LibraryPath::AppFullName(MPT_PATHSTRING("libmpg123")));
-			AddLibrary("mpg123", mpt::LibraryPath::AppFullName(MPT_PATHSTRING("mpg123-0")));
-			AddLibrary("mpg123", mpt::LibraryPath::AppFullName(MPT_PATHSTRING("mpg123")));
-		#elif defined(LIBOPENMPT_BUILD)
-			#if MPT_OS_WINDOWS
-				// libopenmpt on Windows only loads the official builds
-				AddLibrary("mpg123", mpt::LibraryPath::AppFullName(MPT_PATHSTRING("libmpg123-0")));
-			#else // !MPT_OS_WINDOWS
-				AddLibrary("mpg123", mpt::LibraryPath::System(MPT_PATHSTRING("mpg123")));
-			#endif // MPT_OS_WINDOWS
-		#endif
-		MPT_COMPONENT_BIND("mpg123", mpg123_init);
-		MPT_COMPONENT_BIND("mpg123", mpg123_exit);
-		MPT_COMPONENT_BIND("mpg123", mpg123_new);
-		MPT_COMPONENT_BIND("mpg123", mpg123_delete);
-		MPT_COMPONENT_BIND("mpg123", mpg123_param);
-		MPT_COMPONENT_BIND_OPTIONAL("mpg123", mpg123_open_handle);
-		MPT_COMPONENT_BIND_OPTIONAL("mpg123", mpg123_open_handle_64);
-		if(!mpg123_open_handle && !mpg123_open_handle_64)
-		{
-			return false;
-		}
-		MPT_COMPONENT_BIND_OPTIONAL("mpg123", mpg123_replace_reader_handle);
-		MPT_COMPONENT_BIND_OPTIONAL("mpg123", mpg123_replace_reader_handle_64);
-		if(!mpg123_replace_reader_handle && !mpg123_replace_reader_handle_64)
-		{
-			return false;
-		}
-		MPT_COMPONENT_BIND("mpg123", mpg123_read);
-		MPT_COMPONENT_BIND("mpg123", mpg123_getformat);
-		MPT_COMPONENT_BIND("mpg123", mpg123_scan);
-		MPT_COMPONENT_BIND_OPTIONAL("mpg123", mpg123_length);
-		MPT_COMPONENT_BIND_OPTIONAL("mpg123", mpg123_length_64);
-		if(!mpg123_length && !mpg123_length_64)
-		{
-			return false;
-		}
-		#if MPT_COMPILER_MSVC
-			if(!mpg123_open_handle || !mpg123_replace_reader_handle || !mpg123_length)
-			{
-				return false;
-			}
-		#endif
-		if(HasBindFailed())
 		{
 			return false;
 		}
@@ -329,13 +122,13 @@ public:
 };
 MPT_REGISTERED_COMPONENT(ComponentMPG123, "Mpg123")
 
-#endif // MPT_WITH_MPG123 || MPT_ENABLE_MPG123_DYNBIND
+#endif // MPT_WITH_MPG123
 
 
 bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Decode)
 //----------------------------------------------------------------------------------
 {
-#if defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND) || defined(MPT_WITH_MINIMP3)
+#if defined(MPT_WITH_MPG123) || defined(MPT_WITH_MINIMP3)
 
 	// Check file for validity, or else mpg123 will happily munch many files that start looking vaguely resemble an MPEG stream mid-file.
 	file.Rewind();
@@ -381,7 +174,7 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 		}
 	}
 
-#endif // MPT_WITH_MPG123 || MPT_ENABLE_MPG123_DYNBIND || MPT_WITH_MINIMP3
+#endif // MPT_WITH_MPG123 || MPT_WITH_MINIMP3
 
 #if defined(MPT_WITH_MINIMP3)
 
@@ -456,7 +249,7 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 	}
 	return Samples[sample].pSample != nullptr;
 
-#elif defined(MPT_WITH_MPG123) || defined(MPT_ENABLE_MPG123_DYNBIND)
+#elif defined(MPT_WITH_MPG123)
 
 	ComponentHandle<ComponentMPG123> mpg123;
 	if(!IsComponentAvailable(mpg123))
@@ -466,70 +259,49 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 
 	mpg123_handle *mh;
 	int err;
-	if((mh = mpg123->mpg123_new(0, &err)) == nullptr) return false;
+	if((mh = mpg123_new(0, &err)) == nullptr)
+	{
+		return false;
+	}
 	file.Rewind();
 
 	long rate; int nchannels, encoding;
 	SmpLength length;
 	// Set up decoder...
-	if(mpg123->mpg123_param(mh, MPG123_ADD_FLAGS, MPG123_QUIET, 0.0))
+	if(mpg123_param(mh, MPG123_ADD_FLAGS, MPG123_QUIET, 0.0))
 	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND) && !MPT_COMPILER_MSVC
-	if(mpg123->mpg123_replace_reader_handle_64 && mpg123->mpg123_replace_reader_handle_64(mh, ComponentMPG123::FileReaderRead, ComponentMPG123::FileReaderLSeek, 0))
+	if(mpg123_replace_reader_handle(mh, ComponentMPG123::FileReaderRead, ComponentMPG123::FileReaderLSeek, 0))
 	{
-		mpg123->mpg123_delete(mh);
-		return false;
-	} else
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
-	if(mpg123->mpg123_replace_reader_handle(mh, ComponentMPG123::FileReaderRead, ComponentMPG123::FileReaderLSeek, 0))
-	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND) && !MPT_COMPILER_MSVC
-	if(mpg123->mpg123_open_handle_64 && mpg123->mpg123_open_handle_64(mh, &file))
+	if(mpg123_open_handle(mh, &file))
 	{
-		mpg123->mpg123_delete(mh);
-		return false;
-	} else
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
-	if(mpg123->mpg123_open_handle(mh, &file))
-	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
-	if(mpg123->mpg123_scan(mh))
+	if(mpg123_scan(mh))
 	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
-	if(mpg123->mpg123_getformat(mh, &rate, &nchannels, &encoding))
+	if(mpg123_getformat(mh, &rate, &nchannels, &encoding))
 	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
-	if(!nchannels || nchannels > 2
-		|| (encoding & (MPG123_ENC_16 | MPG123_ENC_SIGNED)) != (MPG123_ENC_16 | MPG123_ENC_SIGNED)
-		)
+	if(!nchannels || nchannels > 2 || (encoding & (MPG123_ENC_16 | MPG123_ENC_SIGNED)) != (MPG123_ENC_16 | MPG123_ENC_SIGNED))
 	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
-#if !defined(MPT_WITH_MPG123) && defined(MPT_ENABLE_MPG123_DYNBIND) && !MPT_COMPILER_MSVC
-	if(mpg123->mpg123_length_64)
-	{
-		length = mpg123->mpg123_length_64(mh);
-	} else
-#endif // !MPT_WITH_MPG123 && MPT_ENABLE_MPG123_DYNBIND
-	{
-		length = mpg123->mpg123_length(mh);
-	}
+	length = mpg123_length(mh);
 	if(length == 0)
 	{
-		mpg123->mpg123_delete(mh);
+		mpg123_delete(mh);
 		return false;
 	}
 
@@ -549,9 +321,9 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 	if(Samples[sample].pSample != nullptr)
 	{
 		mpg123_size_t ndecoded = 0;
-		mpg123->mpg123_read(mh, static_cast<unsigned char *>(Samples[sample].pSample), Samples[sample].GetSampleSizeInBytes(), &ndecoded);
+		mpg123_read(mh, static_cast<unsigned char *>(Samples[sample].pSample), Samples[sample].GetSampleSizeInBytes(), &ndecoded);
 	}
-	mpg123->mpg123_delete(mh);
+	mpg123_delete(mh);
 
 	if(!mo3Decode)
 	{
@@ -566,7 +338,7 @@ bool CSoundFile::ReadMP3Sample(SAMPLEINDEX sample, FileReader &file, bool mo3Dec
 	MPT_UNREFERENCED_PARAMETER(file);
 	MPT_UNREFERENCED_PARAMETER(mo3Decode);
 
-#endif // MPT_WITH_MPG123 || MPT_ENABLE_MPG123_DYNBIND || MPT_WITH_MINIMP3
+#endif // MPT_WITH_MPG123 || MPT_WITH_MINIMP3
 
 	return false;
 }
@@ -579,23 +351,17 @@ bool CSoundFile::CanReadMP3()
 	#if defined(MPT_WITH_MPG123)
 		if(!result)
 		{
-			result = true;
+			ComponentHandle<ComponentMPG123> mpg123;
+			if(IsComponentAvailable(mpg123))
+			{
+				result = true;
+			}
 		}
 	#endif
 	#if defined(MPT_WITH_MINIMP3)
 		if(!result)
 		{
 			result = true;
-		}
-	#endif
-	#if defined(MPT_ENABLE_MPG123_DYNBIND)
-		if(!result)
-		{
-			ComponentHandle<ComponentMPG123> mpg123;
-			if(IsComponentAvailable(mpg123))
-			{
-				result = true;
-			}
 		}
 	#endif
 	#if defined(MPT_WITH_MEDIAFOUNDATION)
