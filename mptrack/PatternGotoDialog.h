8f15cfc06a3b9eb300a52c43b7c695b556850db5
Johannes Schultz <sagamusix@openmpt.org>
2017-05-20 21:25:22 +0000

    [Ref] Goto Dialog: General code cleanup.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8183 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/PatternGotoDialog.h
+++ mptrack/PatternGotoDialog.h
@@ -18,36 +18,28 @@ class CSoundFile;
 
 class CPatternGotoDialog : public CDialog
 {
-	DECLARE_DYNAMIC(CPatternGotoDialog)
-
-public:
-	CPatternGotoDialog(CWnd* pParent = NULL);   // standard constructor
-	virtual ~CPatternGotoDialog();
-
-	enum { IDD = IDD_EDIT_GOTO };
-	DECLARE_MESSAGE_MAP()
-
+	const CSoundFile &m_SndFile;
 public:
 	ROWINDEX m_nRow;
 	CHANNELINDEX m_nChannel;
 	PATTERNINDEX m_nPattern;
 	ORDERINDEX m_nOrder, m_nActiveOrder;
-	void UpdatePos(ROWINDEX row, CHANNELINDEX chan, PATTERNINDEX pat, ORDERINDEX ord, CSoundFile &sndFile);
+
+public:
+	CPatternGotoDialog(CWnd *pParent, ROWINDEX row, CHANNELINDEX chan, PATTERNINDEX pat, ORDERINDEX ord, const CSoundFile &sndFile);
 
 protected:
 	bool m_bControlLock;
-	inline bool ControlsLocked() {return m_bControlLock;}
-	inline void LockControls() {m_bControlLock=true;}
-	inline void UnlockControls() {m_bControlLock=false;}
+	inline bool ControlsLocked() { return m_bControlLock; }
+	inline void LockControls() { m_bControlLock = true; }
+	inline void UnlockControls() { m_bControlLock = false; }
 
-	CSoundFile* m_pSndFile;
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 	virtual void OnOK();
 	afx_msg void OnEnChangeGotoPat();
-	
-
-public:
 	afx_msg void OnEnChangeGotoOrd();
+
+	DECLARE_MESSAGE_MAP()
 };
 
 OPENMPT_NAMESPACE_END
