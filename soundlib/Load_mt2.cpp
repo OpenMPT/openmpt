f4a5fb5b028ff68de1cf2755191047253fb8a76a
Johannes Schultz <sagamusix@openmpt.org>
2017-06-02 00:54:22 +0000

    Merged revision(s) 8253 from trunk/OpenMPT: [Fix] MT2: If there were instruments with both sample and plugin assignments, sample data was not read correctly. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.26@8267 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_mt2.cpp
+++ soundlib/Load_mt2.cpp
@@ -1129,14 +1129,8 @@ bool CSoundFile::ReadMT2(FileReader &file, ModLoadingFlags loadFlags)
 	}
 
 	// Read sample groups
-	for(INSTRUMENTINDEX ins = 0; ins < 255; ins++)
+	for(INSTRUMENTINDEX ins = 0; ins < fileHeader.numInstruments; ins++)
 	{
-		ModInstrument *mptIns = Instruments[ins + 1];
-
-		// Instruments with plugin assignments never play samples at the same time!
-		if(mptIns == nullptr || mptIns->nMixPlug != 0)
-			continue;
-
 		if(instrChunks[ins].GetLength())
 		{
 			FileReader &chunk = instrChunks[ins];
@@ -1149,6 +1143,11 @@ bool CSoundFile::ReadMT2(FileReader &file, ModLoadingFlags loadFlags)
 			{
 				file.ReadConvertEndianness(groups[grp]);
 			}
+
+			ModInstrument *mptIns = Instruments[ins + 1];
+			// Instruments with plugin assignments never play samples at the same time!
+			if(mptIns == nullptr || mptIns->nMixPlug != 0)
+				continue;
 
 			mptIns->nGlobalVol = 32;	// Compensate for extended dynamic range of drum instruments
 			mptIns->AssignSample(0);
