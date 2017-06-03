OpenMPT/openmpt

commit 816fa568f35bc12823766e47dc8549a00a43d52e

[Ref] CPattern uses std::vector<ModCommand> instead of raw pointers now. [Ref] Related refactoring.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8260 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/CleanupSong.cpp
@@ -440,22 +440,6 @@ bool CModCleanupDlg::RemoveUnusedPatterns()
 }
 
 
-struct OrigPatSettings
-{
-	// This stuff is needed for copying the old pattern properties to the new pattern number
-	std::string name;			// original pattern name
-	TempoSwing swing;			// original pattern tempo swing
-	ModCommand *data;			// original pattern data
-	ROWINDEX numRows;			// original pattern sizes
-	ROWINDEX rowsPerBeat;		// original pattern highlight
-	ROWINDEX rowsPerMeasure;	// original pattern highlight
-
-	PATTERNINDEX newIndex;		// map old pattern index <-> new pattern index
-};
-
-const OrigPatSettings defaultSettings = { "", TempoSwing(), nullptr, 0, 0, 0, PATTERNINDEX_INVALID };
-
-
 // Rearrange patterns (first pattern in order list = 0, etc...)
 bool CModCleanupDlg::RearrangePatterns()
 //--------------------------------------
@@ -463,7 +447,7 @@ bool CModCleanupDlg::RearrangePatterns()
 	CSoundFile &sndFile = modDoc.GetrSoundFile();
 
 	const PATTERNINDEX numPatterns = sndFile.Patterns.Size();
-	std::vector<OrigPatSettings> patternSettings(numPatterns, defaultSettings);
+	std::vector<PATTERNINDEX> newIndex(numPatterns, PATTERNINDEX_INVALID);
 
 	bool modified = false;
 
@@ -478,48 +462,43 @@ bool CModCleanupDlg::RearrangePatterns()
 		{
 			if(pat < numPatterns)
 			{
-				if(patternSettings[pat].newIndex == PATTERNINDEX_INVALID)
+				if(newIndex[pat] == PATTERNINDEX_INVALID)
 				{
-					patternSettings[pat].newIndex = patOrder++;
+					newIndex[pat] = patOrder++;
 				}
-				pat = patternSettings[pat].newIndex;
+				pat = newIndex[pat];
 			}
 		}
 	}
+	// All unused patterns are moved to the end of the pattern list.
 	for(PATTERNINDEX pat = 0; pat < numPatterns; pat++)
 	{
-		PATTERNINDEX newIndex = patternSettings[pat].newIndex;
-
-		// All unused patterns are moved to the end of the pattern list.
-		if(newIndex == PATTERNINDEX_INVALID && sndFile.Patterns.IsValidPat(pat))
+		PATTERNINDEX &index = newIndex[pat];
+		if(index == PATTERNINDEX_INVALID && sndFile.Patterns.IsValidPat(pat))
 		{
-			newIndex = patOrder++;
+			index = patOrder++;
 		}
-
-		// Create old <-> new pattern ID data mapping.
-		if(newIndex != PATTERNINDEX_INVALID)
+	}
+	// Also need new indices for any non-existent patterns
+	for(auto &index : newIndex)
+	{
+		if(index == PATTERNINDEX_INVALID)
 		{
-			if(pat != newIndex) modified = true;
-
-			patternSettings[newIndex].numRows = sndFile.Patterns[pat].GetNumRows();
-			patternSettings[newIndex].data = sndFile.Patterns[pat];
-			if(sndFile.Patterns[pat].GetOverrideSignature())
-			{
-				patternSettings[newIndex].rowsPerBeat = sndFile.Patterns[pat].GetRowsPerBeat();
-				patternSettings[newIndex].rowsPerMeasure = sndFile.Patterns[pat].GetRowsPerMeasure();
-			}
-			patternSettings[newIndex].swing = sndFile.Patterns[pat].GetTempoSwing();
-			patternSettings[newIndex].name = sndFile.Patterns[pat].GetName();
+			index = patOrder++;
 		}
 	}
 
-	// Copy old data to new pattern location.
-	for(PATTERNINDEX pat = 0; pat < numPatterns; pat++)
+	// Now rearrange the pattern indices
+	for(size_t i = 0; i < newIndex.size(); i++)
 	{
-		sndFile.Patterns[pat].SetData(patternSettings[pat].data, patternSettings[pat].numRows);
-		sndFile.Patterns[pat].SetSignature(patternSettings[pat].rowsPerBeat, patternSettings[pat].rowsPerMeasure);
-		sndFile.Patterns[pat].SetTempoSwing(patternSettings[pat].swing);
-		sndFile.Patterns[pat].SetName(patternSettings[pat].name);
+		auto current = static_cast<PATTERNINDEX>(i), next = newIndex[current];
+		while(i != next)
+		{
+			std::swap(sndFile.Patterns[current], sndFile.Patterns[next]);
+			newIndex[current] = current;
+			current = next;
+		}
+		newIndex[current] = current;
 	}
 
 	EndWaitCursor();

soundlib/pattern.h
@@ -33,11 +33,7 @@ class CPattern
 	
 public:
 //BEGIN: OPERATORS
-	//To mimic ModCommand*
-	operator ModCommand*() { return m_ModCommands; }
-	operator const ModCommand*() const { return m_ModCommands; }
-	CPattern& operator=(ModCommand* const p) { m_ModCommands = p; return *this; }
-	CPattern& operator=(const CPattern& pat)
+	CPattern& operator= (const CPattern &pat)
 	{
 		m_ModCommands = pat.m_ModCommands;
 		m_Rows = pat.m_Rows;
@@ -60,8 +56,10 @@ public:
 	ROWINDEX GetRowsPerMeasure() const { return m_RowsPerMeasure; }		// pattern-specific rows per measure
 	bool GetOverrideSignature() const { return (m_RowsPerBeat + m_RowsPerMeasure > 0); }	// override song time signature?
 
-	// Return true if modcommand can be accessed from given row, false otherwise.
+	// Returns true if pattern data can be accessed at given row, false otherwise.
 	bool IsValidRow(const ROWINDEX row) const { return (row < GetNumRows()); }
+	// Returns true if any pattern data is present.
+	bool IsValid() const { return !m_ModCommands.empty(); }
 
 	// Return PatternRow object which has operator[] defined so that ModCommand
 	// at (iRow, iChn) can be accessed with GetRow(iRow)[iChn].
@@ -88,7 +86,7 @@ public:
 	CSoundFile& GetSoundFile();
 	const CSoundFile& GetSoundFile() const;
 
-	bool SetData(ModCommand* p, const ROWINDEX rows) { m_ModCommands = p; m_Rows = rows; return false; }
+	const std::vector<ModCommand> &GetData() const { return m_ModCommands; }
 
 	// Set pattern signature (rows per beat, rows per measure). Returns true on success.
 	bool SetSignature(const ROWINDEX rowsPerBeat, const ROWINDEX rowsPerMeasure);
@@ -121,24 +119,22 @@ public:
 	// Write some kind of effect data to the pattern
 	bool WriteEffect(EffectWriter &settings);
 
-	bool WriteITPdata(FILE* f) const;
-
-	// Static allocation / deallocation helpers
-	static ModCommand *AllocatePattern(ROWINDEX rows, CHANNELINDEX nchns);
-	static void FreePattern(ModCommand *pat);
-
 //END: INTERFACE METHODS
 
-	typedef ModCommand* iterator;
-	typedef const ModCommand *const_iterator;
+	typedef std::vector<ModCommand>::iterator iterator;
+	typedef std::vector<ModCommand>::const_iterator const_iterator;
 
-	iterator Begin() { return m_ModCommands; }
-	const_iterator Begin() const { return m_ModCommands; }
+	iterator begin() { return m_ModCommands.begin(); }
+	const_iterator begin() const { return m_ModCommands.begin(); }
+	const_iterator cbegin() const { return m_ModCommands.cbegin(); }
 
-	iterator End() { return (m_ModCommands != nullptr) ? m_ModCommands + m_Rows * GetNumChannels() : nullptr; }
-	const_iterator End() const { return (m_ModCommands != nullptr) ? m_ModCommands + m_Rows * GetNumChannels() : nullptr; }
+	iterator end() { return m_ModCommands.end(); }
+	const_iterator end() const { return m_ModCommands.end(); }
+	const_iterator cend() const { return m_ModCommands.cend(); }
 
 	CPattern(CPatternContainer& patCont) : m_ModCommands(0), m_Rows(64), m_RowsPerBeat(0), m_RowsPerMeasure(0), m_rPatternContainer(patCont) {};
+	CPattern(const CPattern &) = default;
+	CPattern(CPattern &&) = default;
 
 protected:
 	ModCommand& GetModCommand(size_t i) { return m_ModCommands[i]; }
@@ -150,7 +146,7 @@ protected:
 
 //BEGIN: DATA
 protected:
-	ModCommand* m_ModCommands;
+	std::vector<ModCommand> m_ModCommands;
 	ROWINDEX m_Rows;
 	ROWINDEX m_RowsPerBeat;		// patterns-specific time signature. if != 0, this is implicitely set.
 	ROWINDEX m_RowsPerMeasure;	// ditto

mptrack/AppendModule.cpp
@@ -73,12 +73,11 @@ void CModDoc::AppendModule(const CSoundFile &source)
 	}
 
 	// Check which samples / instruments are actually referenced.
-	for(PATTERNINDEX i = 0; i < source.Patterns.Size(); i++) if(source.Patterns.IsValidPat(i))
+	for(const auto &pat : source.Patterns) if(pat.IsValid())
 	{
-		const ModCommand *m = source.Patterns[i];
-		for(size_t j = source.GetNumChannels() * source.Patterns[i].GetNumRows(); j; m++, j--)
+		for(const auto &m : pat)
 		{
-			if(!m->IsPcNote() && m->instr < instrMapping.size()) instrMapping[m->instr] = 0;
+			if(!m.IsPcNote() && m.instr < instrMapping.size()) instrMapping[m.instr] = 0;
 		}
 	}
 

mptrack/Draw_pat.cpp
@@ -759,10 +759,9 @@ void CViewPattern::DrawPatternData(HDC hdc, PATTERNINDEX nPattern, bool selEnabl
 	{
 		return;
 	}
+	const CPattern &pattern = sndFile.Patterns[nPattern];
 
 	const PATTERNFONT *pfnt = PatternFont::currentFont;
-	const ModCommand m0 = ModCommand::Empty();
-	const ModCommand *pPattern = sndFile.Patterns[nPattern];
 	CRect rect;
 	int xpaint, ypaint = *pypaint;
 	UINT nColumnWidth;
@@ -794,7 +793,7 @@ void CViewPattern::DrawPatternData(HDC hdc, PATTERNINDEX nPattern, bool selEnabl
 	
 	bool bRowSel = false;
 	int row_col = -1, row_bkcol = -1;
-	for (UINT row=startRow; row<numRows; row++)
+	for (ROWINDEX row=startRow; row<numRows; row++)
 	{
 		UINT col, xbmp, nbmp, oldrowcolor;
 		const int compRow = row + TrackerSettings::Instance().rowDisplayOffset;
@@ -891,13 +890,13 @@ void CViewPattern::DrawPatternData(HDC hdc, PATTERNINDEX nPattern, bool selEnabl
 		{
 			int x, bk_col, tx_col, col_sel, fx_col;
 
-			const ModCommand *m = (pPattern) ? &pPattern[row*ncols+col] : &m0;
+			const ModCommand *m = pattern.GetpModCommand(row, static_cast<CHANNELINDEX>(col));
 
 			// Should empty volume commands be replaced with a volume command showing the default volume?
 			const bool drawDefaultVolume = DrawDefaultVolume(m);
 
 			DWORD dwSpeedUpMask = 0;
-			if (useSpeedUpMask && (selectedCols[col] & COLUMN_BITS_SKIP) && (pPattern) && (row))
+			if (useSpeedUpMask && (selectedCols[col] & COLUMN_BITS_SKIP) && (row))
 			{
 				const ModCommand *mold = m - ncols;
 				const bool drawOldDefaultVolume = DrawDefaultVolume(mold);

mptrack/MainFrm.cpp
@@ -1686,9 +1686,9 @@ void CMainFrame::PreparePreview(ModCommand::NOTE note)
 	m_WaveFile.SetRepeatCount(-1);
 	m_WaveFile.ResetPlayPos();
 
-	ModCommand *m = m_WaveFile.Patterns[0];
-	if(m)
+	if(m_WaveFile.Patterns.IsValidPat(0))
 	{
+		auto m = m_WaveFile.Patterns[0].GetRow(0);
 		if(m_WaveFile.GetNumSamples() > 0)
 		{
 			m[0].note = note;

mptrack/ModConvert.cpp
@@ -180,12 +180,10 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 		}
 	}
 
-	for(PATTERNINDEX pat = 0; pat < m_SndFile.Patterns.Size(); pat++) if (m_SndFile.Patterns[pat])
+	for(auto &pat : m_SndFile.Patterns) if(pat.IsValid())
 	{
-		ModCommand *m = m_SndFile.Patterns[pat];
-
 		// This is used for -> MOD/XM conversion
-		std::vector<std::vector<ModCommand::PARAM> > effMemory(GetNumChannels());
+		std::vector<std::vector<ModCommand::PARAM>> effMemory(GetNumChannels());
 		std::vector<ModCommand::VOL> volMemory(GetNumChannels(), 0);
 		std::vector<ModCommand::INSTR> instrMemory(GetNumChannels(), 0);
 		for(size_t i = 0; i < GetNumChannels(); i++)
@@ -197,7 +195,7 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 		CHANNELINDEX chn = 0;
 		ROWINDEX row = 0;
 
-		for(size_t len = m_SndFile.Patterns[pat].GetNumRows() * m_SndFile.GetNumChannels(); len; m++, len--, chn++)
+		for(auto m = pat.begin(); m != pat.end(); m++, chn++)
 		{
 			if(chn >= GetNumChannels())
 			{
@@ -305,7 +303,7 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 					// If the leftmost row delay command is SE0, ST3 ignores it, IT doesn't.
 
 					// Delete all commands right of the first command
-					ModCommand *p = m + 1;
+					auto p = m + 1;
 					for(CHANNELINDEX c = chn + 1; c < m_SndFile.GetNumChannels(); c++, p++)
 					{
 						if(p->command == CMD_S3MCMDEX && (p->param & 0xF0) == 0xE0)
@@ -318,7 +316,7 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 				&& m->command == CMD_S3MCMDEX && (m->param & 0xF0) == 0xE0)
 			{
 				// Delete all commands left of the last command
-				ModCommand *p = m - 1;
+				auto p = m - 1;
 				for(CHANNELINDEX c = 0; c < chn; c++, p--)
 				{
 					if(p->command == CMD_S3MCMDEX && (p->param & 0xF0) == 0xE0)
@@ -331,7 +329,7 @@ bool CModDoc::ChangeModType(MODTYPE nNewType)
 		}
 		if(addBreak)
 		{
-			m_SndFile.Patterns[pat].WriteEffect(EffectWriter(CMD_PATTERNBREAK, 0).Row(m_SndFile.Patterns[pat].GetNumRows() - 1));
+			pat.WriteEffect(EffectWriter(CMD_PATTERNBREAK, 0).Row(pat.GetNumRows() - 1));
 		}
 	}
 

mptrack/Moddoc.cpp
@@ -1440,7 +1440,7 @@ bool CModDoc::IsInstrumentMuted(INSTRUMENTINDEX nInstr) const
 UINT CModDoc::GetPatternSize(PATTERNINDEX nPat) const
 //---------------------------------------------------
 {
-	if(nPat < m_SndFile.Patterns.Size() && m_SndFile.Patterns[nPat]) return m_SndFile.Patterns[nPat].GetNumRows();
+	if(m_SndFile.Patterns.IsValidIndex(nPat)) return m_SndFile.Patterns[nPat].GetNumRows();
 	return 0;
 }
 

mptrack/Modedit.cpp
@@ -163,42 +163,42 @@ CHANNELINDEX CModDoc::ReArrangeChannels(const std::vector<CHANNELINDEX> &newOrde
 	{
 		CString str;
 		str.Format(_T("Can't apply change: Number of channels should be between %u and %u."), specs.channelsMin, specs.channelsMax);
-		Reporting::Error(str, "Rearrange Channels");
+		Reporting::Error(str, _T("Rearrange Channels"));
 		return CHANNELINDEX_INVALID;
 	}
 
-	CriticalSection cs;
 	if(createUndoPoint)
 	{
 		PrepareUndoForAllPatterns(true, "Rearrange Channels");
 	}
 
+	std::vector<ModCommand> oldRow;
+	try
+	{
+		oldRow.reserve(GetNumChannels());
+	} MPT_EXCEPTION_CATCH_OUT_OF_MEMORY(e)
+	{
+		MPT_EXCEPTION_DELETE_OUT_OF_MEMORY(e);
+		Reporting::Error("Out of memory!", "Rearrange Channels");
+		return CHANNELINDEX_INVALID;
+	}
+	CriticalSection cs;
 	for(auto &pat : m_SndFile.Patterns)
 	{
-		if(pat)
+		if(pat.IsValid())
 		{
-			ModCommand *oldPatData = pat;
-			ModCommand *newPatData = CPattern::AllocatePattern(pat.GetNumRows(), newNumChannels);
-			if(!newPatData)
-			{
-				cs.Leave();
-				Reporting::Error("ERROR: Pattern allocation failed in ReArrangeChannels(...)");
-				return CHANNELINDEX_INVALID;
-			}
-			ModCommand *m = newPatData;
+			auto m = pat.begin();
 			for(ROWINDEX row = 0; row < pat.GetNumRows(); row++)
 			{
+				oldRow.assign(m, m + GetNumChannels());
 				for(CHANNELINDEX chn = 0; chn < newNumChannels; chn++, m++)
 				{
 					if(newOrder[chn] < GetNumChannels()) //Case: getting old channel to the new channel order.
-						*m = *pat.GetpModCommand(row, newOrder[chn]);
+						*m = oldRow[newOrder[chn]];
 					else //Case: figure newOrder[k] is not the index of any current channel, so adding a new channel.
 						*m = ModCommand::Empty();
-
 				}
 			}
-			pat.SetData(newPatData, pat.GetNumRows());
-			CPattern::FreePattern(oldPatData);
 		}
 	}
 
@@ -803,7 +803,7 @@ bool CModDoc::RemoveOrder(SEQUENCEINDEX nSeq, ORDERINDEX nOrd)
 bool CModDoc::RemovePattern(PATTERNINDEX nPat)
 //--------------------------------------------
 {
-	if ((nPat < m_SndFile.Patterns.Size()) && (m_SndFile.Patterns[nPat]))
+	if(m_SndFile.Patterns.IsValidPat(nPat))
 	{
 		CriticalSection cs;
 		GetPatternUndo().PrepareUndo(nPat, 0, 0, GetNumChannels(), m_SndFile.Patterns[nPat].GetNumRows(), "Remove Pattern");
@@ -1183,7 +1183,7 @@ bool CModDoc::IsChannelUnused(CHANNELINDEX nChn) const
 	}
 	for(auto &pat : m_SndFile.Patterns)
 	{
-		if(pat)
+		if(pat.IsValid())
 		{
 			const ModCommand *p = pat.GetpModCommand(0, nChn);
 			for(ROWINDEX nRow = pat.GetNumRows(); nRow > 0; nRow--, p += nChannels)
@@ -1217,7 +1217,7 @@ bool CModDoc::IsSampleUsed(SAMPLEINDEX sample, bool searchInMutedChannels) const
 		for(PATTERNINDEX i = 0; i < m_SndFile.Patterns.Size(); i++) if (m_SndFile.Patterns.IsValidPat(i))
 		{
 			const CPattern &pattern = m_SndFile.Patterns[i];
-			const ModCommand *m = pattern.Begin();
+			auto m = pattern.cbegin();
 			for(ROWINDEX row = 0; row < pattern.GetNumRows(); row++)
 			{
 				for(CHANNELINDEX chn = 0; chn < pattern.GetNumChannels(); chn++, m++)
@@ -1241,7 +1241,7 @@ bool CModDoc::IsInstrumentUsed(INSTRUMENTINDEX instr, bool searchInMutedChannels
 	for(PATTERNINDEX i = 0; i < m_SndFile.Patterns.Size(); i++) if (m_SndFile.Patterns.IsValidPat(i))
 	{
 		const CPattern &pattern = m_SndFile.Patterns[i];
-		const ModCommand *m = pattern.Begin();
+		auto m = pattern.cbegin();
 		for(ROWINDEX row = 0; row < pattern.GetNumRows(); row++)
 		{
 			for(CHANNELINDEX chn = 0; chn < pattern.GetNumChannels(); chn++, m++)

commit 76a49a8b851f4a098da06d28a39430954686278b

[Fix] Rearrange patterns was broken since r8260. [Fix] CPattern::operator= didn't copy the tempo swing settings.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8273 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/CleanupSong.cpp
@@ -490,19 +490,15 @@ bool CModCleanupDlg::RearrangePatterns()
 
 	modDoc.GetPatternUndo().RearrangePatterns(newIndex);
 
-	// Now rearrange the pattern indices
+	// Now rearrange the actual patterns
 	for(size_t i = 0; i < newIndex.size(); i++)
 	{
-		auto current = static_cast<PATTERNINDEX>(i);
-		while(i != newIndex[current])
-		{
-			PATTERNINDEX next = newIndex[current];
-			modified = true;
-			std::swap(sndFile.Patterns[current], sndFile.Patterns[next]);
-			newIndex[current] = current;
-			current = next;
-		}
-		newIndex[current] = current;
+		PATTERNINDEX j = newIndex[i];
+		if(i == j)
+			continue;
+		while(i < j)
+			j = newIndex[j];
+		std::swap(sndFile.Patterns[i], sndFile.Patterns[j]);
 	}
 
 	EndWaitCursor();

soundlib/pattern.h
@@ -39,6 +39,7 @@ public:
 		m_Rows = pat.m_Rows;
 		m_RowsPerBeat = pat.m_RowsPerBeat;
 		m_RowsPerMeasure = pat.m_RowsPerMeasure;
+		m_tempoSwing = pat.m_tempoSwing;
 		m_PatternName = pat.m_PatternName;
 		return *this;
 	}
@@ -134,7 +135,7 @@ public:
 
 	CPattern(CPatternContainer& patCont) : m_ModCommands(0), m_Rows(64), m_RowsPerBeat(0), m_RowsPerMeasure(0), m_rPatternContainer(patCont) {};
 	CPattern(const CPattern &) = default;
-	CPattern(CPattern &&) = default;
+	CPattern(CPattern &&) noexcept = default;
 
 protected:
 	ModCommand& GetModCommand(size_t i) { return m_ModCommands[i]; }