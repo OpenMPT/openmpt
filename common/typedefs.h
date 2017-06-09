ef1da5cf16647b78f06bc817e3a1b5a6e058af9d
Jörn Heusipp <manx@openmpt.org>
2017-06-09 08:44:33 +0000

    [Ref] Activate C++14 constexpr in C++14 mode.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8300 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/typedefs.h
+++ common/typedefs.h
@@ -33,10 +33,13 @@ OPENMPT_NAMESPACE_BEGIN
 // constexpr
 #define MPT_CONSTEXPR11_FUN constexpr MPT_FORCEINLINE
 #define MPT_CONSTEXPR11_VAR constexpr
-// no support for c++14 yet, however when making types constexpr, there are a
-//  couple of situations where it's only possible in c++14
+#if MPT_CXX_AT_LEAST(14)
+#define MPT_CONSTEXPR14_FUN constexpr MPT_FORCEINLINE
+#define MPT_CONSTEXPR14_VAR constexpr
+#else
 #define MPT_CONSTEXPR14_FUN MPT_FORCEINLINE
 #define MPT_CONSTEXPR14_VAR const
+#endif
 
 
 
