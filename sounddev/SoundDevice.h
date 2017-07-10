OpenMPT/openmpt

commit c37a9adf9f5b06bf696b197abcd41e94cbe2a7e9

[Ref] sounddev: Kill SoundDevice::ID and replace all occurences with the unique and stable SoundDevice::Identifier or a per-session unique SoundDevice::Manager::GlobalID. [Ref] sounddev: For WaveOut devices, store the WaveOut device index as string in internalID. [Ref] sounddev: Convert the PortAudio internalID from per-HostAPI index to the global PortAudio PA_DeviceIndex. This simplifies a whole lot of per-HostAPI device counting and confusing number conversions. [Mod] sounddev: Sort sound devices depending on OS version. [Mod] sounddev: Show PortAudio WDM-KS devices as WaveRT in Vista and newer.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5046 56274372-70c3-4bfc-bfc3-4c3a0b034d27

sounddev/SoundDevice.h
@@ -17,8 +17,6 @@
 #include "../common/mptAtomic.h"
 #include "../soundlib/SampleFormat.h"
 
-#include <bitset>
-#include <map>
 #include <vector>
 
 
@@ -110,107 +108,35 @@ public:
 // SoundDevice::Base Interface
 //
 
-enum Type
-{
-	// do not change old values, these get saved to the ini
-	TypeINVALID          =-1,
-	TypeWAVEOUT          = 0,
-	TypeDSOUND           = 1,
-	TypeASIO             = 2,
-	TypePORTAUDIO_WASAPI = 3,
-	TypePORTAUDIO_WDMKS  = 4,
-	TypePORTAUDIO_WMME   = 5,
-	TypePORTAUDIO_DS     = 6,
-	TypePORTAUDIO_ASIO   = 7,
-	TypeNUM_DEVTYPES
-};
-typedef std::bitset<TypeNUM_DEVTYPES> TypesSet;
 
+static const MPT_UCHAR_TYPE TypeWAVEOUT          [] = MPT_ULITERAL("WaveOut");
+static const MPT_UCHAR_TYPE TypeDSOUND           [] = MPT_ULITERAL("DirectSound");
+static const MPT_UCHAR_TYPE TypeASIO             [] = MPT_ULITERAL("ASIO");
+static const MPT_UCHAR_TYPE TypePORTAUDIO_WASAPI [] = MPT_ULITERAL("WASAPI");
+static const MPT_UCHAR_TYPE TypePORTAUDIO_WDMKS  [] = MPT_ULITERAL("WDM-KS");
+static const MPT_UCHAR_TYPE TypePORTAUDIO_WMME   [] = MPT_ULITERAL("MME");
+static const MPT_UCHAR_TYPE TypePORTAUDIO_DS     [] = MPT_ULITERAL("DS");
 
-mpt::ustring TypeToString(SoundDevice::Type type, bool verbose = false);
-
-typedef uint8 Index;
-
-template<typename T>
-bool IndexIsValid(const T & x)
-{
-	return 0 <= x && x <= std::numeric_limits<Index>::max();
-}
-
-//======
-class ID
-//======
-{
-private:
-	SoundDevice::Type type;
-	SoundDevice::Index index;
-public:
-	ID() : type(TypeWAVEOUT), index(0) {}
-	ID(SoundDevice::Type type, SoundDevice::Index index)
-		: type(type)
-		, index(index)
-	{
-		return;
-	}
-	bool IsValid() const
-	{
-		return (type > TypeINVALID);
-	}
-	SoundDevice::Type GetType() const { return type; }
-	SoundDevice::Index GetIndex() const { return index; }
-	bool operator == (const SoundDevice::ID &cmp) const
-	{
-		return (type == cmp.type) && (index == cmp.index);
-	}
-
-	bool operator != (const SoundDevice::ID &cmp) const
-	{
-		return (type != cmp.type) || (index != cmp.index);
-	}
-	bool operator < (const SoundDevice::ID &cmp) const
-	{
-		return (type < cmp.type) || (type == cmp.type && index < cmp.index);
-	}
-public:
-	// Do not change these. These functions are used to manipulate the value that gets stored in the settings.
-	template<typename T>
-	static SoundDevice::ID FromIdRaw(T id_)
-	{
-		uint16 id = static_cast<uint16>(id_);
-		return SoundDevice::ID((SoundDevice::Type)((id>>8)&0xff), (id>>0)&0xff);
-	}
-	uint16 GetIdRaw() const
-	{
-		return static_cast<uint16>(((int)type<<8) | (index<<0));
-	}
-};
+typedef mpt::ustring Type;
 
 
 typedef mpt::ustring Identifier;
 
 SoundDevice::Type ParseType(const SoundDevice::Identifier &identifier);
 
-
 struct Info
 {
-	SoundDevice::ID id;
-	mpt::ustring name;
-	mpt::ustring apiName;
+	SoundDevice::Type type;
 	mpt::ustring internalID;
+	mpt::ustring name; // user visible
+	mpt::ustring apiName; // user visible
+	std::vector<mpt::ustring> apiPath; // i.e. Wine-support, PortAudio
 	bool isDefault;
-	Info() : id(TypeINVALID, 0), isDefault(false) { }
-	Info(SoundDevice::ID id, const mpt::ustring &name, const mpt::ustring &apiName, const mpt::ustring &internalID = mpt::ustring())
-		: id(id)
-		, name(name)
-		, apiName(apiName)
-		, internalID(internalID)
-		, isDefault(false)
-	{
-		return;
-	}
+	bool useNameAsIdentifier;
+	Info() : isDefault(false), useNameAsIdentifier(false) { }
 	bool IsValid() const
 	{
-		return id.IsValid();
+		return !type.empty() && !internalID.empty();
 	}
 	SoundDevice::Identifier GetIdentifier() const
 	{
@@ -218,12 +144,10 @@ struct Info
 		{
 			return mpt::ustring();
 		}
-		mpt::ustring result = apiName;
+		mpt::ustring result = mpt::ustring();
+		result += type;
 		result += MPT_USTRING("_");
-		if(!internalID.empty())
-		{
-			result += internalID; // safe to not contain special characters
-		} else if(!name.empty())
+		if(useNameAsIdentifier)
 		{
 			// UTF8-encode the name and convert the utf8 to hex.
 			// This ensures that no special characters are contained in the configuration key.
@@ -232,7 +156,7 @@ struct Info
 			result += hexString;
 		} else
 		{
-			result += mpt::ufmt::dec(id.GetIndex());
+			result += internalID; // safe to not contain special characters
 		}
 		return result;
 	}
@@ -604,9 +528,7 @@ private:
 
 protected:
 
-	SoundDevice::ID GetDeviceID() const { return m_Info.id; }
-	SoundDevice::Type GetDeviceType() const { return m_Info.id.GetType(); }
-	SoundDevice::Index GetDeviceIndex() const { return m_Info.id.GetIndex(); }
+	SoundDevice::Type GetDeviceType() const { return m_Info.type; }
 	mpt::ustring GetDeviceInternalID() const { return m_Info.internalID; }
 	SoundDevice::Identifier GetDeviceIdentifier() const { return m_Info.GetIdentifier(); }
 
@@ -700,6 +622,16 @@ public:
 
 namespace Legacy
 {
+typedef uint16 ID;
+static const SoundDevice::Legacy::ID MaskType = 0xff00;
+static const SoundDevice::Legacy::ID MaskIndex = 0x00ff;
+static const SoundDevice::Legacy::ID TypeWAVEOUT          = 0;
+static const SoundDevice::Legacy::ID TypeDSOUND           = 1;
+static const SoundDevice::Legacy::ID TypeASIO             = 2;
+static const SoundDevice::Legacy::ID TypePORTAUDIO_WASAPI = 3;
+static const SoundDevice::Legacy::ID TypePORTAUDIO_WDMKS  = 4;
+static const SoundDevice::Legacy::ID TypePORTAUDIO_WMME   = 5;
+static const SoundDevice::Legacy::ID TypePORTAUDIO_DS     = 6;
 mpt::ustring GetDirectSoundDefaultDeviceIdentifierPre_1_25_00_04();
 mpt::ustring GetDirectSoundDefaultDeviceIdentifier_1_25_00_04();
 }

sounddev/SoundDeviceManager.cpp
@@ -32,6 +32,20 @@ namespace SoundDevice {
 //
 
 
+	struct CompareType
+	{
+		std::map<SoundDevice::Type, int> ordering;
+		CompareType(const std::map<SoundDevice::Type, int> &ordering)
+			: ordering(ordering)
+		{
+			return;
+		}
+		bool operator () (const SoundDevice::Info &x, const SoundDevice::Info &y)
+		{
+			return (ordering[x.type] > ordering[y.type]);
+		}
+	};
+
 void Manager::ReEnumerate()
 //-------------------------
 {
@@ -52,6 +66,14 @@ void Manager::ReEnumerate()
 		const std::vector<SoundDevice::Info> infos = CWaveDevice::EnumerateDevices();
 		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
 	}
+	// kind of deprecated by now
+#ifndef NO_DSOUND
+	if(IsComponentAvailable(m_DirectSound))
+	{
+		const std::vector<SoundDevice::Info> infos = CDSoundDevice::EnumerateDevices();
+		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
+	}
+#endif // NO_DSOUND
 
 #ifndef NO_ASIO
 	if(IsComponentAvailable(m_ASIO))
@@ -64,49 +86,56 @@ void Manager::ReEnumerate()
 #ifndef NO_PORTAUDIO
 	if(IsComponentAvailable(m_PortAudio))
 	{
-		const std::vector<SoundDevice::Info> infos = CPortaudioDevice::EnumerateDevices(TypePORTAUDIO_WASAPI);
-		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
-	}
-	if(IsComponentAvailable(m_PortAudio))
-	{
-		const std::vector<SoundDevice::Info> infos = CPortaudioDevice::EnumerateDevices(TypePORTAUDIO_WDMKS);
+		const std::vector<SoundDevice::Info> infos = CPortaudioDevice::EnumerateDevices();
 		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
 	}
 #endif // NO_PORTAUDIO
 
-	// kind of deprecated by now
-#ifndef NO_DSOUND
-	if(IsComponentAvailable(m_DirectSound))
-	{
-		const std::vector<SoundDevice::Info> infos = CDSoundDevice::EnumerateDevices();
-		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
+	std::map<SoundDevice::Type, int> typePriorities;
+	if(mpt::Windows::Version::IsWine())
+	{ // Wine
+		typePriorities[SoundDevice::TypeDSOUND] = 29;
+		typePriorities[SoundDevice::TypeWAVEOUT] = 28;
+		typePriorities[SoundDevice::TypePORTAUDIO_WASAPI] = 21;
+		typePriorities[SoundDevice::TypePORTAUDIO_WMME] = 19;
+		typePriorities[SoundDevice::TypePORTAUDIO_DS] = 18;
+		typePriorities[SoundDevice::TypeASIO] = 10;
+		typePriorities[SoundDevice::TypePORTAUDIO_WDMKS] = -1;
+	} else if(!mpt::Windows::Version::IsNT())
+	{ // Win9x
+		typePriorities[SoundDevice::TypeWAVEOUT] = 29;
+		typePriorities[SoundDevice::TypeDSOUND] = 28;
+		typePriorities[SoundDevice::TypePORTAUDIO_WMME] = 19;
+		typePriorities[SoundDevice::TypePORTAUDIO_DS] = 18;
+		typePriorities[SoundDevice::TypeASIO] = 1;
+		typePriorities[SoundDevice::TypePORTAUDIO_WDMKS] = -1;
+		typePriorities[SoundDevice::TypePORTAUDIO_WASAPI] = -2;
+	} else if(!mpt::Windows::Version::IsAtLeast(mpt::Windows::Version::WinVista))
+	{ // WinXP
+		typePriorities[SoundDevice::TypeWAVEOUT] = 29;
+		typePriorities[SoundDevice::TypeASIO] = 28;
+		typePriorities[SoundDevice::TypeDSOUND] = 27;
+		typePriorities[SoundDevice::TypePORTAUDIO_WDMKS] = 26;
+		typePriorities[SoundDevice::TypePORTAUDIO_WMME] = 19;
+		typePriorities[SoundDevice::TypePORTAUDIO_DS] = 17;
+		typePriorities[SoundDevice::TypePORTAUDIO_WASAPI] = -1;
+	} else
+	{ // >=Vista
+		typePriorities[SoundDevice::TypeWAVEOUT] = 29;
+		typePriorities[SoundDevice::TypePORTAUDIO_WASAPI] = 28;
+		typePriorities[SoundDevice::TypeASIO] = 27;
+		typePriorities[SoundDevice::TypePORTAUDIO_WDMKS] = 26;
+		typePriorities[SoundDevice::TypePORTAUDIO_WMME] = 19;
+		typePriorities[SoundDevice::TypeDSOUND] = -1;
+		typePriorities[SoundDevice::TypePORTAUDIO_DS] = -2;
 	}
-#endif // NO_DSOUND
-
-	// duplicate devices, only used if [Sound Settings]MorePortaudio=1
-#ifndef NO_PORTAUDIO
-	if(IsComponentAvailable(m_PortAudio))
-	{
-		const std::vector<SoundDevice::Info> infos = CPortaudioDevice::EnumerateDevices(TypePORTAUDIO_WMME);
-		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
-	}
-	if(IsComponentAvailable(m_PortAudio))
-	{
-		const std::vector<SoundDevice::Info> infos = CPortaudioDevice::EnumerateDevices(TypePORTAUDIO_ASIO);
-		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
-	}
-	if(IsComponentAvailable(m_PortAudio))
-	{
-		const std::vector<SoundDevice::Info> infos = CPortaudioDevice::EnumerateDevices(TypePORTAUDIO_DS);
-		m_SoundDevices.insert(m_SoundDevices.end(), infos.begin(), infos.end());
-	}
-#endif // NO_PORTAUDIO
+	std::stable_sort(m_SoundDevices.begin(), m_SoundDevices.end(), CompareType(typePriorities));
 
 	Log(LogDebug, MPT_USTRING("Sound Devices enumerated:"));
 	for(std::size_t i = 0; i < m_SoundDevices.size(); ++i)
 	{
 		Log(LogDebug, mpt::String::Print(MPT_USTRING(" Identifier : %1")   , m_SoundDevices[i].GetIdentifier()));
-		Log(LogDebug, mpt::String::Print(MPT_USTRING("  ID        : %1.%2"), static_cast<uint32>(m_SoundDevices[i].id.GetType()), m_SoundDevices[i].id.GetIndex()));
+		Log(LogDebug, mpt::String::Print(MPT_USTRING("  Type      : %1")   , m_SoundDevices[i].type));
 		Log(LogDebug, mpt::String::Print(MPT_USTRING("  InternalID: %1")   , m_SoundDevices[i].internalID));
 		Log(LogDebug, mpt::String::Print(MPT_USTRING("  API Name  : %1")   , m_SoundDevices[i].apiName));
 		Log(LogDebug, mpt::String::Print(MPT_USTRING("  Name      : %1")   , m_SoundDevices[i].name));
@@ -115,17 +144,28 @@ void Manager::ReEnumerate()
 }
 
 
-SoundDevice::Info Manager::FindDeviceInfo(SoundDevice::ID id) const
-//-----------------------------------------------------------------
+SoundDevice::Manager::GlobalID Manager::GetGlobalID(SoundDevice::Identifier identifier) const
+//-------------------------------------------------------------------------------------------
 {
-	for(std::vector<SoundDevice::Info>::const_iterator it = begin(); it != end(); ++it)
+	for(std::size_t i = 0; i < m_SoundDevices.size(); ++i)
 	{
-		if(it->id == id)
+		if(m_SoundDevices[i].GetIdentifier() == identifier)
 		{
-			return *it;
+			return i;
 		}
 	}
-	return SoundDevice::Info();
+	return ~SoundDevice::Manager::GlobalID();
+}
+
+
+SoundDevice::Info Manager::FindDeviceInfo(SoundDevice::Manager::GlobalID id) const
+//--------------------------------------------------------------------------------
+{
+	if(id > m_SoundDevices.size())
+	{
+		return SoundDevice::Info();
+	}
+	return m_SoundDevices[id];
 }
 
 
@@ -154,15 +194,12 @@ SoundDevice::Info Manager::FindDeviceInfo(SoundDevice::Identifier identifier) co
 SoundDevice::Type ParseType(const SoundDevice::Identifier &identifier)
 //--------------------------------------------------------------------
 {
-	for(int i = 0; i < TypeNUM_DEVTYPES; ++i)
+	std::vector<mpt::ustring> tmp = mpt::String::Split<mpt::ustring>(identifier, MPT_USTRING("_"));
+	if(tmp.size() == 0)
 	{
-		const mpt::ustring api = SoundDevice::TypeToString(static_cast<SoundDevice::Type>(i));
-		if(identifier.find(api) == 0)
-		{
-			return static_cast<SoundDevice::Type>(i);
-		}
+		return SoundDevice::Type();
 	}
-	return TypeINVALID;
+	return tmp[0];
 }
 
 
@@ -185,47 +222,31 @@ SoundDevice::Info Manager::FindDeviceInfoBestMatch(SoundDevice::Identifier ident
 		}
 	}
 	const SoundDevice::Type type = ParseType(identifier);
-	switch(type)
+	if(type == TypePORTAUDIO_WASAPI)
 	{
-		case TypePORTAUDIO_WASAPI:
-			// WASAPI devices might change names if a different connector jack is used.
-			// In order to avoid defaulting to wave mapper in that case,
-			// just find the first WASAPI device.
-			for(std::vector<SoundDevice::Info>::const_iterator it = begin(); it != end(); ++it)
+		// WASAPI devices might change names if a different connector jack is used.
+		// In order to avoid defaulting to wave mapper in that case,
+		// just find the first WASAPI device.
+		for(std::vector<SoundDevice::Info>::const_iterator it = begin(); it != end(); ++it)
+		{
+			if((it->type == TypePORTAUDIO_WASAPI) && !IsDeviceUnavailable(it->GetIdentifier()))
 			{
-				if((it->id.GetType() == TypePORTAUDIO_WASAPI) && !IsDeviceUnavailable(it->GetIdentifier()))
-				{
-					return *it;
-				}
+				return *it;
 			}
-			// default to first device
-			return *begin();
-			break;
-		case TypeWAVEOUT:
-		case TypeDSOUND:
-		case TypePORTAUDIO_WMME:
-		case TypePORTAUDIO_DS:
-		case TypeASIO:
-		case TypePORTAUDIO_WDMKS:
-		case TypePORTAUDIO_ASIO:
-			if(preferSameType)
-			{
-				for(std::vector<SoundDevice::Info>::const_iterator it = begin(); it != end(); ++it)
-				{
-					if((it->id.GetType() == type) && !IsDeviceUnavailable(it->GetIdentifier()))
-					{
-						return *it;
-					}
-				}
-			} else
-			{
-				// default to first device
-				return *begin();
-			}
-			break;
+		}
 	}
-	// invalid
-	return SoundDevice::Info();
+	if(preferSameType)
+	{
+		for(std::vector<SoundDevice::Info>::const_iterator it = begin(); it != end(); ++it)
+		{
+			if((it->type == type) && !IsDeviceUnavailable(it->GetIdentifier()))
+			{
+				return *it;
+			}
+		}
+	}
+	// default to first device
+	return *begin();
 }
 
 
@@ -318,28 +339,23 @@ SoundDevice::IBase * Manager::CreateSoundDevice(SoundDevice::Identifier identifi
 		return nullptr;
 	}
 	SoundDevice::IBase *result = nullptr;
-	switch(info.id.GetType())
-	{
-	case TypeWAVEOUT: result = new CWaveDevice(info); break;
+	if(info.type.empty()) { result = nullptr; }
+	if(info.type == TypeWAVEOUT) { result = new CWaveDevice(info); }
 #ifndef NO_DSOUND
-	case TypeDSOUND: result = new CDSoundDevice(info); break;
+	if(info.type == TypeDSOUND) { result = new CDSoundDevice(info); }
 #endif // NO_DSOUND
 #ifndef NO_ASIO
-	case TypeASIO: result = new CASIODevice(info); break;
+	if(info.type == TypeASIO) { result = new CASIODevice(info); }
 #endif // NO_ASIO
 #ifndef NO_PORTAUDIO
-	case TypePORTAUDIO_WASAPI:
-	case TypePORTAUDIO_WDMKS:
-	case TypePORTAUDIO_WMME:
-	case TypePORTAUDIO_DS:
-	case TypePORTAUDIO_ASIO:
-		if(IsComponentAvailable(m_PortAudio))
-		{
-			result = new CPortaudioDevice(info);
-		}
-		break;
-#endif // NO_PORTAUDIO
+	if(IsComponentAvailable(m_PortAudio))
+	{
+		if(info.type == TypePORTAUDIO_WASAPI) { result = new CPortaudioDevice(info); }
+		if(info.type == TypePORTAUDIO_WDMKS) { result = new CPortaudioDevice(info); }
+		if(info.type == TypePORTAUDIO_WMME) { result = new CPortaudioDevice(info); }
+		if(info.type == TypePORTAUDIO_DS) { result = new CPortaudioDevice(info); }
 	}
+#endif // NO_PORTAUDIO
 	if(!result)
 	{
 		return nullptr;
@@ -367,6 +383,66 @@ Manager::~Manager()
 //-----------------
 {
 	return;
+}
+
+
+namespace Legacy
+{
+SoundDevice::Info FindDeviceInfo(SoundDevice::Manager &manager, SoundDevice::Legacy::ID id)
+{
+	if(manager.GetDeviceInfos().empty())
+	{
+		return SoundDevice::Info();
+	}
+	SoundDevice::Type type = SoundDevice::Type();
+	switch(id & SoundDevice::Legacy::MaskType)
+	{
+		case SoundDevice::Legacy::TypeWAVEOUT:
+			type = SoundDevice::TypeWAVEOUT;
+			break;
+		case SoundDevice::Legacy::TypeDSOUND:
+			type = SoundDevice::TypeDSOUND;
+			break;
+		case SoundDevice::Legacy::TypeASIO:
+			type = SoundDevice::TypeASIO;
+			break;
+		case SoundDevice::Legacy::TypePORTAUDIO_WASAPI:
+			type = SoundDevice::TypePORTAUDIO_WASAPI;
+			break;
+		case SoundDevice::Legacy::TypePORTAUDIO_WDMKS:
+			type = SoundDevice::TypePORTAUDIO_WDMKS;
+			break;
+		case SoundDevice::Legacy::TypePORTAUDIO_WMME:
+			type = SoundDevice::TypePORTAUDIO_WMME;
+			break;
+		case SoundDevice::Legacy::TypePORTAUDIO_DS:
+			type = SoundDevice::TypePORTAUDIO_DS;
+			break;
+	}
+	if(type.empty())
+	{	// fallback to first device
+		return *manager.begin();
+	}
+	std::size_t index = static_cast<uint8>(id & SoundDevice::Legacy::MaskIndex);
+	std::size_t seenDevicesOfDesiredType = 0;
+	for(std::vector<SoundDevice::Info>::const_iterator it = manager.begin(); it != manager.end(); ++it)
+	{
+		if(it->type == type)
+		{
+			if(seenDevicesOfDesiredType == index)
+			{
+				if(!it->IsValid())
+				{	// fallback to first device
+					return *manager.begin();
+				}
+				return *it;
+			}
+			seenDevicesOfDesiredType++;
+		}
+	}
+	// default to first device
+	return *manager.begin();
+}
 }
 
 

mptrack/Mpdlgs.cpp
@@ -344,7 +344,6 @@ void COptionsSoundcard::UpdateEverything()
 		m_CbnDevice.ResetContent();
 		m_CbnDevice.SetImageList(&CMainFrame::GetMainFrame()->m_MiscIcons);
 
-		COMBOBOXEXITEM cbi;
 		UINT iItem = 0;
 
 		for(std::vector<SoundDevice::Info>::const_iterator it = theApp.GetSoundDevicesManager()->begin(); it != theApp.GetSoundDevicesManager()->end(); ++it)
@@ -352,7 +351,7 @@ void COptionsSoundcard::UpdateEverything()
 
 			if(!TrackerSettings::Instance().m_MorePortaudio)
 			{
-				if(it->id.GetType() == SoundDevice::TypePORTAUDIO_ASIO || it->id.GetType() == SoundDevice::TypePORTAUDIO_DS || it->id.GetType() == SoundDevice::TypePORTAUDIO_WMME)
+				if(it->type == SoundDevice::TypePORTAUDIO_DS || it->type == SoundDevice::TypePORTAUDIO_WMME)
 				{
 					// skip those portaudio apis that are already implemented via our own SoundDevice class
 					// can be overwritten via [Sound Settings]MorePortaudio=1
@@ -361,53 +360,60 @@ void COptionsSoundcard::UpdateEverything()
 			}
 
 			{
+				COMBOBOXEXITEM cbi;
+				MemsetZero(cbi);
 				CString name = mpt::ToCString(it->name);
-				cbi.mask = CBEIF_IMAGE | CBEIF_LPARAM | CBEIF_TEXT | CBEIF_SELECTEDIMAGE | CBEIF_OVERLAY;
+				cbi.mask = CBEIF_LPARAM | CBEIF_TEXT | CBEIF_IMAGE | CBEIF_SELECTEDIMAGE | CBEIF_OVERLAY;
 				cbi.iItem = iItem;
 				cbi.cchTextMax = 0;
-				switch(it->id.GetType())
+				if(it->type == SoundDevice::TypeWAVEOUT)
 				{
-				case SoundDevice::TypeWAVEOUT:
-				case SoundDevice::TypePORTAUDIO_WMME:
 					cbi.iImage = IMAGE_WAVEOUT;
-					break;
-				case SoundDevice::TypeDSOUND:
-				case SoundDevice::TypePORTAUDIO_DS:
+				} else if(it->type == SoundDevice::TypePORTAUDIO_WMME)
+				{
+					cbi.iImage = IMAGE_WAVEOUT;
+				} else if(it->type == SoundDevice::TypeDSOUND)
+				{
 					cbi.iImage = IMAGE_DIRECTX;
-					break;
-				case SoundDevice::TypeASIO:
-				case SoundDevice::TypePORTAUDIO_ASIO:
+				} else if(it->type == SoundDevice::TypePORTAUDIO_DS)
+				{
+					cbi.iImage = IMAGE_DIRECTX;
+				} else if(it->type == SoundDevice::TypeASIO)
+				{
 					cbi.iImage = IMAGE_ASIO;
-					break;
-				case SoundDevice::TypePORTAUDIO_WASAPI:
+				} else if(it->type == SoundDevice::TypePORTAUDIO_WASAPI)
+				{
 					// No real image available for now,
 					// prepend API name to name and misuse another icon
 					cbi.iImage = IMAGE_SAMPLEMUTE;
-					name = mpt::ToCString(it->apiName) + TEXT(" - ") + name;
-					break;
-				case SoundDevice::TypePORTAUDIO_WDMKS:
+					name = mpt::ToCString(it->apiName) + _T(" - ") + name;
+				} else if(it->type == SoundDevice::TypePORTAUDIO_WDMKS)
+				{
 					// No real image available for now,
 					// prepend API name to name and misuse another icon.
 					cbi.iImage = IMAGE_CHIP;
-					name = mpt::ToCString(it->apiName) + TEXT(" - ") + name;
-					break;
-				default:
-					cbi.iImage = IMAGE_WAVEOUT;
-					break;
+					name = mpt::ToCString(it->apiName) + _T(" - ") + name;
+				} else
+				{
+					cbi.mask &= ~(CBEIF_IMAGE | CBEIF_SELECTEDIMAGE | CBEIF_OVERLAY);
+					std::vector<mpt::ustring> api = it->apiPath;
+					api.push_back(it->apiName);
+					name = mpt::ToCString(mpt::String::Combine(api, MPT_USTRING(" - "))) + _T(" - ") + name;
 				}
 				if(it->isDefault)
 				{
-					name += " (Default)";
+					name += _T(" (Default)");
 				}
 				cbi.iSelectedImage = cbi.iImage;
 				cbi.iOverlay = cbi.iImage;
 				cbi.iIndent = 0;
-				cbi.lParam = it->id.GetIdRaw();
-				TCHAR tmp[256];
-				_tcscpy(tmp, name);
+				cbi.lParam = theApp.GetSoundDevicesManager()->GetGlobalID(it->GetIdentifier());
+				TCHAR tmp[1024];
+				MemsetZero(tmp);
+				lstrcpyn(tmp, name, 1023);
 				cbi.pszText = tmp;
 				int pos = m_CbnDevice.InsertItem(&cbi);
-				if(cbi.lParam == m_CurrentDeviceInfo.id.GetIdRaw())
+				if(static_cast<SoundDevice::Manager::GlobalID>(cbi.lParam) == theApp.GetSoundDevicesManager()->GetGlobalID(m_CurrentDeviceInfo.GetIdentifier()))
 				{
 					m_CbnDevice.SetCurSel(pos);
 				}
@@ -619,7 +625,7 @@ void COptionsSoundcard::OnDeviceChanged()
 	int n = m_CbnDevice.GetCurSel();
 	if(n >= 0)
 	{
-		SetDevice(theApp.GetSoundDevicesManager()->FindDeviceInfo(SoundDevice::ID::FromIdRaw(m_CbnDevice.GetItemData(n))).GetIdentifier());
+		SetDevice(theApp.GetSoundDevicesManager()->FindDeviceInfo(static_cast<SoundDevice::Manager::GlobalID>(m_CbnDevice.GetItemData(n))).GetIdentifier());
 		UpdateDevice();
 		OnSettingsChanged();
 	}
@@ -646,7 +652,7 @@ void COptionsSoundcard::OnSoundCardDriverPanel()
 //----------------------------------------------
 {
 	theApp.GetSoundDevicesManager()->OpenDriverSettings(
-		theApp.GetSoundDevicesManager()->FindDeviceInfo(SoundDevice::ID::FromIdRaw(m_CbnDevice.GetItemData(m_CbnDevice.GetCurSel()))).GetIdentifier(),
+		theApp.GetSoundDevicesManager()->FindDeviceInfo(static_cast<SoundDevice::Manager::GlobalID>(m_CbnDevice.GetItemData(m_CbnDevice.GetCurSel()))).GetIdentifier(),
 		CMainFrame::GetMainFrame(),
 		CMainFrame::GetMainFrame()->gpSoundDevice
 		);

mptrack/Mptrack.cpp
@@ -1003,7 +1003,7 @@ BOOL CTrackApp::InitInstance()
 	if(TrackerSettings::Instance().m_SoundDeviceSettingsUseOldDefaults)
 	{
 		// get the old default device
-		TrackerSettings::Instance().SetSoundDeviceIdentifier(m_pSoundDevicesManager->FindDeviceInfo(TrackerSettings::Instance().m_SoundDeviceID_DEPRECATED).GetIdentifier());
+		TrackerSettings::Instance().SetSoundDeviceIdentifier(SoundDevice::Legacy::FindDeviceInfo(*m_pSoundDevicesManager, TrackerSettings::Instance().m_SoundDeviceID_DEPRECATED).GetIdentifier());
 		// apply old global sound device settings to each found device
 		for(std::vector<SoundDevice::Info>::const_iterator it = m_pSoundDevicesManager->begin(); it != m_pSoundDevicesManager->end(); ++it)
 		{

mptrack/TrackerSettings.cpp
@@ -177,7 +177,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 	, m_SoundSettingsOpenDeviceAtStartup(conf, "Sound Settings", "OpenDeviceAtStartup", false)
 	, m_SoundSettingsStopMode(conf, "Sound Settings", "StopMode", SoundDeviceStopModeClosed)
 	, m_SoundDeviceSettingsUseOldDefaults(false)
-	, m_SoundDeviceID_DEPRECATED(SoundDevice::ID())
+	, m_SoundDeviceID_DEPRECATED(SoundDevice::Legacy::ID())
 	, m_SoundDeviceDirectSoundOldDefaultIdentifier(false)
 	, m_SoundDeviceIdentifier(conf, "Sound Settings", "Device", SoundDevice::Identifier())
 	, m_SoundDevicePreferSameTypeIfDeviceUnavailable(conf, "Sound Settings", "PreferSameTypeIfDeviceUnavailable", false)
@@ -426,7 +426,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 	}
 	if(storedVersion < MAKE_VERSION_NUMERIC(1,22,07,04))
 	{
-		m_SoundDeviceID_DEPRECATED = conf.Read<SoundDevice::ID>("Sound Settings", "WaveDevice", SoundDevice::ID());
+		m_SoundDeviceID_DEPRECATED = conf.Read<SoundDevice::Legacy::ID>("Sound Settings", "WaveDevice", SoundDevice::Legacy::ID());
 		Setting<uint32> m_BufferLength_DEPRECATED(conf, "Sound Settings", "BufferLength", 50);
 		Setting<uint32> m_LatencyMS(conf, "Sound Settings", "Latency", Util::Round<int32>(SoundDevice::Settings().Latency * 1000.0));
 		Setting<uint32> m_UpdateIntervalMS(conf, "Sound Settings", "UpdateInterval", Util::Round<int32>(SoundDevice::Settings().UpdateInterval * 1000.0));
@@ -441,7 +441,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 			{
 				if(m_BufferLength_DEPRECATED < 1) m_BufferLength_DEPRECATED = 1; // 1ms
 				if(m_BufferLength_DEPRECATED > 1000) m_BufferLength_DEPRECATED = 1000; // 1sec
-				if(m_SoundDeviceID_DEPRECATED.GetType() == SoundDevice::TypeASIO)
+				if((m_SoundDeviceID_DEPRECATED & SoundDevice::Legacy::MaskType) == SoundDevice::Legacy::TypeASIO)
 				{
 					m_LatencyMS = m_BufferLength_DEPRECATED;
 					m_UpdateIntervalMS = m_BufferLength_DEPRECATED / 8;
@@ -649,7 +649,7 @@ public:
 			ChannelMapping = SoundDevice::ChannelMapping(Channels);
 		}
 		// store informational data (not read back, just to allow the user to mock with the raw ini file)
-		conf.Write(L"Sound Settings", mpt::ToWide(deviceInfo.GetIdentifier()) + L"_" + L"ID", deviceInfo.id);
+		conf.Write(L"Sound Settings", mpt::ToWide(deviceInfo.GetIdentifier()) + L"_" + L"Type", deviceInfo.type);
 		conf.Write(L"Sound Settings", mpt::ToWide(deviceInfo.GetIdentifier()) + L"_" + L"InternalID", deviceInfo.internalID);
 		conf.Write(L"Sound Settings", mpt::ToWide(deviceInfo.GetIdentifier()) + L"_" + L"API", deviceInfo.apiName);
 		conf.Write(L"Sound Settings", mpt::ToWide(deviceInfo.GetIdentifier()) + L"_" + L"Name", deviceInfo.name);

mptrack/TrackerSettings.h
@@ -261,9 +261,6 @@ template<> inline PLUGVOLUMEHANDLING FromSettingValue(const SettingValue &val)
 template<> inline SettingValue ToSettingValue(const std::vector<uint32> &val) { return mpt::String::Combine(val); }
 template<> inline std::vector<uint32> FromSettingValue(const SettingValue &val) { return mpt::String::Split<uint32>(val); }
 
-template<> inline SettingValue ToSettingValue(const SoundDevice::ID &val) { return SettingValue(int32(val.GetIdRaw())); }
-template<> inline SoundDevice::ID FromSettingValue(const SettingValue &val) { return SoundDevice::ID::FromIdRaw(val.as<int32>()); }
-
 template<> inline SettingValue ToSettingValue(const SampleFormat &val) { return SettingValue(int32(val.value)); }
 template<> inline SampleFormat FromSettingValue(const SettingValue &val) { return SampleFormatEnum(val.as<int32>()); }
 
@@ -442,7 +439,7 @@ public:
 	Setting<SoundDeviceStopMode> m_SoundSettingsStopMode;
 
 	bool m_SoundDeviceSettingsUseOldDefaults;
-	SoundDevice::ID m_SoundDeviceID_DEPRECATED;
+	SoundDevice::Legacy::ID m_SoundDeviceID_DEPRECATED;
 	SoundDevice::Settings m_SoundDeviceSettingsDefaults;
 	SoundDevice::Settings GetSoundDeviceSettingsDefaults() const;
 	bool m_SoundDeviceDirectSoundOldDefaultIdentifier;

commit 5919bb38d4d86da0e3f577c142d088a130000a94

[Fix] sounddev: Importing pre ~1.23.00 sound device settings was broken since r5046 (~1.25.00).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8498 56274372-70c3-4bfc-bfc3-4c3a0b034d27

sounddev/SoundDevice.h
@@ -539,6 +539,8 @@ namespace Legacy
 typedef uint16 ID;
 static const SoundDevice::Legacy::ID MaskType = 0xff00;
 static const SoundDevice::Legacy::ID MaskIndex = 0x00ff;
+static const int ShiftType = 8;
+static const int ShiftIndex = 0;
 static const SoundDevice::Legacy::ID TypeWAVEOUT          = 0;
 static const SoundDevice::Legacy::ID TypeDSOUND           = 1;
 static const SoundDevice::Legacy::ID TypeASIO             = 2;

sounddev/SoundDeviceManager.cpp
@@ -475,7 +475,7 @@ SoundDevice::Info FindDeviceInfo(SoundDevice::Manager &manager, SoundDevice::Leg
 		return SoundDevice::Info();
 	}
 	SoundDevice::Type type = SoundDevice::Type();
-	switch(id & SoundDevice::Legacy::MaskType)
+	switch((id & SoundDevice::Legacy::MaskType) >> SoundDevice::Legacy::ShiftType)
 	{
 		case SoundDevice::Legacy::TypeWAVEOUT:
 			type = SoundDevice::TypeWAVEOUT;
@@ -503,7 +503,7 @@ SoundDevice::Info FindDeviceInfo(SoundDevice::Manager &manager, SoundDevice::Leg
 	{	// fallback to first device
 		return *manager.begin();
 	}
-	std::size_t index = static_cast<uint8>(id & SoundDevice::Legacy::MaskIndex);
+	std::size_t index = static_cast<uint8>((id & SoundDevice::Legacy::MaskIndex) >> SoundDevice::Legacy::ShiftIndex);
 	std::size_t seenDevicesOfDesiredType = 0;
 	for(const auto &info : manager)
 	{