e3f1b883fe1dc9a969de4a444fafbcebeeca7eff
Johannes Schultz <sagamusix@openmpt.org>
2017-08-10 00:28:07 +0000

    Merged revision(s) 8733 from trunk/OpenMPT: [Fix] Pattern tab: Amplification now works correctly for plugin-based instruments with a global volume other than 64 (https://bugs.openmpt.org/view.php?id=1002). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.26@8734 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/View_pat.cpp
+++ mptrack/View_pat.cpp
@@ -3121,110 +3121,45 @@ void CViewPattern::OnPatternAmplify()
 		}
 
 		// Volume memory for each channel.
-		std::vector<uint8> chvol(lastChannel + 1, 64);
+		std::vector<ModCommand::VOL> chvol(lastChannel + 1, 64);
 
+		// First, fill the volume memory in case we start the selection before some note
 		for(ROWINDEX nRow = firstRow; nRow <= lastRow; nRow++)
 		{
 			ModCommand *m = pSndFile->Patterns[m_nPattern].GetpModCommand(nRow, firstChannel);
 			for(CHANNELINDEX nChn = firstChannel; nChn <= lastChannel; nChn++, m++)
 			{
-				if(m->command == CMD_VOLUME && m->param <= 64)
-				{
-					chvol[nChn] = m->param;
-					break;
-				}
-				if(m->volcmd == VOLCMD_VOLUME)
-				{
+				if(m->command == CMD_VOLUME)
+					chvol[nChn] = std::min(m->param, ModCommand::PARAM(64));
+				else if(m->volcmd == VOLCMD_VOLUME)
 					chvol[nChn] = m->vol;
-					break;
-				}
-				if(m->IsNote() && m->instr != 0)
-				{
-					SAMPLEINDEX nSmp = m->instr;
-					if(pSndFile->GetNumInstruments())
-					{
-						if(nSmp <= pSndFile->GetNumInstruments() && pSndFile->Instruments[nSmp] != nullptr)
-						{
-							nSmp = pSndFile->Instruments[nSmp]->Keyboard[m->note];
-							if(!nSmp) chvol[nChn] = 64;	// hack for instruments without samples
-						} else
-						{
-							nSmp = 0;
-						}
-					}
-					if(nSmp > 0 && nSmp <= pSndFile->GetNumSamples())
-					{
-						chvol[nChn] = (uint8)(pSndFile->GetSample(nSmp).nVolume / 4);
-						break;
-					} else
-					{
-						//nonexistant sample and no volume present in patten? assume volume=64.
-						if(useVolCol)
-						{
-							m->volcmd = VOLCMD_VOLUME;
-							m->vol = 64;
-						} else
-						{
-							m->command = CMD_VOLUME;
-							m->param = 64;
-						}
-						chvol[nChn] = 64;
-						break;
-					}
-				}
+				else if(m->instr != 0)
+					chvol[nChn] = static_cast<ModCommand::VOL>(GetDefaultVolume(*m));
 			}
 		}
-
+	
+		// Now do the actual amplification
 		for(ROWINDEX nRow = firstRow; nRow <= lastRow; nRow++)
 		{
 			ModCommand *m = pSndFile->Patterns[m_nPattern].GetpModCommand(nRow, firstChannel);
 			const int cy = lastRow - firstRow + 1; // total rows (for fading)
 			for(CHANNELINDEX nChn = firstChannel; nChn <= lastChannel; nChn++, m++)
 			{
-				if(m->volcmd == VOLCMD_NONE && m->command != CMD_VOLUME
-					&& m->IsNote() && m->instr)
-				{
-					SAMPLEINDEX nSmp = m->instr;
-					bool overrideSampleVol = false;
-					if(pSndFile->GetNumInstruments())
-					{
-						if(nSmp <= pSndFile->GetNumInstruments() && pSndFile->Instruments[nSmp] != nullptr)
-						{
-							nSmp = pSndFile->Instruments[nSmp]->Keyboard[m->note];
-							// hack for instruments without samples
-							if(!nSmp)
-							{
-								nSmp = 1;
-								overrideSampleVol = true;
-							}
-						} else
-						{
-							nSmp = 0;
-						}
-					}
-					if(nSmp > 0 && (nSmp <= pSndFile->GetNumSamples()))
-					{
-						if(useVolCol)
-						{
-							m->volcmd = VOLCMD_VOLUME;
-							m->vol = static_cast<ModCommand::VOL>((overrideSampleVol) ? 64 : pSndFile->GetSample(nSmp).nVolume / 4u);
-						} else
-						{
-							m->command = CMD_VOLUME;
-							m->param = static_cast<ModCommand::PARAM>((overrideSampleVol) ? 64 : pSndFile->GetSample(nSmp).nVolume / 4u);
-						}
-					}
-				}
+				if(m->command == CMD_VOLUME)
+					chvol[nChn] = std::min(m->param, ModCommand::PARAM(64));
+				else if(m->volcmd == VOLCMD_VOLUME)
+					chvol[nChn] = m->vol;
+				else if(m->instr != 0)
+					chvol[nChn] = static_cast<ModCommand::VOL>(GetDefaultVolume(*m));
 
-				if(m->volcmd == VOLCMD_VOLUME) chvol[nChn] = m->vol;
-
-				if((dlg.m_bFadeIn || dlg.m_bFadeOut) && m->command != CMD_VOLUME && m->volcmd == VOLCMD_NONE)
+				if(dlg.m_bFadeIn || dlg.m_bFadeOut || (m->IsNote() && m->instr != 0))
 				{
-					if(useVolCol)
+					// Insert new volume commands where necessary
+					if(useVolCol && m->volcmd == VOLCMD_NONE)
 					{
 						m->volcmd = VOLCMD_VOLUME;
 						m->vol = chvol[nChn];
-					} else
+					} else if(!useVolCol && m->command == CMD_NONE)
 					{
 						m->command = CMD_VOLUME;
 						m->param = chvol[nChn];
