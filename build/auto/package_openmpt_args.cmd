dd0d824cbbdab2a0ba6a0f29c2dd999b3912dfdc
Jörn Heusipp <manx@openmpt.org>
2017-08-12 13:46:45 +0000

    [Imp] build: Support building .zip packages automatically as an alternative to .7z. That way, we can automatically build release packages as .zip files instead of .7z later on. Test builds will stay with .7z for size reasons.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8758 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_openmpt_args.cmd
+++ build/auto/package_openmpt_args.cmd
@@ -15,6 +15,9 @@ call build\auto\helper_get_openmpt_version.cmd
 
 set MPT_REVISION=%OPENMPT_VERSION%-%SVNVERSION%
 
+set MPT_PKG_FORMAT=%5
+if "%MPT_PKG_FORMAT%" == "" set MPT_PKG_FORMAT=zip
+
 
 
 cd bin || goto error
@@ -26,8 +29,8 @@ mkdir openmpt\dbg.%MPT_DIST_VARIANT%
 mkdir openmpt\dbg.%MPT_DIST_VARIANT%\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%
 rmdir /s /q openmpt-%MPT_DIST_VARIANT%
 del /f /q openmpt-%MPT_DIST_VARIANT%.tar
-del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%.7z
-del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%-symbols.7z
+del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%.%MPT_PKG_FORMAT%
+del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%-symbols.%MPT_PKG_FORMAT%
 mkdir openmpt-%MPT_DIST_VARIANT%
 cd openmpt-%MPT_DIST_VARIANT% || goto error
 copy /y ..\..\LICENSE .\LICENSE.txt || goto error
@@ -44,7 +47,7 @@ copy /y ..\..\bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%
 copy /y ..\..\bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET%\PluginBridge32.exe .\ || goto error
 copy /y ..\..\bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET%\PluginBridge64.exe .\ || goto error
 copy /y ..\..\bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET%\openmpt-wine-support.zip .\ || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\openmpt\bin.%MPT_DIST_VARIANT%\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%.7z ^
+"C:\Program Files\7-Zip\7z.exe" a -t%MPT_PKG_FORMAT% -mx=9 ..\openmpt\bin.%MPT_DIST_VARIANT%\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%.%MPT_PKG_FORMAT% ^
  LICENSE.txt ^
  Licenses ^
  mptrack.exe ^
@@ -55,11 +58,11 @@ copy /y ..\..\bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%
  openmpt-wine-support.zip ^
  extraKeymaps ^
  || goto error
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx=9 ..\openmpt\dbg.%MPT_DIST_VARIANT%\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%-symbols.7z mptrack.pdb || goto error
+"C:\Program Files\7-Zip\7z.exe" a -t%MPT_PKG_FORMAT% -mx=9 ..\openmpt\dbg.%MPT_DIST_VARIANT%\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%-symbols.%MPT_PKG_FORMAT% mptrack.pdb || goto error
 cd .. || goto error
 "C:\Program Files\7-Zip\7z.exe" a -ttar openmpt-%MPT_DIST_VARIANT%.tar openmpt || goto error
-del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%.7z
-del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%-symbols.7z
+del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%.%MPT_PKG_FORMAT%
+del /f /q openmpt-%MPT_DIST_VARIANT%-%MPT_REVISION%-symbols.%MPT_PKG_FORMAT%
 rmdir /s /q openmpt-%MPT_DIST_VARIANT%
 cd .. || goto error
 
