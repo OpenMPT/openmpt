af8ba77621a3aed4133a3d0be9b057e3215a95f7
Johannes Schultz <sagamusix@openmpt.org>
2017-08-14 14:14:44 +0000

    [Fix] Pattern tab: Resizing a pattern did not update the row numbers properly in other tabs.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8769 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/View_pat.cpp
+++ mptrack/View_pat.cpp
@@ -247,7 +247,7 @@ bool CViewPattern::SetCurrentPattern(PATTERNINDEX pat, ROWINDEX row)
 		SetScrollPos(SB_VERT, (int)GetCurrentRow() * GetColumnHeight());
 
 	UpdateScrollPos();
-	InvalidatePattern(true);
+	InvalidatePattern(true, true);
 	SendCtrlMessage(CTRLMSG_PATTERNCHANGED, m_nPattern);
 
 	return true;
@@ -623,7 +623,7 @@ bool CViewPattern::SetPlayCursor(PATTERNINDEX pat, ROWINDEX row, uint32 tick)
 	m_nPlayTick = tick;
 
 	if(m_nPlayTick != oldTick && (TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SMOOTHSCROLL))
-		InvalidatePattern(true);
+		InvalidatePattern(true, true);
 	else if (oldPat == m_nPattern)
 		InvalidateRow(oldRow);
 	else if (m_nPlayPat == m_nPattern)
@@ -878,7 +878,7 @@ void CViewPattern::OnGrowSelection()
 	// Adjust selection
 	m_Selection = PatternRect(startSel, PatternCursor(MIN(finalDest, pSndFile->Patterns[m_nPattern].GetNumRows() - 1), endSel));
 
-	InvalidatePattern(false);
+	InvalidatePattern();
 	SetModified();
 	EndWaitCursor();
 	SetFocus();
@@ -979,7 +979,7 @@ void CViewPattern::OnShrinkSelection()
 	// Adjust selection
 	m_Selection = PatternRect(startSel, PatternCursor(MIN(finalDest, pSndFile->Patterns[m_nPattern].GetNumRows() - 1), endSel));
 
-	InvalidatePattern(false);
+	InvalidatePattern();
 	SetModified();
 	EndWaitCursor();
 	SetFocus();
@@ -1344,7 +1344,7 @@ void CViewPattern::OnLButtonUp(UINT nFlags, CPoint point)
 					// Number of channels changed: Update channel headers and other information.
 					SetCurrentPattern(m_nPattern);
 				}
-				InvalidatePattern(true);
+				InvalidatePattern(true, false);
 				SetModified(false);
 			}
 		}
@@ -1375,7 +1375,7 @@ void CViewPattern::OnPatternProperties()
 		if(dlg.DoModal() == IDOK)
 		{
 			UpdateScrollSize();
-			InvalidatePattern(true);
+			InvalidatePattern(true, true);
 			SanitizeCursor();
 			pModDoc->UpdateAllViews(this, PatternHint(pat).Data(), this);
 		}
@@ -2030,7 +2030,7 @@ void CViewPattern::PatternStep(ROWINDEX row)
 
 		// In case we were previously in smooth scrolling mode during live playback, the pattern might be misaligned.
 		if(GetSmoothScrollOffset() != 0)
-			InvalidatePattern(true);
+			InvalidatePattern(true, true);
 
 		// Cut instruments/samples in virtual channels
 		for(CHANNELINDEX i = pSndFile->GetNumChannels(); i < MAX_CHANNELS; i++)
@@ -3030,7 +3030,7 @@ void CViewPattern::UndoRedo(bool undo)
 				SetCurrentPattern(pat);
 			} else
 			{
-				InvalidatePattern(true);
+				InvalidatePattern(true, true);
 			}
 			SetModified(false);
 			SanitizeCursor();
@@ -3261,7 +3261,7 @@ LRESULT CViewPattern::OnPlayerNotify(Notification *pnotify)
 						if(nPat != m_nPattern)
 							SetCurrentPattern(nPat, nRow);
 						else if(TrackerSettings::Instance().m_dwPatternSetup & PATTERN_SHOWPREVIOUS)
-							InvalidatePattern(true);	// Redraw previous / next pattern
+							InvalidatePattern(true, true);	// Redraw previous / next pattern
 
 						if (nOrd < pSndFile->Order().GetLength())
 						{
@@ -3725,14 +3725,14 @@ LRESULT CViewPattern::OnModViewMsg(WPARAM wParam, LPARAM lParam)
 		m_Status.set(psShowVUMeters, !!lParam);
 		UpdateSizes();
 		UpdateScrollSize();
-		InvalidatePattern(true);
+		InvalidatePattern(true, true);
 		break;
 
 	case VIEWMSG_SETPLUGINNAMES:
 		m_Status.set(psShowPluginNames, !!lParam);
 		UpdateSizes();
 		UpdateScrollSize();
-		InvalidatePattern(true);
+		InvalidatePattern(true, true);
 		break;
 
 	case VIEWMSG_DOMIDISPACING:
@@ -3836,7 +3836,7 @@ LRESULT CViewPattern::OnModViewMsg(WPARAM wParam, LPARAM lParam)
 			UpdateSizes();
 			UpdateScrollSize();
 			SetCurrentColumn(m_Cursor);
-			InvalidatePattern(true);
+			InvalidatePattern(true, true);
 		}
 		break;
 	case VIEWMSG_DOSCROLL:
@@ -5511,7 +5511,7 @@ void CViewPattern::OnLockPatternRows()
 	{
 		sndFile.m_lockRowStart = sndFile.m_lockRowEnd = ROWINDEX_INVALID;
 	}
-	InvalidatePattern(true);
+	InvalidatePattern(true, true);
 }
 
 
