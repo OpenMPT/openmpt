cb58922a9cb4144eadfca52bea74a42917f928cd
Jörn Heusipp <manx@openmpt.org>
2017-09-18 12:51:04 +0000

    [Imp] examples: Simplify probing example and remove about half of the ifdefs.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8920 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- examples/libopenmpt_example_c_probe.c
+++ examples/libopenmpt_example_c_probe.c
@@ -17,12 +17,7 @@
 #define LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY 1
 #define LIBOPENMPT_EXAMPLE_PROBE_RESULT_FLOAT  2
 
-#define LIBOPENMPT_EXAMPLE_PROBE_STYLE_CALLBACKS 1
-#define LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX    2
-
-#define LIBOPENMPT_EXAMPLE_PROBE_STYLE  LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX
-
-#define LIBOPENMPT_EXAMPLE_PROBE_RESULT LIBOPENMPT_EXAMPLE_PROBE_RESULT_FLOAT
+#define LIBOPENMPT_EXAMPLE_PROBE_RESULT LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY
 
 #include <memory.h>
 #include <stdint.h>
@@ -31,10 +26,7 @@
 #include <string.h>
 
 #include <libopenmpt/libopenmpt.h>
-
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_CALLBACKS )
 #include <libopenmpt/libopenmpt_stream_callbacks_file.h>
-#endif
 
 static void libopenmpt_example_logfunc( const char * message, void * userdata ) {
 	(void)userdata;
@@ -44,110 +36,6 @@ static void libopenmpt_example_logfunc( const char * message, void * userdata ) 
 	}
 }
 
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX )
-
-typedef struct blob_t {
-	size_t size;
-	void * data;
-} blob_t;
-
-static void free_blob( blob_t * blob ) {
-	if ( blob ) {
-		if ( blob->data ) {
-			free( blob->data );
-			blob->data = 0;
-		}
-		blob->size = 0;
-		free( blob );
-	}
-}
-
-#if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-static blob_t * load_file( const wchar_t * filename, size_t prefix_size ) {
-#else
-static blob_t * load_file( const char * filename, size_t prefix_size ) {
-#endif
-	blob_t * result = 0;
-
-	blob_t * blob = 0;
-	FILE * file = 0;
-	long tell_result = 0;
-
-	blob = malloc( sizeof( blob_t ) );
-	if ( !blob ) {
-		goto fail;
-	}
-	memset( blob, 0, sizeof( blob_t ) );
-
-#if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-	file = _wfopen( filename, L"rb" );
-#else
-	file = fopen( filename, "rb" );
-#endif
-	if ( !file ) {
-		goto fail;
-	}
-
-	if ( fseek( file, 0, SEEK_END ) != 0 ) {
-		goto fail;
-	}
-
-	tell_result = ftell( file );
-	if ( tell_result < 0 ) {
-		goto fail;
-	}
-	if ( (unsigned long long)tell_result > SIZE_MAX ) {
-		goto fail;
-	}
-	blob->size = (size_t)tell_result;
-
-	if ( fseek( file, 0, SEEK_SET ) != 0 ) {
-		goto fail;
-	}
-
-	if ( prefix_size > 0 ) {
-		if ( prefix_size > blob->size ) {
-			prefix_size = blob->size;
-		}
-	} else {
-		prefix_size = blob->size;
-	}
-
-	blob->data = malloc( prefix_size );
-	if ( !blob->data ) {
-		goto fail;
-	}
-	memset( blob->data, 0, prefix_size );
-
-	if ( fread( blob->data, 1, prefix_size, file ) != prefix_size ) {
-		goto fail;
-	}
-
-	result = blob;
-	blob = 0;
-	goto cleanup;
-
-fail:
-
-	result = 0;
-
-cleanup:
-
-	if ( blob ) {
-		free_blob( blob );
-		blob = 0;
-	}
-
-	if ( file ) {
-		fclose( file );
-		file = 0;
-	}
-
-	return result;
-}
-
-#endif
-
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
 static int probe_file( const wchar_t * filename ) {
 #else
@@ -156,21 +44,15 @@ static int probe_file( const char * filename ) {
 
 	int result = 0;
 	int mod_err = OPENMPT_ERROR_OK;
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX )
-	blob_t * blob = 0;
-#elif ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_CALLBACKS )
 	FILE * file = NULL;
-#endif
 
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX )
-	size_t prefix_size = openmpt_probe_file_header_get_recommended_size();
+#if ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY )
+	int result_binary = 0;
 	int probe_file_header_result = OPENMPT_PROBE_FILE_HEADER_RESULT_FAILURE;
+	const char * probe_file_header_result_str = NULL;
 #endif
 #if ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_FLOAT )
 	double probability = 0.0;
-#endif
-#if ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY )
-	int result_binary = 0;
 #endif
 
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
@@ -185,13 +67,6 @@ static int probe_file( const char * filename ) {
 	}
 #endif
 
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX )
-	blob = load_file( filename, prefix_size );
-	if ( !blob ) {
-		fprintf( stderr, "Error: %s\n", "load_file() failed." );
-		goto fail;
-	}
-#elif ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_CALLBACKS )
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
 	file = _wfopen( filename, L"rb" );
 #else
@@ -201,117 +76,59 @@ static int probe_file( const char * filename ) {
 		fprintf( stderr, "Error: %s\n", "fopen() failed." );
 		goto fail;
 	}
-#endif
 
-	#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX )
-
-		if ( prefix_size > blob->size ) {
-			prefix_size = blob->size;
+	#if ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY )
+		probe_file_header_result = openmpt_probe_file_header_from_stream( OPENMPT_PROBE_FILE_HEADER_FLAGS_DEFAULT, openmpt_stream_get_file_callbacks(), file, &libopenmpt_example_logfunc, NULL, &openmpt_error_func_default, NULL, &mod_err, NULL );
+		probe_file_header_result_str = NULL;
+		result_binary = 0;
+		switch ( probe_file_header_result ) {
+			case OPENMPT_PROBE_FILE_HEADER_RESULT_SUCCESS:
+				probe_file_header_result_str = "Success     ";
+				result_binary = 1;
+				break;
+			case OPENMPT_PROBE_FILE_HEADER_RESULT_FAILURE:
+				probe_file_header_result_str = "Failure     ";
+				result_binary = 0;
+				break;
+			case OPENMPT_PROBE_FILE_HEADER_RESULT_WANTMOREDATA:
+				probe_file_header_result_str = "WantMoreData";
+				result_binary = 0;
+				break;
+			case OPENMPT_PROBE_FILE_HEADER_RESULT_ERROR:
+				result_binary = 0;
+				fprintf( stderr, "Error: %s\n", "openmpt_probe_file_header() failed." );
+				goto fail;
+				break;
+			default:
+				result_binary = 0;
+				fprintf( stderr, "Error: %s\n", "openmpt_probe_file_header() failed." );
+				goto fail;
+				break;
 		}
-		#if ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY )
-			probe_file_header_result = openmpt_probe_file_header( OPENMPT_PROBE_FILE_HEADER_FLAGS_DEFAULT, blob->data, prefix_size, blob->size, &libopenmpt_example_logfunc, NULL, &openmpt_error_func_default, NULL, &mod_err, NULL );
-			result_binary = 0;
-			switch ( probe_file_header_result ) {
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_SUCCESS:
-					result_binary = 1;
-					break;
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_FAILURE:
-					result_binary = 0;
-					break;
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_WANTMOREDATA:
-					result_binary = 0;
-					break;
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_ERROR:
-					result_binary = 0;
-					fprintf( stderr, "Error: %s\n", "openmpt_probe_file_header() failed." );
-					goto fail;
-					break;
-				default:
-					result_binary = 0;
-					fprintf( stderr, "Error: %s\n", "openmpt_probe_file_header() failed." );
-					goto fail;
-					break;
-			}
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-			fprintf( stdout, "%s - %ls\n", result_binary ? "Success" : "Failure", filename );
+		fprintf( stdout, "%s - %ls\n", probe_file_header_result_str, filename );
 #else
-			fprintf( stdout, "%s - %s\n", result_binary ? "Success" : "Failure", filename );
+		fprintf( stdout, "%s - %s\n", probe_file_header_result_str, filename );
 #endif
-			if ( result_binary ) {
-				result = 0;
-			} else {
-				result = 1;
-			}
-		#elif ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_FLOAT )
-			probe_file_header_result = openmpt_probe_file_header( OPENMPT_PROBE_FILE_HEADER_FLAGS_DEFAULT, blob->data, prefix_size, blob->size, &libopenmpt_example_logfunc, NULL, &openmpt_error_func_default, NULL, &mod_err, NULL );
-			probability = 0.0;
-			switch ( probe_file_header_result ) {
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_SUCCESS:
-					probability = 1.0;
-					break;
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_FAILURE:
-					probability = 0.0;
-					break;
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_WANTMOREDATA:
-					probability = 0.5;
-					break;
-				case OPENMPT_PROBE_FILE_HEADER_RESULT_ERROR:
-					probability = 0.6;
-					fprintf( stderr, "Error: %s\n", "openmpt_probe_file_header() failed." );
-					goto fail;
-					break;
-				default:
-					probability = 0.6;
-					fprintf( stderr, "Error: %s\n", "openmpt_probe_file_header() failed." );
-					goto fail;
-					break;
-			}
+		if ( result_binary ) {
+			result = 0;
+		} else {
+			result = 1;
+		}
+	#elif ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_FLOAT )
+		probability = openmpt_could_open_probability2( openmpt_stream_get_file_callbacks(), file, 0.25, &libopenmpt_example_logfunc, NULL, &openmpt_error_func_default, NULL, &mod_err, NULL );
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-			fprintf( stdout, "%s: %f - %ls\n", "Result", probability, filename );
+		fprintf( stdout, "%s: %f - %ls\n", "Result", probability, filename );
 #else
-			fprintf( stdout, "%s: %f - %s\n", "Result", probability, filename );
+		fprintf( stdout, "%s: %f - %s\n", "Result", probability, filename );
 #endif
-			if ( probability >= 0.5 ) {
-				result = 0;
-			} else {
-				result = 1;
-			}
-		#else
-			#error "LIBOPENMPT_EXAMPLE_PROBE_RESULT is wrong"
-		#endif
-
-	#elif ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_CALLBACKS )
-
-		#if ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_BINARY )
-			result_binary = ( openmpt_probe_file_header_from_stream( OPENMPT_PROBE_FILE_HEADER_FLAGS_DEFAULT, openmpt_stream_get_file_callbacks(), file, &libopenmpt_example_logfunc, NULL, &openmpt_error_func_default, NULL, &mod_err, NULL ) != OPENMPT_PROBE_FILE_HEADER_RESULT_SUCCESS ) ? 0 : 1;
-#if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-			fprintf( stdout, "%s - %ls\n", result_binary ? "Success" : "Failure", filename );
-#else
-			fprintf( stdout, "%s - %s\n", result_binary ? "Success" : "Failure", filename );
-#endif
-			if ( result_binary ) {
-				result = 0;
-			} else {
-				result = 1;
-			}
-		#elif ( LIBOPENMPT_EXAMPLE_PROBE_RESULT == LIBOPENMPT_EXAMPLE_PROBE_RESULT_FLOAT )
-			probability = openmpt_could_open_probability2( openmpt_stream_get_file_callbacks(), file, 0.25, &libopenmpt_example_logfunc, NULL, &openmpt_error_func_default, NULL, &mod_err, NULL );
-#if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-			fprintf( stdout, "%s: %f - %ls\n", "Result", probability, filename );
-#else
-			fprintf( stdout, "%s: %f - %s\n", "Result", probability, filename );
-#endif
-			if ( probability >= 0.5 ) {
-				result = 0;
-			} else {
-				result = 1;
-			}
-		#else
-			#error "LIBOPENMPT_EXAMPLE_PROBE_RESULT is wrong"
-		#endif
-
+		if ( probability >= 0.5 ) {
+			result = 0;
+		} else {
+			result = 1;
+		}
 	#else
-		#error "LIBOPENMPT_EXAMPLE_PROBE_STYLE is wrong"
+		#error "LIBOPENMPT_EXAMPLE_PROBE_RESULT is wrong"
 	#endif
 
 	goto cleanup;
@@ -322,23 +139,13 @@ fail:
 
 cleanup:
 
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_PREFIX )
-	if ( blob ) {
-		free_blob( blob );
-		blob = 0;
-	}
-#endif
-
-#if ( LIBOPENMPT_EXAMPLE_PROBE_STYLE == LIBOPENMPT_EXAMPLE_PROBE_STYLE_CALLBACKS )
 	if ( file ) {
 		fclose( file );
 		file = 0;
 	}
-#endif
 
 	return result;
 }
-
 
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
 int wmain( int argc, wchar_t * argv[] ) {
@@ -371,3 +178,4 @@ cleanup:
 	return global_result;
 
 }
+
