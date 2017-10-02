OpenMPT/openmpt

commit 2320ec35917ea5f8d4888fa6d985bc292d1e5f72

[Fix] Fix resource exhaustion on Wine when invoking any popup windows via a keyboard hotkey (http://bugs.openmpt.org/view.php?id=713). The fix is done by working around the real problem and just preventing the keyboard hook from re-entering itself. The real fix would involve a lot of refactoring work which remains deferred until some point in the future, for now. The work-around is also only applied on Wine as it prevents global hotkeys from working in modal dialog windows.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@5636 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/MainFrm.cpp
@@ -477,9 +477,31 @@ void CMainFrame::OnDropFiles(HDROP hDropInfo)
 LRESULT CALLBACK CMainFrame::KeyboardProc(int code, WPARAM wParam, LPARAM lParam)
 //-------------------------------------------------------------------------------
 {
-	if (code>=0)
+
+	static bool s_KeyboardHookReentryFlag = false; // work-around for https://bugs.openmpt.org/view.php?id=713
+
+	if(mpt::Windows::Version::IsWine()) // work-around for https://bugs.openmpt.org/view.php?id=713
 	{
-		//Check if textbox has focus
+		// TODO: Properly fix this in same way or another.
+		if(code < 0)
+		{
+			return CallNextHookEx(ghKbdHook, code, wParam, lParam); // required by spec
+		}
+		if(theApp.InGuiThread())
+		{
+			if(s_KeyboardHookReentryFlag)
+			{
+				return -1; // exit early without calling further hooks when re-entering
+			}
+			s_KeyboardHookReentryFlag = true;
+		}
+	}
+
+	bool skipFurtherProcessing = false;
+
+	if(code >= 0)
+	{
+		// Check if textbox has focus
 		bool textboxHasFocus = false;
 		bool handledByTextBox = false;
 
@@ -498,13 +520,33 @@ LRESULT CALLBACK CMainFrame::KeyboardProc(int code, WPARAM wParam, LPARAM lParam
 		if(!handledByTextBox && m_InputHandler->GeneralKeyEvent(kCtxAllContexts, code, wParam, lParam) != kcNull)
 		{
 			if(wParam != VK_ESCAPE)
-				return -1;	// We've handled the keypress. No need to take it further.
-							// Unless it was esc, in which case we need it to close Windows
-							// (there might be other special cases, we'll see.. )
+			{
+				// We've handled the keypress. No need to take it further.
+				// Unless it was esc, in which case we need it to close Windows
+				// (there might be other special cases, we'll see.. )
+				skipFurtherProcessing = true;
+			}
 		}
 	}
 
-	return CallNextHookEx(ghKbdHook, code, wParam, lParam);
+	LRESULT result = 0;
+	if(skipFurtherProcessing)
+	{
+		result = -1;
+	} else
+	{
+		result = CallNextHookEx(ghKbdHook, code, wParam, lParam);
+	}
+
+	if(mpt::Windows::Version::IsWine()) // work-around for https://bugs.openmpt.org/view.php?id=713
+	{
+		if(theApp.InGuiThread())
+		{
+			s_KeyboardHookReentryFlag = false;
+		}
+	}
+
+	return result;
 }
 
 

commit 5d3c7529918904d20e0a6faac33543fe965fe3fd

[Fix] The work-around to create dialog windows repeatedly when invoked via the Keyboard Hook on Wine must still call other chained hook functions. Otherwise almost all keyboard input does not work in dialog windows. Broken since r5636. Fixes <https://bugs.openmpt.org/view.php?id=1037>.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9023 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/MainFrm.cpp
@@ -494,7 +494,9 @@ LRESULT CALLBACK CMainFrame::KeyboardProc(int code, WPARAM wParam, LPARAM lParam
 		{
 			if(s_KeyboardHookReentryFlag)
 			{
-				return -1; // exit early without calling further hooks when re-entering
+				// Exit early without calling our hook when re-entering.
+				// We still need to call other hooks though.
+				return CallNextHookEx(ghKbdHook, code, wParam, lParam); // required by spec
 			}
 			s_KeyboardHookReentryFlag = true;
 		}