OpenMPT/openmpt

commit 1443df4f4d282809ecf9b94e9ce5fb199a547f5d

[New] mptString: Allow decoding strings with an arbitrary Windows codepage encoding. On Windows, we use WinAPI for codepages we do not support directly, on other systems, we fall back to some caller provided charset.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9057 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptString.cpp
@@ -1570,6 +1570,87 @@ CString ToCString(const mpt::ustring &str)
 
 
 
+static mpt::Charset CharsetFromCodePage(uint16 codepage, mpt::Charset fallback, bool * isFallback)
+{
+	mpt::Charset result = fallback;
+	switch(codepage)
+	{
+	case 65001:
+		result = mpt::CharsetUTF8;
+		if(isFallback) *isFallback = false;
+		break;
+	case 20127:
+		result = mpt::CharsetASCII;
+		if(isFallback) *isFallback = false;
+		break;
+	case 28591:
+		result = mpt::CharsetISO8859_1;
+		if(isFallback) *isFallback = false;
+		break;
+	case 28605:
+		result = mpt::CharsetISO8859_15;
+		if(isFallback) *isFallback = false;
+		break;
+	case 437:
+		result = mpt::CharsetCP437;
+		if(isFallback) *isFallback = false;
+		break;
+	case 1252:
+		result = mpt::CharsetWindows1252;
+		if(isFallback) *isFallback = false;
+		break;
+	default:
+		result = fallback;
+		if(isFallback) *isFallback = true;
+		break;
+	}
+	return result;
+}
+
+#if MPT_OS_WINDOWS
+
+static bool TestCodePage(uint16 codepage)
+{
+	return IsValidCodePage(codepage) ? true : false;
+}
+
+static mpt::ustring FromCodePageDirect(uint16 codepage, const std::string & src)
+{
+	int required_size = MultiByteToWideChar(codepage, 0, reinterpret_cast<const char*>(src.c_str()), -1, nullptr, 0);
+	if(required_size <= 0)
+	{
+		return std::wstring();
+	}
+	std::vector<WCHAR> decoded_string(required_size);
+	MultiByteToWideChar(codepage, 0, reinterpret_cast<const char*>(src.c_str()), -1, decoded_string.data(), required_size);
+	return mpt::ToUnicode(std::wstring(decoded_string.data()));
+}
+
+#endif // MPT_OS_WINDOWS
+
+mpt::ustring ToUnicode(uint16 codepage, mpt::Charset fallback, const std::string &str)
+{
+	#if MPT_OS_WINDOWS
+		mpt::ustring result;
+		bool noCharsetMatch = true;
+		mpt::Charset fileCharset = mpt::CharsetFromCodePage(codepage, fallback, &noCharsetMatch);
+		if(noCharsetMatch && TestCodePage(codepage))
+		{
+			result = mpt::FromCodePageDirect(codepage, str);
+		} else
+		{
+			result = mpt::ToUnicode(fileCharset, str);
+		}
+		return result;
+	#else // !MPT_OS_WINDOWS
+		return mpt::ToUnicode(mpt::CharsetFromCodePage(codepage, fallback), str);
+	#endif // MPT_OS_WINDOWS
+}
+
+
+
+
+
 char ToLowerCaseAscii(char c)
 {
 	if('A' <= c && c <= 'Z')

common/mptString.h
@@ -377,6 +377,12 @@ static inline CStringW ToCStringW(const mpt::ustring &str) { return ToCStringW(T
 
 
 
+mpt::ustring ToUnicode(uint16 codepage, mpt::Charset fallback, const std::string &str);
+
+
+
+
+
 #ifdef MODPLUG_TRACKER
 
 #if MPT_OS_WINDOWS

commit 5d274f1805662622e213ce143419124126a1f714

[Fix] MinGW build broke in r9057.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9060 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptString.cpp
@@ -1619,7 +1619,7 @@ static mpt::ustring FromCodePageDirect(uint16 codepage, const std::string & src)
 	int required_size = MultiByteToWideChar(codepage, 0, reinterpret_cast<const char*>(src.c_str()), -1, nullptr, 0);
 	if(required_size <= 0)
 	{
-		return std::wstring();
+		return mpt::ustring();
 	}
 	std::vector<WCHAR> decoded_string(required_size);
 	MultiByteToWideChar(codepage, 0, reinterpret_cast<const char*>(src.c_str()), -1, decoded_string.data(), required_size);