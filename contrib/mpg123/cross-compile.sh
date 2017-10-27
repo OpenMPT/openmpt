67ee4c27f891d47d2ac89b0105e935e8238764f2
Jörn Heusipp <manx@openmpt.org>
2017-10-27 12:18:26 +0000

    [New] Add script to compile libmpg123 with MingW64. libmpg123 tends to be slow with providing compiled Windows binaries, and we cannot build a current (and thus secure) libmpg123 with MSVC ourselves, because older MSVC versions (as used by libopenmpt 0.2) are no longer supported by libmpg123.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.26@9145 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ contrib/mpg123/cross-compile.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+
+# preferrably run on Ubuntu 14.04 / GCC 4.8
+# (other systems will also work though)
+
+
+set -e
+
+
+MPG123VERSION="1.25.7"
+OPENMPTVERSION="1"
+
+
+BASEURL="https://mpg123.de/download/"
+FILE="mpg123-${MPG123VERSION}.tar.bz2"
+DIR="mpg123-${MPG123VERSION}"
+
+
+rm -rf "${FILE}"
+rm -rf dist-x86
+rm -rf dist-x86_64
+mkdir dist-x86
+mkdir dist-x86_64
+rm -rf "libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}.openmpt${OPENMPTVERSION}-x86.zip"
+rm -rf "libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}.openmpt${OPENMPTVERSION}-x86_64.zip"
+
+
+wget "${BASEURL}${FILE}"
+
+
+rm -rf "${DIR}"
+rm -rf "${DIR}-x86"
+tar xvaf "${FILE}"
+mv "${DIR}" "${DIR}-x86"
+cd "${DIR}-x86"
+ ./configure --host=i686-w64-mingw32 --enable-debug=no --enable-modules=no --enable-network=no --disable-static --enable-shared --with-cpu=generic --with-default-audio=win32 --with-audio=win32 --with-optimization=2 LDFLAGS=-static-libgcc
+ make
+ i686-w64-mingw32-strip src/libmpg123/.libs/libmpg123-0.dll
+ cp COPYING ../dist-x86/
+ cp AUTHORS ../dist-x86/
+ cp src/libmpg123/.libs/libmpg123-0.dll ../dist-x86/
+cd ..
+
+
+rm -rf "${DIR}"
+rm -rf "${DIR}-x86_64"
+tar xvaf "${FILE}"
+mv "${DIR}" "${DIR}-x86_64"
+cd "${DIR}-x86_64"
+ ./configure --host=x86_64-w64-mingw32 --enable-debug=no --enable-modules=no --enable-network=no --disable-static --enable-shared --with-cpu=generic --with-default-audio=win32 --with-audio=win32 --with-optimization=2 LDFLAGS=-static-libgcc
+ make
+ x86_64-w64-mingw32-strip src/libmpg123/.libs/libmpg123-0.dll
+ cp COPYING ../dist-x86_64/
+ cp AUTHORS ../dist-x86_64/
+ cp src/libmpg123/.libs/libmpg123-0.dll ../dist-x86_64/
+cd ..
+
+
+cd dist-x86
+ 7z a -tzip -mx=9 ../libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}.openmpt${OPENMPTVERSION}-x86.zip    COPYING AUTHORS libmpg123-0.dll
+cd ..
+
+cd dist-x86_64
+ 7z a -tzip -mx=9 ../libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}.openmpt${OPENMPTVERSION}-x86-64.zip COPYING AUTHORS libmpg123-0.dll
+cd ..
+
+
+echo "mpg123version = \"${MPG123VERSION}.openmpt${OPENMPTVERSION}\""
+echo "mpg123x86size = $(stat --printf="%s" libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}-x86.zip)"
+echo "mpg123x86md5  = \"$(cat libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}-x86.zip | md5sum | awk '{ print $1; }')\""
+echo "mpg123x86sha1 = \"$(cat libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}-x86.zip | sha1sum | awk '{ print $1; }')\""
+echo "mpg123x64size = $(stat --printf="%s" libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}-x86-64.zip)"
+echo "mpg123x64md5  = \"$(cat libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}-x86-64.zip | md5sum | awk '{ print $1; }')\""
+echo "mpg123x64sha1 = \"$(cat libopenmpt-0.2-contrib-mpg123-${MPG123VERSION}-x86-64.zip | sha1sum | awk '{ print $1; }')\""
+
+
+rm -rf "${FILE}"
+rm -rf dist-x86
+rm -rf dist-x86_64
+rm -rf "${DIR}-x86"
+rm -rf "${DIR}-x86_64"
+
