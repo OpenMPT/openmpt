0900febae1295d46eaf822b0e4350175becefc5c
Jörn Heusipp <manx@openmpt.org>
2017-11-01 17:38:35 +0000

    [Mod] openmpt123: Deprecate --output in --render mode and deprecate --output-type in --ui and --batch modes. See r9193 in trunk / 0.4. We should not reject these options in 0.3 because it could break currently working scripts.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.27@9195 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- openmpt123/openmpt123.hpp
+++ openmpt123/openmpt123.hpp
@@ -485,6 +485,16 @@ struct commandlineflags {
 		if ( samplerate < 0 ) {
 			samplerate = commandlineflags().samplerate;
 		}
+		if ( mode == ModeRender && !output_filename.empty() ) {
+			std::ostringstream warning;
+			warning << "Warning: --output is deprecated in --render mode. Use --output-type instead." << std::endl;
+			warnings += warning.str();
+		}
+		if ( mode != ModeRender && output_extension != "wav" ) {
+			std::ostringstream warning;
+			warning << "Warning: --output-type is deprecated in modes other than --render. Use --output instead." << std::endl;
+			warnings += warning.str();
+		}
 		if ( !output_filename.empty() ) {
 			output_extension = get_extension( output_filename );
 		}
