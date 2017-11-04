6c7a66a3d551eae78582ea407eb16df31cd88816
Jörn Heusipp <manx@openmpt.org>
2017-11-04 16:29:07 +0000

    [Fix] Stream Export: Do not default to RAW encoder for lossless. [Fix] Stream Export: Default to the first lossy encoder for lossy. [Ref] Stream Export: Make lossy/lossless encoder selection at least a tiny bit less fragile by introducing MPT_STREAMEXPORT_NUM_LOSSLESS_ENCODERS.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9231 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mod2wave.cpp
+++ mptrack/Mod2wave.cpp
@@ -852,7 +852,7 @@ std::size_t CWaveConvertSettings::FindEncoder(const mpt::ustring &name) const
 			return i;
 		}
 	}
-	return EncoderFactories.size() > 2 ? 2 : 0;
+	return EncoderFactories.size() > MPT_STREAMEXPORT_NUM_LOSSLESS_ENCODERS ? MPT_STREAMEXPORT_NUM_LOSSLESS_ENCODERS : 0;
 }
 
 
@@ -888,7 +888,7 @@ Encoder::Settings &CWaveConvertSettings::GetEncoderSettings() const
 
 CWaveConvertSettings::CWaveConvertSettings(SettingsContainer &conf, const std::vector<EncoderFactoryBase*> &encFactories)
 	: EncoderFactories(encFactories)
-	, EncoderName(conf, "Export", encFactories.size() > 4 ? "LossyEncoder" : "LosslessEncoder", MPT_USTRING(""))
+	, EncoderName(conf, "Export", encFactories.size() > MPT_STREAMEXPORT_NUM_LOSSLESS_ENCODERS ? "LossyEncoder" : "LosslessEncoder", MPT_USTRING(""))
 	, EncoderIndex(FindEncoder(EncoderName))
 	, FinalSampleFormat(SampleFormatInt16)
 	, storedTags(conf)
