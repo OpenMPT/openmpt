OpenMPT/openmpt

commit 5ac80bbeb55c8b55fb7959f74e8b619534f8e558

[Ref] Use std::byte as mpt::byte in C++17 mode. Adjust mpt::byte to be compatible with C++17 std::byte for earlier C++ versions.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9282 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_mo3.cpp
@@ -1542,7 +1542,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 
 				sampleChunk.chunk.Rewind();
 				FileReader::PinnedRawDataView sampleChunkView = sampleChunk.chunk.GetPinnedRawDataView();
-				mergedData.insert(mergedData.end(), sampleChunkView.begin(), sampleChunkView.end());
+				mergedData.insert(mergedData.end(), mpt::byte_cast<const char*>(sampleChunkView.begin()), mpt::byte_cast<const char*>(sampleChunkView.end()));
 
 #endif
 

common/Endianness.h
@@ -84,7 +84,7 @@ namespace detail {
 	static MPT_FORCEINLINE endian_type endian_probe()
 	{
 		STATIC_ASSERT(sizeof(endian_type) == 2);
-		const mpt::byte probe[2] = { 0x12, 0x34 };
+		const mpt::byte probe[2] = { mpt::as_byte(0x12), mpt::as_byte(0x34) };
 		endian_type test;
 		std::memcpy(&test, probe, 2);
 		return test;
@@ -719,9 +719,9 @@ public:
 	MPT_FORCEINLINE mpt::byte GetByte(std::size_t i) const
 	{
 		#if defined(MPT_PLATFORM_LITTLE_ENDIAN)
-			return static_cast<mpt::byte>(EncodeIEEE754binary64(value) >> (i*8));
+			return mpt::byte_cast<mpt::byte>(static_cast<uint8>(EncodeIEEE754binary64(value) >> (i*8)));
 		#elif defined(MPT_PLATFORM_BIG_ENDIAN)
-			return static_cast<mpt::byte>(EncodeIEEE754binary64(value) >> ((8-1-i)*8));
+			return mpt::byte_cast<mpt::byte>(static_cast<uint8>(EncodeIEEE754binary64(value) >> ((8-1-i)*8)));
 		#else
 			STATIC_ASSERT(false);
 		#endif
@@ -1024,7 +1024,8 @@ public:
 	const_unaligned_ptr_le() : mem(nullptr) {}
 	const_unaligned_ptr_le(const const_unaligned_ptr_le<value_type> & other) : mem(other.mem) {}
 	const_unaligned_ptr_le & operator = (const const_unaligned_ptr_le<value_type> & other) { mem = other.mem; return *this; }
-	explicit const_unaligned_ptr_le(const uint8 *mem) : mem(mem) {}
+	explicit const_unaligned_ptr_le(const mpt::byte *mem) : mem(mem) {}
+	explicit const_unaligned_ptr_le(const uint8 *mem) : mem(mpt::byte_cast<const mpt::byte*>(mem)) {}
 	explicit const_unaligned_ptr_le(const char *mem) : mem(mpt::byte_cast<const mpt::byte*>(mem)) {}
 	const_unaligned_ptr_le & operator += (std::size_t count) { mem += count * sizeof(value_type); return *this; }
 	const_unaligned_ptr_le & operator -= (std::size_t count) { mem -= count * sizeof(value_type); return *this; }
@@ -1061,7 +1062,8 @@ public:
 	const_unaligned_ptr_be() : mem(nullptr) {}
 	const_unaligned_ptr_be(const const_unaligned_ptr_be<value_type> & other) : mem(other.mem) {}
 	const_unaligned_ptr_be & operator = (const const_unaligned_ptr_be<value_type> & other) { mem = other.mem; return *this; }
-	explicit const_unaligned_ptr_be(const uint8 *mem) : mem(mem) {}
+	explicit const_unaligned_ptr_be(const mpt::byte *mem) : mem(mem) {}
+	explicit const_unaligned_ptr_be(const uint8 *mem) : mem(mpt::byte_cast<const mpt::byte*>(mem)) {}
 	explicit const_unaligned_ptr_be(const char *mem) : mem(mpt::byte_cast<const mpt::byte*>(mem)) {}
 	const_unaligned_ptr_be & operator += (std::size_t count) { mem += count * sizeof(value_type); return *this; }
 	const_unaligned_ptr_be & operator -= (std::size_t count) { mem -= count * sizeof(value_type); return *this; }
@@ -1093,7 +1095,8 @@ public:
 	const_unaligned_ptr() : mem(nullptr) {}
 	const_unaligned_ptr(const const_unaligned_ptr<value_type> & other) : mem(other.mem) {}
 	const_unaligned_ptr & operator = (const const_unaligned_ptr<value_type> & other) { mem = other.mem; return *this; }
-	explicit const_unaligned_ptr(const uint8 *mem) : mem(mem) {}
+	explicit const_unaligned_ptr(const mpt::byte *mem) : mem(mem) {}
+	explicit const_unaligned_ptr(const uint8 *mem) : mem(mpt::byte_cast<const mpt::byte*>(mem)) {}
 	explicit const_unaligned_ptr(const char *mem) : mem(mpt::byte_cast<const mpt::byte*>(mem)) {}
 	const_unaligned_ptr & operator += (std::size_t count) { mem += count * sizeof(value_type); return *this; }
 	const_unaligned_ptr & operator -= (std::size_t count) { mem -= count * sizeof(value_type); return *this; }

common/FileReader.h
@@ -477,22 +477,22 @@ public:
 	std::string GetRawDataAsString() const
 	{
 		PinnedRawDataView view = GetPinnedRawDataView();
-		return std::string(view.span().begin(), view.span().end());
+		return std::string(mpt::byte_cast<const char*>(view.span().begin()), mpt::byte_cast<const char*>(view.span().end()));
 	}
 	std::string ReadRawDataAsString()
 	{
 		PinnedRawDataView view = ReadPinnedRawDataView();
-		return std::string(view.span().begin(), view.span().end());
+		return std::string(mpt::byte_cast<const char*>(view.span().begin()), mpt::byte_cast<const char*>(view.span().end()));
 	}
 	std::string GetRawDataAsString(std::size_t size) const
 	{
 		PinnedRawDataView view = GetPinnedRawDataView(size);
-		return std::string(view.span().begin(), view.span().end());
+		return std::string(mpt::byte_cast<const char*>(view.span().begin()), mpt::byte_cast<const char*>(view.span().end()));
 	}
 	std::string ReadRawDataAsString(std::size_t size)
 	{
 		PinnedRawDataView view = ReadPinnedRawDataView(size);
-		return std::string(view.span().begin(), view.span().end());
+		return std::string(mpt::byte_cast<const char*>(view.span().begin()), mpt::byte_cast<const char*>(view.span().end()));
 	}
 
 protected:
@@ -1022,7 +1022,7 @@ public:
 		off_t avail = DataContainer().Read(bytes, streamPos, sizeof(bytes)), readPos = 1;
 		
 		size_t writtenBits = 0;
-		uint8 b = bytes[0];
+		uint8 b = mpt::byte_cast<uint8>(bytes[0]);
 		target = (b & 0x7F);
 
 		// Count actual bits used in most significant byte (i.e. this one)
@@ -1036,7 +1036,7 @@ public:
 
 		while(readPos < avail && (b & 0x80) != 0)
 		{
-			b = bytes[readPos++];
+			b = mpt::byte_cast<uint8>(bytes[readPos++]);
 			target <<= 7;
 			target |= (b & 0x7F);
 			writtenBits += 7;

common/misc_util.cpp
@@ -79,10 +79,10 @@ mpt::ustring BinToHex(mpt::const_byte_span src)
 {
 	mpt::ustring result;
 	result.reserve(src.size() * 2);
-	for(uint8 byte : src)
+	for(mpt::byte byte : src)
 	{
-		result.push_back(EncodeNibble[(byte & 0xf0) >> 4]);
-		result.push_back(EncodeNibble[byte & 0x0f]);
+		result.push_back(EncodeNibble[(mpt::byte_cast<uint8>(byte) & 0xf0) >> 4]);
+		result.push_back(EncodeNibble[mpt::byte_cast<uint8>(byte) & 0x0f]);
 	}
 	return result;
 }
@@ -98,7 +98,7 @@ std::vector<mpt::byte> HexToBin(const mpt::ustring &src)
 		{
 			return result;
 		}
-		result.push_back(byte);
+		result.push_back(mpt::byte_cast<mpt::byte>(byte));
 	}
 	return result;
 }

common/misc_util.h
@@ -372,8 +372,8 @@ struct byte_cast_impl
 		STATIC_ASSERT(sizeof(Tdst) == sizeof(mpt::byte));
 		// not checking is_byte_castable here because we are actually
 		// doing a static_cast and converting the value
-		STATIC_ASSERT(std::is_integral<Tsrc>::value);
-		STATIC_ASSERT(std::is_integral<Tdst>::value);
+		STATIC_ASSERT(std::is_integral<Tsrc>::value || mpt::is_byte<Tsrc>::value);
+		STATIC_ASSERT(std::is_integral<Tdst>::value || mpt::is_byte<Tdst>::value);
 		return static_cast<Tdst>(src);
 	}
 };
@@ -386,8 +386,8 @@ struct byte_cast_impl<mpt::span<Tdst>, mpt::span<Tsrc> >
 		STATIC_ASSERT(sizeof(Tdst) == sizeof(mpt::byte));
 		STATIC_ASSERT(mpt::is_byte_castable<Tsrc>::value);
 		STATIC_ASSERT(mpt::is_byte_castable<Tdst>::value);
-		STATIC_ASSERT(std::is_integral<Tsrc>::value);
-		STATIC_ASSERT(std::is_integral<Tdst>::value);
+		STATIC_ASSERT(std::is_integral<Tsrc>::value || mpt::is_byte<Tsrc>::value);
+		STATIC_ASSERT(std::is_integral<Tdst>::value || mpt::is_byte<Tdst>::value);
 		return mpt::as_span(mpt::byte_cast_impl<Tdst*, Tsrc*>()(src.begin()), mpt::byte_cast_impl<Tdst*, Tsrc*>()(src.end()));
 	}
 };
@@ -400,8 +400,8 @@ struct byte_cast_impl<Tdst*, Tsrc*>
 		STATIC_ASSERT(sizeof(Tdst) == sizeof(mpt::byte));
 		STATIC_ASSERT(mpt::is_byte_castable<Tsrc>::value);
 		STATIC_ASSERT(mpt::is_byte_castable<Tdst>::value);
-		STATIC_ASSERT(std::is_integral<Tsrc>::value);
-		STATIC_ASSERT(std::is_integral<Tdst>::value);
+		STATIC_ASSERT(std::is_integral<Tsrc>::value || mpt::is_byte<Tsrc>::value);
+		STATIC_ASSERT(std::is_integral<Tdst>::value || mpt::is_byte<Tdst>::value);
 		return reinterpret_cast<Tdst*>(src);
 	}
 };
@@ -416,7 +416,7 @@ struct void_cast_impl<Tdst*, void*>
 	{
 		STATIC_ASSERT(sizeof(Tdst) == sizeof(mpt::byte));
 		STATIC_ASSERT(mpt::is_byte_castable<Tdst>::value);
-		STATIC_ASSERT(std::is_integral<Tdst>::value);
+		STATIC_ASSERT(std::is_integral<Tdst>::value || mpt::is_byte<Tdst>::value);
 		return reinterpret_cast<Tdst*>(src);
 	}
 };
@@ -427,7 +427,7 @@ struct void_cast_impl<Tdst*, const void*>
 	{
 		STATIC_ASSERT(sizeof(Tdst) == sizeof(mpt::byte));
 		STATIC_ASSERT(mpt::is_byte_castable<Tdst>::value);
-		STATIC_ASSERT(std::is_integral<Tdst>::value);
+		STATIC_ASSERT(std::is_integral<Tdst>::value || mpt::is_byte<Tdst>::value);
 		return reinterpret_cast<Tdst*>(src);
 	}
 };
@@ -438,7 +438,7 @@ struct void_cast_impl<void*, Tsrc*>
 	{
 		STATIC_ASSERT(sizeof(Tsrc) == sizeof(mpt::byte));
 		STATIC_ASSERT(mpt::is_byte_castable<Tsrc>::value);
-		STATIC_ASSERT(std::is_integral<Tsrc>::value);
+		STATIC_ASSERT(std::is_integral<Tsrc>::value || mpt::is_byte<Tsrc>::value);
 		return reinterpret_cast<void*>(src);
 	}
 };
@@ -449,7 +449,7 @@ struct void_cast_impl<const void*, Tsrc*>
 	{
 		STATIC_ASSERT(sizeof(Tsrc) == sizeof(mpt::byte));
 		STATIC_ASSERT(mpt::is_byte_castable<Tsrc>::value);
-		STATIC_ASSERT(std::is_integral<Tsrc>::value);
+		STATIC_ASSERT(std::is_integral<Tsrc>::value || mpt::is_byte<Tsrc>::value);
 		return reinterpret_cast<const void*>(src);
 	}
 };
@@ -467,6 +467,16 @@ inline Tdst void_cast(Tsrc src)
 {
 	return void_cast_impl<Tdst, Tsrc>()(src);
 }
+
+
+
+template <typename T>
+MPT_CONSTEXPR14_FUN mpt::byte as_byte(T src) noexcept
+{
+	MPT_STATIC_ASSERT(std::is_integral<T>::value);
+	return static_cast<mpt::byte>(static_cast<uint8>(src));
+}
+
 
 
 // Saturate the value of src to the domain of Tdst

common/mptCRC.h
@@ -134,7 +134,7 @@ public:
 
 	inline crc & process(char c)
 	{
-		processByte(static_cast<byte_type>(c));
+		processByte(mpt::byte_cast<byte_type>(c));
 		return *this;
 	}
 
@@ -146,7 +146,13 @@ public:
 
 	inline crc & process(unsigned char c)
 	{
-		processByte(static_cast<byte_type>(c));
+		processByte(mpt::byte_cast<byte_type>(c));
+		return *this;
+	}
+
+	inline crc & process(mpt::byte c)
+	{
+		processByte(mpt::byte_cast<byte_type>(c));
 		return *this;
 	}
 
@@ -170,7 +176,7 @@ public:
 
 	inline crc & operator () (char c)
 	{
-		processByte(static_cast<byte_type>(c));
+		processByte(mpt::byte_cast<byte_type>(c));
 		return *this;
 	}
 
@@ -182,7 +188,13 @@ public:
 
 	inline crc & operator () (unsigned char c)
 	{
-		processByte(static_cast<byte_type>(c));
+		processByte(mpt::byte_cast<byte_type>(c));
+		return *this;
+	}
+
+	inline crc & operator () (mpt::byte c)
+	{
+		processByte(mpt::byte_cast<byte_type>(c));
 		return *this;
 	}
 

common/mptIO.h
@@ -17,6 +17,7 @@
 #include "../common/mptTypeTraits.h"
 #include "../common/Endianness.h"
 #include <algorithm>
+#include <array>
 #include <iosfwd>
 #include <limits>
 #include <cstring>
@@ -207,12 +208,29 @@ inline bool WritePartial(Tfile & f, const T & v, size_t size = sizeof(T))
 	return IO::WriteRaw(f, mpt::as_raw_memory(v), size);
 }
 
+template <typename Tfile>
+inline bool ReadByte(Tfile & f, mpt::byte & v)
+{
+	bool result = false;
+	mpt::byte byte = mpt::as_byte(0);
+	const IO::Offset readResult = IO::ReadRaw(f, &byte, sizeof(mpt::byte));
+	if(readResult < 0)
+	{
+		result = false;
+	} else
+	{
+		result = (static_cast<uint64>(readResult) == sizeof(mpt::byte));
+	}
+	v = byte;
+	return result;
+}
+
 template <typename T, typename Tfile>
 inline bool ReadBinaryTruncatedLE(Tfile & f, T & v, std::size_t size)
 {
 	bool result = false;
 	MPT_STATIC_ASSERT(std::numeric_limits<T>::is_integer);
-	mpt::byte bytes[sizeof(T)];
+	uint8 bytes[sizeof(T)];
 	std::memset(bytes, 0, sizeof(T));
 	const IO::Offset readResult = IO::ReadRaw(f, bytes, std::min(size, sizeof(T)));
 	if(readResult < 0)
@@ -234,7 +252,7 @@ inline bool ReadIntLE(Tfile & f, T & v)
 {
 	bool result = false;
 	STATIC_ASSERT(std::numeric_limits<T>::is_integer);
-	mpt::byte bytes[sizeof(T)];
+	uint8 bytes[sizeof(T)];
 	std::memset(bytes, 0, sizeof(T));
 	const IO::Offset readResult = IO::ReadRaw(f, bytes, sizeof(T));
 	if(readResult < 0)
@@ -255,7 +273,7 @@ inline bool ReadIntBE(Tfile & f, T & v)
 {
 	bool result = false;
 	STATIC_ASSERT(std::numeric_limits<T>::is_integer);
-	mpt::byte bytes[sizeof(T)];
+	uint8 bytes[sizeof(T)];
 	std::memset(bytes, 0, sizeof(T));
 	const IO::Offset readResult = IO::ReadRaw(f, bytes, sizeof(T));
 	if(readResult < 0)
@@ -275,17 +293,17 @@ template <typename Tfile>
 inline bool ReadAdaptiveInt16LE(Tfile & f, uint16 & v)
 {
 	bool result = true;
-	mpt::byte byte = 0;
+	uint8 byte = 0;
 	std::size_t additionalBytes = 0;
 	v = 0;
 	byte = 0;
-	if(!IO::ReadIntLE<mpt::byte>(f, byte)) result = false;
+	if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
 	additionalBytes = (byte & 0x01);
 	v = byte >> 1;
 	for(std::size_t i = 0; i < additionalBytes; ++i)
 	{
 		byte = 0;
-		if(!IO::ReadIntLE<mpt::byte>(f, byte)) result = false;
+		if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
 		v |= (static_cast<uint16>(byte) << (((i+1)*8) - 1));
 	}
 	return result;
@@ -295,17 +313,17 @@ template <typename Tfile>
 inline bool ReadAdaptiveInt32LE(Tfile & f, uint32 & v)
 {
 	bool result = true;
-	mpt::byte byte = 0;
+	uint8 byte = 0;
 	std::size_t additionalBytes = 0;
 	v = 0;
 	byte = 0;
-	if(!IO::ReadIntLE<mpt::byte>(f, byte)) result = false;
+	if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
 	additionalBytes = (byte & 0x03);
 	v = byte >> 2;
 	for(std::size_t i = 0; i < additionalBytes; ++i)
 	{
 		byte = 0;
-		if(!IO::ReadIntLE<mpt::byte>(f, byte)) result = false;
+		if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
 		v |= (static_cast<uint32>(byte) << (((i+1)*8) - 2));
 	}
 	return result;
@@ -315,17 +333,17 @@ template <typename Tfile>
 inline bool ReadAdaptiveInt64LE(Tfile & f, uint64 & v)
 {
 	bool result = true;
-	mpt::byte byte = 0;
+	uint8 byte = 0;
 	std::size_t additionalBytes = 0;
 	v = 0;
 	byte = 0;
-	if(!IO::ReadIntLE<mpt::byte>(f, byte)) result = false;
+	if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
 	additionalBytes = (1 << (byte & 0x03)) - 1;
 	v = byte >> 2;
 	for(std::size_t i = 0; i < additionalBytes; ++i)
 	{
 		byte = 0;
-		if(!IO::ReadIntLE<mpt::byte>(f, byte)) result = false;
+		if(!IO::ReadIntLE<uint8>(f, byte)) result = false;
 		v |= (static_cast<uint64>(byte) << (((i+1)*8) - 2));
 	}
 	return result;

common/mptTypeTraits.h
@@ -51,8 +51,15 @@ template <> struct uint_of_size<8> { typedef uint64 type; };
 template <typename T> struct is_byte_castable : public std::false_type { };
 template <> struct is_byte_castable<char>                : public std::true_type { };
 template <> struct is_byte_castable<unsigned char>       : public std::true_type { };
+template <> struct is_byte_castable<mpt::byte>           : public std::true_type { };
 template <> struct is_byte_castable<const char>          : public std::true_type { };
 template <> struct is_byte_castable<const unsigned char> : public std::true_type { };
+template <> struct is_byte_castable<const mpt::byte>     : public std::true_type { };
+
+
+template <typename T> struct is_byte        : public std::false_type { };
+template <> struct is_byte<mpt::byte>       : public std::true_type  { };
+template <> struct is_byte<const mpt::byte> : public std::true_type  { };
 
 
 // Tell which types are safe to binary write into files.
@@ -62,9 +69,10 @@ template <> struct is_byte_castable<const unsigned char> : public std::true_type
 template <typename T> struct is_binary_safe : public std::false_type { }; 
 
 // Specialization for byte types.
-template <> struct is_binary_safe<char>  : public std::true_type { };
-template <> struct is_binary_safe<uint8> : public std::true_type { };
-template <> struct is_binary_safe<int8>  : public std::true_type { };
+template <> struct is_binary_safe<char>      : public std::true_type { };
+template <> struct is_binary_safe<uint8>     : public std::true_type { };
+template <> struct is_binary_safe<int8>      : public std::true_type { };
+template <> struct is_binary_safe<mpt::byte> : public std::true_type { };
 
 // Generic Specialization for arrays.
 template <typename T, std::size_t N> struct is_binary_safe<T[N]> : public is_binary_safe<T> { };

commit 311ea315c77290e641de4f16c13345c320b7b829

[Fix] Compile fix for stb_vorbis breakage introduced in r9282.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9283 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_mo3.cpp
@@ -1650,7 +1650,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 			if(sharedHeader)
 			{
 				FileReader::PinnedRawDataView headChunkView = headerChunk.GetPinnedRawDataView(initialRead);
-				vorb = stb_vorbis_open_pushdata(headChunkView.data(), headChunkView.size(), &consumed, &error, nullptr);
+				vorb = stb_vorbis_open_pushdata(mpt::byte_cast<const unsigned char*>(headChunkView.data()), headChunkView.size(), &consumed, &error, nullptr);
 				headerChunk.Skip(consumed);
 			}
 			FileReader::PinnedRawDataView sampleDataView = sampleData.GetPinnedRawDataView();
@@ -1658,7 +1658,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 			std::size_t dataLeft = sampleDataView.size();
 			if(!sharedHeader)
 			{
-				vorb = stb_vorbis_open_pushdata(data, mpt::saturate_cast<int>(dataLeft), &consumed, &error, nullptr);
+				vorb = stb_vorbis_open_pushdata(mpt::byte_cast<const unsigned char*>(data), mpt::saturate_cast<int>(dataLeft), &consumed, &error, nullptr);
 				sampleData.Skip(consumed);
 				data += consumed;
 				dataLeft -= consumed;
@@ -1674,7 +1674,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 				{
 					int channels = 0, decodedSamples = 0;
 					float **output;
-					consumed = stb_vorbis_decode_frame_pushdata(vorb, data, mpt::saturate_cast<int>(dataLeft), &channels, &output, &decodedSamples);
+					consumed = stb_vorbis_decode_frame_pushdata(vorb, mpt::byte_cast<const unsigned char*>(data), mpt::saturate_cast<int>(dataLeft), &channels, &output, &decodedSamples);
 					sampleData.Skip(consumed);
 					data += consumed;
 					dataLeft -= consumed;

soundlib/SampleFormatVorbis.cpp
@@ -249,7 +249,7 @@ bool CSoundFile::ReadVorbisSample(SAMPLEINDEX sample, FileReader &file)
 	std::size_t offset = 0;
 	int consumed = 0;
 	int error = 0;
-	stb_vorbis *vorb = stb_vorbis_open_pushdata(data, mpt::saturate_cast<int>(dataLeft), &consumed, &error, nullptr);
+	stb_vorbis *vorb = stb_vorbis_open_pushdata(mpt::byte_cast<const unsigned char*>(data), mpt::saturate_cast<int>(dataLeft), &consumed, &error, nullptr);
 	file.Skip(consumed);
 	data += consumed;
 	dataLeft -= consumed;
@@ -268,7 +268,7 @@ bool CSoundFile::ReadVorbisSample(SAMPLEINDEX sample, FileReader &file)
 		int frame_channels = 0;
 		int decodedSamples = 0;
 		float **output = nullptr;
-		consumed = stb_vorbis_decode_frame_pushdata(vorb, data, mpt::saturate_cast<int>(dataLeft), &frame_channels, &output, &decodedSamples);
+		consumed = stb_vorbis_decode_frame_pushdata(vorb, mpt::byte_cast<const unsigned char*>(data), mpt::saturate_cast<int>(dataLeft), &frame_channels, &output, &decodedSamples);
 		file.Skip(consumed);
 		data += consumed;
 		dataLeft -= consumed;