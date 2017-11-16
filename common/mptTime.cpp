1609ececdc3c6f7ce373f5d15ce840ae4f553302
Johannes Schultz <sagamusix@openmpt.org>
2017-11-16 20:49:40 +0000

    Merged revision(s) 9292 from trunk/OpenMPT: [Fix] MultimediaClock: Contrary to its documentation, SetResolution(0) actually started a timer period of 1 due to being clamped to the supported timer period range. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.27@9293 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/mptTime.cpp
+++ common/mptTime.cpp
@@ -275,7 +275,10 @@ uint32 MultimediaClock::SetResolution(uint32 ms)
 		return m_CurrentPeriod;
 	}
 	Cleanup();
-	SetPeriod(ms);
+	if(ms != 0)
+	{
+		SetPeriod(ms);
+	}
 	return GetResolution();
 }
 
