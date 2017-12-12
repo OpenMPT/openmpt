OpenMPT/openmpt

commit 9872d87bd5b612390154f171936e0c39a1486d3d

[Ref] Make CModDoc::SetModified work in non-GUI threads, avoiding the use of the potentially lossy CMainFrame::ThreadSafeSetModified.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9197 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Moddoc.cpp
@@ -152,11 +152,14 @@ CModDoc::~CModDoc()
 }
 
 
-void CModDoc::SetModifiedFlag(BOOL bModified)
+void CModDoc::SetModified(bool modified)
 {
-	bool changed = (!!bModified != IsModified());
-	CDocument::SetModifiedFlag(bModified);
-	if (changed) UpdateFrameCounts();
+	STATIC_ASSERT(sizeof(long) == sizeof(m_bModified));
+	if(!!InterlockedExchange(reinterpret_cast<long *>(&m_bModified), modified ? TRUE : FALSE) != modified)
+	{
+		// Update window titles in GUI thread
+		CMainFrame::GetMainFrame()->SendNotifyMessage(WM_MOD_SETMODIFIED, reinterpret_cast<WPARAM>(this), 0);
+	}
 }
 
 
@@ -172,7 +175,7 @@ BOOL CModDoc::OnNewDocument()
 
 	ReinitRecordState();
 	InitializeMod();
-	SetModifiedFlag(FALSE);
+	SetModified(false);
 	return TRUE;
 }
 
@@ -248,7 +251,7 @@ BOOL CModDoc::OnOpenDocument(LPCTSTR lpszPathName)
 			MptVersion::AsUString()));
 	}
 
-	SetModifiedFlag(FALSE);
+	SetModified(false);
 	m_bHasValidPath = true;
 
 	// Check if there are any missing samples, and if there are, show a dialog to relocate them.
@@ -474,7 +477,7 @@ BOOL CModDoc::DoSave(LPCTSTR lpszPathName, BOOL)
 	}
 	if(OnSaveDocument(saveFileName))
 	{
-		SetModified(FALSE);
+		SetModified(false);
 		m_bHasValidPath=true;
 		m_ShowSavedialog = false;
 		CMainFrame::GetMainFrame()->UpdateTree(this, GeneralHint().General()); // Update treeview (e.g. filename might have changed).
@@ -845,7 +848,7 @@ void CModDoc::ProcessMIDI(uint32 midiData, INSTRUMENTINDEX ins, IMixPlugin *plug
 			&& event != MIDIEvents::evPitchBend
 			&& m_SndFile.GetModSpecifications().supportsPlugins)
 		{
-			CMainFrame::GetMainFrame()->ThreadSafeSetModified(this);
+			SetModified();
 		}
 	}
 }
@@ -1100,7 +1103,7 @@ bool CModDoc::MuteChannel(CHANNELINDEX nChn, bool doMute)
 	const bool success = UpdateChannelMuteStatus(nChn);
 	if(success && MuteToggleModifiesDocument())
 	{
-		CMainFrame::GetMainFrame()->ThreadSafeSetModified(this);
+		SetModified();
 	}
 
 	return success;

mptrack/Moddoc.h
@@ -126,7 +126,7 @@ public:
 	const CSoundFile &GetrSoundFile() const { return m_SndFile; }
 
 	bool IsModified() const { return m_bModified != FALSE; }	// Work-around: CDocument::IsModified() is not const...
-	void SetModified(BOOL bModified=TRUE) { SetModifiedFlag(bModified); bModifiedAutosave = (bModified != FALSE); }
+	void SetModified(bool modified = true);
 	bool ModifiedSinceLastAutosave() { bool bRetval = bModifiedAutosave; bModifiedAutosave = false; return bRetval; } // return "IsModified" value and reset it until the next SetModified() (as this is only used for polling)
 	void SetShowSaveDialog(bool b) {m_ShowSavedialog = b;}
 	void PostMessageToAllViews(UINT uMsg, WPARAM wParam=0, LPARAM lParam=0);
@@ -338,7 +338,6 @@ protected:
 		return DoSave(filename.ToCString(), bSaveAs);
 	}
 	void DeleteContents() override;
-	void SetModifiedFlag(BOOL bModified=TRUE) override;
 	//}}AFX_VIRTUAL
 
 	uint8 GetPlaybackMidiChannel(const ModInstrument *pIns, CHANNELINDEX nChn) const;

mptrack/MIDIMapping.cpp
@@ -9,7 +9,7 @@
 
 
 #include "stdafx.h"
-#include "Mainfrm.h"
+#include "Moddoc.h"
 #include "MIDIMapping.h"
 #include "../common/FileReader.h"
 #include "../soundlib/MIDIEvents.h"
@@ -143,7 +143,8 @@ bool CMIDIMapper::OnMIDImsg(const DWORD midimsg, PLUGINDEX &mappedIndex, PlugPar
 			IMixPlugin *pPlug = m_rSndFile.m_MixPlugins[plugindex - 1].pMixPlugin;
 			if(!pPlug) continue;
 			pPlug->SetParameter(param, val / 16383.0f);
-			CMainFrame::GetMainFrame()->ThreadSafeSetModified(m_rSndFile.GetpModDoc());
+			if(m_rSndFile.GetpModDoc() != nullptr)
+				m_rSndFile.GetpModDoc()->SetModified();
 #endif // NO_PLUGINS
 		}
 		if(d.GetCaptureMIDI())

mptrack/MainFrm.cpp
@@ -30,7 +30,6 @@
 #include "InputHandler.h"
 #include "globals.h"
 #include "ChannelManagerDlg.h"
-#include <direct.h>
 #include "../common/version.h"
 #include "Ctrl_pat.h"
 #include "UpdateCheck.h"
@@ -104,6 +103,7 @@ BEGIN_MESSAGE_MAP(CMainFrame, CMDIFrameWnd)
 	ON_MESSAGE(WM_MOD_KEYCOMMAND,			OnCustomKeyMsg)
 	ON_MESSAGE(WM_MOD_MIDIMAPPING,			OnViewMIDIMapping)
 	ON_MESSAGE(WM_MOD_UPDATEVIEWS,			OnUpdateViews)
+	ON_MESSAGE(WM_MOD_SETMODIFIED,			OnSetModified)
 	ON_COMMAND(ID_INTERNETUPDATE,			OnInternetUpdate)
 	ON_COMMAND(ID_HELP_SHOWSETTINGSFOLDER,	OnShowSettingsFolder)
 	ON_MESSAGE(MPT_WM_APP_UPDATECHECK_PROGRESS, OnUpdateCheckProgress)
@@ -199,7 +199,6 @@ CMainFrame::CMainFrame()
 
 	m_SoundCardOptionsDialog = nullptr;
 
-	m_pJustModifiedDoc = nullptr;
 	m_hWndMidi = NULL;
 	m_pSndFile = nullptr;
 	m_dwTimeSec = 0;
@@ -1840,9 +1839,6 @@ void CMainFrame::OnDocumentClosed(CModDoc *pModDoc)
 {
 	if (pModDoc == GetModPlaying()) PauseMod();
 
-	// Make sure that OnTimer() won't try to set the closed document modified anymore.
-	if (pModDoc == m_pJustModifiedDoc) m_pJustModifiedDoc = nullptr;
-
 	m_wndTree.OnDocumentClosed(pModDoc);
 }
 
@@ -2019,15 +2015,6 @@ void CMainFrame::OnTimerGUI()
 			CMainFrame::m_nLastOptionsPage = OPTIONS_PAGE_PATHS;
 			OnViewOptions();
 		}
-	}
-
-	// Ensure the modified flag gets set in the WinMain thread, even if modification
-	// originated from Audio Thread (access to CWnd is not thread safe).
-	// Flaw: if 2 docs are modified in between Timer ticks (very rare), one mod will be lost.
-	if (m_pJustModifiedDoc)
-	{
-		m_pJustModifiedDoc->SetModified(true);
-		m_pJustModifiedDoc = NULL;
 	}
 
 	if(m_SoundCardOptionsDialog)
@@ -2256,6 +2243,27 @@ LRESULT CMainFrame::OnUpdateViews(WPARAM modDoc, LPARAM hint)
 	if(modDoc)
 	{
 		reinterpret_cast<CModDoc *>(modDoc)->UpdateAllViews(nullptr, UpdateHint::FromLPARAM(hint));
+	}
+	return 0;
+}
+
+
+LRESULT CMainFrame::OnSetModified(WPARAM modDoc, LPARAM)
+{
+	CModDoc *doc = reinterpret_cast<CModDoc *>(modDoc);
+	CDocTemplate *pDocTmpl = theApp.GetModDocTemplate();
+	if(pDocTmpl)
+	{
+		// Since this message is potentially posted, we first need to verify if the document still exists
+		POSITION pos = pDocTmpl->GetFirstDocPosition();
+		CDocument *pDoc;
+		while((pos != nullptr) && ((pDoc = pDocTmpl->GetNextDoc(pos)) != nullptr))
+		{
+			if(static_cast<CModDoc *>(pDoc) == doc)
+			{
+				doc->UpdateFrameCounts();
+			}
+		}
 	}
 	return 0;
 }

mptrack/Mainfrm.h
@@ -53,6 +53,7 @@ enum
 	WM_MOD_PLUGPARAMAUTOMATE,
 	WM_MOD_MIDIMAPPING,
 	WM_MOD_UPDATEVIEWS,
+	WM_MOD_SETMODIFIED,
 };
 
 enum
@@ -294,7 +295,6 @@ protected:
 	UINT_PTR m_nTimer;
 	UINT m_nAvgMixChn, m_nMixChn;
 	// Misc
-	CModDoc* m_pJustModifiedDoc;
 	class COptionsSoundcard *m_SoundCardOptionsDialog;
 	DWORD helpCookie;
 	bool m_bOptionsLocked;
@@ -380,7 +380,6 @@ public:
 	bool m_bModTreeHasFocus;
 	CWnd *m_pNoteMapHasFocus;
 	CWnd* m_pOrderlistHasFocus;
-	void ThreadSafeSetModified(CModDoc* modified) { InterlockedExchangePointer(reinterpret_cast<void **>(&m_pJustModifiedDoc), modified); }
 	void SetElapsedTime(double t) { m_dwTimeSec = static_cast<CSoundFile::samplecount_t>(t); }
 
 	CModTree *GetUpperTreeview() { return m_wndTree.m_pModTree; }
@@ -512,6 +511,7 @@ protected:
 	afx_msg BOOL OnInternetLink(UINT nID);
 	afx_msg LRESULT OnUpdatePosition(WPARAM, LPARAM lParam);
 	afx_msg LRESULT OnUpdateViews(WPARAM modDoc, LPARAM hint);
+	afx_msg LRESULT OnSetModified(WPARAM modDoc, LPARAM);
 	afx_msg void OnOpenTemplateModule(UINT nId);
 	afx_msg void OnExampleSong(UINT nId);
 	afx_msg void OnOpenMRUItem(UINT nId);

mptrack/View_pat.cpp
@@ -3441,7 +3441,7 @@ LRESULT CViewPattern::OnMidiMsg(WPARAM dwMidiDataParam, LPARAM)
 		m.Set(NOTE_PCS, mappedIndex, static_cast<uint16>(paramIndex), static_cast<uint16>((paramValue * ModCommand::maxColumnValue) / 16383));
 		if(!liveRecord)
 			InvalidateRow(editpos.row);
-		pMainFrm->ThreadSafeSetModified(pModDoc);
+		pModDoc->SetModified();
 		pModDoc->UpdateAllViews(this, PatternHint(editpos.pattern).Data(), this);
 	}
 
@@ -3552,7 +3552,7 @@ LRESULT CViewPattern::OnMidiMsg(WPARAM dwMidiDataParam, LPARAM)
 		}
 		if(update)
 		{
-			pMainFrm->ThreadSafeSetModified(pModDoc);
+			pModDoc->SetModified();
 			pModDoc->UpdateAllViews(this, PatternHint(editpos.pattern).Data(), this);
 
 			// Update GUI only if not recording live.
@@ -3575,7 +3575,7 @@ LRESULT CViewPattern::OnMidiMsg(WPARAM dwMidiDataParam, LPARAM)
 			// Sending MIDI may modify the plugin. For now, if MIDI data
 			// is not active sensing, set modified.
 			if(dwMidiData != MIDIEvents::System(MIDIEvents::sysActiveSense))
-				pMainFrm->ThreadSafeSetModified(pModDoc);
+				pModDoc->SetModified();
 		}
 
 	}

mptrack/View_smp.cpp
@@ -1962,7 +1962,7 @@ void CViewSample::OnSetSustainLoop()
 		{
 			if ((sample.nSustainStart != m_dwBeginSel) || (sample.nSustainEnd != m_dwEndSel))
 			{
-				pModDoc->GetSampleUndo().PrepareUndo(m_nSample, sundo_none, "Set  Sustain Loop");
+				pModDoc->GetSampleUndo().PrepareUndo(m_nSample, sundo_none, "Set Sustain Loop");
 				sample.SetSustainLoop(m_dwBeginSel, m_dwEndSel, true, sample.uFlags[CHN_PINGPONGSUSTAIN], sndFile);
 				SetModified(SampleHint().Info().Data(), true, false);
 			}

soundlib/plugins/PlugInterface.cpp
@@ -637,7 +637,7 @@ void IMixPlugin::SetModified()
 	CModDoc *modDoc = GetModDoc();
 	if(modDoc != nullptr && m_SndFile.GetModSpecifications().supportsPlugins)
 	{
-		CMainFrame::GetMainFrame()->ThreadSafeSetModified(modDoc);
+		modDoc->SetModified();
 	}
 }
 

commit 120e2cb517ee2d192c3c1b3360068b59578f6219

[Fix] Autosave was broken since r9197 (https://bugs.openmpt.org/view.php?id=1072)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9357 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Moddoc.cpp
@@ -133,7 +133,7 @@ CModDoc::CModDoc()
 	, m_PatternUndo(*this)
 	, m_SampleUndo(*this)
 	, m_InstrumentUndo(*this)
-	, bModifiedAutosave(false)
+	, m_modifiedAutosave(false)
 {
 	// Set the creation date of this file (or the load time if we're loading an existing file)
 	time(&m_creationTime);
@@ -156,11 +156,19 @@ CModDoc::~CModDoc()
 void CModDoc::SetModified(bool modified)
 {
 	STATIC_ASSERT(sizeof(long) == sizeof(m_bModified));
+	InterlockedExchange(&m_modifiedAutosave, modified);
 	if(!!InterlockedExchange(reinterpret_cast<long *>(&m_bModified), modified ? TRUE : FALSE) != modified)
 	{
 		// Update window titles in GUI thread
 		CMainFrame::GetMainFrame()->SendNotifyMessage(WM_MOD_SETMODIFIED, reinterpret_cast<WPARAM>(this), 0);
 	}
+}
+
+
+// Return "modified since last autosave" status and reset it until the next SetModified() (as this is only used for polling during autosave)
+bool CModDoc::ModifiedSinceLastAutosave()
+{
+	return !!InterlockedExchange(&m_modifiedAutosave, false);
 }
 
 

mptrack/Moddoc.h
@@ -105,7 +105,7 @@ protected:
 	SplitKeyboardSettings m_SplitKeyboardSettings;	// this is maybe not the best place to keep them, but it should do the job
 	time_t m_creationTime;
 
-	bool bModifiedAutosave : 1; // Modified since last autosave?
+	long m_modifiedAutosave; // Modified since last autosave?
 public:
 	bool m_ShowSavedialog : 1;
 	bool m_bHasValidPath : 1; //becomes true if document is loaded or saved.
@@ -125,7 +125,7 @@ public:
 
 	bool IsModified() const { return m_bModified != FALSE; }	// Work-around: CDocument::IsModified() is not const...
 	void SetModified(bool modified = true);
-	bool ModifiedSinceLastAutosave() { bool bRetval = bModifiedAutosave; bModifiedAutosave = false; return bRetval; } // return "IsModified" value and reset it until the next SetModified() (as this is only used for polling)
+	bool ModifiedSinceLastAutosave();
 	void SetShowSaveDialog(bool b) {m_ShowSavedialog = b;}
 	void PostMessageToAllViews(UINT uMsg, WPARAM wParam=0, LPARAM lParam=0);
 	void SendMessageToActiveViews(UINT uMsg, WPARAM wParam=0, LPARAM lParam=0);