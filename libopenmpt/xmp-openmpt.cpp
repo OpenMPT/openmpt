91cfc4fae79287c5c6deb1d87417b84947f007ad
Johannes Schultz <sagamusix@openmpt.org>
2018-01-23 22:19:55 +0000

    [Fix] xmp-openmpt: Saved settings were not applied instantly.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9511 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/xmp-openmpt.cpp
+++ libopenmpt/xmp-openmpt.cpp
@@ -323,6 +323,7 @@ static void WINAPI openmpt_SetConfig( void * config, DWORD size ) {
 	reset_options();
 	if ( config ) {
 		load_settings_from_xml( self->settings, std::string( (char*)config, (char*)config + size ) );
+		apply_options();
 	}
 }
 
