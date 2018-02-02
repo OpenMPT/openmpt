e8a307ed51be4f931a4d57b8ae73044ede009e36
Johannes Schultz <sagamusix@openmpt.org>
2018-02-02 15:41:52 +0000

    Merged revision(s) 9564 from trunk/OpenMPT: [Fix] STP: Small memory leak when creating a 0-sample slice. (Note: The 1.26 branch does not support the STP format and does not contain similar code that could trigger this leak. Still, for consistency the patch is applied here as well.) ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.26@9566 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/ModSample.cpp
+++ soundlib/ModSample.cpp
@@ -211,7 +211,7 @@ size_t ModSample::GetRealSampleBufferSize(SmpLength numSamples, size_t bytesPerS
 	const SmpLength maxSize = Util::MaxValueOfType(numSamples);
 	const SmpLength lookaheadBufferSize = 16 + (1 + 4 + 4) * InterpolationMaxLookahead;
 
-	if(numSamples > MAX_SAMPLE_LENGTH || lookaheadBufferSize > maxSize - numSamples)
+	if(numSamples == 0 || numSamples > MAX_SAMPLE_LENGTH || lookaheadBufferSize > maxSize - numSamples)
 	{
 		return 0;
 	}
