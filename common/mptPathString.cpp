OpenMPT/openmpt

commit 3b57ea8e63f851bb1834923dca84fb8c6e13f608

[Ref] mptPathString: Make PathString based on TCHAR instead of WCHAR in Windows builds. This reverts ANSI builds (which are not supported any more and only useful for type checking) to ANSI path names, which in turn would break various setups for people using the ANSI builds. See r9653. Using TCHAR has the advantge of allowing us to revert to standard WinAPI functions in most cases instead of using the Unicode versions explicitly. See <https://bugs.openmpt.org/view.php?id=1027>. [Mod] OpenMPT: Version is now 1.28.00.11
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9657 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptPathString.cpp
@@ -19,6 +19,7 @@
 #if defined(MODPLUG_TRACKER)
 #include <shlwapi.h>
 #endif
+#include <tchar.h>
 #endif
 
 #if MPT_OS_WINDOWS && MPT_OS_WINDOWS_WINRT
@@ -33,7 +34,11 @@ WINBASEAPI DWORD WINAPI GetFullPathNameW(LPCWSTR lpFileName, DWORD nBufferLength
 OPENMPT_NAMESPACE_BEGIN
 
 #if MPT_OS_WINDOWS
+#ifdef UNICODE
 #define MPT_PATHSTRING_LITERAL(x) ( L ## x )
+#else
+#define MPT_PATHSTRING_LITERAL(x) ( x )
+#endif
 #else
 #define MPT_PATHSTRING_LITERAL(x) ( x )
 #endif
@@ -68,7 +73,7 @@ RawPathString PathString::AsNativePrefixed() const
 
 int PathString::CompareNoCase(const PathString & a, const PathString & b)
 {
-	return lstrcmpiW(a.path.c_str(), b.path.c_str());
+	return lstrcmpi(a.path.c_str(), b.path.c_str());
 }
 
 #endif // !MPT_OS_WINDOWS_WINRT
@@ -285,7 +290,7 @@ bool PathString::IsDirectory() const
 		}
 		DWORD dwAttrib = data.dwFileAttributes;
 	#else // !MPT_OS_WINDOWS_WINRT
-		DWORD dwAttrib = ::GetFileAttributesW(path.c_str());
+		DWORD dwAttrib = ::GetFileAttributes(path.c_str());
 	#endif // MPT_OS_WINDOWS_WINRT
 	return ((dwAttrib != INVALID_FILE_ATTRIBUTES) && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
@@ -301,7 +306,7 @@ bool PathString::IsFile() const
 		}
 		DWORD dwAttrib = data.dwFileAttributes;
 	#else // !MPT_OS_WINDOWS_WINRT
-		DWORD dwAttrib = ::GetFileAttributesW(path.c_str());
+		DWORD dwAttrib = ::GetFileAttributes(path.c_str());
 	#endif // MPT_OS_WINDOWS_WINRT
 	return ((dwAttrib != INVALID_FILE_ATTRIBUTES) && !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
 }
@@ -313,7 +318,7 @@ bool PathString::IsFile() const
 
 bool PathString::FileOrDirectoryExists() const
 {
-	return ::PathFileExistsW(path.c_str()) != FALSE;
+	return ::PathFileExists(path.c_str()) != FALSE;
 }
 
 #endif // MODPLUG_TRACKER && MPT_OS_WINDOWS
@@ -343,12 +348,12 @@ PathString PathString::AbsolutePathToRelative(const PathString &relativeTo) cons
 	{
 		return result;
 	}
-	if(!_wcsnicmp(relativeTo.AsNative().c_str(), AsNative().c_str(), relativeTo.AsNative().length()))
+	if(!_tcsncicmp(relativeTo.AsNative().c_str(), AsNative().c_str(), relativeTo.AsNative().length()))
 	{
 		// Path is OpenMPT's directory or a sub directory ("C:\OpenMPT\Somepath" => ".\Somepath")
 		result = MPT_PATHSTRING(".\\"); // ".\"
 		result += mpt::PathString::FromNative(AsNative().substr(relativeTo.AsNative().length()));
-	} else if(!_wcsnicmp(relativeTo.AsNative().c_str(), AsNative().c_str(), 2))
+	} else if(!_tcsncicmp(relativeTo.AsNative().c_str(), AsNative().c_str(), 2))
 	{
 		// Path is on the same drive as OpenMPT ("C:\Somepath" => "\Somepath")
 		result = mpt::PathString::FromNative(AsNative().substr(2));
@@ -395,9 +400,9 @@ bool PathString::IsPathSeparator(RawPathString::value_type c)
 RawPathString::value_type PathString::GetDefaultPathSeparator()
 {
 #if MPT_OS_WINDOWS
-	return L'\\';
+	return MPT_PATHSTRING_LITERAL('\\');
 #else
-	return '/';
+	return MPT_PATHSTRING_LITERAL('/');
 #endif
 }
 
@@ -438,13 +443,13 @@ bool PathIsAbsolute(const mpt::PathString &path) {
 
 mpt::PathString GetAbsolutePath(const mpt::PathString &path)
 {
-	DWORD size = GetFullPathNameW(path.AsNative().c_str(), 0, nullptr, nullptr);
+	DWORD size = GetFullPathName(path.AsNative().c_str(), 0, nullptr, nullptr);
 	if(size == 0)
 	{
 		return path;
 	}
-	std::vector<WCHAR> fullPathName(size, L'\0');
-	if(GetFullPathNameW(path.AsNative().c_str(), size, fullPathName.data(), nullptr) == 0)
+	std::vector<TCHAR> fullPathName(size, _T('\0'));
+	if(GetFullPathName(path.AsNative().c_str(), size, fullPathName.data(), nullptr) == 0)
 	{
 		return path;
 	}
@@ -459,7 +464,7 @@ bool DeleteWholeDirectoryTree(mpt::PathString path)
 	{
 		return false;
 	}
-	if(PathIsRelativeW(path.AsNative().c_str()) == TRUE)
+	if(PathIsRelative(path.AsNative().c_str()) == TRUE)
 	{
 		return false;
 	}
@@ -473,9 +478,9 @@ bool DeleteWholeDirectoryTree(mpt::PathString path)
 	}
 	path.EnsureTrailingSlash();
 	HANDLE hFind = NULL;
-	WIN32_FIND_DATAW wfd;
+	WIN32_FIND_DATA wfd;
 	MemsetZero(wfd);
-	hFind = FindFirstFileW((path + MPT_PATHSTRING("*.*")).AsNative().c_str(), &wfd);
+	hFind = FindFirstFile((path + MPT_PATHSTRING("*.*")).AsNative().c_str(), &wfd);
 	if(hFind != NULL && hFind != INVALID_HANDLE_VALUE)
 	{
 		do
@@ -492,16 +497,16 @@ bool DeleteWholeDirectoryTree(mpt::PathString path)
 					}
 				} else if(filename.IsFile())
 				{
-					if(DeleteFileW(filename.AsNative().c_str()) == 0)
+					if(DeleteFile(filename.AsNative().c_str()) == 0)
 					{
 						return false;
 					}
 				}
 			}
-		} while(FindNextFileW(hFind, &wfd));
+		} while(FindNextFile(hFind, &wfd));
 		FindClose(hFind);
 	}
-	if(RemoveDirectoryW(path.AsNative().c_str()) == 0)
+	if(RemoveDirectory(path.AsNative().c_str()) == 0)
 	{
 		return false;
 	}
@@ -520,8 +525,8 @@ bool DeleteWholeDirectoryTree(mpt::PathString path)
 
 mpt::PathString GetAppPath()
 {
-	std::vector<WCHAR> exeFileName(MAX_PATH);
-	while(GetModuleFileNameW(0, exeFileName.data(), mpt::saturate_cast<DWORD>(exeFileName.size())) >= exeFileName.size())
+	std::vector<TCHAR> exeFileName(MAX_PATH);
+	while(GetModuleFileName(0, exeFileName.data(), mpt::saturate_cast<DWORD>(exeFileName.size())) >= exeFileName.size())
 	{
 		if(GetLastError() != ERROR_INSUFFICIENT_BUFFER)
 		{
@@ -541,9 +546,9 @@ mpt::PathString GetAppPath()
 
 mpt::PathString GetSystemPath()
 {
-	DWORD size = GetSystemDirectoryW(nullptr, 0);
-	std::vector<WCHAR> path(size + 1);
-	if(!GetSystemDirectoryW(path.data(), size + 1))
+	DWORD size = GetSystemDirectory(nullptr, 0);
+	std::vector<TCHAR> path(size + 1);
+	if(!GetSystemDirectory(path.data(), size + 1))
 	{
 		return mpt::PathString();
 	}
@@ -563,11 +568,11 @@ mpt::PathString GetSystemPath()
 
 mpt::PathString GetTempDirectory()
 {
-	DWORD size = GetTempPathW(0, nullptr);
+	DWORD size = GetTempPath(0, nullptr);
 	if(size)
 	{
-		std::vector<WCHAR> tempPath(size + 1);
-		if(GetTempPathW(size + 1, tempPath.data()))
+		std::vector<TCHAR> tempPath(size + 1);
+		if(GetTempPath(size + 1, tempPath.data()))
 		{
 			return mpt::PathString::FromNative(tempPath.data());
 		}
@@ -600,7 +605,7 @@ TempFileGuard::~TempFileGuard()
 {
 	if(!filename.empty())
 	{
-		DeleteFileW(filename.AsNative().c_str());
+		DeleteFile(filename.AsNative().c_str());
 	}
 }
 
@@ -613,7 +618,7 @@ TempDirGuard::TempDirGuard(const mpt::PathString &dirname_)
 	{
 		return;
 	}
-	if(::CreateDirectoryW(dirname.AsNative().c_str(), NULL) == 0)
+	if(::CreateDirectory(dirname.AsNative().c_str(), NULL) == 0)
 	{ // fail
 		dirname = mpt::PathString();
 	}

common/FileReader.cpp
@@ -74,7 +74,7 @@ OnDiskFileWrapper::OnDiskFileWrapper(FileReader &file, const mpt::PathString &fi
 			#if MPT_OS_WINDOWS_WINRT
 				hFile = CreateFile2(tempName.AsNative().c_str(), GENERIC_WRITE, FILE_SHARE_READ, CREATE_ALWAYS, NULL);
 			#else
-				hFile = CreateFileW(tempName.AsNative().c_str(), GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_TEMPORARY, NULL);
+				hFile = CreateFile(tempName.AsNative().c_str(), GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_TEMPORARY, NULL);
 			#endif
 			if(hFile == NULL || hFile == INVALID_HANDLE_VALUE)
 			{
@@ -123,7 +123,7 @@ OnDiskFileWrapper::~OnDiskFileWrapper()
 {
 	if(m_IsTempFile)
 	{
-		DeleteFileW(m_Filename.AsNative().c_str());
+		DeleteFile(m_Filename.AsNative().c_str());
 		m_IsTempFile = false;
 	}
 	m_Filename = mpt::PathString();

common/WriteMemoryDump.h
@@ -25,18 +25,18 @@ typedef BOOL (WINAPI *MINIDUMPWRITEDUMP)(HANDLE hProcess, DWORD dwPid, HANDLE hF
 	CONST PMINIDUMP_CALLBACK_INFORMATION CallbackParam
 	);
 
-static bool WriteMemoryDump(_EXCEPTION_POINTERS *pExceptionInfo, const WCHAR *filename, bool fullMemDump)
+static bool WriteMemoryDump(_EXCEPTION_POINTERS *pExceptionInfo, const TCHAR *filename, bool fullMemDump)
 {
 	bool result = false;
 
-	HMODULE hDll = ::LoadLibraryW(L"DBGHELP.DLL");
+	HMODULE hDll = ::LoadLibrary(_T("DBGHELP.DLL"));
 	if (hDll)
 	{
 		MINIDUMPWRITEDUMP pDump = (MINIDUMPWRITEDUMP)::GetProcAddress(hDll, "MiniDumpWriteDump");
 		if (pDump)
 		{
 
-			HANDLE hFile = ::CreateFileW(filename, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			HANDLE hFile = ::CreateFile(filename, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 			if (hFile != INVALID_HANDLE_VALUE)
 			{
 				_MINIDUMP_EXCEPTION_INFORMATION ExInfo;

common/mptFileIO.cpp
@@ -70,7 +70,7 @@ bool SetFilesystemCompression(FILE *file)
 #endif // MPT_ENABLE_FILEIO_STDIO
 bool SetFilesystemCompression(const mpt::PathString &filename)
 {
-	DWORD attributes = GetFileAttributesW(filename.AsNativePrefixed().c_str());
+	DWORD attributes = GetFileAttributes(filename.AsNativePrefixed().c_str());
 	if(attributes == INVALID_FILE_ATTRIBUTES)
 	{
 		return false;
@@ -79,7 +79,7 @@ bool SetFilesystemCompression(const mpt::PathString &filename)
 	{
 		return true;
 	}
-	HANDLE hFile = CreateFileW(filename.AsNativePrefixed().c_str(), GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
+	HANDLE hFile = CreateFile(filename.AsNativePrefixed().c_str(), GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
 	if(hFile == INVALID_HANDLE_VALUE)
 	{
 		return false;
@@ -184,7 +184,7 @@ CMappedFile::~CMappedFile()
 
 bool CMappedFile::Open(const mpt::PathString &filename)
 {
-	m_hFile = CreateFileW(
+	m_hFile = CreateFile(
 		filename.AsNativePrefixed().c_str(),
 		GENERIC_READ,
 		FILE_SHARE_READ,

common/mptFileIO.h
@@ -35,7 +35,11 @@ OPENMPT_NAMESPACE_BEGIN
 static inline FILE * mpt_fopen(const mpt::PathString &filename, const char *mode)
 {
 	#if MPT_OS_WINDOWS
-		return _wfopen(filename.AsNativePrefixed().c_str(), mode ? mpt::ToWide(mpt::CharsetASCII, mode).c_str() : L"");
+		#ifdef UNICODE
+			return _wfopen(filename.AsNativePrefixed().c_str(), mode ? mpt::ToWide(mpt::CharsetASCII, mode).c_str() : L"");
+		#else
+			return fopen(filename.AsNativePrefixed().c_str(), mode ? mode : "");
+		#endif
 	#else // !MPT_OS_WINDOWS
 		return fopen(filename.AsNative().c_str(), mode);
 	#endif // MPT_OS_WINDOWS

common/mptLibrary.cpp
@@ -119,10 +119,10 @@ public:
 			switch(path.GetSearchPath())
 			{
 				case mpt::LibrarySearchPathDefault:
-					hModule = LoadLibraryExW(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+					hModule = LoadLibraryEx(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
 					break;
 				case mpt::LibrarySearchPathSystem:
-					hModule = LoadLibraryExW(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_SYSTEM32);
+					hModule = LoadLibraryEx(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_SYSTEM32);
 					break;
 #if defined(MODPLUG_TRACKER)
 					// Using restricted search paths applies to potential DLL dependencies
@@ -136,20 +136,20 @@ public:
 						const mpt::PathString dllPath = mpt::GetAppPath();
 						if(!dllPath.empty() && mpt::PathIsAbsolute(dllPath) && dllPath.IsDirectory())
 						{
-							hModule = LoadLibraryW((dllPath + path.GetFileName()).AsNative().c_str());
+							hModule = LoadLibrary((dllPath + path.GetFileName()).AsNative().c_str());
 						}
 					}
 					break;
 				case mpt::LibrarySearchPathFullPath:
-					hModule = LoadLibraryW(path.GetFileName().AsNative().c_str());
+					hModule = LoadLibrary(path.GetFileName().AsNative().c_str());
 					break;
 #else
 					// For libopenmpt, do the safe thing.
 				case mpt::LibrarySearchPathApplication:
-					hModule = LoadLibraryExW(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_APPLICATION_DIR);
+					hModule = LoadLibraryEx(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_APPLICATION_DIR);
 					break;
 				case mpt::LibrarySearchPathFullPath:
-					hModule = LoadLibraryExW(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
+					hModule = LoadLibraryEx(path.GetFileName().AsNative().c_str(), NULL, LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
 					break;
 #endif
 				case mpt::LibrarySearchPathInvalid:
@@ -161,14 +161,14 @@ public:
 			switch(path.GetSearchPath())
 			{
 				case mpt::LibrarySearchPathDefault:
-					hModule = LoadLibraryW(path.GetFileName().AsNative().c_str());
+					hModule = LoadLibrary(path.GetFileName().AsNative().c_str());
 					break;
 				case mpt::LibrarySearchPathApplication:
 					{
 						const mpt::PathString dllPath = mpt::GetAppPath();
 						if(!dllPath.empty() && mpt::PathIsAbsolute(dllPath) && dllPath.IsDirectory())
 						{
-							hModule = LoadLibraryW((dllPath + path.GetFileName()).AsNative().c_str());
+							hModule = LoadLibrary((dllPath + path.GetFileName()).AsNative().c_str());
 						}
 					}
 					break;
@@ -177,12 +177,12 @@ public:
 						const mpt::PathString dllPath = mpt::GetSystemPath();
 						if(!dllPath.empty() && mpt::PathIsAbsolute(dllPath) && dllPath.IsDirectory())
 						{
-							hModule = LoadLibraryW((dllPath + path.GetFileName()).AsNative().c_str());
+							hModule = LoadLibrary((dllPath + path.GetFileName()).AsNative().c_str());
 						}
 					}
 					break;
 				case mpt::LibrarySearchPathFullPath:
-					hModule = LoadLibraryW(path.GetFileName().AsNative().c_str());
+					hModule = LoadLibrary(path.GetFileName().AsNative().c_str());
 					break;
 				case mpt::LibrarySearchPathInvalid:
 					MPT_ASSERT_NOTREACHED();

common/mptPathString.h
@@ -27,7 +27,7 @@ namespace mpt
 {
 
 #if MPT_OS_WINDOWS
-typedef std::wstring RawPathString;
+typedef mpt::winstring RawPathString;
 #else // !MPT_OS_WINDOWS
 typedef std::string RawPathString;
 #endif // if MPT_OS_WINDOWS
@@ -205,15 +205,15 @@ public:
 	MPT_DEPRECATED_PATH std::string ToLocale() const { return mpt::ToCharset(mpt::CharsetLocale, path); }
 #endif
 	std::string ToUTF8() const { return mpt::ToCharset(mpt::CharsetUTF8, path); }
-	std::wstring ToWide() const { return path; }
+	std::wstring ToWide() const { return mpt::ToWide(path); }
 	mpt::ustring ToUnicode() const { return mpt::ToUnicode(path); }
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
-	MPT_DEPRECATED_PATH static PathString FromLocale(const std::string &path) { return PathString(mpt::ToWide(mpt::CharsetLocale, path)); }
-	static PathString FromLocaleSilent(const std::string &path) { return PathString(mpt::ToWide(mpt::CharsetLocale, path)); }
+	MPT_DEPRECATED_PATH static PathString FromLocale(const std::string &path) { return PathString(mpt::ToWin(mpt::CharsetLocale, path)); }
+	static PathString FromLocaleSilent(const std::string &path) { return PathString(mpt::ToWin(mpt::CharsetLocale, path)); }
 #endif
-	static PathString FromUTF8(const std::string &path) { return PathString(mpt::ToWide(mpt::CharsetUTF8, path)); }
-	static PathString FromWide(const std::wstring &path) { return PathString(path); }
-	static PathString FromUnicode(const mpt::ustring &path) { return PathString(mpt::ToWide(path)); }
+	static PathString FromUTF8(const std::string &path) { return PathString(mpt::ToWin(mpt::CharsetUTF8, path)); }
+	static PathString FromWide(const std::wstring &path) { return PathString(mpt::ToWin(path)); }
+	static PathString FromUnicode(const mpt::ustring &path) { return PathString(mpt::ToWin(path)); }
 	RawPathString AsNative() const { return path; }
 	// Return native string, with possible \\?\ prefix if it exceeds MAX_PATH characters.
 	RawPathString AsNativePrefixed() const;
@@ -221,7 +221,7 @@ public:
 #if defined(_MFC_VER)
 	// CString TCHAR, so this is CHAR or WCHAR, depending on UNICODE
 	CString ToCString() const { return mpt::ToCString(path); }
-	static PathString FromCString(const CString &path) { return PathString(mpt::ToWide(path)); }
+	static PathString FromCString(const CString &path) { return PathString(mpt::ToWin(path)); }
 #endif
 
 	// Convert a path to its simplified form, i.e. remove ".\" and "..\" entries
@@ -284,7 +284,11 @@ static inline std::wstring ToWString(const mpt::PathString & x) { return x.ToWid
 
 #if MPT_OS_WINDOWS
 
+#ifdef UNICODE
 #define MPT_PATHSTRING(x) mpt::PathString::FromNative( L ## x )
+#else
+#define MPT_PATHSTRING(x) mpt::PathString::FromNative( x )
+#endif
 
 #else // !MPT_OS_WINDOWS
 

common/mptWine.cpp
@@ -136,7 +136,7 @@ std::string Context::PathToPosix(mpt::PathString windowsPath)
 		throw mpt::Wine::Exception("Path too long.");
 	}
 	LPSTR tmp = nullptr;
-	tmp = wine_get_unix_file_name(windowsPath.AsNative().c_str());
+	tmp = wine_get_unix_file_name(windowsPath.ToWide().c_str());
 	if(!tmp)
 	{
 		throw mpt::Wine::Exception("Wine kernel32.dll:wine_get_unix_file_name failed.");
@@ -164,7 +164,7 @@ mpt::PathString Context::PathToWindows(std::string hostPath)
 	{
 		throw mpt::Wine::Exception("Wine kernel32.dll:wine_get_dos_file_name failed.");
 	}
-	result = mpt::PathString::FromNative(tmp);
+	result = mpt::PathString::FromWide(tmp);
 	HeapFree(GetProcessHeap(), 0, tmp);
 	tmp = nullptr;
 	return result;
@@ -346,7 +346,7 @@ ExecResult Context::ExecutePosixShellScript(std::string script, FlagSet<ExecFlag
 
 	progress(userdata);
 
-	::CreateDirectoryW((dirWindows + MPT_PATHSTRING("filetree")).AsNative().c_str(), NULL);
+	::CreateDirectory((dirWindows + MPT_PATHSTRING("filetree")).AsNative().c_str(), NULL);
 	for(const auto &file : filetree)
 	{
 		std::vector<mpt::ustring> path = mpt::String::Split<mpt::ustring>(mpt::ToUnicode(mpt::CharsetUTF8, file.first), MPT_USTRING("/"));
@@ -362,7 +362,7 @@ ExecResult Context::ExecutePosixShellScript(std::string script, FlagSet<ExecFlag
 				combinedPath += mpt::PathString::FromUnicode(path[singlepath]);
 				if(!combinedPath.IsDirectory())
 				{
-					if(::CreateDirectoryW(combinedPath.AsNative().c_str(), NULL) == 0)
+					if(::CreateDirectory(combinedPath.AsNative().c_str(), NULL) == 0)
 					{
 						throw mpt::Wine::Exception("Error writing filetree.");
 					}
@@ -658,9 +658,9 @@ ExecResult Context::ExecutePosixShellScript(std::string script, FlagSet<ExecFlag
 		paths.pop_front();
 		path.EnsureTrailingSlash();
 		HANDLE hFind = NULL;
-		WIN32_FIND_DATAW wfd;
+		WIN32_FIND_DATA wfd;
 		MemsetZero(wfd);
-		hFind = FindFirstFileW((path + MPT_PATHSTRING("*.*")).AsNative().c_str(), &wfd);
+		hFind = FindFirstFile((path + MPT_PATHSTRING("*.*")).AsNative().c_str(), &wfd);
 		if(hFind != NULL && hFind != INVALID_HANDLE_VALUE)
 		{
 			do
@@ -687,7 +687,7 @@ ExecResult Context::ExecutePosixShellScript(std::string script, FlagSet<ExecFlag
 						}
 					}
 				}
-			} while(FindNextFileW(hFind, &wfd));
+			} while(FindNextFile(hFind, &wfd));
 			FindClose(hFind);
 		}
 	}

commit 0ded0c7cf46def9bfedd3e49abd4a96973d1be4f

[Fix] mptPathString: Fix MinGW WinRT builds broken by r9657.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9660 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptPathString.cpp
@@ -27,6 +27,9 @@
 // MinGW-w64 headers do not declare this for WinRT, which is wrong.
 extern "C" {
 WINBASEAPI DWORD WINAPI GetFullPathNameW(LPCWSTR lpFileName, DWORD nBufferLength, LPWSTR lpBuffer, LPWSTR *lpFilePart);
+#ifndef GetFullPathName
+#define GetFullPathName GetFullPathNameW
+#endif
 }
 #endif
 #endif