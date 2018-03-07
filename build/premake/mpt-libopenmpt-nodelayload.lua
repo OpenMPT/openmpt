a8dab684faec72ffd11613aff5aa5e0b14788c78
Jörn Heusipp <manx@openmpt.org>
2018-03-07 10:12:28 +0000

    [Mod] foo_openmpt: Do not delay-load openmpt-mpg123.dll any more. Use normal startup-time dynamic linking instead. See <https://bugs.openmpt.org/view.php?id=1100>.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.27@9858 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/premake/mpt-libopenmpt-nodelayload.lua
@@ -0,0 +1,96 @@
+ 
+ project "libopenmpt-nodelayload"
+  uuid "f328ce15-f251-4bf8-bd99-823c78a82beb"
+  language "C++"
+  location ( "../../build/" .. mpt_projectpathname )
+  vpaths { ["*"] = "../../" }
+  mpt_projectname = "libopenmpt-nodelayload"
+  dofile "../../build/premake/premake-defaults-LIBorDLL.lua"
+  dofile "../../build/premake/premake-defaults.lua"
+  local extincludedirs = {
+   "../../include",
+   "../../include/mpg123/ports/MSVC++",
+   "../../include/mpg123/src/libmpg123",
+   "../../include/ogg/include",
+   "../../include/vorbis/include",
+   "../../include/zlib",
+  }
+  filter { "action:vs*" }
+    includedirs ( extincludedirs )
+  filter { "not action:vs*" }
+    sysincludedirs ( extincludedirs )
+  filter {}
+  includedirs {
+   "../..",
+   "../../common",
+   "../../soundlib",
+   "$(IntDir)/svn_version",
+   "../../build/svn_version",
+  }
+  files {
+   "../../common/*.cpp",
+   "../../common/*.h",
+   "../../soundbase/*.cpp",
+   "../../soundbase/*.h",
+   "../../soundlib/*.cpp",
+   "../../soundlib/*.h",
+   "../../soundlib/plugins/*.cpp",
+   "../../soundlib/plugins/*.h",
+   "../../soundlib/plugins/dmo/*.cpp",
+   "../../soundlib/plugins/dmo/*.h",
+   "../../sounddsp/*.cpp",
+   "../../sounddsp/*.h",
+   "../../libopenmpt/libopenmpt.h",
+   "../../libopenmpt/libopenmpt.hpp",
+   "../../libopenmpt/libopenmpt_config.h",
+   "../../libopenmpt/libopenmpt_ext.h",
+   "../../libopenmpt/libopenmpt_ext.hpp",
+   "../../libopenmpt/libopenmpt_ext_impl.hpp",
+   "../../libopenmpt/libopenmpt_impl.hpp",
+   "../../libopenmpt/libopenmpt_internal.h",
+   "../../libopenmpt/libopenmpt_stream_callbacks_buffer.h",
+   "../../libopenmpt/libopenmpt_stream_callbacks_fd.h",
+   "../../libopenmpt/libopenmpt_stream_callbacks_file.h",
+   "../../libopenmpt/libopenmpt_version.h",
+   "../../libopenmpt/libopenmpt_c.cpp",
+   "../../libopenmpt/libopenmpt_cxx.cpp",
+   "../../libopenmpt/libopenmpt_ext_impl.cpp",
+   "../../libopenmpt/libopenmpt_impl.cpp",
+  }
+
+	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
+		resdefines {
+			"MPT_BUILD_VER_FILENAME=\"" .. mpt_projectname .. ".dll\"",
+			"MPT_BUILD_VER_FILEDESC=\"" .. mpt_projectname .. "\"",
+		}
+	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
+		resincludedirs {
+			"$(IntDir)/svn_version",
+			"../../build/svn_version",
+			"$(ProjDir)/../../build/svn_version",
+		}
+		files {
+			"../../libopenmpt/libopenmpt_version.rc",
+		}
+	filter { "action:vs*", "kind:SharedLib" }
+		resdefines { "MPT_BUILD_VER_DLL" }
+	filter { "action:vs*", "kind:ConsoleApp or WindowedApp" }
+		resdefines { "MPT_BUILD_VER_EXE" }
+	filter {}
+
+  characterset "Unicode"
+  warnings "Extra"
+  defines { "LIBOPENMPT_BUILD" }
+  defines { "LIBOPENMPT_NODELAYLOAD" }
+	defines { "MPT_ENABLE_DLOPEN" }
+  filter { "kind:SharedLib" }
+   defines { "LIBOPENMPT_BUILD_DLL" }
+  filter { "kind:SharedLib" }
+  filter {}
+  links {
+   "vorbis",
+   "ogg",
+   "mpg123",
+   "zlib",
+  }
+  prebuildcommands { "..\\..\\build\\svn_version\\update_svn_version_vs_premake.cmd $(IntDir)" }
