OpenMPT/openmpt

commit a417d6a598beb323abe3405c77a8fa9cb5e8f7a5

[Fix] Loading the default keymap didn't load all keys since OpenMPT 1.27.01.00, making for example instrument number entry with the regular number keys impossible.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9595 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/CommandSet.cpp
@@ -1556,6 +1556,18 @@ ctx:UID:Description:Modifier:Key:EventMask
 }
 
 
+static mpt::istringstream GetDefaultKeymap()
+{
+	const char *pData = nullptr;
+	HGLOBAL hglob = nullptr;
+	size_t nSize = 0;
+	if(LoadResource(MAKEINTRESOURCE(IDR_DEFAULT_KEYBINDINGS), TEXT("KEYBINDINGS"), pData, nSize, hglob) != nullptr)
+		return mpt::istringstream(std::string(pData, nSize));
+	else
+		return mpt::istringstream();
+}
+
+
 bool CCommandSet::LoadFile(std::istream& iStrm, const std::wstring &filenameDescription, CCommandSet *commandSet)
 {
 	KeyCombination kc;
@@ -1671,14 +1683,7 @@ bool CCommandSet::LoadFile(std::istream& iStrm, const std::wstring &filenameDesc
 	if(!fillExistingSet)
 	{
 		// Add the default command set to our freshly loaded command set.
-		const char *pData = nullptr;
-		HGLOBAL hglob = nullptr;
-		size_t nSize = 0;
-		if(LoadResource(MAKEINTRESOURCE(IDR_DEFAULT_KEYBINDINGS), TEXT("KEYBINDINGS"), pData, nSize, hglob) != nullptr)
-		{
-			mpt::istringstream intStrm(std::string(pData, nSize));
-			LoadFile(intStrm, std::wstring(), pTempCS);
-		}
+		LoadFile(GetDefaultKeymap(), std::wstring(), pTempCS);
 	} else
 	{
 		// We were just adding stuff to an existing command set - don't delete it!
@@ -1715,8 +1720,7 @@ bool CCommandSet::LoadFile(const mpt::PathString &filename)
 
 bool CCommandSet::LoadDefaultKeymap()
 {
-	mpt::istringstream s;
-	return LoadFile(s, L"\"executable resource\"");
+	return LoadFile(GetDefaultKeymap(), L"\"executable resource\"");
 }
 
 

commit 386f0bfd4e3b1405b62f174fcac0827b35fed047

[Fix] Do not show error message for empty lines in keyboard config files with CRLF line endings (probably broken since r9595).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9867 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/CommandSet.cpp
@@ -1587,7 +1587,7 @@ bool CCommandSet::LoadFile(std::istream& iStrm, const std::wstring &filenameDesc
 	GetKeyboardLayoutList(static_cast<int>(layouts.size()), layouts.data());
 
 	const std::string whitespace(" \n\r\t");
-	while(iStrm.getline(s, MPT_ARRAY_COUNT(s)))
+	while(iStrm.getline(s, mpt::size(s)))
 	{
 		curLine = s;
 		l++;
@@ -1596,7 +1596,8 @@ bool CCommandSet::LoadFile(std::istream& iStrm, const std::wstring &filenameDesc
 		auto pos = curLine.find("//");
 		if(pos != std::string::npos) curLine.resize(pos);
 		pos = curLine.find_first_not_of(whitespace);
-		if(pos != std::string::npos) curLine.erase(0, pos);
+		if(pos == std::string::npos) continue;
+		curLine.erase(0, pos);
 		pos = curLine.find_last_not_of(whitespace);
 		if(pos != std::string::npos) curLine.resize(pos + 1);
 