e49311bbff906b911c708ca473f23cd5adfe0aa1
Jörn Heusipp <manx@openmpt.org>
2018-03-09 10:25:24 +0000

    [Ref] File Dialog: Remove std::wstring.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@9898 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/FileDialog.h
+++ mptrack/FileDialog.h
@@ -39,15 +39,12 @@ protected:
 public:
 	// Default extension to use if none is specified.
 	FileDialog &DefaultExtension(const mpt::PathString &ext) { m_defaultExtension = ext.AsNative(); return *this; }
-	FileDialog &DefaultExtension(const std::wstring &ext) { m_defaultExtension = mpt::ToWin(ext); return *this; }
 	FileDialog &DefaultExtension(const AnyStringLocale &ext) { m_defaultExtension = mpt::ToWin(ext); return *this; }
 	// Default suggested filename.
 	FileDialog &DefaultFilename(const mpt::PathString &name) { m_defaultFilename = name.AsNative(); return *this; }
-	FileDialog &DefaultFilename(const std::wstring &name) { m_defaultFilename = mpt::ToWin(name); return *this; }
 	FileDialog &DefaultFilename(const AnyStringLocale &name) { m_defaultFilename = mpt::ToWin(name); return *this; }
 	// List of possible extensions. Format: "description|extensions|...|description|extensions||"
 	FileDialog &ExtensionFilter(const mpt::PathString &filter) { m_extFilter = filter.AsNative(); return *this; }
-	FileDialog &ExtensionFilter(const std::wstring &filter) { m_extFilter = mpt::ToWin(filter); return *this; }
 	FileDialog &ExtensionFilter(const AnyStringLocale &filter) { m_extFilter = mpt::ToWin(filter); return *this; }
 	// Default directory of the dialog.
 	FileDialog &WorkingDirectory(const mpt::PathString &dir) { m_workingDirectory = dir.AsNative(); return *this; }
