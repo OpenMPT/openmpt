c082e589705323ea57d2546759c324763f345b5d
Jörn Heusipp <manx@openmpt.org>
2018-03-24 12:33:20 +0000

    [New] build: Add scripts to automatically run Visual Studio static analyzer.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10003 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/check_libopenmpt_winrt.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+cd ..\..
+
+set MY_DIR=%CD%
+
+call "build\auto\setup_arguments_winrt.cmd" %1 %2 %3 %4 %5 %6
+
+call "build\auto\setup_%MPT_VS_VER%.cmd"
+
+
+
+cd "build\%MPT_VS_TARGET%" || goto error
+
+ msbuild libopenmpt.sln /target:Build /p:RunCodeAnalysis=true /property:Configuration=%MPT_VS_CONF%;Platform=%MPT_VS_ARCH% /maxcpucount /verbosity:minimal || goto error
+
+cd ..\.. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
---
+++ build/auto/check_openmpt_args.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+cd ..\..
+
+set MY_DIR=%CD%
+
+call "build\auto\setup_arguments.cmd" %1 %2 %3 %4 %5 %6
+
+call "build\auto\setup_%MPT_VS_VER%.cmd"
+
+
+
+cd "build\%MPT_VS_WITHTARGET%" || goto error
+
+msbuild OpenMPT%MPT_VS_FLAVOUR%.sln /target:Build /p:RunCodeAnalysis=true /property:Configuration=%MPT_VS_CONF%;Platform=%MPT_VS_ARCH% /maxcpucount /verbosity:minimal || goto error
+msbuild PluginBridge.sln /target:Build /p:RunCodeAnalysis=true /property:Configuration=%MPT_VS_CONF%;Platform=%MPT_VS_ARCH_OTHER% /maxcpucount /verbosity:minimal || goto error
+
+cd ..\.. || goto error
+
+
+
+goto noerror
+
+:error
+cd "%MY_DIR%"
+exit 1
+
+:noerror
+cd "%MY_DIR%"
+exit 0
