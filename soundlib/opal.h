75db642157daf623c91349e1f97a509ba1f440bc
Johannes Schultz <sagamusix@openmpt.org>
2018-05-29 08:53:18 +0000

    [Fix] Opal: Explicitely mark fall-through in switch statement.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10324 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/opal.h
+++ soundlib/opal.h
@@ -933,6 +933,7 @@ int16_t Opal::Operator::Output(uint16_t /*keyscalenum*/, uint32_t phase_step, in
                 break;
 
             // Note: fall-through!
+            MPT_FALLTHROUGH;
         }
 
         // Release stage
