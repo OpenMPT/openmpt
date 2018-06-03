b18315426c6908785e8f0a43dc59575d1d3455c7
Jörn Heusipp <manx@openmpt.org>
2018-06-03 07:19:11 +0000

    [Ref] FileReader: Remove declaration of copy/move constructor/assignment-operator. The defaults are just fine.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10384 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/FileReader.h
+++ common/FileReader.h
@@ -141,14 +141,6 @@ public:
 	// Initialize file reader object based on an existing file reader object window.
 	explicit FileReader(value_data_type other, const mpt::PathString *filename = nullptr) : m_data(other), streamPos(0), fileName(filename) { }
 
-	// Initialize file reader object based on an existing file reader object. The other object's stream position is copied.
-	FileReader(const FileReader &) = default;
-	FileReader& operator=(const FileReader &) = default;
-
-	// Move an existing file reader object
-	FileReader(FileReader &&) noexcept = default;
-	FileReader& operator=(FileReader &&) noexcept = default;
-
 public:
 
 	mpt::PathString GetFileName() const
