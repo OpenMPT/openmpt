2d79e652374aa2955e47bcbdcd288ca04cd16303
Jörn Heusipp <manx@openmpt.org>
2018-06-10 05:38:10 +0000

    [Imp] libopenmpt: Make use of m_moduleFormat.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10416 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_impl.cpp
+++ libopenmpt/libopenmpt_impl.cpp
@@ -1111,7 +1111,7 @@ std::string module_impl::get_metadata( const std::string & key ) const {
 	if ( key == std::string("type") ) {
 		return mpt::ToCharset(mpt::CharsetUTF8, CSoundFile::ModTypeToString( m_sndFile->GetType() ) );
 	} else if ( key == std::string("type_long") ) {
-		return mpt::ToCharset(mpt::CharsetUTF8, CSoundFile::ModTypeToTracker( m_sndFile->GetType() ) );
+		return mpt::ToCharset(mpt::CharsetUTF8, !m_sndFile->m_moduleFormat.empty() ? m_sndFile->m_moduleFormat : CSoundFile::ModTypeToTracker( m_sndFile->GetType() ) );
 	} else if ( key == std::string("container") ) {
 		return mpt::ToCharset(mpt::CharsetUTF8, CSoundFile::ModContainerTypeToString( m_sndFile->GetContainerType() ) );
 	} else if ( key == std::string("container_long") ) {
