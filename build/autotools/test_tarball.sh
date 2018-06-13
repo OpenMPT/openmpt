ddbf9bfc6c9b3d506edf3186ac7f1066be40c02f
Jörn Heusipp <manx@openmpt.org>
2018-06-13 07:44:23 +0000

    [Imp] build: Allow testing MinGW-w64 posix threading model when cross-compiling the autotools package.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10463 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/autotools/test_tarball.sh
+++ build/autotools/test_tarball.sh
@@ -24,7 +24,11 @@ else
 	cd test-tarball2
 	gunzip -c ../libopenmpt*.tar.gz | tar -xvf -
 	cd libopenmpt*
-	./configure --host=$1 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --enable-libmodplug --disable-openmpt123 --disable-examples --disable-tests
+	if [ "x$2" = "x" ] ; then
+		./configure --host=$1 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --enable-libmodplug --disable-openmpt123 --disable-examples --disable-tests
+	else
+		./configure --host=$1 CC=$1-gcc-$2 CXX=$1-g++-$2 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --enable-libmodplug --disable-openmpt123 --disable-examples --disable-tests
+	fi
 	make
 	cd ..
 	cd ..
