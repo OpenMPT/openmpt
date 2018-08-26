42584c38b0f26bb1c536e53bc6c21286afa9e779
Jörn Heusipp <manx@openmpt.org>
2018-08-26 12:11:43 +0000

    Merged revision(s) 10717 from trunk/OpenMPT: [Fix] Work-around VS2017 15.8 compiler bug (only triggers in modern standards-compliant mode). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.27@10720 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -5463,7 +5463,12 @@ void CSoundFile::SetTempo(TEMPO param, bool setFromUI)
 		// ProTracker sets the tempo after the first tick.
 		// Note: The case of one tick per row is handled in ProcessRow() instead.
 		// Test case: TempoChange.mod
+#if MPT_MSVC_AT_LEAST(2017,8)
+		// Work-around MSVC getting confused about deduced const input type in noexcept operator inside noexcept condition.
+		m_PlayState.m_nMusicTempo.SetRaw(std::min(param.GetRaw(), specs.GetTempoMax().GetRaw()));
+#else
 		m_PlayState.m_nMusicTempo = std::min(param, specs.GetTempoMax());
+#endif
 	} else if(param < minTempo && !m_SongFlags[SONG_FIRSTTICK])
 	{
 		// Tempo Slide
