9f2e28176a612cf98d9a9d536528d034167e8188
Jörn Heusipp <manx@openmpt.org>
2018-09-16 13:53:09 +0000

    [Imp] build: Verify integrity of downloaded source and tools.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10799 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/download_externals.cmd
+++ build/download_externals.cmd
@@ -36,7 +36,7 @@ if "%MPT_DOWNLOAD%" == "yes" (
 
  rem call build\scriptlib\download.cmd "https://github.com/bkaradzic/GENie/archive/78817a9707c1a02e845fb38b3adcc5353b02d377.zip"         "build\externals\GENie-78817a9707c1a02e845fb38b3adcc5353b02d377.zip"        || goto error
  rem call build\scriptlib\download.cmd "https://github.com/premake/premake-core/archive/2e7ca5fb18acdbcd5755fb741710622b20f2e0f6.zip"    "build\externals\premake-core-2e7ca5fb18acdbcd5755fb741710622b20f2e0f6.zip" || goto error
- copy /Y "build\externals-mirror\genie\GENie-78817a9707c1a02e845fb38b3adcc5353b02d377.zip" "build\externals\GENie-78817a9707c1a02e845fb38b3adcc5353b02d377.zip"               || goto error
+ copy /Y "build\externals-mirror\genie\GENie-78817a9707c1a02e845fb38b3adcc5353b02d377.zip"          "build\externals\GENie-78817a9707c1a02e845fb38b3adcc5353b02d377.zip"               || goto error
  copy /Y "build\externals-mirror\premake\premake-core-2e7ca5fb18acdbcd5755fb741710622b20f2e0f6.zip" "build\externals\premake-core-2e7ca5fb18acdbcd5755fb741710622b20f2e0f6.zip" || goto error
 
  call build\scriptlib\download.cmd "http://download.nullsoft.com/winamp/plugin-dev/WA5.55_SDK.exe"                                   "build\externals\WA5.55_SDK.exe"  || goto error
@@ -46,6 +46,16 @@ if "%MPT_DOWNLOAD%" == "yes" (
  call build\scriptlib\download.cmd "https://download.microsoft.com/download/0/A/9/0A939EF6-E31C-430F-A3DF-DFAE7960D564/htmlhelp.exe" "build\externals\htmlhelp.exe"    || goto error
 
 )
+
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue e81805f2039c18d4311fb1e04297656f6efbbe600a7eda91c835375dad03dbe805dc6f704355cfd085600a0f755392e96df7a1d80d4b6f416e177314cc20d666 -filesize 1181017 -filename build/externals/7z1805.exe || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue 09bca8018272c3a4e7dd68f62a832acfe0581d3713f29473463725fa5c1708bc34b30a126b069e741e0e4939b645f6818e1f100cf1b2b021e851d132a6abcca5 -filesize  923870 -filename build/externals/7z1805-extra.7z || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue 84e830c91a0e8ae499cc4814080da6569d8a6acbddc585c8b62abc86c809793aeb669b0a741063a379fd281ade85f120bc27efeb67d63bf961be893eec8bc3b3 -filesize  384846 -filename build/externals/7za920.zip || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue aba21883cd026a789395757f7dcc127d7d6372965693ddc3794c8adfc3a9675c255cedf2a87177729fa0b094e1bdb4de9d2e47555c61ddd6976c24d71cbd5e38 -filesize  422934 -filename build/externals/asiosdk2.3.zip || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue d93650059cf10faff343e28a5eda60a9e78c123574f1ff479ecea85a179912f3241a36734e9f54ca3041ebcf80b732a9d21a3433b0da6c48b95bed16677af0cb -filesize  685026 -filename build/externals/GENie-78817a9707c1a02e845fb38b3adcc5353b02d377.zip || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue d91371244ea98c691b4674ee266c4a2496a296800c176adae069d21f5c52c0763b21cc7859cfffa865b89e50171a2c99a6d14620c32f7d72c0ef04045348f856 -filesize 3509072 -filename build/externals/htmlhelp.exe || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue ec3e56e3fe452accafbb89096112119dc737913f4b533d2f19347ebe1d42099d80f0b6cd1a5b0b3bdb5e7c5cd68348613211f3e3d93493893c6143d15935b14f -filesize 5106004 -filename build/externals/premake-core-2e7ca5fb18acdbcd5755fb741710622b20f2e0f6.zip || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue 394375db8a16bf155b5de9376f6290488ab339e503dbdfdc4e2f5bede967799e625c559cca363bc988324f1a8e86e5fd28a9f697422abd7bb3dcde4a766607b5 -filesize  336166 -filename build/externals/WA5.55_SDK.exe || goto error
+powershell -ExecutionPolicy Unrestricted .\build\scriptlib\Verify-File.ps1 -hashvalue ddaea1ba4b3ead2b3c870652c6a2ce0a9bdab2f0d2f01030214959dd4ab9ca0f9d34c88c817a1a2847b3c39597215e0e3b749a0489a0e6ce806a4d7e0d44ef9b -filesize   28971 -filename build/externals/xmp-sdk.zip || goto error
 
 call :killdir "build\tools\7zipold" || goto error
 call :killdir "build\tools\7zipa" || goto error
---
+++ build/scriptlib/Verify-File.ps1
@@ -0,0 +1,16 @@
+param([String]$filename="", [Int32]$filesize=0, [String]$hashvalue="", [String]$hashname="SHA512")
+Function Get-FileHashWin7([String] $FileName, $HashName) {
+	$FileStream = New-Object System.IO.FileStream($FileName,[System.IO.FileMode]::Open)
+	$StringBuilder = New-Object System.Text.StringBuilder
+	[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash($FileStream)|%{[Void]$StringBuilder.Append($_.ToString("x2"))}
+	$FileStream.Close()
+	$StringBuilder.ToString()
+}
+Write-Output "Verify " $filename
+if ((Get-Item $filename).length -ne $filesize) {
+	exit 1
+}
+if ((Get-FileHashWin7 $filename $hashname) -ne $hashvalue) {
+	exit 1
+}
+exit 0
