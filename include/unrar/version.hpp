c01039da7721cb0247b4a908e8be56b4fdfde5fe
Johannes Schultz <sagamusix@openmpt.org>
2018-09-21 21:43:11 +0000

    [Var] unrar: Update to v5.6.6.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10841 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
-#define RARVER_MINOR    60
-#define RARVER_BETA      0
-#define RARVER_DAY      24
-#define RARVER_MONTH     6
+#define RARVER_MINOR    61
+#define RARVER_BETA      1
+#define RARVER_DAY       3
+#define RARVER_MONTH     9
 #define RARVER_YEAR   2018
