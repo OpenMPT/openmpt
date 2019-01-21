0afa89b3d1dc8e4eae69be8e31cda1fac2853ee2
Jörn Heusipp <manx@openmpt.org>
2019-01-21 19:18:25 +0000

    [Var] Automate libopenmpt release process even more.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@11235 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/svn/do_libopenmpt_release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+svn up
+build/update_libopenmpt_version.sh release
+svn ci -m "[Mod] libopenmpt: Prepare for release."
+svn up
+NEWVER=$(make distversion-pure)
+NEWREV=$(svn info --xml . | xpath -e '/info/entry/commit/@revision' -q | sed 's/revision//g' | tr '"' ' ' | tr '=' ' ' | sed 's/ //g')
+svn cp -m "tag libopenmpt-${NEWVER}" -r ${NEWREV} https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28 https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER}
+build/update_libopenmpt_version.sh bumppatch
+build/update_libopenmpt_version.sh bumpltrev
+svn ci -m "[Mod] libopenmpt: Bump patch version."
+
+echo "ALL DONE."
+echo "run './release-0.4.sh $NEWVER +release' in a website checkout after buildbot has finished."
+
