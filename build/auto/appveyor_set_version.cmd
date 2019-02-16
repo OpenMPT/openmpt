7163caad4e61f8c2333501d0a0230eacd0ef5f98
Jörn Heusipp <manx@openmpt.org>
2019-02-16 12:42:28 +0000

    [Imp] appveyor: Set build version.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@11369 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/appveyor_set_version.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+cd ..\..
+
+set MY_DIR=%CD%
+
+call build\auto\setup_vs_any.cmd
+
+call build\auto\helper_get_svnversion.cmd
+call build\auto\helper_get_openmpt_version.cmd
+
+set MPT_REVISION=%OPENMPT_VERSION%-%SVNVERSION%
+
+appveyor UpdateBuild -Version "%OPENMPT_VERSION%-%SVNVERSION%-appveyor{build}"
+
+cd "%MY_DIR%"
