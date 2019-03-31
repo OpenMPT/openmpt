OpenMPT/openmpt

commit 8a0f4a245babbd4c380f0903bf81d2d08ffc53a3

[Ref] mptStringBuffer: Add mpt::charbuf<len> and mpt::charbuf<len, mode>. [Ref] mptStringBuffer: Add empty() method to string buffers. [Ref] Convert instrument name and filename to mpt::charbuf. [Ref] Convert sample filename to mpt::charbuf. [Ref] Convert sample name to mpt::charbuf. [Ref] Convert channel name to mpt::charbuf. [Ref] Remove all now unnecessary calls to FixNullString(). [Ref] Convert all remaining mpt::String::Read() to mpt::String::ReadBuf(). [Ref] mptStringBuffer: Remove mpt::String::Read(). [Ref] Convert SNDMIXPLUGININFO to mpt::charbuf. [Ref] Remove remaining 10 uses of mpt::String::Copy() and mpt::String::CopyN(). [Ref] mptStringBuffer: Remove mpt::String::Copy() and mpt::String::CopyN(). [Ref] Convert all remaining mpt::String::Write() to mpt::String::WriteBuf(). [Ref] mptStringBuffer: Remove mpt::String::Write().
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@11484 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Draw_pat.cpp
@@ -1641,7 +1641,7 @@ CString CViewPattern::GetCursorDescription() const
 					if((m->instr <= sndFile.GetNumInstruments()) && (sndFile.Instruments[m->instr]))
 					{
 						ModInstrument *pIns = sndFile.Instruments[m->instr];
-						s += pIns->name;
+						s += mpt::ToCString(sndFile.GetCharsetInternal(), pIns->name);
 						if((m->note) && (m->note <= NOTE_MAX))
 						{
 							const SAMPLEINDEX nsmp = pIns->Keyboard[m->note - 1];
@@ -1650,7 +1650,7 @@ CString CViewPattern::GetCursorDescription() const
 								if(sndFile.m_szNames[nsmp][0])
 								{
 									s.AppendFormat(_T(" (%d: "), nsmp);
-									s += sndFile.m_szNames[nsmp];
+									s += mpt::ToCString(sndFile.GetCharsetInternal(), sndFile.m_szNames[nsmp]);
 									s.AppendChar(_T(')'));
 								}
 							}
@@ -1658,7 +1658,7 @@ CString CViewPattern::GetCursorDescription() const
 					}
 				} else if(m->instr <= sndFile.GetNumSamples())
 				{
-					s += sndFile.m_szNames[m->instr];
+					s += mpt::ToCString(sndFile.GetCharsetInternal(), sndFile.m_szNames[m->instr]);
 				}
 
 			}

commit 41f7d7ea384eae5a8e220a4ea1073858c5a25ce9

[Fix] Pattern channel name drawing broke in r11484.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@11497 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Draw_pat.cpp
@@ -661,7 +661,7 @@ void CViewPattern::OnDraw(CDC *pDC)
 					pszfmt = sndFile.m_bChannelMuteTogglePending[ncolhdr] ? "%u: [%s]" : "%u: %s";
 				else if (m_nDetailLevel < PatternCursor::volumeColumn) pszfmt = sndFile.m_bChannelMuteTogglePending[ncolhdr] ? "[Ch%u]" : "Ch%u";
 				else if (m_nDetailLevel < PatternCursor::effectColumn) pszfmt = sndFile.m_bChannelMuteTogglePending[ncolhdr] ? "[Chn %u]" : "Chn %u";
-				sprintf(s, pszfmt, ncolhdr + 1, sndFile.ChnSettings[ncolhdr].szName);
+				sprintf(s, pszfmt, ncolhdr + 1, sndFile.ChnSettings[ncolhdr].szName.buf);
 				DrawButtonRect(hdc, &rect, s,
 					sndFile.ChnSettings[ncolhdr].dwFlags[CHN_MUTE] ? TRUE : FALSE,
 					((m_bInItemRect) && ((m_nDragItem & DRAGITEM_MASK) == DRAGITEM_CHNHEADER) && ((m_nDragItem & DRAGITEM_VALUEMASK) == ncolhdr)) ? TRUE : FALSE,