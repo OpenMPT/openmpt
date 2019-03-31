OpenMPT/openmpt

commit bc65979e253331d4c7b8f81e82269520179863ad

[Fix] SampleIO: Due to assertion in ModSample::sample8(), conversion to 8bit output failed in WriteSample() when the source sample was 16bit. Split into 2 separate loops depending on source sample type. (broken since r9769).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@10290 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/SampleIO.cpp
@@ -1008,31 +1008,53 @@ size_t SampleIO::WriteSample(std::ostream &f, const ModSample &sample, SmpLength
 	{
 		MPT_ASSERT(GetBitDepth() == 8);
 		MPT_ASSERT(len == numSamples);
-		const int8 *const pSample8 = sample.sample8();
-		const int8 *p = pSample8;
-		int sinc = sample.GetElementarySampleSize();
-		int s_old = 0;
-		const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
-		MPT_MAYBE_CONSTANT_IF(mpt::endian_is_little())
+		if(sample.uFlags[CHN_16BIT])
 		{
-			if (sample.uFlags[CHN_16BIT]) p++;
-		}
-		for (SmpLength j = 0; j < numSamples; j++)
-		{
-			int s_new = *p;
-			p += sinc;
-			if (sample.uFlags[CHN_STEREO])
+			const int16 *const pSample16 = sample.sample16();
+			const int16 *p = pSample16;
+			int s_old = 0;
+			const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
+			for(SmpLength j = 0; j < numSamples; j++)
 			{
-				s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
-				p += sinc;
+				int s_new = mpt::rshift_signed(*p, 8);
+				p++;
+				if(sample.uFlags[CHN_STEREO])
+				{
+					s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
+					p++;
+				}
+				if(GetEncoding() == deltaPCM)
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new - s_old));
+					s_old = s_new;
+				} else
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new + s_ofs));
+				}
 			}
-			if (GetEncoding() == deltaPCM)
+		} else
+		{
+			const int8 *const pSample8 = sample.sample8();
+			const int8 *p = pSample8;
+			int s_old = 0;
+			const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
+			for(SmpLength j = 0; j < numSamples; j++)
 			{
-				mpt::IO::Write(fb, static_cast<int8>(s_new - s_old));
-				s_old = s_new;
-			} else
-			{
-				mpt::IO::Write(fb, static_cast<int8>(s_new + s_ofs));
+				int s_new = *p;
+				p++;
+				if(sample.uFlags[CHN_STEREO])
+				{
+					s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
+					p++;
+				}
+				if(GetEncoding() == deltaPCM)
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new - s_old));
+					s_old = s_new;
+				} else
+				{
+					mpt::IO::Write(fb, static_cast<int8>(s_new + s_ofs));
+				}
 			}
 		}
 	}

commit a1faf122297cfe71a92a11370ca0c2154f538883

[Fix] MOD: Saving 16-bit stereo samples (being converted to 8-bit mono) broke in r10290.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@11499 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/SampleIO.cpp
@@ -1018,8 +1018,7 @@ size_t SampleIO::WriteSample(std::ostream &f, const ModSample &sample, SmpLength
 		MPT_ASSERT(len == numSamples);
 		if(sample.uFlags[CHN_16BIT])
 		{
-			const int16 *const pSample16 = sample.sample16();
-			const int16 *p = pSample16;
+			const int16 *p = sample.sample16();
 			int s_old = 0;
 			const int s_ofs = (GetEncoding() == unsignedPCM) ? 0x80 : 0;
 			for(SmpLength j = 0; j < numSamples; j++)
@@ -1028,7 +1027,7 @@ size_t SampleIO::WriteSample(std::ostream &f, const ModSample &sample, SmpLength
 				p++;
 				if(sample.uFlags[CHN_STEREO])
 				{
-					s_new = (s_new + (static_cast<int>(*p)) + 1) / 2;
+					s_new = (s_new + mpt::rshift_signed(*p, 8) + 1) / 2;
 					p++;
 				}
 				if(GetEncoding() == deltaPCM)