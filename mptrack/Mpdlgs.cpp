OpenMPT/openmpt

commit 55f9932b3fe83ee82a0f329c701b6e357ebce318

[Ref] Sample Formats: Add SampleFormatInt8 support for output sample format. [Ref] Sample Formats: Use SampleFormatInt8 as default, except when SampleFormatUnsigned8 is required. [New] Stream Export: Support int8 and uint8 raw export. Previously, 8bit raw was always unsigned. [Imp] sounddev: RtAudio: Support 8bit output.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@11760 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Mpdlgs.cpp
@@ -491,7 +491,7 @@ void COptionsSoundcard::UpdateSampleFormat()
 			}
 		} else
 		{
-			if(m_CurrentDeviceCaps.CanSampleFormat || ((SampleFormat)bits == m_Settings.sampleFormat))
+			if(m_CurrentDeviceCaps.CanSampleFormat || (bits == m_Settings.sampleFormat.GetBitsPerSample()))
 			{
 				if(m_CurrentDeviceDynamicCaps.supportedExclusiveModeSampleFormats.size() > 0
 					&& std::find(
@@ -504,7 +504,7 @@ void COptionsSoundcard::UpdateSampleFormat()
 				}
 				UINT ndx = m_CbnSampleFormat.AddString(mpt::cformat(_T("%1 Bit"))(bits));
 				m_CbnSampleFormat.SetItemData(ndx, bits);
-				if((SampleFormat)bits == m_Settings.sampleFormat)
+				if(bits == m_Settings.sampleFormat.GetBitsPerSample())
 				{
 					n = ndx;
 				}

mptrack/Mod2wave.cpp
@@ -1235,6 +1235,14 @@ void CDoWaveConvert::Run()
 						}
 					);
 					break;
+				case SampleFormatInt8:
+					dither.WithDither(
+						[&](auto &ditherInstance)
+						{
+							return ConvertInterleavedFixedPointToInterleaved<MIXING_FRACTIONAL_BITS,false>(reinterpret_cast<int8*>(buffer), mixbuffer, ditherInstance, channels, framesChunk);
+						}
+					);
+					break;
 				case SampleFormatInt16:
 					dither.WithDither(
 						[&](auto &ditherInstance)

mptrack/StreamEncoderAU.cpp
@@ -189,6 +189,10 @@ AUEncoder::AUEncoder()
 				{
 					format.Sampleformat = SampleFormatFloat32;
 					format.Description = U_("Floating Point");
+				} else if(bytes == 1)
+				{
+					format.Sampleformat = (SampleFormat)(0 - (bytes * 8));
+					format.Description = mpt::format(U_("%1 Bit (unsigned)"))(bytes * 8);
 				} else
 				{
 					format.Sampleformat = (SampleFormat)(bytes * 8);

mptrack/StreamEncoderFLAC.cpp
@@ -140,7 +140,7 @@ public:
 				{
 					for(int channel = 0; channel < formatInfo.Channels; ++channel)
 					{
-						sampleBuf[frame * formatInfo.Channels + channel] = *p - 0x80;
+						sampleBuf[frame * formatInfo.Channels + channel] = *p;
 						p++;
 					}
 				}

mptrack/StreamEncoderRAW.cpp
@@ -84,7 +84,7 @@ RAWEncoder::RAWEncoder()
 		int samplerate = traits.samplerates[i];
 		for(int channels = 1; channels <= traits.maxChannels; channels *= 2)
 		{
-			for(int bytes = 5; bytes >= 1; --bytes)
+			for(int bytes = 5; bytes >= 0; --bytes)
 			{
 				Encoder::Format format;
 				format.Samplerate = samplerate;
@@ -93,6 +93,10 @@ RAWEncoder::RAWEncoder()
 				{
 					format.Sampleformat = SampleFormatFloat32;
 					format.Description = U_("Floating Point Little-Endian");
+				} else if(bytes == 0)
+				{
+					format.Sampleformat = SampleFormatUnsigned8;
+					format.Description = mpt::format(U_("%1 Bit Little-Endian (unsigned)"))(8);
 				} else
 				{
 					format.Sampleformat = (SampleFormat)(bytes * 8);

mptrack/StreamEncoderWAV.cpp
@@ -143,6 +143,10 @@ WAVEncoder::WAVEncoder()
 				{
 					format.Sampleformat = SampleFormatFloat32;
 					format.Description = U_("Floating Point");
+				} else if(bytes == 1)
+				{
+					format.Sampleformat = (SampleFormat)(0 - (bytes * 8));
+					format.Description = mpt::format(U_("%1 Bit (unsigned)"))(bytes * 8);
 				} else
 				{
 					format.Sampleformat = (SampleFormat)(bytes * 8);

soundbase/SampleFormat.h
@@ -20,7 +20,8 @@ struct int24;
 
 enum SampleFormatEnum
 {
-	SampleFormatUnsigned8 =  8,       // do not change value (for compatibility with old configuration settings)
+	SampleFormatUnsigned8 = -8,       // do not change value (for compatibility with old configuration settings)
+	SampleFormatInt8      =  8,       // do not change value (for compatibility with old configuration settings)
 	SampleFormatInt16     = 16,       // do not change value (for compatibility with old configuration settings)
 	SampleFormatInt24     = 24,       // do not change value (for compatibility with old configuration settings)
 	SampleFormatInt32     = 32,       // do not change value (for compatibility with old configuration settings)
@@ -29,18 +30,20 @@ enum SampleFormatEnum
 };
 
 template<typename Tsample> struct SampleFormatTraits;
-template<> struct SampleFormatTraits<uint8>     { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatUnsigned8; } };
-template<> struct SampleFormatTraits<int16>     { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt16;     } };
-template<> struct SampleFormatTraits<int24>     { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt24;     } };
-template<> struct SampleFormatTraits<int32>     { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt32;     } };
-template<> struct SampleFormatTraits<float>     { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatFloat32;   } };
+template<> struct SampleFormatTraits<uint8> { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatUnsigned8; } };
+template<> struct SampleFormatTraits<int8>  { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt8;      } };
+template<> struct SampleFormatTraits<int16> { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt16;     } };
+template<> struct SampleFormatTraits<int24> { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt24;     } };
+template<> struct SampleFormatTraits<int32> { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatInt32;     } };
+template<> struct SampleFormatTraits<float> { static MPT_CONSTEXPR11_FUN SampleFormatEnum sampleFormat() { return SampleFormatFloat32;   } };
 
 template<SampleFormatEnum sampleFormat> struct SampleFormatToType;
-template<> struct SampleFormatToType<SampleFormatUnsigned8> { typedef uint8     type; };
-template<> struct SampleFormatToType<SampleFormatInt16>     { typedef int16     type; };
-template<> struct SampleFormatToType<SampleFormatInt24>     { typedef int24     type; };
-template<> struct SampleFormatToType<SampleFormatInt32>     { typedef int32     type; };
-template<> struct SampleFormatToType<SampleFormatFloat32>   { typedef float     type; };
+template<> struct SampleFormatToType<SampleFormatUnsigned8> { typedef uint8 type; };
+template<> struct SampleFormatToType<SampleFormatInt8>      { typedef int8  type; };
+template<> struct SampleFormatToType<SampleFormatInt16>     { typedef int16 type; };
+template<> struct SampleFormatToType<SampleFormatInt24>     { typedef int24 type; };
+template<> struct SampleFormatToType<SampleFormatInt32>     { typedef int32 type; };
+template<> struct SampleFormatToType<SampleFormatFloat32>   { typedef float type; };
 
 
 struct SampleFormat
@@ -76,6 +79,7 @@ struct SampleFormat
 		return
 			!IsValid() ? 0 :
 			(value == SampleFormatUnsigned8) ?  8 :
+			(value == SampleFormatInt8)      ?  8 :
 			(value == SampleFormatInt16)     ? 16 :
 			(value == SampleFormatInt24)     ? 24 :
 			(value == SampleFormatInt32)     ? 32 :

soundbase/SampleFormatConverters.h
@@ -937,6 +937,20 @@ struct ConvertToFixedPoint<int32, uint8, fractionalBits>
 };
 
 template <int fractionalBits>
+struct ConvertToFixedPoint<int32, int8, fractionalBits>
+{
+	typedef int8 input_t;
+	typedef int32 output_t;
+	static const int shiftBits = fractionalBits + 1 - sizeof(input_t) * 8;
+	MPT_FORCEINLINE output_t operator() (input_t val)
+	{
+		STATIC_ASSERT(fractionalBits >= 0 && fractionalBits <= sizeof(output_t)*8-1);
+		STATIC_ASSERT(shiftBits >= 1);
+		return MPT_SC_LSHIFT_SIGNED(static_cast<output_t>(val), shiftBits);
+	}
+};
+
+template <int fractionalBits>
 struct ConvertToFixedPoint<int32, int16, fractionalBits>
 {
 	typedef int16 input_t;

commit 6a2e14d4fdf4148ea874ebd73583af4595aaa7b6

[Fix] Soundcard settings: Fix setting float sample format. (broken since r11760)
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@11765 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Mpdlgs.cpp
@@ -491,7 +491,7 @@ void COptionsSoundcard::UpdateSampleFormat()
 			}
 		} else
 		{
-			if(m_CurrentDeviceCaps.CanSampleFormat || (bits == m_Settings.sampleFormat.GetBitsPerSample()))
+			if(m_CurrentDeviceCaps.CanSampleFormat || ((SampleFormatFloat32 != m_Settings.sampleFormat) && (bits == m_Settings.sampleFormat.GetBitsPerSample())))
 			{
 				if(m_CurrentDeviceDynamicCaps.supportedExclusiveModeSampleFormats.size() > 0
 					&& std::find(