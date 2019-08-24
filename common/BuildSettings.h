47f8ad434ad3e349f1b10a5f5ac019443d8f1db7
Jörn Heusipp <manx@openmpt.org>
2019-08-24 13:23:31 +0000

    Merged revision(s) 11940 from trunk/OpenMPT: [Fix] Fix detection of libstdc++ for MPT_COMPILER_QUIRK_NO_ALIGNEDALLOC. Macro __GLIBCXX__ is available only after we have included some standard library header. Broken since r11643. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@11941 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/BuildSettings.h
+++ common/BuildSettings.h
@@ -731,6 +731,24 @@
 
 
 
+
+
+// standard library quirks
+
+#if MPT_CXX_AT_LEAST(17)
+#if (MPT_COMPILER_GCC || MPT_COMPILER_CLANG)
+// we need to detect the standard library via macro __GLIBCXX__
+#include <vector>
+#endif
+#if MPT_COMPILER_MSVC || MPT_GCC_BEFORE(8,1,0) || MPT_CLANG_BEFORE(5,0,0) || (MPT_COMPILER_GCC && defined(__GLIBCXX__) && (defined(__MINGW32__) || defined(__MINGW64__))) || (MPT_COMPILER_CLANG && defined(__GLIBCXX__)) || (MPT_COMPILER_CLANG && MPT_OS_MACOSX_OR_IOS) || MPT_OS_OPENBSD || MPT_OS_EMSCRIPTEN || MPT_OS_HAIKU || (defined(__clang__) && defined(_MSC_VER))
+#define MPT_COMPILER_QUIRK_NO_ALIGNEDALLOC
+#endif
+#endif
+
+
+
+
+
 // third-party library configuration
 
 #if defined(MODPLUG_TRACKER)
--- common/CompilerDetect.h
+++ common/CompilerDetect.h
@@ -316,9 +316,3 @@
 #define MPT_COMPILER_QUIRK_MSVC_STRINGSTREAM
 #endif
 
-#if MPT_CXX_AT_LEAST(17)
-#if MPT_COMPILER_MSVC || MPT_GCC_BEFORE(8,1,0) || MPT_CLANG_BEFORE(5,0,0) || (MPT_COMPILER_GCC && defined(__GLIBCXX__) && (defined(__MINGW32__) || defined(__MINGW64__))) || (MPT_COMPILER_CLANG && defined(__GLIBCXX__)) || (MPT_COMPILER_CLANG && MPT_OS_MACOSX_OR_IOS) || MPT_OS_OPENBSD || MPT_OS_EMSCRIPTEN || MPT_OS_HAIKU || (defined(__clang__) && defined(_MSC_VER))
-#define MPT_COMPILER_QUIRK_NO_ALIGNEDALLOC
-#endif
-#endif
-
