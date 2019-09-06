2dd921cebceefdfaddf4514d99289061c3de58b4
Johannes Schultz <sagamusix@openmpt.org>
2019-09-06 16:48:57 +0000

    [Imp] Update to UnRAR 5.8.1
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12016 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
-#define RARVER_MINOR    71
-#define RARVER_BETA      0
-#define RARVER_DAY      28
-#define RARVER_MONTH     4
+#define RARVER_MINOR    80
+#define RARVER_BETA      1
+#define RARVER_DAY      30
+#define RARVER_MONTH     8
 #define RARVER_YEAR   2019
