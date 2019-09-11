177274114e8d85be579e9a4654382bb6036c85d9
Jörn Heusipp <manx@openmpt.org>
2019-09-11 07:06:20 +0000

    [Fix] build: Enable C++17 in genie projects.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12042 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/genie/genie-defaults.lua
+++ build/genie/genie-defaults.lua
@@ -2,6 +2,8 @@
   configuration {}
 
 	objdir ( "../../build/obj/" .. mpt_projectpathname .. "/" .. mpt_projectname )
+	
+	buildoptions { "/std:c++17" }
 
 	configuration { "Debug", "x32" }
 		targetdir ( "../../bin/debug/" .. _ACTION .. "-" .. mpt_bindirsuffix .. "-static/x86" )
