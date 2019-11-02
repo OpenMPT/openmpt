6db6a12dc2cda82f479fcf9ee8d18b810781f004
Jörn Heusipp <manx@openmpt.org>
2019-11-02 12:54:12 +0000

    [Fix] Wine Integration: Use GNU and permissive language extensions. These are required in C++17 mode for modern Wine headers. Fixes <https://bugs.openmpt.org/view.php?id=1263>.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12293 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/wine/wine_wrapper.mk
+++ build/wine/wine_wrapper.mk
@@ -77,8 +77,8 @@ MPT_ARCH_TARGET:=-m$(MPT_ARCH_BITS)
 endif
 
 CPPFLAGS += $(MPT_ARCH_TARGET) -DMPT_WINEGCC -Icommon 
-CXXFLAGS += $(MPT_ARCH_TARGET) -std=c++17 -fPIC -fvisibility=hidden
-CFLAGS   += $(MPT_ARCH_TARGET) -std=c99   -fPIC -fvisibility=hidden
+CXXFLAGS += $(MPT_ARCH_TARGET) -std=gnu++17 -fpermissive -fPIC -fvisibility=hidden
+CFLAGS   += $(MPT_ARCH_TARGET) -std=gnu99                -fPIC -fvisibility=hidden
 LDFLAGS  += $(MPT_ARCH_TARGET) 
 LDLIBS   += -lm 
 ARFLAGS  += 
