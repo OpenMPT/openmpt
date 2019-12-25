OpenMPT/openmpt

commit 97fe2abf52a27d2a09ca5a695b76ce590a2ff6c6

[Ref] Tuning: Use std::unique_ptr for CTuning::Create*() construction.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12405 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/TuningDialog.cpp
@@ -435,7 +435,7 @@ bool CTuningDialog::AddTuning(CTuningCollection* pTC, Tuning::TUNINGTYPE type)
 		return false;
 	}
 
-	CTuning* pNewTuning = nullptr;
+	std::unique_ptr<CTuning> pNewTuning;
 	if(type == TT_GROUPGEOMETRIC)
 	{
 		std::vector<Tuning::RATIOTYPE> ratios;
@@ -452,14 +452,14 @@ bool CTuningDialog::AddTuning(CTuningCollection* pTC, Tuning::TUNINGTYPE type)
 		pNewTuning = CTuning::CreateGeneral("Unnamed");
 	}
 
-	if(!pTC->AddTuning(pNewTuning))
+	CTuning *pT = pTC->AddTuning(std::move(pNewTuning));
+	if(!pT)
 	{
 		Reporting::Notification("Add tuning failed");
-		delete pNewTuning;
 		return false;
 	}
-	AddTreeItem(pNewTuning, m_TreeItemTuningItemMap.GetMapping_21(TUNINGTREEITEM(pTC)), NULL);
-	m_pActiveTuning = pNewTuning;
+	AddTreeItem(pT, m_TreeItemTuningItemMap.GetMapping_21(TUNINGTREEITEM(pTC)), NULL);
+	m_pActiveTuning = pT;
 	m_ModifiedTCs[pTC] = true;
 	UpdateView();
 
@@ -799,7 +799,7 @@ void CTuningDialog::OnBnClickedButtonImport()
 		CTuningCollection *pTC = nullptr;
 		CString tcName;
 		mpt::PathString tcFilename;
-		CTuning *pT = nullptr;
+		std::unique_ptr<CTuning> pT;
 
 		if(bIsTun && CheckMagic(fin, 0, magicTC))
 		{
@@ -814,7 +814,7 @@ void CTuningDialog::OnBnClickedButtonImport()
 				if(pTC->GetNumTunings() == 1)
 				{
 					Reporting::Message(LogInformation, U_("- Tuning Collection with a Tuning file extension (.tun) detected. It only contains a single Tuning, importing the file as a Tuning.\n"), this);
-					pT = new CTuning(pTC->GetTuning(0));
+					pT = std::unique_ptr<CTuning>(new CTuning(pTC->GetTuning(0)));
 					delete pTC;
 					pTC = nullptr;
 					// ok
@@ -863,11 +863,7 @@ void CTuningDialog::OnBnClickedButtonImport()
 			EnSclImport a = ImportScl(file, fileName.ToUnicode(), pT);
 			if(a != enSclImportOk)
 			{ // failure
-				if(pT)
-				{
-					delete pT;
-					pT = nullptr;
-				}
+				pT = nullptr;
 			}
 
 		}
@@ -875,10 +871,9 @@ void CTuningDialog::OnBnClickedButtonImport()
 		if(pT)
 		{
 			CTuningCollection &tc = *m_TuningCollections.front();
-			if(!tc.AddTuning(pT))
+			CTuning *activeTuning = tc.AddTuning(std::move(pT));
+			if(!activeTuning)
 			{
-				delete pT;
-				pT = nullptr;
 				if(tc.GetNumTunings() >= CTuningCollection::s_nMaxTuningCount)
 				{
 					sLoadReport += mpt::format(U_("- Failed to load file \"%1\": maximum number(%2) of temporary tunings is already open.\n"))(fileNameExt, static_cast<std::size_t>(CTuningCollection::s_nMaxTuningCount));
@@ -888,7 +883,7 @@ void CTuningDialog::OnBnClickedButtonImport()
 				}
 			} else
 			{
-				m_pActiveTuning = pT;
+				m_pActiveTuning = activeTuning;
 				AddTreeItem(m_pActiveTuning, m_TreeItemTuningItemMap.GetMapping_21(TUNINGTREEITEM(&tc)), NULL);
 			}
 		}
@@ -1269,23 +1264,23 @@ bool CTuningDialog::AddTuning(CTuningCollection* pTC, CTuning* pT)
 		return false;
 	}
 
-	CTuning* pNewTuning = nullptr;
+	std::unique_ptr<CTuning> pNewTuning;
 	if(pT)
 	{
-		pNewTuning = new CTuning(*pT);
+		pNewTuning = std::unique_ptr<CTuning>(new CTuning(*pT));
 	} else
 	{
 		Reporting::Notification("Add tuning failed");
 		return false;
 	}
-	if(!pTC->AddTuning(pNewTuning))
+	CTuning *pNewTuningTmp = pTC->AddTuning(std::move(pNewTuning));
+	if(!pNewTuningTmp)
 	{
 		Reporting::Notification("Add tuning failed");
-		delete pNewTuning;
 		return false;
 	}
-	AddTreeItem(pNewTuning, m_TreeItemTuningItemMap.GetMapping_21(TUNINGTREEITEM(pTC)), NULL);
-	m_pActiveTuning = pNewTuning;
+	AddTreeItem(pNewTuningTmp, m_TreeItemTuningItemMap.GetMapping_21(TUNINGTREEITEM(pTC)), NULL);
+	m_pActiveTuning = pNewTuningTmp;
 	m_ModifiedTCs[pTC] = true;
 	UpdateView();
 
@@ -1543,7 +1538,7 @@ static inline SclFloat CentToRatio(const SclFloat& val)
 }
 
 
-CTuningDialog::EnSclImport CTuningDialog::ImportScl(const mpt::PathString &filename, const mpt::ustring &name, CTuning * & result)
+CTuningDialog::EnSclImport CTuningDialog::ImportScl(const mpt::PathString &filename, const mpt::ustring &name, std::unique_ptr<CTuning> & result)
 {
 	MPT_ASSERT(result == nullptr);
 	result = nullptr;
@@ -1556,7 +1551,7 @@ CTuningDialog::EnSclImport CTuningDialog::ImportScl(const mpt::PathString &filen
 }
 
 
-CTuningDialog::EnSclImport CTuningDialog::ImportScl(std::istream& iStrm, const mpt::ustring &name, CTuning * & result)
+CTuningDialog::EnSclImport CTuningDialog::ImportScl(std::istream& iStrm, const mpt::ustring &name, std::unique_ptr<CTuning> & result)
 {
 	MPT_ASSERT(result == nullptr);
 	result = nullptr;
@@ -1690,7 +1685,7 @@ CTuningDialog::EnSclImport CTuningDialog::ImportScl(std::istream& iStrm, const m
 		tuningName = mpt::format(U_("%1 notes: %2:%3"))(nNotes - 1, mpt::ufmt::fix(groupRatio), 1);
 	}
 
-	CTuning* pT = CTuning::CreateGroupGeometric(mpt::ToCharset(mpt::Charset::Locale, tuningName), fRatios, groupRatio, 15);
+	std::unique_ptr<CTuning> pT = CTuning::CreateGroupGeometric(mpt::ToCharset(mpt::Charset::Locale, tuningName), fRatios, groupRatio, 15);
 	if(!pT)
 	{
 		return enSclImportTuningCreationFailure;
@@ -1726,7 +1721,7 @@ CTuningDialog::EnSclImport CTuningDialog::ImportScl(std::istream& iStrm, const m
 		}
 	}
 
-	result = pT;
+	result = std::move(pT);
 
 	return enSclImportOk;
 }

mptrack/TrackerSettings.cpp
@@ -839,22 +839,18 @@ void TrackerSettings::MigrateTunings(const Version storedVersion)
 		mpt::PathString fn = PathTunings.GetDefaultDir() + P_("Built-in\\12TET.tun");
 		if(!fn.FileOrDirectoryExists())
 		{
-			CTuning *pT = CSoundFile::CreateTuning12TET("12TET");
+			std::unique_ptr<CTuning> pT = CSoundFile::CreateTuning12TET("12TET");
 			mpt::SafeOutputFile sf(fn, std::ios::binary, mpt::FlushMode::Full);
 			pT->Serialize(sf);
-			delete pT;
-			pT = nullptr;
 		}
 	}
 	{
 		mpt::PathString fn = PathTunings.GetDefaultDir() + P_("Built-in\\12TET [[fs15 1.17.02.49]].tun");
 		if(!fn.FileOrDirectoryExists())
 		{
-			CTuning *pT = CSoundFile::CreateTuning12TET("12TET [[fs15 1.17.02.49]]");
+			std::unique_ptr<CTuning> pT = CSoundFile::CreateTuning12TET("12TET [[fs15 1.17.02.49]]");
 			mpt::SafeOutputFile sf(fn, std::ios::binary, mpt::FlushMode::Full);
 			pT->Serialize(sf);
-			delete pT;
-			pT = nullptr;
 		}
 	}
 	oldLocalTunings = LoadLocalTunings();

mptrack/TuningDialog.h
@@ -258,8 +258,8 @@ private:
 	bool IsDeletable(const CTuningCollection* const pTC) const;
 
 	// Scl-file import.
-	EnSclImport ImportScl(const mpt::PathString &filename, const mpt::ustring &name, CTuning * & result);
-	EnSclImport ImportScl(std::istream& iStrm, const mpt::ustring &name, CTuning * & result);
+	EnSclImport ImportScl(const mpt::PathString &filename, const mpt::ustring &name, std::unique_ptr<CTuning> & result);
+	EnSclImport ImportScl(std::istream& iStrm, const mpt::ustring &name, std::unique_ptr<CTuning> & result);
 
 
 private:

soundlib/Load_it.cpp
@@ -210,11 +210,12 @@ static void ReadTuningMapImpl(std::istream& iStrm, CSoundFile& csf, const size_t
 			CTuning *localTuning = TrackerSettings::Instance().oldLocalTunings->GetTuning(str);
 			if(localTuning)
 			{
-				CTuning* pNewTuning = new CTuning(*localTuning);
-				if(csf.GetTuneSpecificTunings().AddTuning(pNewTuning))
+				std::unique_ptr<CTuning> pNewTuning = std::unique_ptr<CTuning>(new CTuning(*localTuning));
+				CTuning *pT = csf.GetTuneSpecificTunings().AddTuning(std::move(pNewTuning));
+				if(pT)
 				{
 					csf.AddToLog("Local tunings are deprecated and no longer supported. Tuning '" + str + "' found in Local tunings has been copied to Tune-specific tunings and will be saved in the module file.");
-					csf.Instruments[i]->pTuning = pNewTuning;
+					csf.Instruments[i]->pTuning = pT;
 					if(csf.GetpModDoc() != nullptr)
 					{
 						csf.GetpModDoc()->SetModified();
@@ -222,7 +223,6 @@ static void ReadTuningMapImpl(std::istream& iStrm, CSoundFile& csf, const size_t
 					continue;
 				} else
 				{
-					delete pNewTuning;
 					csf.AddToLog("Copying Local tuning '" + str + "' to Tune-specific tunings failed.");
 				}
 			}
@@ -230,12 +230,13 @@ static void ReadTuningMapImpl(std::istream& iStrm, CSoundFile& csf, const size_t
 
 			if(str == "12TET [[fs15 1.17.02.49]]" || str == "12TET")
 			{
-				CTuning* pNewTuning = csf.CreateTuning12TET(str);
-				if(csf.GetTuneSpecificTunings().AddTuning(pNewTuning))
+				std::unique_ptr<CTuning> pNewTuning = csf.CreateTuning12TET(str);
+				CTuning *pT = csf.GetTuneSpecificTunings().AddTuning(std::move(pNewTuning));
+				if(pT)
 				{
 					#ifdef MODPLUG_TRACKER
 						csf.AddToLog("Built-in tunings will no longer be used. Tuning '" + str + "' has been copied to Tune-specific tunings and will be saved in the module file.");
-						csf.Instruments[i]->pTuning = pNewTuning;
+						csf.Instruments[i]->pTuning = pT;
 						if(csf.GetpModDoc() != nullptr)
 						{
 							csf.GetpModDoc()->SetModified();
@@ -244,7 +245,6 @@ static void ReadTuningMapImpl(std::istream& iStrm, CSoundFile& csf, const size_t
 					continue;
 				} else
 				{
-					delete pNewTuning;
 					#ifdef MODPLUG_TRACKER
 						csf.AddToLog("Copying Built-in tuning '" + str + "' to Tune-specific tunings failed.");
 					#endif

soundlib/Sndfile.cpp
@@ -1443,9 +1443,9 @@ bool CSoundFile::DestroySampleThreadsafe(SAMPLEINDEX nSample)
 }
 
 
-CTuning* CSoundFile::CreateTuning12TET(const std::string &name)
+std::unique_ptr<CTuning> CSoundFile::CreateTuning12TET(const std::string &name)
 {
-	CTuning* pT = CTuning::CreateGeometric(name, 12, 2, 15);
+	std::unique_ptr<CTuning> pT = CTuning::CreateGeometric(name, 12, 2, 15);
 	for(ModCommand::NOTE note = 0; note < 12; ++note)
 	{
 		pT->SetNoteName(note, mpt::ToCharset(mpt::Charset::ASCII, mpt::ustring(NoteNamesSharp[note])));

soundlib/Sndfile.h
@@ -324,7 +324,7 @@ public:
 
 	//Tuning-->
 public:
-	static CTuning* CreateTuning12TET(const std::string &name);
+	static std::unique_ptr<CTuning> CreateTuning12TET(const std::string &name);
 	static CTuning *GetDefaultTuning() {return nullptr;}
 	CTuningCollection& GetTuneSpecificTunings() {return *m_pTuningsTuneSpecific;}
 

soundlib/tuning.h
@@ -93,72 +93,67 @@ public:
 
 	void SetNoteName(const NOTEINDEXTYPE&, const std::string&);
 
-	static CTuning* CreateDeserialize(std::istream & f)
+	static std::unique_ptr<CTuning> CreateDeserialize(std::istream & f)
 	{
-		CTuning *pT = new CTuning();
+		std::unique_ptr<CTuning> pT = std::unique_ptr<CTuning>(new CTuning());
 		if(pT->InitDeserialize(f) != SerializationResult::Success)
 		{
-			delete pT;
 			return nullptr;
 		}
 		return pT;
 	}
 
 	//Try to read old version (v.3) and return pointer to new instance if succesfull, else nullptr.
-	static CTuning* CreateDeserializeOLD(std::istream & f)
+	static std::unique_ptr<CTuning> CreateDeserializeOLD(std::istream & f)
 	{
-		CTuning *pT = new CTuning();
+		std::unique_ptr<CTuning> pT = std::unique_ptr<CTuning>(new CTuning());
 		if(pT->InitDeserializeOLD(f) != SerializationResult::Success)
 		{
-			delete pT;
 			return nullptr;
 		}
 		return pT;
 	}
 
-	static CTuning* CreateGeneral(const std::string &name)
+	static std::unique_ptr<CTuning> CreateGeneral(const std::string &name)
 	{
-		CTuning *pT = new CTuning();
+		std::unique_ptr<CTuning> pT = std::unique_ptr<CTuning>(new CTuning());
 		pT->SetName(name);
 		return pT;
 	}
 
-	static CTuning* CreateGroupGeometric(const std::string &name, UNOTEINDEXTYPE groupsize, RATIOTYPE groupratio, USTEPINDEXTYPE finestepcount)
+	static std::unique_ptr<CTuning> CreateGroupGeometric(const std::string &name, UNOTEINDEXTYPE groupsize, RATIOTYPE groupratio, USTEPINDEXTYPE finestepcount)
 	{
-		CTuning *pT = new CTuning();
+		std::unique_ptr<CTuning> pT = std::unique_ptr<CTuning>(new CTuning());
 		pT->SetName(name);
 		if(pT->CreateGroupGeometric(groupsize, groupratio, 0) != false)
 		{
-			delete pT;
 			return nullptr;
 		}
 		pT->SetFineStepCount(finestepcount);
 		return pT;
 	}
 
-	static CTuning* CreateGroupGeometric(const std::string &name, const std::vector<RATIOTYPE> &ratios, RATIOTYPE groupratio, USTEPINDEXTYPE finestepcount)
+	static std::unique_ptr<CTuning> CreateGroupGeometric(const std::string &name, const std::vector<RATIOTYPE> &ratios, RATIOTYPE groupratio, USTEPINDEXTYPE finestepcount)
 	{
-		CTuning *pT = new CTuning();
+		std::unique_ptr<CTuning> pT = std::unique_ptr<CTuning>(new CTuning());
 		pT->SetName(name);
 		VRPAIR range = std::make_pair(s_StepMinDefault, static_cast<NOTEINDEXTYPE>(s_StepMinDefault + s_RatioTableSizeDefault - 1));
 		range.second = std::max(range.second, mpt::saturate_cast<NOTEINDEXTYPE>(ratios.size() - 1));
 		range.first = 0 - range.second - 1;
 		if(pT->CreateGroupGeometric(ratios, groupratio, range, 0) != false)
 		{
-			delete pT;
 			return nullptr;
 		}
 		pT->SetFineStepCount(finestepcount);
 		return pT;
 	}
 
-	static CTuning* CreateGeometric(const std::string &name, UNOTEINDEXTYPE groupsize, RATIOTYPE groupratio, USTEPINDEXTYPE finestepcount)
+	static std::unique_ptr<CTuning> CreateGeometric(const std::string &name, UNOTEINDEXTYPE groupsize, RATIOTYPE groupratio, USTEPINDEXTYPE finestepcount)
 	{
-		CTuning *pT = new CTuning();
+		std::unique_ptr<CTuning> pT = std::unique_ptr<CTuning>(new CTuning());
 		pT->SetName(name);
 		if(pT->CreateGeometric(groupsize, groupratio) != false)
 		{
-			delete pT;
 			return nullptr;
 		}
 		pT->SetFineStepCount(finestepcount);

soundlib/tuningCollection.cpp
@@ -227,42 +227,44 @@ bool CTuningCollection::Remove(const std::size_t i)
 }
 
 
-bool CTuningCollection::AddTuning(CTuning *pT)
+CTuning* CTuningCollection::AddTuning(std::unique_ptr<CTuning> pT)
 {
 	if(m_Tunings.size() >= s_nMaxTuningCount)
 	{
-		return false;
+		return nullptr;
 	}
 	if(!pT)
 	{
-		return false;
+		return nullptr;
 	}
-	m_Tunings.push_back(std::unique_ptr<CTuning>(pT));
-	return true;
+	CTuning *result = pT.get();
+	m_Tunings.push_back(std::move(pT));
+	return result;
 }
 
 
-bool CTuningCollection::AddTuning(std::istream& inStrm)
+CTuning* CTuningCollection::AddTuning(std::istream& inStrm)
 {
 	if(m_Tunings.size() >= s_nMaxTuningCount)
 	{
-		return false;
+		return nullptr;
 	}
 	if(!inStrm.good())
 	{
-		return false;
+		return nullptr;
 	}
-	CTuning *pT = CTuning::CreateDeserializeOLD(inStrm);
+	std::unique_ptr<CTuning> pT = CTuning::CreateDeserializeOLD(inStrm);
 	if(!pT)
 	{
 		pT = CTuning::CreateDeserialize(inStrm);
 	}
 	if(!pT)
 	{
-		return false;
+		return nullptr;
 	}
-	m_Tunings.push_back(std::unique_ptr<CTuning>(pT));
-	return true;
+	CTuning *result = pT.get();
+	m_Tunings.push_back(std::move(pT));
+	return result;
 }
 
 

commit a5632e788df845038df349a986624e661688156a

[Fix] Tuning Dialog: Do not show error after successful loading. Broken since r12405.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12417 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/TuningDialog.cpp
@@ -862,6 +862,8 @@ void CTuningDialog::OnBnClickedButtonImport()
 
 		}
 
+		bool success = false;
+
 		if(pT)
 		{
 			CTuningCollection &tc = *m_TuningCollections.front();
@@ -879,6 +881,7 @@ void CTuningDialog::OnBnClickedButtonImport()
 			{
 				m_pActiveTuning = activeTuning;
 				AddTreeItem(m_pActiveTuning, m_TreeItemTuningItemMap.GetMapping_21(TUNINGTREEITEM(&tc)), NULL);
+				success = true;
 			}
 		}
 
@@ -889,9 +892,10 @@ void CTuningDialog::OnBnClickedButtonImport()
 			m_TuningCollectionsFilenames[pTC] = tcFilename;
 			m_DeletableTuningCollections.push_back(pTC);
 			AddTreeItem(pTC, NULL, NULL);
+			success = true;
 		}
 
-		if(!pT && !pTC)
+		if(!success)
 		{
 			sLoadReport += mpt::format(U_("- Unable to load \"%1\": unrecognized file format.\n"))(fileNameExt);
 		}