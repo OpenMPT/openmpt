OpenMPT/openmpt

commit 6611618ffccbc34e1590187fc7af72b0067a582b

[Fix] More accurate DBM/DIGI vibrato command. [Fix] IT Compatibility: Impulse Tracker increments the vibrato position before applying the vibrato, not afterwards. [Mod] OpenMPT: Version is now 1.27.00.61
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8805 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               27
 #define VER_MINOR               00
-#define VER_MINORMINOR          60
+#define VER_MINORMINOR          61
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

soundlib/Sndmix.cpp
@@ -745,6 +745,14 @@ int CSoundFile::GetVibratoDelta(int type, int position) const
 		case 3:	// Random
 			return mpt::random<int, 7>(AccessPRNG()) - 0x40;
 		}
+	} else if(GetType() & (MOD_TYPE_DIGI | MOD_TYPE_DBM))
+	{
+		static const int8 DBMSinus[] =
+		{
+			-128, -104, -79, -54, -31, -8, 13, 33, 52, 69, 84, 96, 107, 116, 122, 125,
+			127, 125, 122, 116, 107, 96, 84, 69, 52, 33, 13, -8, -31, -54, -79, -104,
+		};
+		return DBMSinus[(position / 2u) & 0x1F];
 	} else
 	{
 		position &= 0x3F;
@@ -1111,7 +1119,7 @@ void CSoundFile::ProcessPitchFilterEnvelope(ModChannel *pChn, int &period) const
 				}
 			} else //Original behavior
 			{
-				const bool useFreq = m_SongFlags[SONG_LINEARSLIDES] && m_playBehaviour[kHertzInLinearMode];
+				const bool useFreq = PeriodsAreFrequencies();
 				const uint32 (&upTable)[256] = useFreq ? LinearSlideUpTable : LinearSlideDownTable;
 				const uint32 (&downTable)[256] = useFreq ? LinearSlideDownTable : LinearSlideUpTable;
 
@@ -1426,7 +1434,7 @@ void CSoundFile::ProcessArpeggio(CHANNELINDEX nChn, int &period, Tuning::NOTEIND
 					case 1: arpRatio = LinearSlideUpTable[(pChn->nArpeggio >> 4) * 16]; break;
 					case 2: arpRatio = LinearSlideUpTable[(pChn->nArpeggio & 0x0F) * 16]; break;
 					}
-					if(m_SongFlags[SONG_LINEARSLIDES] && m_playBehaviour[kHertzInLinearMode])
+					if(PeriodsAreFrequencies())
 						period = Util::muldivr(period, arpRatio, 65536);
 					else
 						period = Util::muldivr(period, 65536, arpRatio);
@@ -1525,6 +1533,10 @@ void CSoundFile::ProcessVibrato(CHANNELINDEX nChn, int &period, Tuning::RATIOTYP
 			return;
 		}
 
+		// IT compatibility: IT has its own, more precise tables and pre-increments the vibrato position
+		if(m_playBehaviour[kITVibratoTremoloPanbrello])
+			chn.nVibratoPos += 4 * chn.nVibratoSpeed;
+
 		int vdelta = GetVibratoDelta(chn.nVibratoType, chn.nVibratoPos);
 
 		if(GetType() == MOD_TYPE_MPT && chn.pModInstrument && chn.pModInstrument->pTuning)
@@ -1569,9 +1581,15 @@ void CSoundFile::ProcessVibrato(CHANNELINDEX nChn, int &period, Tuning::RATIOTYP
 				}
 			} else
 			{
-				vdepth = ((!(GetType() & (MOD_TYPE_IT | MOD_TYPE_MPT))) || m_SongFlags[SONG_ITOLDEFFECTS]) ? 6 : 7;
-				if(GetType() & (MOD_TYPE_DBM | MOD_TYPE_MTM)) vdepth = 7;	// Closer than 6, but not quite.
-				if(m_SongFlags[SONG_S3MOLDVIBRATO]) vdepth = 5;
+				if(m_SongFlags[SONG_S3MOLDVIBRATO])
+					vdepth = 5;
+				else if(GetType() & (MOD_TYPE_DBM | MOD_TYPE_MTM))
+					vdepth = 7;
+				else if((GetType() & (MOD_TYPE_IT | MOD_TYPE_MPT)) && !m_SongFlags[SONG_ITOLDEFFECTS])
+					vdepth = 7;
+				else
+					vdepth = 6;
+
 				// ST3 compatibility: Do not distinguish between vibrato types in effect memory
 				// Test case: VibratoTypeChange.s3m
 				if(m_playBehaviour[kST3VibratoMemory] && chn.rowCommand.command == CMD_FINEVIBRATO)
@@ -1625,10 +1643,8 @@ void CSoundFile::ProcessVibrato(CHANNELINDEX nChn, int &period, Tuning::RATIOTYP
 				return;
 			}
 
-			// IT compatibility: IT has its own, more precise tables
-			if(m_playBehaviour[kITVibratoTremoloPanbrello])
-				chn.nVibratoPos += 4 * chn.nVibratoSpeed;
-			else
+			// IT compatibility: IT has its own, more precise tables and pre-increments the vibrato position
+			if(!m_playBehaviour[kITVibratoTremoloPanbrello])
 				chn.nVibratoPos += chn.nVibratoSpeed;
 		}
 	} else if(chn.dwOldFlags[CHN_VIBRATO])
@@ -1656,7 +1672,7 @@ void CSoundFile::ProcessSampleAutoVibrato(ModChannel *pChn, int &period, Tuning:
 
 		// In IT linear slide mode, we use frequencies, otherwise we use periods, which are upside down.
 		// In this context, the "up" tables refer to the tables that increase frequency, and the down tables are the ones that decrease frequency.
-		const bool useFreq = m_SongFlags[SONG_LINEARSLIDES] && m_playBehaviour[kHertzInLinearMode];
+		const bool useFreq = PeriodsAreFrequencies();
 		const uint32 (&upTable)[256] = useFreq ? LinearSlideUpTable : LinearSlideDownTable;
 		const uint32 (&downTable)[256] = useFreq ? LinearSlideDownTable : LinearSlideUpTable;
 		const uint32 (&fineUpTable)[16] = useFreq ? FineLinearSlideUpTable : FineLinearSlideDownTable;
@@ -2090,7 +2106,7 @@ bool CSoundFile::ReadNote()
 			// Test case: PeriodLimit.s3m
 			if (pChn->nPeriod < m_nMinPeriod
 				&& GetType() != MOD_TYPE_S3M
-				&& !(m_playBehaviour[kHertzInLinearMode] && m_SongFlags[SONG_LINEARSLIDES]))
+				&& !PeriodsAreFrequencies())
 			{
 				pChn->nPeriod = m_nMinPeriod;
 			}

soundlib/Load_dbm.cpp
@@ -33,7 +33,7 @@ struct DBMFileHeader
 MPT_BINARY_STRUCT(DBMFileHeader, 8)
 
 
-// RIFF-style Chunk
+// IFF-style Chunk
 struct DBMChunk
 {
 	// 32-Bit chunk identifiers
@@ -294,7 +294,7 @@ bool CSoundFile::ReadDBM(FileReader &file, ModLoadingFlags loadFlags)
 	}
 
 	ChunkReader chunkFile(file);
-	ChunkReader::ChunkList<DBMChunk> chunks = chunkFile.ReadChunks<DBMChunk>(1);
+	auto chunks = chunkFile.ReadChunks<DBMChunk>(1);
 
 	// Globals
 	DBMInfoChunk infoData;
@@ -311,6 +311,7 @@ bool CSoundFile::ReadDBM(FileReader &file, ModLoadingFlags loadFlags)
 	m_nSamples = std::min<SAMPLEINDEX>(infoData.samples, MAX_SAMPLES - 1);
 	m_madeWithTracker = mpt::format(MPT_USTRING("DigiBooster Pro %1.%2"))(mpt::ufmt::hex(fileHeader.trkVerHi), mpt::ufmt::hex(fileHeader.trkVerLo));
 	m_playBehaviour.set(kSlidesAtSpeed1);
+	m_playBehaviour.reset(kITVibratoTremoloPanbrello);
 
 	// Name chunk
 	FileReader nameChunk = chunks.GetChunk(DBMChunk::idNAME);
@@ -357,8 +358,7 @@ bool CSoundFile::ReadDBM(FileReader &file, ModLoadingFlags loadFlags)
 #endif // MPT_DBM_USE_REAL_SUBSONGS
 
 	// Read instruments
-	FileReader instChunk = chunks.GetChunk(DBMChunk::idINST);
-	if(instChunk.IsValid())
+	if(FileReader instChunk = chunks.GetChunk(DBMChunk::idINST))
 	{
 		for(INSTRUMENTINDEX i = 1; i <= GetNumInstruments(); i++)
 		{
@@ -523,8 +523,7 @@ bool CSoundFile::ReadDBM(FileReader &file, ModLoadingFlags loadFlags)
 		// DBP 3 Documentation says that the defaults are 64/128/128/255, but they appear to be 80/150/80/255 in DBP 2.21
 		uint8 settings[8] = { 0, 80, 0, 150, 0, 80, 0, 255 };
 
-		FileReader dspChunk = chunks.GetChunk(DBMChunk::idDSPE);
-		if(dspChunk.IsValid())
+		if(FileReader dspChunk = chunks.GetChunk(DBMChunk::idDSPE))
 		{
 			uint16 maskLen = dspChunk.ReadUint16BE();
 			for(uint16 i = 0; i < maskLen; i++)
@@ -559,7 +558,7 @@ bool CSoundFile::ReadDBM(FileReader &file, ModLoadingFlags loadFlags)
 			plugin.Info.gain = 10;
 			plugin.Info.reserved = 0;
 			plugin.Info.dwOutputRouting = 0;
-			std::fill(plugin.Info.dwReserved, plugin.Info.dwReserved + CountOf(plugin.Info.dwReserved), 0);
+			std::fill(plugin.Info.dwReserved, plugin.Info.dwReserved + mpt::size(plugin.Info.dwReserved), 0);
 			mpt::String::Write<mpt::String::nullTerminated>(plugin.Info.szName, "Echo");
 			mpt::String::Write<mpt::String::nullTerminated>(plugin.Info.szLibraryName, "DigiBooster Pro Echo");
 
@@ -623,20 +622,24 @@ bool CSoundFile::ReadDBM(FileReader &file, ModLoadingFlags loadFlags)
 		{
 			ModSample &srcSample = Samples[0];
 			const int8 *smpData = srcSample.pSample8;
+			uint32 predelay = Util::muldiv_unsigned(20116, srcSample.nC5Speed, 100000);
+			smpData += predelay * srcSample.GetBytesPerSample();
+			srcSample.nLength -= predelay;
 
 			for(SAMPLEINDEX smp = 1; smp <= GetNumSamples(); smp++)
 			{
 				ModSample &sample = Samples[smp];
 				sample.uFlags.set(srcSample.uFlags);
-				sample.nLength *= 2;
 				LimitMax(sample.nLength, srcSample.nLength);
 				if(sample.nLength)
 				{
 					sample.AllocateSample();
 					memcpy(sample.pSample, smpData, sample.GetSampleSizeInBytes());
-					CSoundFile::AdjustSampleLoop(sample);
 					smpData += sample.GetSampleSizeInBytes();
 					srcSample.nLength -= sample.nLength;
+					uint32 gap = Util::muldiv_unsigned(454, srcSample.nC5Speed, 10000);
+					smpData += gap * srcSample.GetBytesPerSample();
+					srcSample.nLength -= gap;
 				}
 			}
 			srcSample.FreeSample();

soundlib/Snd_fx.cpp
@@ -1833,7 +1833,7 @@ void CSoundFile::NoteChange(ModChannel *pChn, int note, bool bPorta, bool bReset
 					pChn->nVibratoPos = 0x10;
 				else if(GetType() == MOD_TYPE_MTM)
 					pChn->nVibratoPos = 0x20;
-				else
+				else if(!(GetType() & (MOD_TYPE_DIGI | MOD_TYPE_DBM)))
 					pChn->nVibratoPos = 0;
 			}
 			// IT Compatibility: No "retrigger" waveform here

commit 3e1774b9a7163e4f424dcbcd9ff46e2efb9f236d

Merged revision(s) 12608 from trunk/OpenMPT: [Fix] IT: Do not advance vibrato position on first tick in Old Effects mode. Broke in r8805 / OpenMPT 1.27.00.61 (https://bugs.openmpt.org/view.php?id=1309) [Mod] OpenMPT: Version is now 1.27.17.05 ........
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.27@12610 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/versionNumber.h
@@ -19,7 +19,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR          1
 #define VER_MAJOR               27
 #define VER_MINOR               17
-#define VER_MINORMINOR          04
+#define VER_MINORMINOR          05
 
 //Version string. For example "1.17.02.28"
 #define MPT_VERSION_STR         VER_STRINGIZE(VER_MAJORMAJOR) "." VER_STRINGIZE(VER_MAJOR) "." VER_STRINGIZE(VER_MINOR) "." VER_STRINGIZE(VER_MINORMINOR)

soundlib/Sndmix.cpp
@@ -1498,6 +1498,8 @@ void CSoundFile::ProcessVibrato(CHANNELINDEX nChn, int &period, Tuning::RATIOTYP
 
 	if(chn.dwFlags[CHN_VIBRATO])
 	{
+		const bool advancePosition = !m_SongFlags[SONG_FIRSTTICK] || ((GetType() & (MOD_TYPE_IT | MOD_TYPE_MPT)) && !(m_SongFlags[SONG_ITOLDEFFECTS]));
+
 		if(GetType() == MOD_TYPE_669)
 		{
 			if(chn.nVibratoPos % 2u)
@@ -1509,7 +1511,7 @@ void CSoundFile::ProcessVibrato(CHANNELINDEX nChn, int &period, Tuning::RATIOTYP
 		}
 
 		// IT compatibility: IT has its own, more precise tables and pre-increments the vibrato position
-		if(m_playBehaviour[kITVibratoTremoloPanbrello])
+		if(advancePosition && m_playBehaviour[kITVibratoTremoloPanbrello])
 			chn.nVibratoPos += 4 * chn.nVibratoSpeed;
 
 		int vdelta = GetVibratoDelta(chn.nVibratoType, chn.nVibratoPos);
@@ -1612,12 +1614,9 @@ void CSoundFile::ProcessVibrato(CHANNELINDEX nChn, int &period, Tuning::RATIOTYP
 		}
 
 		// Advance vibrato position - IT updates on every tick, unless "old effects" are enabled (in this case it only updates on non-first ticks like other trackers)
-		if(!m_SongFlags[SONG_FIRSTTICK] || ((GetType() & (MOD_TYPE_IT | MOD_TYPE_MPT)) && !(m_SongFlags[SONG_ITOLDEFFECTS])))
-		{
-			// IT compatibility: IT has its own, more precise tables and pre-increments the vibrato position
-			if(!m_playBehaviour[kITVibratoTremoloPanbrello])
-				chn.nVibratoPos += chn.nVibratoSpeed;
-		}
+		// IT compatibility: IT has its own, more precise tables and pre-increments the vibrato position
+		if(advancePosition && !m_playBehaviour[kITVibratoTremoloPanbrello])
+			chn.nVibratoPos += chn.nVibratoSpeed;
 	} else if(chn.dwOldFlags[CHN_VIBRATO])
 	{
 		// Stop MIDI vibrato for plugins: