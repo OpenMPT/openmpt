d2040b3e2f12b8afe02efab2b121cf79cde7eba9
Johannes Schultz <sagamusix@openmpt.org>
2020-05-01 15:52:18 +0000

    Merged revision(s) 12762 from trunk/OpenMPT: [Fix] MOD / S3M: Previous note offset should not be considered for retriggered notes (test case: OxxMemoryWithRetrig.s3m). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@12763 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -5479,10 +5479,14 @@ void CSoundFile::RetrigNote(CHANNELINDEX nChn, int param, int offset)
 			}
 			if (param < 0x100) resetEnv = true;
 		}
-		bool fading = chn.dwFlags[CHN_NOTEFADE];
+		const bool fading = chn.dwFlags[CHN_NOTEFADE];
+		const auto oldPrevNoteOffset = chn.prevNoteOffset;
+		chn.prevNoteOffset = 0;  // Retriggered notes should not use previous offset (test case: OxxMemoryWithRetrig.s3m)
 		// IT compatibility: Really weird combination of envelopes and retrigger (see Storlek's q.it testcase)
 		// Test case: retrig.it
 		NoteChange(chn, note, m_playBehaviour[kITRetrigger], resetEnv, false, nChn);
+		if(!chn.rowCommand.instr)
+			chn.prevNoteOffset = oldPrevNoteOffset;
 		// XM compatibility: Prevent NoteChange from resetting the fade flag in case an instrument number + note-off is present.
 		// Test case: RetrigFade.xm
 		if(fading && GetType() == MOD_TYPE_XM)
