c9f253bda485a9b254b5756201f2982e8abb030a
Jörn Heusipp <manx@openmpt.org>
2020-05-28 12:31:39 +0000

    [Ref] installer/multiarch: Add copy of installer build script.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12944 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/auto/build_openmpt_release_packages_multiarch.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+set BATCH_DIR=%~dp0
+cd %BATCH_DIR%
+
+set MY_DIR=%CD%
+
+if "x%1" == "xauto" (
+	..\..\build\tools\python3\python.exe ..\..\build\auto\build_openmpt_release_packages.py --localtools --singlethreaded --noexamplesongs --noninteractive || goto error
+) else (
+	..\..\build\tools\python3\python.exe ..\..\build\auto\build_openmpt_release_packages.py --localtools --singlethreaded --noexamplesongs || goto error
+)
+
+cd "%MY_DIR%"
+
+goto ok
+
+:ok
+echo "All OK."
+if "x%1" == "xauto" (
+	exit 0
+)
+goto end
+:error
+echo "Error!"
+if "x%1" == "xauto" (
+	exit 1
+)
+goto end
+:end
+cd %MY_DIR%
+pause
+
