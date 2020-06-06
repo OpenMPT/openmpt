59e29ee2fa01a33fa1c7396692844f5c5bde3bbf
Johannes Schultz <sagamusix@openmpt.org>
2020-06-06 14:36:35 +0000

    [Var] unrar: Update to v5.9.3.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12977 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
-#define RARVER_MINOR    90
-#define RARVER_BETA      0
-#define RARVER_DAY      26
-#define RARVER_MONTH     3
+#define RARVER_MINOR    91
+#define RARVER_BETA      1
+#define RARVER_DAY       4
+#define RARVER_MONTH     6
 #define RARVER_YEAR   2020
