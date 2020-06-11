OpenMPT/openmpt

commit 98306efa5ce5900aa7fcb9399e121b409342801e

[Fix] FT2 compatibility: Tentatively fix arpeggio after pitch slide behaviour (http://bugs.openmpt.org/view.php?id=542) - note that the current behaviour is not 100% correct because of the finetune weirdness in FT2's RelocateTon function. Test case: ArpSlide.xm
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@4200 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndmix.cpp
@@ -1219,15 +1219,18 @@ void CSoundFile::ProcessArpeggio(CHANNELINDEX nChn, int &period, CTuning::NOTEIN
 				}
 			} else if(IsCompatibleMode(TRK_FASTTRACKER2))
 			{
-				uint8 note = pChn->nNote;
-				int arpPos = 0;
-
+				// FastTracker 2: Swedish tracker logic (TM) arpeggio
 				if(!m_SongFlags[SONG_FIRSTTICK])
 				{
-					arpPos = m_PlayState.m_nMusicSpeed - (m_PlayState.m_nTickCount % m_PlayState.m_nMusicSpeed);
+					// Arpeggio is added on top of current note, but cannot do it the IT way because of
+					// the behaviour in ArpeggioClamp.xm.
+					// Test case: ArpSlide.xm
+					uint8 note = GetNoteFromPeriod(period, pChn->nFineTune, pChn->nC5Speed);//pChn->nNote;
+
 					// The fact that arpeggio behaves in a totally fucked up way at 16 ticks/row or more is that the arpeggio offset LUT only has 16 entries in FT2.
 					// At more than 16 ticks/row, FT2 reads into the vibrato table, which is placed right after the arpeggio table.
 					// Test case: Arpeggio.xm
+					int arpPos = m_PlayState.m_nMusicSpeed - (m_PlayState.m_nTickCount % m_PlayState.m_nMusicSpeed);
 					if(arpPos > 16) arpPos = 2;
 					else if(arpPos == 16) arpPos = 0;
 					else arpPos %= 3;
@@ -1236,13 +1239,13 @@ void CSoundFile::ProcessArpeggio(CHANNELINDEX nChn, int &period, CTuning::NOTEIN
 					case 1: note += (pChn->nArpeggio >> 4); break;
 					case 2: note += (pChn->nArpeggio & 0x0F); break;
 					}
+
+					// Test case: ArpeggioClamp.xm
+					if(note > 108 + NOTE_MIN && arpPos != 0)
+						note = 108 + NOTE_MIN; // FT2's note limit
+
+					period = GetPeriodFromNote(note, pChn->nFineTune, pChn->nC5Speed);
 				}
-
-				// Test case: ArpeggioClamp.xm
-				if(note > 108 + NOTE_MIN && arpPos != 0)
-					note = 108 + NOTE_MIN; // FT2's note limit
-
-				period = GetPeriodFromNote(note, pChn->nFineTune, pChn->nC5Speed);
 			}
 			// Other trackers
 			else
@@ -2171,7 +2174,7 @@ void CSoundFile::ProcessMidiOut(CHANNELINDEX nChn)
 		{
 			ModCommand::NOTE realNote = note;
 			if(ModCommand::IsNote(note))
-				realNote = pIns->NoteMap[note - 1];
+				realNote = pIns->NoteMap[note - NOTE_MIN];
 			pPlugin->MidiCommand(GetBestMidiChannel(nChn), pIns->nMidiProgram, pIns->wMidiBank, realNote, static_cast<uint16>(chn.nVolume), nChn);
 		} else if(hasVolCommand)
 		{
@@ -2195,7 +2198,7 @@ void CSoundFile::ProcessMidiOut(CHANNELINDEX nChn)
 
 		ModCommand::NOTE realNote = note;
 		if(ModCommand::IsNote(note))
-			realNote = pIns->NoteMap[note - 1];
+			realNote = pIns->NoteMap[note - NOTE_MIN];
 		// Experimental VST panning
 		//ProcessMIDIMacro(nChn, false, m_MidiCfg.szMidiGlb[MIDIOUT_PAN], 0, nPlugin);
 		pPlugin->MidiCommand(GetBestMidiChannel(nChn), pIns->nMidiProgram, pIns->wMidiBank, realNote, velocity, nChn);

soundlib/Snd_fx.cpp
@@ -4899,8 +4899,9 @@ void CSoundFile::GlobalVolSlide(UINT param, UINT &nOldGlobalVolSlide)
 //////////////////////////////////////////////////////
 // Note/Period/Frequency functions
 
-UINT CSoundFile::GetNoteFromPeriod(UINT period) const
-//---------------------------------------------------
+// Find lowest note which has same or lower period as a given period (i.e. the note has the same or higher frequency)
+UINT CSoundFile::GetNoteFromPeriod(UINT period, int nFineTune, UINT nC5Speed) const
+//---------------------------------------------------------------------------------
 {
 	if (!period) return 0;
 	if (GetType() & (MOD_TYPE_MED|MOD_TYPE_MOD|MOD_TYPE_DIGI|MOD_TYPE_MTM|MOD_TYPE_669|MOD_TYPE_OKT|MOD_TYPE_AMF0))
@@ -4922,12 +4923,22 @@ UINT CSoundFile::GetNoteFromPeriod(UINT period) const
 		return 6*12+36;
 	} else
 	{
-		for (UINT i=NOTE_MIN; i<NOTE_MAX; i++)
+		// This essentially implements std::lower_bound, with the difference that we don't need an iterable container.
+		uint32 minNote = NOTE_MIN, maxNote = NOTE_MAX, count = maxNote - minNote + 1;
+		while(count > 0)
 		{
-			LONG n = GetPeriodFromNote(i, 0, 0);
-			if ((n > 0) && (n <= (LONG)period)) return i;
+			const uint32 step = count / 2, midNote = minNote + step;
+			uint32 n = GetPeriodFromNote(midNote, nFineTune, nC5Speed);
+			if(n > period || !n)
+			{
+				minNote = midNote + 1;
+				count -= step + 1;
+			} else
+			{
+				count = step;
+			}
 		}
-		return NOTE_MAX;
+		return minNote;
 	}
 }
 

soundlib/Sndfile.h
@@ -874,9 +874,9 @@ public:
 	bool ReadSampleFromSong(SAMPLEINDEX targetSample, const CSoundFile &srcSong, SAMPLEINDEX sourceSample);
 
 	// Period/Note functions
-	UINT GetNoteFromPeriod(UINT period) const;
+	UINT GetNoteFromPeriod(UINT period, int nFineTune = 0, UINT nC5Speed = 0) const;
 	UINT GetPeriodFromNote(UINT note, int nFineTune, UINT nC5Speed) const;
-	UINT GetFreqFromPeriod(UINT period, UINT nC5Speed, int nPeriodFrac=0) const;
+	UINT GetFreqFromPeriod(UINT period, UINT nC5Speed, int nPeriodFrac = 0) const;
 	// Misc functions
 	ModSample &GetSample(SAMPLEINDEX sample) { ASSERT(sample <= m_nSamples && sample < CountOf(Samples)); return Samples[sample]; }
 	const ModSample &GetSample(SAMPLEINDEX sample) const { ASSERT(sample <= m_nSamples && sample < CountOf(Samples)); return Samples[sample]; }

commit 8c5c8c8fe18528ad576c93a1bea4e857acd708e1

Merged revision(s) 13006 from trunk/OpenMPT: [Fix] XM: Out-of-range arpeggio clamping behaviour broke in r4200 (release OpenMPT 1.23.05.00). Fixes an edge case now also covered by ArpeggioClamp.xm, and fixes the arpeggios in Binary World by Dakato (which sounded correct in OpenMPT 1.23.04.00). [Mod] OpenMPT: Version is now 1.28.10.05
........


git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@13008 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/versionNumber.h
@@ -21,7 +21,7 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR  1
 #define VER_MAJOR      28
 #define VER_MINOR      10
-#define VER_MINORMINOR 04
+#define VER_MINORMINOR 05
 
 //Numerical value of the version.
 #define MPT_VERSION_CURRENT MAKE_VERSION_NUMERIC(VER_MAJORMAJOR,VER_MAJOR,VER_MINOR,VER_MINORMINOR)

soundlib/Sndmix.cpp
@@ -1479,30 +1479,40 @@ void CSoundFile::ProcessArpeggio(CHANNELINDEX nChn, int &period, Tuning::NOTEIND
 					// Arpeggio is added on top of current note, but cannot do it the IT way because of
 					// the behaviour in ArpeggioClamp.xm.
 					// Test case: ArpSlide.xm
-					auto note = GetNoteFromPeriod(period, chn.nFineTune, chn.nC5Speed);
+					uint32 note = 0;
 
 					// The fact that arpeggio behaves in a totally fucked up way at 16 ticks/row or more is that the arpeggio offset LUT only has 16 entries in FT2.
 					// At more than 16 ticks/row, FT2 reads into the vibrato table, which is placed right after the arpeggio table.
 					// Test case: Arpeggio.xm
 					int arpPos = m_PlayState.m_nMusicSpeed - (m_PlayState.m_nTickCount % m_PlayState.m_nMusicSpeed);
-					if(arpPos > 16) arpPos = 2;
-					else if(arpPos == 16) arpPos = 0;
-					else arpPos %= 3;
+					if(arpPos > 16)
+						arpPos = 2;
+					else if(arpPos == 16)
+						arpPos = 0;
+					else
+						arpPos %= 3;
 					switch(arpPos)
 					{
-					case 1: note += (chn.nArpeggio >> 4); break;
-					case 2: note += (chn.nArpeggio & 0x0F); break;
+					case 1: note = (chn.nArpeggio >> 4); break;
+					case 2: note = (chn.nArpeggio & 0x0F); break;
 					}
 
-					period = GetPeriodFromNote(note, chn.nFineTune, chn.nC5Speed);
-
-					// FT2 compatibility: FT2 has a different note limit for Arpeggio.
-					// Test case: ArpeggioClamp.xm
-					if(note >= 108 + NOTE_MIN && arpPos != 0)
+					if(arpPos != 0)
 					{
-						period = std::max<uint32>(period, GetPeriodFromNote(108 + NOTE_MIN, 0, chn.nC5Speed));
-					}
+						// Arpeggio is added on top of current note, but cannot do it the IT way because of
+						// the behaviour in ArpeggioClamp.xm.
+						// Test case: ArpSlide.xm
+						note += GetNoteFromPeriod(period, chn.nFineTune, chn.nC5Speed);
 
+						period = GetPeriodFromNote(note, chn.nFineTune, chn.nC5Speed);
+
+						// FT2 compatibility: FT2 has a different note limit for Arpeggio.
+						// Test case: ArpeggioClamp.xm
+						if(note >= 108 + NOTE_MIN)
+						{
+							period = std::max(static_cast<uint32>(period), GetPeriodFromNote(108 + NOTE_MIN, 0, chn.nC5Speed));
+						}
+					}
 				}
 			}
 			// Other trackers