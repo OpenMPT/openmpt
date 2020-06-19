a3f7524be616fbe358e1b0d4e0661fea09c0923c
Jörn Heusipp <manx@openmpt.org>
2020-06-19 12:37:25 +0000

    [Imp] build: Do not add svn revision to release version packages.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13042 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_openmpt_installer_multiarch_args.cmd
+++ build/auto/package_openmpt_installer_multiarch_args.cmd
@@ -14,6 +14,9 @@ call build\auto\helper_get_svnversion.cmd
 call build\auto\helper_get_openmpt_version.cmd
 
 set MPT_REVISION=%OPENMPT_VERSION%-%SVNVERSION%
+if "x%OPENMPT_VERSION_MINORMINOR%" == "x00" (
+	set MPT_REVISION=%OPENMPT_VERSION%
+)
 
 
 
@@ -25,22 +28,22 @@ mkdir openmpt\pkg.win
 mkdir openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%
 mkdir openmpt\dbg.win
 mkdir openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.exe                         openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-Setup.exe
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.exe.digests                 openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-Setup.exe.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.zip                  openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-x86.zip
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.zip.digests          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-x86.zip.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.zip           openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-x86-legacy.zip
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.zip.digests   openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-x86-legacy.zip.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.zip                openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-amd64.zip
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.zip.digests        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-amd64.zip.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.zip         openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-amd64-legacy.zip
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.zip.digests openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-amd64-legacy.zip.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.zip                  openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-arm.zip
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.zip.digests          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-arm.zip.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.zip                openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-arm64.zip
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.zip.digests        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-portable-arm64.zip.digests
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-symbols.7z                        openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-symbols.7z
-copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-symbols.7z.digests                openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%OPENMPT_VERSION%-%SVNVERSION%-symbols.7z.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.exe                         openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-Setup.exe
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.exe.digests                 openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-Setup.exe.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.zip                  openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86.zip
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.zip.digests          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.zip           openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86-legacy.zip
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.zip.digests   openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86-legacy.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.zip                openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64.zip
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.zip.digests        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.zip         openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64-legacy.zip
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.zip.digests openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64-legacy.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.zip                  openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm.zip
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.zip.digests          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.zip                openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm64.zip
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.zip.digests        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm64.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-symbols.7z                        openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-symbols.7z
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-symbols.7z.digests                openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-symbols.7z.digests
 ..\build\tools\7zip\7z.exe a -ttar openmpt-pkg.win-multi.tar openmpt || goto error
 rmdir /s /q openmpt
 cd .. || goto error
