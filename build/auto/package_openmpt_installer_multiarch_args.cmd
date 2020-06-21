5f1b97c0fa02627a10de6ecc1a0b6590dd9d269b
Jörn Heusipp <manx@openmpt.org>
2020-06-21 07:43:25 +0000

    [Mod] installer: autoupdate: Split update json into parts requiering signature and parts which do not. [Mod] installer: autoupdate: Add SHA512 and SHA3-512 checksums of downloads to json.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13056 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_openmpt_installer_multiarch_args.cmd
+++ build/auto/package_openmpt_installer_multiarch_args.cmd
@@ -28,18 +28,25 @@ mkdir openmpt\dbg.win
 mkdir openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.exe                         openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-Setup.exe
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.exe.digests                 openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-Setup.exe.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-Setup.update.json                 openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-Setup.update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.zip                  openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86.zip
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.zip.digests          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86.update.json          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86.update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.zip           openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86-legacy.zip
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.zip.digests   openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86-legacy.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-x86-legacy.update.json   openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-x86-update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.zip                openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64.zip
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.zip.digests        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64.update.json        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.zip         openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64-legacy.zip
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.zip.digests openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64-legacy.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-amd64-legacy.update.json openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-amd64-legacy.update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.zip                  openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm.zip
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.zip.digests          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm.update.json          openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm.update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.zip                openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm64.zip
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.zip.digests        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm64.zip.digests
+copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-portable-arm64.update.json        openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-portable-arm64.update.json
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-symbols.7z                        openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-symbols.7z
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-symbols.7z.digests                openmpt\dbg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-symbols.7z.digests
 copy /y ..\installer\OpenMPT-%OPENMPT_VERSION%-update.json                       openmpt\pkg.win\%OPENMPT_VERSION_MAJORMAJOR%.%OPENMPT_VERSION_MAJOR%\OpenMPT-%MPT_REVISION%-update.json
--- installer/generate_update_json.py
+++ installer/generate_update_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
+import hashlib
 import json
 import os
 
@@ -13,7 +14,154 @@ SVNVERSION = os.environ['SVNVERSION']
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 os.chdir("..")
 
+plainversion = OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR
 version = OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR if OPENMPT_VERSION_MINORMINOR == "00" else OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-" + SVNVERSION
+
+def hash_file_sha512(filename):
+	sha512 = hashlib.sha512()
+	with open(filename, "rb") as f:
+		sha512.update(f.read())
+	return sha512.hexdigest()
+def hash_file_sha3_512(filename):
+	sha3_512 = hashlib.sha3_512()
+	with open(filename, "rb") as f:
+		sha3_512.update(f.read())
+	return sha3_512.hexdigest()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-Setup.exe",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-Setup.exe"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-Setup.exe"),
+	},
+	"filename": "OpenMPT-" + version + "-Setup.exe",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": {
+		"required_architectures": { "x86":True },
+		"arguments": [ "/SP-", "/SILENT", "/NOCANCEL", "/AUTOUPDATE=yes" ]
+	},
+	"autoupdate_archive": None
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-Setup.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-x86.zip",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-portable-x86.zip"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-portable-x86.zip"),
+	},
+	"filename": "OpenMPT-" + version + "-portable-x86.zip",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": None,
+	"autoupdate_archive": {
+		"subfolder": "",
+		"restartbinary": "OpenMPT.exe"
+	}
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-portable-x86.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-x86-legacy.zip",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-portable-x86-legacy.zip"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-portable-x86-legacy.zip"),
+	},
+	"filename": "OpenMPT-" + version + "-portable-x86-legacy.zip",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": None,
+	"autoupdate_archive": {
+		"subfolder": "",
+		"restartbinary": "OpenMPT.exe"
+	}
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-portable-x86-legacy.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-amd64.zip",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-portable-amd64.zip"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-portable-amd64.zip"),
+	},
+	"filename": "OpenMPT-" + version + "-portable-amd64.zip",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": None,
+	"autoupdate_archive": {
+		"subfolder": "",
+		"restartbinary": "OpenMPT.exe"
+	}
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-portable-amd64.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-amd64-legacy.zip",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-portable-amd64-legacy.zip"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-portable-amd64-legacy.zip"),
+	},
+	"filename": "OpenMPT-" + version + "-portable-amd64-legacy.zip",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": None,
+	"autoupdate_archive": {
+		"subfolder": "",
+		"restartbinary": "OpenMPT.exe"
+	}
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-portable-amd64-legacy.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-arm.zip",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-portable-arm.zip"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-portable-arm.zip"),
+	},
+	"filename": "OpenMPT-" + version + "-portable-arm.zip",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": None,
+	"autoupdate_archive": {
+		"subfolder": "",
+		"restartbinary": "OpenMPT.exe"
+	}
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-portable-arm.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+update = {
+	"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-arm64.zip",
+	"checksums": {
+		"SHA-512": hash_file_sha512("installer/OpenMPT-" + plainversion + "-portable-arm64.zip"),
+		"SHA3-512": hash_file_sha3_512("installer/OpenMPT-" + plainversion + "-portable-arm64.zip"),
+	},
+	"filename": "OpenMPT-" + version + "-portable-arm64.zip",
+	"can_autoupdate": True,
+	"autoupdate_minversion": "1.30.00.04",
+	"autoupdate_installer": None,
+	"autoupdate_archive": {
+		"subfolder": "",
+		"restartbinary": "OpenMPT.exe"
+	}
+}
+with open("installer/" + "OpenMPT-" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "." + OPENMPT_VERSION_MINOR + "." + OPENMPT_VERSION_MINORMINOR + "-portable-arm64.update.json", "wb") as f:
+	f.write((json.dumps(update, ensure_ascii=False, indent=1)).encode('utf-8'))
+	f.close()
+
+
 
 update = {
 	"OpenMPT " + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR: {
@@ -23,112 +171,56 @@ update = {
 		"changelog_url": "https://source.openmpt.org/browse/openmpt/trunk/OpenMPT/?op=log&isdir=1&",
 		"downloads": {
 			"installer": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-Setup.exe",
-				"filename": "OpenMPT-" + version + "-Setup.exe",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-Setup.update.json",
 				"type": "installer",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": {
-					"required_architectures": { "x86":True },
-					"arguments": [ "/SP-", "/SILENT", "/NOCANCEL", "/AUTOUPDATE=yes" ]
-				},
-				"autoupdate_archive": None,
 				"os": "windows",
 				"required_windows_version": { "version_major":6, "version_minor":1, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "x86":True,"amd64":True,"arm":True,"arm64":True },
 				"required_processor_features": { "x86":{"sse2":True}, "amd64":{"sse2":True} }
 			},
 			"portable-x86": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-x86.zip",
-				"filename": "OpenMPT-" + version + "-portable-x86.zip",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-x86.update.json",
 				"type": "archive",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": None,
-				"autoupdate_archive": {
-					"subfolder": "",
-					"restartbinary": "OpenMPT.exe"
-				},
 				"os": "windows",
 				"required_windows_version": { "version_major":10, "version_minor":0, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "x86":True },
 				"required_processor_features": { "x86":{"sse2":True} }
 			},
 			"portable-x86-legacy": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-x86-legacy.zip",
-				"filename": "OpenMPT-" + version + "-portable-x86-legacy.zip",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-x86-legacy.update.json",
 				"type": "archive",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": None,
-				"autoupdate_archive": {
-					"subfolder": "",
-					"restartbinary": "OpenMPT.exe"
-				},
 				"os": "windows",
 				"required_windows_version": { "version_major":6, "version_minor":1, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "x86":True },
 				"required_processor_features": { "x86":{"sse2":True} }
 			},
 			"portable-amd64": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-amd64.zip",
-				"filename": "OpenMPT-" + version + "-portable-amd64.zip",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-amd64.update.json",
 				"type": "archive",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": None,
-				"autoupdate_archive": {
-					"subfolder": "",
-					"restartbinary": "OpenMPT.exe"
-				},
 				"os": "windows",
 				"required_windows_version": { "version_major":10, "version_minor":0, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "amd64":True },
 				"required_processor_features": { "amd64":{"sse2":True} }
 			},
 			"portable-amd64-legacy": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-amd64-legacy.zip",
-				"filename": "OpenMPT-" + version + "-portable-amd64-legacy.zip",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-amd64-legacy.update.json",
 				"type": "archive",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": None,
-				"autoupdate_archive": {
-					"subfolder": "",
-					"restartbinary": "OpenMPT.exe"
-				},
 				"os": "windows",
 				"required_windows_version": { "version_major":6, "version_minor":1, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "amd64":True },
 				"required_processor_features": { "amd64":{"sse2":True} }
 			},
 			"portable-arm": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-arm.zip",
-				"filename": "OpenMPT-" + version + "-portable-arm.zip",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-arm.update.json",
 				"type": "archive",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": None,
-				"autoupdate_archive": {
-					"subfolder": "",
-					"restartbinary": "OpenMPT.exe"
-				},
 				"os": "windows",
 				"required_windows_version": { "version_major":10, "version_minor":0, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "arm":True },
 				"required_processor_features": { "arm":{} }
 			},
 			"portable-arm64": {
-				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-arm64.zip",
-				"filename": "OpenMPT-" + version + "-portable-arm64.zip",
+				"url": "https://builds.openmpt.org/builds/auto/openmpt/pkg.win/" + OPENMPT_VERSION_MAJORMAJOR + "." + OPENMPT_VERSION_MAJOR + "/OpenMPT-" + version + "-portable-arm64.update.json",
 				"type": "archive",
-				"can_autoupdate": True,
-				"autoupdate_minversion": "1.30.00.04",
-				"autoupdate_installer": None,
-				"autoupdate_archive": {
-					"subfolder": "",
-					"restartbinary": "OpenMPT.exe"
-				},
 				"os": "windows",
 				"required_windows_version": { "version_major":10, "version_minor":0, "servicepack_major":0, "servicepack_minor":0, "build":0, "wine_major":1, "wine_minor":8, "wine_update":0 },
 				"supported_architectures": { "arm64":True },
