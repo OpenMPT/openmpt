3939f13fa3a0e448c45aa6e3bc6199f313ff0b56
Jörn Heusipp <manx@openmpt.org>
2020-07-11 08:58:03 +0000

    Merged revision(s) 13194 from trunk/OpenMPT: [Ref] libopenmpt: Add warning about Clang powerpc codegen bug. See <https://bugs.llvm.org/show_bug.cgi?id=46683>. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@13195 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_impl.cpp
+++ libopenmpt/libopenmpt_impl.cpp
@@ -66,6 +66,10 @@ MPT_WARNING("Warning: libopenmpt built in non thread-safe mode because mutexes a
 MPT_WARNING("Warning: Building libopenmpt with MinGW-w64 without std::thread support is not recommended and is deprecated. Please use MinGW-w64 with posix threading model (as opposed to win32 threading model), or build with mingw-std-threads.")
 #endif // MINGW
 
+#if MPT_CLANG_AT_LEAST(5,0,0)
+#if defined(__powerpc__) && !defined(__powerpc64__)
+MPT_WARNING("Warning: libopenmpt is known to trigger bad code generation with Clang 5 or later on powerpc (32bit) when using -O3. See <https://bugs.llvm.org/show_bug.cgi?id=46683>.")
+#endif
 #endif // !MPT_BUILD_SILENCE_LIBOPENMPT_CONFIGURATION_WARNINGS
 
 #if defined(MPT_ASSERT_HANDLER_NEEDED) && !defined(ENABLE_TESTS)
