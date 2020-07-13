1b69721ab31744d9286ca671d87259ce97651115
Jörn Heusipp <manx@openmpt.org>
2020-07-13 07:04:46 +0000

    [Fix] build: Running libopenmpt test suite for automated builds on Windows broke in r11263.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13224 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/test_libopenmpt_args.cmd
+++ build/auto/test_libopenmpt_args.cmd
@@ -12,11 +12,11 @@ call "build\auto\setup_%MPT_VS_VER%.cmd"
 
 
 
-if "%MPT_BIN_ARCH%" == "x86-32" (
+if "%MPT_BIN_ARCH%" == "x86" (
 	"bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET32%\libopenmpt_test.exe" || goto error
 )
 
-if "%MPT_BIN_ARCH%" == "x86-64" (
+if "%MPT_BIN_ARCH%" == "amd64" (
 	if "%MPT_HOST_BITNESS%" == "64" (
 		"bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET64%\libopenmpt_test.exe" || goto error
 	) else (
