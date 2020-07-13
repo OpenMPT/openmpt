3ebc31f4e72995c6bbfd34c08fc1f4eb21251dd3
Jörn Heusipp <manx@openmpt.org>
2020-07-13 07:28:57 +0000

    Merged revision(s) 13226 from trunk/OpenMPT: [Fix] build: Running libopenmpt test suite for automated builds on Windows broke in r11263. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@13227 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/test_libopenmpt_args.cmd
+++ build/auto/test_libopenmpt_args.cmd
@@ -13,12 +13,12 @@ call "build\auto\setup_%MPT_VS_VER%.cmd"
 
 
 if "%MPT_BIN_ARCH%" == "x86" (
-	"bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET32%\libopenmpt_test.exe" || goto error
+	"bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_TARGET32%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%\libopenmpt_test.exe" || goto error
 )
 
 if "%MPT_BIN_ARCH%" == "amd64" (
 	if "%MPT_HOST_BITNESS%" == "64" (
-		"bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%-%MPT_BIN_TARGET64%\libopenmpt_test.exe" || goto error
+		"bin\%MPT_BIN_CONF%\%MPT_VS_VER%-%MPT_BIN_TARGET64%-%MPT_BIN_RUNTIME%\%MPT_BIN_ARCH%\libopenmpt_test.exe" || goto error
 	) else (
 		echo "Warning: Host is not 64 bit. Skipping 64 bit test suite."
 	)
