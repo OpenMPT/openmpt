OpenMPT/openmpt

commit db6d3b9762653097bd1438d77088131f16e92821

[Ref] Wav Writer: Use mpt::IO.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13213 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/WAVTools.cpp
@@ -335,15 +335,8 @@ void WAVSampleLoop::ConvertToWAV(SmpLength start, SmpLength end, bool bidi)
 
 
 // Output to stream: Initialize with std::ostream*.
-WAVWriter::WAVWriter(std::ostream *stream) : s(stream)
-{
-	// Skip file header for now
-	Seek(sizeof(RIFFHeader));
-}
-
-
-// Output to clipboard: Initialize with pointer to memory and size of reserved memory.
-WAVWriter::WAVWriter(mpt::byte_span data) : memory(data)
+WAVWriter::WAVWriter(mpt::IO::OFileBase &stream)
+	: s(stream)
 {
 	// Skip file header for now
 	Seek(sizeof(RIFFHeader));
@@ -352,15 +345,12 @@ WAVWriter::WAVWriter(mpt::byte_span data) : memory(data)
 
 WAVWriter::~WAVWriter() noexcept(false)
 {
-	if(!s || s->good())
-	{
-		Finalize();
-	}
+	Finalize();
 }
 
 
 // Finalize the file by closing the last open chunk and updating the file header. Returns total size of file.
-size_t WAVWriter::Finalize()
+std::size_t WAVWriter::Finalize()
 {
 	FinalizeChunk();
 
@@ -372,9 +362,6 @@ size_t WAVWriter::Finalize()
 
 	Seek(0);
 	Write(fileHeader);
-
-	s = nullptr;
-	memory = {};
 
 	return totalSize;
 }
@@ -396,10 +383,10 @@ void WAVWriter::FinalizeChunk()
 {
 	if(chunkStartPos != 0)
 	{
-		const size_t chunkSize = position - (chunkStartPos + sizeof(RIFFChunk));
+		const std::size_t chunkSize = position - (chunkStartPos + sizeof(RIFFChunk));
 		chunkHeader.length = mpt::saturate_cast<uint32>(chunkSize);
 
-		size_t curPos = position;
+		std::size_t curPos = position;
 		Seek(chunkStartPos);
 		Write(chunkHeader);
 
@@ -417,36 +404,19 @@ void WAVWriter::FinalizeChunk()
 
 
 // Seek to a position in file.
-void WAVWriter::Seek(size_t pos)
+void WAVWriter::Seek(std::size_t pos)
 {
 	position = pos;
 	totalSize = std::max(totalSize, position);
-
-	if(s != nullptr)
-	{
-		s->seekp(position);
-	}
+	mpt::IO::SeekAbsolute(s, pos);
 }
 
 
 // Write some data to the file.
-void WAVWriter::Write(const void *data, size_t numBytes)
+void WAVWriter::Write(mpt::const_byte_span data)
 {
-	if(s != nullptr)
-	{
-		s->write(mpt::void_cast<const char*>(data), numBytes);
-	} else if(!memory.empty())
-	{
-		if(position <= memory.size() && numBytes <= memory.size() - position)
-		{
-			memcpy(memory.data() + position, data, numBytes);
-		} else
-		{
-			// Should never happen - did we calculate a wrong memory size?
-			MPT_ASSERT_NOTREACHED();
-		}
-	}
-	position += numBytes;
+	MPT_ASSERT(mpt::IO::WriteRaw(s, data));
+	position += data.size();
 	totalSize = std::max(totalSize, position);
 }
 
@@ -510,7 +480,7 @@ void WAVWriter::WriteMetatags(const FileTags &tags)
 
 	StartChunk(RIFFChunk::idLIST);
 	const char info[] = { 'I', 'N', 'F', 'O' };
-	WriteArray(info);
+	Write(info);
 
 	WriteTag(RIFFChunk::idINAM, tags.title);
 	WriteTag(RIFFChunk::idIART, tags.artist);
@@ -539,7 +509,7 @@ void WAVWriter::WriteTag(RIFFChunk::ChunkIdentifiers id, const mpt::ustring &ute
 		chunk.id = static_cast<uint32>(id);
 		chunk.length = length;
 		Write(chunk);
-		Write(text.c_str(), length);
+		Write(mpt::byte_cast<mpt::const_byte_span>(mpt::span(text.c_str(), length)));
 
 		if((length % 2u) != 0)
 		{
@@ -630,11 +600,11 @@ void WAVWriter::WriteExtraInformation(const ModSample &sample, MODTYPE modType, 
 
 		char name[MAX_SAMPLENAME];
 		mpt::String::WriteBuf(mpt::String::nullTerminated, name) = sampleName;
-		WriteArray(name);
+		Write(name);
 
 		char filename[MAX_SAMPLEFILENAME];
 		mpt::String::WriteBuf(mpt::String::nullTerminated, filename) = sample.filename;
-		WriteArray(filename);
+		Write(filename);
 	}
 }
 

mptrack/StreamEncoderWAV.cpp
@@ -29,6 +29,7 @@ private:
 
 	const WAVEncoder &enc;
 	std::ostream &f;
+	mpt::IO::OFile<std::ostream> ff;
 	std::unique_ptr<WAVWriter> fileWAV;
 	Encoder::Format formatInfo;
 
@@ -36,6 +37,7 @@ public:
 	WavStreamWriter(const WAVEncoder &enc_, std::ostream &file, const Encoder::Settings &settings, const FileTags &tags)
 		: enc(enc_)
 		, f(file)
+		, ff(f)
 		, fileWAV(nullptr)
 	{
 
@@ -44,7 +46,7 @@ public:
 		ASSERT(formatInfo.Samplerate > 0);
 		ASSERT(formatInfo.Channels > 0);
 
-		fileWAV = std::make_unique<WAVWriter>(&f);
+		fileWAV = std::make_unique<WAVWriter>(ff);
 		fileWAV->WriteFormat(formatInfo.Samplerate, formatInfo.Sampleformat.GetBitsPerSample(), (uint16)formatInfo.Channels, formatInfo.Sampleformat.IsFloat() ? WAVFormatChunk::fmtFloat : WAVFormatChunk::fmtPCM);
 
 		if(settings.Tags)
@@ -74,14 +76,14 @@ public:
 				{
 					frameData[channel] = IEEE754binary32LE(interleaved[channel]);
 				}
-				fileWAV->WriteBuffer(reinterpret_cast<const std::byte*>(frameData.data()), formatInfo.Channels * (formatInfo.Sampleformat.GetBitsPerSample()/8));
+				fileWAV->Write(mpt::span(reinterpret_cast<const std::byte*>(frameData.data()), formatInfo.Channels * (formatInfo.Sampleformat.GetBitsPerSample()/8)));
 				interleaved += formatInfo.Channels;
 			}
 		}
 	}
 	void WriteInterleavedConverted(size_t frameCount, const std::byte *data) override
 	{
-		fileWAV->WriteBuffer(data, frameCount * formatInfo.Channels * (formatInfo.Sampleformat.GetBitsPerSample()/8));
+		fileWAV->Write(mpt::span(data, frameCount * formatInfo.Channels * (formatInfo.Sampleformat.GetBitsPerSample()/8)));
 	}
 	void WriteCues(const std::vector<uint64> &cues) override
 	{

mptrack/View_smp.cpp
@@ -2014,7 +2014,9 @@ void CViewSample::OnEditCopy()
 	Clipboard clipboard(CF_WAVE, memSize);
 	if(auto data = clipboard.Get(); data.data())
 	{
-		WAVWriter file(data);
+		std::pair<mpt::byte_span, mpt::IO::Offset> mf(data, 0);
+		mpt::IO::OFile<std::pair<mpt::byte_span, mpt::IO::Offset>> ff(mf);
+		WAVWriter file(ff);
 
 		// Write sample format
 		file.WriteFormat(sample.GetSampleRate(sndFile.GetType()), sample.GetElementarySampleSize() * 8, sample.GetNumChannels(), WAVFormatChunk::fmtPCM);

soundlib/SampleFormats.cpp
@@ -541,10 +541,8 @@ bool CSoundFile::SaveWAVSample(SAMPLEINDEX nSample, std::ostream &f) const
 	if(sample.uFlags[CHN_ADLIB])
 		return false;
 
-	WAVWriter file(&f);
-
-	if(!file.IsValid())
-		return false;
+	mpt::IO::OFile<std::ostream> ff(f);
+	WAVWriter file(ff);
 
 	file.WriteFormat(sample.GetSampleRate(GetType()), sample.GetElementarySampleSize() * 8, sample.GetNumChannels(), WAVFormatChunk::fmtPCM);
 

soundlib/WAVTools.h
@@ -331,40 +331,33 @@ public:
 class WAVWriter
 {
 protected:
-	// When writing to a stream: Stream pointer
-	std::ostream *s = nullptr;
-	// When writing to memory: Memory address + length
-	mpt::byte_span memory;
+	// Output stream
+	mpt::IO::OFileBase &s;
 
 	// Cursor position
-	size_t position = 0;
+	std::size_t position = 0;
 	// Total number of bytes written to file / memory
-	size_t totalSize = 0;
+	std::size_t totalSize = 0;
 
 	// Currently written chunk
-	size_t chunkStartPos = 0;
+	std::size_t chunkStartPos = 0;
 	RIFFChunk chunkHeader;
 
 public:
-	// Output to stream: Initialize with std::ostream*.
-	WAVWriter(std::ostream *stream);
-	// Output to clipboard: Initialize with pointer to memory and size of reserved memory.
-	WAVWriter(mpt::byte_span data);
+	// Output to stream
+	WAVWriter(mpt::IO::OFileBase &stream);
 
 	~WAVWriter() noexcept(false);
 
-	// Check if anything can be written to the file.
-	bool IsValid() const { return s != nullptr || !memory.empty(); }
-
 	// Finalize the file by closing the last open chunk and updating the file header. Returns total size of file.
-	size_t Finalize();
+	std::size_t Finalize();
 	// Begin writing a new chunk to the file.
 	void StartChunk(RIFFChunk::ChunkIdentifiers id);
 
 	// Skip some bytes... For example after writing sample data.
 	void Skip(size_t numBytes) { Seek(position + numBytes); }
 	// Get position in file (not counting any changes done to the file from outside this class, i.e. through GetFile())
-	size_t GetPosition() const { return position; }
+	std::size_t GetPosition() const { return position; }
 
 	// Shrink file size to current position.
 	void Truncate() { totalSize = position; }
@@ -373,22 +366,11 @@ public:
 	template<typename T>
 	void Write(const T &data)
 	{
-		static_assert((mpt::is_binary_safe<T>::value));
-		Write(&data, sizeof(T));
+		Write(mpt::as_raw_memory(data));
 	}
 
 	// Write a buffer to the file.
-	void WriteBuffer(const std::byte *data, size_t size)
-	{
-		Write(data, size);
-	}
-
-	// Write an array to the file.
-	template<typename T, size_t size>
-	void WriteArray(const T (&data)[size])
-	{
-		Write(data, sizeof(T) * size);
-	}
+	void Write(mpt::const_byte_span data);
 
 	// Write the WAV format to the file.
 	void WriteFormat(uint32 sampleRate, uint16 bitDepth, uint16 numChannels, WAVFormatChunk::SampleFormats encoding);
@@ -403,12 +385,9 @@ public:
 
 protected:
 	// Seek to a position in file.
-	void Seek(size_t pos);
+	void Seek(std::size_t pos);
 	// End current chunk by updating the chunk header and writing a padding byte if necessary.
 	void FinalizeChunk();
-
-	// Write some data to the file.
-	void Write(const void *data, size_t numBytes);
 
 	// Write a single tag into a open idLIST chunk
 	void WriteTag(RIFFChunk::ChunkIdentifiers id, const mpt::ustring &utext);

commit 0d533975ac503ef02b8d96ab2067250244e0f587

[Fix] WAV Writer broke in r13213 in release builds.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13294 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/WAVTools.cpp
@@ -415,7 +415,7 @@ void WAVWriter::Seek(std::size_t pos)
 // Write some data to the file.
 void WAVWriter::Write(mpt::const_byte_span data)
 {
-	MPT_ASSERT(mpt::IO::WriteRaw(s, data));
+	mpt::IO::WriteRaw(s, data);
 	position += data.size();
 	totalSize = std::max(totalSize, position);
 }