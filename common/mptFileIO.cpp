a00640f4f5da9cdfa019adab3ea35f4c7f1fb690
Jörn Heusipp <manx@openmpt.org>
2020-07-18 12:55:07 +0000

    Merged revision(s) 13299 from trunk/OpenMPT: [Fix] mptFileIO: SafeOutputFile: Fix FILE* leak with MSVC and ostream error state in ~SafeOutputFile(). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@13302 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- common/mptFileIO.cpp
+++ common/mptFileIO.cpp
@@ -174,10 +174,22 @@ SafeOutputFile::~SafeOutputFile() noexcept(false)
 	const bool mayThrow = (std::uncaught_exceptions() == 0);
 	if(!stream())
 	{
+		#if MPT_COMPILER_MSVC
+			if(m_f)
+			{
+				fclose(m_f);
+			}
+		#endif // MPT_COMPILER_MSVC
 		return;
 	}
 	if(!stream().rdbuf())
 	{
+		#if MPT_COMPILER_MSVC
+			if(m_f)
+			{
+				fclose(m_f);
+			}
+		#endif // MPT_COMPILER_MSVC
 		return;
 	}
 #if MPT_COMPILER_MSVC
