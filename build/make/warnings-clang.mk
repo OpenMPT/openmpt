OpenMPT/openmpt

commit 6a966f07b6a35c85396e20f26be55c08fd3fe341

[Imp] build: Makefile: Clang: Add -Wmissing-prototypes warning.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13725 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/make/warnings-clang.mk
@@ -1,15 +1,13 @@
 
-CXXFLAGS_WARNINGS += -Wcast-align -Wcast-qual -Wshift-count-negative -Wshift-count-overflow -Wshift-op-parentheses -Wshift-overflow -Wshift-sign-overflow -Wundef
-CFLAGS_WARNINGS   += -Wcast-align -Wcast-qual -Wshift-count-negative -Wshift-count-overflow -Wshift-op-parentheses -Wshift-overflow -Wshift-sign-overflow -Wundef
+CXXFLAGS_WARNINGS += -Wcast-align -Wcast-qual -Wmissing-prototypes -Wshift-count-negative -Wshift-count-overflow -Wshift-op-parentheses -Wshift-overflow -Wshift-sign-overflow -Wundef
+CFLAGS_WARNINGS   += -Wcast-align -Wcast-qual -Wmissing-prototypes -Wshift-count-negative -Wshift-count-overflow -Wshift-op-parentheses -Wshift-overflow -Wshift-sign-overflow -Wundef
 
-CXXFLAGS_WARNINGS += -Wframe-larger-than=16000 -Wmissing-declarations
-CFLAGS_WARNINGS   +=                           -Wmissing-prototypes
+CXXFLAGS_WARNINGS += -Wdeprecated -Wextra-semi -Wframe-larger-than -Wglobal-constructors -Wimplicit-fallthrough -Wmissing-declarations -Wnon-virtual-dtor -Wreserved-id-macro
 
-CXXFLAGS_WARNINGS += -Wdeprecated -Wextra-semi -Wglobal-constructors -Wimplicit-fallthrough -Wnon-virtual-dtor -Wreserved-id-macro
-
+#CXXFLAGS_WARNINGS += -Wfloat-equal
 #CXXFLAGS_WARNINGS += -Wdocumentation
 #CXXFLAGS_WARNINGS += -Wconversion
-#CXXFLAGS_WARNINGS += -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-padded -Wno-shadow -Wno-sign-conversion -Wno-weak-vtables
+#CXXFLAGS_WARNINGS += -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++98-c++11-c++14-compat -Wno-padded -Wno-weak-vtables -Wno-sign-conversion -Wno-shadow-field-in-constructor -Wno-conversion -Wno-switch-enum -Wno-old-style-cast
 
 ifeq ($(MODERN),1)
 LDFLAGS  += -fuse-ld=lld

commit 909131623ebe69ee55cb2cfd31f744742f38bf49

[Fix] build: Fix typo introduced in r13725.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13727 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/make/warnings-clang.mk
@@ -2,7 +2,7 @@
 CXXFLAGS_WARNINGS += -Wcast-align -Wcast-qual -Wmissing-prototypes -Wshift-count-negative -Wshift-count-overflow -Wshift-op-parentheses -Wshift-overflow -Wshift-sign-overflow -Wundef
 CFLAGS_WARNINGS   += -Wcast-align -Wcast-qual -Wmissing-prototypes -Wshift-count-negative -Wshift-count-overflow -Wshift-op-parentheses -Wshift-overflow -Wshift-sign-overflow -Wundef
 
-CXXFLAGS_WARNINGS += -Wdeprecated -Wextra-semi -Wframe-larger-than -Wglobal-constructors -Wimplicit-fallthrough -Wmissing-declarations -Wnon-virtual-dtor -Wreserved-id-macro
+CXXFLAGS_WARNINGS += -Wdeprecated -Wextra-semi -Wframe-larger-than=16000 -Wglobal-constructors -Wimplicit-fallthrough -Wmissing-declarations -Wnon-virtual-dtor -Wreserved-id-macro
 
 #CXXFLAGS_WARNINGS += -Wfloat-equal
 #CXXFLAGS_WARNINGS += -Wdocumentation