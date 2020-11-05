OpenMPT/openmpt

commit f31d5516318a515b6cc9e6b3cbe6f0e71cdd10a1

[Ref] CommandSet: Less verbose command definition. [Ref] CommandSet: Remove UIDs from hidden commands (apart from legacy commands that were previously found in MKB files).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@12468 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/CommandSet.h
@@ -955,13 +955,29 @@ using KeyMapRange = std::pair<KeyMap::const_iterator, KeyMap::const_iterator>;
 
 //KeyMap
 
-struct CommandStruct
+struct KeyCommand
 {
+	static constexpr uint32 Dummy = 1u << 31;
+	static constexpr uint32 Hidden = 1u << 30;
+	static constexpr uint32 UIDMask = Hidden - 1u;
+
 	std::vector<KeyCombination> kcList;
 	CString Message;
-	UINT UID;
-	bool isDummy;
-	bool isHidden;
+
+protected:
+	uint32 UID;
+
+public:
+	void Define(uint32 uid, const TCHAR *message);
+
+	// Unique ID for on-disk keymap format.
+	// Note that hidden commands do not have a unique ID, because they are never written to keymap files.
+	uint32 ID() const noexcept { return UID & UIDMask; }
+	// e.g. Chord modifier is a dummy command, which serves only to automatically
+	// generate a set of key combinations for chords
+	bool IsDummy() const noexcept { return (UID & Dummy) != 0; }
+	// Hidden commands are not configurable by the user (e.g. derived from dummy commands or note entry keys duplicated into other contexts)
+	bool IsHidden() const noexcept { return (UID & Hidden) != 0; }
 };
 
 
@@ -984,36 +1000,21 @@ enum RuleID
 	kNumRules
 };
 
-// For defining key commands
-enum enmKcVisibility
-{
-	kcVisible,
-	kcHidden
-};
-
-enum enmKcDummy
-{
-	kcNoDummy,
-	kcDummy
-};
-
 struct CModSpecifications;
 
 class CCommandSet
 {
 protected:
 	//util
-	inline void DefineKeyCommand(CommandID kc, UINT uid, const TCHAR *message, enmKcVisibility visible = kcVisible, enmKcDummy dummy = kcNoDummy);
 	void SetupCommands();
 	void SetupContextHierarchy();
-	bool IsDummyCommand(CommandID cmd) const;
 	CString EnforceAll(KeyCombination kc, CommandID cmd, bool adding);
 
-	CommandID FindCmd(UINT uid) const;
+	CommandID FindCmd(uint32 uid) const;
 	bool KeyCombinationConflict(KeyCombination kc1, KeyCombination kc2, bool checkEventConflict = true) const;
 
 	const CModSpecifications *m_oldSpecs = nullptr;
-	CommandStruct m_commands[kcNumCommands];
+	KeyCommand m_commands[kcNumCommands];
 	std::bitset<kCtxMaxInputContexts> m_isParentContext[kCtxMaxInputContexts];
 	std::bitset<kNumRules> m_enforceRule;
 
@@ -1034,7 +1035,7 @@ public:
 
 	// Communication
 	KeyCombination GetKey(CommandID cmd, UINT key) const { return m_commands[cmd].kcList[key]; }
-	bool isHidden(UINT c) const { return m_commands[c].isHidden; }
+	bool isHidden(UINT c) const { return m_commands[c].IsHidden(); }
 	int GetKeyListSize(CommandID cmd) const { return (int)m_commands[cmd].kcList.size(); }
 	CString GetCommandText(CommandID cmd) const { return m_commands[cmd].Message; }
 	CString GetKeyTextFromCommand(CommandID c, UINT key) const;

commit 704bf1f3fcd4c15e59a73fa4f55d45b6c72a6d0f

Merged revision(s) 13806-13807 from trunk/OpenMPT: [Fix] Fix possible crash when resetting keyboard mapping while a separator item was focussed. ........ [Fix] Converting legacy note-off / note cut / note fade commands to new ones broke in r12468. As a consequence, note off / cut / fade never worked in new installations since OpenMPT 1.29.01.00 or when resetting keys to defaults. [Mod] Change default keybindings to no longer use legacy keys. ........ [Mod] OpenMPT: Version is now 1.29.05.02
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@13808 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/versionNumber.h
@@ -18,6 +18,6 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR  1
 #define VER_MAJOR      29
 #define VER_MINOR      05
-#define VER_MINORMINOR 01
+#define VER_MINORMINOR 02
 
 OPENMPT_NAMESPACE_END

mptrack/CommandSet.cpp
@@ -1554,7 +1554,7 @@ static std::string GetDefaultKeymap()
 }
 
 
-bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDescription, CCommandSet *commandSet)
+bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDescription, const bool fillExistingSet)
 {
 	KeyCombination kc;
 	char s[1024];
@@ -1563,10 +1563,11 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 	int l = 0;
 	m_oldSpecs = nullptr;  // After clearing the key set, need to fix effect letters
 
-	const bool fillExistingSet = commandSet != nullptr;
-
-	// If commandSet is valid, add new commands to it (this is used for adding the default shortcuts to existing keymaps)
-	CCommandSet *pTempCS = fillExistingSet ? commandSet : new CCommandSet();
+	if(!fillExistingSet)
+	{
+		for(auto &cmd : m_commands)
+			cmd.kcList.clear();
+	}
 
 	CString errText;
 	int errorCount = 0;
@@ -1638,7 +1639,7 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 			// Event
 			kc.EventType(static_cast<KeyEventType>(ConvertStrTo<int>(tokens[4])));
 
-			if(fillExistingSet && cmd != kcNull && pTempCS->GetKeyListSize(cmd) != 0)
+			if(fillExistingSet && cmd != kcNull && GetKeyListSize(cmd) != 0)
 			{
 				// Do not map shortcuts that already have custom keys assigned.
 				// In particular with default note keys, this can create awkward keymaps when loading
@@ -1648,7 +1649,7 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 		}
 
 		// Error checking
-		if (cmd < 0 || cmd >= kcNumCommands || kc.Context() >= kCtxMaxInputContexts || tokens.size() < 4)
+		if(cmd < 0 || cmd >= kcNumCommands || kc.Context() >= kCtxMaxInputContexts || tokens.size() < 4)
 		{
 			errorCount++;
 			if (errorCount < 10)
@@ -1663,7 +1664,7 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 			}
 		} else
 		{
-			pTempCS->Add(kc, cmd, !fillExistingSet, -1, !fillExistingSet);
+			Add(kc, cmd, !fillExistingSet, -1, !fillExistingSet);
 		}
 	}
 
@@ -1671,7 +1672,7 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 	{
 		// Add the default command set to our freshly loaded command set.
 		std::istringstream ss{ GetDefaultKeymap() };
-		LoadFile(ss, mpt::ustring(), pTempCS);
+		LoadFile(ss, mpt::ustring(), true);
 	} else
 	{
 		// We were just adding stuff to an existing command set - don't delete it!
@@ -1686,10 +1687,10 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 		{kcNoteOffOld, kcNoteOff},
 		{kcNoteFadeOld, kcNoteFade},
 	};
-	for(const auto cmd : MergeCommands)
+	for(const auto [from, to] : MergeCommands)
 	{
-		m_commands[cmd.second].kcList.insert(m_commands[cmd.second].kcList.end(), m_commands[cmd.first].kcList.begin(), m_commands[cmd.first].kcList.end());
-		m_commands[cmd.first].kcList.clear();
+		m_commands[to].kcList.insert(m_commands[to].kcList.end(), m_commands[from].kcList.begin(), m_commands[from].kcList.end());
+		m_commands[from].kcList.clear();
 	}
 
 	if(!errText.IsEmpty())
@@ -1698,9 +1699,7 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 			(mpt::ToCString(filenameDescription), errText));
 	}
 
-	Copy(pTempCS);
-	delete pTempCS;
-
+	m_oldSpecs = nullptr;
 	return true;
 }
 
@@ -1721,7 +1720,7 @@ bool CCommandSet::LoadFile(const mpt::PathString &filename)
 
 bool CCommandSet::LoadDefaultKeymap()
 {
-	std::istringstream ss{ GetDefaultKeymap() };
+	std::istringstream ss;
 	return LoadFile(ss, U_("\"executable resource\""));
 }
 

mptrack/CommandSet.h
@@ -1050,7 +1050,7 @@ public:
 	// Communication
 	KeyCombination GetKey(CommandID cmd, UINT key) const { return m_commands[cmd].kcList[key]; }
 	bool isHidden(UINT c) const { return m_commands[c].IsHidden(); }
-	int GetKeyListSize(CommandID cmd) const { return (int)m_commands[cmd].kcList.size(); }
+	int GetKeyListSize(CommandID cmd) const { return (cmd != kcNull) ? static_cast<int>(m_commands[cmd].kcList.size()) : 0; }
 	CString GetCommandText(CommandID cmd) const { return m_commands[cmd].Message; }
 	CString GetKeyTextFromCommand(CommandID c, UINT key) const;
 
@@ -1059,7 +1059,7 @@ public:
 	void GenKeyMap(KeyMap &km);            // Generate a keymap from this command set
 	bool SaveFile(const mpt::PathString &filename);
 	bool LoadFile(const mpt::PathString &filename);
-	bool LoadFile(std::istream &iStrm, const mpt::ustring &filenameDescription, CCommandSet *commandSet = nullptr);
+	bool LoadFile(std::istream &iStrm, const mpt::ustring &filenameDescription, const bool fillExistingSet = false);
 	bool LoadDefaultKeymap();
 };
 

mptrack/KeyConfigDlg.cpp
@@ -537,7 +537,7 @@ void COptionsKeyboard::OnKeyChoiceSelect()
 	CommandID cmd = m_curCommand;
 
 	//If nothing there, clear
-	if(choice >= m_localCmdSet->GetKeyListSize(cmd) || choice < 0)
+	if(cmd == kcNull || choice >= m_localCmdSet->GetKeyListSize(cmd) || choice < 0)
 	{
 		m_curKeyChoice = choice;
 		m_forceUpdate = true;
@@ -586,7 +586,7 @@ void COptionsKeyboard::OnRestoreKeyChoice()
 	CInputHandler *ih = CMainFrame::GetInputHandler();
 
 	// Do nothing if there's nothing to restore
-	if(cmd < 0 || m_curKeyChoice < 0 || m_curKeyChoice >= ih->GetKeyListSize(cmd))
+	if(cmd == kcNull || m_curKeyChoice < 0 || m_curKeyChoice >= ih->GetKeyListSize(cmd))
 	{
 		::MessageBeep(MB_ICONWARNING);
 		return;
@@ -623,7 +623,7 @@ void COptionsKeyboard::OnDeleteKeyChoice()
 void COptionsKeyboard::OnSetKeyChoice()
 {
 	CommandID cmd = m_curCommand;
-	if(cmd < 0)
+	if(cmd == kcNull)
 	{
 		Reporting::Warning("Invalid slot.", "Invalid key data", this);
 		return;

mptrack/res/defaultKeybindings.mkb
@@ -205,9 +205,9 @@ version:1
 3:1221:0:57:1		//Set octave 9: 9 (KeyDown)
 3:1221:0:105:1		//Set octave 9: NUM 9 (KeyDown)
 3:1316:1:16:1		//Chord Modifier: Shift (KeyDown)
-3:1667:0:192:1		//Note Cut: ' (KeyDown)
-3:1668:0:187:1		//Note Off: = (KeyDown)
-3:1792:1:187:1		//Note Fade: Shift+= (KeyDown)
+3:1200:0:192:1		//Note Cut: ' (KeyDown)
+3:1201:0:187:1		//Note Off: = (KeyDown)
+3:1791:1:187:1		//Note Fade: Shift+= (KeyDown)
 3:1788:1:189:1		//Parameter Control: Shift+- (KeyDown)
 3:1789:0:189:1		//Parameter Control (smooth): - (KeyDown)
 