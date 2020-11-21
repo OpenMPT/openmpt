dbb6595299fb3767b14dd711bdbd2ae9b74ab3f6
Jörn Heusipp <manx@openmpt.org>
2020-11-21 13:07:14 +0000

    [Ref] mptCPU: Replace static constexpr constants by inline constexpr constants.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@13881 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- misc/mptCPU.h
+++ misc/mptCPU.h
@@ -23,25 +23,25 @@ namespace CPU
 #ifdef MODPLUG_TRACKER
 
 namespace feature {
-static constexpr uint32 asm_intrinsics = 0x00001; // assembly and intrinsics are enabled at runtime
-static constexpr uint32 cpuid          = 0x00002; // Processor supports modern cpuid
-static constexpr uint32 lm             = 0x00004; // Processor supports long mode (amd64)
-static constexpr uint32 mmx            = 0x00010; // Processor supports MMX instructions
-static constexpr uint32 sse            = 0x00100; // Processor supports SSE instructions
-static constexpr uint32 sse2           = 0x00200; // Processor supports SSE2 instructions
-static constexpr uint32 sse3           = 0x00400; // Processor supports SSE3 instructions
-static constexpr uint32 ssse3          = 0x00800; // Processor supports SSSE3 instructions
-static constexpr uint32 sse4_1         = 0x01000; // Processor supports SSE4.1 instructions
-static constexpr uint32 sse4_2         = 0x02000; // Processor supports SSE4.2 instructions
-static constexpr uint32 avx            = 0x10000; // Processor supports AVX instructions
-static constexpr uint32 avx2           = 0x20000; // Processor supports AVX2 instructions
+inline constexpr uint32 asm_intrinsics = 0x00001; // assembly and intrinsics are enabled at runtime
+inline constexpr uint32 cpuid          = 0x00002; // Processor supports modern cpuid
+inline constexpr uint32 lm             = 0x00004; // Processor supports long mode (amd64)
+inline constexpr uint32 mmx            = 0x00010; // Processor supports MMX instructions
+inline constexpr uint32 sse            = 0x00100; // Processor supports SSE instructions
+inline constexpr uint32 sse2           = 0x00200; // Processor supports SSE2 instructions
+inline constexpr uint32 sse3           = 0x00400; // Processor supports SSE3 instructions
+inline constexpr uint32 ssse3          = 0x00800; // Processor supports SSSE3 instructions
+inline constexpr uint32 sse4_1         = 0x01000; // Processor supports SSE4.1 instructions
+inline constexpr uint32 sse4_2         = 0x02000; // Processor supports SSE4.2 instructions
+inline constexpr uint32 avx            = 0x10000; // Processor supports AVX instructions
+inline constexpr uint32 avx2           = 0x20000; // Processor supports AVX2 instructions
 } // namespace feature
 
 namespace featureset {
-static constexpr uint32 x86_i586 = 0u | feature::cpuid                                             ;
-static constexpr uint32 x86_sse  = 0u | feature::cpuid | feature::sse                              ;
-static constexpr uint32 x86_sse2 = 0u | feature::cpuid | feature::sse | feature::sse2              ;
-static constexpr uint32 amd64    = 0u | feature::cpuid | feature::sse | feature::sse2 | feature::lm;
+inline constexpr uint32 x86_i586 = 0u | feature::cpuid                                             ;
+inline constexpr uint32 x86_sse  = 0u | feature::cpuid | feature::sse                              ;
+inline constexpr uint32 x86_sse2 = 0u | feature::cpuid | feature::sse | feature::sse2              ;
+inline constexpr uint32 amd64    = 0u | feature::cpuid | feature::sse | feature::sse2 | feature::lm;
 } // namespace featureset
 
 #endif
