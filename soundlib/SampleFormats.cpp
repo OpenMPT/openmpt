8f1ba365227c8c24b0ae51d9489d34af0dea02ed
Jörn Heusipp <manx@openmpt.org>
2020-11-29 21:32:39 +0000

    Merged revision(s) 13947 from trunk/OpenMPT: [Fix] Build fix for libopenmpt with NO_PLUGINS. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@13948 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/SampleFormats.cpp
+++ soundlib/SampleFormats.cpp
@@ -20,6 +20,7 @@
 #include "../common/mptFileIO.h"
 #endif // !MODPLUG_NO_FILESAVE
 #include "../common/misc_util.h"
+#include "../common/Endianness.h"
 #include "Tagging.h"
 #include "ITTools.h"
 #include "XMTools.h"
