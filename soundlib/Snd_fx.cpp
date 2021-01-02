a06c172680069b2b62b3a2e84c6b20cab7cd200f
Johannes Schultz <sagamusix@openmpt.org>
2021-01-02 22:30:28 +0000

    [Imp] Update OPL channel state (in particular current patch) when seeking.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14014 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -154,6 +154,9 @@ public:
 			int vol = 0;
 			sndFile.ProcessInstrumentFade(chn, vol);
 
+			if(chn.dwFlags[CHN_ADLIB])
+				continue;
+
 			if(updateInc || chnSettings[channel].incChanged)
 			{
 				if(chn.m_CalculateFreq || chn.m_ReCalculateFreqOnFirstTick)
@@ -565,11 +568,6 @@ std::vector<GetLengthType> CSoundFile::GetLength(enmGetLengthResetMode adjustMod
 					{
 						if(Samples[smp].uFlags[CHN_PANNING])
 							chn.SetInstrumentPan(Samples[smp].nPan, *this);
-						if(Samples[smp].uFlags[CHN_ADLIB])
-						{
-							memory.state->Chn[nChn].Stop();
-							memory.chnSettings[nChn].ticksToRender = 0;
-						}
 					}
 				}
 
@@ -1295,18 +1293,25 @@ std::vector<GetLengthType> CSoundFile::GetLength(enmGetLengthResetMode adjustMod
 			m_PlayState.m_nFrameDelay = m_PlayState.m_nPatternDelay = 0;
 			m_PlayState.m_nTickCount = Util::MaxValueOfType(m_PlayState.m_nTickCount) - 1;
 			m_PlayState.m_bPositionChanged = true;
+			if(m_opl != nullptr)
+				m_opl->Reset();
 			for(CHANNELINDEX n = 0; n < GetNumChannels(); n++)
 			{
-				if(m_PlayState.Chn[n].nLastNote != NOTE_NONE)
+				auto &chn = m_PlayState.Chn[n];
+				if(chn.nLastNote != NOTE_NONE)
 				{
-					m_PlayState.Chn[n].nNewNote = m_PlayState.Chn[n].nLastNote;
+					chn.nNewNote = chn.nLastNote;
 				}
 				if(memory.chnSettings[n].vol != 0xFF && !adjustSamplePos)
 				{
-					m_PlayState.Chn[n].nVolume = std::min(memory.chnSettings[n].vol, uint8(64)) * 4;
+					chn.nVolume = std::min(memory.chnSettings[n].vol, uint8(64)) * 4;
+				}
+				if(chn.pModSample != nullptr && chn.pModSample->uFlags[CHN_ADLIB] && m_opl)
+				{
+					m_opl->Patch(n, chn.pModSample->adlib);
+					m_opl->NoteCut(n);
 				}
 			}
-			if(m_opl != nullptr) m_opl->Reset();
 
 #ifndef NO_PLUGINS
 			// If there were any PC events, update plugin parameters to their latest value.
