3c6ba6ecfacc8c256cadb02b4539bd5c9fd5aa1b
Johannes Schultz <sagamusix@openmpt.org>
2021-01-28 17:06:11 +0000

    Merged revision(s) 14092 from trunk/OpenMPT: [Var] unrar: Update to v6.0.3. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@14093 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     6
 #define RARVER_MINOR     0
-#define RARVER_BETA      2
-#define RARVER_DAY      12
-#define RARVER_MONTH    11
+#define RARVER_BETA      0
+#define RARVER_DAY       1
+#define RARVER_MONTH    12
 #define RARVER_YEAR   2020
