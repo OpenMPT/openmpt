0d2e241f2852d4eba15b9826d3d6de3d1be78c67
Jörn Heusipp <manx@openmpt.org>
2021-02-16 10:58:25 +0000

    [Var] build: Disable run_clang-format.sh for folders which have not yet been formatted.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14162 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/run_clang-format.sh
+++ build/svn/run_clang-format.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-cd libopenmpt
- clang-format-10 -i *.hpp *.cpp *.h
-cd ..
+#cd libopenmpt
+# clang-format-10 -i *.hpp *.cpp *.h
+#cd ..
 
 cd examples
  clang-format-10 -i *.cpp *.c
 cd ..
 
-cd openmpt123
- clang-format-10 -i *.hpp *.cpp
-cd ..
+#cd openmpt123
+# clang-format-10 -i *.hpp *.cpp
+#cd ..
