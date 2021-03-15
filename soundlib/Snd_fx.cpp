b4651ac0684ff83ec6895c5fb7d032fbc02de5aa
Johannes Schultz <sagamusix@openmpt.org>
2021-03-15 20:35:36 +0000

    [Fix] Compile fix.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.27@14328 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -1231,7 +1231,7 @@ std::vector<GetLengthType> CSoundFile::GetLength(enmGetLengthResetMode adjustMod
 				{
 					m_PlayState.Chn[n].nVolume = std::min(memory.chnSettings[n].vol, uint8(64)) * 4;
 				}
-				chn.pCurrentSample = nullptr;
+				m_PlayState.Chn[n].pCurrentSample = nullptr;
 			}
 
 #ifndef NO_PLUGINS
