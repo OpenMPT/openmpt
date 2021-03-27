88f7d19c89b7932b483ed73e5c8c241eb1c6986a
Jörn Heusipp <manx@openmpt.org>
2021-03-27 12:37:49 +0000

    [Ref] soundbase: Make audio_buffer compatible with the current revision of std::audio proposal. [Ref] soundbase: Move audio_buffer::advance() outside of class audio_buffer into its own adapter class audio_buffer_offset.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14449 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundbase/SampleBuffer.h
+++ soundbase/SampleBuffer.h
@@ -31,13 +31,21 @@ private:
 	std::size_t m_offset;
 
 public:
-	constexpr audio_buffer_planar(SampleType *const *buffers, std::size_t channels, std::size_t frames)
+	constexpr audio_buffer_planar(SampleType *const *buffers, std::size_t channels, std::size_t frames) noexcept
 		: m_buffers(buffers)
 		, m_channels(channels)
 		, m_frames(frames)
 		, m_offset(0)
 	{
 		return;
+	}
+	SampleType *data() noexcept
+	{
+		return nullptr;
+	}
+	const SampleType *data() const noexcept
+	{
+		return nullptr;
 	}
 	SampleType &operator()(std::size_t channel, std::size_t frame)
 	{
@@ -47,6 +55,18 @@ public:
 	{
 		return m_buffers[channel][m_offset + frame];
 	}
+	bool is_contiguous() const noexcept
+	{
+		return false;
+	}
+	bool channels_are_contiguous() const noexcept
+	{
+		return false;
+	}
+	bool frames_are_contiguous() const noexcept
+	{
+		return false;
+	}
 	std::size_t size_channels() const noexcept
 	{
 		return m_channels;
@@ -55,13 +75,12 @@ public:
 	{
 		return m_frames;
 	}
-	audio_buffer_planar &advance(std::size_t numFrames)
+	std::size_t size_samples() const noexcept
 	{
-		m_offset += numFrames;
-		m_frames -= numFrames;
-		return *this;
+		return m_channels * m_frames;
 	}
 };
+
 
 template <typename SampleType>
 struct audio_buffer_interleaved
@@ -70,23 +89,23 @@ public:
 	using sample_type = SampleType;
 
 private:
-	SampleType *m_buffer;
+	SampleType * const m_buffer;
 	std::size_t m_channels;
 	std::size_t m_frames;
 
 public:
-	constexpr audio_buffer_interleaved(SampleType *buffer, std::size_t channels, std::size_t frames)
+	constexpr audio_buffer_interleaved(SampleType *buffer, std::size_t channels, std::size_t frames) noexcept
 		: m_buffer(buffer)
 		, m_channels(channels)
 		, m_frames(frames)
 	{
 		return;
 	}
-	SampleType *data()
+	SampleType *data() noexcept
 	{
 		return m_buffer;
 	}
-	const SampleType *data() const
+	const SampleType *data() const noexcept
 	{
 		return m_buffer;
 	}
@@ -98,6 +117,18 @@ public:
 	{
 		return m_buffer[m_channels * frame + channel];
 	}
+	bool is_contiguous() const noexcept
+	{
+		return true;
+	}
+	bool channels_are_contiguous() const noexcept
+	{
+		return false;
+	}
+	bool frames_are_contiguous() const noexcept
+	{
+		return true;
+	}
 	std::size_t size_channels() const noexcept
 	{
 		return m_channels;
@@ -106,33 +137,90 @@ public:
 	{
 		return m_frames;
 	}
-	audio_buffer_interleaved &advance(std::size_t numFrames)
+	std::size_t size_samples() const noexcept
 	{
-		m_buffer += size_channels() * numFrames;
-		m_frames -= numFrames;
-		return *this;
+		return m_channels * m_frames;
 	}
 };
 
+
 template <typename SampleType>
-std::size_t planar_audio_buffer_valid_channels(SampleType *const *buffers, std::size_t maxChannels)
+using audio_buffer = audio_buffer_interleaved<SampleType>;
+
+
+template <typename Taudio_buffer>
+struct audio_buffer_offset
 {
-	std::size_t channel;
-	for(channel = 0; channel < maxChannels; ++channel)
+public:
+	using sample_type = typename Taudio_buffer::sample_type;
+
+private:
+	Taudio_buffer &m_buffer;
+	std::size_t m_offset;
+
+public:
+	constexpr audio_buffer_offset(Taudio_buffer &buffer, std::size_t offset) noexcept
+		: m_buffer(buffer)
+		, m_offset(offset)
 	{
-		if(!buffers[channel])
-		{
-			break;
-		}
+		return;
 	}
-	return channel;
-}
+	sample_type *data() noexcept
+	{
+		if(!is_contiguous())
+		{
+			return nullptr;
+		}
+		return m_buffer.data() + (size_channels() * m_offset);
+	}
+	const sample_type *data() const noexcept
+	{
+		if(!is_contiguous())
+		{
+			return nullptr;
+		}
+		return m_buffer.data() + (size_channels() * m_offset);
+	}
+	sample_type &operator()(std::size_t channel, std::size_t frame)
+	{
+		return m_buffer(channel, m_offset + frame);
+	}
+	const sample_type &operator()(std::size_t channel, std::size_t frame) const
+	{
+		return m_buffer(channel, m_offset + frame);
+	}
+	bool is_contiguous() const noexcept
+	{
+		return m_buffer.is_contiguous() && m_buffer.frames_are_contiguous();
+	}
+	bool channels_are_contiguous() const noexcept
+	{
+		return false;
+	}
+	bool frames_are_contiguous() const noexcept
+	{
+		return m_buffer.frames_are_contiguous();
+	}
+	std::size_t size_channels() const noexcept
+	{
+		return m_buffer.size_channels();
+	}
+	std::size_t size_frames() const noexcept
+	{
+		return m_buffer.size_frames() - m_offset;
+	}
+	std::size_t size_samples() const noexcept
+	{
+		return size_channels() * size_frames();
+	}
+};
+
 
 template <typename BufferType>
-BufferType advance_audio_buffer(BufferType buf, std::size_t numFrames)
+inline audio_buffer_offset<BufferType> advance_audio_buffer(BufferType buf, std::size_t numFrames) noexcept
 {
 	MPT_ASSERT(numFrames <= buf.size_frames());
-	return buf.advance(numFrames);
+	return audio_buffer_offset<BufferType>{buf, numFrames};
 }
 
 
