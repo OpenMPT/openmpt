9aec0c8aed02853abb132c22a0f4dd6ce5002ed7
Jörn Heusipp <manx@openmpt.org>
2021-03-28 12:14:52 +0000

    [Mod] build: Use "-retro-winxp" packaging suffix instead of "retro-winxp" for retro builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14500 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_arguments.cmd
+++ build/auto/setup_arguments.cmd
@@ -116,11 +116,11 @@ if "%MPT_VS_ARCH%" == "x64"   set MPT_DIST_VARIANT_PREFIX=win64
 if "%MPT_VS_ARCH%" == "ARM"   set MPT_DIST_VARIANT_PREFIX=arm32
 if "%MPT_VS_ARCH%" == "ARM64" set MPT_DIST_VARIANT_PREFIX=arm64
 
-if "%MPT_VS_TARGET%" == "winxp"   set MPT_DIST_VARIANT_SUFFIX=retro-winxp
-if "%MPT_VS_TARGET%" == "vista"   set MPT_DIST_VARIANT_SUFFIX=retro-winvista
-if "%MPT_VS_TARGET%" == "win7"    set MPT_DIST_VARIANT_SUFFIX=win7
-if "%MPT_VS_TARGET%" == "win81"   set MPT_DIST_VARIANT_SUFFIX=win81
-if "%MPT_VS_TARGET%" == "win10"   set MPT_DIST_VARIANT_SUFFIX=win10
+if "%MPT_VS_TARGET%" == "winxp"   set MPT_DIST_VARIANT_SUFFIX=-retro-winxp
+if "%MPT_VS_TARGET%" == "vista"   set MPT_DIST_VARIANT_SUFFIX=-retro-winvista
+if "%MPT_VS_TARGET%" == "win7"    set MPT_DIST_VARIANT_SUFFIX=-win7
+if "%MPT_VS_TARGET%" == "win81"   set MPT_DIST_VARIANT_SUFFIX=-win81
+if "%MPT_VS_TARGET%" == "win10"   set MPT_DIST_VARIANT_SUFFIX=-win10
 if "%MPT_VS_TARGET%" == "default" set MPT_DIST_VARIANT_SUFFIX=
 
 set MPT_DIST_VARIANT_TRK=%MPT_DIST_VARIANT_PREFIX%%MPT_DIST_VARIANT_SUFFIX%
