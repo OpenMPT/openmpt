dfef238be1298481259974ddb956f85a6020681d
Jörn Heusipp <manx@openmpt.org>
2021-05-05 20:15:08 +0000

    Merged revision(s) 14833 from trunk/OpenMPT: [Fix] Stream Export: Do not enable dither selection combobox for float64. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@14834 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mod2wave.cpp
+++ mptrack/Mod2wave.cpp
@@ -597,7 +597,7 @@ void CWaveConvert::FillDither()
 	Encoder::Settings &encSettings = m_Settings.GetEncoderSettings();
 	m_CbnDither.CComboBox::ResetContent();
 	int format = m_CbnSampleFormat.GetItemData(m_CbnSampleFormat.GetCurSel()) & 0xffff;
-	if((encTraits->modes & Encoder::ModeEnumerated) && encTraits->formats[format].Sampleformat != SampleFormatInvalid && encTraits->formats[format].Sampleformat != SampleFormatFloat32)
+	if((encTraits->modes & Encoder::ModeEnumerated) && encTraits->formats[format].Sampleformat != SampleFormatInvalid && !encTraits->formats[format].Sampleformat.IsFloat())
 	{
 		m_CbnDither.EnableWindow(TRUE);
 		for(int dither = 0; dither < NumDitherModes; ++dither)
