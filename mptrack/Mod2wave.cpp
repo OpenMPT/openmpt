16d62a64a4d0dd5106522e950d7a5d7a598375f5
Jörn Heusipp <manx@openmpt.org>
2021-05-06 08:03:41 +0000

    [Fix] Stream Export: Fix buffer overflow when exporting quad channels in float64 format.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14837 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/Mod2wave.cpp
+++ mptrack/Mod2wave.cpp
@@ -1001,7 +1001,7 @@ CWaveConvertSettings::CWaveConvertSettings(SettingsContainer &conf, const std::v
 
 void CDoWaveConvert::Run()
 {
-	static std::byte buffer[MIXBUFFERSIZE * 4 * 4]; // channels * sizeof(biggestsample)
+	static std::byte buffer[MIXBUFFERSIZE * 4 * 8]; // channels * sizeof(biggestsample)
 	static MixSampleFloat floatbuffer[MIXBUFFERSIZE * 4]; // channels
 	static MixSampleInt mixbuffer[MIXBUFFERSIZE * 4]; // channels
 
