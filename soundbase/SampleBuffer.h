bceebdb602b39ab84e56d8e13c5b8c51293f1eb4
Jörn Heusipp <manx@openmpt.org>
2021-05-12 17:46:22 +0000

    [Ref] soundbase: Add a general audio_buffer which supports all possible layouts (at a performance cost).
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14908 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundbase/SampleBuffer.h
+++ soundbase/SampleBuffer.h
@@ -204,6 +204,116 @@ public:
 };
 
 
+struct audio_buffer_frames_are_contiguous_t
+{
+};
+struct audio_buffer_channels_are_contiguous_t
+{
+};
+struct audio_buffer_channels_are_planar_t
+{
+};
+struct audio_buffer_channels_are_planar_and_strided_t
+{
+};
+
+inline constexpr audio_buffer_frames_are_contiguous_t audio_buffer_frames_are_contiguous;
+inline constexpr audio_buffer_channels_are_contiguous_t audio_buffer_channels_are_contiguous;
+inline constexpr audio_buffer_channels_are_planar_t audio_buffer_channels_are_planar;
+inline constexpr audio_buffer_channels_are_planar_and_strided_t audio_buffer_channels_are_planar_and_strided;
+
+template <typename SampleType>
+struct audio_buffer
+{
+public:
+	using sample_type = SampleType;
+
+private:
+	union {
+		SampleType *const contiguous;
+		SampleType *const *const planes;
+	} m_buffer;
+	std::size_t m_frame_stride;
+	std::size_t m_channel_stride;
+	std::size_t m_channels;
+	std::size_t m_frames;
+
+public:
+	constexpr audio_buffer(SampleType *buffer, std::size_t channels, std::size_t frames, audio_buffer_frames_are_contiguous_t) noexcept
+		: m_frame_stride(channels)
+		, m_channel_stride(1)
+		, m_channels(channels)
+		, m_frames(frames)
+	{
+		m_buffer.contiguous = buffer;
+	}
+	constexpr audio_buffer(SampleType *buffer, std::size_t channels, std::size_t frames, audio_buffer_channels_are_contiguous_t) noexcept
+		: m_frame_stride(1)
+		, m_channel_stride(frames)
+		, m_channels(channels)
+		, m_frames(frames)
+	{
+		m_buffer.contiguous = buffer;
+	}
+	constexpr audio_buffer(SampleType *const *planes, std::size_t channels, std::size_t frames, audio_buffer_channels_are_planar_t) noexcept
+		: m_frame_stride(1)
+		, m_channel_stride(0)
+		, m_channels(channels)
+		, m_frames(frames)
+	{
+		m_buffer.planes = planes;
+	}
+	constexpr audio_buffer(SampleType *const *planes, std::size_t channels, std::size_t frames, std::size_t frame_stride, audio_buffer_channels_are_planar_and_strided_t) noexcept
+		: m_frame_stride(frame_stride)
+		, m_channel_stride(0)
+		, m_channels(channels)
+		, m_frames(frames)
+	{
+		m_buffer.planes = planes;
+	}
+	bool is_contiguous() const noexcept
+	{
+		return (m_channel_stride != 0);
+	}
+	SampleType *data() noexcept
+	{
+		return is_contiguous() ? m_buffer.contiguous : nullptr;
+	}
+	const SampleType *data() const noexcept
+	{
+		return is_contiguous() ? m_buffer.contiguous : nullptr;
+	}
+	SampleType &operator()(std::size_t channel, std::size_t frame)
+	{
+		return is_contiguous() ? m_buffer.contiguous[(m_channel_stride * channel) + (m_frame_stride * frame)] : m_buffer.planes[channel][frame * m_frame_stride];
+	}
+	const SampleType &operator()(std::size_t channel, std::size_t frame) const
+	{
+		return is_contiguous() ? m_buffer.contiguous[(m_channel_stride * channel) + (m_frame_stride * frame)] : m_buffer.planes[channel][frame * m_frame_stride];
+	}
+	bool channels_are_contiguous() const noexcept
+	{
+		return (m_channel_stride == m_frames);
+	}
+	bool frames_are_contiguous() const noexcept
+	{
+		return (m_frame_stride == m_channels);
+	}
+	std::size_t size_channels() const noexcept
+	{
+		return m_channels;
+	}
+	std::size_t size_frames() const noexcept
+	{
+		return m_frames;
+	}
+	std::size_t size_samples() const noexcept
+	{
+		return m_channels * m_frames;
+	}
+};
+
+
 template <typename Taudio_buffer>
 struct audio_buffer_with_offset
 {
