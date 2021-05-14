3b4e0770da638b282bbe76468e9eedf2b5ff42b9
Johannes Schultz <sagamusix@openmpt.org>
2021-05-14 22:48:54 +0000

    [Mod] Change default channel color more to "random".
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14971 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/TrackerSettings.cpp
+++ mptrack/TrackerSettings.cpp
@@ -190,7 +190,7 @@ TrackerSettings::TrackerSettings(SettingsContainer &conf)
 	, rememberSongWindows(conf, U_("Display"), U_("RememberSongWindows"), true)
 	, showDirsInSampleBrowser(conf, U_("Display"), U_("ShowDirsInSampleBrowser"), false)
 	, commentsFont(conf, U_("Display"), U_("Comments Font"), FontSetting(U_("Courier New"), 120))
-	, defaultRainbowChannelColors(conf, U_("Display"), U_("DefaultChannelColors"), DefaultChannelColors::Rainbow)
+	, defaultRainbowChannelColors(conf, U_("Display"), U_("DefaultChannelColors"), DefaultChannelColors::Random)
 	// Misc
 	, defaultModType(conf, U_("Misc"), U_("DefaultModType"), MOD_TYPE_IT)
 	, defaultNewFileAction(conf, U_("Misc"), U_("DefaultNewFileAction"), nfDefaultFormat)
