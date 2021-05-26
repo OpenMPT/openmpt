0aeb70a94db866cb9f8e56d663840bd647aa0461
Jörn Heusipp <manx@openmpt.org>
2021-05-26 12:11:43 +0000

    [Imp] build: Parallelize regenerate_vs_projects.sh.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15119 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/regenerate_vs_projects.sh
+++ build/regenerate_vs_projects.sh
@@ -28,13 +28,92 @@ fi
 
 
 
+echo dofile \"build/premake/premake.lua\" > premake5.lua
+
+${PREMAKE} --group=libopenmpt_test vs2017 --winxp && \
+${PREMAKE} --group=in_openmpt vs2017 --winxp && \
+${PREMAKE} --group=xmp-openmpt vs2017 --winxp && \
+${PREMAKE} --group=libopenmpt-small vs2017 --winxp && \
+${PREMAKE} --group=libopenmpt vs2017 --winxp && \
+${PREMAKE} --group=openmpt123 vs2017 --winxp && \
+${PREMAKE} --group=PluginBridge vs2017 --winxp && \
+${PREMAKE} --group=OpenMPT vs2017 --winxp && \
+${PREMAKE} --group=all-externals vs2017 --winxp && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2017 --win7 && \
+${PREMAKE} --group=in_openmpt vs2017 --win7 && \
+${PREMAKE} --group=xmp-openmpt vs2017 --win7 && \
+${PREMAKE} --group=libopenmpt-small vs2017 --win7 && \
+${PREMAKE} --group=libopenmpt vs2017 --win7 && \
+${PREMAKE} --group=openmpt123 vs2017 --win7 && \
+${PREMAKE} --group=PluginBridge vs2017 --win7 && \
+${PREMAKE} --group=OpenMPT vs2017 --win7 && \
+${PREMAKE} --group=all-externals vs2017 --win7 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2017 --win10 && \
+${PREMAKE} --group=in_openmpt vs2017 --win10 && \
+${PREMAKE} --group=xmp-openmpt vs2017 --win10 && \
+${PREMAKE} --group=libopenmpt-small vs2017 --win10 && \
+${PREMAKE} --group=libopenmpt vs2017 --win10 && \
+${PREMAKE} --group=openmpt123 vs2017 --win10 && \
+${PREMAKE} --group=PluginBridge vs2017 --win10 && \
+${PREMAKE} --group=OpenMPT vs2017 --win10 && \
+${PREMAKE} --group=all-externals vs2017 --win10 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2019 --win7 && \
+${PREMAKE} --group=in_openmpt vs2019 --win7 && \
+${PREMAKE} --group=xmp-openmpt vs2019 --win7 && \
+${PREMAKE} --group=libopenmpt-small vs2019 --win7 && \
+${PREMAKE} --group=libopenmpt vs2019 --win7 && \
+${PREMAKE} --group=openmpt123 vs2019 --win7 && \
+${PREMAKE} --group=PluginBridge vs2019 --win7 && \
+${PREMAKE} --group=OpenMPT vs2019 --win7 && \
+${PREMAKE} --group=all-externals vs2019 --win7 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2019 --win81 && \
+${PREMAKE} --group=in_openmpt vs2019 --win81 && \
+${PREMAKE} --group=xmp-openmpt vs2019 --win81 && \
+${PREMAKE} --group=libopenmpt-small vs2019 --win81 && \
+${PREMAKE} --group=libopenmpt vs2019 --win81 && \
+${PREMAKE} --group=openmpt123 vs2019 --win81 && \
+${PREMAKE} --group=PluginBridge vs2019 --win81 && \
+${PREMAKE} --group=OpenMPT vs2019 --win81 && \
+${PREMAKE} --group=all-externals vs2019 --win81 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2019 --win10 && \
+${PREMAKE} --group=in_openmpt vs2019 --win10 && \
+${PREMAKE} --group=xmp-openmpt vs2019 --win10 && \
+${PREMAKE} --group=libopenmpt-small vs2019 --win10 && \
+${PREMAKE} --group=libopenmpt vs2019 --win10 && \
+${PREMAKE} --group=openmpt123 vs2019 --win10 && \
+${PREMAKE} --group=PluginBridge vs2019 --win10 && \
+${PREMAKE} --group=OpenMPT vs2019 --win10 && \
+${PREMAKE} --group=all-externals vs2019 --win10 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2019 --clang --win10 && \
+${PREMAKE} --group=in_openmpt vs2019 --clang --win10 && \
+${PREMAKE} --group=xmp-openmpt vs2019 --clang --win10 && \
+${PREMAKE} --group=libopenmpt-small vs2019 --clang --win10 && \
+${PREMAKE} --group=libopenmpt vs2019 --clang --win10 && \
+${PREMAKE} --group=openmpt123 vs2019 --clang --win10 && \
+${PREMAKE} --group=PluginBridge vs2019 --clang --win10 && \
+${PREMAKE} --group=OpenMPT vs2019 --clang --win10 && \
+${PREMAKE} --group=all-externals vs2019 --clang --win10 && \
+echo ok &
+
+
+
 echo dofile \"build/genie/genie.lua\" > genie.lua
 
 ${GENIE} --target="winstore82"   vs2017
 
 ${GENIE} --target="winstore10"   vs2019
-
-rm genie.lua
 
 
 
@@ -43,81 +122,7 @@ echo dofile \"build/xcode-genie/genie.lua\" > genie.lua
 ${GENIE} --target="macosx"   --os=macosx xcode9
 ${GENIE} --target="iphoneos" --os=macosx xcode9
 
-rm genie.lua
 
 
-
-echo dofile \"build/premake/premake.lua\" > premake5.lua
-
-${PREMAKE} --group=libopenmpt_test vs2017 --winxp
-${PREMAKE} --group=in_openmpt vs2017 --winxp
-${PREMAKE} --group=xmp-openmpt vs2017 --winxp
-${PREMAKE} --group=libopenmpt-small vs2017 --winxp
-${PREMAKE} --group=libopenmpt vs2017 --winxp
-${PREMAKE} --group=openmpt123 vs2017 --winxp
-${PREMAKE} --group=PluginBridge vs2017 --winxp
-${PREMAKE} --group=OpenMPT vs2017 --winxp
-${PREMAKE} --group=all-externals vs2017 --winxp
-
-${PREMAKE} --group=libopenmpt_test vs2017 --win7
-${PREMAKE} --group=in_openmpt vs2017 --win7
-${PREMAKE} --group=xmp-openmpt vs2017 --win7
-${PREMAKE} --group=libopenmpt-small vs2017 --win7
-${PREMAKE} --group=libopenmpt vs2017 --win7
-${PREMAKE} --group=openmpt123 vs2017 --win7
-${PREMAKE} --group=PluginBridge vs2017 --win7
-${PREMAKE} --group=OpenMPT vs2017 --win7
-${PREMAKE} --group=all-externals vs2017 --win7
-
-${PREMAKE} --group=libopenmpt_test vs2017 --win10
-${PREMAKE} --group=in_openmpt vs2017 --win10
-${PREMAKE} --group=xmp-openmpt vs2017 --win10
-${PREMAKE} --group=libopenmpt-small vs2017 --win10
-${PREMAKE} --group=libopenmpt vs2017 --win10
-${PREMAKE} --group=openmpt123 vs2017 --win10
-${PREMAKE} --group=PluginBridge vs2017 --win10
-${PREMAKE} --group=OpenMPT vs2017 --win10
-${PREMAKE} --group=all-externals vs2017 --win10
-
-${PREMAKE} --group=libopenmpt_test vs2019 --win7
-${PREMAKE} --group=in_openmpt vs2019 --win7
-${PREMAKE} --group=xmp-openmpt vs2019 --win7
-${PREMAKE} --group=libopenmpt-small vs2019 --win7
-${PREMAKE} --group=libopenmpt vs2019 --win7
-${PREMAKE} --group=openmpt123 vs2019 --win7
-${PREMAKE} --group=PluginBridge vs2019 --win7
-${PREMAKE} --group=OpenMPT vs2019 --win7
-${PREMAKE} --group=all-externals vs2019 --win7
-
-${PREMAKE} --group=libopenmpt_test vs2019 --win81
-${PREMAKE} --group=in_openmpt vs2019 --win81
-${PREMAKE} --group=xmp-openmpt vs2019 --win81
-${PREMAKE} --group=libopenmpt-small vs2019 --win81
-${PREMAKE} --group=libopenmpt vs2019 --win81
-${PREMAKE} --group=openmpt123 vs2019 --win81
-${PREMAKE} --group=PluginBridge vs2019 --win81
-${PREMAKE} --group=OpenMPT vs2019 --win81
-${PREMAKE} --group=all-externals vs2019 --win81
-
-${PREMAKE} --group=libopenmpt_test vs2019 --win10
-${PREMAKE} --group=in_openmpt vs2019 --win10
-${PREMAKE} --group=xmp-openmpt vs2019 --win10
-${PREMAKE} --group=libopenmpt-small vs2019 --win10
-${PREMAKE} --group=libopenmpt vs2019 --win10
-${PREMAKE} --group=openmpt123 vs2019 --win10
-${PREMAKE} --group=PluginBridge vs2019 --win10
-${PREMAKE} --group=OpenMPT vs2019 --win10
-${PREMAKE} --group=all-externals vs2019 --win10
-
-${PREMAKE} --group=libopenmpt_test vs2019 --clang --win10
-${PREMAKE} --group=in_openmpt vs2019 --clang --win10
-${PREMAKE} --group=xmp-openmpt vs2019 --clang --win10
-${PREMAKE} --group=libopenmpt-small vs2019 --clang --win10
-${PREMAKE} --group=libopenmpt vs2019 --clang --win10
-${PREMAKE} --group=openmpt123 vs2019 --clang --win10
-${PREMAKE} --group=PluginBridge vs2019 --clang --win10
-${PREMAKE} --group=OpenMPT vs2019 --clang --win10
-${PREMAKE} --group=all-externals vs2019 --clang --win10
-
-rm premake5.lua
+wait
 
