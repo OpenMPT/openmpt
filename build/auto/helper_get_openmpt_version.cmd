4e4647d925138f93c105ddd69e59be0246e9cd7f
Jörn Heusipp <manx@openmpt.org>
2021-05-28 11:16:10 +0000

    [Fix] build: Fix typo in helper_get_openmpt_version.cmd.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15163 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/helper_get_openmpt_version.cmd
+++ build/auto/helper_get_openmpt_version.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 cd build\auto
-cl /I..\.. /I..\..\src /..\..\common /EHsc helper_get_openmpt_version.cpp
+cl /I..\.. /I..\..\src /I..\..\common /EHsc helper_get_openmpt_version.cpp
 cd ..\..
 
 build\auto\helper_get_openmpt_version.exe openmpt > openmpt_version.txt
