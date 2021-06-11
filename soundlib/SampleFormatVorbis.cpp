OpenMPT/openmpt

commit a60fe9d2d96ea5b3316e6f1a61f53f181f3dd276

[Ref] soundbase: Rename CopyInterleavedSampleStreams, ConvertBufferToBuffer, CopyInterleavedToChannel, and CopyChannelToInterleaved to CopyAudio and friends. [Ref] soundbase: Move knowledge about channel and frame layout out of CopyAudio into a template-deduced audio_span. [Ref] soundbase: As all SampleCoversions except for Decode and Normalize are stateless, do not require a per-channel instantiated SampleConversion argument. Instead, just use automatically deduced sample_cast internally.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15061 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/SampleFormatVorbis.cpp
@@ -20,6 +20,7 @@
 #include "modsmp_ctrl.h"
 #include "../soundbase/SampleFormatConverters.h"
 #include "../soundbase/SampleFormatCopy.h"
+#include "mpt/audio/span.hpp"
 #include "../soundlib/ModSampleCopy.h"
 //#include "mpt/crc/crc.hpp"
 #include "OggStream.h"
@@ -232,10 +233,7 @@ bool CSoundFile::ReadVorbisSample(SAMPLEINDEX sample, FileReader &file)
 						if(decodedSamples > 0 && (channels == 1 || channels == 2))
 						{
 							raw_sample_data.resize(raw_sample_data.size() + (channels * decodedSamples));
-							for(int chn = 0; chn < channels; chn++)
-							{
-								CopyChannelToInterleaved<SC::Convert<int16, float> >(&(raw_sample_data[0]) + offset * channels, output[chn], channels, decodedSamples, chn);
-							}
+							CopyAudio(mpt::audio_span_interleaved(raw_sample_data.data() + offset, channels, decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 							offset += decodedSamples;
 							if((raw_sample_data.size() / channels) > MAX_SAMPLE_LENGTH)
 							{
@@ -305,10 +303,7 @@ bool CSoundFile::ReadVorbisSample(SAMPLEINDEX sample, FileReader &file)
 		if(decodedSamples > 0 && (frame_channels == 1 || frame_channels == 2))
 		{
 			raw_sample_data.resize(raw_sample_data.size() + (channels * decodedSamples));
-			for(int chn = 0; chn < frame_channels; chn++)
-			{
-				CopyChannelToInterleaved<SC::Convert<int16, float> >(&(raw_sample_data[0]) + offset * channels, output[chn], channels, decodedSamples, chn);
-			}
+			CopyAudio(mpt::audio_span_interleaved(raw_sample_data.data() + (offset * channels), channels, decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 			offset += decodedSamples;
 			if((raw_sample_data.size() / channels) > MAX_SAMPLE_LENGTH)
 			{

mptrack/Ctrl_smp.cpp
@@ -37,6 +37,7 @@
 #include "ProgressDialog.h"
 #include "../include/r8brain/CDSPResampler.h"
 #include "../soundlib/MixFuncTable.h"
+#include "mpt/audio/span.hpp"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -2302,10 +2303,6 @@ public:
 		constexpr SmpLength MaxInputChunkSize = 1024;
 
 		std::vector<float> buffer(MaxInputChunkSize * numChannels);
-		std::vector<SC::Convert<float, int16>> convf32(numChannels);
-		std::vector<SC::Convert<int16, float>> convi16(numChannels);
-		std::vector<SC::Convert<float, int8>> conv8f32(numChannels);
-		std::vector<SC::Convert<int8, float>> convint8(numChannels);
 
 		SmpLength inPos = selection.nStart;
 		SmpLength outPos = selection.nStart; // Keeps count of the sample length received from stretching process.
@@ -2338,10 +2335,10 @@ public:
 			switch(smpSize)
 			{
 			case 1:
-				CopyInterleavedSampleStreams(buffer.data(), sample.sample8() + inPos * numChannels, inChunkSize, numChannels, mpt::as_span(conv8f32));
+				CopyAudioChannelsInterleaved(buffer.data(), sample.sample8() + inPos * numChannels, numChannels, inChunkSize);
 				break;
 			case 2:
-				CopyInterleavedSampleStreams(buffer.data(), sample.sample16() + inPos * numChannels, inChunkSize, numChannels, mpt::as_span(convf32));
+				CopyAudioChannelsInterleaved(buffer.data(), sample.sample16() + inPos * numChannels, numChannels, inChunkSize);
 				break;
 			}
 			soundtouch_putSamples(handleSt, buffer.data(), inChunkSize);
@@ -2356,10 +2353,10 @@ public:
 					switch(smpSize)
 					{
 					case 1:
-						CopyInterleavedSampleStreams(static_cast<int8 *>(pNewSample) + numChannels * outPos, buffer.data(), outChunkSize, numChannels, mpt::as_span(convint8));
+						CopyAudioChannelsInterleaved(static_cast<int8 *>(pNewSample) + numChannels * outPos, buffer.data(), numChannels, outChunkSize);
 						break;
 					case 2:
-						CopyInterleavedSampleStreams(static_cast<int16 *>(pNewSample) + numChannels * outPos, buffer.data(), outChunkSize, numChannels, mpt::as_span(convi16));
+						CopyAudioChannelsInterleaved(static_cast<int16 *>(pNewSample) + numChannels * outPos, buffer.data(), numChannels, outChunkSize);
 						break;
 					}
 					outPos += outChunkSize;
@@ -2382,10 +2379,10 @@ public:
 				switch(smpSize)
 				{
 				case 1:
-					CopyInterleavedSampleStreams(static_cast<int8 *>(pNewSample) + numChannels * outPos, buffer.data(), outChunkSize, numChannels, mpt::as_span(convint8));
+					CopyAudioChannelsInterleaved(static_cast<int8 *>(pNewSample) + numChannels * outPos, buffer.data(), numChannels, outChunkSize);
 					break;
 				case 2:
-					CopyInterleavedSampleStreams(static_cast<int16 *>(pNewSample) + numChannels * outPos, buffer.data(), outChunkSize, numChannels, mpt::as_span(convi16));
+					CopyAudioChannelsInterleaved(static_cast<int16 *>(pNewSample) + numChannels * outPos, buffer.data(), numChannels, outChunkSize);
 					break;
 				}
 				outPos += outChunkSize;

soundbase/SampleFormatCopy.h
@@ -16,6 +16,7 @@
 #include "../common/Endianness.h"
 #include "SampleFormatConverters.h"
 #include "SampleFormat.h"
+#include "mpt/audio/span.hpp"
 
 
 OPENMPT_NAMESPACE_BEGIN
@@ -23,6 +24,66 @@ OPENMPT_NAMESPACE_BEGIN
 
 //////////////////////////////////////////////////////
 // Actual sample conversion functions
+
+
+template <typename TBufOut, typename TBufIn>
+void CopyAudio(TBufOut buf_out, TBufIn buf_in)
+{
+	MPT_ASSERT(buf_in.size_frames() == buf_out.size_frames());
+	MPT_ASSERT(buf_in.size_channels() == buf_out.size_channels());
+	std::size_t countFrames = std::min(buf_in.size_frames(), buf_out.size_frames());
+	std::size_t channels = std::min(buf_in.size_channels(), buf_out.size_channels());
+	for(std::size_t frame = 0; frame < countFrames; ++frame)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buf_out(channel, frame) = SC::sample_cast<typename TBufOut::sample_type>(buf_in(channel, frame));
+		}
+	}
+}
+
+
+template <typename TBufOut, typename TBufIn>
+void CopyAudio(TBufOut buf_out, TBufIn buf_in, std::size_t countFrames)
+{
+	MPT_ASSERT(countFrames <= buf_in.size_frames());
+	MPT_ASSERT(countFrames <= buf_out.size_frames());
+	MPT_ASSERT(buf_in.size_channels() == buf_out.size_channels());
+	std::size_t channels = std::min(buf_in.size_channels(), buf_out.size_channels());
+	for(std::size_t frame = 0; frame < countFrames; ++frame)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buf_out(channel, frame) = SC::sample_cast<typename TBufOut::sample_type>(buf_in(channel, frame));
+		}
+	}
+}
+
+
+template <typename TBufOut, typename TBufIn>
+void CopyAudioChannels(TBufOut buf_out, TBufIn buf_in, std::size_t channels, std::size_t countFrames)
+{
+	MPT_ASSERT(countFrames <= buf_in.size_frames());
+	MPT_ASSERT(countFrames <= buf_out.size_frames());
+	MPT_ASSERT(channels <= buf_in.size_channels());
+	MPT_ASSERT(channels <= buf_out.size_channels());
+	for(std::size_t frame = 0; frame < countFrames; ++frame)
+	{
+		for(std::size_t channel = 0; channel < channels; ++channel)
+		{
+			buf_out(channel, frame) = SC::sample_cast<typename TBufOut::sample_type>(buf_in(channel, frame));
+		}
+	}
+}
+
+
+// Copy numChannels interleaved sample streams.
+template <typename Tin, typename Tout>
+void CopyAudioChannelsInterleaved(typename Tout *MPT_RESTRICT outBuf, const Tin *MPT_RESTRICT inBuf, std::size_t numChannels, std::size_t countFrames)
+{
+	CopyAudio(mpt::audio_span_interleaved<Tout>(outBuf, numChannels, countFrames), mpt::audio_span_interleaved<const Tin>(inBuf, numChannels, countFrames));
+}
+
 
 // Copy a sample data buffer.
 // targetBuffer: Buffer in which the sample should be copied into.
@@ -50,31 +111,6 @@ size_t CopySample(typename SampleConversion::output_t *MPT_RESTRICT outBuf, size
 	}
 
 	return copySize;
-}
-
-
-// Copy numChannels interleaved sample streams.
-template <typename SampleConversion>
-void CopyInterleavedSampleStreams(typename SampleConversion::output_t *MPT_RESTRICT outBuf, const typename SampleConversion::input_t *MPT_RESTRICT inBuf, size_t numFrames, size_t numChannels, SampleConversion *conv)
-{
-	while(numFrames--)
-	{
-		for(size_t channel = 0; channel < numChannels; ++channel)
-		{
-			*outBuf = conv[channel](*inBuf);
-			inBuf++;
-			outBuf++;
-		}
-	}
-}
-
-
-// Copy numChannels interleaved sample streams.
-template <typename SampleConversion>
-void CopyInterleavedSampleStreams(typename SampleConversion::output_t *MPT_RESTRICT outBuf, const typename SampleConversion::input_t *MPT_RESTRICT inBuf, size_t numFrames, size_t numChannels, mpt::span<SampleConversion> conv)
-{
-	MPT_ASSERT(conv.size() >= numChannels);
-	CopyInterleavedSampleStreams(outBuf, inBuf, numFrames, numChannels, &(conv[0]));
 }
 
 
@@ -162,56 +198,6 @@ void ConvertBufferToBufferMixInternal(TOutBuf outBuf, TInBuf inBuf, std::size_t 
 		{
 			outBuf(channel, i) = conv(inBuf(channel, i));
 		}
-	}
-}
-
-
-template <typename TOutBuf, typename TInBuf>
-void ConvertBufferToBuffer(TOutBuf outBuf, TInBuf inBuf, std::size_t channels, std::size_t count)
-{
-	using TOutSample = typename std::remove_const<typename TOutBuf::sample_type>::type;
-	using TInSample = typename std::remove_const<typename TInBuf::sample_type>::type;
-	MPT_ASSERT(inBuf.size_channels() >= channels);
-	MPT_ASSERT(outBuf.size_channels() >= channels);
-	MPT_ASSERT(inBuf.size_frames() >= count);
-	MPT_ASSERT(outBuf.size_frames() >= count);
-	SC::Convert<TOutSample, TInSample> conv;
-	for(std::size_t i = 0; i < count; ++i)
-	{
-		for(std::size_t channel = 0; channel < channels; ++channel)
-		{
-			outBuf(channel, i) = conv(inBuf(channel, i));
-		}
-	}
-}
-
-
-// Copy from an interleaed buffer of #channels.
-template <typename SampleConversion>
-void CopyInterleavedToChannel(typename SampleConversion::output_t *MPT_RESTRICT dst, const typename SampleConversion::input_t *MPT_RESTRICT src, std::size_t channels, std::size_t countChunk, std::size_t channel, SampleConversion conv = SampleConversion())
-{
-	SampleConversion sampleConv(conv);
-	src += channel;
-	for(std::size_t i = 0; i < countChunk; ++i)
-	{
-		*dst = sampleConv(*src);
-		src += channels;
-		dst++;
-	}
-}
-
-
-// Copy buffer to an interleaed buffer of #channels.
-template <typename SampleConversion>
-void CopyChannelToInterleaved(typename SampleConversion::output_t *MPT_RESTRICT dst, const typename SampleConversion::input_t *MPT_RESTRICT src, std::size_t channels, std::size_t countChunk, std::size_t channel, SampleConversion conv = SampleConversion())
-{
-	SampleConversion sampleConv(conv);
-	dst += channel;
-	for(std::size_t i = 0; i < countChunk; ++i)
-	{
-		*dst = sampleConv(*src);
-		src++;
-		dst += channels;
 	}
 }
 

soundlib/Load_mo3.cpp
@@ -18,7 +18,7 @@
 
 #include "Tables.h"
 #include "../common/version.h"
-
+#include "mpt/audio/span.hpp"
 #include "MPEGFrame.h"
 #include "OggStream.h"
 
@@ -1659,15 +1659,12 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 								LimitMax(decodedSamples, mpt::saturate_cast<long>(sample.nLength - offset));
 								if(decodedSamples > 0 && channels == sample.GetNumChannels())
 								{
-									for(int chn = 0; chn < channels; chn++)
+									if(sample.uFlags[CHN_16BIT])
 									{
-										if(sample.uFlags[CHN_16BIT])
-										{
-											CopyChannelToInterleaved<SC::Convert<int16, float>>(sample.sample16() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
-										} else
-										{
-											CopyChannelToInterleaved<SC::Convert<int8, float>>(sample.sample8() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
-										}
+										CopyAudio(mpt::audio_span_interleaved(sample.sample16() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
+									} else
+									{
+										CopyAudio(mpt::audio_span_interleaved(sample.sample8() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 									}
 								}
 								offset += decodedSamples;
@@ -1734,12 +1731,12 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 					LimitMax(decodedSamples, mpt::saturate_cast<int>(sample.nLength - offset));
 					if(decodedSamples > 0 && channels == sample.GetNumChannels())
 					{
-						for(int chn = 0; chn < channels; chn++)
+						if(sample.uFlags[CHN_16BIT])
 						{
-							if(sample.uFlags[CHN_16BIT])
-								CopyChannelToInterleaved<SC::Convert<int16, float>>(sample.sample16() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
-							else
-								CopyChannelToInterleaved<SC::Convert<int8, float>>(sample.sample8() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
+							CopyAudio(mpt::audio_span_interleaved(sample.sample16() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
+						} else
+						{
+							CopyAudio(mpt::audio_span_interleaved(sample.sample8() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 						}
 					}
 					offset += decodedSamples;

soundlib/Load_xm.cpp
@@ -22,6 +22,7 @@
 #ifdef MODPLUG_TRACKER
 #include "../mptrack/TrackerSettings.h"	// For super smooth ramping option
 #endif // MODPLUG_TRACKER
+#include "mpt/audio/span.hpp"
 
 #if defined(MPT_WITH_VORBIS) && defined(MPT_WITH_VORBISFILE)
 #include <sstream>
@@ -480,15 +481,12 @@ static bool ReadSampleData(ModSample &sample, SampleIO sampleFlags, FileReader &
 							LimitMax(decodedSamples, mpt::saturate_cast<long>(sample.nLength - offset));
 							if(decodedSamples > 0 && channels == sample.GetNumChannels())
 							{
-								for(int chn = 0; chn < channels; chn++)
+								if(sample.uFlags[CHN_16BIT])
 								{
-									if(sample.uFlags[CHN_16BIT])
-									{
-										CopyChannelToInterleaved<SC::Convert<int16, float> >(sample.sample16() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
-									} else
-									{
-										CopyChannelToInterleaved<SC::Convert<int8, float> >(sample.sample8() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
-									}
+									CopyAudio(mpt::audio_span_interleaved(sample.sample16() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
+								} else
+								{
+									CopyAudio(mpt::audio_span_interleaved(sample.sample8() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 								}
 							}
 							offset += decodedSamples;
@@ -544,12 +542,12 @@ static bool ReadSampleData(ModSample &sample, SampleIO sampleFlags, FileReader &
 				LimitMax(decodedSamples, mpt::saturate_cast<int>(sample.nLength - offset));
 				if(decodedSamples > 0 && channels == sample.GetNumChannels())
 				{
-					for(int chn = 0; chn < channels; chn++)
+					if(sample.uFlags[CHN_16BIT])
 					{
-						if(sample.uFlags[CHN_16BIT])
-							CopyChannelToInterleaved<SC::Convert<int16, float> >(sample.sample16() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
-						else
-							CopyChannelToInterleaved<SC::Convert<int8, float> >(sample.sample8() + offset * sample.GetNumChannels(), output[chn], channels, decodedSamples, chn);
+						CopyAudio(mpt::audio_span_interleaved(sample.sample16() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
+					} else
+					{
+						CopyAudio(mpt::audio_span_interleaved(sample.sample8() + (offset * sample.GetNumChannels()), sample.GetNumChannels(), decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 					}
 				}
 				offset += decodedSamples;

commit 210a8e8177e55e8414aade47e744eb8049678161

[Fix] Vorbis Samples: Fix loading stereo streams via libvorbisfile. Broken since r15061. Fixes <https://bugs.openmpt.org/view.php?id=1470>.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15351 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/SampleFormatVorbis.cpp
@@ -232,7 +232,7 @@ bool CSoundFile::ReadVorbisSample(SAMPLEINDEX sample, FileReader &file)
 						if(decodedSamples > 0 && (channels == 1 || channels == 2))
 						{
 							raw_sample_data.resize(raw_sample_data.size() + (channels * decodedSamples));
-							CopyAudio(mpt::audio_span_interleaved(raw_sample_data.data() + offset, channels, decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
+							CopyAudio(mpt::audio_span_interleaved(raw_sample_data.data() + (offset * channels), channels, decodedSamples), mpt::audio_span_planar(output, channels, decodedSamples));
 							offset += decodedSamples;
 							if((raw_sample_data.size() / channels) > MAX_SAMPLE_LENGTH)
 							{