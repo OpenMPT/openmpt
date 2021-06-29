2c9c484f4ab33455af9c085e251a64e23e31bc07
Jörn Heusipp <manx@openmpt.org>
2021-06-29 06:49:33 +0000

    [Fix] build: Autotools: Do not use removed option --enable-libmodplug when testing.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15387 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/autotools/test_tarball.sh
+++ build/autotools/test_tarball.sh
@@ -25,9 +25,9 @@ else
 	gunzip -c ../libopenmpt*.tar.gz | tar -xvf -
 	cd libopenmpt*
 	if [ "x$2" = "x" ] ; then
-		./configure --host=$1 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --enable-libmodplug --disable-openmpt123 --disable-examples --disable-tests
+		./configure --host=$1 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --disable-openmpt123 --disable-examples --disable-tests
 	else
-		./configure --host=$1 CC=$1-gcc-$2 CXX=$1-g++-$2 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --enable-libmodplug --disable-openmpt123 --disable-examples --disable-tests
+		./configure --host=$1 CC=$1-gcc-$2 CXX=$1-g++-$2 --without-zlib --without-ogg --without-vorbis --without-vorbisfile --without-mpg123 --without-pulseaudio --without-portaudio -without-sndfile --without-flac --without-portaudiocpp --disable-openmpt123 --disable-examples --disable-tests
 	fi
 	make
 	cd ..
