ee7d81f393854131a4a8d492f5fa14348671e788
Jörn Heusipp <manx@openmpt.org>
2021-07-20 12:16:23 +0000

    [Ref] mpt/mutex: Due to previous Windows XP compatibility and runtime ABI stability, VS2019 still uses CRITICAL_SECTION to implement std::mutex instead of the more space- and time-efficient SRWLock. std::shared_mutex however (due to being newer), always uses the more efficient variant. Switch our mpt::mutex to std::shared_mutex on MSVC platforms when targeting Vista or newer. mpt::recursive_mutex has no more-efficient implementation because SRWLocks are not recursive.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15458 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/mpt/mutex/mutex.hpp
+++ src/mpt/mutex/mutex.hpp
@@ -36,6 +36,10 @@
 #include <mingw.mutex.h>
 #else
 #include <mutex>
+#ifdef MPT_COMPILER_QUIRK_COMPLEX_STD_MUTEX
+#include <shared_mutex>
+#include <type_traits>
+#endif
 #endif
 #elif MPT_MUTEX_WIN32
 #include <windows.h>
@@ -50,10 +54,37 @@ inline namespace MPT_INLINE_NS {
 
 #if MPT_MUTEX_STD
 
+#ifdef MPT_COMPILER_QUIRK_COMPLEX_STD_MUTEX
+using mutex = std::conditional<sizeof(std::shared_mutex) < sizeof(std::mutex), std::shared_mutex, std::mutex>::type;
+#else
 using mutex = std::mutex;
+#endif
 using recursive_mutex = std::recursive_mutex;
 
 #elif MPT_MUTEX_WIN32
+
+#if (_WIN32_WINNT >= 0x0600) // _WIN32_WINNT_VISTA
+
+// compatible with c++11 std::mutex, can eventually be replaced without touching any usage site
+class mutex {
+private:
+	SRWLOCK impl = SRWLOCK_INIT;
+
+public:
+	mutex() = default;
+	~mutex() = default;
+	void lock() {
+		AcquireSRWLockExclusive(&impl);
+	}
+	bool try_lock() {
+		return TryAcquireSRWLockExclusive(&impl) ? true : false;
+	}
+	void unlock() {
+		ReleaseSRWLockExclusive(&impl);
+	}
+};
+
+#else // !_WIN32_WINNT_VISTA
 
 // compatible with c++11 std::mutex, can eventually be replaced without touching any usage site
 class mutex {
@@ -77,6 +108,8 @@ public:
 		LeaveCriticalSection(&impl);
 	}
 };
+
+#endif // _WIN32_WINNT_VISTA
 
 // compatible with c++11 std::recursive_mutex, can eventually be replaced without touching any usage site
 class recursive_mutex {
