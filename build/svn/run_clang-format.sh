1f68a34c7e7788ce7386ff647572558f3bf8ab20
Jörn Heusipp <manx@openmpt.org>
2021-08-11 13:29:17 +0000

    [Var] build: Use clang-format 12.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15524 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/run_clang-format.sh
+++ build/svn/run_clang-format.sh
@@ -2,22 +2,22 @@
 set -e
 
 #cd libopenmpt
-#	clang-format-11 -i *.hpp *.cpp *.h
+#	clang-format-12 -i *.hpp *.cpp *.h
 #cd ..
 
 cd examples
-	clang-format-11 -i *.cpp *.c
+	clang-format-12 -i *.cpp *.c
 cd ..
 
 #cd openmpt123
-#	clang-format-11 -i *.hpp *.cpp
+#	clang-format-12 -i *.hpp *.cpp
 #cd ..
 
 cd src/mpt
-	find . -type f -iname '*\.hpp' | xargs clang-format-11 -i
+	find . -type f -iname '*\.hpp' | xargs clang-format-12 -i
 cd ../..
 
 cd src/openmpt
-	find . -type f -iname '*\.cpp' | xargs --no-run-if-empty clang-format-11 -i
-	find . -type f -iname '*\.hpp' | xargs --no-run-if-empty clang-format-11 -i
+	find . -type f -iname '*\.cpp' | xargs --no-run-if-empty clang-format-12 -i
+	find . -type f -iname '*\.hpp' | xargs --no-run-if-empty clang-format-12 -i
 cd ../..
