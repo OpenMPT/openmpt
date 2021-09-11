2def2db0a1b25b7d63e0e429562b1b50e1779a34
Jörn Heusipp <manx@openmpt.org>
2021-09-11 08:40:00 +0000

    [Fix] IT: Use mpt::clamp.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@15656 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_it.cpp
+++ soundlib/Load_it.cpp
@@ -2094,7 +2094,7 @@ void CSoundFile::ReadMixPluginChunk(FileReader &file, SNDMIXPLUGIN &plugin)
 
 			if(!memcmp(code, "DWRT", 4))
 			{
-				plugin.fDryRatio = std::clamp(dataChunk.ReadFloatLE(), 0.0f, 1.0f);
+				plugin.fDryRatio = mpt::clamp(dataChunk.ReadFloatLE(), 0.0f, 1.0f);
 				if(!std::isnormal(plugin.fDryRatio))
 					plugin.fDryRatio = 0.0f;
 			} else if(!memcmp(code, "PROG", 4))
