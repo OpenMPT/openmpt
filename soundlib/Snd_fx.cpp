31d940abafa4061090c0d410d805ac7869393c16
Johannes Schultz <sagamusix@openmpt.org>
2021-10-03 11:56:57 +0000

    Merged revision(s) 15740 from trunk/OpenMPT: [Fix] Tremor: Store effect memory properly for command I11 in IT-compatible mode. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@15741 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -3345,8 +3345,11 @@ bool CSoundFile::ProcessEffects()
 				if(param && !m_SongFlags[SONG_ITOLDEFFECTS])
 				{
 					// Old effects have different length interpretation (+1 for both on and off)
-					if(param & 0xF0) param -= 0x10;
-					if(param & 0x0F) param -= 0x01;
+					if(param & 0xF0)
+						param -= 0x10;
+					if(param & 0x0F)
+						param -= 0x01;
+					chn.nTremorParam = static_cast<ModCommand::PARAM>(param);
 				}
 				chn.nTremorCount |= 0x80; // set on/off flag
 			} else if(m_playBehaviour[kFT2Tremor])
@@ -3356,7 +3359,8 @@ bool CSoundFile::ProcessEffects()
 			}
 
 			chn.nCommand = CMD_TREMOR;
-			if (param) chn.nTremorParam = static_cast<ModCommand::PARAM>(param);
+			if(param)
+				chn.nTremorParam = static_cast<ModCommand::PARAM>(param);
 
 			break;
 
