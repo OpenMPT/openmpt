5355deb1de8d1adf0d910519fc60a9519d98756f
Jörn Heusipp <manx@openmpt.org>
2021-11-06 17:40:51 +0000

    [Ref] build: Add VS2022 setup script.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15889 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_vs_any.cmd
+++ build/auto/setup_vs_any.cmd
@@ -1,6 +1,15 @@
 @echo off
 
 
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call %~dp0\setup_vs2022.cmd
+ goto vsdone
+)
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call %~dp0\setup_vs2022.cmd
+ goto vsdone
+)
+
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call %~dp0\setup_vs2019.cmd
  goto vsdone
