a6b8791cf784e008ec1473469c768d13df5e323c
Jörn Heusipp <manx@openmpt.org>
2021-11-06 16:29:41 +0000

    [Var] build: Add VS2022 project files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15885 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/regenerate_vs_projects.sh
+++ build/regenerate_vs_projects.sh
@@ -107,6 +107,50 @@ ${PREMAKE} --group=OpenMPT vs2019 --clang --win10 && \
 ${PREMAKE} --group=all-externals vs2019 --clang --win10 && \
 echo ok &
 
+${PREMAKE} --group=libopenmpt_test vs2022 --win7 && \
+${PREMAKE} --group=in_openmpt vs2022 --win7 && \
+${PREMAKE} --group=xmp-openmpt vs2022 --win7 && \
+${PREMAKE} --group=libopenmpt-small vs2022 --win7 && \
+${PREMAKE} --group=libopenmpt vs2022 --win7 && \
+${PREMAKE} --group=openmpt123 vs2022 --win7 && \
+${PREMAKE} --group=PluginBridge vs2022 --win7 && \
+${PREMAKE} --group=OpenMPT vs2022 --win7 && \
+${PREMAKE} --group=all-externals vs2022 --win7 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2022 --win81 && \
+${PREMAKE} --group=in_openmpt vs2022 --win81 && \
+${PREMAKE} --group=xmp-openmpt vs2022 --win81 && \
+${PREMAKE} --group=libopenmpt-small vs2022 --win81 && \
+${PREMAKE} --group=libopenmpt vs2022 --win81 && \
+${PREMAKE} --group=openmpt123 vs2022 --win81 && \
+${PREMAKE} --group=PluginBridge vs2022 --win81 && \
+${PREMAKE} --group=OpenMPT vs2022 --win81 && \
+${PREMAKE} --group=all-externals vs2022 --win81 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2022 --win10 && \
+${PREMAKE} --group=in_openmpt vs2022 --win10 && \
+${PREMAKE} --group=xmp-openmpt vs2022 --win10 && \
+${PREMAKE} --group=libopenmpt-small vs2022 --win10 && \
+${PREMAKE} --group=libopenmpt vs2022 --win10 && \
+${PREMAKE} --group=openmpt123 vs2022 --win10 && \
+${PREMAKE} --group=PluginBridge vs2022 --win10 && \
+${PREMAKE} --group=OpenMPT vs2022 --win10 && \
+${PREMAKE} --group=all-externals vs2022 --win10 && \
+echo ok &
+
+${PREMAKE} --group=libopenmpt_test vs2022 --clang --win10 && \
+${PREMAKE} --group=in_openmpt vs2022 --clang --win10 && \
+${PREMAKE} --group=xmp-openmpt vs2022 --clang --win10 && \
+${PREMAKE} --group=libopenmpt-small vs2022 --clang --win10 && \
+${PREMAKE} --group=libopenmpt vs2022 --clang --win10 && \
+${PREMAKE} --group=openmpt123 vs2022 --clang --win10 && \
+${PREMAKE} --group=PluginBridge vs2022 --clang --win10 && \
+${PREMAKE} --group=OpenMPT vs2022 --clang --win10 && \
+${PREMAKE} --group=all-externals vs2022 --clang --win10 && \
+echo ok &
+
 
 
 echo dofile \"build/genie/genie.lua\" > genie.lua
