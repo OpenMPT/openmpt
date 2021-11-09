77b31a52ca3b35d140c25f3f3c29344661a42357
Jörn Heusipp <manx@openmpt.org>
2021-11-09 08:41:26 +0000

    [Fix] premake: Set VS2022 solution version number to 17. [Fix] build: Fix install path of VS2022. [Var] build: Regenerate project files. [Var] Set some more svn:ignore for VS2022.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15893 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_vs2022.cmd
+++ build/auto/setup_vs2022.cmd
@@ -1,16 +1,11 @@
 @echo off
 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" goto win64
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" goto win32
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" goto win64
 
-goto setupdone
-
-:win32
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 goto setupdone
 
 :win64
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 goto setupdone
 
 :setupdone
