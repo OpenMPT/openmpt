ce044a862d1f0d75b62b34c59b831e59c2d99f38
Jörn Heusipp <manx@openmpt.org>
2021-11-09 08:44:29 +0000

    [Fix] build: Fix install path of VS2022.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15894 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/setup_vs_any.cmd
+++ build/auto/setup_vs_any.cmd
@@ -1,10 +1,5 @@
 @echo off
 
-
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
- call %~dp0\setup_vs2022.cmd
- goto vsdone
-)
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call %~dp0\setup_vs2022.cmd
  goto vsdone
