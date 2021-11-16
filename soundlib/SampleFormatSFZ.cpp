e58de301f4d69a70cf7d5125deaa59dbc8e4d0ce
Johannes Schultz <sagamusix@openmpt.org>
2021-11-16 19:19:14 +0000

    [Mod] Simplify sample paths after turning them into absolute paths, not before. This fixes some MT2 files with relative paths starting with "..\Samples".
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@15983 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/SampleFormatSFZ.cpp
+++ soundlib/SampleFormatSFZ.cpp
@@ -258,7 +258,7 @@ struct SFZEnvelope
 		if(hold > 0)
 		{
 			if(env.empty())
-				env.push_back({0, 100.0});
+				env.push_back({0.0, 100.0});
 			env.push_back({hold, env.back().second});
 		}
 		if(env.empty())
@@ -816,6 +816,7 @@ bool CSoundFile::ReadSFZInstrument(INSTRUMENTINDEX nInstr, FileReader &file)
 			{
 				filename = file.GetOptionalFileName().value_or(P_("")).GetPath() + filename;
 			}
+			filename = filename.Simplify();
 			SetSamplePath(smp, filename);
 			InputFile f(filename, SettingCacheCompleteFileBeforeLoading());
 			FileReader smpFile = GetFileReader(f);
