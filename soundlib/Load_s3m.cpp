a738004aa1f6ec58b8289102b3ddd65cd2d05e88
Johannes Schultz <sagamusix@openmpt.org>
2021-12-04 18:50:55 +0000

    Merged revision(s) 16013, 16024, 16026 from trunk/OpenMPT: [Fix] STM: Use S3M-like sample swap behaviour. ........ [Mod] MTM: Another approach for r16020 that also works if inserting patterns fails for whatever reason. ........ [Mod] S3M: Ignore O00 commends in files created with ST3.00/ST3.01. This command only got effect memory since ST3.03. Noticable e.g. in the dot landscape part of Future Crew's Unreal demo soundtrack. Other tracks where the no-memory behaviour can be identifed as being most likely intended are Necros' "Disco Feva Baby" and Humanoid's "Das Kalumet". ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@16101 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_s3m.cpp
+++ soundlib/Load_s3m.cpp
@@ -598,6 +598,10 @@ bool CSoundFile::ReadS3M(FileReader &file, ModLoadingFlags loadFlags)
 						else if(m.param > 0x08)
 							zxxCountRight++;
 					}
+				} else if(m.command == CMD_OFFSET && m.param == 0 && fileHeader.cwtv <= S3MFileHeader::trkST3_01)
+				{
+					// Offset command didn't have effect memory in ST3.01; fixed in ST3.03
+					m.command = CMD_DUMMY;
 				}
 			}
 		}
