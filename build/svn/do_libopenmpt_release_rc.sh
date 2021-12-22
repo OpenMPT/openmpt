d99f5bf0464a8720d3b733c6b88eafc6e74e561b
Jörn Heusipp <manx@openmpt.org>
2021-12-22 14:39:53 +0000

    Merged revision(s) 16221 from trunk/OpenMPT: [Fix] build: Fix filenames assumed by libopenmpt rc release script. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@16222 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/do_libopenmpt_release_rc.sh
+++ build/svn/do_libopenmpt_release_rc.sh
@@ -7,6 +7,7 @@ svn up
 NEWVER=$(make distversion-pure)
 NEWREV=$(svn info --xml . | xpath -e '/info/entry/commit/@revision' -q | sed 's/revision//g' | tr '"' ' ' | tr '=' ' ' | sed 's/ //g')
 svn cp -m "tag libopenmpt-${NEWVER}" -r ${NEWREV} https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30 https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER}
+TAGREV=$(svn info --xml https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER} | xpath -e '/info/entry/commit/@revision' -q | sed 's/revision//g' | tr '"' ' ' | tr '=' ' ' | sed 's/ //g')
 
 echo "ALL DONE."
-echo "run './release-0.6.sh $NEWVER +release' in a website checkout after buildbot has finished."
+echo "run './release-0.6.sh $NEWVER +r${TAGREV}' in a website checkout after buildbot has finished."
