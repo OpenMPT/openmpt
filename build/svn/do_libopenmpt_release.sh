67551471eaa9f2ab8f811f65141105e1f243edd1
Jörn Heusipp <manx@openmpt.org>
2021-12-22 10:47:03 +0000

    [Fix] Revert r16203. Was merged into wrong branch. Merged revision(s) 16203 from branches/OpenMPT-1.29: Merged revision(s) 16201 from trunk/OpenMPT: [Fix] build: Update version numbers in libopenmpt release script. ........ ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@16206 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/do_libopenmpt_release.sh
+++ build/svn/do_libopenmpt_release.sh
@@ -16,7 +16,7 @@ svn ci -m "[Mod] libopenmpt: Prepare for release."
 svn up
 NEWVER=$(make distversion-pure)
 NEWREV=$(svn info --xml . | xpath -e '/info/entry/commit/@revision' -q | sed 's/revision//g' | tr '"' ' ' | tr '=' ' ' | sed 's/ //g')
-svn cp -m "tag libopenmpt-${NEWVER}" -r ${NEWREV} https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30 https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER}
+svn cp -m "tag libopenmpt-${NEWVER}" -r ${NEWREV} https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29 https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER}
 build/update_libopenmpt_version.sh bumppatch
 build/update_libopenmpt_version.sh bumpltrev
 svn ci -m "[Mod] libopenmpt: Bump patch version."
@@ -29,5 +29,5 @@ svn ci -m "[Mod] libopenmpt: Bump patch version."
 #svn ci -m "[Mod] OpenMPT: Version is now ${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}.${VER_BUILD}"
 
 echo "ALL DONE."
-echo "run './release-0.6.sh $NEWVER +release' in a website checkout after buildbot has finished."
+echo "run './release-0.5.sh $NEWVER +release' in a website checkout after buildbot has finished."
 
