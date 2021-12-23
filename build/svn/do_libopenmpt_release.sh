273ee9edbf607e974b872980300b45b6ddf08d2d
Jörn Heusipp <manx@openmpt.org>
2021-12-23 13:54:44 +0000

    [Mod] libopenmpt: Update release process due to OpenMPT stable shifting to 1.30 branch.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@16287 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/do_libopenmpt_release.sh
+++ build/svn/do_libopenmpt_release.sh
@@ -4,12 +4,12 @@ set -e
 
 svn up
 
-#build/update_openmpt_version.sh bumpbuild
-#VER_MAJOR=$(cat common/versionNumber.h | grep "VER_MAJORMAJOR " | awk '{print $3;}')
-#VER_MINOR=$(cat common/versionNumber.h | grep "VER_MAJOR " | awk '{print $3;}')
-#VER_PATCH=$(cat common/versionNumber.h | grep "VER_MINOR " | awk '{print $3;}')
-#VER_BUILD=$(cat common/versionNumber.h | grep "VER_MINORMINOR " | awk '{print $3;}')
-#svn ci -m "[Mod] OpenMPT: Version is now ${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}.${VER_BUILD}"
+build/update_openmpt_version.sh bumpbuild
+VER_MAJOR=$(cat common/versionNumber.h | grep "VER_MAJORMAJOR " | awk '{print $3;}')
+VER_MINOR=$(cat common/versionNumber.h | grep "VER_MAJOR " | awk '{print $3;}')
+VER_PATCH=$(cat common/versionNumber.h | grep "VER_MINOR " | awk '{print $3;}')
+VER_BUILD=$(cat common/versionNumber.h | grep "VER_MINORMINOR " | awk '{print $3;}')
+svn ci -m "[Mod] OpenMPT: Version is now ${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}.${VER_BUILD}"
 
 build/update_libopenmpt_version.sh release
 svn ci -m "[Mod] libopenmpt: Prepare for release."
@@ -21,12 +21,12 @@ build/update_libopenmpt_version.sh bumppatch
 build/update_libopenmpt_version.sh bumpltrev
 svn ci -m "[Mod] libopenmpt: Bump patch version."
 
-#build/update_openmpt_version.sh bumpbuild
-#VER_MAJOR=$(cat common/versionNumber.h | grep "VER_MAJORMAJOR " | awk '{print $3;}')
-#VER_MINOR=$(cat common/versionNumber.h | grep "VER_MAJOR " | awk '{print $3;}')
-#VER_PATCH=$(cat common/versionNumber.h | grep "VER_MINOR " | awk '{print $3;}')
-#VER_BUILD=$(cat common/versionNumber.h | grep "VER_MINORMINOR " | awk '{print $3;}')
-#svn ci -m "[Mod] OpenMPT: Version is now ${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}.${VER_BUILD}"
+build/update_openmpt_version.sh bumpbuild
+VER_MAJOR=$(cat common/versionNumber.h | grep "VER_MAJORMAJOR " | awk '{print $3;}')
+VER_MINOR=$(cat common/versionNumber.h | grep "VER_MAJOR " | awk '{print $3;}')
+VER_PATCH=$(cat common/versionNumber.h | grep "VER_MINOR " | awk '{print $3;}')
+VER_BUILD=$(cat common/versionNumber.h | grep "VER_MINORMINOR " | awk '{print $3;}')
+svn ci -m "[Mod] OpenMPT: Version is now ${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}.${VER_BUILD}"
 
 echo "ALL DONE."
 echo "run './release-0.5.sh $NEWVER +release' in a website checkout after buildbot has finished."
--- doc/libopenmpt_release.md
+++ doc/libopenmpt_release.md
@@ -18,17 +18,10 @@ https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28/doc/libopenmpt_rele
 0.5
 ---
 
- 1. ensure that the OpenMPT version is preferrably at a aa.bb.cc.00 version,
-    otherwise increment the minorminor part to a new value used specifically for
-    the libopenmpt release
- 2. from a clean checkout, run (requires xpath!!!)
+ 1. from a clean checkout, run (requires xpath!!!)
         ./build/svn/do_libopenmpt_release.sh
- 3. website: add release announcement
- 4. website: update download links
- 5. wait for buildbot
- 6. in a website checkout, run (as printed by the release script)
+ 2. website: add release announcement
+ 3. website: update download links
+ 4. wait for buildbot
+ 5. in a website checkout, run (as printed by the release script)
         ./release-0.5.sh $NEWVER +release
- 7. increment OpenMPT version minorminor in `common/versionNumber.h` when all
-    releases are done on the svn side (either libopenmpt only, or both
-    libopenmpt and OpenMPT)
-
