eddd7ce9415778700b4edc817a69ef63c3b341be
Johannes Schultz <sagamusix@openmpt.org>
2022-01-10 21:02:50 +0000

    [Fix] Reject a rows per beat value of 0 when loading a file with modern tempo mode, as this would result in infinite tick length and thus undefined behaviour when trying to convert the tick length to int (found with afl++ + ubsan)
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16484 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Sndfile.cpp
+++ soundlib/Sndfile.cpp
@@ -556,6 +556,8 @@ bool CSoundFile::Create(FileReader file, ModLoadingFlags loadFlags)
 	if(!m_nDefaultSpeed)
 		m_nDefaultSpeed = 6;
 
+	if(!m_nDefaultRowsPerBeat && m_nTempoMode == TempoMode::Modern)
+		m_nDefaultRowsPerBeat = 1;
 	if(m_nDefaultRowsPerMeasure < m_nDefaultRowsPerBeat)
 		m_nDefaultRowsPerMeasure = m_nDefaultRowsPerBeat;
 	LimitMax(m_nDefaultRowsPerBeat, MAX_ROWS_PER_BEAT);
