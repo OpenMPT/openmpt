OpenMPT/openmpt

commit e2a15d11e6551c84dce7f28ca6efe8ca2f96f998

[Ref] Add typesafe command+param setter functions to ModCommand.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16443 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_mo3.cpp
@@ -998,22 +998,22 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 	L= 06 00 22 x
 	*/
 
-	static constexpr ModCommand::COMMAND effTrans[] =
+	static constexpr EffectCommand effTrans[] =
 	{
 		CMD_NONE,               CMD_NONE,               CMD_NONE,               CMD_ARPEGGIO,
 		CMD_PORTAMENTOUP,       CMD_PORTAMENTODOWN,     CMD_TONEPORTAMENTO,     CMD_VIBRATO,
 		CMD_TONEPORTAVOL,       CMD_VIBRATOVOL,         CMD_TREMOLO,            CMD_PANNING8,
 		CMD_OFFSET,             CMD_VOLUMESLIDE,        CMD_POSITIONJUMP,       CMD_VOLUME,
 		CMD_PATTERNBREAK,       CMD_MODCMDEX,           CMD_TEMPO,              CMD_TREMOR,
-		VOLCMD_VOLSLIDEUP,      VOLCMD_FINEVOLUP,       CMD_GLOBALVOLUME,       CMD_GLOBALVOLSLIDE,
-		CMD_KEYOFF,             CMD_SETENVPOSITION,     CMD_PANNINGSLIDE,       VOLCMD_PANSLIDELEFT,
-		CMD_RETRIG,             CMD_XFINEPORTAUPDOWN,   CMD_XFINEPORTAUPDOWN,   VOLCMD_VIBRATOSPEED,
-		VOLCMD_VIBRATODEPTH,    CMD_SPEED,              CMD_VOLUMESLIDE,        CMD_PORTAMENTODOWN,
+		CMD_NONE,/*VolSlideUp*/ CMD_NONE,/*VolSlideDn*/ CMD_GLOBALVOLUME,       CMD_GLOBALVOLSLIDE,
+		CMD_KEYOFF,             CMD_SETENVPOSITION,     CMD_PANNINGSLIDE,       CMD_NONE,/*PanSlide*/
+		CMD_RETRIG,             CMD_XFINEPORTAUPDOWN,   CMD_XFINEPORTAUPDOWN,   CMD_NONE,/*VibSpeed*/
+		CMD_NONE,/*VibDepth*/   CMD_SPEED,              CMD_VOLUMESLIDE,        CMD_PORTAMENTODOWN,
 		CMD_PORTAMENTOUP,       CMD_TREMOR,             CMD_RETRIG,             CMD_FINEVIBRATO,
 		CMD_CHANNELVOLUME,      CMD_CHANNELVOLSLIDE,    CMD_PANNINGSLIDE,       CMD_S3MCMDEX,
 		CMD_TEMPO,              CMD_GLOBALVOLSLIDE,     CMD_PANBRELLO,          CMD_MIDI,
-		VOLCMD_FINEVOLUP,       VOLCMD_PORTADOWN,       VOLCMD_PORTAUP,         CMD_NONE,
-		VOLCMD_OFFSET,          CMD_XPARAM,             CMD_SMOOTHMIDI,         CMD_DELAYCUT,
+		CMD_NONE,/*FineVolSld*/ CMD_NONE,/*PortaDown*/  CMD_NONE, /*PortaUp*/   CMD_NONE,
+		CMD_NONE,/*ITVolCol*/   CMD_XPARAM,             CMD_SMOOTHMIDI,         CMD_DELAYCUT,
 		CMD_FINETUNE,           CMD_FINETUNE_SMOOTH,
 	};
 
@@ -1048,11 +1048,10 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 					break;
 
 				const uint8 numCommands = (b & 0x0F), rep = (b >> 4);
-				ModCommand m = ModCommand::Empty();
+				ModCommand m;
 				for(uint8 c = 0; c < numCommands; c++)
 				{
-					uint8 cmd[2];
-					track.ReadArray(cmd);
+					const auto cmd = track.ReadArray<uint8, 2>();
 
 					// Import pattern commands
 					switch(cmd[0])
@@ -1079,8 +1078,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 						// Tone portamento
 						if(m.volcmd == VOLCMD_NONE && m_nType == MOD_TYPE_XM && !(cmd[1] & 0x0F))
 						{
-							m.volcmd = VOLCMD_TONEPORTAMENTO;
-							m.vol = cmd[1] >> 4;
+							m.SetVolumeCommand(VOLCMD_TONEPORTAMENTO, cmd[1] >> 4);
 							break;
 						} else if(m.volcmd == VOLCMD_NONE && m_nType == MOD_TYPE_IT)
 						{
@@ -1088,27 +1086,23 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 							{
 								if(ImpulseTrackerPortaVolCmd[i] == cmd[1])
 								{
-									m.volcmd = VOLCMD_TONEPORTAMENTO;
-									m.vol = i;
+									m.SetVolumeCommand(VOLCMD_TONEPORTAMENTO, i);
 									break;
 								}
 							}
 							if(m.volcmd != VOLCMD_NONE)
 								break;
 						}
-						m.command = CMD_TONEPORTAMENTO;
-						m.param = cmd[1];
+						m.SetEffectCommand(CMD_TONEPORTAMENTO, cmd[1]);
 						break;
 					case 0x07:
 						// Vibrato
 						if(m.volcmd == VOLCMD_NONE && cmd[1] < 10 && m_nType == MOD_TYPE_IT)
 						{
-							m.volcmd = VOLCMD_VIBRATODEPTH;
-							m.vol = cmd[1];
+							m.SetVolumeCommand(VOLCMD_VIBRATODEPTH, cmd[1]);
 						} else
 						{
-							m.command = CMD_VIBRATO;
-							m.param = cmd[1];
+							m.SetEffectCommand(CMD_VIBRATO, cmd[1]);
 						}
 						break;
 					case 0x0B:
@@ -1117,88 +1111,65 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 						{
 							if(m_nType == MOD_TYPE_IT && cmd[1] == 0xFF)
 							{
-								m.volcmd = VOLCMD_PANNING;
-								m.vol = 64;
+								m.SetVolumeCommand(VOLCMD_PANNING, 64);
 								break;
 							}
 							if((m_nType == MOD_TYPE_IT && !(cmd[1] & 0x03))
 							   || (m_nType == MOD_TYPE_XM && !(cmd[1] & 0x0F)))
 							{
-								m.volcmd = VOLCMD_PANNING;
-								m.vol = cmd[1] / 4;
+								m.SetVolumeCommand(VOLCMD_PANNING, cmd[1] / 4);
 								break;
 							}
 						}
-						m.command = CMD_PANNING8;
-						m.param = cmd[1];
+						m.SetEffectCommand(CMD_PANNING8, cmd[1]);
 						break;
 					case 0x0F:
 						// Volume
 						if(m_nType != MOD_TYPE_MOD && m.volcmd == VOLCMD_NONE && cmd[1] <= 64)
-						{
-							m.volcmd = VOLCMD_VOLUME;
-							m.vol = cmd[1];
-						} else
-						{
-							m.command = CMD_VOLUME;
-							m.param = cmd[1];
-						}
+							m.SetVolumeCommand(VOLCMD_VOLUME, cmd[1]);
+						else
+							m.SetEffectCommand(CMD_VOLUME, cmd[1]);
 						break;
 					case 0x10:
 						// Pattern break
-						m.command = CMD_PATTERNBREAK;
-						m.param = cmd[1];
+						m.SetEffectCommand(CMD_PATTERNBREAK, cmd[1]);
 						if(m_nType != MOD_TYPE_IT)
 							m.param = ((m.param >> 4) * 10) + (m.param & 0x0F);
 						break;
 					case 0x12:
 						// Combined Tempo / Speed command
-						m.param = cmd[1];
-						if(m.param < 0x20)
-							m.command = CMD_SPEED;
-						else
-							m.command = CMD_TEMPO;
+						m.SetEffectCommand((m.param < 0x20) ? CMD_SPEED : CMD_TEMPO, cmd[1]);
 						break;
 					case 0x14:
 					case 0x15:
 						// XM volume column volume slides
 						if(cmd[1] & 0xF0)
 						{
-							m.volcmd = static_cast<ModCommand::VOLCMD>((cmd[0] == 0x14) ? VOLCMD_VOLSLIDEUP : VOLCMD_FINEVOLUP);
-							m.vol = cmd[1] >> 4;
+							m.SetVolumeCommand((cmd[0] == 0x14) ? VOLCMD_VOLSLIDEUP : VOLCMD_FINEVOLUP, cmd[1] >> 4);
 						} else
 						{
-							m.volcmd = static_cast<ModCommand::VOLCMD>((cmd[0] == 0x14) ? VOLCMD_VOLSLIDEDOWN : VOLCMD_FINEVOLDOWN);
-							m.vol = cmd[1] & 0x0F;
+							m.SetVolumeCommand((cmd[0] == 0x14) ? VOLCMD_VOLSLIDEDOWN : VOLCMD_FINEVOLDOWN, cmd[1] & 0x0F);
 						}
 						break;
 					case 0x1B:
 						// XM volume column panning slides
 						if(cmd[1] & 0xF0)
-						{
-							m.volcmd = VOLCMD_PANSLIDERIGHT;
-							m.vol = cmd[1] >> 4;
-						} else
-						{
-							m.volcmd = VOLCMD_PANSLIDELEFT;
-							m.vol = cmd[1] & 0x0F;
-						}
+							m.SetVolumeCommand(VOLCMD_PANSLIDERIGHT, cmd[1] >> 4);
+						else
+							m.SetVolumeCommand(VOLCMD_PANSLIDELEFT, cmd[1] & 0x0F);
 						break;
 					case 0x1D:
 						// XM extra fine porta up
-						m.command = CMD_XFINEPORTAUPDOWN;
-						m.param = 0x10 | cmd[1];
+						m.SetEffectCommand(CMD_XFINEPORTAUPDOWN, 0x10 | cmd[1]);
 						break;
 					case 0x1E:
 						// XM extra fine porta down
-						m.command = CMD_XFINEPORTAUPDOWN;
-						m.param = 0x20 | cmd[1];
+						m.SetEffectCommand(CMD_XFINEPORTAUPDOWN, 0x20 | cmd[1]);
 						break;
 					case 0x1F:
 					case 0x20:
 						// XM volume column vibrato
-						m.volcmd = effTrans[cmd[0]];
-						m.vol = cmd[1];
+						m.SetVolumeCommand((cmd[0] == 0x1F) ? VOLCMD_VIBRATOSPEED: VOLCMD_VIBRATODEPTH, cmd[1]);
 						break;
 					case 0x22:
 						// IT / S3M volume slide
@@ -1225,23 +1196,16 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 					case 0x31:
 					case 0x32:
 						// IT volume column portamento
-						m.volcmd = effTrans[cmd[0]];
-						m.vol = cmd[1];
+						m.SetVolumeCommand((cmd[0] == 0x31) ? VOLCMD_PORTADOWN: VOLCMD_PORTAUP, cmd[1]);
 						break;
 					case 0x34:
 						// Any unrecognized IT volume command
 						if(cmd[1] >= 223 && cmd[1] <= 232)
-						{
-							m.volcmd = VOLCMD_OFFSET;
-							m.vol = cmd[1] - 223;
-						}
+							m.SetVolumeCommand(VOLCMD_OFFSET, cmd[1] - 223);
 						break;
 					default:
 						if(cmd[0] < std::size(effTrans))
-						{
-							m.command = effTrans[cmd[0]];
-							m.param = cmd[1];
-						}
+							m.SetEffectCommand(effTrans[cmd[0]], cmd[1]);
 						break;
 					}
 				}

soundlib/Load_mdl.cpp
@@ -345,10 +345,8 @@ static bool ImportMDLCommands(ModCommand &m, uint8 vol, uint8 e1, uint8 e2, uint
 
 	if(offset != uint32_max && offset > 0xFF && ModCommand::GetEffectWeight(otherCmd) < ModCommand::GetEffectWeight(CMD_OFFSET))
 	{
-		m.command = CMD_OFFSET;
-		m.param = static_cast<ModCommand::PARAM>(offset & 0xFF);
-		m.volcmd = VOLCMD_OFFSET;
-		m.vol = static_cast<ModCommand::VOL>(offset >> 8);
+		m.SetEffectCommand(CMD_OFFSET, static_cast<ModCommand::PARAM>(offset & 0xFF));
+		m.SetVolumeCommand(VOLCMD_OFFSET, static_cast<ModCommand::VOL>(offset >> 8));
 		return otherCmd != CMD_NONE || vol != 0;
 	}
 

soundlib/Load_okt.cpp
@@ -275,8 +275,7 @@ static void ReadOKTPattern(FileReader &chunk, PATTERNINDEX pat, CSoundFile &sndF
 						other.volcmd = other.command;
 						other.vol = other.param;
 					}
-					other.command = m.command;
-					other.param = m.param;
+					other.SetEffectCommand(m);
 				}
 				break;
 

soundlib/Load_ult.cpp
@@ -227,13 +227,9 @@ static int ReadULTEvent(ModCommand &m, FileReader &file, uint8 version)
 	if(cmd1 == CMD_OFFSET && cmd2 == CMD_OFFSET)
 	{
 		uint32 offset = ((param2 << 8) | param1) >> 6;
-		m.command = CMD_OFFSET;
-		m.param = static_cast<ModCommand::PARAM>(offset);
+		m.SetEffectCommand(CMD_OFFSET, static_cast<ModCommand::PARAM>(offset));
 		if(offset > 0xFF)
-		{
-			m.volcmd = VOLCMD_OFFSET;
-			m.vol = static_cast<ModCommand::VOL>(offset >> 8);
-		}
+			m.SetVolumeCommand(VOLCMD_OFFSET, static_cast<ModCommand::VOL>(offset >> 8));
 		return repeat;
 	} else if(cmd1 == CMD_OFFSET)
 	{
@@ -241,10 +237,8 @@ static int ReadULTEvent(ModCommand &m, FileReader &file, uint8 version)
 		param1 = mpt::saturate_cast<uint8>(offset);
 		if(offset > 0xFF && ModCommand::GetEffectWeight(cmd2) < ModCommand::GetEffectType(CMD_OFFSET))
 		{
-			m.command = CMD_OFFSET;
-			m.param = static_cast<ModCommand::PARAM>(offset);
-			m.volcmd = VOLCMD_OFFSET;
-			m.vol = static_cast<ModCommand::VOL>(offset >> 8);
+			m.SetEffectCommand(CMD_OFFSET, static_cast<ModCommand::PARAM>(offset));
+			m.SetVolumeCommand(VOLCMD_OFFSET, static_cast<ModCommand::VOL>(offset >> 8));
 			return repeat;
 		}
 	} else if(cmd2 == CMD_OFFSET)
@@ -253,10 +247,8 @@ static int ReadULTEvent(ModCommand &m, FileReader &file, uint8 version)
 		param2 = mpt::saturate_cast<uint8>(offset);
 		if(offset > 0xFF && ModCommand::GetEffectWeight(cmd1) < ModCommand::GetEffectType(CMD_OFFSET))
 		{
-			m.command = CMD_OFFSET;
-			m.param = static_cast<ModCommand::PARAM>(offset);
-			m.volcmd = VOLCMD_OFFSET;
-			m.vol = static_cast<ModCommand::VOL>(offset >> 8);
+			m.SetEffectCommand(CMD_OFFSET, static_cast<ModCommand::PARAM>(offset));
+			m.SetVolumeCommand(VOLCMD_OFFSET, static_cast<ModCommand::VOL>(offset >> 8));
 			return repeat;
 		}
 	} else if(cmd1 == cmd2)

soundlib/modcommand.h
@@ -151,6 +151,13 @@ public:
 	}
 	bool operator!=(const ModCommand& mc) const { return !(*this == mc); }
 
+	MPT_FORCEINLINE void SetVolumeCommand(const VolumeCommand c, const VOL v) { volcmd = c; vol = v; }
+	MPT_FORCEINLINE void SetVolumeCommand(const std::pair<VolumeCommand, VOL> cmd) { volcmd = cmd.first; vol = cmd.second; }
+	MPT_FORCEINLINE void SetVolumeCommand(const ModCommand &other) { volcmd = other.volcmd; vol = other. vol; }
+	MPT_FORCEINLINE void SetEffectCommand(const EffectCommand c, const PARAM p) { command = c; param = p; }
+	MPT_FORCEINLINE void SetEffectCommand(const std::pair<EffectCommand, PARAM> cmd) { command = cmd.first; param = cmd.second; }
+	MPT_FORCEINLINE void SetEffectCommand(const ModCommand &other) { command = other.command; param = other.param; }
+
 	void Set(NOTE n, INSTR ins, uint16 volcol, uint16 effectcol) { note = n; instr = ins; SetValueVolCol(volcol); SetValueEffectCol(effectcol); }
 
 	uint16 GetValueVolCol() const { return GetValueVolCol(volcmd, vol); }

commit 2c6cf16d037b431c565b6393176d6fd3b47c50ba

[Fix] MO3: MOD/XM combined speed/tempo command broke in r16443.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16495 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_mo3.cpp
@@ -1138,7 +1138,7 @@ bool CSoundFile::ReadMO3(FileReader &file, ModLoadingFlags loadFlags)
 						break;
 					case 0x12:
 						// Combined Tempo / Speed command
-						m.SetEffectCommand((m.param < 0x20) ? CMD_SPEED : CMD_TEMPO, cmd[1]);
+						m.SetEffectCommand((cmd[1] < 0x20) ? CMD_SPEED : CMD_TEMPO, cmd[1]);
 						break;
 					case 0x14:
 					case 0x15: