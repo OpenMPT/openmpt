OpenMPT/openmpt

commit 12c371b9731de1ef603d942fc306081adfa70dd5

[Ref] Avoid directly putting untyped effect command bytes into ModCommand::command in various loaders.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16445 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_ptm.cpp
@@ -249,17 +249,16 @@ bool CSoundFile::ReadPTM(FileReader &file, ModLoadingFlags loadFlags)
 			if(b & 0x40)
 			{
 				const auto [command, param] = file.ReadArray<uint8, 2>();
-				m.command = command;
 				m.param = param;
 
 				static constexpr EffectCommand effTrans[] = { CMD_GLOBALVOLUME, CMD_RETRIG, CMD_FINEVIBRATO, CMD_NOTESLIDEUP, CMD_NOTESLIDEDOWN, CMD_NOTESLIDEUPRETRIG, CMD_NOTESLIDEDOWNRETRIG, CMD_REVERSEOFFSET };
 				if(m.command < 0x10)
 				{
 					// Beware: Effect letters are as in MOD, but portamento and volume slides behave like in S3M (i.e. fine slides share the same effect letters)
-					ConvertModCommand(m);
+					ConvertModCommand(m, command, param);
 				} else if(m.command < 0x10 + std::size(effTrans))
 				{
-					m.command = effTrans[m.command - 0x10];
+					m.command = effTrans[command - 0x10];
 				} else
 				{
 					m.command = CMD_NONE;

soundlib/Load_amf.cpp
@@ -169,9 +169,7 @@ bool CSoundFile::ReadAMF_Asylum(FileReader &file, ModLoadingFlags loadFlags)
 				m.note = note + 12 + NOTE_MIN;
 			}
 			m.instr = instr;
-			m.command = command;
-			m.param = param;
-			ConvertModCommand(m);
+			ConvertModCommand(m, command, param);
 #ifdef MODPLUG_TRACKER
 			if(m.command == CMD_PANNING8)
 			{
@@ -324,7 +322,7 @@ static void AMFReadPattern(CPattern &pattern, CHANNELINDEX chn, FileReader &file
 		} else
 		{
 			// Effect
-			static constexpr ModCommand::COMMAND effTrans[] =
+			static constexpr EffectCommand effTrans[] =
 			{
 				CMD_NONE,			CMD_SPEED,			CMD_VOLUMESLIDE,		CMD_VOLUME,
 				CMD_PORTAMENTOUP,	CMD_NONE,			CMD_TONEPORTAMENTO,		CMD_TREMOR,
@@ -334,13 +332,10 @@ static void AMFReadPattern(CPattern &pattern, CHANNELINDEX chn, FileReader &file
 				CMD_S3MCMDEX,		CMD_TEMPO,			CMD_PORTAMENTOUP,		CMD_PANNING8,
 			};
 
-			uint8 cmd = (command & 0x7F);
 			uint8 param = value;
-
-			if(cmd < std::size(effTrans))
-				cmd = effTrans[cmd];
-			else
-				cmd = CMD_NONE;
+			EffectCommand cmd = CMD_NONE;
+			if(uint8 maskedCmd = command & 0x7F; maskedCmd < std::size(effTrans))
+				cmd = effTrans[maskedCmd];
 
 			// Fix some commands...
 			switch(command & 0x7F)
@@ -396,7 +391,7 @@ static void AMFReadPattern(CPattern &pattern, CHANNELINDEX chn, FileReader &file
 			case 0x16:
 				if(param)
 				{
-					cmd = static_cast<uint8>((param & 0x80) ? CMD_PORTAMENTOUP : CMD_PORTAMENTODOWN);
+					cmd = (param & 0x80) ? CMD_PORTAMENTOUP : CMD_PORTAMENTODOWN;
 					if(param & 0x80)
 					{
 						param = ((-static_cast<int8>(param)) & 0x0F);

soundlib/Load_ams.cpp
@@ -42,7 +42,7 @@ static void ReadAMSPattern(CPattern &pattern, bool newVersion, FileReader &patte
 	};
 
 	// Effect translation table for extended (non-Protracker) effects
-	static constexpr ModCommand::COMMAND effTrans[] =
+	static constexpr EffectCommand effTrans[] =
 	{
 		CMD_S3MCMDEX,		// Forward / Backward
 		CMD_PORTAMENTOUP,	// Extra fine slide up
@@ -128,8 +128,7 @@ static void ReadAMSPattern(CPattern &pattern, bool newVersion, FileReader &patte
 					if(effect < 0x10)
 					{
 						// PT commands
-						m.command = effect;
-						CSoundFile::ConvertModCommand(m);
+						CSoundFile::ConvertModCommand(m, effect, m.param);
 
 						// Post-fix some commands
 						switch(m.command)

soundlib/Load_digi.cpp
@@ -37,8 +37,8 @@ MPT_BINARY_STRUCT(DIGIFileHeader, 610)
 
 static void ReadDIGIPatternEntry(FileReader &file, ModCommand &m)
 {
-	CSoundFile::ReadMODPatternEntry(file, m);
-	CSoundFile::ConvertModCommand(m);
+	const auto [command, param] = CSoundFile::ReadMODPatternEntry(file, m);
+	CSoundFile::ConvertModCommand(m, command, param);
 	if(m.command == CMD_MODCMDEX)
 	{
 		switch(m.param & 0xF0)

soundlib/Load_dsm.cpp
@@ -307,9 +307,7 @@ bool CSoundFile::ReadDSM(FileReader &file, ModLoadingFlags loadFlags)
 						if(command > 0x10)
 							command = ((command & 0xF0) == 0x20) ? 0x09 : 0xFF;
 					}
-					m.command = command;
-					m.param = param;
-					ConvertModCommand(m);
+					ConvertModCommand(m, command, param);
 				}
 			}
 			patNum++;

soundlib/Load_dsym.cpp
@@ -112,7 +112,7 @@ static std::vector<std::byte> DecompressDSymLZW(FileReader &file, uint32 size)
 
 			dictionary[nextIndex].value = match[writeOffset];
 			dictionary[nextIndex].prev = prevCode;
-			
+
 			nextIndex++;
 			if(nextIndex != MaxNodes && nextIndex == (1u << codeSize))
 				codeSize++;
@@ -312,10 +312,9 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 				if(command == 0 && param == 0)
 					continue;
 
-				m->command = command;
 				m->param = static_cast<uint8>(param);
 				m->vol = static_cast<ModCommand::VOL>(param >> 8);
-				
+
 				switch(command)
 				{
 					case 0x00:  // 00 xyz Normal play or Arpeggio + Volume Slide Up
@@ -324,8 +323,7 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 					case 0x20:  // 20 xyz Normal play or Arpeggio + Volume Slide Down
 					case 0x21:  // 21 xyy Slide Up + Volume Slide Down
 					case 0x22:  // 22 xyy Slide Down + Volume Slide Down
-						m->command &= 0x0F;
-						ConvertModCommand(*m);
+						ConvertModCommand(*m, command & 0x0F, m->param);
 						if(m->vol)
 							m->volcmd = (command < 0x20) ? VOLCMD_VOLSLIDEUP : VOLCMD_VOLSLIDEDOWN;
 						break;
@@ -335,7 +333,7 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 					case 0x06:  // 06 xyz Vibrato + Volume Slide
 					case 0x07:  // 07 xyz Tremolo
 					case 0x0C:  // 0C xyy Set Volume
-						ConvertModCommand(*m);
+						ConvertModCommand(*m, command, m->param);
 						break;
 					case 0x09:  // 09 xxx Set Sample Offset
 						m->command = CMD_OFFSET;
@@ -350,8 +348,7 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 					case 0x2A:  // 2A xyz Volume Slide + Fine Slide Down
 						if(param < 0xFF)
 						{
-							m->command &= 0x0F;
-							ConvertModCommand(*m);
+							ConvertModCommand(*m, command & 0x0F, m->param);
 						} else
 						{
 							m->command = CMD_MODCMDEX;
@@ -608,3 +605,4 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 
 
 OPENMPT_NAMESPACE_END
+

soundlib/Load_dtm.cpp
@@ -463,9 +463,7 @@ bool CSoundFile::ReadDTM(FileReader &file, ModLoadingFlags loadFlags)
 					}
 					if(command || param)
 					{
-						m->command = command;
-						m->param = param;
-						ConvertModCommand(*m);
+						ConvertModCommand(*m, command, param);
 #ifdef MODPLUG_TRACKER
 						m->Convert(MOD_TYPE_MOD, MOD_TYPE_IT, *this);
 #endif
@@ -487,6 +485,7 @@ bool CSoundFile::ReadDTM(FileReader &file, ModLoadingFlags loadFlags)
 				for(CHANNELINDEX chn = 0; chn < GetNumChannels(); chn++, m++)
 				{
 					const auto data = chunk.ReadArray<uint8, 4>();
+					uint8 command = 0;
 					if(patternFormat == DTM_204_PATTERN_FORMAT)
 					{
 						const auto [note, instrVol, instrCmd, param] = data;
@@ -501,14 +500,14 @@ bool CSoundFile::ReadDTM(FileReader &file, ModLoadingFlags loadFlags)
 							m->vol = vol - 1u;
 						}
 						m->instr = ((instrVol & 0x03) << 4) | (instrCmd >> 4);
-						m->command = instrCmd & 0x0F;
+						command = instrCmd & 0x0F;
 						m->param = param;
 					} else
 					{
-						ReadMODPatternEntry(data, *m);
+						std::tie(command, m->param) = ReadMODPatternEntry(data, *m);
 						m->instr |= data[0] & 0x30;	// Allow more than 31 instruments
 					}
-					ConvertModCommand(*m);
+					ConvertModCommand(*m, command, m->param);
 					// Fix commands without memory and slide nibble precedence
 					switch(m->command)
 					{

soundlib/Load_gt2.cpp
@@ -176,8 +176,7 @@ static void TranslateGraoumfEffect(CSoundFile &sndFile, ModCommand &m, const uin
 	case 0x07:
 	case 0x0B:
 	case 0x0F:
-		m.command = effect;
-		CSoundFile::ConvertModCommand(m);
+		CSoundFile::ConvertModCommand(m, effect, param);
 		if(m.command == CMD_TEMPO)
 			currentTempo.Set(m.param, 0);
 		else if(m.command == CMD_SPEED && m.param > 0)

commit eac144a9180cbb4d2e7a0ed4c3797505c26ecb1f

[Fix] PTM: Effect import broke in r16445.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16539 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_ptm.cpp
@@ -252,11 +252,11 @@ bool CSoundFile::ReadPTM(FileReader &file, ModLoadingFlags loadFlags)
 				m.param = param;
 
 				static constexpr EffectCommand effTrans[] = { CMD_GLOBALVOLUME, CMD_RETRIG, CMD_FINEVIBRATO, CMD_NOTESLIDEUP, CMD_NOTESLIDEDOWN, CMD_NOTESLIDEUPRETRIG, CMD_NOTESLIDEDOWNRETRIG, CMD_REVERSEOFFSET };
-				if(m.command < 0x10)
+				if(command < 0x10)
 				{
 					// Beware: Effect letters are as in MOD, but portamento and volume slides behave like in S3M (i.e. fine slides share the same effect letters)
 					ConvertModCommand(m, command, param);
-				} else if(m.command < 0x10 + std::size(effTrans))
+				} else if(command < 0x10 + std::size(effTrans))
 				{
 					m.command = effTrans[command - 0x10];
 				} else