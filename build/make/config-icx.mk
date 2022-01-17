5621133c1d315134f39572d59541cbd7950b885f
Jörn Heusipp <manx@openmpt.org>
2022-01-17 19:29:51 +0000

    [New] build: Makefile: Add icx (Intel C++) compiler support.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16545 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ build/make/config-icx.mk
@@ -0,0 +1,54 @@
+
+CC  = $(TOOLCHAIN_PREFIX)icx$(TOOLCHAIN_SUFFIX) 
+CXX = $(TOOLCHAIN_PREFIX)icpx$(TOOLCHAIN_SUFFIX) 
+LD  = $(TOOLCHAIN_PREFIX)icpx$(TOOLCHAIN_SUFFIX) 
+AR  = $(TOOLCHAIN_PREFIX)ar$(TOOLCHAIN_SUFFIX) 
+
+ifneq ($(STDCXX),)
+CXXFLAGS_STDCXX = -std=$(STDCXX)
+else ifeq ($(shell printf '\n' > bin/empty.cpp ; if $(CXX) -std=c++20 -c bin/empty.cpp -o bin/empty.out > /dev/null 2>&1 ; then echo 'c++20' ; fi ), c++20)
+CXXFLAGS_STDCXX = -std=c++20
+else ifeq ($(shell printf '\n' > bin/empty.cpp ; if $(CXX) -std=c++17 -c bin/empty.cpp -o bin/empty.out > /dev/null 2>&1 ; then echo 'c++17' ; fi ), c++17)
+CXXFLAGS_STDCXX = -std=c++17
+endif
+CFLAGS_STDC = -std=c99
+CXXFLAGS += $(CXXFLAGS_STDCXX)
+CFLAGS += $(CFLAGS_STDC)
+
+CPPFLAGS +=
+CXXFLAGS += -fPIC
+CFLAGS   += -fPIC
+LDFLAGS  += 
+LDLIBS   += 
+ARFLAGS  := rcs
+
+MODERN=0
+NATIVE=0
+OPTIMIZE=1
+OPTIMIZE_SIZE=0
+OPTIMIZE_MEDIUM=0
+OPTIMIZE_FASTMATH=0
+OPTIMIZE_LTO=1
+
+CXXFLAGS += -fp-model=precise
+CFLAGS   += -fp-model=precise
+
+ifeq ($(OPTIMIZE_LTO),1)
+CXXFLAGS += -ipo
+CFLAGS   += -ipo
+LDFLAGS  += -ipo
+endif
+
+ifeq ($(CHECKED_ADDRESS),1)
+CXXFLAGS += -fsanitize=address
+CFLAGS   += -fsanitize=address
+endif
+
+ifeq ($(CHECKED_UNDEFINED),1)
+CXXFLAGS += -fsanitize=undefined
+CFLAGS   += -fsanitize=undefined
+endif
+
+include build/make/warnings-clang.mk
+
+EXESUFFIX=
