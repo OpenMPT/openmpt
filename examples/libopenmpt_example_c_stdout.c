9b54ff12e9d16507ca9b61cbd0931546e8a606d9
Jörn Heusipp <manx@openmpt.org>
2022-01-20 10:09:08 +0000

    [Fix] libopenmpt: examples: Do not use deprecated function.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16549 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- examples/libopenmpt_example_c_stdout.c
+++ examples/libopenmpt_example_c_stdout.c
@@ -122,7 +122,7 @@ int main( int argc, char * argv[] ) {
 		goto fail;
 	}
 
-	mod = openmpt_module_create2( openmpt_stream_get_file_callbacks(), file, &libopenmpt_example_logfunc, NULL, &libopenmpt_example_errfunc, NULL, &mod_err, &mod_err_str, NULL );
+	mod = openmpt_module_create2( openmpt_stream_get_file_callbacks2(), file, &libopenmpt_example_logfunc, NULL, &libopenmpt_example_errfunc, NULL, &mod_err, &mod_err_str, NULL );
 	if ( !mod ) {
 		libopenmpt_example_print_error( "openmpt_module_create2()", mod_err, mod_err_str );
 		openmpt_free_string( mod_err_str );
