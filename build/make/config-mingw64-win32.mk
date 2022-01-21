876211940d24269db25d411341d5360684f59d56
Jörn Heusipp <manx@openmpt.org>
2022-01-21 17:37:39 +0000

    [Ref] build: #define NOMINMAX with every Windows build system.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16578 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/make/config-mingw64-win32.mk
+++ build/make/config-mingw64-win32.mk
@@ -9,7 +9,7 @@ CFLAGS_STDC = -std=c99
 CXXFLAGS += $(CXXFLAGS_STDCXX)
 CFLAGS += $(CFLAGS_STDC)
 
-CPPFLAGS +=
+CPPFLAGS += -DNOMINMAX
 CXXFLAGS += -municode -mconsole -mthreads
 CFLAGS   += -municode -mconsole -mthreads
 LDFLAGS  +=
