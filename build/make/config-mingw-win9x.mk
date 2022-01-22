7e93a6ea4932927f6432fd4bd48a64cf2bbbe650
Jörn Heusipp <manx@openmpt.org>
2022-01-22 19:09:04 +0000

    [Fix] build: Makefile: Fix typo.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16628 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/make/config-mingw-win9x.mk
+++ build/make/config-mingw-win9x.mk
@@ -4,7 +4,7 @@ CXX = mingw32-g++$(MINGW_FLAVOUR)
 LD  = mingw32-g++$(MINGW_FLAVOUR)
 AR  = mingw32-gcc-ar$(MINGW_FLAVOUR)
 
-CXXFLAGS_STDCXX = -std=gnu++17 -fexceptions -rtti
+CXXFLAGS_STDCXX = -std=gnu++17 -fexceptions -frtti
 CFLAGS_STDC = -std=gnu99
 CXXFLAGS += $(CXXFLAGS_STDCXX)
 CFLAGS += $(CFLAGS_STDC)
