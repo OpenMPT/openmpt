OpenMPT/openmpt

commit 4d2d0678222ba715fde5362d1b5089b4dca7ca2f

[Mod] Allow letter "z" to be evaluated in fixed Zxx macros (xx > 0x80) as well. Impulse Tracker allows this. No migration of old macros needs to happen because we don't allow the letter "z" to be entered for fixed macros anyway.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16715 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndmix.cpp
@@ -2536,7 +2536,7 @@ void CSoundFile::ProcessMacroOnChannel(CHANNELINDEX nChn)
 			if(chn.rowCommand.param < 0x80)
 				ProcessMIDIMacro(nChn, (chn.rowCommand.command == CMD_SMOOTHMIDI), m_MidiCfg.szMidiSFXExt[chn.nActiveMacro], chn.rowCommand.param);
 			else
-				ProcessMIDIMacro(nChn, (chn.rowCommand.command == CMD_SMOOTHMIDI), m_MidiCfg.szMidiZXXExt[(chn.rowCommand.param & 0x7F)], 0);
+				ProcessMIDIMacro(nChn, (chn.rowCommand.command == CMD_SMOOTHMIDI), m_MidiCfg.szMidiZXXExt[(chn.rowCommand.param & 0x7F)], chn.rowCommand.param);
 		}
 	}
 }

soundlib/Snd_fx.cpp
@@ -4930,7 +4930,7 @@ void CSoundFile::ProcessMIDIMacro(CHANNELINDEX nChn, bool isSmooth, const char *
 		} else if(macro[pos] == 'z')
 		{
 			// Zxx parameter
-			data = param & 0x7F;
+			data = param;
 			if(isSmooth && chn.lastZxxParam < 0x80
 				&& (outPos < 3 || out[outPos - 3] != 0xF0 || out[outPos - 2] < 0xF0))
 			{

commit 048183ee8de7e69976cb5ca2d06cde3096601ef6

[Fix] r16716 broke the macro change introduced in r16715.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16728 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndmix.cpp
@@ -2536,7 +2536,7 @@ void CSoundFile::ProcessMacroOnChannel(CHANNELINDEX nChn)
 			if(chn.rowCommand.param < 0x80)
 				ProcessMIDIMacro(m_PlayState, nChn, (chn.rowCommand.command == CMD_SMOOTHMIDI), m_MidiCfg.SFx[chn.nActiveMacro], chn.rowCommand.param);
 			else
-				ProcessMIDIMacro(m_PlayState, nChn, (chn.rowCommand.command == CMD_SMOOTHMIDI), m_MidiCfg.Zxx[chn.rowCommand.param & 0x7F], 0);
+				ProcessMIDIMacro(m_PlayState, nChn, (chn.rowCommand.command == CMD_SMOOTHMIDI), m_MidiCfg.Zxx[chn.rowCommand.param & 0x7F], chn.rowCommand.param);
 		}
 	}
 }