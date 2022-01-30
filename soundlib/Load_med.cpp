4908f6e78c56692a7a34cf4e826cc5951bd90249
Johannes Schultz <sagamusix@openmpt.org>
2022-01-30 14:50:58 +0000

    [Fix] MED: Command 20 00 00 (reverse sample) should only be applied when it's next to a note.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16754 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_med.cpp
+++ soundlib/Load_med.cpp
@@ -549,8 +549,11 @@ static void ConvertMEDEffect(ModCommand &m, const uint8 command, const bool is8c
 	case 0x20:  // Reverse sample + skip samples
 		if(m.param == 0 && m.vol == 0)
 		{
-			m.command = CMD_S3MCMDEX;
-			m.param = 0x9F;
+			if(m.IsNote())
+			{
+				m.command = CMD_S3MCMDEX;
+				m.param = 0x9F;
+			}
 		} else
 		{
 			// Skip given number of samples
