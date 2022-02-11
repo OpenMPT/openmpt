efb8a44c72a5017d58dbe64026de083fb1ba3048
Jörn Heusipp <manx@openmpt.org>
2022-02-11 13:07:05 +0000

    [Imp] build: Autotools: Enable C++20 when available. This uses AX_CXX_COMPILE_STDCXX serial 14 from upstream master, since they merged <https://github.com/autoconf-archive/autoconf-archive/pull/244>. Fixes <https://bugs.openmpt.org/view.php?id=1538>.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16918 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/autotools/autoconfiscate.sh
+++ build/autotools/autoconfiscate.sh
@@ -89,6 +89,7 @@ mkdir bin/dist-autotools/m4
 touch bin/dist-autotools/m4/emptydir
 svn export ./build/autotools/configure.ac bin/dist-autotools/configure.ac
 svn export ./build/autotools/Makefile.am bin/dist-autotools/Makefile.am
+svn export ./build/autotools/ax_cxx_compile_stdcxx.m4 bin/dist-autotools/m4/ax_cxx_compile_stdcxx.m4
 else
 echo "Exporting git ..."
 cp -r ./LICENSE         bin/dist-autotools/LICENSE
@@ -151,6 +152,7 @@ mkdir bin/dist-autotools/m4
 touch bin/dist-autotools/m4/emptydir
 cp -r ./build/autotools/configure.ac bin/dist-autotools/configure.ac
 cp -r ./build/autotools/Makefile.am bin/dist-autotools/Makefile.am
+cp -r ./build/autotools/ax_cxx_compile_stdcxx.m4 bin/dist-autotools/m4/ax_cxx_compile_stdcxx.m4
 fi
 
 echo "Querying svn version ..."
