6e58a168c5a40830a9addb44875803904358d44a
Jörn Heusipp <manx@openmpt.org>
2022-02-16 09:55:20 +0000

    [Var] Update clang-format to v13.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@16942 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/run_clang-format.sh
+++ build/svn/run_clang-format.sh
@@ -2,22 +2,22 @@
 set -e
 
 #cd libopenmpt
-#	clang-format-12 -i *.hpp *.cpp *.h
+#	clang-format-13 -i *.hpp *.cpp *.h
 #cd ..
 
 cd examples
-	clang-format-12 -i *.cpp *.c
+	clang-format-13 -i *.cpp *.c
 cd ..
 
 #cd openmpt123
-#	clang-format-12 -i *.hpp *.cpp
+#	clang-format-13 -i *.hpp *.cpp
 #cd ..
 
 cd src/mpt
-	find . -type f -iname '*\.hpp' | xargs clang-format-12 -i
+	find . -type f -iname '*\.hpp' | xargs clang-format-13 -i
 cd ../..
 
 cd src/openmpt
-	find . -type f -iname '*\.cpp' | xargs --no-run-if-empty clang-format-12 -i
-	find . -type f -iname '*\.hpp' | xargs --no-run-if-empty clang-format-12 -i
+	find . -type f -iname '*\.cpp' | xargs --no-run-if-empty clang-format-13 -i
+	find . -type f -iname '*\.hpp' | xargs --no-run-if-empty clang-format-13 -i
 cd ../..
