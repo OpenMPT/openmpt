732a69d479de8d5ac1b3672cb34ab0cd02fe0ccb
Jörn Heusipp <manx@openmpt.org>
2022-03-27 09:41:32 +0000

    [Fix] build: 1.31 installer builds are made with VS2022. Update manual building script accordingly.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17165 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/build_openmpt_release_manual.cmd
+++ build/auto/build_openmpt_release_manual.cmd
@@ -10,15 +10,15 @@ set MY_DIR=%CD%
 
 cmd /c build\download_externals.cmd auto nodownload                                                    || goto error
 cmd /c build\auto\update_package_template.cmd                                                          || goto error
-cmd /c build\auto\build_openmpt_args.cmd vs2019 win10 Win32 Release 7z default                         || goto error
-cmd /c build\auto\build_openmpt_args.cmd vs2019 win10 x64   Release 7z default                         || goto error
-cmd /c build\auto\build_openmpt_args.cmd vs2019 win10 ARM   Release 7z default                         || goto error
-cmd /c build\auto\build_openmpt_args.cmd vs2019 win10 ARM64 Release 7z default                         || goto error
-cmd /c build\auto\build_openmpt_args.cmd vs2019 win7  Win32 Release 7z default                         || goto error
-cmd /c build\auto\build_openmpt_args.cmd vs2019 win7  x64   Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2022 win10 Win32 Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2022 win10 x64   Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2022 win10 ARM   Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2022 win10 ARM64 Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2022 win7  Win32 Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2022 win7  x64   Release 7z default                         || goto error
 cmd /c build\auto\build_openmpt_release_packages_multiarch.cmd auto sign                               || goto error
 cmd /c build\auto\build_openmpt_update_information.cmd auto                                            || goto error
-cmd /c build\auto\package_openmpt_installer_multiarch_args.cmd vs2019 win10 Win32 Release 7z default   || goto error
+cmd /c build\auto\package_openmpt_installer_multiarch_args.cmd vs2022 win10 Win32 Release 7z default   || goto error
 
 
 
