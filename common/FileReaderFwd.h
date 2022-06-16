OpenMPT/openmpt

commit 8c6b8aa9336db0df5b0084d0715f32ae640170bd

[Ref] mpt/path/native_path.hpp: Add mpt::native_path. [Ref] mptPathString: Base on mpt::native_path if MPT_ENABLE_CHARSET_LOCALE.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17524 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/FileReaderFwd.h
@@ -18,6 +18,8 @@ inline namespace MPT_INLINE_NS {
 
 template <typename Traits, bool allow_transcode_locale>
 class BasicPathString;
+struct NativePathTraits;
+using native_path = BasicPathString<NativePathTraits, true>;
 
 namespace IO {
 
@@ -55,11 +57,10 @@ class FileReader;
 } // namespace detail
 
 namespace mpt {
-struct NativePathTraits;
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
-using PathString = mpt::BasicPathString<NativePathTraits, true>;
+using PathString = mpt::native_path;
 #else
-using PathString = mpt::BasicPathString<NativePathTraits, false>;
+using PathString = mpt::BasicPathString<mpt::NativePathTraits, false>;
 #endif
 } // namespace mpt
 

common/mptPathString.h
@@ -15,6 +15,7 @@
 #include "mpt/base/detect.hpp"
 #include "mpt/base/namespace.hpp"
 #include "mpt/path/basic_path.hpp"
+#include "mpt/path/native_path.hpp"
 #include "mpt/path/os_path.hpp"
 #include "mpt/string/types.hpp"
 
@@ -47,30 +48,10 @@ using RawPathString = mpt::utf8string;
 
 
 
-struct NativePathStyleTag
-{
-#if MPT_OS_WINDOWS
-#if defined(NTDDI_VERSION) || defined(_WIN32_WINNT)
-	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::WindowsNT;
-#else
-	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::Windows9x;
-#endif
-#elif MPT_OS_DJGPP
-	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::DOS_DJGPP;
-#else
-	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::Posix;
-#endif
-};
-
-struct NativePathTraits
-	: public PathTraits<RawPathString, NativePathStyleTag>
-{
-};
-
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
-using PathString = mpt::BasicPathString<NativePathTraits, true>;
+using PathString = mpt::native_path;
 #else
-using PathString = mpt::BasicPathString<NativePathTraits, false>;
+using PathString = mpt::BasicPathString<mpt::NativePathTraits, false>;
 #endif
 
 

libopenmpt/libopenmpt_impl.hpp
@@ -27,6 +27,8 @@ namespace mpt {
 inline namespace mpt_libopenmpt {
 template <typename Traits, bool allow_transcode_locale>
 class BasicPathString;
+struct NativePathTraits;
+using native_path = BasicPathString<NativePathTraits, true>;
 namespace IO {
 class FileCursorTraitsFileData;
 template <typename Tpath>
@@ -44,9 +46,9 @@ using FileCursor = mpt::IO::FileCursor<Ttraits, Tfilenametraits>;
 namespace mpt {
 struct NativePathTraits;
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
-using PathString = mpt::BasicPathString<NativePathTraits, true>;
+using PathString = mpt::native_path;
 #else
-using PathString = mpt::BasicPathString<NativePathTraits, false>;
+using PathString = mpt::BasicPathString<mpt::NativePathTraits, false>;
 #endif
 } // namespace mpt
 using FileCursor = detail::FileCursor<mpt::IO::FileCursorTraitsFileData, mpt::IO::FileCursorFilenameTraits<mpt::PathString>>;

src/mpt/path/basic_path.hpp
@@ -9,7 +9,6 @@
 #include "mpt/base/macros.hpp"
 #include "mpt/base/namespace.hpp"
 #include "mpt/detect/mfc.hpp"
-#include "mpt/path/os_path.hpp"
 #include "mpt/string_transcode/transcode.hpp"
 
 #include <stdexcept>
@@ -352,9 +351,9 @@ struct literals<char32_t> {
 
 enum class PathStyle {
 	Posix,
-	WindowsNT,
-	Windows9x,
 	DOS_DJGPP,
+	Windows9x,
+	WindowsNT,
 };
 
 template <PathStyle EStyle>

src/mpt/path/native_path.hpp
@@ -1,0 +1,47 @@
+/* SPDX-License-Identifier: BSL-1.0 OR BSD-3-Clause */
+
+#ifndef MPT_PATH_NATIVE_PATH_HPP
+#define MPT_PATH_NATIVE_PATH_HPP
+
+
+
+#include "mpt/base/detect.hpp"
+#include "mpt/base/macros.hpp"
+#include "mpt/base/namespace.hpp"
+#include "mpt/path/basic_path.hpp"
+#include "mpt/path/os_path.hpp"
+
+
+
+namespace mpt {
+inline namespace MPT_INLINE_NS {
+
+
+
+struct NativePathStyleTag {
+#if MPT_OS_WINDOWS_WINNT
+	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::WindowsNT;
+#elif MPT_OS_WINDOWS_WIN9X
+	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::Windows9x;
+#elif MPT_OS_WINDOWS
+	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::Windows9x;
+#elif MPT_OS_DJGPP
+	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::DOS_DJGPP;
+#else
+	static inline constexpr mpt::PathStyle path_style = mpt::PathStyle::Posix;
+#endif
+};
+
+struct NativePathTraits : public PathTraits<mpt::os_path, NativePathStyleTag> {
+};
+
+using native_path = BasicPathString<NativePathTraits, true>;
+
+
+
+} // namespace MPT_INLINE_NS
+} // namespace mpt
+
+
+
+#endif // MPT_PATH_NATIVE_PATH_HPP

commit ea397fe497beb0cc58e0389262d241d40a5e8d89

[Fix] mptPathString: Fix definition if !MPT_ENABLE_CHARSET_LOCALE. Broken since r17524.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17527 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/FileReaderFwd.h
@@ -60,7 +60,7 @@ namespace mpt {
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
 using PathString = mpt::native_path;
 #else
-using PathString = mpt::BasicPathString<mpt::NativePathTraits, false>;
+using PathString = mpt::BasicPathString<mpt::Utf8PathTraits, false>;
 #endif
 } // namespace mpt
 

common/mptPathString.h
@@ -33,26 +33,26 @@ namespace mpt
 
 
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
-using RawPathString = mpt::os_path;
+
+using PathString = mpt::native_path;
+
 #define MPT_PATHSTRING_LITERAL(x) MPT_OSPATH_LITERAL( x )
 #define MPT_PATHSTRING(x) mpt::PathString::FromNative(MPT_OSPATH_LITERAL( x ))
+
 #else // !MPT_ENABLE_CHARSET_LOCALE
-using RawPathString = mpt::utf8string;
+
+using PathString = mpt::BasicPathString<mpt::Utf8PathTraits, false>;
+
 #define MPT_PATHSTRING_LITERAL(x) ( x )
 #define MPT_PATHSTRING(x) mpt::PathString::FromNative( x )
+
 #endif // MPT_ENABLE_CHARSET_LOCALE
+
+using RawPathString = PathString::raw_path_type;
 
 #define PC_(x) MPT_PATHSTRING_LITERAL(x)
 #define PL_(x) MPT_PATHSTRING_LITERAL(x)
 #define P_(x) MPT_PATHSTRING(x)
-
-
-
-#if defined(MPT_ENABLE_CHARSET_LOCALE)
-using PathString = mpt::native_path;
-#else
-using PathString = mpt::BasicPathString<mpt::NativePathTraits, false>;
-#endif
 
 
 

libopenmpt/libopenmpt_impl.hpp
@@ -47,7 +47,7 @@ namespace mpt {
 #if defined(MPT_ENABLE_CHARSET_LOCALE)
 using PathString = mpt::native_path;
 #else
-using PathString = mpt::BasicPathString<mpt::NativePathTraits, false>;
+using PathString = mpt::BasicPathString<mpt::Utf8PathTraits, false>;
 #endif
 } // namespace mpt
 using FileCursor = detail::FileCursor<mpt::IO::FileCursorTraitsFileData, mpt::IO::FileCursorFilenameTraits<mpt::PathString>>;

src/mpt/path/basic_path.hpp
@@ -734,11 +734,13 @@ struct PathTraits {
 template <typename Traits, bool allow_transcode_locale = true>
 class BasicPathString {
 
-private:
+public:
 
 	using path_traits = Traits;
 	using raw_path_type = typename path_traits::raw_path_type;
 	using char_type = typename raw_path_type::value_type;
+
+private:
 
 	raw_path_type path;
 

src/mpt/path/native_path.hpp
@@ -10,6 +10,7 @@
 #include "mpt/base/namespace.hpp"
 #include "mpt/path/basic_path.hpp"
 #include "mpt/path/os_path.hpp"
+#include "mpt/string/types.hpp"
 
 
 
@@ -33,6 +34,9 @@ struct NativePathStyleTag {
 };
 
 struct NativePathTraits : public PathTraits<mpt::os_path, NativePathStyleTag> {
+};
+
+struct Utf8PathTraits : public PathTraits<mpt::utf8string, NativePathStyleTag> {
 };
 
 using native_path = BasicPathString<NativePathTraits, true>;