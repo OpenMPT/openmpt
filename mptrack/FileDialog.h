c3c18c059175620c28ba00a9f5d5b550a375b3f6
Jörn Heusipp <manx@openmpt.org>
2022-06-17 04:23:24 +0000

    [Fix] FileDialog: Fix charset ambiguities in ANSI build.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17533 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/FileDialog.h
+++ mptrack/FileDialog.h
@@ -44,14 +44,20 @@ public:
 	FileDialog &DefaultExtension(const mpt::RawPathString &ext) { m_defaultExtension = ext; return *this; }
 	FileDialog &DefaultExtension(const mpt::PathString &ext) { m_defaultExtension = ext.AsNative(); return *this; }
 	FileDialog &DefaultExtension(const AnyStringLocale &ext) { m_defaultExtension = mpt::ToWin(ext); return *this; }
+	FileDialog &DefaultExtension(const CString &ext) { m_defaultExtension = mpt::ToWin(ext); return *this; }
+	FileDialog &DefaultExtension(const char *ext) { m_defaultExtension = mpt::ToWin(mpt::Charset::Locale, ext); return *this; }
 	// Default suggested filename.
 	FileDialog &DefaultFilename(const mpt::RawPathString &name) { m_defaultFilename = name; return *this; }
 	FileDialog &DefaultFilename(const mpt::PathString &name) { m_defaultFilename = name.AsNative(); return *this; }
 	FileDialog &DefaultFilename(const AnyStringLocale &name) { m_defaultFilename = mpt::ToWin(name); return *this; }
+	FileDialog &DefaultFilename(const CString &name) { m_defaultFilename = mpt::ToWin(name); return *this; }
+	FileDialog &DefaultFilename(const char *name) { m_defaultFilename = mpt::ToWin(mpt::Charset::Locale, name); return *this; }
 	// List of possible extensions. Format: "description|extensions|...|description|extensions||"
 	FileDialog &ExtensionFilter(const mpt::RawPathString &filter) { m_extFilter = filter; return *this; }
 	FileDialog &ExtensionFilter(const mpt::PathString &filter) { m_extFilter = filter.AsNative(); return *this; }
 	FileDialog &ExtensionFilter(const AnyStringLocale &filter) { m_extFilter = mpt::ToWin(filter); return *this; }
+	FileDialog &ExtensionFilter(const CString &filter) { m_extFilter = mpt::ToWin(filter); return *this; }
+	FileDialog &ExtensionFilter(const char *filter) { m_extFilter = mpt::ToWin(mpt::Charset::Locale, filter); return *this; }
 	// Default directory of the dialog.
 	FileDialog &WorkingDirectory(const mpt::PathString &dir) { m_workingDirectory = dir.AsNative(); return *this; }
 	// Pointer to a variable holding the index of the last extension filter to use. Holds the selected filter after the dialog has been closed.
