cdb2c341072d68c1cb95b6f47d4b7865bf7ec5eb
Jörn Heusipp <manx@openmpt.org>
2022-06-22 07:02:35 +0000

    Merged revision(s) 17566 from trunk/OpenMPT: [Fix] Enable missing wmain prototype work-around also for MinGW clang. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@17567 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- examples/libopenmpt_example_cxx.cpp
+++ examples/libopenmpt_example_cxx.cpp
@@ -34,7 +34,7 @@
 #endif
 
 #if ( defined( _WIN32 ) || defined( WIN32 ) ) && ( defined( _UNICODE ) || defined( UNICODE ) )
-#if defined( __GNUC__ )
+#if defined( __GNUC__ ) || ( defined( __clang__ ) && !defined( _MSC_VER ) )
 // mingw-w64 g++ does only default to special C linkage for "main", but not for "wmain" (see <https://sourceforge.net/p/mingw-w64/wiki2/Unicode%20apps/>).
 extern "C" int wmain( int /*argc*/, wchar_t * /*argv*/[] );
 extern "C" int wmain( int argc, wchar_t * argv[] ) {
