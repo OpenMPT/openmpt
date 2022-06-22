c2e6d528a14ed34d0a4fea8150a6081e7d98b54a
Jörn Heusipp <manx@openmpt.org>
2022-06-22 05:18:59 +0000

    Merged revision(s) 17563 from trunk/OpenMPT: [Ref] openmpt123: Silence clang warning. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@17564 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- openmpt123/openmpt123_config.hpp
+++ openmpt123/openmpt123_config.hpp
@@ -32,7 +32,14 @@
 #endif
 #ifdef UNICODE
 #ifndef _UNICODE
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#endif
 #define _UNICODE
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 #endif
 #endif
 #endif // WIN32
