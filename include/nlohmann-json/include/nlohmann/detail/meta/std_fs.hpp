268ee625db09acd4043e88ee9bedc6b6311d5314
Jörn Heusipp <manx@openmpt.org>
2022-08-01 12:35:06 +0000

    Merged revision(s) 17676 from trunk/OpenMPT: [Var] nlohmann-json: Update to v3.11.0 (2022-08-01). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@17677 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/nlohmann-json/include/nlohmann/detail/meta/std_fs.hpp
+++ include/nlohmann-json/include/nlohmann/detail/meta/std_fs.hpp
@@ -8,14 +8,22 @@
 
 #pragma once
 
-#include <nlohmann/detail/abi_macros.hpp>
+#include <nlohmann/detail/macro_scope.hpp>
 
+#if JSON_HAS_EXPERIMENTAL_FILESYSTEM
+#include <experimental/filesystem>
 NLOHMANN_JSON_NAMESPACE_BEGIN
 namespace detail
 {
-
-// dispatching helper struct
-template <class T> struct identity_tag {};
-
+namespace std_fs = std::experimental::filesystem;
 }  // namespace detail
 NLOHMANN_JSON_NAMESPACE_END
+#elif JSON_HAS_FILESYSTEM
+#include <filesystem>
+NLOHMANN_JSON_NAMESPACE_BEGIN
+namespace detail
+{
+namespace std_fs = std::filesystem;
+}  // namespace detail
+NLOHMANN_JSON_NAMESPACE_END
+#endif
