cc4eaa39c19272feb0449267d8d447a547df7c17
Jörn Heusipp <manx@openmpt.org>
2022-08-11 12:09:36 +0000

    [Var] Update clang-format to v14.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17709 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/run_clang-format.sh
+++ build/svn/run_clang-format.sh
@@ -2,22 +2,22 @@
 set -e
 
 #cd libopenmpt
-#	clang-format-13 -i *.hpp *.cpp *.h
+#	clang-format-14 -i *.hpp *.cpp *.h
 #cd ..
 
 cd examples
-	clang-format-13 -i *.cpp *.c
+	clang-format-14 --verbose -i *.cpp *.c
 cd ..
 
 #cd openmpt123
-#	clang-format-13 -i *.hpp *.cpp
+#	clang-format-14 -i *.hpp *.cpp
 #cd ..
 
 cd src/mpt
-	find . -type f -iname '*\.hpp' | xargs clang-format-13 -i
+	find . -type f -iname '*\.hpp' | xargs clang-format-14 --verbose -i
 cd ../..
 
 cd src/openmpt
-	find . -type f -iname '*\.cpp' | xargs --no-run-if-empty clang-format-13 -i
-	find . -type f -iname '*\.hpp' | xargs --no-run-if-empty clang-format-13 -i
+	find . -type f -iname '*\.cpp' | xargs --no-run-if-empty clang-format-14 --verbose -i
+	find . -type f -iname '*\.hpp' | xargs --no-run-if-empty clang-format-14 --verbose -i
 cd ../..
