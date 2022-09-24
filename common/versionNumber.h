OpenMPT/openmpt

commit ab9277cee0527e73c42af9efe1748fb8a7441d98

[Imp] Most tree view shortcuts are now configurable. [New] Tree view: Can now filter the instrument library browser. The default shortcut for invoking the filter is Ctrl+F (https://bugs.openmpt.org/view.php?id=1566). [New] Tree view: Can now sort the instrument library by date and file size as well. [Mod] OpenMPT: Version is now 1.31.00.14
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17825 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/versionNumber.h
@@ -18,6 +18,6 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR  1
 #define VER_MAJOR      31
 #define VER_MINOR      00
-#define VER_MINORMINOR 13
+#define VER_MINORMINOR 14
 
 OPENMPT_NAMESPACE_END

mptrack/CommandSet.cpp
@@ -422,6 +422,13 @@ constexpr struct
 	{kcToggleSmpInsList,                       VK_TAB,             ModNone,            kKeyEventDown,                   kCtxViewComments,        MPT_V("1.31")},
 	{kcExecuteSmpInsListItem,                  VK_RETURN,          ModNone,            kKeyEventDown,                   kCtxViewComments,        MPT_V("1.31")},
 	{kcRenameSmpInsListItem,                   VK_RETURN,          ModCtrl,            kKeyEventDown,                   kCtxViewComments,        MPT_V("1.31")},
+	{kcTreeViewOpen,                           VK_RETURN,          ModNone,            kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewPlay,                           VK_SPACE,           ModNone,            kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewInsert,                         VK_INSERT,          ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewDuplicate,                      VK_INSERT,          ModShift,           kKeyEventDown | kKeyEventRepeat, kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewDelete,                         VK_DELETE,          ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewRename,                         VK_RETURN,          ModCtrl,            kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewFind,                           'F',                ModCtrl,            kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.14")},
 	{kcVSTGUIPrevPreset,                       VK_SUBTRACT,        ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxVSTGUI,              MPT_V("1.31")},
 	{kcVSTGUINextPreset,                       VK_ADD,             ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxVSTGUI,              MPT_V("1.31")},
 	{kcVSTGUIPrevPresetJump,                   VK_SUBTRACT,        ModCtrl,            kKeyEventDown | kKeyEventRepeat, kCtxVSTGUI,              MPT_V("1.31")},
@@ -1212,6 +1219,16 @@ static constexpr struct
 	{2033, kcInsNoteMapTransposeSamples, _T("Transpose Samples / Reset Map") },
 	{KeyCommand::Hidden, kcPrevEntryInColumnSelect, _T("kcPrevEntryInColumnSelect")},
 	{KeyCommand::Hidden, kcNextEntryInColumnSelect, _T("kcNextEntryInColumnSelect")},
+	{2034, kcTreeViewOpen, _T("Open / View Item")},
+	{2035, kcTreeViewPlay, _T("Play Item")},
+	{2036, kcTreeViewInsert, _T("Insert Item")},
+	{2037, kcTreeViewDuplicate, _T("Duplicate Item")},
+	{2038, kcTreeViewDelete, _T("Delete Item")},
+	{2039, kcTreeViewRename, _T("Rename Item / Send To Editor")},
+	{2040, kcTreeViewFind, _T("Find in Instrument Library")},
+	{2041, kcTreeViewSortByName, _T("Sort Instrument Library By Name")},
+	{2042, kcTreeViewSortByDate, _T("Sort Instrument Library By Date")},
+	{2043, kcTreeViewSortBySize, _T("Sort Instrument Library By Size")},
 };
 
 // Get command descriptions etc.. loaded up.
@@ -2262,6 +2279,7 @@ CString KeyCombination::GetContextText(InputTargetContext ctx)
 		case kCtxViewSamples:			return _T("Sample Context [bottom]");
 		case kCtxViewInstruments:		return _T("Instrument Context [bottom]");
 		case kCtxViewComments:			return _T("Comments Context [bottom]");
+		case kCtxViewTree:				return _T("Tree View");
 		case kCtxCtrlGeneral:			return _T("General Context [top]");
 		case kCtxCtrlPatterns:			return _T("Pattern Context [top]");
 		case kCtxCtrlSamples:			return _T("Sample Context [top]");

mptrack/CommandSet.h
@@ -813,7 +813,19 @@ enum CommandID
 	kcCommentsStartNoteStops,
 	kcCommentsEndNoteStops = kcCommentsStartNoteStops + kcCommandSetNumNotes,
 
-	kcTreeViewStopPreview,
+	kcStartTreeViewCommands,
+	kcTreeViewStopPreview = kcStartTreeViewCommands,
+	kcTreeViewOpen,
+	kcTreeViewPlay,
+	kcTreeViewInsert,
+	kcTreeViewDuplicate,
+	kcTreeViewDelete,
+	kcTreeViewRename,
+	kcTreeViewFind,
+	kcTreeViewSortByName,
+	kcTreeViewSortByDate,
+	kcTreeViewSortBySize,
+	kcEndTreeViewCommands = kcTreeViewSortBySize,
 
 	kcStartVSTGUICommands,
 	kcVSTGUIPrevPreset = kcStartVSTGUICommands,

mptrack/KeyConfigDlg.cpp
@@ -332,6 +332,12 @@ void COptionsKeyboard::DefineCommandCategories()
 		newCat.AddCommands(kcStartVSTGUICommands, kcEndVSTGUICommands);
 		commandCategories.push_back(newCat);
 	}
+
+	{
+		CommandCategory newCat(_T("  Tree View"), kCtxViewTree);
+		newCat.AddCommands(kcStartTreeViewCommands, kcEndTreeViewCommands);
+		commandCategories.push_back(newCat);
+	}
 }
 
 

mptrack/Mainbar.cpp
@@ -781,7 +781,12 @@ BEGIN_MESSAGE_MAP(CModTreeBar, CDialogBar)
 	ON_WM_NCLBUTTONDOWN()
 	ON_WM_LBUTTONUP()
 	ON_WM_NCLBUTTONUP()
-	ON_MESSAGE(WM_INITDIALOG,	&CModTreeBar::OnInitDialog)
+	ON_MESSAGE(WM_INITDIALOG, &CModTreeBar::OnInitDialog)
+
+	ON_EN_CHANGE(IDC_EDIT1,             &CModTreeBar::OnFilterChanged)
+	ON_EN_KILLFOCUS(IDC_EDIT1,          &CModTreeBar::OnFilterLostFocus)
+	ON_COMMAND(ID_CLOSE_LIBRARY_FILTER, &CModTreeBar::CloseTreeFilter)
+
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -832,6 +837,46 @@ void CModTreeBar::Init()
 }
 
 
+BOOL CModTreeBar::PreTranslateMessage(MSG *pMsg)
+{
+	if(m_filterEdit && pMsg->hwnd == m_filterEdit && pMsg->message == WM_KEYDOWN && m_filterSource != nullptr)
+	{
+		switch(pMsg->wParam)
+		{
+		case VK_RETURN:
+			if(const auto modItem = m_filterSource->GetModItem(m_filterSource->GetSelectedItem()); (modItem.type == CModTree::MODITEM_INSLIB_FOLDER || modItem.type == CModTree::MODITEM_INSLIB_SONG))
+			{
+				m_filterSource->PostMessage(WM_COMMAND, ID_MODTREE_EXECUTE);
+			}
+			[[fallthrough]];
+		case VK_ESCAPE:
+			CloseTreeFilter();
+			return TRUE;
+
+		case VK_TAB:
+			if(m_filterSource)
+			{
+				m_filterSource->SetFocus();
+				return TRUE;
+			}
+			break;
+
+		case VK_UP:
+		case VK_DOWN:
+			if(const auto selectedItem = m_filterSource->GetSelectedItem(); selectedItem != nullptr)
+			{
+				const auto item = m_filterSource->GetNextItem(selectedItem, (pMsg->wParam == VK_UP) ? TVGN_PREVIOUS : TVGN_NEXT);
+				if(item)
+					m_filterSource->SelectItem(item);
+				return TRUE;
+			}
+			break;
+		}
+	}
+	return CDialogBar::PreTranslateMessage(pMsg);
+}
+
+
 void CModTreeBar::RefreshDlsBanks()
 {
 	if(m_pModTree) m_pModTree->RefreshDlsBanks();
@@ -863,8 +908,23 @@ void CModTreeBar::RecalcLayout()
 		if(cyavail < 0) cyavail = 0;
 		cytree = (cyavail * m_nTreeSplitRatio) >> 8;
 		cydata = cyavail - cytree;
-		m_pModTree->SetWindowPos(NULL, 0,0, rect.Width(), cytree, SWP_NOZORDER|SWP_NOACTIVATE);
-		m_pModTreeData->SetWindowPos(NULL, 0,cytree+3, rect.Width(), cydata, SWP_NOZORDER|SWP_NOACTIVATE);
+		if(m_filterSource == m_pModTree)
+		{
+			int editHeight = Util::ScalePixels(20, m_hWnd);
+			m_pModTree->SetWindowPos(nullptr, 0, 0, rect.Width(), cytree - editHeight, SWP_NOZORDER | SWP_NOACTIVATE);
+			m_pModTreeData->SetWindowPos(nullptr, 0, cytree + 3, rect.Width(), cydata, SWP_NOZORDER | SWP_NOACTIVATE);
+			m_filterEdit.SetWindowPos(m_pModTree, 0, cytree - editHeight, rect.Width(), editHeight, SWP_NOACTIVATE);
+		} else if(m_filterSource == m_pModTreeData)
+		{
+			int editHeight = Util::ScalePixels(20, m_hWnd);
+			m_pModTree->SetWindowPos(nullptr, 0, 0, rect.Width(), cytree, SWP_NOZORDER | SWP_NOACTIVATE);
+			m_pModTreeData->SetWindowPos(nullptr, 0, cytree + 3, rect.Width(), cydata - editHeight, SWP_NOZORDER | SWP_NOACTIVATE);
+			m_filterEdit.SetWindowPos(m_pModTreeData, 0, cytree + 3 + cydata - editHeight, rect.Width(), editHeight, SWP_NOACTIVATE);
+		} else
+		{
+			m_pModTree->SetWindowPos(nullptr, 0, 0, rect.Width(), cytree, SWP_NOZORDER | SWP_NOACTIVATE);
+			m_pModTreeData->SetWindowPos(nullptr, 0, cytree + 3, rect.Width(), cydata, SWP_NOZORDER | SWP_NOACTIVATE);
+		}
 	}
 }
 
@@ -1229,6 +1289,61 @@ bool CModTreeBar::SetTreeSoundfile(FileReader &file)
 	return m_pModTree->SetSoundFile(file);
 }
 
+
+void CModTreeBar::StartTreeFilter(CModTree &source)
+{
+	if(!m_filterEdit)
+	{
+		CRect rect;
+		GetClientRect(rect);
+		rect.bottom = Util::ScalePixels(20, m_hWnd);
+
+		m_pModTree->GetItemRect(m_pModTree->GetFirstVisibleItem(), rect, FALSE);
+		m_filterEdit.Create(WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP | ES_LEFT | ES_AUTOHSCROLL, rect, this, IDC_EDIT1);
+		m_filterEdit.SetFont(GetFont());
+	} else if(m_filterSource != &source)
+	{
+		m_filterEdit.SetWindowText(_T(""));
+	}
+	m_filterEdit.SetFocus();
+	m_filterSource = &source;
+	RecalcLayout();
+}
+
+
+void CModTreeBar::OnFilterChanged()
+{
+	if(m_filterSource)
+	{
+		CString filter;
+		m_filterEdit.GetWindowText(filter);
+		m_filterSource->SetInstrumentLibraryFilter(mpt::ToWin(filter));
+	}
+}
+
+
+void CModTreeBar::OnFilterLostFocus()
+{
+	if(m_filterEdit && !m_filterEdit.GetWindowTextLength())
+		CloseTreeFilter();
+}
+
+
+void CModTreeBar::CloseTreeFilter()
+{
+	if(m_filterSource)
+	{
+		m_filterSource->SetInstrumentLibraryFilter({});
+		if(GetFocus() == &m_filterEdit)
+			m_filterSource->SetFocus();
+		m_filterSource = nullptr;
+	}
+	if(m_filterEdit)
+	{
+		m_filterEdit.DestroyWindow();
+		RecalcLayout();
+	}
+}
 
 
 

mptrack/Mainbar.h
@@ -143,6 +143,9 @@ protected:
 	UINT m_cxOriginal = 0, m_cyOriginal = 0, m_nTrackPos = 0;
 	UINT m_nTreeSplitRatio = 0;
 
+	CEdit m_filterEdit;
+	CModTree *m_filterSource = nullptr;
+
 public:
 	CModTree *m_pModTree = nullptr, *m_pModTreeData = nullptr;
 
@@ -168,11 +171,15 @@ public:
 	LRESULT SendMessageToModTree(UINT cmdID, WPARAM wParam, LPARAM lParam);
 	bool SetTreeSoundfile(FileReader &file);
 
+	void StartTreeFilter(CModTree &source);
 
 protected:
 	//{{AFX_VIRTUAL(CModTreeBar)
 	CSize CalcFixedLayout(BOOL bStretch, BOOL bHorz) override;
+	BOOL PreTranslateMessage(MSG *pMsg) override;
 	//}}AFX_VIRTUAL
+
+	void CloseTreeFilter();
 
 protected:
 	//{{AFX_MSG(CModTreeBar)
@@ -189,6 +196,8 @@ protected:
 	afx_msg void OnNcRButtonDown(UINT, CPoint) { CancelTracking(); }
 	afx_msg void OnRButtonDown(UINT, CPoint) { CancelTracking(); }
 	afx_msg LRESULT OnInitDialog(WPARAM, LPARAM);
+	afx_msg void OnFilterChanged();
+	afx_msg void OnFilterLostFocus();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };

mptrack/View_tre.h
@@ -1,5 +1,5 @@
 /*
- * view_tre.h
+ * View_tre.h
  * ----------
  * Purpose: Tree view for managing open songs, sound files, file browser, ...
  * Notes  : (currently none)
@@ -12,6 +12,13 @@
 #pragma once
 
 #include "openmpt/all/BuildSettings.hpp"
+
+#include "../soundlib/modcommand.h"
+#include "../soundlib/Snd_defs.h"
+
+#include "Mptrack.h"
+#include "Notification.h"
+#include "UpdateHints.h"
 
 #include <vector>
 #include <bitset>
@@ -60,15 +67,7 @@ public:
 
 class CModTree: public CTreeCtrl
 {
-protected:
-	enum TreeStatus
-	{
-		TREESTATUS_RDRAG        = 0x01,
-		TREESTATUS_LDRAG        = 0x02,
-		TREESTATUS_SINGLEEXPAND = 0x04,
-		TREESTATUS_DRAGGING     = (TREESTATUS_RDRAG | TREESTATUS_LDRAG)
-	};
-
+public:
 	enum ModItemType : uint8
 	{
 		MODITEM_NULL = 0,
@@ -100,6 +99,15 @@ protected:
 		MODITEM_INSLIB_SONG,
 		MODITEM_DLSBANK_FOLDER,
 		MODITEM_DLSBANK_INSTRUMENT,
+	};
+
+protected:
+	enum TreeStatus
+	{
+		TREESTATUS_RDRAG = 0x01,
+		TREESTATUS_LDRAG = 0x02,
+		TREESTATUS_SINGLEEXPAND = 0x04,
+		TREESTATUS_DRAGGING = (TREESTATUS_RDRAG | TREESTATUS_LDRAG)
 	};
 
 	// Bit mask magic
@@ -173,7 +181,6 @@ protected:
 	std::unique_ptr<CDLSBank> m_cachedBank;
 	mpt::PathString m_cachedBankName;
 
-	CString m_compareStrL, m_compareStrR;  // Cache for ModTreeInsLibCompareNamesProc to avoid constant re-allocations
 #if (_WIN32_WINNT >= _WIN32_WINNT_WIN7)
 	DWORD m_stringCompareFlags = NORM_IGNORECASE | NORM_IGNOREWIDTH | SORT_DIGITSASNUMBERS;
 #else
@@ -185,18 +192,37 @@ protected:
 	mpt::PathString m_InstrLibHighlightPath;  // Folder to highlight in browser after a refresh
 	mpt::PathString m_SongFileName;           // Name of open module, without path (== m_InstrLibPath).
 	mpt::PathString m_previousPath;           // The folder from which we came from when navigating one folder up
-	std::vector<const char*> m_modExtensions;                  // cached in order to avoid querying too often when changing browsed folder
-	std::vector<mpt::PathString> m_MediaFoundationExtensions;  // cached in order to avoid querying too often when changing browsed folder
-	bool m_showAllFiles = false;
+	
+	std::vector<const char*> m_modExtensions;                  // Cached in order to avoid querying too often when changing browsed folder
+	std::vector<mpt::PathString> m_MediaFoundationExtensions;  // Cached in order to avoid querying too often when changing browsed folder
 
+	struct FileBrowserEntry
+	{
+		mpt::winstring name;
+		uint64 size;
+		uint64 modtime;
+		uint32 image;
+		bool hidden;
+	};
+	std::vector<FileBrowserEntry> m_fileBrowserEntries;
+	mpt::winstring m_filterString;
+
+	enum class LibrarySortOrder
+	{
+		Name,
+		Date,
+		Size,
+	};
+
+	static LibrarySortOrder m_librarySort;
+
+	bool m_showAllFiles = false;
 	bool m_doLabelEdit = false;
 
 public:
 	CModTree(CModTree *pDataTree);
 	~CModTree();
 
-// Attributes
-public:
 	void Init();
 	bool InsLibSetFullPath(const mpt::PathString &libPath, const mpt::PathString &songFolder);
 	mpt::PathString InsLibGetFullPath(HTREEITEM hItem) const;
@@ -204,8 +230,6 @@ public:
 	void RefreshMidiLibrary();
 	void RefreshDlsBanks();
 	void RefreshInstrumentLibrary();
-	void EmptyInstrumentLibrary();
-	void FillInstrumentLibrary(const TCHAR *selectedItem = nullptr);
 	void MonitorInstrumentLibrary();
 	ModItem GetModItem(HTREEITEM hItem);
 	BOOL SetMidiInstrument(UINT nIns, const mpt::PathString &fileName);
@@ -233,12 +257,17 @@ public:
 
 	bool IsSampleBrowser() const { return m_pDataTree == nullptr; }
 	CModTree *GetSampleBrowser() { return IsSampleBrowser() ? this : m_pDataTree; }
+	CModTree *GetOtherView();
+
+	void SetInstrumentLibraryFilter(const mpt::winstring &filter);
+	void SetInstrumentLibraryFilterSortOrder(LibrarySortOrder sortType);
+	void SortInstrumentLibrary();
 
 // Overrides
 	// ClassWizard generated virtual function overrides
 	//{{AFX_VIRTUAL(CModTree)
 public:
-	BOOL PreTranslateMessage(MSG* pMsg) override;
+	BOOL PreTranslateMessage(MSG *pMsg) override;
 	//}}AFX_VIRTUAL
 
 // Drag & Drop operations
@@ -248,11 +277,9 @@ public:
 	BOOL OnDrop(COleDataObject* pDataObject, DROPEFFECT dropEffect, CPoint point);
 
 protected:
-	int ModTreeInsLibCompareNamesGetItem(HTREEITEM item, CString &resultStr);
-	static int CALLBACK ModTreeInsLibCompareNamesProc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
 	static int CALLBACK ModTreeInsLibCompareProc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
 	static int CALLBACK ModTreeDrumCompareProc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort);
-	HTREEITEM InsertInsLibItem(const TCHAR *name, int image, const TCHAR *selectIfMatch);
+	int ImageToSortOrder(int image) const;
 	ModTreeDocInfo *GetDocumentInfoFromItem(HTREEITEM hItem);
 	CModDoc *GetDocumentFromItem(HTREEITEM hItem) { ModTreeDocInfo *info = GetDocumentInfoFromItem(hItem); return info ? &info->modDoc : nullptr; }
 	ModTreeDocInfo *GetDocumentInfoFromModDoc(CModDoc &modDoc);
@@ -267,7 +294,11 @@ protected:
 	static bool AllPluginsBypassed(const CSoundFile &sndFile, bool onlyEffects);
 	static void BypassAllPlugins(CSoundFile &sndFile, bool bypass, bool onlyEffects);
 
-// Generated message map functions
+	void FillInstrumentLibrary(const TCHAR *selectedItem = nullptr);
+	void FilterInstrumentLibrary(mpt::winstring filter, const TCHAR *selectedItem = nullptr);
+
+	HMENU AddLibraryFindAndSortMenus(HMENU hMenu) const;
+
 protected:
 	//{{AFX_MSG(CModTree)
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
@@ -283,7 +314,7 @@ protected:
 	afx_msg void OnItemLeftClick(LPNMHDR pNMHDR, LRESULT *pResult);
 	afx_msg void OnItemRightClick(LPNMHDR, LRESULT *pResult);
 	afx_msg void OnItemExpanded(LPNMHDR pnmhdr, LRESULT *pResult);
-	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnGetDispInfo(LPNMHDR pnmhdr, LRESULT *pResult);
 	afx_msg void OnRefreshTree();
 	afx_msg void OnExecuteItem();
 	afx_msg void OnPlayTreeItem();
@@ -301,6 +332,8 @@ protected:
 	afx_msg void OnBeginLabelEdit(NMHDR *nmhdr, LRESULT *result);
 	afx_msg void OnEndLabelEdit(NMHDR *nmhdr, LRESULT *result);
 	afx_msg void OnDropFiles(HDROP hDropInfo);
+	afx_msg void OnKillFocus(CWnd *pNewWnd);
+	afx_msg void OnSetFocus(CWnd *pOldWnd);
 
 	afx_msg void OnSetItemPath();
 	afx_msg void OnSaveItem();
@@ -321,13 +354,15 @@ protected:
 	afx_msg void OnGotoInstrumentDir();
 	afx_msg void OnGotoSampleDir();
 
+	afx_msg void OnOpenInstrumentLibraryFilter();
+	afx_msg void OnSortByName() { SetInstrumentLibraryFilterSortOrder(LibrarySortOrder::Name); }
+	afx_msg void OnSortByDate() { SetInstrumentLibraryFilterSortOrder(LibrarySortOrder::Date); }
+	afx_msg void OnSortBySize() { SetInstrumentLibraryFilterSortOrder(LibrarySortOrder::Size); }
+
 	afx_msg LRESULT OnCustomKeyMsg(WPARAM, LPARAM);
 	LRESULT OnMidiMsg(WPARAM midiData, LPARAM);
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
-public:
-	afx_msg void OnKillFocus(CWnd *pNewWnd);
-	afx_msg void OnSetFocus(CWnd *pOldWnd);
 };
 
 

commit 293501d23d0d022ad800e60cbdc0515526906742

[Fix] Tree view: Inserting samples/instruments into a new slot broke in r17825. [Mod] OpenMPT: Version is now 1.31.00.15
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17941 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/versionNumber.h
@@ -18,6 +18,6 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR  1
 #define VER_MAJOR      31
 #define VER_MINOR      00
-#define VER_MINORMINOR 14
+#define VER_MINORMINOR 15
 
 OPENMPT_NAMESPACE_END

mptrack/CommandSet.cpp
@@ -428,6 +428,7 @@ constexpr struct
 	{kcTreeViewDuplicate,                      VK_INSERT,          ModShift,           kKeyEventDown | kKeyEventRepeat, kCtxViewTree,            MPT_V("1.31.00.14")},
 	{kcTreeViewDelete,                         VK_DELETE,          ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxViewTree,            MPT_V("1.31.00.14")},
 	{kcTreeViewRename,                         VK_RETURN,          ModCtrl,            kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.14")},
+	{kcTreeViewSendToEditorInsertNew,          VK_RETURN,          ModShift | ModCtrl, kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.15")},
 	{kcTreeViewFind,                           'F',                ModCtrl,            kKeyEventDown,                   kCtxViewTree,            MPT_V("1.31.00.14")},
 	{kcVSTGUIPrevPreset,                       VK_SUBTRACT,        ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxVSTGUI,              MPT_V("1.31")},
 	{kcVSTGUINextPreset,                       VK_ADD,             ModNone,            kKeyEventDown | kKeyEventRepeat, kCtxVSTGUI,              MPT_V("1.31")},
@@ -1229,6 +1230,7 @@ static constexpr struct
 	{2041, kcTreeViewSortByName, _T("Sort Instrument Library By Name")},
 	{2042, kcTreeViewSortByDate, _T("Sort Instrument Library By Date")},
 	{2043, kcTreeViewSortBySize, _T("Sort Instrument Library By Size")},
+	{2044, kcTreeViewSendToEditorInsertNew, _T("Send To Editor (Insert New)")},
 };
 
 // Get command descriptions etc.. loaded up.

mptrack/CommandSet.h
@@ -821,6 +821,7 @@ enum CommandID
 	kcTreeViewDuplicate,
 	kcTreeViewDelete,
 	kcTreeViewRename,
+	kcTreeViewSendToEditorInsertNew,
 	kcTreeViewFind,
 	kcTreeViewSortByName,
 	kcTreeViewSortByDate,

mptrack/Mptrack.h
@@ -84,8 +84,16 @@ enum DragonDropType
 
 struct DRAGONDROP
 {
+	enum class InsertType
+	{
+		Unspecified,
+		Replace,
+		InsertNew
+	};
+
 	const CSoundFile *sndFile = nullptr;
 	DragonDropType dropType = DRAGONDROP_NOTHING;
+	InsertType insertType = InsertType::Unspecified;
 	uint32 dropItem = 0;
 	LPARAM dropParam = 0;
 

mptrack/View_ins.cpp
@@ -2110,7 +2110,10 @@ BOOL CViewInstrument::OnDragonDrop(BOOL doDrop, const DRAGONDROP *dropInfo)
 		break;
 	}
 	
-	const bool insertNew = CMainFrame::GetInputHandler()->ShiftPressed() && sndFile.GetNumInstruments() > 0;
+	bool insertNew = CMainFrame::GetInputHandler()->ShiftPressed() && sndFile.GetNumInstruments() > 0;
+	if(dropInfo->insertType != DRAGONDROP::InsertType::Unspecified)
+		insertNew = dropInfo->insertType == DRAGONDROP::InsertType::InsertNew && sndFile.GetNumInstruments() > 0;
+
 	if(insertNew && !sndFile.CanAddMoreInstruments())
 		canDrop = false;
 

mptrack/View_smp.cpp
@@ -3160,7 +3160,10 @@ BOOL CViewSample::OnDragonDrop(BOOL doDrop, const DRAGONDROP *dropInfo)
 		break;
 	}
 	
-	const bool insertNew = CMainFrame::GetInputHandler()->ShiftPressed();
+	bool insertNew = CMainFrame::GetInputHandler()->ShiftPressed();
+	if(dropInfo->insertType != DRAGONDROP::InsertType::Unspecified)
+		insertNew = dropInfo->insertType == DRAGONDROP::InsertType::InsertNew;
+
 	if(insertNew && !sndFile.CanAddMoreSamples())
 		canDrop = false;
 	

mptrack/View_tre.cpp
@@ -4191,14 +4191,14 @@ LRESULT CModTree::OnCustomKeyMsg(WPARAM wParam, LPARAM /*lParam*/)
 		return wParam;
 
 	case kcTreeViewRename:
+	case kcTreeViewSendToEditorInsertNew:
 		if(HTREEITEM hItem = GetSelectedItem(); hItem)
 		{
 			const ModItem modItem = GetModItem(hItem);
 			static constexpr ModItemType instrumentTypes[] = {MODITEM_INSLIB_SAMPLE, MODITEM_INSLIB_INSTRUMENT, MODITEM_MIDIINSTRUMENT, MODITEM_MIDIPERCUSSION, MODITEM_DLSBANK_INSTRUMENT};
 			if(mpt::contains(instrumentTypes, modItem.type))
 			{
-				// Load sample into currently selected sample or instrument slot
-				// Additionally pressing Shift creates a new sample or instrument slot. Shift key is handled in the Drag&drop handler
+				// Load sample into currently selected (or new) sample or instrument slot
 				CModScrollView *view = static_cast<CModScrollView *>(CMainFrame::GetMainFrame()->GetActiveView());
 				if(view)
 				{
@@ -4211,12 +4211,13 @@ LRESULT CModTree::OnCustomKeyMsg(WPARAM wParam, LPARAM /*lParam*/)
 					m_itemDrag = modItem;
 					if((isSampleView || isInstrumentView) && GetDropInfo(dropInfo, fullPath))
 					{
+						dropInfo.insertType = (wParam == kcTreeViewSendToEditorInsertNew) ? DRAGONDROP::InsertType::InsertNew : DRAGONDROP::InsertType::Replace;
 						view->SendMessage(WM_MOD_DRAGONDROPPING, TRUE, reinterpret_cast<LPARAM>(&dropInfo));
 						// In case a message box like "create instrument for sample?" showed up
 						SetFocus();
 					}
 				}
-			} else if(!IsSampleBrowser())
+			} else if(!IsSampleBrowser() && wParam != kcTreeViewSendToEditorInsertNew)
 			{
 				EditLabel(hItem);
 			}