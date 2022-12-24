e9590fad70a9e00c6fe1c46af4fb37cb90904a9a
Jörn Heusipp <manx@openmpt.org>
2022-12-24 15:11:38 +0000

    Merged revision(s) 18287 from trunk/OpenMPT: [Fix] mpt/parse: r14814 broke parsing of boolean values for libopenmpt ctls. Adapt mpt::parse to correctly parse integer numbers as boolean the same way ConvertStrTo did. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@18290 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- test/test.cpp
+++ test/test.cpp
@@ -653,6 +653,12 @@ static MPT_NOINLINE void TestStringFormatting()
 		VERIFY_EQUAL(mpt::cfmt::center(4, CString(_T("a"))), CString(_T(" a  ")));
 	#endif // MPT_WITH_MFC
 
+	VERIFY_EQUAL(ConvertStrTo<bool>("1"), true);
+	VERIFY_EQUAL(ConvertStrTo<bool>("0"), false);
+	VERIFY_EQUAL(ConvertStrTo<bool>("2"), true);
+	VERIFY_EQUAL(ConvertStrTo<bool>("-0"), false);
+	VERIFY_EQUAL(ConvertStrTo<bool>("-1"), true);
+
 	VERIFY_EQUAL(ConvertStrTo<uint32>("586"), 586u);
 	VERIFY_EQUAL(ConvertStrTo<uint32>("2147483647"), (uint32)int32_max);
 	VERIFY_EQUAL(ConvertStrTo<uint32>("4294967295"), uint32_max);
