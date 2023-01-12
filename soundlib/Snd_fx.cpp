00ddb0d22917cee654cdeced9d27b6fc69cd258c
Jörn Heusipp <manx@openmpt.org>
2023-01-12 08:15:13 +0000

    [Var] Fix typo in comment.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18498 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Snd_fx.cpp
+++ soundlib/Snd_fx.cpp
@@ -682,7 +682,7 @@ std::vector<GetLengthType> CSoundFile::GetLength(enmGetLengthResetMode adjustMod
 					{
 #if MPT_MSVC_BEFORE(2019, 0)
 						// Work-around for VS2017 /std:c++17 /permissive-
-						// which fails to find operator < for tempalted user types inside std::min.
+						// which fails to find operator < for templated user types inside std::min.
 						playState.m_nMusicTempo.SetRaw(std::min(tempo.GetRaw(), specs.GetTempoMax().GetRaw()));
 #else
 						playState.m_nMusicTempo = std::min(tempo, specs.GetTempoMax());
