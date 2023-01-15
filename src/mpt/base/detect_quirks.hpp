4bda170e7e7eedfec94fb6d031def4171b7dae93
Jörn Heusipp <manx@openmpt.org>
2023-01-15 18:50:11 +0000

    [Fix] mpt/base/detect_quirks.hpp: Also apply MPT_LIBCXX_QUIRK_CHRONO_TZ_MEMLEAK for VS2022.3 and VS2022.4. It's still not fixed.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18546 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/mpt/base/detect_quirks.hpp
+++ src/mpt/base/detect_quirks.hpp
@@ -249,7 +249,7 @@
 #elif MPT_LIBCXX_GNU
 #define MPT_LIBCXX_QUIRK_NO_CHRONO_DATE_PARSE
 #endif
-#if MPT_LIBCXX_MS && (MPT_MSVC_BEFORE(2022, 3) || !MPT_COMPILER_MSVC)
+#if MPT_LIBCXX_MS && (MPT_MSVC_BEFORE(2022, 5) || !MPT_COMPILER_MSVC)
 // Causes massive memory leaks.
 // See
 // <https://developercommunity.visualstudio.com/t/stdchronoget-tzdb-list-memory-leak/1644641>
