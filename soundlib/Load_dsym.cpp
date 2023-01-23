OpenMPT/openmpt

commit b01928d381b8b5abba6e4c37aa2470425746947d

[Fix] DSYM: Fix Undefined Behaviour due to reinterpret_cast to uint16le.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18263 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_dsym.cpp
@@ -258,14 +258,18 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 
 	const auto allowedCommands = file.ReadArray<uint8, 8>();
 
-	std::vector<std::byte> sequenceData;
-	if(fileHeader.numOrders)
+	std::vector<uint16le> sequence;
 	{
-		const uint32 sequenceSize = fileHeader.numOrders * fileHeader.numChannels * 2u;
-		if(!ReadDSymChunk(file, sequenceData, sequenceSize))
-			return false;
+		std::vector<std::byte> sequenceData;
+		if(fileHeader.numOrders)
+		{
+			const uint32 sequenceSize = fileHeader.numOrders * fileHeader.numChannels * 2u;
+			if(!ReadDSymChunk(file, sequenceData, sequenceSize))
+				return false;
+			FileReader sequenceChunk = FileReader(mpt::as_span(sequenceData));
+			sequenceChunk.ReadVector(sequenceData, sequenceData.size() / 2u);
+		}
 	}
-	const auto sequence = mpt::as_span(reinterpret_cast<uint16le *>(sequenceData.data()), sequenceData.size() / 2u);
 
 	std::vector<std::byte> trackData;
 	trackData.reserve(fileHeader.numTracks * 256u);

commit a7327c3647d8523246d142dd4ea47f359fb26579

[Fix] Loading any DSym files was broken since r18263.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18595 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_dsym.cpp
@@ -259,16 +259,14 @@ bool CSoundFile::ReadDSym(FileReader &file, ModLoadingFlags loadFlags)
 	const auto allowedCommands = file.ReadArray<uint8, 8>();
 
 	std::vector<uint16le> sequence;
+	if(fileHeader.numOrders)
 	{
 		std::vector<std::byte> sequenceData;
-		if(fileHeader.numOrders)
-		{
-			const uint32 sequenceSize = fileHeader.numOrders * fileHeader.numChannels * 2u;
-			if(!ReadDSymChunk(file, sequenceData, sequenceSize))
-				return false;
-			FileReader sequenceChunk = FileReader(mpt::as_span(sequenceData));
-			sequenceChunk.ReadVector(sequenceData, sequenceData.size() / 2u);
-		}
+		const uint32 sequenceSize = fileHeader.numOrders * fileHeader.numChannels * 2u;
+		if(!ReadDSymChunk(file, sequenceData, sequenceSize))
+			return false;
+		FileReader sequenceChunk = FileReader(mpt::as_span(sequenceData));
+		sequenceChunk.ReadVector(sequence, sequenceData.size() / 2u);
 	}
 
 	std::vector<std::byte> trackData;