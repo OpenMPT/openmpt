d9bdeaa8d590ba5a56a4ed23fba7f60ff8167a15
Jörn Heusipp <manx@openmpt.org>
2023-02-06 14:48:29 +0000

    [Fix] mpg123: Modernize Makefile port headers. This enables ACCURATE_ROUNDING, which is on by default in the CMake port.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18713 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/mpg123/ports/makefile/mpg123.h
+++ include/mpg123/ports/makefile/mpg123.h
@@ -1,18 +1,28 @@
+
 #ifndef MPG123_MAKEFILE_H
 #define MPG123_MAKEFILE_H
 
-#include <stdlib.h>
-#include <sys/types.h>
-
-#include <stdint.h>
 #include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
 
 #define MPG123_API_VERSION 47
 #define MPG123_NO_CONFIGURE
 #define MPG123_NO_LARGENAME
-#include <stddef.h>
-typedef ssize_t mpg123_ssize_t;
-#include "mpg123.h.in"
 
+#ifdef __cplusplus
+extern "C" {
 #endif
 
+typedef ssize_t mpg123_ssize_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "mpg123.h.in"
+
+#endif /* MPG123_MAKEFILE_H */
