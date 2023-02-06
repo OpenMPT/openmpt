b882f9d61d673c39b7d27eccfe19c7b2b762e6a4
Jörn Heusipp <manx@openmpt.org>
2023-02-06 15:01:09 +0000

    Merged revision(s) 18712-18714 from trunk/OpenMPT: [Fix] mpg123: Modernize MSVC port headers. This enables IEEE_FLOAT and ACCURATE_ROUNDING, which are on by default in the CMake port. ........ [Fix] mpg123: Modernize Makefile port headers. This enables ACCURATE_ROUNDING, which is on by default in the CMake port. ........ [Var] mpg123: Update OpenMPT.txt. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@18715 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/mpg123/ports/makefile/config.h
+++ include/mpg123/ports/makefile/config.h
@@ -1,44 +1,56 @@
 
-#define HAVE_STRERROR 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_LIMITS_H 1
+/* C99 headers */
 
-#define HAVE_STRDUP 1
-#define HAVE_STDLIB_H 1
-#define HAVE_STDINT_H 1
 #define HAVE_INTTYPES_H 1
+#define HAVE_LIMITS_H 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
 #define HAVE_STRING_H 1
 
-#define HAVE_DIRENT_H 1
+/* POSIX headers */
 
-#define HAVE_UNISTD_H 1
+#define HAVE_DIRENT_H 1
+#define HAVE_STRINGS_H 1
 #if !defined(__DJGPP__)
 #define HAVE_SYS_SIGNAL_H 1
 #endif
-#define HAVE_SYS_TIME_H 1
-#define HAVE_STRINGS_H 1
-#define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
 
-/*
-#ifdef HAVE_STDLIB_H
-#ifdef HAVE_SIGNAL_H
-#ifdef HAVE_SYS_SIGNAL_H
-#ifdef HAVE_UNISTD_H
-#ifdef HAVE_SYS_TYPES_H
-#ifdef HAVE_INTTYPES_H
-#ifdef HAVE_STDINT_H
-#ifdef HAVE_LIMITS_H
-#ifdef HAVE_STRING_H
-#ifdef HAVE_STRINGS_H
-#ifdef HAVE_SYS_TIME_H
-#ifdef HAVE_SYS_SELECT_H
-*/
+/* C99 functions */
 
+#define HAVE_ATOLL 1
+#define HAVE_STRERROR 1
+
+/* yeah, POSIX ... */
+
+#define OFF_MAX ((off_t)((sizeof(off_t) == 4) ? ((uint32_t)-1/2) : (sizeof(off_t) == 8) ? ((uint64_t)-1/2) : 0))
+
+/* Features */
+
+/* We want some frame index, eh? */
+#define FRAME_INDEX 1
+#define INDEX_SIZE 1000
+
+/* also gapless playback! */
 #define GAPLESS 1
 
-#define REAL_IS_FLOAT
+/* Debugging */
 
-#define inline __inline
+/* #define DEBUG */
+/* #define EXTRA_DEBUG */
 
-#define OFF_MAX ((off_t)((sizeof(off_t) <= 4) ? ((uint32_t)-1/2) : ((uint64_t)-1/2))) /* OpenMPT */
+/* Precision */
+
+/* use floating point */
+#define REAL_IS_FLOAT 1
+
+/* floating point is IEEE754 */
+/* #define IEEE_FLOAT 1 */
+
+/* use rounding instead of trunction */
+#define ACCURATE_ROUNDING 1
