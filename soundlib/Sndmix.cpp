OpenMPT/openmpt

commit 61eef3dfda09e99504c531f7c1919179f2c62de9

[Fix] IT: In sample mode, portamento to a different sample resets the channel filter if cutoff / resonance was previously 127 / 0. Test cases: FilterPortaSmpChange.it, FilterPortaSmpChange-InsMode.it [Mod] OpenMPT: Version is now 1.31.00.20
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18417 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndmix.cpp
@@ -2266,19 +2266,54 @@ bool CSoundFile::ReadNote()
 			chn.nRealPan = 128;
 		}
 
+		// Setup Initial Filter for this note
+		int cutoff = -1;
+		if(chn.triggerNote)
+		{
+			bool useFilter = !m_SongFlags[SONG_MPTFILTERMODE];
+			if(pIns)
+			{
+				if(pIns->IsResonanceEnabled())
+				{
+					chn.nResonance = pIns->GetResonance();
+					useFilter = true;
+				}
+				if(pIns->IsCutoffEnabled())
+				{
+					chn.nCutOff = pIns->GetCutoff();
+					useFilter = true;
+				}
+				if(useFilter && (pIns->filterMode != FilterMode::Unchanged))
+				{
+					chn.nFilterMode = pIns->filterMode;
+				}
+			} else
+			{
+				chn.nVolSwing = chn.nPanSwing = 0;
+				chn.nCutSwing = chn.nResSwing = 0;
+			}
+			if((chn.nCutOff < 0x7F || m_playBehaviour[kITFilterBehaviour]) && useFilter)
+			{
+				cutoff = SetupChannelFilter(chn, true);
+				if(cutoff >= 0)
+					cutoff = chn.nCutOff / 2u;
+			}
+		}
+
 		// Now that all relevant envelopes etc. have been processed, we can parse the MIDI macro data.
 		ProcessMacroOnChannel(nChn);
 
 		// After MIDI macros have been processed, we can also process the pitch / filter envelope and other pitch-related things.
 		if(samplePlaying)
 		{
-			int cutoff = ProcessPitchFilterEnvelope(chn, period);
-			if(cutoff >= 0 && chn.dwFlags[CHN_ADLIB] && m_opl)
-			{
-				// Cutoff doubles as modulator intensity for FM instruments
-				m_opl->Volume(nChn, static_cast<uint8>(cutoff / 4), true);
-			}
+			cutoff = ProcessPitchFilterEnvelope(chn, period);
+			if(cutoff >= 0)
+				cutoff /= 4;
 		}
+
+		// Cutoff doubles as modulator intensity for FM instruments
+		if(cutoff >= 0 && chn.dwFlags[CHN_ADLIB] && m_opl)
+			m_opl->Volume(nChn, static_cast<uint8>(cutoff), true);
 
 		if(chn.rowCommand.volcmd == VOLCMD_VIBRATODEPTH &&
 			(chn.rowCommand.command == CMD_VIBRATO || chn.rowCommand.command == CMD_VIBRATOVOL || chn.rowCommand.command == CMD_FINEVIBRATO))

common/versionNumber.h
@@ -18,6 +18,6 @@ OPENMPT_NAMESPACE_BEGIN
 #define VER_MAJORMAJOR  1
 #define VER_MAJOR      31
 #define VER_MINOR      00
-#define VER_MINORMINOR 19
+#define VER_MINORMINOR 20
 
 OPENMPT_NAMESPACE_END

mptrack/dlg_misc.cpp
@@ -702,6 +702,7 @@ void CLegacyPlaybackSettingsDlg::OnFilterStringChanged()
 				desc = _T("Ignore tone portamento for instrument plugins");
 			break;
 		case kST3TonePortaWithAdlibNote: desc = _T("Adlib Notes with Tone Portamento are delayed until the next row"); break;
+		case kITResetFilterOnPortaSmpChange: desc = _T("Reset filter on portamento if new note plays a different sample"); break;
 
 		default: MPT_ASSERT_NOTREACHED();
 		}

soundlib/Snd_defs.h
@@ -549,6 +549,7 @@ enum PlayBehaviour
 	kImprecisePingPongLoops,        // Use old (less precise) ping-pong overshoot calculation
 	kPluginIgnoreTonePortamento,    // Use old tone portamento behaviour for plugins (XM: no plugin pitch slides with commands E1x/E2x/X1x/X2x)
 	kST3TonePortaWithAdlibNote,     // Adlib note next to tone portamento is delayed until next row
+	kITResetFilterOnPortaSmpChange, // Filter is reset on portamento if sample is swapped
 
 	// Add new play behaviours here.
 

soundlib/Snd_flt.cpp
@@ -112,10 +112,10 @@ int CSoundFile::SetupChannelFilter(ModChannel &chn, bool bReset, int envModifier
 	// Filtering is only ever done in IT if either cutoff is not full or if resonance is set.
 	if(m_playBehaviour[kITFilterBehaviour] && resonance == 0 && computedCutoff >= 254)
 	{
-		if(chn.rowCommand.IsNote() && !chn.rowCommand.IsPortamento() && !chn.nMasterChn && chn.triggerNote)
+		if(chn.triggerNote)
 		{
 			// Z7F next to a note disables the filter, however in other cases this should not happen.
-			// Test cases: filter-reset.it, filter-reset-carry.it, filter-reset-envelope.it, filter-nna.it, FilterResetPatDelay.it
+			// Test cases: filter-reset.it, filter-reset-carry.it, filter-reset-envelope.it, filter-nna.it, FilterResetPatDelay.it, FilterPortaSmpChange.it, FilterPortaSmpChange-InsMode.it
 			chn.dwFlags.reset(CHN_FILTER);
 		}
 		return -1;

soundlib/Snd_fx.cpp
@@ -1422,6 +1422,10 @@ void CSoundFile::InstrumentChange(ModChannel &chn, uint32 instr, bool bPorta, bo
 			// but still uses the sample info from the old one (bug?)
 			returnAfterVolumeAdjust = true;
 		}
+		// IT compatbility: Reset filter if portamento results in sample change
+		// Test case: FilterPortaSmpChange.it, FilterPortaSmpChange-InsMode.it
+		if(m_playBehaviour[kITResetFilterOnPortaSmpChange] && !m_nInstruments)
+			chn.triggerNote = true;
 	}
 	// IT compatibility: A lone instrument number should only reset sample properties to those of the corresponding sample in instrument mode.
 	// C#5 01 ... <-- sample 1
@@ -2024,6 +2028,7 @@ void CSoundFile::NoteChange(ModChannel &chn, int note, bool bPorta, bool bResetE
 	// Enable Ramping
 	if(!bPorta)
 	{
+		chn.triggerNote = true;
 		chn.nLeftVU = chn.nRightVU = 0xFF;
 		chn.dwFlags.reset(CHN_FILTER);
 		chn.dwFlags.set(CHN_FASTVOLRAMP);
@@ -2045,35 +2050,6 @@ void CSoundFile::NoteChange(ModChannel &chn, int note, bool bPorta, bool bResetE
 			chn.nAutoVibPos = 0;
 		}
 		chn.rightVol = chn.leftVol = 0;
-		bool useFilter = !m_SongFlags[SONG_MPTFILTERMODE];
-		// Setup Initial Filter for this note
-		if(pIns)
-		{
-			if(pIns->IsResonanceEnabled())
-			{
-				chn.nResonance = pIns->GetResonance();
-				useFilter = true;
-			}
-			if(pIns->IsCutoffEnabled())
-			{
-				chn.nCutOff = pIns->GetCutoff();
-				useFilter = true;
-			}
-			if(useFilter && (pIns->filterMode != FilterMode::Unchanged))
-			{
-				chn.nFilterMode = pIns->filterMode;
-			}
-		} else
-		{
-			chn.nVolSwing = chn.nPanSwing = 0;
-			chn.nCutSwing = chn.nResSwing = 0;
-		}
-		if((chn.nCutOff < 0x7F || m_playBehaviour[kITFilterBehaviour]) && useFilter)
-		{
-			int cutoff = SetupChannelFilter(chn, true);
-			if(cutoff >= 0 && chn.dwFlags[CHN_ADLIB] && m_opl && channelHint != CHANNELINDEX_INVALID)
-				m_opl->Volume(channelHint, chn.nCutOff / 2u, true);
-		}
 
 		if(chn.dwFlags[CHN_ADLIB] && m_opl && channelHint != CHANNELINDEX_INVALID)
 		{
@@ -2637,7 +2613,7 @@ bool CSoundFile::ProcessEffects()
 		{
 			chn.isFirstTick = tickCount == nStartTick;
 		}
-		chn.triggerNote = triggerNote;
+		chn.triggerNote = false;
 
 		// FT2 compatibility: Note + portamento + note delay = no portamento
 		// Test case: PortaDelay.xm

soundlib/Sndfile.cpp
@@ -1151,6 +1151,7 @@ PlayBehaviourSet CSoundFile::GetSupportedPlaybackBehaviour(MODTYPE type)
 		playBehaviour.set(kITDoNotOverrideChannelPan);
 		playBehaviour.set(kITDCTBehaviour);
 		playBehaviour.set(kITPitchPanSeparation);
+		playBehaviour.set(kITResetFilterOnPortaSmpChange);
 		if(type == MOD_TYPE_MPT)
 		{
 			playBehaviour.set(kOPLFlexibleNoteOff);

soundlib/UpgradeModule.cpp
@@ -577,6 +577,7 @@ void CSoundFile::UpgradeModule()
 			{ kITPatternLoopWithJumps,        MPT_V("1.29.00.32") },
 			{ kITDCTBehaviour,                MPT_V("1.29.00.57") },
 			{ kITPitchPanSeparation,          MPT_V("1.30.00.53") },
+			{ kITResetFilterOnPortaSmpChange, MPT_V("1.31.00.20") },
 		};
 
 		for(const auto &b : behaviours)

commit d399e275caf6432cd1e6f76e40c1740e16a083c3

Merged revision(s) 18782 from trunk/OpenMPT: [Fix] OPL cutoff was still broken (introduced in r18417), it needs to be applied in two steps (https://bugs.openmpt.org/view.php?id=1671).
........


git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@18783 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Sndmix.cpp
@@ -2289,7 +2289,8 @@ bool CSoundFile::ReadNote()
 		}
 
 		// Setup Initial Filter for this note
-		int cutoff = HandleNoteChangeFilter(chn);
+		if(int cutoff = HandleNoteChangeFilter(chn); cutoff >= 0 && chn.dwFlags[CHN_ADLIB] && m_opl)
+			m_opl->Volume(nChn, static_cast<uint8>(cutoff), true);
 
 		// Now that all relevant envelopes etc. have been processed, we can parse the MIDI macro data.
 		ProcessMacroOnChannel(nChn);
@@ -2298,13 +2299,10 @@ bool CSoundFile::ReadNote()
 		if(samplePlaying)
 		{
 			int envCutoff = ProcessPitchFilterEnvelope(chn, period);
-			if(envCutoff >= 0)
-				cutoff = envCutoff / 4;
+			// Cutoff doubles as modulator intensity for FM instruments
+			if(envCutoff >= 0 && chn.dwFlags[CHN_ADLIB] && m_opl)
+				m_opl->Volume(nChn, static_cast<uint8>(envCutoff / 4), true);
 		}
-
-		// Cutoff doubles as modulator intensity for FM instruments
-		if(cutoff >= 0 && chn.dwFlags[CHN_ADLIB] && m_opl)
-			m_opl->Volume(nChn, static_cast<uint8>(cutoff), true);
 
 		if(chn.rowCommand.volcmd == VOLCMD_VIBRATODEPTH &&
 			(chn.rowCommand.command == CMD_VIBRATO || chn.rowCommand.command == CMD_VIBRATOVOL || chn.rowCommand.command == CMD_FINEVIBRATO))