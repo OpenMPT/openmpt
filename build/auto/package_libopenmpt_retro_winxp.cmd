71a79e776357df7ee663692b065e31c11b4d8130
Jörn Heusipp <manx@openmpt.org>
2023-03-15 14:04:28 +0000

    [Imp] build: libopenmpt: Also build and package retro-winxp development DLLs.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18849 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/auto/package_libopenmpt_retro_winxp.cmd
+++ build/auto/package_libopenmpt_retro_winxp.cmd
@@ -105,6 +105,144 @@ del /f /q libopenmpt\bin.retro.winxp\%LIBOPENMPT_VERSION_STRING%\libopenmpt-%MPT
 rmdir /s /q libopenmpt
 cd .. || goto error
 
+cd bin || goto error
+rmdir /s /q libopenmpt
+mkdir libopenmpt || goto error
+mkdir libopenmpt\dev.windows.retro.winxp
+mkdir libopenmpt\dev.windows.retro.winxp\%LIBOPENMPT_VERSION_STRING%
+rmdir /s /q libopenmpt-dev-retro-winxp
+del /f /q libopenmpt-dev-retro-winxp.tar
+del /f /q libopenmpt-%MPT_REVISION%.dev.windows.retro.winxp.%MPT_PKG_FORMAT%
+mkdir libopenmpt-dev-retro-winxp
+cd libopenmpt-dev-retro-winxp || goto error
+mkdir inc
+mkdir inc\libopenmpt
+mkdir lib
+mkdir lib\x86-retro-winxp
+mkdir lib\amd64-retro-winxp
+mkdir bin
+mkdir bin\x86-retro-winxp
+mkdir bin\amd64-retro-winxp
+copy /y ..\..\LICENSE .\LICENSE.txt || goto error
+rmdir /s /q Licenses
+mkdir Licenses
+copy /y ..\..\src\mpt\LICENSE.BSD-3-Clause.txt               .\Licenses\License.mpt.BSD-3-Clause.txt || goto error
+copy /y ..\..\src\mpt\LICENSE.BSL-1.0.txt                    .\Licenses\License.mpt.BSL-1.0.txt || goto error
+rem copy /y ..\..\include\ancient\LICENSE                        .\Licenses\License.ancient.txt || goto error
+rem copy /y ..\..\include\ancient\src\BZIP2Table.hpp             .\Licenses\License.ancient.bzip2.txt || goto error
+rem copy /y ..\..\include\flac\COPYING.Xiph                      .\Licenses\License.FLAC.txt || goto error
+rem copy /y ..\..\include\lame\COPYING                           .\Licenses\License.lame.txt || goto error
+rem copy /y ..\..\include\lhasa\COPYING                          .\Licenses\License.lhasa.txt || goto error
+rem copy /y ..\..\include\minimp3\LICENSE                        .\Licenses\License.minimp3.txt || goto error
+rem copy /y ..\..\include\miniz\LICENSE                          .\Licenses\License.miniz.txt || goto error
+copy /y ..\..\include\mpg123\COPYING                         .\Licenses\License.mpg123.txt || goto error
+copy /y ..\..\include\mpg123\AUTHORS                         .\Licenses\License.mpg123.Authors.txt || goto error
+rem copy /y ..\..\include\nlohmann-json\LICENSE.MIT              .\Licenses\License.nlohmann-json.txt || goto error
+copy /y ..\..\include\ogg\COPYING                            .\Licenses\License.ogg.txt || goto error
+rem copy /y ..\..\include\opus\COPYING                           .\Licenses\License.Opus.txt || goto error
+rem copy /y ..\..\include\opusenc\COPYING                        .\Licenses\License.Opusenc.txt || goto error
+rem copy /y ..\..\include\opusenc\AUTHORS                        .\Licenses\License.Opusenc.Authors.txt || goto error
+rem copy /y ..\..\include\opusfile\COPYING                       .\Licenses\License.Opusfile.txt || goto error
+rem copy /y ..\..\include\portaudio\LICENSE.txt                  .\Licenses\License.PortAudio.txt || goto error
+rem copy /y ..\..\include\portaudio\bindings\cpp\COPYING         .\Licenses\License.portaudiocpp.txt || goto error
+rem copy /y ..\..\include\pugixml\LICENSE.md                     .\Licenses\License.PugiXML.txt || goto error
+rem copy /y ..\..\include\r8brain\LICENSE                        .\Licenses\License.R8Brain.txt || goto error
+rem copy /y ..\..\include\rtaudio\README.md                      .\Licenses\License.RtAudio.txt || goto error
+rem copy /y ..\..\include\rtmidi\LICENSE                         .\Licenses\License.RtMidi.txt || goto error
+rem copy /y ..\..\include\smbPitchShift\smbPitchShift.cpp        .\Licenses\License.smbPitchShift.txt || goto error
+rem copy /y ..\..\include\soundtouch\COPYING.TXT                 .\Licenses\License.SoundTouch.txt || goto error
+rem copy /y ..\..\include\stb_vorbis\stb_vorbis.c                .\Licenses\License.stb_vorbis.txt || goto error
+rem copy /y ..\..\include\unrar\license.txt                      .\Licenses\License.UnRAR.txt || goto error
+copy /y ..\..\include\vorbis\COPYING                         .\Licenses\License.Vorbis.txt || goto error
+copy /y ..\..\include\zlib\README                            .\Licenses\License.zlib.txt || goto error
+rem copy /y ..\..\include\zlib\contrib\minizip\MiniZip64_info.txt .\Licenses\License.minizip.txt || goto error
+copy /y ..\..\doc\libopenmpt\changelog.md .\changelog.md || goto error
+copy /y ..\..\libopenmpt\libopenmpt.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt.hpp inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_config.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_version.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_ext.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_ext.hpp inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_buffer.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_fd.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_file.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_file_mingw.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_file_msvcrt.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_file_posix.h inc\libopenmpt\ || goto error
+copy /y ..\..\libopenmpt\libopenmpt_stream_callbacks_file_posix_lfs64.h inc\libopenmpt\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\x86\libopenmpt.lib lib\x86-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\x86\libopenmpt.dll bin\x86-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\x86\openmpt-mpg123.dll bin\x86-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\x86\openmpt-ogg.dll bin\x86-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\x86\openmpt-vorbis.dll bin\x86-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\x86\openmpt-zlib.dll bin\x86-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\amd64\libopenmpt.lib lib\amd64-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\amd64\libopenmpt.dll bin\amd64-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\amd64\openmpt-mpg123.dll bin\amd64-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\amd64\openmpt-ogg.dll bin\amd64-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\amd64\openmpt-vorbis.dll bin\amd64-retro-winxp\ || goto error
+copy /y ..\..\bin\release\vs2017-winxp-shared\amd64\openmpt-zlib.dll bin\amd64-retro-winxp\ || goto error
+..\..\build\tools\7zip\7z.exe a -t%MPT_PKG_FORMAT% -mx=9 ..\libopenmpt\dev.windows.retro.winxp\%LIBOPENMPT_VERSION_STRING%\libopenmpt-%MPT_REVISION%.dev.windows.retro.winxp.%MPT_PKG_FORMAT% ^
+ LICENSE.txt ^
+ Licenses ^
+ changelog.md ^
+ inc\libopenmpt\libopenmpt.h ^
+ inc\libopenmpt\libopenmpt.hpp ^
+ inc\libopenmpt\libopenmpt_config.h ^
+ inc\libopenmpt\libopenmpt_version.h ^
+ inc\libopenmpt\libopenmpt_ext.h ^
+ inc\libopenmpt\libopenmpt_ext.hpp ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_buffer.h ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_fd.h ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_file.h ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_file_mingw.h ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_file_msvcrt.h ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_file_posix.h ^
+ inc\libopenmpt\libopenmpt_stream_callbacks_file_posix_lfs64.h ^
+ lib\x86\libopenmpt.lib ^
+ lib\amd64\libopenmpt.lib ^
+ lib\arm\libopenmpt.lib ^
+ lib\arm64\libopenmpt.lib ^
+ lib\x86-legacy\libopenmpt.lib ^
+ lib\amd64-legacy\libopenmpt.lib ^
+ bin\x86\libopenmpt.dll ^
+ bin\x86\openmpt-mpg123.dll ^
+ bin\x86\openmpt-ogg.dll ^
+ bin\x86\openmpt-vorbis.dll ^
+ bin\x86\openmpt-zlib.dll ^
+ bin\amd64\libopenmpt.dll ^
+ bin\amd64\openmpt-mpg123.dll ^
+ bin\amd64\openmpt-ogg.dll ^
+ bin\amd64\openmpt-vorbis.dll ^
+ bin\amd64\openmpt-zlib.dll ^
+ bin\arm\libopenmpt.dll ^
+ bin\arm\openmpt-mpg123.dll ^
+ bin\arm\openmpt-ogg.dll ^
+ bin\arm\openmpt-vorbis.dll ^
+ bin\arm\openmpt-zlib.dll ^
+ bin\arm64\libopenmpt.dll ^
+ bin\arm64\openmpt-mpg123.dll ^
+ bin\arm64\openmpt-ogg.dll ^
+ bin\arm64\openmpt-vorbis.dll ^
+ bin\arm64\openmpt-zlib.dll ^
+ bin\x86-legacy\libopenmpt.dll ^
+ bin\x86-legacy\openmpt-mpg123.dll ^
+ bin\x86-legacy\openmpt-ogg.dll ^
+ bin\x86-legacy\openmpt-vorbis.dll ^
+ bin\x86-legacy\openmpt-zlib.dll ^
+ bin\amd64-legacy\libopenmpt.dll ^
+ bin\amd64-legacy\openmpt-mpg123.dll ^
+ bin\amd64-legacy\openmpt-ogg.dll ^
+ bin\amd64-legacy\openmpt-vorbis.dll ^
+ bin\amd64-legacy\openmpt-zlib.dll ^
+ || goto error
+cd .. || goto error
+rmdir /s /q libopenmpt-dev-retro-winxp
+..\build\tools\7zip\7z.exe a -r -ttar libopenmpt-dev-retro-winxp.tar libopenmpt || goto error
+del /f /q libopenmpt\dev.windows.retro.winxp\%LIBOPENMPT_VERSION_STRING%\libopenmpt-%MPT_REVISION%.dev.windows.retro.winxp.%MPT_PKG_FORMAT%
+rmdir /s /q libopenmpt
+cd .. || goto error
+
 
 
 goto noerror
