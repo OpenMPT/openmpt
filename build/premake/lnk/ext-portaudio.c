3b791eb58fa305011c51b5c03061f3c36c0ec1da
Jörn Heusipp <manx@openmpt.org>
2023-03-15 16:49:37 +0000

    [Fix] build: PortAudio: PortAudio WASAPI does not actually require linking mmdevapi.lib. Fixes RETRO-WinXP builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18851 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake/lnk/ext-portaudio.c
+++ build/premake/lnk/ext-portaudio.c
@@ -1,7 +1,6 @@
 #pragma comment(lib, "advapi32.lib")
 #pragma comment(lib, "ksuser.lib")
 #if (PA_USE_WASAPI == 1)
-#pragma comment(lib, "mmdevapi.lib")
 #pragma comment(lib, "ole32.lib")
 #endif
 #pragma comment(lib, "winmm.lib")
