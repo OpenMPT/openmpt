1a7ca6805e0d9882ad5a12b023a043dd271d50d3
Jörn Heusipp <manx@openmpt.org>
2023-04-22 06:48:13 +0000

    [Var] libopenmpt: Update release script version numbers.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@19071 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/svn/do_libopenmpt_release_rc.sh
+++ build/svn/do_libopenmpt_release_rc.sh
@@ -6,8 +6,8 @@ svn up
 
 NEWVER=$(make distversion-pure)
 NEWREV=$(svn info --xml . | xpath -e '/info/entry/commit/@revision' -q | sed 's/revision//g' | tr '"' ' ' | tr '=' ' ' | sed 's/ //g')
-svn cp -m "tag libopenmpt-${NEWVER}" -r ${NEWREV} https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30 https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER}
+svn cp -m "tag libopenmpt-${NEWVER}" -r ${NEWREV} https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.32 https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER}
 TAGREV=$(svn info --xml https://source.openmpt.org/svn/openmpt/tags/libopenmpt-${NEWVER} | xpath -e '/info/entry/commit/@revision' -q | sed 's/revision//g' | tr '"' ' ' | tr '=' ' ' | sed 's/ //g')
 
 echo "ALL DONE."
-echo "run './release-0.6.sh $NEWVER +r${TAGREV}' in a website checkout after buildbot has finished."
+echo "run './release-0.8.sh $NEWVER +r${TAGREV}' in a website checkout after buildbot has finished."
