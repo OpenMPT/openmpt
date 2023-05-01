OpenMPT/openmpt

commit 206aaf7c4ef6798a8abd5ad8b61762c8dede53df

[Ref] build: Remove build/svn_version/ from include path.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@17796 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/premake/mpt-in_openmpt.lua
@@ -17,7 +17,6 @@
   includedirs {
    "../..",
    "$(IntDir)/svn_version",
-   "../../build/svn_version",
   }
   files {
    "../../libopenmpt/in_openmpt.cpp",
@@ -36,8 +35,6 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
-			"../../build/svn_version",
-			"$(ProjDir)/../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-libopenmpt-small.lua
@@ -17,7 +17,6 @@
    "../../src",
    "../../common",
    "$(IntDir)/svn_version",
-   "../../build/svn_version",
   }
   files {
    "../../src/mpt/**.cpp",
@@ -83,8 +82,6 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
-			"../../build/svn_version",
-			"$(ProjDir)/../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-libopenmpt.lua
@@ -21,7 +21,6 @@
    "../../src",
    "../../common",
    "$(IntDir)/svn_version",
-   "../../build/svn_version",
   }
   files {
    "../../src/mpt/**.cpp",
@@ -83,8 +82,6 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
-			"../../build/svn_version",
-			"$(ProjDir)/../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-openmpt123.lua
@@ -20,7 +20,6 @@
    "../../src",
    "../../openmpt123",
    "$(IntDir)/svn_version",
-   "../../build/svn_version",
   }
   files {
    "../../src/mpt/base/*.hpp",
@@ -38,8 +37,6 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
-			"../../build/svn_version",
-			"$(ProjDir)/../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-xmp-openmpt.lua
@@ -18,7 +18,6 @@
   includedirs {
    "../..",
    "$(IntDir)/svn_version",
-   "../../build/svn_version",
   }
   files {
    "../../libopenmpt/xmp-openmpt.cpp",
@@ -37,8 +36,6 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
-			"../../build/svn_version",
-			"$(ProjDir)/../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake-xcode/mpt-libopenmpt.lua
@@ -21,7 +21,6 @@
    "../../common",
    "../../soundlib",
    "$(IntDir)/svn_version",
-   "../../build/svn_version",
   }
   files {
    "../../src/mpt/**.cpp",

commit 82c7118dffbe918d6516544f1b54e64444d47815

[Fix] build: Fix building libopenmpt DLLs/EXEs from MSVC source package. Broken since r17796.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@19173 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/premake/mpt-in_openmpt.lua
@@ -30,6 +30,7 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
+			"../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-libopenmpt-small.lua
@@ -85,6 +85,7 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
+			"../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-libopenmpt.lua
@@ -70,6 +70,7 @@
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",
+			"../../build/svn_version",
 		}
 	filter { "action:vs*", "kind:SharedLib" }
 		resdefines { "MPT_BUILD_VER_DLL" }

build/premake/mpt-openmpt123.lua
@@ -48,6 +48,7 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
+			"../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",

build/premake/mpt-xmp-openmpt.lua
@@ -32,6 +32,7 @@
 	filter { "action:vs*", "kind:SharedLib or ConsoleApp or WindowedApp" }
 		resincludedirs {
 			"$(IntDir)/svn_version",
+			"../../build/svn_version",
 		}
 		files {
 			"../../libopenmpt/libopenmpt_version.rc",