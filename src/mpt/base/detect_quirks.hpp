b20527d604f70c9dc28acc6afb0f13f8cdabad71
Jörn Heusipp <manx@openmpt.org>
2023-05-26 07:37:04 +0000

    Merged revision(s) 19354 from trunk/OpenMPT: [Fix] mptTime: Use a better work-around for <https://developercommunity.visualstudio.com/t/INTERNAL-COMPILER-ERROR-when-compiling-s/10366948> which allows us to still use std::chrono. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@19355 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/mpt/base/detect_quirks.hpp
+++ src/mpt/base/detect_quirks.hpp
@@ -270,9 +270,7 @@
 
 #if MPT_MSVC_AT_LEAST(2022, 6) && MPT_MSVC_BEFORE(2022, 7)
 // std::chrono triggers ICE in VS2022 17.6.0, see <https://developercommunity.visualstudio.com/t/INTERNAL-COMPILER-ERROR-when-compiling-s/10366948>.
-#ifndef MPT_LIBCXX_QUIRK_NO_CHRONO_DATE
-#define MPT_LIBCXX_QUIRK_NO_CHRONO_DATE
-#endif
+#define MPT_LIBCXX_QUIRK_CHRONO_DATE_BROKEN_ZONED_TIME
 #endif
 
 
