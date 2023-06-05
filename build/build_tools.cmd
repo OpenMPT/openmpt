0611e7fbc6c7dc6806ca8b2ec8d989cebda54a0f
Jörn Heusipp <manx@openmpt.org>
2023-06-05 14:41:57 +0000

    [Fix] build: Support building premake with VS2022.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@19376 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/build_tools.cmd
+++ build/build_tools.cmd
@@ -24,6 +24,19 @@ goto main
 
 
 
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call build\auto\setup_vs2022.cmd || goto error
+ rem cd include\premake || goto error
+ rem  nmake -f Bootstrap.mak windows MSDEV=vs2022 || goto error
+ rem  bin\release\premake5 embed --bytecode || goto error
+ rem  bin\release\premake5 --to=build/vs2022 vs2022 --no-curl --no-zlib --no-luasocket || goto error
+ rem cd ..\.. || goto error
+ cd include\premake\build\vs2022 || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+ cd ..\..\..\.. || goto error
+ goto premakedone
+)
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call build\auto\setup_vs2019.cmd || goto error
  rem cd include\premake || goto error
