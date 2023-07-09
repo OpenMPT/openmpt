OpenMPT/openmpt

commit e3a4fbdb09234f8510a4a1c705b15963750af2ab

Merged revision(s) 19376-19377 from trunk/OpenMPT: [Fix] build: Support building premake with VS2022. ........ [Var] Update .gitignore. ........
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@19381 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/build_tools.cmd
@@ -24,6 +24,19 @@ goto main
 
 
 
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call build\auto\setup_vs2022.cmd || goto error
+ rem cd include\premake || goto error
+ rem  nmake -f Bootstrap.mak windows MSDEV=vs2022 || goto error
+ rem  bin\release\premake5 embed --bytecode || goto error
+ rem  bin\release\premake5 --to=build/vs2022 vs2022 --no-curl --no-zlib --no-luasocket || goto error
+ rem cd ..\.. || goto error
+ cd include\premake\build\vs2022 || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+ cd ..\..\..\.. || goto error
+ goto premakedone
+)
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call build\auto\setup_vs2019.cmd || goto error
  cd include\genie\build\vs2019 || goto error

.gitignore
@@ -677,6 +677,8 @@
 /include/premake/build/vs2019/.vs
 /include/premake/build/vs2019/bin
 /include/premake/build/vs2019/obj
+/include/premake/build/vs2022/bin
+/include/premake/build/vs2022/obj
 /include/pugixml/CMakeLists.txt
 /include/pugixml/docs
 /include/pugixml/scripts

commit 6eba2998898aa6d98dc90dd97ad15389c6b42bfc

[Fix] build: Genie: Repair building genie on VS2022-only systems. Broken since r19381. [Fix] build: Premake: Repair building premake. Broken since r19382.
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@19446 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/build_tools.cmd
@@ -26,16 +26,13 @@ goto main
 
 if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call build\auto\setup_vs2022.cmd || goto error
- rem cd include\premake || goto error
- rem  nmake -f Bootstrap.mak windows MSDEV=vs2022 || goto error
- rem  bin\release\premake5 embed --bytecode || goto error
- rem  bin\release\premake5 --to=build/vs2022 vs2022 --no-curl --no-zlib --no-luasocket || goto error
- rem cd ..\.. || goto error
- cd include\premake\build\vs2022 || goto error
-  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
-  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
+ cd include\genie\build\vs2019 || goto error
+ devenv genie.sln /Upgrade || goto error
+ msbuild genie.sln /target:Build /property:Configuration=Release;Platform=Win32;WindowsTargetPlatformVersion=10.0 /maxcpucount /verbosity:minimal || goto error
+ svn revert genie.sln || goto error
+ svn revert genie.vcxproj || goto error
  cd ..\..\..\.. || goto error
- goto premakedone
+ goto geniedone
 )
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call build\auto\setup_vs2019.cmd || goto error
@@ -72,6 +69,19 @@ copy /y include\genie\OpenMPT.txt include\genie\OpenMPT-version.txt
 
 
 
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+ call build\auto\setup_vs2022.cmd || goto error
+ rem cd include\premake || goto error
+ rem  nmake -f Bootstrap.mak windows MSDEV=vs2022 || goto error
+ rem  bin\release\premake5 embed --bytecode || goto error
+ rem  bin\release\premake5 --to=build/vs2022 vs2022 --no-curl --no-zlib --no-luasocket || goto error
+ rem cd ..\.. || goto error
+ cd include\premake\build\vs2022 || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+ cd ..\..\..\.. || goto error
+ goto premakedone
+)
 if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
  call build\auto\setup_vs2019.cmd || goto error
  rem cd include\premake || goto error
@@ -80,8 +90,8 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxil
  rem  bin\release\premake5 --to=build/vs2019 vs2019 --no-curl --no-zlib --no-luasocket || goto error
  rem cd ..\.. || goto error
  cd include\premake\build\vs2019 || goto error
-  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
-  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
  cd ..\..\..\.. || goto error
  goto premakedone
 )
@@ -93,8 +103,8 @@ if exist "C:\Program Files\Microsoft Visual Studio\2019\Community\VC\Auxiliary\B
  rem  bin\release\premake5 --to=build/vs2019 vs2019 --no-curl --no-zlib --no-luasocket || goto error
  rem cd ..\.. || goto error
  cd include\premake\build\vs2019 || goto error
-  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
-  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
  cd ..\..\..\.. || goto error
  goto premakedone
 )
@@ -106,8 +116,8 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxil
  rem  bin\release\premake5 --to=build/vs2017 vs2017 --no-curl --no-zlib --no-luasocket || goto error
  rem cd ..\.. || goto error
  cd include\premake\build\vs2017 || goto error
-  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
-  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
  cd ..\..\..\.. || goto error
  goto premakedone
 )
@@ -119,8 +129,8 @@ if exist "C:\Program Files\Microsoft Visual Studio\2017\Community\VC\Auxiliary\B
  rem  bin\release\premake5 --to=build/vs2017 vs2017 --no-curl --no-zlib --no-luasocket || goto error
  rem cd ..\.. || goto error
  cd include\premake\build\vs2017 || goto error
-  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
-  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=x64 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Clean /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
+  msbuild Premake5.sln /target:Build /property:Configuration=Release;Platform=Win32 /maxcpucount /verbosity:minimal || goto error
  cd ..\..\..\.. || goto error
  goto premakedone
 )