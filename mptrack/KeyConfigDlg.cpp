OpenMPT/openmpt

commit 8e57bbac2939496bb9d239f6833b01c674f2b57c

[Fix] Keyboard config: Clear find text boxes when manually switching categories dropdown to avoid inconsistent UI state.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@19525 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/KeyConfigDlg.cpp
@@ -364,8 +364,12 @@ void COptionsKeyboard::OnKeyboardChanged()
 void COptionsKeyboard::OnCategorySelChanged()
 {
 	int cat = static_cast<int>(m_cmbCategory.GetItemData(m_cmbCategory.GetCurSel()));
+	if(cat < 0)
+		return;
 
-	if(cat >= 0 && cat != m_curCategory)
+	m_eFind.SetWindowText(_T(""));
+	m_eFindHotKey.SetWindowText(_T(""));
+	if(cat != m_curCategory)
 	{
 		// Changed category
 		UpdateShortcutList(cat);

commit bec83941bc4872dfb4e3386a8824d8d7b10f08d1

Merged revision(s) 19649 from trunk/OpenMPT: [Fix] Keyboard config dialog: Switching between categories became somewhat broken in r19525. ........
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@19650 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/KeyConfigDlg.cpp
@@ -26,7 +26,7 @@ OPENMPT_NAMESPACE_BEGIN
 BEGIN_MESSAGE_MAP(CCustEdit, CEdit)
 	ON_WM_SETFOCUS()
 	ON_WM_KILLFOCUS()
-	ON_MESSAGE(WM_MOD_MIDIMSG,		&CCustEdit::OnMidiMsg)
+	ON_MESSAGE(WM_MOD_MIDIMSG, &CCustEdit::OnMidiMsg)
 END_MESSAGE_MAP()
 
 
@@ -118,41 +118,41 @@ void CCustEdit::OnKillFocus(CWnd *pNewWnd)
 // Initialisation
 
 BEGIN_MESSAGE_MAP(COptionsKeyboard, CPropertyPage)
-	ON_LBN_SELCHANGE(IDC_CHOICECOMBO,		&COptionsKeyboard::OnKeyChoiceSelect)
-	ON_LBN_SELCHANGE(IDC_COMMAND_LIST,		&COptionsKeyboard::OnCommandKeySelChanged)
-	ON_LBN_SELCHANGE(IDC_KEYCATEGORY,		&COptionsKeyboard::OnCategorySelChanged)
-	ON_EN_UPDATE(IDC_CHORDDETECTWAITTIME,	&COptionsKeyboard::OnChordWaitTimeChanged) //rewbs.autochord
-	ON_COMMAND(IDC_DELETE,					&COptionsKeyboard::OnDeleteKeyChoice)
-	ON_COMMAND(IDC_RESTORE,					&COptionsKeyboard::OnRestoreKeyChoice)
-	ON_COMMAND(IDC_LOAD,					&COptionsKeyboard::OnLoad)
-	ON_COMMAND(IDC_SAVE,					&COptionsKeyboard::OnSave)
-	ON_COMMAND(IDC_CHECKKEYDOWN,			&COptionsKeyboard::OnCheck)
-	ON_COMMAND(IDC_CHECKKEYHOLD,			&COptionsKeyboard::OnCheck)
-	ON_COMMAND(IDC_CHECKKEYUP,				&COptionsKeyboard::OnCheck)
-	ON_COMMAND(IDC_NOTESREPEAT,				&COptionsKeyboard::OnNotesRepeat)
-	ON_COMMAND(IDC_NONOTESREPEAT,			&COptionsKeyboard::OnNoNotesRepeat)
-	ON_COMMAND(IDC_CLEARLOG,				&COptionsKeyboard::OnClearLog)
-	ON_COMMAND(IDC_RESTORE_KEYMAP,			&COptionsKeyboard::OnRestoreDefaultKeymap)
-	ON_EN_CHANGE(IDC_FIND,					&COptionsKeyboard::OnSearchTermChanged)
-	ON_EN_CHANGE(IDC_FINDHOTKEY,			&COptionsKeyboard::OnFindHotKey)
-	ON_EN_SETFOCUS(IDC_FINDHOTKEY,			&COptionsKeyboard::OnClearHotKey)
+	ON_LBN_SELCHANGE(IDC_CHOICECOMBO,     &COptionsKeyboard::OnKeyChoiceSelect)
+	ON_LBN_SELCHANGE(IDC_COMMAND_LIST,    &COptionsKeyboard::OnCommandKeySelChanged)
+	ON_LBN_SELCHANGE(IDC_KEYCATEGORY,     &COptionsKeyboard::OnCategorySelChanged)
+	ON_EN_UPDATE(IDC_CHORDDETECTWAITTIME, &COptionsKeyboard::OnChordWaitTimeChanged)
+	ON_COMMAND(IDC_DELETE,                &COptionsKeyboard::OnDeleteKeyChoice)
+	ON_COMMAND(IDC_RESTORE,               &COptionsKeyboard::OnRestoreKeyChoice)
+	ON_COMMAND(IDC_LOAD,                  &COptionsKeyboard::OnLoad)
+	ON_COMMAND(IDC_SAVE,                  &COptionsKeyboard::OnSave)
+	ON_COMMAND(IDC_CHECKKEYDOWN,          &COptionsKeyboard::OnCheck)
+	ON_COMMAND(IDC_CHECKKEYHOLD,          &COptionsKeyboard::OnCheck)
+	ON_COMMAND(IDC_CHECKKEYUP,            &COptionsKeyboard::OnCheck)
+	ON_COMMAND(IDC_NOTESREPEAT,           &COptionsKeyboard::OnNotesRepeat)
+	ON_COMMAND(IDC_NONOTESREPEAT,         &COptionsKeyboard::OnNoNotesRepeat)
+	ON_COMMAND(IDC_CLEARLOG,              &COptionsKeyboard::OnClearLog)
+	ON_COMMAND(IDC_RESTORE_KEYMAP,        &COptionsKeyboard::OnRestoreDefaultKeymap)
+	ON_EN_CHANGE(IDC_FIND,                &COptionsKeyboard::OnSearchTermChanged)
+	ON_EN_CHANGE(IDC_FINDHOTKEY,          &COptionsKeyboard::OnFindHotKey)
+	ON_EN_SETFOCUS(IDC_FINDHOTKEY,        &COptionsKeyboard::OnClearHotKey)
 	ON_WM_DESTROY()
 END_MESSAGE_MAP()
 
 void COptionsKeyboard::DoDataExchange(CDataExchange *pDX)
 {
 	CPropertyPage::DoDataExchange(pDX);
-	DDX_Control(pDX, IDC_KEYCATEGORY,	m_cmbCategory);
-	DDX_Control(pDX, IDC_COMMAND_LIST,	m_lbnCommandKeys);
-	DDX_Control(pDX, IDC_CHOICECOMBO,	m_cmbKeyChoice);
-	DDX_Control(pDX, IDC_CHORDDETECTWAITTIME, m_eChordWaitTime);//rewbs.autochord
-	DDX_Control(pDX, IDC_KEYREPORT,		m_eReport);
-	DDX_Control(pDX, IDC_CUSTHOTKEY,	m_eCustHotKey);
-	DDX_Control(pDX, IDC_FINDHOTKEY,	m_eFindHotKey);
-	DDX_Control(pDX, IDC_CHECKKEYDOWN,	m_bKeyDown);
-	DDX_Control(pDX, IDC_CHECKKEYHOLD,	m_bKeyHold);
-	DDX_Control(pDX, IDC_CHECKKEYUP,	m_bKeyUp);
-	DDX_Control(pDX, IDC_FIND,			m_eFind);
+	DDX_Control(pDX, IDC_KEYCATEGORY, m_cmbCategory);
+	DDX_Control(pDX, IDC_COMMAND_LIST, m_lbnCommandKeys);
+	DDX_Control(pDX, IDC_CHOICECOMBO, m_cmbKeyChoice);
+	DDX_Control(pDX, IDC_CHORDDETECTWAITTIME, m_eChordWaitTime);
+	DDX_Control(pDX, IDC_KEYREPORT, m_eReport);
+	DDX_Control(pDX, IDC_CUSTHOTKEY, m_eCustHotKey);
+	DDX_Control(pDX, IDC_FINDHOTKEY, m_eFindHotKey);
+	DDX_Control(pDX, IDC_CHECKKEYDOWN, m_bKeyDown);
+	DDX_Control(pDX, IDC_CHECKKEYHOLD, m_bKeyHold);
+	DDX_Control(pDX, IDC_CHECKKEYUP, m_bKeyUp);
+	DDX_Control(pDX, IDC_FIND, m_eFind);
 }
 
 
@@ -360,6 +360,7 @@ void COptionsKeyboard::OnKeyboardChanged()
 
 void COptionsKeyboard::OnCategorySelChanged()
 {
+	LockControls();
 	int cat = static_cast<int>(m_cmbCategory.GetItemData(m_cmbCategory.GetCurSel()));
 	if(cat < 0)
 		return;
@@ -371,6 +372,7 @@ void COptionsKeyboard::OnCategorySelChanged()
 		// Changed category
 		UpdateShortcutList(cat);
 	}
+	UnlockControls();
 }
 
 
@@ -379,7 +381,7 @@ void COptionsKeyboard::UpdateCategory()
 {
 	for(int i = 0; i < m_cmbCategory.GetCount(); i++)
 	{
-		if((int)m_cmbCategory.GetItemData(i) == m_curCategory)
+		if(static_cast<int>(m_cmbCategory.GetItemData(i)) == m_curCategory)
 		{
 			m_cmbCategory.SetCurSel(i);
 			break;
@@ -389,6 +391,9 @@ void COptionsKeyboard::UpdateCategory()
 
 void COptionsKeyboard::OnSearchTermChanged()
 {
+	if(IsLocked())
+		return;
+
 	CString findString;
 	m_eFind.GetWindowText(findString);
 
@@ -402,6 +407,9 @@ void COptionsKeyboard::OnSearchTermChanged()
 
 void COptionsKeyboard::OnFindHotKey()
 {
+	if(IsLocked())
+		return;
+	
 	if(m_eFindHotKey.code == 0)
 	{
 		UpdateCategory();

mptrack/KeyConfigDlg.h
@@ -43,7 +43,7 @@ public:
 class CCustEdit: public CEdit
 {
 protected:
-	COptionsKeyboard *m_pOptKeyDlg;
+	COptionsKeyboard *m_pOptKeyDlg = nullptr;
 	HWND m_hParent = nullptr;
 	UINT m_nCtrlId = 0;
 	bool m_isFocussed = false, m_isDummy = false;
@@ -52,7 +52,7 @@ public:
 	FlagSet<Modifiers> mod = ModNone;
 	UINT code = 0;
 
-	CCustEdit(bool dummyField) : m_isDummy(dummyField) { }
+	explicit CCustEdit(bool dummyField) : m_isDummy(dummyField) { }
 	void SetParent(HWND h, UINT nID, COptionsKeyboard *pOKD)
 	{
 		m_hParent = h;
@@ -61,10 +61,11 @@ public:
 	}
 	void SetKey(FlagSet<Modifiers> mod, UINT code);
 	
+protected:
 	BOOL PreTranslateMessage(MSG *pMsg) override;
 	
-	afx_msg void OnSetFocus(CWnd* pOldWnd);
-	afx_msg void OnKillFocus(CWnd* pNewWnd);
+	afx_msg void OnSetFocus(CWnd *pOldWnd);
+	afx_msg void OnKillFocus(CWnd *pNewWnd);
 	afx_msg LRESULT OnMidiMsg(WPARAM, LPARAM);
 	
 	DECLARE_MESSAGE_MAP()
@@ -72,6 +73,8 @@ public:
 
 class COptionsKeyboard: public CPropertyPage
 {
+	friend class CCustEdit;
+
 protected:
 	CListBox m_lbnHotKeys;
 	CListBox m_lbnCommandKeys;
@@ -82,29 +85,34 @@ protected:
 	CCustEdit m_eCustHotKey, m_eFindHotKey;
 	CEdit m_eFind;
 	CEdit m_eReport, m_eChordWaitTime;
+	
+	std::vector<CommandCategory> commandCategories;
+	std::unique_ptr<CCommandSet> m_localCmdSet;
+	mpt::PathString m_fullPathName;
 	CommandID m_curCommand = kcNull;
 	int m_curCategory = -1, m_curKeyChoice = -1;
-	mpt::PathString m_fullPathName;
-	std::unique_ptr<CCommandSet> m_localCmdSet;
+	int m_lockCount = 0;
 	bool m_forceUpdate = false;
-
-	void ForceUpdateGUI();
-	void UpdateShortcutList(int category = -1);
-	void UpdateCategory();
-	int GetCategoryFromCommandID(CommandID command) const;
 
 public:
 	COptionsKeyboard() : CPropertyPage(IDD_OPTIONS_KEYBOARD), m_eCustHotKey(false), m_eFindHotKey(true) { }
-	std::vector<CommandCategory> commandCategories;
-	void DefineCommandCategories();
-
-	void OnSetKeyChoice();
 
 protected:
 	BOOL OnInitDialog() override;
 	void OnOK() override;
 	BOOL OnSetActive() override;
 	void DoDataExchange(CDataExchange* pDX) override;
+
+	void DefineCommandCategories();
+	void ForceUpdateGUI();
+	void UpdateShortcutList(int category = -1);
+	void UpdateCategory();
+	int GetCategoryFromCommandID(CommandID command) const;
+	void OnSetKeyChoice();
+
+	void LockControls() { m_lockCount++; }
+	void UnlockControls() { m_lockCount--; MPT_ASSERT(m_lockCount >= 0); }
+	bool IsLocked() const noexcept { return m_lockCount != 0; }
 
 	afx_msg void UpdateDialog();
 	afx_msg void OnKeyboardChanged();