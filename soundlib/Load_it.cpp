029eef01e4ff9d71c85d7edd698aafd02475afcb
Johannes Schultz <sagamusix@openmpt.org>
2024-01-10 20:18:30 +0000

    Merged revision(s) 19869, 19871 from trunk/OpenMPT: [Imp] OKT: Work around missing negative arpeggio implementation by transposing the notes up an octave (https://bugs.openmpt.org/view.php?id=1732). [Fix] OKT: Setting channel volume should take precedence over other effects. Another fix for https://bugs.openmpt.org/view.php?id=1732 ........ [Imp] IT: Set/unset a few more compatibility flags for recently-implemented playback fixes in SchismTracker. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.30@20075 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_it.cpp
+++ soundlib/Load_it.cpp
@@ -1241,6 +1241,21 @@ bool CSoundFile::ReadIT(FileReader &file, ModLoadingFlags loadFlags)
 			// Pitch/Pan Separation can be overridden by panning commands: Added 2021-11-01, https://github.com/schismtracker/schismtracker/commit/6e9f1207015cae0fe1b829fff7bb867e02ec6dea
 			if(schismDateVersion < SchismVersionFromDate<2021, 11, 01>::date)
 				m_playBehaviour.reset(kITPitchPanSeparation);
+			// 2023-10-16: kITEnvelopePositionHandling https://github.com/schismtracker/schismtracker/commit/bc81f605d927ca931a886417641da29fc89283b8
+			if(schismDateVersion < SchismVersionFromDate<2023, 10, 19>::date)
+			{
+				// Panbrello sample & hold random waveform: Added 2023-10-19, https://github.com/schismtracker/schismtracker/commit/411ec16b190ba1a486d8b0907ad8d74f8fdc2840
+				m_playBehaviour.reset(kITPanbrelloHold);
+				// Don't apply any portamento if no previous note is playing: Added 2023-10-19, https://github.com/schismtracker/schismtracker/commit/8ff0a86a715efb50c89770fb9095d4c4089ff187
+				m_playBehaviour.reset(kITPortaNoNote);
+			}
+			if(schismDateVersion < SchismVersionFromDate<2023, 10, 22>::date)
+			{
+				// Note delay delays first-tick behaviour for slides: Added 2023-10-22, https://github.com/schismtracker/schismtracker/commit/b9609e4f827e1b6ce9ebe6573b85e69388ca0ea0
+				m_playBehaviour.reset(kITFirstTickHandling);
+				// Added 2023-10-22, https://github.com/schismtracker/schismtracker/commit/a9e5df533ab52c35190fcc1cbfed4f0347b660bb
+				m_playBehaviour.reset(kITMultiSampleInstrumentNumber);
+			}
 			break;
 		case 4:
 			madeWithTracker = MPT_UFORMAT("pyIT {}.{}")((fileHeader.cwtv & 0x0F00) >> 8, mpt::ufmt::hex0<2>(fileHeader.cwtv & 0xFF));
