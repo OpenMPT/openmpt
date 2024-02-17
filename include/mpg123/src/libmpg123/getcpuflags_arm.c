d9f75a7a5a87635476b15090932d8073efffb80a
Jörn Heusipp <manx@openmpt.org>
2024-02-17 16:00:07 +0000

    Merged revision(s) 20136 from trunk/OpenMPT: [Var] mpg123: Update to 1.32.5 (2024-02-17). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@20137 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/mpg123/src/libmpg123/getcpuflags_arm.c
+++ include/mpg123/src/libmpg123/getcpuflags_arm.c
@@ -1,10 +1,16 @@
 /*
 	getcpuflags_arm: get cpuflags for ARM
 
-	copyright 1995-2014 by the mpg123 project - free software under the terms of the LGPL 2.1
+	copyright 1995-2024 by the mpg123 project - free software under the terms of the LGPL 2.1
 	see COPYING and AUTHORS files in distribution or http://mpg123.org
 	initially written by Taihei Momma
 */
+
+// For sigsetjmp, we need POSIX 2001
+#define _POSIX_C_SOURCE 200112L
+// For SA_RESTART, XSI is needed (or POSIX 2008).
+// This here should include the above, keeping that for clarity.
+#define _XOPEN_SOURCE 600
 
 #include <setjmp.h>
 #include <signal.h>
