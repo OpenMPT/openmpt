fec3e3b00f746659272856920cf1a83bd7e86686
Jörn Heusipp <manx@openmpt.org>
2024-03-03 15:32:41 +0000

    [Ref] mpt/base/macros.hpp: Add MPT_ASSUME(expr) for C++23 [[assume(expr)]].
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@20217 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/mpt/base/macros.hpp
+++ src/mpt/base/macros.hpp
@@ -152,4 +152,23 @@
 
 
 
+#if MPT_CXX_AT_LEAST(23)
+#define MPT_ASSUME(expr) [[assume(expr)]]
+#else // !C++23
+#if MPT_COMPILER_CLANG
+#define MPT_ASSUME(expr) __builtin_assume(expr)
+#endif
+#if MPT_COMPILER_MSVC
+#define MPT_ASSUME(expr) __assume(expr)
+#endif
+#if MPT_COMPILER_GCC
+#define MPT_ASSUME(expr) do { if (!expr) { __builtin_unreachable(); } } while(0)
+#endif
+#if !defined(MPT_ASSUME)
+#define MPT_ASSUME(expr) MPT_DISCARD(expr)
+#endif
+#endif // C++23
+
+
+
 #endif // MPT_BASE_MACROS_HPP
