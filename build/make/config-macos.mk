a26a94a218540d46cf9f8b275715444b5169d5a3
Jörn Heusipp <manx@openmpt.org>
2024-03-07 08:09:45 +0000

    [Fix] build: Makefile: macOS: Assume cross-compiling when specifying ARCH=. [Fix] build: Makefile: macOS: Fix copy-paste-error.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@20259 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/make/config-macos.mk
+++ build/make/config-macos.mk
@@ -10,7 +10,6 @@ MPT_COMPILER_NOGCSECTIONS=1
 # 10.13 .. 
 ifeq ($(MACOSX_VERSION_MIN),)
 else
-MPT_OS_MACOSX_OR_IOS
 CFLAGS   += -mmacosx-version-min=$(MACOSX_VERSION_MIN)
 CXXFLAGS += -mmacosx-version-min=$(MACOSX_VERSION_MIN)
 LDFLAGS  += -mmacosx-version-min=$(MACOSX_VERSION_MIN)
@@ -19,6 +18,7 @@ endif
 # arm64/x86_64/i386
 ifeq ($(ARCH),)
 else
+IS_CROSS=1
 CFLAGS   += -arch $(ARCH)
 CXXFLAGS += -arch $(ARCH)
 LDFLAGS  += -arch $(ARCH)
