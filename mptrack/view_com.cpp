OpenMPT/openmpt

commit 741a859a04d6757b9b5eb0b0522cf62aadc5692b

[Mod] Comments tab: Avoid Windows sound when holding keys for note preview.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@18909 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/view_com.cpp
@@ -268,6 +268,10 @@ BOOL CViewComments::PreTranslateMessage(MSG *pMsg)
 			{
 				return TRUE;  // Mapped to a command, no need to pass message on.
 			}
+		} else if(pMsg->message == WM_CHAR)
+		{
+			// Avoid Windows warning sound when holding note key
+			return TRUE;
 		}
 	}
 

commit 0e7107623abed11336e7a22ec4aa85945fd83189

[Fix] Comments tab: Sample / instrument name editing was broken since r18909 (tx SoDa7).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@20488 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/view_com.cpp
@@ -272,7 +272,8 @@ BOOL CViewComments::PreTranslateMessage(MSG *pMsg)
 		} else if(pMsg->message == WM_CHAR)
 		{
 			// Avoid Windows warning sound when holding note key
-			return TRUE;
+			if(!CMainFrame::GetInputHandler()->IsBypassed() && CInputHandler::GetKeyEventType(HIWORD(pMsg->lParam)) == kKeyEventRepeat)
+				return TRUE;
 		}
 	}
 