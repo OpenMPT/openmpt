07cf9c4559995482bd4e79371d9122d04cbdbfaf
Johannes Schultz <sagamusix@openmpt.org>
2024-04-20 16:12:47 +0000

    [Mod] GMC: Add missing madeWithTracker information.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@20621 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_gmc.cpp
+++ soundlib/Load_gmc.cpp
@@ -237,6 +237,7 @@ bool CSoundFile::ReadGMC(FileReader &file, ModLoadingFlags loadFlags)
 		}
 	}
 
+	m_modFormat.madeWithTracker = U_("Game Music Creator");
 	m_modFormat.formatName = U_("Game Music Creator");
 	m_modFormat.type = UL_("GMC");
 	m_modFormat.charset = mpt::Charset::Amiga_no_C1;  // No strings in this format...
