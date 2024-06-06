67ab66f0c62b5c30f5e05f5e23a8b22113aec57f
Johannes Schultz <sagamusix@openmpt.org>
2024-06-06 19:55:31 +0000

    Merged revision(s) 20936-20937 from trunk/OpenMPT: [Imp] MED: Further improvements to effect juggling to potentially keep X-param if the other effect can be moved to the volume colum instead. ........ [Imp] MED: Emulate echo DSP through DMO Echo plugin (https://www.un4seen.com/forum/?topic=15448.msg143034#msg143034). ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@20938 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_med.cpp
+++ soundlib/Load_med.cpp
@@ -84,10 +84,10 @@ struct MMD2Song
 	uint32be flags3;
 	uint16be volAdjust;       // Volume adjust (%)
 	uint16be mixChannels;     // Mixing channels, 0 means 4
-	uint8be  mixEchoType;     // 0 = nothing, 1 = normal, 2 = cross
-	uint8be  mixEchoDepth;    // 1 - 6, 0 = default
+	uint8    mixEchoType;     // 0 = nothing, 1 = normal, 2 = cross
+	uint8    mixEchoDepth;    // 1 - 6, 0 = default
 	uint16be mixEchoLength;   // Echo length in milliseconds
-	int8be   mixStereoSep;    // Stereo separation
+	int8     mixStereoSep;    // Stereo separation
 	char     pad0[223];
 };
 
@@ -616,11 +616,11 @@ static bool TranslateMEDPattern(FileReader &file, FileReader &cmdExt, CPattern &
 
 			if(oldCmd.first != CMD_NONE && m->command != oldCmd.first)
 			{
-				// Restore effect from previous page, or reset X-Param to 8-bit value if this cell was overwritten with a "useful" effect
-				if(row > 0 && oldCmd.first == CMD_XPARAM)
-					pattern.GetpModCommand(row - 1, chn)->param = Util::MaxValueOfType(m->param);
-				else if(!ModCommand::CombineEffects(m->command, m->param, oldCmd.first, oldCmd.second) && m->volcmd == VOLCMD_NONE)
+				if(!ModCommand::CombineEffects(m->command, m->param, oldCmd.first, oldCmd.second) && m->volcmd == VOLCMD_NONE)
 					m->FillInTwoCommands(m->command, m->param, oldCmd.first, oldCmd.second);
+				// Reset X-Param to 8-bit value if this cell was overwritten with a "useful" effect
+				if(row > 0 && oldCmd.first == CMD_XPARAM && m->command != CMD_XPARAM)
+					pattern.GetpModCommand(row - 1, chn)->param = Util::MaxValueOfType(m->param);
 			}
 			if(extraCmd.first != CMD_NONE)
 			{
@@ -829,7 +829,7 @@ bool CSoundFile::ReadMED(FileReader &file, ModLoadingFlags loadFlags)
 			if(type == L"VST")
 			{
 				auto &mixPlug = m_MixPlugins[numPlugins];
-				mixPlug = {};
+				mpt::reconstruct(mixPlug);
 				mixPlug.Info.dwPluginId1 = Vst::kEffectMagic;
 				mixPlug.Info.gain = 10;
 				mixPlug.Info.szName = mpt::ToCharset(mpt::Charset::Locale, name);
@@ -1163,6 +1163,34 @@ bool CSoundFile::ReadMED(FileReader &file, ModLoadingFlags loadFlags)
 			{
 				SetupMODPanning(true);
 			}
+
+#ifndef NO_PLUGINS
+			if((header.mixEchoType == 1 || header.mixEchoType == 2) && numPlugins < MAX_MIXPLUGINS)
+			{
+				// Emulating MED echo using the DMO echo requires to compensate for the differences in initial feedback in the latter.
+				const float feedback = 1.0f / (1 << std::max(header.mixEchoDepth, uint8(1)));  // The feedback we want
+				const float initialFeedback = std::sqrt(1.0f - (feedback * feedback));         // Actual strength of first delay's feedback
+				const float wetFactor = feedback / initialFeedback;                            // Factor to compensate for this
+				SNDMIXPLUGIN &mixPlug = m_MixPlugins[numPlugins];
+				mpt::reconstruct(mixPlug);
+				memcpy(&mixPlug.Info.dwPluginId1, "OMXD", 4);
+				memcpy(&mixPlug.Info.dwPluginId2, "\x2C\x93\x3E\xEF", 4);
+				mixPlug.Info.routingFlags = SNDMIXPLUGININFO::irApplyToMaster | SNDMIXPLUGININFO::irAutoSuspend;
+				mixPlug.fDryRatio = 1.0f - wetFactor / (wetFactor + 1.0f);
+				mixPlug.Info.gain = 10;
+				mixPlug.Info.szName = "Echo";
+				mixPlug.Info.szLibraryName = "Echo";
+
+				std::array<float32le, 6> params{};
+				params[1] = 1.0f;                                   // WetDryMix
+				params[2] = feedback;                               // Feedback
+				params[3] = header.mixEchoLength / 2000.0f;         // LeftDelay
+				params[4] = params[3];                              // RightDelay
+				params[5] = header.mixEchoType == 2 ? 1.0f : 0.0f;  // PanDelay
+				mixPlug.pluginData.resize(sizeof(params));
+				memcpy(mixPlug.pluginData.data(), params.data(), sizeof(params));
+			}
+#endif
 
 			std::vector<uint16be> sections;
 			if(!file.Seek(header.sectionTableOffset)
