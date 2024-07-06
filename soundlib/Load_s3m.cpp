ba462f4ff0783cd19f4bd58884692f4fe82cf422
Johannes Schultz <sagamusix@openmpt.org>
2024-07-06 10:27:06 +0000

    [Imp] S3M: Detect all Liquid Tracker versions (presumably).
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@21128 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_s3m.cpp
+++ soundlib/Load_s3m.cpp
@@ -335,9 +335,14 @@ bool CSoundFile::ReadS3M(FileReader &file, ModLoadingFlags loadFlags)
 		nonCompatTracker = true;
 		break;
 	case S3MFileHeader::trkOpenMPT:
-		if(fileHeader.cwtv == S3MFileHeader::trkLiquidTracker && fileHeader.ultraClicks == 16)
+		// Liquid Tracker's ID clashes with OpenMPT's.
+		// OpenMPT started writing full version information with OpenMPT 1.29 and later changed the ultraClicks value from 8 to 16.
+		// Liquid Tracker writes an ultraClicks value of 16.
+		// So we assume that a file was saved with Liquid Tracker if the reserved fields are 0 and ultraClicks is 16.
+		if(fileHeader.reserved2 == 0 && fileHeader.ultraClicks == 16 && fileHeader.channels[1] != 1)
 		{
 			madeWithTracker = U_("Liquid Tracker");
+			formatTrackerStr = true;
 		} else if((fileHeader.cwtv & 0xFF00) == S3MFileHeader::trkNESMusa)
 		{
 			madeWithTracker = U_("NESMusa");
