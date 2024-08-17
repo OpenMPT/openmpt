c5b5b11f3e1640d06c9cb7203017cdf0afd79e82
Jörn Heusipp <manx@openmpt.org>
2024-08-17 14:09:56 +0000

    [Fix] MinGW32 runtime headers require __off64_t when including some C and/or C++ stdlib headers. This is declared in <sys/types.h>, which however is not included in some header chains. Forcibly include it in openmpt/all/BuildSettings.hpp. See r17412.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@21503 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/openmpt/all/BuildSettings.hpp
+++ src/openmpt/all/BuildSettings.hpp
@@ -60,12 +60,12 @@ OPENMPT_NAMESPACE_END
 
 
 
-// immediate quirk fixups
-
-#if defined(MPT_LIBC_QUIRK_REQUIRES_SYS_TYPES_H)
-#include <sys/types.h>
 #endif
 
 
 
+// immediate quirk fixups
+
+#if defined(MPT_LIBC_QUIRK_REQUIRES_SYS_TYPES_H)
+#include <sys/types.h>
 #endif
