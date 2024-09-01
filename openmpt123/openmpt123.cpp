7fdf169e6385bed57eecf7071556f151b9195305
Jörn Heusipp <manx@openmpt.org>
2024-09-01 08:31:56 +0000

    [Fix] openmpt123: Work-around cppcheck 2.15.0 getting very confused about Linux glibc headers: "/usr/include/x86_64-linux-gnu/sys/types.h:230: warning: error: There is an unknown macro here somewhere. Configuration is required. If __END_DECLS is a macro then please configure it. [unknownMacro]".
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@21562 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- openmpt123/openmpt123.cpp
+++ openmpt123/openmpt123.cpp
@@ -73,7 +73,9 @@ static const char * const license =
 #include <cstdlib>
 #include <cstring>
 
-#if MPT_OS_DJGPP
+#if defined(CPPCHECK)
+// cppcheck 2.15.0 gets confused by linux/glibc headers
+#elif MPT_OS_DJGPP
 #include <conio.h>
 #include <crt0.h>
 #include <dpmi.h>
