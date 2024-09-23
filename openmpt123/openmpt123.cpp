7c8c7cea21936275831b13b852da4a896a4b1379
Jörn Heusipp <manx@openmpt.org>
2024-09-23 21:12:59 +0000

    Merged revision(s) 21727 from branches/OpenMPT-1.30: [Fix] openmpt123: Fixup backport of r21721. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@21729 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- openmpt123/openmpt123.cpp
+++ openmpt123/openmpt123.cpp
@@ -2364,7 +2364,7 @@ static int main( int argc, char * argv [] ) {
 	} catch ( silent_exit_exception & ) {
 		return 0;
 	} catch ( exception & e ) {
-		std_err << MPT_USTRING("error: ") << mpt::get_exception_text<mpt::ustring>( e ) << lf;
+		std_err << "error: " << e.what() << std::endl;
 		std_err.writeout();
 		return 1;
 	} catch ( std::exception & e ) {
@@ -2543,7 +2543,7 @@ static int main( int argc, char * argv [] ) {
 	} catch ( silent_exit_exception & ) {
 		return 0;
 	} catch ( exception & e ) {
-		std_err << MPT_USTRING("error: ") << mpt::get_exception_text<mpt::ustring>( e ) << lf;
+		std_err << "error: " << e.what() << std::endl;
 		std_err.writeout();
 		return 1;
 	} catch ( std::exception & e ) {
