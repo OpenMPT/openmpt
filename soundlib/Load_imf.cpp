OpenMPT/openmpt

commit feb0d34c69508e8fa3717552fd7c079fdfcd7c22

[Ref] CSoundFile::InitializeGlobals now takes the channel count in addition to the format to initialize to. With almost all supported formats, we can determine the correct channel count very early on, making it easier to initialize the correct number of channel headers, and allowing us to remove CSoundFile::InitializeChannels completely. In a second step, the fixed-size ChnSettings array and m_nChannels can be merged into a ChnSettings vector. [Reg] SymMOD: Require first chunk to be number of channels to simplify initialization. In practice, all SymMOD files are structured like that.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@20949 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_imf.cpp
@@ -45,6 +45,19 @@ struct IMFFileHeader
 	uint8le  unused2[8];
 	char     im10[4];         // 'IM10'
 	IMFChannel channels[32];  // Channel settings
+
+	CHANNELINDEX GetNumChannels() const
+	{
+		uint8 detectedChannels = 0;
+		for(uint8 chn = 0; chn < 32; chn++)
+		{
+			if(channels[chn].status < 2)
+				detectedChannels = chn + 1;
+			else if(channels[chn].status > 2)
+				return 0;
+		}
+		return detectedChannels;
+	}
 };
 
 MPT_BINARY_STRUCT(IMFFileHeader, 576)
@@ -365,29 +378,8 @@ static bool ValidateHeader(const IMFFileHeader &fileHeader)
 	   || fileHeader.bpm < 32
 	   || fileHeader.master > 64
 	   || fileHeader.amp < 4
-	   || fileHeader.amp > 127)
-	{
-		return false;
-	}
-	bool channelFound = false;
-	for(const auto &chn : fileHeader.channels)
-	{
-		switch(chn.status)
-		{
-		case 0: // enabled; don't worry about it
-			channelFound = true;
-			break;
-		case 1: // mute
-			channelFound = true;
-			break;
-		case 2: // disabled
-			// nothing
-			break;
-		default: // uhhhh.... freak out
-			return false;
-		}
-	}
-	if(!channelFound)
+	   || fileHeader.amp > 127
+	   || !fileHeader.GetNumChannels())
 	{
 		return false;
 	}
@@ -437,52 +429,40 @@ bool CSoundFile::ReadIMF(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	// Read channel configuration
-	std::bitset<32> ignoreChannels; // bit set for each channel that's completely disabled
-	uint8 detectedChannels = 0;
-	for(uint8 chn = 0; chn < 32; chn++)
-	{
-		ChnSettings[chn].Reset();
-		ChnSettings[chn].nPan = static_cast<uint16>(fileHeader.channels[chn].panning * 256 / 255);
-
-		ChnSettings[chn].szName = mpt::String::ReadBuf(mpt::String::nullTerminated, fileHeader.channels[chn].name);
-
-		// TODO: reverb/chorus?
-		switch(fileHeader.channels[chn].status)
-		{
-		case 0: // enabled; don't worry about it
-			detectedChannels = chn + 1;
-			break;
-		case 1: // mute
-			ChnSettings[chn].dwFlags = CHN_MUTE;
-			detectedChannels = chn + 1;
-			break;
-		case 2: // disabled
-			ChnSettings[chn].dwFlags = CHN_MUTE;
-			ignoreChannels[chn] = true;
-			break;
-		default: // uhhhh.... freak out
-			return false;
-		}
-	}
-
-	InitializeGlobals(MOD_TYPE_IMF);
-	m_nChannels = detectedChannels;
+	InitializeGlobals(MOD_TYPE_IMF, fileHeader.GetNumChannels());
 
 	m_modFormat.formatName = U_("Imago Orpheus");
 	m_modFormat.type = U_("imf");
 	m_modFormat.charset = mpt::Charset::CP437;
 
-	//From mikmod: work around an Orpheus bug
-	if(fileHeader.channels[0].status == 0)
+	// Read channel configuration
+	std::bitset<32> ignoreChannels;  // bit set for each channel that's completely disabled
+	uint64 channelMuteStatus = 0xAAAA'AAAA << (m_nChannels * 2);
+	for(CHANNELINDEX chn = 0; chn < m_nChannels; chn++)
 	{
-		CHANNELINDEX chn;
-		for(chn = 1; chn < 16; chn++)
-			if(fileHeader.channels[chn].status != 1)
-				break;
-		if(chn == 16)
-			for(chn = 1; chn < 16; chn++)
-				ChnSettings[chn].dwFlags.reset(CHN_MUTE);
+		ChnSettings[chn].nPan = static_cast<uint16>(fileHeader.channels[chn].panning * 256 / 255);
+		ChnSettings[chn].szName = mpt::String::ReadBuf(mpt::String::nullTerminated, fileHeader.channels[chn].name);
+		channelMuteStatus |= fileHeader.channels[chn].status << (chn * 2);
+		// TODO: reverb/chorus?
+		switch(fileHeader.channels[chn].status)
+		{
+		case 0:  // enabled; don't worry about it
+			break;
+		case 1:  // mute
+			ChnSettings[chn].dwFlags = CHN_MUTE;
+			break;
+		case 2:  // disabled
+			ChnSettings[chn].dwFlags = CHN_MUTE;
+			ignoreChannels[chn] = true;
+			break;
+		}
+	}
+	// BEHIND.IMF: All channels but the first are muted
+	// mikmod refers to this as an Orpheus bug, but I haven't seen any other files like this, so maybe it's just an incorrectly saved file?
+	if(channelMuteStatus == 0xAAAA'AAAA'5555'5554)
+	{
+		for(CHANNELINDEX chn = 1; chn < m_nChannels; chn++)
+			ChnSettings[chn].dwFlags.reset(CHN_MUTE);
 	}
 
 	// Song Name

mptrack/Moddoc.cpp
@@ -643,14 +643,6 @@ void CModDoc::InitializeMod()
 		// Set up mix levels
 		m_SndFile.m_PlayState.m_nGlobalVolume = m_SndFile.m_nDefaultGlobalVolume = MAX_GLOBAL_VOLUME;
 		m_SndFile.m_nSamplePreAmp = m_SndFile.m_nVSTiVolume = 48;
-
-		for (CHANNELINDEX nChn = 0; nChn < MAX_BASECHANNELS; nChn++)
-		{
-			m_SndFile.ChnSettings[nChn].dwFlags.reset();
-			m_SndFile.ChnSettings[nChn].nVolume = 64;
-			m_SndFile.ChnSettings[nChn].nPan = 128;
-			m_SndFile.m_PlayState.Chn[nChn].nGlobalVol = 64;
-		}
 		// Setup LRRL panning scheme for MODs
 		m_SndFile.SetupMODPanning();
 	}
@@ -1178,9 +1170,9 @@ bool CModDoc::NoteOff(UINT note, bool fade, INSTRUMENTINDEX ins, CHANNELINDEX cu
 		{
 			PLUGINDEX plug = pIns->nMixPlug;      // First try intrument VST
 			if((!plug || plug > MAX_MIXPLUGINS)   // No good plug yet
-				&& currentChn < MAX_BASECHANNELS) // Chan OK
+				&& currentChn < m_SndFile.ChnSettings.size())
 			{
-				plug = m_SndFile.ChnSettings[currentChn].nMixPlugin;// Then try Channel VST
+				plug = m_SndFile.ChnSettings[currentChn].nMixPlugin;  // Then try Channel VST
 			}
 
 			if(plug && plug <= MAX_MIXPLUGINS)

mptrack/Undo.cpp
@@ -152,7 +152,7 @@ bool CPatternUndo::PrepareBuffer(undobuf_t &buffer, PATTERNINDEX pattern, CHANNE
 
 	if(storeChannelInfo)
 	{
-		undo.channelInfo.assign(std::begin(sndFile.ChnSettings) + firstChn, std::begin(sndFile.ChnSettings) + firstChn + numChns);
+		undo.channelInfo.assign(sndFile.ChnSettings.begin() + firstChn, sndFile.ChnSettings.begin() + firstChn + numChns);
 	}
 
 	buffer.push_back(std::move(undo));

mptrack/View_gen.cpp
@@ -371,7 +371,7 @@ void CViewGlobals::UpdateView(UpdateHint hint, CObject *pObject)
 		{
 			const CHANNELINDEX nChn = m_nActiveTab * CHANNELS_IN_TAB + ichn;
 			const BOOL bEnable = (nChn < sndFile.GetNumChannels()) ? TRUE : FALSE;
-			if(nChn < MAX_BASECHANNELS)
+			if(nChn < sndFile.GetNumChannels())
 			{
 				const auto &chnSettings = sndFile.ChnSettings[nChn];
 				// Text
@@ -605,7 +605,7 @@ void CViewGlobals::PopulateChannelPlugins(UpdateHint hint, const CObject *pObj)
 	const CSoundFile &sndFile = GetDocument()->GetSoundFile();
 	for(CHANNELINDEX ichn = 0; ichn < CHANNELS_IN_TAB; ichn++)
 	{
-		if(const CHANNELINDEX nChn = m_nActiveTab * CHANNELS_IN_TAB + ichn; nChn < MAX_BASECHANNELS)
+		if(const CHANNELINDEX nChn = m_nActiveTab * CHANNELS_IN_TAB + ichn; nChn < sndFile.GetNumChannels())
 		{
 			PLUGINDEX sel = sndFile.ChnSettings[nChn].nMixPlugin ? sndFile.ChnSettings[nChn].nMixPlugin - 1 : PLUGINDEX_INVALID;
 			m_CbnEffects[ichn].Update(PluginComboBox::Config{PluginComboBox::ShowNoPlugin}.Hint(hint, pObj).CurrentSelection(sel), sndFile);

soundlib/Load_667.cpp
@@ -77,11 +77,10 @@ bool CSoundFile::Read667(FileReader &file, ModLoadingFlags loadFlags)
 	if(loadFlags == onlyVerifyHeader)
 		return true;
 
-	InitializeGlobals(MOD_TYPE_S3M);
+	InitializeGlobals(MOD_TYPE_S3M, 18);
 	m_SongFlags.set(SONG_IMPORTED);
 	Order().SetDefaultTempoInt(150);
 	Order().SetDefaultSpeed(fileHeader.speed);
-	m_nChannels = 18;
 	m_nSamples = 64;
 
 	ReadOrderFromFile<uint8>(Order(), file, fileHeader.numOrders);
@@ -90,8 +89,6 @@ bool CSoundFile::Read667(FileReader &file, ModLoadingFlags loadFlags)
 		if(pat >= 128)
 			return false;
 	}
-
-	InitializeChannels();
 
 	for(SAMPLEINDEX smp = 1; smp <= m_nSamples; smp++)
 	{

soundlib/Load_669.cpp
@@ -135,12 +135,11 @@ bool CSoundFile::Read669(FileReader &file, ModLoadingFlags loadFlags)
 		return false;
 	}
 
-	InitializeGlobals(MOD_TYPE_669);
+	InitializeGlobals(MOD_TYPE_669, 8);
 	m_nMinPeriod = 28 << 2;
 	m_nMaxPeriod = 1712 << 3;
 	Order().SetDefaultTempoInt(78);
 	Order().SetDefaultSpeed(4);
-	m_nChannels = 8;
 	m_playBehaviour.set(kPeriodsAreHertz);
 	m_SongFlags.set(SONG_FASTPORTAS | SONG_AUTO_TONEPORTA);
 #ifdef MODPLUG_TRACKER
@@ -179,7 +178,6 @@ bool CSoundFile::Read669(FileReader &file, ModLoadingFlags loadFlags)
 	// Set up panning
 	for(CHANNELINDEX chn = 0; chn < 8; chn++)
 	{
-		ChnSettings[chn].Reset();
 		ChnSettings[chn].nPan = (chn & 1) ? 0xD0 : 0x30;
 	}
 

soundlib/Load_amf.cpp
@@ -125,10 +125,8 @@ bool CSoundFile::ReadAMF_Asylum(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	InitializeGlobals(MOD_TYPE_AMF0);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_AMF0, 8);
 	SetupMODPanning(true);
-	m_nChannels = 8;
 	Order().SetDefaultSpeed(fileHeader.defaultSpeed);
 	Order().SetDefaultTempoInt(fileHeader.defaultTempo);
 	m_nSamples = fileHeader.numSamples;
@@ -573,8 +571,7 @@ bool CSoundFile::ReadAMF_DSMI(FileReader &file, ModLoadingFlags loadFlags)
 	if(loadFlags == onlyVerifyHeader)
 		return true;
 
-	InitializeGlobals(MOD_TYPE_AMF);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_AMF, (fileSignature.version < 9) ? 4 : fileHeader.numChannels);
 
 	if(isDMF)
 	{
@@ -588,13 +585,11 @@ bool CSoundFile::ReadAMF_DSMI(FileReader &file, ModLoadingFlags loadFlags)
 	}
 	m_modFormat.charset = mpt::Charset::CP437;
 
-	m_nChannels = fileHeader.numChannels;
 	m_nSamples = fileHeader.numSamples;
 
 	if(fileSignature.version < 9)
 	{
 		// Old format revisions are fixed to 4 channels
-		m_nChannels = 4;
 		for(CHANNELINDEX chn = 0; chn < 4; chn++)
 		{
 			ChnSettings[chn].nPan = (chn & 1) ? 0xC0 : 0x40;

soundlib/Load_ams.cpp
@@ -400,11 +400,9 @@ bool CSoundFile::ReadAMS(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	InitializeGlobals(MOD_TYPE_AMS);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_AMS, (fileHeader.channelConfig & 0x1F) + 1);
 
 	m_SongFlags = SONG_ITCOMPATGXX | SONG_ITOLDEFFECTS;
-	m_nChannels = (fileHeader.channelConfig & 0x1F) + 1;
 	m_nSamples = fileHeader.numSamps;
 	SetupMODPanning(true);
 
@@ -774,12 +772,11 @@ bool CSoundFile::ReadAMS2(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	InitializeGlobals(MOD_TYPE_AMS);
+	InitializeGlobals(MOD_TYPE_AMS, 32);
 
 	m_songName = std::move(songName);
 
 	m_nInstruments = fileHeader.numIns;
-	m_nChannels = 32;
 	SetupMODPanning(true);
 
 	m_modFormat.formatName = U_("Velvet Studio");
@@ -912,7 +909,6 @@ bool CSoundFile::ReadAMS2(FileReader &file, ModLoadingFlags loadFlags)
 	// Channel names
 	for(CHANNELINDEX chn = 0; chn < 32; chn++)
 	{
-		ChnSettings[chn].Reset();
 		file.ReadSizedString<uint8le, mpt::String::spacePadded>(ChnSettings[chn].szName);
 	}
 

commit baebd1108c051c5df074e23b198d89b1897cff19

[Fix] IMF: Muted channel bug detection was broken due to missing integer promotion. Broken since r20949.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22223 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_imf.cpp
@@ -437,12 +437,12 @@ bool CSoundFile::ReadIMF(FileReader &file, ModLoadingFlags loadFlags)
 
 	// Read channel configuration
 	std::bitset<32> ignoreChannels;  // bit set for each channel that's completely disabled
-	uint64 channelMuteStatus = 0xAAAA'AAAA << (GetNumChannels() * 2);
+	uint64 channelMuteStatus = static_cast<uint64>(0xAAAA'AAAA) << (GetNumChannels() * 2);
 	for(CHANNELINDEX chn = 0; chn < GetNumChannels(); chn++)
 	{
 		ChnSettings[chn].nPan = static_cast<uint16>(fileHeader.channels[chn].panning * 256 / 255);
 		ChnSettings[chn].szName = mpt::String::ReadBuf(mpt::String::nullTerminated, fileHeader.channels[chn].name);
-		channelMuteStatus |= fileHeader.channels[chn].status << (chn * 2);
+		channelMuteStatus |= static_cast<uint64>(fileHeader.channels[chn].status) << (chn * 2);
 		// TODO: reverb/chorus?
 		switch(fileHeader.channels[chn].status)
 		{