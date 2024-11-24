OpenMPT/openmpt

commit d87c753d71ea896c0852c0d490d53494b1df1ce6

[Mod] Open various context menus on right mouse button release as it should be done, not on button down.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22264 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/View_pat.cpp
@@ -57,9 +57,9 @@ BEGIN_MESSAGE_MAP(CViewPattern, CModScrollView)
 	ON_WM_LBUTTONDBLCLK()
 	ON_WM_LBUTTONUP()
 	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
 	ON_WM_SETFOCUS()
 	ON_WM_KILLFOCUS()
-	ON_WM_SYSKEYDOWN()
 	ON_WM_DESTROY()
 	ON_MESSAGE(WM_MOD_KEYCOMMAND,	&CViewPattern::OnCustomKeyMsg)
 	ON_MESSAGE(WM_MOD_MIDIMSG,		&CViewPattern::OnMidiMsg)
@@ -136,12 +136,7 @@ BEGIN_MESSAGE_MAP(CViewPattern, CModScrollView)
 	ON_UPDATE_COMMAND_UI(ID_EDIT_UNDO,			&CViewPattern::OnUpdateUndo)
 	ON_UPDATE_COMMAND_UI(ID_EDIT_REDO,			&CViewPattern::OnUpdateRedo)
 	ON_COMMAND_RANGE(ID_PLUGSELECT, ID_PLUGSELECT+MAX_MIXPLUGINS, &CViewPattern::OnSelectPlugin)
-
-
 	//}}AFX_MSG_MAP
-	ON_WM_INITMENU()
-	ON_WM_RBUTTONDBLCLK()
-	ON_WM_RBUTTONUP()
 END_MESSAGE_MAP()
 
 static_assert(ModCommand::maxColumnValue <= 999, "Command range for ID_CHANGE_PCNOTE_PARAM is designed for 999");
@@ -1422,16 +1417,7 @@ void CViewPattern::ShowPatternProperties(PATTERNINDEX pat)
 
 void CViewPattern::OnRButtonDown(UINT flags, CPoint pt)
 {
-	CModDoc *modDoc = GetDocument();
-	HMENU hMenu;
-
-	// Too far left to get a ctx menu:
-	if(!modDoc || pt.x < m_szHeader.cx)
-	{
-		return;
-	}
-
-	// Handle drag n drop
+	// Abort drag n drop
 	if(m_Status[psDragnDropEdit])
 	{
 		if(m_Status[psDragnDropping])
@@ -1450,10 +1436,46 @@ void CViewPattern::OnRButtonDown(UINT flags, CPoint pt)
 		return;
 	}
 
-	if((hMenu = ::CreatePopupMenu()) == NULL)
+	const bool inChannelHeader = (pt.y < m_szHeader.cy);
+	if(/*(flags & MK_SHIFT) &&*/ inChannelHeader)
 	{
-		return;
+		// Drag-select record channels
+		StartRecordGroupDragging(GetDragItem(pt, m_rcDragItem));
+		m_Status.set(psShiftDragging, (flags & MK_SHIFT) != 0);
 	}
+}
+
+
+void CViewPattern::OnRButtonUp(UINT flags, CPoint pt)
+{
+	CModDoc *modDoc = GetDocument();
+
+	// Too far left to get a ctx menu:
+	if(!modDoc || pt.x < m_szHeader.cx)
+		return;
+
+	ResetRecordGroupDragging();
+	const CHANNELINDEX sourceChn = static_cast<CHANNELINDEX>(m_nDragItem.Value());
+	const CHANNELINDEX targetChn = m_nDropItem.IsValid() ? static_cast<CHANNELINDEX>(m_nDropItem.Value()) : CHANNELINDEX_INVALID;
+	switch(m_nDragItem.Type())
+	{
+	case DragItem::ChannelHeader:
+		if(m_Status[psShiftDragging])
+		{
+			if(sourceChn < modDoc->GetNumChannels() && sourceChn == targetChn)
+			{
+				modDoc->ToggleChannelRecordGroup(sourceChn, RecordGroup::Group2);
+				InvalidateChannelsHeaders(sourceChn);
+			}
+		}
+		break;
+	default:
+		break;
+	}
+	
+	HMENU hMenu = ::CreatePopupMenu();
+	if(hMenu == nullptr)
+		return;
 
 	CSoundFile &sndFile = modDoc->GetSoundFile();
 	m_MenuCursor = GetPositionFromPoint(pt);
@@ -1477,10 +1499,6 @@ void CViewPattern::OnRButtonDown(UINT flags, CPoint pt)
 		// Ctrl+Right-Click: Open quick channel properties.
 		ClientToScreen(&pt);
 		m_quickChannelProperties.Show(GetDocument(), nChn, pt);
-	} else if((flags & MK_SHIFT) && inChannelHeader)
-	{
-		// Drag-select record channels
-		StartRecordGroupDragging(GetDragItem(pt, m_rcDragItem));
 	} else if(nChn < sndFile.GetNumChannels() && sndFile.Patterns.IsValidPat(m_nPattern) && !(flags & (MK_CONTROL | MK_SHIFT)))
 	{
 		CInputHandler *ih = CMainFrame::GetInputHandler();
@@ -1556,34 +1574,6 @@ void CViewPattern::OnRButtonDown(UINT flags, CPoint pt)
 		::TrackPopupMenu(hMenu, TPM_LEFTALIGN | TPM_RIGHTBUTTON, pt.x, pt.y, 0, m_hWnd, NULL);
 	}
 	::DestroyMenu(hMenu);
-}
-
-void CViewPattern::OnRButtonUp(UINT nFlags, CPoint point)
-{
-	CModDoc *pModDoc = GetDocument();
-	if(!pModDoc)
-		return;
-
-	ResetRecordGroupDragging();
-	const CHANNELINDEX sourceChn = static_cast<CHANNELINDEX>(m_nDragItem.Value());
-	const CHANNELINDEX targetChn = m_nDropItem.IsValid() ? static_cast<CHANNELINDEX>(m_nDropItem.Value()) : CHANNELINDEX_INVALID;
-	switch(m_nDragItem.Type())
-	{
-	case DragItem::ChannelHeader:
-		if(nFlags & MK_SHIFT)
-		{
-			if(sourceChn < pModDoc->GetNumChannels() && sourceChn == targetChn)
-			{
-				pModDoc->ToggleChannelRecordGroup(sourceChn, RecordGroup::Group2);
-				InvalidateChannelsHeaders(sourceChn);
-			}
-		}
-		break;
-	default:
-		break;
-	}
-
-	CModScrollView::OnRButtonUp(nFlags, point);
 }
 
 
@@ -4373,7 +4363,7 @@ LRESULT CViewPattern::OnCustomKeyMsg(WPARAM wParam, LPARAM lParam)
 
 	switch(wParam)
 	{
-		case kcContextMenu: OnRButtonDown(0, GetPointFromPosition(m_Cursor)); return wParam;
+		case kcContextMenu: OnRButtonUp(0, GetPointFromPosition(m_Cursor)); return wParam;
 
 		case kcPrevInstrument:        OnPrevInstrument(); return wParam;
 		case kcNextInstrument:        OnNextInstrument(); return wParam;
@@ -4676,7 +4666,7 @@ LRESULT CViewPattern::OnCustomKeyMsg(WPARAM wParam, LPARAM lParam)
 				CPoint pt = GetPointFromPosition(m_Cursor);
 				pt.x += GetChannelWidth() / 2;
 				pt.y += GetRowHeight() / 2;
-				OnRButtonDown(0, pt);
+				OnRButtonUp(0, pt);
 			}
 			return wParam;
 		case kcShowChannelCtxMenu:
@@ -4684,7 +4674,7 @@ LRESULT CViewPattern::OnCustomKeyMsg(WPARAM wParam, LPARAM lParam)
 				CPoint pt = GetPointFromPosition(m_Cursor);
 				pt.x += GetChannelWidth() / 2;
 				pt.y = (m_szHeader.cy - m_szPluginHeader.cy) / 2;
-				OnRButtonDown(0, pt);
+				OnRButtonUp(0, pt);
 			}
 			return wParam;
 		case kcShowChannelPluginCtxMenu:
@@ -4692,7 +4682,7 @@ LRESULT CViewPattern::OnCustomKeyMsg(WPARAM wParam, LPARAM lParam)
 				CPoint pt = GetPointFromPosition(m_Cursor);
 				pt.x += GetChannelWidth() / 2;
 				pt.y = m_szHeader.cy - m_szPluginHeader.cy / 2;
-				OnRButtonDown(0, pt);
+				OnRButtonUp(0, pt);
 			}
 			return wParam;
 		case kcPatternGoto: OnEditGoto(); return wParam;
@@ -6408,12 +6398,6 @@ void CViewPattern::OnClearField(const std::bitset<PatternCursor::numColumns> mas
 }
 
 
-
-void CViewPattern::OnInitMenu(CMenu *pMenu)
-{
-	CModScrollView::OnInitMenu(pMenu);
-}
-
 void CViewPattern::TogglePluginEditor(int chan)
 {
 	CModDoc *modDoc = GetDocument();
@@ -7076,13 +7060,6 @@ bool CViewPattern::IsInterpolationPossible(ROWINDEX startRow, ROWINDEX endRow, C
 		result = false;
 	}
 	return result;
-}
-
-
-void CViewPattern::OnRButtonDblClk(UINT nFlags, CPoint point)
-{
-	OnRButtonDown(nFlags, point);
-	CModScrollView::OnRButtonDblClk(nFlags, point);
 }
 
 

mptrack/Ctrl_ins.cpp
@@ -54,7 +54,7 @@ BEGIN_MESSAGE_MAP(CNoteMapWnd, CStatic)
 	ON_WM_KILLFOCUS()
 	ON_WM_LBUTTONDOWN()
 	ON_WM_MBUTTONDOWN()
-	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
 	ON_WM_LBUTTONDBLCLK()
 	ON_WM_MOUSEWHEEL()
 	ON_COMMAND(ID_NOTEMAP_TRANS_UP,          &CNoteMapWnd::OnMapTransposeUp)
@@ -322,7 +322,7 @@ void CNoteMapWnd::OnLButtonDblClk(UINT, CPoint)
 }
 
 
-void CNoteMapWnd::OnRButtonDown(UINT, CPoint pt)
+void CNoteMapWnd::OnRButtonUp(UINT, CPoint pt)
 {
 	CInputHandler* ih = CMainFrame::GetInputHandler();
 
@@ -623,7 +623,7 @@ LRESULT CNoteMapWnd::OnCustomKeyMsg(WPARAM wParam, LPARAM lParam)
 			CRect clientRect;
 			GetClientRect(clientRect);
 			clientRect.bottom = clientRect.top + mpt::align_up(clientRect.Height(), m_cyFont);
-			OnRButtonDown(0, clientRect.CenterPoint());
+			OnRButtonUp(0, clientRect.CenterPoint());
 		}
 		return wParam;
 	case kcInsNoteMapTransposeDown:		MapTranspose(-1); return wParam;

mptrack/Ctrl_ins.h
@@ -69,7 +69,7 @@ protected:
 	//{{AFX_MSG(CNoteMapWnd)
 	afx_msg void OnLButtonDown(UINT, CPoint);
 	afx_msg void OnMButtonDown(UINT flags, CPoint pt) { OnLButtonDown(flags, pt); }
-	afx_msg void OnRButtonDown(UINT, CPoint);
+	afx_msg void OnRButtonUp(UINT, CPoint);
 	afx_msg void OnLButtonDblClk(UINT, CPoint);
 	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
 	afx_msg void OnSetFocus(CWnd *pOldWnd);

mptrack/Ctrl_pat.h
@@ -148,7 +148,7 @@ protected:
 	afx_msg void OnKillFocus(CWnd *);
 	afx_msg void OnLButtonDown(UINT, CPoint);
 	afx_msg void OnLButtonDblClk(UINT, CPoint);
-	afx_msg void OnRButtonDown(UINT, CPoint);
+	afx_msg void OnRButtonUp(UINT, CPoint);
 	afx_msg void OnLButtonUp(UINT, CPoint);
 	afx_msg void OnMButtonDown(UINT, CPoint);
 	afx_msg void OnMouseMove(UINT, CPoint);

mptrack/Ctrl_seq.cpp
@@ -52,7 +52,7 @@ BEGIN_MESSAGE_MAP(COrderList, CWnd)
 	ON_WM_LBUTTONDOWN()
 	ON_WM_LBUTTONDBLCLK()
 	ON_WM_LBUTTONUP()
-	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
 	ON_WM_MBUTTONDOWN()
 	ON_WM_SETFOCUS()
 	ON_WM_KILLFOCUS()
@@ -478,7 +478,7 @@ LRESULT COrderList::OnCustomKeyMsg(WPARAM wParam, LPARAM lParam)
 			GetClientRect(clientRect);
 			if(!clientRect.PtInRect(pt))
 				pt = clientRect.CenterPoint();
-			OnRButtonDown(0, pt);
+			OnRButtonUp(0, pt);
 		}
 		return wParam;
 
@@ -1073,7 +1073,7 @@ void COrderList::OnSelectSequence(UINT nid)
 }
 
 
-void COrderList::OnRButtonDown(UINT nFlags, CPoint pt)
+void COrderList::OnRButtonUp(UINT nFlags, CPoint pt)
 {
 	CRect rect;
 	GetClientRect(&rect);

mptrack/Globals.h
@@ -43,7 +43,7 @@ protected:
 	CModControlView &m_parent;
 	HWND m_hWndView = nullptr;
 	HWND m_lastFocusItem = nullptr;
-	LONG m_nLockCount = 0;
+	int m_nLockCount = 0;
 	bool m_initialized = false;
 
 public:

mptrack/Mpdlgs.cpp
@@ -1298,7 +1298,7 @@ void CEQSlider::Init(UINT nID, UINT n, CWnd *parent)
 
 BOOL CEQSlider::PreTranslateMessage(MSG *pMsg)
 {
-	if ((pMsg) && (pMsg->message == WM_RBUTTONDOWN) && (m_pParent))
+	if ((pMsg) && (pMsg->message == WM_RBUTTONUP) && (m_pParent))
 	{
 		m_x = LOWORD(pMsg->lParam);
 		m_y = HIWORD(pMsg->lParam);

mptrack/View_ins.cpp
@@ -92,9 +92,10 @@ BEGIN_MESSAGE_MAP(CViewInstrument, CModScrollView)
 	ON_WM_LBUTTONDOWN()
 	ON_WM_LBUTTONUP()
 	ON_WM_LBUTTONDBLCLK()
-	ON_WM_RBUTTONDOWN()
-	ON_WM_MBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MBUTTONUP()
 	ON_WM_XBUTTONUP()
+	ON_WM_MOUSEWHEEL()
 	ON_WM_NCLBUTTONDOWN()
 	ON_WM_NCLBUTTONUP()
 	ON_WM_NCLBUTTONDBLCLK()
@@ -133,7 +134,6 @@ BEGIN_MESSAGE_MAP(CViewInstrument, CModScrollView)
 	ON_UPDATE_COMMAND_UI(ID_EDIT_REDO,		&CViewInstrument::OnUpdateRedo)
 
 	//}}AFX_MSG_MAP
-	ON_WM_MOUSEWHEEL()
 END_MESSAGE_MAP()
 
 
@@ -1747,7 +1747,7 @@ void CViewInstrument::OnLButtonUp(UINT, CPoint)
 }
 
 
-void CViewInstrument::OnRButtonDown(UINT flags, CPoint pt)
+void CViewInstrument::OnRButtonUp(UINT flags, CPoint pt)
 {
 	const CModDoc *pModDoc = GetDocument();
 	if(!pModDoc)
@@ -1760,7 +1760,7 @@ void CViewInstrument::OnRButtonDown(UINT flags, CPoint pt)
 	// Ctrl + Right-Click = Delete point
 	if(flags & MK_CONTROL)
 	{
-		OnMButtonDown(flags, pt);
+		OnMButtonUp(flags, pt);
 		return;
 	}
 
@@ -1789,7 +1789,7 @@ void CViewInstrument::OnRButtonDown(UINT flags, CPoint pt)
 	}
 }
 
-void CViewInstrument::OnMButtonDown(UINT, CPoint pt)
+void CViewInstrument::OnMButtonUp(UINT, CPoint pt)
 {
 	// Middle mouse button: Remove envelope point
 	int point = ScreenToPoint(pt.x, pt.y);
@@ -2289,7 +2289,7 @@ LRESULT CViewInstrument::OnCustomKeyMsg(WPARAM wParam, LPARAM)
 					uint32 point = DragItemToEnvPoint();
 					pt.SetPoint(PointToScreen(point), ValueToScreen(EnvGetValue(point)));
 				}
-				OnRButtonDown(0, pt);
+				OnRButtonUp(0, pt);
 			}
 			return wParam;
 		case kcPrevInstrument:	OnPrevInstrument(); return wParam;

commit 98a7ce6345fd4978d6878640bb900684dd4f28f0

[Fix] Pattern tab: Right-click record group assignment was kinda broken since r22264.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22292 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/View_pat.cpp
@@ -1437,11 +1437,12 @@ void CViewPattern::OnRButtonDown(UINT flags, CPoint pt)
 	}
 
 	const bool inChannelHeader = (pt.y < m_szHeader.cy);
-	if(/*(flags & MK_SHIFT) &&*/ inChannelHeader)
+	if(inChannelHeader)
 	{
 		// Drag-select record channels
 		StartRecordGroupDragging(GetDragItem(pt, m_rcDragItem));
 		m_Status.set(psShiftDragging, (flags & MK_SHIFT) != 0);
+		m_bInItemRect = true;
 	}
 }
 
@@ -1467,6 +1468,8 @@ void CViewPattern::OnRButtonUp(UINT flags, CPoint pt)
 				modDoc->ToggleChannelRecordGroup(sourceChn, RecordGroup::Group2);
 				InvalidateChannelsHeaders(sourceChn);
 			}
+			m_Status.reset(psShiftDragging);
+			return;
 		}
 		break;
 	default:
@@ -1643,7 +1646,7 @@ void CViewPattern::OnMouseMove(UINT nFlags, CPoint point)
 			InvalidateRect(&m_rcDragItem, FALSE);
 
 			// Drag-select record channels
-			if(isDraggingRecordGroup && m_nDropItem.Type() == DragItem::ChannelHeader)
+			if(isDraggingRecordGroup && m_nDropItem.Type() == DragItem::ChannelHeader && m_Status[psShiftDragging])
 			{
 				auto modDoc = GetDocument();
 				auto startChn = static_cast<CHANNELINDEX>(m_nDragItem.Value());