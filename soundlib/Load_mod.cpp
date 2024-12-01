39b26874aeba523a3315baffdb3c0fdbc1e28950
Johannes Schultz <sagamusix@openmpt.org>
2024-12-01 12:15:19 +0000

    [Fix] Fix compilation.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@22401 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_mod.cpp
+++ soundlib/Load_mod.cpp
@@ -1307,13 +1307,7 @@ bool CSoundFile::ReadMOD(FileReader &file, ModLoadingFlags loadFlags)
 				if(++emptySampleReferences > 1)
 				{
 #ifdef MPT_EXTERNAL_SAMPLES
-					mpt::ustring filenameHint;
-					if(!file.GetFileName().empty())
-					{
-						const auto filename = file.GetFileName().GetFilename().ToUnicode();
-						filenameHint = mpt::format(U_(" ({}.nt or {}.as)"))(filename, filename);
-					}
-					AddToLog(LogWarning, mpt::format(U_("This Startrekker AM file is most likely missing its companion file{}. Synthesized instruments will not play."))(filenameHint));
+					AddToLog(LogWarning, U_("This Startrekker AM file is most likely missing its companion file. Synthesized instruments will not play."));
 #else
 					AddToLog(LogWarning, U_("This appears to be a Startrekker AM file with external synthesizes instruments. External instruments are currently not supported."));
 #endif  // MPT_EXTERNAL_SAMPLES
