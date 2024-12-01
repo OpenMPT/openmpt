70279b2bb1d9796f0e66c187988758f81ce25925
Johannes Schultz <sagamusix@openmpt.org>
2024-12-01 11:35:40 +0000

    Merged revision(s) 22056, 22377 from trunk/OpenMPT: [Imp] Warn when Startrekker AM companion file for synthesized instruments is most likely missing. ........ [Imp] Allow Startrekker AM external sample check to also work for libopenmpt. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@22380 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- soundlib/Load_mod.cpp
+++ soundlib/Load_mod.cpp
@@ -1038,6 +1038,7 @@ bool CSoundFile::ReadMOD(FileReader &file, ModLoadingFlags loadFlags)
 
 	// Reading patterns
 	Patterns.ResizeArray(numPatterns);
+	std::bitset<32> referencedSamples;
 	for(PATTERNINDEX pat = 0; pat < numPatterns; pat++)
 	{
 		ModCommand *rowBase = nullptr;
@@ -1145,6 +1146,8 @@ bool CSoundFile::ReadMOD(FileReader &file, ModLoadingFlags loadFlags)
 				if(m.instr != 0)
 				{
 					lastInstrument[chn] = m.instr;
+					if(isStartrekker)
+						referencedSamples.set(m.instr & 0x1F);
 				}
 			}
 			if(hasSpeedOnRow && hasTempoOnRow)
@@ -1271,7 +1274,7 @@ bool CSoundFile::ReadMOD(FileReader &file, ModLoadingFlags loadFlags)
 		m_nInstruments = 31;
 #endif
 
-		for(SAMPLEINDEX smp = 1; smp <= m_nInstruments; smp++)
+		for(SAMPLEINDEX smp = 1; smp <= GetNumInstruments(); smp++)
 		{
 			// For Startrekker AM synthesis, we need instrument envelopes.
 			ModInstrument *ins = AllocateInstrument(smp, smp);
@@ -1293,6 +1296,32 @@ bool CSoundFile::ReadMOD(FileReader &file, ModLoadingFlags loadFlags)
 		}
 	}
 #endif  // MPT_EXTERNAL_SAMPLES || MPT_BUILD_FUZZER
+
+	if((loadFlags & loadSampleData) && isStartrekker && !m_nInstruments)
+	{
+		uint8 emptySampleReferences = 0;
+		for(SAMPLEINDEX smp = 1; smp <= 31; smp++)
+		{
+			if(referencedSamples[smp] && !Samples[smp].nLength)
+			{
+				if(++emptySampleReferences > 1)
+				{
+					mpt::ustring filenameHint;
+					if(file.GetOptionalFileName())
+					{
+						const auto filename = file.GetOptionalFileName()->GetFilename().ToUnicode();
+						filenameHint = MPT_UFORMAT(" ({}.nt or {}.as)")(filename, filename);
+					}
+#ifdef MPT_EXTERNAL_SAMPLES
+					AddToLog(LogWarning, MPT_UFORMAT("This Startrekker AM file is most likely missing its companion file{}. Synthesized instruments will not play.")(filenameHint));
+#else
+					AddToLog(LogWarning, U_("This appears to be a Startrekker AM file with external synthesizes instruments. External instruments are currently not supported."));
+#endif  // MPT_EXTERNAL_SAMPLES
+					break;
+				}
+			}
+		}
+	}
 
 	// Fix VBlank MODs. Arbitrary threshold: 8 minutes (enough for "frame of mind" by Dascon...).
 	// Basically, this just converts all tempo commands into speed commands
