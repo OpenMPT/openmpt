d853b5189d4dc17f11598d28b72aa5bf1e8f43be
Jörn Heusipp <manx@openmpt.org>
2025-01-02 21:09:59 +0000

    Merged revision(s) 22699 from trunk/OpenMPT: [Fix] xmp-openmpt: Update for newer XMPlay SDK 2025-02-01. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@22700 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/xmp-openmpt/xmp-openmpt.cpp
+++ libopenmpt/xmp-openmpt/xmp-openmpt.cpp
@@ -998,6 +998,7 @@ static void WINAPI openmpt_SetFormat( XMPFORMAT * form ) {
 		form->rate = 0;
 		form->chan = 0;
 		form->res = 0;
+		form->chanmask = 0;
 		return;
 	}
 	if ( self->settings.samplerate != 0 ) {
@@ -1026,6 +1027,7 @@ static void WINAPI openmpt_SetFormat( XMPFORMAT * form ) {
 		}
 	}
 	form->res = 4; // float
+	form->chanmask = 0;
 }
 
 // get the tags
