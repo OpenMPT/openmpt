OpenMPT/openmpt

commit 796b3a0dae9032dbdf46b2b384a90efe24122e13

[Mod] Set working directory to application directory on launch. This should help further with https://bugs.openmpt.org/view.php?id=1444 and also avoid other code looking for files in wrong places (e.g. I could observe audio backends trying to load wdmaud.drv from the current directory).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@14621 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Mptrack.cpp
@@ -753,6 +753,9 @@ BOOL CTrackApp::InitInstanceEarly(CMPTCommandLineInfo &cmdInfo)
 		_CrtSetDebugFillThreshold(0); // Disable buffer filling in secure enhanced CRT functions.
 	#endif
 
+	// Avoid e.g. audio APIs trying to load wdmaud.drv from arbitrary working directory
+	::SetCurrentDirectory(mpt::GetExecutablePath().AsNative().c_str());
+
 	// Initialize OLE MFC support
 	BOOL oleinit = AfxOleInit();
 	ASSERT(oleinit != FALSE); // no MPT_ASSERT here!

commit 63e38ae040749cb0441767a03f1e2af54115afd5

[Fix] Passing a relative path as command-line argument was broken again since r14621, and probably never worked correctly when such a path was passed to another OpenMPT instance.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22805 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Mptrack.cpp
@@ -329,6 +329,13 @@ public:
 #endif
 
 public:
+	CMPTCommandLineInfo()
+	{
+		std::vector<TCHAR> curDir(::GetCurrentDirectory(0, nullptr), _T('\0'));
+		::GetCurrentDirectory(static_cast<DWORD>(curDir.size()), curDir.data());
+		m_workingDir = mpt::PathString::FromNative(curDir.data());
+	}
+
 	void ParseParam(LPCTSTR param, BOOL isFlag, BOOL isLast) override
 	{
 		if(isFlag)
@@ -350,11 +357,14 @@ public:
 #endif
 		} else
 		{
-			m_fileNames.push_back(mpt::PathString::FromNative(param));
+			m_fileNames.push_back(mpt::RelativePathToAbsolute(mpt::PathString::FromNative(param), m_workingDir));
 			if(m_nShellCommand == FileNew) m_nShellCommand = FileOpen;
 		}
 		CCommandLineInfo::ParseParam(param, isFlag, isLast);
 	}
+
+protected:
+	mpt::PathString m_workingDir;
 };
 
 
@@ -1088,7 +1098,7 @@ static bool ProcessorCanRunCurrentBuild()
 	return true;
 }
 
-static bool SystemCanRunCurrentBuild() 
+static bool SystemCanRunCurrentBuild()
 {
 	if(mpt::OS::Windows::IsOriginal())
 	{