a4574e805b03b8f0f2529a5d6545c9c6b7e83e86
Jörn Heusipp <manx@openmpt.org>
2025-01-06 08:44:05 +0000

    [Var] mpt/check/libcxx.hpp: Fix typo.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22813 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/mpt/check/libcxx.hpp
+++ src/mpt/check/libcxx.hpp
@@ -9,7 +9,7 @@
 
 #ifndef MPT_CHECK_LIBCXX_IGNORE_WARNING_NO_THREADS
 #if MPT_OS_WINDOWS && MPT_WIN_BEFORE(MPT_WIN_7) && MPT_LIBCXX_GNU_AT_LEAST(13) && !defined(_GLIBCXX_HAS_GTHREADS)
-#error "GNU libstdc++ is compiled without gthreads support (likely due to using Win32 threading model as opposed to POSIX or mcfgthread threading model. This a severely crippled C++11 implementation and no is no longer supported ofr libstdc++ version 13 or later."
+#error "GNU libstdc++ is compiled without gthreads support (likely due to using Win32 threading model as opposed to POSIX or mcfgthread threading model. This a severely crippled C++11 implementation and no is no longer supported for libstdc++ version 13 or later."
 #endif
 #endif
 
