5a9713fbdaffd5922b9873648298f074583814c5
Johannes Schultz <sagamusix@openmpt.org>
2025-01-09 20:12:05 +0000

    Merged revision(s) 22852-22853 from trunk/OpenMPT: [Doc] Update HTMLHelp path documentation. ........ [Fix] If multiple keyboard layouts were installed, scancode conversion could be based on the wrong layout, causing e.g. B-4 and Note-Off keys to be swapped on a setup with active US keyboard layout but also a German layout being present. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@22854 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/CommandSet.cpp
+++ mptrack/CommandSet.cpp
@@ -511,6 +511,15 @@ constexpr struct
 };
 // clang-format on
 
+std::vector<HKL> GetKeyboardLayouts()
+{
+	std::vector<HKL> layouts(GetKeyboardLayoutList(0, nullptr));
+	GetKeyboardLayoutList(static_cast<int>(layouts.size()), layouts.data());
+	// GetKeyboardLayoutList appears to return the layouts in no particular order. Always force the active layout to be evaluated first.
+	layouts.insert(layouts.begin(), GetKeyboardLayout(0));
+	return layouts;
+}
+
 };  // namespace
 
 #ifdef MPT_ALL_LOGGING
@@ -2127,8 +2136,7 @@ ctx:UID:Description:Modifier:Key:EventMask
 	     "//----------------------------------------------------------------------\n"
 	     "version:" << mpt::ToCharset(mpt::Charset::ASCII, Version::Current().ToUString()) << "\n";
 
-	std::vector<HKL> layouts(GetKeyboardLayoutList(0, nullptr));
-	GetKeyboardLayoutList(static_cast<int>(layouts.size()), layouts.data());
+	const std::vector<HKL> layouts = GetKeyboardLayouts();
 
 	for(int ctx = 0; ctx < kCtxMaxInputContexts; ctx++)
 	{
@@ -2185,8 +2193,7 @@ bool CCommandSet::LoadFile(std::istream &iStrm, const mpt::ustring &filenameDesc
 	CString errText;
 	int errorCount = 0;
 
-	std::vector<HKL> layouts(GetKeyboardLayoutList(0, nullptr));
-	GetKeyboardLayoutList(static_cast<int>(layouts.size()), layouts.data());
+	const std::vector<HKL> layouts = GetKeyboardLayouts();
 
 	const std::string whitespace(" \n\r\t");
 	while(iStrm.getline(s, std::size(s)))
@@ -2330,8 +2337,7 @@ void CCommandSet::ApplyDefaultKeybindings(const Version onlyCommandsAfterVersion
 		QuickChange_SetEffects(*specs);
 	}
 
-	std::vector<HKL> layouts(GetKeyboardLayoutList(0, nullptr));
-	GetKeyboardLayoutList(static_cast<int>(layouts.size()), layouts.data());
+	const std::vector<HKL> layouts = GetKeyboardLayouts();
 
 	const auto previousVersion = TrackerSettings::Instance().PreviousSettingsVersion;
 
@@ -2558,8 +2564,7 @@ bool CCommandSet::QuickChange_SetEffects(const CModSpecifications &modSpecs)
 		{
 			// Hack for situations where a non-latin keyboard layout without A...Z key code mapping may the current layout (e.g. Russian),
 			// but a latin layout (e.g. EN-US) is installed as well.
-			std::vector<HKL> layouts(GetKeyboardLayoutList(0, nullptr));
-			GetKeyboardLayoutList(static_cast<int>(layouts.size()), layouts.data());
+			const std::vector<HKL> layouts = GetKeyboardLayouts();
 			SHORT codeNmod = -1;
 			for(auto i = layouts.begin(); i != layouts.end() && codeNmod == -1; i++)
 			{
