0e0b407882b90a5cd4fc6ec75f333dae98276b2e
Johannes Schultz <sagamusix@openmpt.org>
2025-01-09 18:15:25 +0000

    [Var] Update manual generator to cope with MediaWiki 1.43 output. CSS now uses variables in a few places, which the CHM viewer (probably still using IE10 under the hood) does not comprehend.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22850 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/manual_generator/wiki.py
+++ mptrack/manual_generator/wiki.py
@@ -17,10 +17,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 shutil.rmtree('html', ignore_errors=True)
 shutil.copytree('source', 'html')
 
-style = urlopen(base_url + '/load.php?debug=false&lang=en&modules=mediawiki.legacy.commonPrint%2Cshared%7Cmediawiki.page.gallery.styles%7Cmediawiki.skinning.interface%7Cskins.vector.styles%7Csite.styles&only=styles&skin=vector').read().decode('UTF-8')
+style = urlopen(base_url + '/load.php?debug=false&lang=en&modules=mediawiki.page.gallery.styles%7Cmediawiki.skinning.interface%7Cskins.vector.styles%7Csite.styles&only=styles&skin=vector').read().decode('UTF-8')
 
 # Remove a few unused CSS classes
 style = re.sub(r'\}(\w+)?[\.#]vector([\w >]+)\{.+?\}', '}', style)
+style = re.sub(r'var\([A-Za-z\-]+,([^)]*)\)', '\\1', style)
 style_file = open('html/style.css', 'w')
 style_file.write(style)
 style_file.close()
