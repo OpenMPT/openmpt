OpenMPT/openmpt

commit a992cef2e03377f5e274f4ea368eb61b636b32a4

[Imp] build: Makefile: Unconditionally set --no-undefined linker option. Without the typo, this should hopefully work on all supported platforms.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22884 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/make/warnings-clang.mk
@@ -15,10 +15,12 @@ endif
 #CXXFLAGS_WARNINGS += -Wconversion
 #CXXFLAGS_WARNINGS += -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++98-c++11-c++14-compat -Wno-padded -Wno-weak-vtables -Wno-sign-conversion -Wno-shadow-field-in-constructor -Wno-conversion -Wno-switch-enum -Wno-old-style-cast
 
+LDFLAGS_WARNINGS  += -Wl,--no-undefined
+
 ifeq ($(MODERN),1)
 CXXFLAGS_WARNINGS += 
 CFLAGS_WARNINGS   += 
-LDFLAGS_WARNINGS  += -Wl,--no-undefined
+LDFLAGS_WARNINGS  += 
 endif
 
 CFLAGS_SILENT += -Wno-\#warnings

build/make/warnings-gcc.mk
@@ -4,6 +4,8 @@ CFLAGS_WARNINGS   += -Wcast-align -Wcast-qual -Wdouble-promotion -Wfloat-convers
 
 CXXFLAGS_WARNINGS += -Wno-psabi
 
+LDFLAGS_WARNINGS  += -Wl,--no-undefined
+
 ifeq ($(MODERN),1)
 # GCC >= 12
 # -Wconversion is way too noisy for earlier GCC versions
@@ -11,7 +13,7 @@ CFLAGS_WARNINGS += -Wframe-larger-than=4000
 #CXXFLAGS_WARNINGS += -Wshadow -Wswitch-enum 
 CXXFLAGS_WARNINGS += -Wconversion
 # gold
-LDFLAGS_WARNINGS  += -Wl,--no-undefined -Wl,--detect-odr-violations
+LDFLAGS_WARNINGS  += -Wl,--detect-odr-violations
 # GCC 8
 CXXFLAGS_WARNINGS += -Wcast-align=strict
 CFLAGS_WARNINGS   += -Wcast-align=strict

commit 5b50a2f0b60e494017e8a28ca5efcf3e06a39fe3

[Fix] build: Makefile: Emscripten: Fix build. Broken since r22884.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22941 56274372-70c3-4bfc-bfc3-4c3a0b034d27

build/make/config-emscripten.mk
@@ -144,6 +144,8 @@ CFLAGS   += -s DISABLE_EXCEPTION_CATCHING=0 -fno-strict-aliasing
 LDFLAGS  += -s DISABLE_EXCEPTION_CATCHING=0 -s ERROR_ON_UNDEFINED_SYMBOLS=1 -s ERROR_ON_MISSING_LIBRARIES=1 -s EXPORT_NAME="'libopenmpt'"
 SO_LDFLAGS += -s EXPORTED_FUNCTIONS="['_malloc','_free']"
 
+NO_NO_UNDEFINED_LINKER_FLAG=1
+
 include build/make/warnings-clang.mk
 
 REQUIRES_RUNPREFIX=1

build/make/warnings-clang.mk
@@ -15,7 +15,9 @@ endif
 #CXXFLAGS_WARNINGS += -Wconversion
 #CXXFLAGS_WARNINGS += -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++98-c++11-c++14-compat -Wno-padded -Wno-weak-vtables -Wno-sign-conversion -Wno-shadow-field-in-constructor -Wno-conversion -Wno-switch-enum -Wno-old-style-cast
 
+ifneq ($(NO_NO_UNDEFINED_LINKER_FLAG),1)
 LDFLAGS_WARNINGS  += -Wl,--no-undefined
+endif
 
 ifeq ($(MODERN),1)
 CXXFLAGS_WARNINGS += 

build/make/warnings-gcc.mk
@@ -4,7 +4,9 @@ CFLAGS_WARNINGS   += -Wcast-align -Wcast-qual -Wdouble-promotion -Wfloat-convers
 
 CXXFLAGS_WARNINGS += -Wno-psabi
 
+ifneq ($(NO_NO_UNDEFINED_LINKER_FLAG),1)
 LDFLAGS_WARNINGS  += -Wl,--no-undefined
+endif
 
 ifeq ($(MODERN),1)
 # GCC >= 12