OpenMPT/openmpt

commit fbca1410e4da61e3f0df4c8de7cd7761103ae512

[Ref] mptFileIO: Move libopenmpt build-time warning into libopenmpt/libopenmpt_impl.cpp.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22980 56274372-70c3-4bfc-bfc3-4c3a0b034d27

libopenmpt/libopenmpt_impl.cpp
@@ -77,6 +77,12 @@ MPT_WARNING("Warning: Platform (Windows) supports multi-threading, however the t
 MPT_WARNING("Warning: libopenmpt is known to trigger bad code generation with Clang 5..10 on powerpc (32bit) when using -O3. See <https://bugs.llvm.org/show_bug.cgi?id=46683>.")
 #endif
 
+#if defined(MPT_COMPILER_QUIRK_WINDOWS_FSTREAM_NO_WCHAR)
+#if MPT_GCC_BEFORE(9,1,0)
+MPT_WARNING("Warning: MinGW with GCC earlier than 9.1 detected. Standard library does neither provide std::fstream wchar_t overloads nor std::filesystem with wchar_t support. Unicode filename support is thus unavailable.")
+#endif // MPT_GCC_AT_LEAST(9,1,0)
+#endif // MPT_COMPILER_QUIRK_WINDOWS_FSTREAM_NO_WCHAR
+
 #endif // !MPT_BUILD_SILENCE_LIBOPENMPT_CONFIGURATION_WARNINGS
 
 #if defined(MPT_ASSERT_HANDLER_NEEDED) && !defined(ENABLE_TESTS)

common/mptFileIO.cpp
@@ -25,18 +25,6 @@ OPENMPT_NAMESPACE_BEGIN
 
 #if defined(MPT_ENABLE_FILEIO)
 
-
-#if !defined(MPT_BUILD_SILENCE_LIBOPENMPT_CONFIGURATION_WARNINGS)
-
-#if defined(MPT_COMPILER_QUIRK_WINDOWS_FSTREAM_NO_WCHAR)
-#if MPT_GCC_BEFORE(9,1,0)
-MPT_WARNING("Warning: MinGW with GCC earlier than 9.1 detected. Standard library does neither provide std::fstream wchar_t overloads nor std::filesystem with wchar_t support. Unicode filename support is thus unavailable.")
-#endif // MPT_GCC_AT_LEAST(9,1,0)
-#endif // MPT_COMPILER_QUIRK_WINDOWS_FSTREAM_NO_WCHAR
-
-#endif // !MPT_BUILD_SILENCE_LIBOPENMPT_CONFIGURATION_WARNINGS
-
-
 #ifdef MODPLUG_TRACKER
 
 #if MPT_OS_WINDOWS

commit f13209bfcd591a1c0b406cdbe8e6d404ae550f00

[Fix] libopenmpt: Only show build-time MinGW Unicode warning when it actually applies. Broken since r22980.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22983 56274372-70c3-4bfc-bfc3-4c3a0b034d27

libopenmpt/libopenmpt_impl.cpp
@@ -77,11 +77,13 @@ MPT_WARNING("Warning: Platform (Windows) supports multi-threading, however the t
 MPT_WARNING("Warning: libopenmpt is known to trigger bad code generation with Clang 5..10 on powerpc (32bit) when using -O3. See <https://bugs.llvm.org/show_bug.cgi?id=46683>.")
 #endif
 
+#if defined(MPT_ENABLE_FILEIO)
 #if defined(MPT_COMPILER_QUIRK_WINDOWS_FSTREAM_NO_WCHAR)
 #if MPT_GCC_BEFORE(9,1,0)
 MPT_WARNING("Warning: MinGW with GCC earlier than 9.1 detected. Standard library does neither provide std::fstream wchar_t overloads nor std::filesystem with wchar_t support. Unicode filename support is thus unavailable.")
 #endif // MPT_GCC_AT_LEAST(9,1,0)
 #endif // MPT_COMPILER_QUIRK_WINDOWS_FSTREAM_NO_WCHAR
+#endif // MPT_ENABLE_FILEIO
 
 #endif // !MPT_BUILD_SILENCE_LIBOPENMPT_CONFIGURATION_WARNINGS
 