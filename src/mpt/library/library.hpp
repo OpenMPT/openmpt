OpenMPT/openmpt

commit 18c9764c06b5b0ae9813234842bc2cf17330c4ca

[Ref] mpt/base/utility.hpp: Add mpt::function_pointer_cast<Tdst>(Tsrc). [Fix] mpt/base/utility.hpp: Silence Clang 19 warning that warns when casting between function pointer types of different signature, which is a necessity to do when using WinAPI GetProcAddress(). [Ref] mpt/library/library.hpp: Do not cast from void* to void(*)(void) and then to the destination function pointer. Instead, do the cast directly. [Ref] Replace all reinterpret_cast function pointer casts with mpt::function_pointer_cast. [Ref] mptLibrary: Use mpt::library::bind() instead of mpt::library::get_address().
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@22969 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/library/library.hpp
@@ -9,6 +9,7 @@
 #include "mpt/base/macros.hpp"
 #include "mpt/base/namespace.hpp"
 #include "mpt/base/saturate_cast.hpp"
+#include "mpt/base/utility.hpp"
 #include "mpt/detect/dl.hpp"
 #include "mpt/detect/ltdl.hpp"
 #include "mpt/fs/common_directories.hpp"
@@ -315,8 +316,8 @@ public:
 		return library{hModule};
 	}
 
-	func_ptr get_address(const std::string & symbol) const {
-		return reinterpret_cast<func_ptr>(::GetProcAddress(m_hModule, symbol.c_str()));
+	auto get_address(const std::string & symbol) const {
+		return ::GetProcAddress(m_hModule, symbol.c_str());
 	}
 
 	~library() {
@@ -370,8 +371,8 @@ public:
 		return library{handle};
 	}
 
-	func_ptr get_address(const std::string & symbol) const {
-		return reinterpret_cast<func_ptr>(dlsym(handle, symbol.c_str()));
+	auto get_address(const std::string & symbol) const {
+		return dlsym(handle, symbol.c_str());
 	}
 
 	~library() {
@@ -428,8 +429,8 @@ public:
 		return library{handle};
 	}
 
-	func_ptr get_address(const std::string & symbol) const {
-		return reinterpret_cast<func_ptr>(lt_dlsym(handle, symbol.c_str()));
+	auto get_address(const std::string & symbol) const {
+		return lt_dlsym(handle, symbol.c_str());
 	}
 
 	~library() {
@@ -468,7 +469,7 @@ public:
 		return std::nullopt;
 	}
 
-	func_ptr get_address(const std::string & symbol) const {
+	void * get_address(const std::string & symbol) const {
 		MPT_UNUSED(symbol);
 		return nullptr;
 	}
@@ -486,9 +487,13 @@ public:
 		// Issue is similar to <https://connect.microsoft.com/VisualStudio/feedback/details/774720/stl-is-function-bug>.
 		static_assert(std::is_function<Tfunc>::value);
 #endif
-		const func_ptr addr = get_address(symbol);
-		f = reinterpret_cast<Tfunc *>(addr);
-		return (addr != nullptr);
+		auto sym_ptr = get_address(symbol);
+		if constexpr (std::is_same<decltype(sym_ptr), void>::value) {
+			f = reinterpret_cast<Tfunc *>(sym_ptr);
+		} else {
+			f = mpt::function_pointer_cast<Tfunc *>(sym_ptr);
+		}
+		return (sym_ptr != nullptr);
 	}
 };
 

misc/WriteMemoryDump.h
@@ -12,6 +12,8 @@
 
 #include "openmpt/all/BuildSettings.hpp"
 
+#include "mpt/base/utility.hpp"
+
 #if MPT_COMPILER_MSVC
 #pragma warning(push)
 #pragma warning(disable:4091) // 'typedef ': ignored on left of '' when no variable is declared
@@ -32,7 +34,7 @@ inline bool WriteMemoryDump(_EXCEPTION_POINTERS *pExceptionInfo, const TCHAR *fi
 	HMODULE hDll = ::LoadLibrary(_T("DBGHELP.DLL"));
 	if(hDll)
 	{
-		MINIDUMPWRITEDUMP pDump = (MINIDUMPWRITEDUMP)::GetProcAddress(hDll, "MiniDumpWriteDump");
+		MINIDUMPWRITEDUMP pDump = mpt::function_pointer_cast<MINIDUMPWRITEDUMP>(::GetProcAddress(hDll, "MiniDumpWriteDump"));
 		if(pDump)
 		{
 

misc/mptLibrary.cpp
@@ -119,7 +119,9 @@ public:
 		{
 			return nullptr;
 		}
-		return lib->get_address(symbol);
+		FuncPtr result = nullptr;
+		lib->bind(result, symbol);
+		return result;
 	}
 
 };

misc/mptLibrary.h
@@ -12,6 +12,8 @@
 
 #include "openmpt/all/BuildSettings.hpp"
 
+#include "mpt/base/utility.hpp"
+
 
 OPENMPT_NAMESPACE_BEGIN
 
@@ -97,7 +99,7 @@ public:
 			static_assert(std::is_function<Tfunc>::value);
 		#endif
 		const FuncPtr addr = GetProcAddress(symbol);
-		f = reinterpret_cast<Tfunc*>(addr);
+		f = mpt::function_pointer_cast<Tfunc*>(addr);
 		return (addr != nullptr);
 	}
 };

mptrack/Vstplug.cpp
@@ -219,9 +219,9 @@ std::pair<Vst::AEffect *, Vst::MainProc> CVstPlugin::LoadPluginInternal(bool mas
 
 	if(library != nullptr && library != INVALID_HANDLE_VALUE)
 	{
-		auto pMainProc = reinterpret_cast<Vst::MainProc>(GetProcAddress(library, "VSTPluginMain"));
+		auto pMainProc = mpt::function_pointer_cast<Vst::MainProc>(GetProcAddress(library, "VSTPluginMain"));
 		if(pMainProc == nullptr)
-			pMainProc = reinterpret_cast<Vst::MainProc>(GetProcAddress(library, "main"));
+			pMainProc = mpt::function_pointer_cast<Vst::MainProc>(GetProcAddress(library, "main"));
 
 		if(pMainProc != nullptr)
 		{

pluginBridge/Bridge.cpp
@@ -368,9 +368,9 @@ void PluginBridge::InitBridge(InitMsg &msg)
 		return;
 	}
 
-	m_mainProc = reinterpret_cast<Vst::MainProc>(GetProcAddress(m_library, "VSTPluginMain"));
+	m_mainProc = mpt::function_pointer_cast<Vst::MainProc>(GetProcAddress(m_library, "VSTPluginMain"));
 	if(m_mainProc == nullptr)
-		m_mainProc = reinterpret_cast<Vst::MainProc>(GetProcAddress(m_library, "main"));
+		m_mainProc = mpt::function_pointer_cast<Vst::MainProc>(GetProcAddress(m_library, "main"));
 
 	if(m_mainProc != nullptr)
 	{

src/mpt/base/utility.hpp
@@ -5,8 +5,7 @@
 
 
 
-#include "mpt/base/detect_compiler.hpp"
-#include "mpt/base/detect_libcxx.hpp"
+#include "mpt/base/detect.hpp"
 #include "mpt/base/namespace.hpp"
 
 #if MPT_CXX_BEFORE(20) || MPT_LIBCXX_LLVM_BEFORE(13000)
@@ -33,6 +32,28 @@ inline namespace MPT_INLINE_NS {
 template <typename Tdst, typename Tsrc>
 MPT_CONSTEXPRINLINE Tdst c_cast(Tsrc && x) {
 	return (Tdst)std::forward<Tsrc>(x);
+}
+
+
+
+template <typename Tdst, typename Tsrc>
+MPT_CONSTEXPRINLINE Tdst function_pointer_cast(Tsrc f) {
+#if !(MPT_OS_WINDOWS && MPT_COMPILER_GCC)
+	// MinGW64 std::is_function is always false for non __cdecl functions.
+	// Issue is similar to <https://connect.microsoft.com/VisualStudio/feedback/details/774720/stl-is-function-bug>.
+	static_assert(std::is_pointer<typename std::remove_cv<Tsrc>::type>::value);
+	static_assert(std::is_pointer<typename std::remove_cv<Tdst>::type>::value);
+	static_assert(std::is_function<typename std::remove_pointer<typename std::remove_cv<Tsrc>::type>::type>::value);
+	static_assert(std::is_function<typename std::remove_pointer<typename std::remove_cv<Tdst>::type>::type>::value);
+#endif
+#if MPT_CLANG_AT_LEAST(19, 0, 0)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-function-type-mismatch"
+#endif
+	return reinterpret_cast<Tdst>(f);
+#if MPT_CLANG_AT_LEAST(19, 0, 0)
+#pragma clang diagnostic pop
+#endif
 }
 
 

commit e7ff2d0c5176abd50de5eed411962c4492beddce

[Fix] mpt/library/library.hpp: Fix bind() on non-Windows. Broken since r22969.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23024 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/library/library.hpp
@@ -488,7 +488,7 @@ public:
 		static_assert(std::is_function<Tfunc>::value);
 #endif
 		auto sym_ptr = get_address(symbol);
-		if constexpr (std::is_same<decltype(sym_ptr), void>::value) {
+		if constexpr (std::is_same<decltype(sym_ptr), void *>::value) {
 			f = reinterpret_cast<Tfunc *>(sym_ptr);
 		} else {
 			f = mpt::function_pointer_cast<Tfunc *>(sym_ptr);