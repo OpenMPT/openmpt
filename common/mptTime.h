OpenMPT/openmpt

commit b6f7db2bdd2de237816d6c1af5a7e910e6765546

[Ref] mptTime: Enable C++20 chrono even when MPT_LIBCXX_QUIRK_CHRONO_DATE_NO_ZONED_TIME. Only use fallback for timezones specifically. [Ref] mptTime: Only reference std::time when needed.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23031 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptTime.h
@@ -20,7 +20,9 @@
 #endif
 #include <string>
 
+#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
 #include <ctime>
+#endif
 
 #if MPT_WINNT_AT_LEAST(MPT_WIN_8)
 #define MPT_FALLBACK_TIMEZONE_WINDOWS_HISTORIC
@@ -66,6 +68,8 @@ namespace mpt
 namespace Date
 {
 
+
+
 #if defined(MODPLUG_TRACKER)
 
 #if MPT_OS_WINDOWS
@@ -83,6 +87,8 @@ mpt::ustring ToUString(uint64 time100ns); // i.e. 2015-01-15 18:32:01.718
 #endif // MPT_OS_WINDOWS
 
 #endif // MODPLUG_TRACKER
+
+
 
 enum class LogicalTimezone
 {
@@ -170,10 +176,14 @@ struct Unix
 	}
 };
 
+#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
+
 inline Unix UnixNow()
 {
 	return Unix{static_cast<int64>(std::time(nullptr))};
 }
+
+#endif
 
 inline int64 UnixAsSeconds(Unix tp)
 {
@@ -199,7 +209,7 @@ Local UnixAsLocal(Unix tp);
 
 } // namespace nochrono
 
-#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE) && !(defined(MODPLUG_TRACKER) && defined(MPT_LIBCXX_QUIRK_CHRONO_DATE_NO_ZONED_TIME))
+#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
 
 using Unix = std::chrono::system_clock::time_point;
 
@@ -262,6 +272,7 @@ inline mpt::Date::UTC UnixAsUTC(Unix tp)
 
 inline mpt::Date::Unix UnixFromLocal(Local local)
 {
+#if !defined(MPT_LIBCXX_QUIRK_CHRONO_DATE_NO_ZONED_TIME)
 	try
 	{
 		std::chrono::time_point<std::chrono::local_t, std::chrono::seconds> local_tp =
@@ -279,6 +290,7 @@ inline mpt::Date::Unix UnixFromLocal(Local local)
 		return std::chrono::zoned_time{std::chrono::current_zone(), local_tp}.get_sys_time();
 #endif
 	} catch(const std::exception &)
+#endif
 	{
 		return mpt::Date::UnixFromSeconds(mpt::Date::nochrono::UnixAsSeconds(mpt::Date::nochrono::UnixFromLocal(local)));
 	}
@@ -286,6 +298,7 @@ inline mpt::Date::Unix UnixFromLocal(Local local)
 
 inline mpt::Date::Local UnixAsLocal(Unix tp)
 {
+#if !defined(MPT_LIBCXX_QUIRK_CHRONO_DATE_NO_ZONED_TIME)
 	try
 	{
 		std::chrono::zoned_time local_tp{ std::chrono::current_zone(), tp };
@@ -301,6 +314,7 @@ inline mpt::Date::Local UnixAsLocal(Unix tp)
 		result.seconds = static_cast<int64>(hms.seconds().count());
 		return result;
 	} catch(const std::exception &)
+#endif
 	{
 		return mpt::Date::nochrono::UnixAsLocal(mpt::Date::nochrono::UnixFromSeconds(mpt::Date::UnixAsSeconds(tp)));
 	}

common/mptTime.cpp
@@ -17,13 +17,17 @@
 
 #include "mptStringBuffer.h"
 
-#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE) && !(defined(MODPLUG_TRACKER) && defined(MPT_LIBCXX_QUIRK_CHRONO_DATE_NO_ZONED_TIME))
+#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
 #include <chrono>
 #endif
 
 #if defined(MODPLUG_TRACKER) && MPT_OS_WINDOWS
 #include <optional>
 #endif // MODPLUG_TRACKER && MPT_OS_WINDOWS
+
+#if defined(MPT_FALLBACK_TIMEZONE_C)
+#include <ctime>
+#endif // MPT_FALLBACK_TIMEZONE_C
 
 #if MPT_OS_WINDOWS
 #include <windows.h>
@@ -37,6 +41,8 @@ namespace mpt
 {
 namespace Date
 {
+
+
 
 #if defined(MODPLUG_TRACKER)
 
@@ -87,6 +93,8 @@ mpt::ustring ToUString(uint64 time100ns)
 #endif // MPT_OS_WINDOWS
 
 #endif // MODPLUG_TRACKER
+
+
 
 namespace nochrono
 {

commit db2d0cd2c911e5d3f9004300156cefd38573c2b7

[Fix] mptTime: Fix inverted preprocessor logic. Broken since r23031.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23034 56274372-70c3-4bfc-bfc3-4c3a0b034d27

common/mptTime.h
@@ -20,7 +20,7 @@
 #endif
 #include <string>
 
-#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
+#if MPT_CXX_BEFORE(20) || defined(MPT_LIBCXX_QUIRK_NO_CHRONO) || defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
 #include <ctime>
 #endif
 
@@ -176,7 +176,7 @@ struct Unix
 	}
 };
 
-#if MPT_CXX_AT_LEAST(20) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
+#if MPT_CXX_BEFORE(20) || defined(MPT_LIBCXX_QUIRK_NO_CHRONO) || defined(MPT_LIBCXX_QUIRK_NO_CHRONO_DATE)
 
 inline Unix UnixNow()
 {