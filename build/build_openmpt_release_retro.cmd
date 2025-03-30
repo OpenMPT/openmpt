8f521398dab1c38a5f8f6f5c7d0a6a428625b48d
Johannes Schultz <sagamusix@openmpt.org>
2025-03-30 16:42:38 +0000

    [Mod] Allow retro builds to be built and signed locally.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23112 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/build_openmpt_release_retro.cmd
+++ build/build_openmpt_release_retro.cmd
@@ -10,9 +10,9 @@ set MY_DIR=%CD%
 
 cmd /c build\download_externals.cmd auto nodownload                                                    || goto error
 cmd /c build\auto\update_package_template_retro.cmd                                                    || goto error
-rem cmd /c build\auto\build_openmpt_args.cmd vs2017 winxp Win32 Release 7z default                         || goto error
-rem cmd /c build\auto\build_openmpt_args.cmd vs2017 winxp x64   Release 7z default                         || goto error
-cmd /c build\auto\build_openmpt_release_packages_retro.cmd auto                                        || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2017 winxp Win32 Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_args.cmd vs2017 winxp x64   Release 7z default                         || goto error
+cmd /c build\auto\build_openmpt_release_packages_retro.cmd auto sign                                   || goto error
 cmd /c build\auto\build_openmpt_update_information_retro.cmd auto                                      || goto error
 cmd /c build\auto\package_openmpt_installer_retro_args.cmd vs2017 winxp Win32 Release 7z default       || goto error
 
