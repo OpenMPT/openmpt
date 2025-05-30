24095867fcc014955d0a6ab754721f5b074ebb40
Johannes Schultz <sagamusix@openmpt.org>
2025-05-30 12:09:34 +0000

    Merged revision(s) 23230 from trunk/OpenMPT: [Var] Update to unrar 7.1.6. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.32@23231 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     7
-#define RARVER_MINOR    10
-#define RARVER_BETA      2
-#define RARVER_DAY       4
-#define RARVER_MONTH    12
-#define RARVER_YEAR   2024
+#define RARVER_MINOR    11
+#define RARVER_BETA      0
+#define RARVER_DAY      20
+#define RARVER_MONTH     3
+#define RARVER_YEAR   2025
