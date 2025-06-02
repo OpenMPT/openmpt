OpenMPT/openmpt

commit 4d457a0c06b78aa9754dc3902950614c3cf54bd8

[Ref] mpt/string_transcode/transcode.hpp: Add MPT_LIBCXX_QUIRK_ASSUME_USER_LOCALE_UTF8 for platforms with broken user locale support that always use UTF8 anyway.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23294 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/string_transcode/transcode.hpp
@@ -1696,6 +1696,9 @@ inline Tdststring encode(logical_encoding encoding, const mpt::widestring & src)
 	switch (encoding) {
 		case logical_encoding::locale:
 #if defined(MPT_LIBCXX_QUIRK_BROKEN_USER_LOCALE)
+#if defined(MPT_LIBCXX_QUIRK_ASSUME_USER_LOCALE_UTF8)
+			return encode_utf<Tdststring>(src);
+#else
 			try {
 				return encode_locale<Tdststring>(std::locale(""), src);
 			} catch (mpt::out_of_memory e) {
@@ -1718,6 +1721,7 @@ inline Tdststring encode(logical_encoding encoding, const mpt::widestring & src)
 				// nothing
 			}
 			return encode_ascii<Tdststring>(src);
+#endif
 #else
 			return encode_locale<Tdststring>(std::locale(""), src);
 #endif
@@ -1917,6 +1921,9 @@ inline mpt::widestring decode(logical_encoding encoding, const Tsrcstring & src)
 	switch (encoding) {
 		case logical_encoding::locale:
 #if defined(MPT_LIBCXX_QUIRK_BROKEN_USER_LOCALE)
+#if defined(MPT_LIBCXX_QUIRK_ASSUME_USER_LOCALE_UTF8)
+			return decode_utf8(src);
+#else
 			try {
 				return decode_locale(std::locale(""), src);
 			} catch (mpt::out_of_memory e) {
@@ -1939,6 +1946,7 @@ inline mpt::widestring decode(logical_encoding encoding, const Tsrcstring & src)
 				// nothing
 			}
 			return decode_ascii(src);
+#endif
 #else
 			return decode_locale(std::locale(""), src);
 #endif

src/mpt/base/detect_quirks.hpp
@@ -264,6 +264,7 @@
 
 #if MPT_OS_CYGWIN
 #define MPT_LIBCXX_QUIRK_BROKEN_USER_LOCALE
+// #define MPT_LIBCXX_QUIRK_ASSUME_USER_LOCALE_UTF8
 #endif
 
 

commit 5f003394ed01f096c02ef93c34f10da5a7b68314

[Fix] mpt/string_transcode/transcode.hpp: Fix typo. Broken since r23294.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23305 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/string_transcode/transcode.hpp
@@ -1697,7 +1697,7 @@ inline Tdststring encode(logical_encoding encoding, const mpt::widestring & src)
 		case logical_encoding::locale:
 #if defined(MPT_LIBCXX_QUIRK_BROKEN_USER_LOCALE)
 #if defined(MPT_LIBCXX_QUIRK_ASSUME_USER_LOCALE_UTF8)
-			return encode_utf<Tdststring>(src);
+			return encode_utf8<Tdststring>(src);
 #else
 			try {
 				return encode_locale<Tdststring>(std::locale(""), src);