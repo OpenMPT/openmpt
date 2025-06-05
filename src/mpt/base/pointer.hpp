OpenMPT/openmpt

commit 7d7f8802e8fb792961c0f7b7a355b565382b4210

[Ref] mpt/base/pointer.hpp: Allow mpt::pointer_cast to also cast from std::uintptr_t to pointer, in addition to the opposite direction.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23356 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/base/pointer.hpp
@@ -21,25 +21,23 @@ inline constexpr std::size_t pointer_size = sizeof(void *);
 
 
 template <typename Tdst, typename Tsrc>
-struct pointer_cast_helper {
-	static constexpr Tdst cast(const Tsrc & src) noexcept {
-		return src;
-	}
-};
+struct pointer_cast_helper;
 
-template <typename Tdst, typename Tptr>
-struct pointer_cast_helper<Tdst, const Tptr *> {
-	static constexpr Tdst cast(const Tptr * const & src) noexcept {
-		return reinterpret_cast<const Tdst>(src);
-	}
-};
 template <typename Tdst, typename Tptr>
 struct pointer_cast_helper<Tdst, Tptr *> {
-	static constexpr Tdst cast(const Tptr * const & src) noexcept {
-		return reinterpret_cast<const Tdst>(src);
+	static constexpr Tdst cast(Tptr * const & src) noexcept {
+		static_assert(sizeof(Tdst) == sizeof(Tptr *));
+		return static_cast<Tdst>(reinterpret_cast<std::uintptr_t>(src));
 	}
 };
 
+template <typename Tptr, typename Tsrc>
+struct pointer_cast_helper<Tptr *, Tsrc> {
+	static constexpr Tptr * cast(const Tsrc & src) noexcept {
+		static_assert(sizeof(Tptr *) == sizeof(Tsrc));
+		return reinterpret_cast<Tptr *>(static_cast<std::uintptr_t>(src));
+	}
+};
 
 template <typename Tdst, typename Tsrc>
 constexpr Tdst pointer_cast(const Tsrc & src) noexcept {

commit b12931b36301a7b78bc0e15a6bf82ac190cec657

[Fix] mpt/base/pointer.hpp: Add missing <cstdint>. Broken since r23356.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23364 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/base/pointer.hpp
@@ -9,6 +9,7 @@
 #include "mpt/base/namespace.hpp"
 
 #include <cstddef>
+#include <cstdint>
 
 
 