963f3d86233159a37785541c904c01700334e9cb
Jörn Heusipp <manx@openmpt.org>
2025-06-21 14:14:40 +0000

    [Imp] mpt/random/device.hpp: Improve and simplify seeding mpt::sane_random_device. We do not actually need to whiten the time bits with CRC, std:seed_seq does that for us.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23600 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- src/mpt/random/device.hpp
+++ src/mpt/random/device.hpp
@@ -12,8 +12,6 @@
 #include "mpt/base/integer.hpp"
 #include "mpt/base/namespace.hpp"
 #include "mpt/chrono/system_clock.hpp"
-#include "mpt/crc/crc.hpp"
-#include "mpt/endian/integer.hpp"
 #include "mpt/mutex/mutex.hpp"
 #include "mpt/out_of_memory/out_of_memory.hpp"
 #include "mpt/random/engine.hpp"
@@ -23,7 +21,6 @@
 #if !defined(MPT_LIBCXX_QUIRK_NO_CHRONO)
 #include <chrono>
 #endif // !MPT_LIBCXX_QUIRK_NO_CHRONO
-#include <iterator>
 #include <limits>
 #include <memory>
 #include <random>
@@ -31,7 +28,7 @@
 #include <vector>
 
 #include <cmath>
-#include <cstring>
+#include <cstddef>
 
 
 
@@ -43,62 +40,25 @@ inline constexpr uint32 DETERMINISTIC_RNG_SEED = 3141592653u; // pi
 
 
 
-template <typename T>
-struct default_random_seed_hash {
-};
-
-template <>
-struct default_random_seed_hash<uint8> {
-	using type = mpt::crc16;
-};
-
-template <>
-struct default_random_seed_hash<uint16> {
-	using type = mpt::crc16;
-};
-
-template <>
-struct default_random_seed_hash<uint32> {
-	using type = mpt::crc32c;
-};
-
-template <>
-struct default_random_seed_hash<uint64> {
-	using type = mpt::crc64_jones;
-};
-
-
 class prng_random_device_time_seeder {
 
 public:
-	template <typename T>
-	inline T generate_seed() {
-		// Note: CRC is actually not that good a choice here, but it is simple and we
-		// already have an implementaion available. Better choices for mixing entropy
-		// would be a hash function with proper avalanche characteristics or a block
-		// or stream cipher with any pre-choosen random key and IV. The only aspect we
-		// really need here is whitening of the bits.
-		typename mpt::default_random_seed_hash<T>::type hash;
-
+	inline std::vector<unsigned int> generate_seeds() {
+		std::vector<unsigned int> seeds;
 		{
-			uint64be time;
-			time = mpt::chrono::default_system_clock::to_unix_nanoseconds(mpt::chrono::default_system_clock::now());
-			std::byte bytes[sizeof(time)];
-			std::memcpy(bytes, &time, sizeof(time));
-			hash(std::begin(bytes), std::end(bytes));
+			const uint64 time = mpt::chrono::default_system_clock::to_unix_nanoseconds(mpt::chrono::default_system_clock::now());
+			seeds.push_back(static_cast<uint32>(time >> 32));
+			seeds.push_back(static_cast<uint32>(time >> 0));
 		}
 #if !defined(MPT_LIBCXX_QUIRK_NO_CHRONO) && !defined(MPT_COMPILER_QUIRK_CHRONO_NO_HIGH_RESOLUTION_CLOCK)
 		// Avoid std::chrono::high_resolution_clock on Emscripten because availability is problematic in AudioWorklet context.
 		{
-			uint64be time;
-			time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock().now().time_since_epoch()).count();
-			std::byte bytes[sizeof(time)];
-			std::memcpy(bytes, &time, sizeof(time));
-			hash(std::begin(bytes), std::end(bytes));
+			const uint64 time = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock().now().time_since_epoch()).count();
+			seeds.push_back(static_cast<uint32>(time >> 32));
+			seeds.push_back(static_cast<uint32>(time >> 0));
 		}
 #endif // !MPT_COMPILER_QUIRK_CHRONO_NO_HIGH_RESOLUTION_CLOCK
-
-		return static_cast<T>(hash.result());
+		return seeds;
 	}
 
 public:
@@ -127,24 +87,14 @@ public:
 private:
 	void init_fallback() {
 		if (!rd_fallback) {
+			std::vector<unsigned int> seeds = mpt::prng_random_device_time_seeder().generate_seeds();
 			if (token.length() > 0) {
-				uint64 seed_val = mpt::prng_random_device_time_seeder().generate_seed<uint64>();
-				std::vector<unsigned int> seeds;
-				seeds.push_back(static_cast<uint32>(seed_val >> 32));
-				seeds.push_back(static_cast<uint32>(seed_val >> 0));
-				for (std::size_t i = 0; i < token.length(); ++i) {
-					seeds.push_back(static_cast<unsigned int>(static_cast<unsigned char>(token[i])));
+				for (const char c : token) {
+					seeds.push_back(static_cast<unsigned int>(static_cast<unsigned char>(c)));
 				}
-				std::seed_seq seed(seeds.begin(), seeds.end());
-				rd_fallback = std::make_unique<std::mt19937>(seed);
-			} else {
-				uint64 seed_val = mpt::prng_random_device_time_seeder().generate_seed<uint64>();
-				unsigned int seeds[2];
-				seeds[0] = static_cast<uint32>(seed_val >> 32);
-				seeds[1] = static_cast<uint32>(seed_val >> 0);
-				std::seed_seq seed(seeds + 0, seeds + 2);
-				rd_fallback = std::make_unique<std::mt19937>(seed);
 			}
+			std::seed_seq seed(seeds.begin(), seeds.end());
+			rd_fallback = std::make_unique<std::mt19937>(seed);
 		}
 	}
 
