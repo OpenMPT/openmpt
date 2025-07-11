893ee65be211d4e6e548fc82af80260705efed6a
Jörn Heusipp <manx@openmpt.org>
2025-07-11 08:25:59 +0000

    Merged revision(s) 23733 from trunk/OpenMPT: [Fix] openmpt123: Apply MPT_COMPILER_SETTING_QUIRK_GCC_BROKEN_IPA fix also in openmpt123. ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@23735 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- openmpt123/openmpt123.cpp
+++ openmpt123/openmpt123.cpp
@@ -41,6 +41,26 @@ static const char * const license =
 #include <sys/types.h>
 #endif
 
+#include "mpt/base/detect_compiler.hpp"
+
+#if MPT_COMPILER_GCC
+
+#ifdef MPT_COMPILER_SETTING_QUIRK_GCC_BROKEN_IPA
+// See <https://gcc.gnu.org/bugzilla/show_bug.cgi?id=115049>.
+#if MPT_GCC_BEFORE(9, 0, 0)
+// Earlier GCC get confused about setting a global function attribute.
+// We need to check for 9.0 instead of 9.1 because of
+// <https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1028580>.
+// It also gets confused when setting global optimization -O1,
+// so we have no way of fixing GCC 8 or earlier.
+//#pragma GCC optimize("O1")
+#else
+#pragma GCC optimize("no-ipa-ra")
+#endif
+#endif // MPT_COMPILER_SETTING_QUIRK_GCC_BROKEN_IPA
+
+#endif // MPT_COMPILER_GCC
+
 #include "mpt/base/check_platform.hpp"
 #include "mpt/base/detect.hpp"
 
