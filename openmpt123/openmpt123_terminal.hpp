OpenMPT/openmpt

commit 82fb95093ec01d6447569235749551dd6871d62f

[Imp] openmpt123: Query screen size on DJGPP.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23764 56274372-70c3-4bfc-bfc3-4c3a0b034d27

openmpt123/openmpt123_terminal.hpp
@@ -33,6 +33,7 @@
 #include <dpmi.h>
 #include <fcntl.h>
 #include <io.h>
+#include <pc.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <termios.h>
@@ -571,10 +572,10 @@ inline void query_terminal_size( int & terminal_width, int & terminal_height ) {
 #endif // MPT_OS_WINDOWS && !MPT_WINRT_BEFORE(MPT_WIN_10)
 #if MPT_OS_DJGPP
 		if ( terminal_width <= 0 ) {
-			terminal_width = 80;
+			terminal_width = ScreenCols();
 		}
 		if ( terminal_height <= 0 ) {
-			terminal_height = 25;
+			terminal_height = ScreenRows();
 		}
 #else
 		if ( terminal_width <= 0 ) {

commit 064ed594aa1d13fa6b7002e711cc0eef34e4ecfa

[Fix] openmpt123: Fix terminal size fallback. Broken since r23764.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23767 56274372-70c3-4bfc-bfc3-4c3a0b034d27

openmpt123/openmpt123_terminal.hpp
@@ -577,14 +577,13 @@ inline void query_terminal_size( int & terminal_width, int & terminal_height ) {
 		if ( terminal_height <= 0 ) {
 			terminal_height = ScreenRows();
 		}
-#else
+#endif
 		if ( terminal_width <= 0 ) {
 			terminal_width = 72;
 		}
 		if ( terminal_height <= 0 ) {
 			terminal_height = 23;
 		}
-#endif
 }
 
 