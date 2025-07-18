b2d8fb6407a7fbb7459c7c6dbdae3f1b9a3f6eb4
Jörn Heusipp <manx@openmpt.org>
2025-07-18 17:21:58 +0000

    [Imp] mpt/arch/feature_fence.hpp: Add mpt::arch::feature_fence, which guards against instruction reordering after checking features. See <https://devblogs.microsoft.com/oldnewthing/20250718-00/?p=111390>.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23804 56274372-70c3-4bfc-bfc3-4c3a0b034d27

---
+++ src/mpt/arch/feature_fence.hpp
@@ -0,0 +1,57 @@
+/* SPDX-License-Identifier: BSL-1.0 OR BSD-3-Clause */
+
+#ifndef MPT_ARCH_FEATURE_FENCE_HPP
+#define MPT_ARCH_FEATURE_FENCE_HPP
+
+
+#include "mpt/base/detect.hpp"
+#include "mpt/base/macros.hpp"
+#include "mpt/base/namespace.hpp"
+
+#include <atomic>
+
+
+
+namespace mpt {
+inline namespace MPT_INLINE_NS {
+
+
+
+namespace arch {
+
+
+
+MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE void feature_fence_aquire() noexcept {
+	std::atomic_signal_fence(std::memory_order::acquire);
+}
+
+MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE void feature_fence_release() noexcept {
+	std::atomic_signal_fence(std::memory_order::release);
+}
+
+class feature_fence_guard {
+public:
+	MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE feature_fence_guard() noexcept {
+		mpt::arch::feature_fence_aquire();
+	}
+	MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE ~feature_fence_guard() noexcept {
+		mpt::arch::feature_fence_release();
+	}
+	feature_fence_guard(feature_fence_guard &&) = delete;
+	feature_fence_guard(const feature_fence_guard &) = delete;
+	feature_fence_guard & operator=(feature_fence_guard &&) = delete;
+	feature_fence_guard & operator=(const feature_fence_guard &) = delete;
+};
+
+
+
+} // namespace arch
+
+
+
+} // namespace MPT_INLINE_NS
+} // namespace mpt
+
+
+
+#endif // MPT_ARCH_FEATURE_FENCE_HPP
