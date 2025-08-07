fb32b57a6b456fc2396d53c9a6d0d9dd88258124
Jörn Heusipp <manx@openmpt.org>
2025-08-07 07:07:29 +0000

    [Mod] build: premake: VS2019: Update Windows SDK from 10.0.19041.0 to 10.0.22621.0. 19041 causes warnings in Desktop builds, and fails to build in UWP builds.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@23988 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake/premake-defaults.lua
+++ build/premake/premake-defaults.lua
@@ -356,7 +356,7 @@
 			systemversion "10.0.19041.0"
 		filter {}
 		filter { "action:vs2019" }
-			systemversion "10.0.19041.0"
+			systemversion "10.0.22621.0"
 		filter {}
 		filter { "action:vs2022" }
 			if _OPTIONS["windows-version"] == "win7" then
