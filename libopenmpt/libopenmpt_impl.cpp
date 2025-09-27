e88c1750e8fcde9444c7355c0b27809e6d256e61
Jörn Heusipp <manx@openmpt.org>
2025-09-27 12:22:30 +0000

    [Fix] mptIO: Avoid GCC 15 warning.
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.29@24227 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- libopenmpt/libopenmpt_impl.cpp
+++ libopenmpt/libopenmpt_impl.cpp
@@ -780,8 +780,8 @@ int module_impl::probe_file_header( std::uint64_t flags, std::istream & stream )
 	if ( stream.bad() ) {
 		throw exception("error reading stream");
 	}
-	const bool seekable = FileDataContainerStdStreamSeekable::IsSeekable( &stream );
-	const uint64 filesize = ( seekable ? FileDataContainerStdStreamSeekable::GetLength( &stream ) : 0 );
+	const bool seekable = FileDataContainerStdStreamSeekable::StaticIsSeekable( &stream );
+	const uint64 filesize = ( seekable ? FileDataContainerStdStreamSeekable::StaticGetLength( &stream ) : 0 );
 	while ( ( size_toread > 0 ) && stream ) {
 		stream.read( buffer + size_read, size_toread );
 		if ( stream.bad() ) {
@@ -827,8 +827,8 @@ int module_impl::probe_file_header( std::uint64_t flags, callback_stream_wrapper
 	fstream.read = stream.read;
 	fstream.seek = stream.seek;
 	fstream.tell = stream.tell;
-	const bool seekable = FileDataContainerCallbackStreamSeekable::IsSeekable( fstream );
-	const uint64 filesize = ( seekable ? FileDataContainerCallbackStreamSeekable::GetLength( fstream ) : 0 );
+	const bool seekable = FileDataContainerCallbackStreamSeekable::StaticIsSeekable( fstream );
+	const uint64 filesize = ( seekable ? FileDataContainerCallbackStreamSeekable::StaticGetLength( fstream ) : 0 );
 	while ( size_toread > 0 ) {
 		std::size_t read_count = stream.read( stream.stream, buffer + size_read, size_toread );
 		size_read += read_count;
