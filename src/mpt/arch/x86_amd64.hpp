OpenMPT/openmpt

commit 6563fdbafaf7251568119d3e0d6cb75ca3614c30

[Ref] mpt/arch/arch.hpp: Prevent accidentally calling mpt::arch::current::cpu_info default constructor.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@24476 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/arch/x86_amd64.hpp
@@ -1019,6 +1019,12 @@ private:
 
 public:
 
+	[[nodiscard]] MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE static cpu_info query() noexcept {
+		return cpu_info{};
+	}
+
+private:
+
 	MPT_ATTR_NOINLINE MPT_DECL_NOINLINE cpu_info() noexcept {
 
 #if MPT_ARCH_X86 || MPT_ARCH_AMD64

src/mpt/arch/arch.hpp
@@ -87,6 +87,10 @@ inline constexpr mode_flags none = mode_flags{};
 
 struct cpu_info {
 public:
+	[[nodiscard]] MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE static cpu_info query() noexcept {
+		return cpu_info{};
+	}
+private:
 	cpu_info() = default;
 public:
 	MPT_ATTR_ALWAYSINLINE MPT_INLINE_FORCE constexpr bool operator[](feature_flags) const noexcept {
@@ -132,7 +136,7 @@ namespace current = unknown;
 using cpu_info = mpt::arch::current::cpu_info;
 
 MPT_ATTR_NOINLINE MPT_DECL_NOINLINE inline const cpu_info & get_cpu_info() {
-	static cpu_info info;
+	static cpu_info info = cpu_info::query();
 	return info;
 }
 

commit b7fd0c938f24b77b84fa9ea4d02cab271908d709

[Fix] mpt/arch/x86_amd64.hpp: Compile fix. Broken since r24476.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@24478 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/arch/x86_amd64.hpp
@@ -1746,7 +1746,7 @@ public:
 #ifdef MPT_ARCH_X86_FXSR
 		return true;
 #else
-		return cpu_info{}[mpt::arch::x86::feature::fxsr];
+		return mpt::arch::x86::cpu_info::query()[mpt::arch::x86::feature::fxsr];
 #endif
 	}
 
@@ -1754,7 +1754,7 @@ public:
 #ifdef MPT_ARCH_X86_SSE
 		return true;
 #else
-		const cpu_info cpu_info;
+		const cpu_info cpu_info{mpt::arch::x86::cpu_info::query()};
 		return cpu_info[mpt::arch::x86::feature::sse] && cpu_info[mpt::arch::x86::mode::xmm128sse];
 #endif
 	}
@@ -1763,11 +1763,10 @@ public:
 #ifdef MPT_ARCH_X86_FSIN
 		return 3;
 #elif defined(MPT_ARCH_X86_FPU)
-		return cpu_info{}[mpt::arch::x86::feature::fsin] ? 3 : 2;
+		return mpt::arch::x86::cpu_info::query()[mpt::arch::x86::feature::fsin] ? 3 : 2;
 #else
-		cpu_info tmp{};
-		return tmp[mpt::arch::x86::feature::fsin] ? 3 : tmp[mpt::arch::x86::feature::fpu] ? 2
-																						  : 0;
+		const cpu_info cpu_info{mpt::arch::x86::cpu_info::query()};
+		return cpu_info[mpt::arch::x86::feature::fsin] ? 3 : cpu_info[mpt::arch::x86::feature::fpu] ? 2 : 0;
 #endif
 	}
 