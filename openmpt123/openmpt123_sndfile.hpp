OpenMPT/openmpt

commit dbc507d89241983ef2c29ae4eabeb729d3f05b1f

[Fix] openmpt123: We need to also consider file extensions of subformats. This is necessary because MP3 is a subformat and the major format lists only .m1a as file extension.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@23436 56274372-70c3-4bfc-bfc3-4c3a0b034d27

openmpt123/openmpt123_sndfile.hpp
@@ -106,7 +106,7 @@ private:
 					switch ( match_mode ) {
 					case match_print:
 						log << MPT_USTRING("sndfile: ")
-						    << mpt::transcode<mpt::ustring>( sndfile_encoding, ( format_info.name ? format_info.name : "" ) ) << MPT_USTRING(" (.") << mpt::transcode<mpt::ustring>( sndfile_encoding, ( format_info.extension ? format_info.extension : "" ) ) << MPT_USTRING(")")
+						    << mpt::transcode<mpt::ustring>( sndfile_encoding, ( format_info.name ? format_info.name : "" ) ) << MPT_USTRING(" (.") << mpt::transcode<mpt::ustring>( sndfile_encoding, ( subformat_info.extension ? subformat_info.extension : format_info.extension ? format_info.extension : "" ) ) << MPT_USTRING(")")
 						    << MPT_USTRING(" / ")
 						    << mpt::transcode<mpt::ustring>( sndfile_encoding, ( subformat_info.name ? subformat_info.name : "" ) )
 						    << MPT_USTRING(" [")
@@ -117,6 +117,14 @@ private:
 					case match_recurse:
 						break;
 					case match_exact:
+						if ( subformat_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == subformat_info.extension ) ) {
+							if ( flags.use_float && ( subformat_info.format == SF_FORMAT_FLOAT ) ) {
+								return matched_result( format, format_info, subformat_info, match_mode );
+							} else if ( !flags.use_float && ( subformat_info.format == SF_FORMAT_PCM_16 ) ) {
+								return matched_result( format, format_info, subformat_info, match_mode );
+							}
+						}
+						break;
 						if ( format_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == format_info.extension ) ) {
 							if ( flags.use_float && ( subformat_info.format == SF_FORMAT_FLOAT ) ) {
 								return matched_result( format, format_info, subformat_info, match_mode );
@@ -126,6 +134,14 @@ private:
 						}
 						break;
 					case match_better:
+						if ( subformat_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == subformat_info.extension ) ) {
+							if ( flags.use_float && ( subformat_info.format == SF_FORMAT_FLOAT || subformat_info.format == SF_FORMAT_DOUBLE ) ) {
+								return matched_result( format, format_info, subformat_info, match_mode );
+							} else if ( !flags.use_float && ( subformat_info.format & ( subformat_info.format == SF_FORMAT_PCM_16 || subformat_info.format == SF_FORMAT_PCM_24 || subformat_info.format == SF_FORMAT_PCM_32 ) ) ) {
+								return matched_result( format, format_info, subformat_info, match_mode );
+							}
+						}
+						break;
 						if ( format_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == format_info.extension ) ) {
 							if ( flags.use_float && ( subformat_info.format == SF_FORMAT_FLOAT || subformat_info.format == SF_FORMAT_DOUBLE ) ) {
 								return matched_result( format, format_info, subformat_info, match_mode );
@@ -135,6 +151,9 @@ private:
 						}
 						break;
 					case match_any:
+						if ( subformat_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == subformat_info.extension ) ) {
+							return matched_result( format, format_info, subformat_info, match_mode );
+						}
 						if ( format_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == format_info.extension ) ) {
 							return matched_result( format, format_info, subformat_info, match_mode );
 						}

commit 4ade4ad57c7412949cb44ad3b3e876992b381c3d

[Fix] openmpt123: sndfile: Do not exit early after subformat checks and skip the format's sampleformat. Fixes float32 sampleformat in WAV and other files. Broken since r23436. Fixes <https://bugs.openmpt.org/view.php?id=1932>. Patch by laamaa (slightly modified).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@24551 56274372-70c3-4bfc-bfc3-4c3a0b034d27

openmpt123/openmpt123_sndfile.hpp
@@ -124,7 +124,6 @@ private:
 								return matched_result( format, format_info, subformat_info, match_mode );
 							}
 						}
-						break;
 						if ( format_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == format_info.extension ) ) {
 							if ( flags.use_float && ( subformat_info.format == SF_FORMAT_FLOAT ) ) {
 								return matched_result( format, format_info, subformat_info, match_mode );
@@ -141,7 +140,6 @@ private:
 								return matched_result( format, format_info, subformat_info, match_mode );
 							}
 						}
-						break;
 						if ( format_info.extension && ( mpt::transcode<std::string>( sndfile_encoding, extension ) == format_info.extension ) ) {
 							if ( flags.use_float && ( subformat_info.format == SF_FORMAT_FLOAT || subformat_info.format == SF_FORMAT_DOUBLE ) ) {
 								return matched_result( format, format_info, subformat_info, match_mode );