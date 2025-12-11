OpenMPT/openmpt

commit 063b2b2b68310a288cf9560004faf0c786b29121

[Ref] Settings: Encapsulate SettingState in std::optional in order to signal queued removed settings.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@24589 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Settings.cpp
@@ -147,7 +147,11 @@ SettingValue SettingsContainer::ReadSetting(const SettingPath &path, const Setti
 	{
 		entry = map.insert(map.begin(), std::make_pair(path, SettingState(def).assign(BackendsReadSetting(path, def), false)));
 	}
-	return entry->second;
+	if(!entry->second.has_value())
+	{
+		entry->second.emplace(SettingState(def).assign(BackendsReadSetting(path, def), false));
+	}
+	return entry->second.value();
 }
 
 bool SettingsContainer::IsDefaultSetting(const SettingPath &path) const
@@ -159,7 +163,11 @@ bool SettingsContainer::IsDefaultSetting(const SettingPath &path) const
 	{
 		return true;
 	}
-	return entry->second.IsDefault();
+	if(!entry->second.has_value())
+	{
+		return true;
+	}
+	return entry->second.value().IsDefault();
 }
 
 void SettingsContainer::WriteSetting(const SettingPath &path, const SettingValue &val)
@@ -235,13 +243,25 @@ void SettingsContainer::WriteSettings()
 {
 	ASSERT(theApp.InGuiThread());
 	ASSERT(!CMainFrame::GetMainFrame() || (CMainFrame::GetMainFrame() && !CMainFrame::GetMainFrame()->InNotifyHandler())); // This is a slow path, use CachedSetting for stuff that is accessed in notify handler.
+	std::set<SettingPath> removedsettings;
 	for(auto &[path, value] : map)
 	{
-		if(value.IsDirty())
+		if(value.has_value())
 		{
-			BackendsWriteSetting(path, value);
-			value.Clean();
+			if(value.value().IsDirty())
+			{
+				BackendsWriteSetting(path, value.value());
+				value.value().Clean();
+			}
+		} else
+		{
+			removedsettings.insert(path);
+			BackendsRemoveSetting(path);
 		}
+	}
+	for(const auto & path : removedsettings)
+	{
+		map.erase(path);
 	}
 }
 

mptrack/AdvancedConfigDlg.cpp
@@ -125,6 +125,10 @@ void COptionsAdvanced::ReInit()
 	int i = 0;
 	for(const auto &[path, state] : theApp.GetSettings())
 	{
+		if(!state.has_value())
+		{
+			continue;
+		}
 		// In MPT_USTRING_MODE_WIDE mode,
 		// this loop is heavily optimized to avoid as much string copies as possible
 		// in order to perform ok-ish in debug builds.
@@ -132,8 +136,8 @@ void COptionsAdvanced::ReInit()
 		// this mode by default.
 		const mpt::ustring &section = path.GetRefSection();
 		const mpt::ustring &key = path.GetRefKey();
-		const SettingValue &value = state.GetRefValue();
-		const SettingValue &defaultValue = state.GetRefDefault();
+		const SettingValue &value = state.value().GetRefValue();
+		const SettingValue &defaultValue = state.value().GetRefDefault();
 
 		if(!findStr.empty())
 		{
@@ -273,7 +277,7 @@ void COptionsAdvanced::OnCustomDrawList(NMHDR* pNMHDR, LRESULT* pResult)
 		break;
 	case CDDS_ITEMPREPAINT:
 		{
-			const bool isDefault = theApp.GetSettings().GetMap().find(m_indexToPath[pLVCD->nmcd.lItemlParam])->second.IsDefault();
+			const bool isDefault = theApp.GetSettings().GetMap().find(m_indexToPath[pLVCD->nmcd.lItemlParam])->second.value().IsDefault();
 			COLORREF defColor = m_List.GetBkColor();
 			COLORREF txtColor = m_List.GetTextColor();
 			COLORREF modColor = RGB(GetRValue(defColor) * 0.9 + GetRValue(txtColor) * 0.1, GetGValue(defColor) * 0.9 + GetGValue(txtColor) * 0.1, GetBValue(defColor) * 0.9 + GetBValue(txtColor) * 0.1);
@@ -293,7 +297,7 @@ void COptionsAdvanced::OnOptionDblClick(NMHDR *, LRESULT *)
 	if(index < 0)
 		return;
 	const SettingPath path = m_indexToPath[m_List.GetItemData(index)];
-	SettingValue val = theApp.GetSettings().GetMap().find(path)->second;
+	SettingValue val = theApp.GetSettings().GetMap().find(path)->second.value();
 	if(val.GetType() == SettingTypeBool)
 	{
 		val = !val.as<bool>();

mptrack/ExceptionHandler.cpp
@@ -375,6 +375,10 @@ void DebugReporter::ReportError(mpt::ustring errorMessage)
 			SettingsContainer &settings = theApp.GetSettings();
 			for(const auto &it : settings)
 			{
+				if(!it.second.has_value())
+				{
+					continue;
+				}
 				bool skipPath = false;
 				for(const auto &path : configAnonymize)
 				{
@@ -389,7 +393,7 @@ void DebugReporter::ReportError(mpt::ustring errorMessage)
 					continue;
 				}
 				f
-					<< mpt::ToCharset(mpt::Charset::UTF8, it.first.FormatAsString() + U_(" = ") + it.second.GetRefValue().FormatValueAsString())
+					<< mpt::ToCharset(mpt::Charset::UTF8, it.first.FormatAsString() + U_(" = ") + it.second.value().GetRefValue().FormatValueAsString())
 					<< std::endl;
 			}
 		}

mptrack/Settings.h
@@ -427,8 +427,9 @@ class SettingsContainer
 {
 
 public:
-	using SettingsMap = std::map<SettingPath,SettingState>;
+	using SettingsMap = std::map<SettingPath, std::optional<SettingState>>;
 	using SettingsListenerMap = std::map<SettingPath,std::set<ISettingChanged*>>;
+public:
 	void WriteSettings();
 private:
 	mutable SettingsMap map;

commit b5a6c0a70bf3449ba4ce440afd63525d46210546

[Fix] Settings: Flag settings that are written but so far unknown in SettingsMap as dirty again. Broken since r24589.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@24590 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/Settings.cpp
@@ -177,12 +177,14 @@ void SettingsContainer::WriteSetting(const SettingPath &path, const SettingValue
 	auto entry = map.find(path);
 	if(entry == map.end())
 	{
-		map[path] = val;
+		map[path] = SettingValue();
 		entry = map.find(path);
-	} else
-	{
-		entry->second = val;
 	}
+	if(!entry->second.has_value())
+	{
+		entry->second = SettingValue();
+	}
+	entry->second = val;
 	NotifyListeners(path);
 }
 