93b9deaee6eb37652d408cceb8ba897e07dd605e
Johannes Schultz <sagamusix@openmpt.org>
2026-02-18 17:32:48 +0000

    [Fix] Instrument note map context was missing name in key config files.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@24979 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- mptrack/CommandSet.cpp
+++ mptrack/CommandSet.cpp
@@ -2030,6 +2030,7 @@ CString KeyCombination::GetContextText(InputTargetContext ctx)
 		case kCtxCtrlOrderlist:			return _T("Orderlist");
 		case kCtxVSTGUI:				return _T("Plugin GUI Context");
 		case kCtxChannelSettings:		return _T("Quick Channel Settings Context");
+		case kCtxInsNoteMap:			return _T("Instrument Note Map");
 		case kCtxUnknownContext:
 		default:						return _T("Unknown Context");
 	}
