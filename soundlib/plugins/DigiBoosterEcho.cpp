OpenMPT/openmpt

commit 8f18959f42861356afb6042a7cdc915613af3d5d

[Ref] Plugin interface: Use mpt::span for chunk data.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@8363 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/plugins/DigiBoosterEcho.cpp
@@ -90,8 +90,8 @@ void DigiBoosterEcho::SaveAllParameters()
 	m_pMixStruct->defaultProgram = -1;
 	try
 	{
-		m_pMixStruct->pluginData.resize(sizeof(chunk));
-		memcpy(m_pMixStruct->pluginData.data(), &chunk, sizeof(chunk));
+		m_pMixStruct->pluginData.resize(sizeof(m_chunk));
+		memcpy(m_pMixStruct->pluginData.data(), &m_chunk, sizeof(m_chunk));
 	} MPT_EXCEPTION_CATCH_OUT_OF_MEMORY(e)
 	{
 		MPT_EXCEPTION_DELETE_OUT_OF_MEMORY(e);
@@ -103,9 +103,9 @@ void DigiBoosterEcho::SaveAllParameters()
 void DigiBoosterEcho::RestoreAllParameters(int32 program)
 //-------------------------------------------------------
 {
-	if(m_pMixStruct->pluginData.size() == sizeof(chunk) && !memcmp(m_pMixStruct->pluginData.data(), "Echo", 4))
+	if(m_pMixStruct->pluginData.size() == sizeof(m_chunk) && !memcmp(m_pMixStruct->pluginData.data(), "Echo", 4))
 	{
-		memcpy(&chunk, m_pMixStruct->pluginData.data(), sizeof(chunk));
+		memcpy(&m_chunk, m_pMixStruct->pluginData.data(), sizeof(m_chunk));
 	} else
 	{
 		IMixPlugin::RestoreAllParameters(program);
@@ -119,7 +119,7 @@ PlugParamValue DigiBoosterEcho::GetParameter(PlugParamIndex index)
 {
 	if(index < kEchoNumParameters)
 	{
-		return chunk.param[index] / 255.0f;
+		return m_chunk.param[index] / 255.0f;
 	}
 	return 0;
 }
@@ -130,7 +130,7 @@ void DigiBoosterEcho::SetParameter(PlugParamIndex index, PlugParamValue value)
 {
 	if(index < kEchoNumParameters)
 	{
-		chunk.param[index] = Util::Round<uint8>(value * 255.0f);
+		m_chunk.param[index] = Util::Round<uint8>(value * 255.0f);
 		RecalculateEchoParams();
 	}
 }
@@ -186,11 +186,11 @@ CString DigiBoosterEcho::GetParamDisplay(PlugParamIndex param)
 	CString s;
 	if(param == kEchoMix)
 	{
-		int wet = (chunk.param[kEchoMix] * 100) / 255;
+		int wet = (m_chunk.param[kEchoMix] * 100) / 255;
 		s.Format(_T("%d%% / %d%%"), wet, 100 - wet);
 	} else if(param < kEchoNumParameters)
 	{
-		int val = chunk.param[param];
+		int val = m_chunk.param[param];
 		if(param == kEchoDelay)
 			val *= 2;
 		s.Format(_T("%d"), val);
@@ -201,20 +201,21 @@ CString DigiBoosterEcho::GetParamDisplay(PlugParamIndex param)
 #endif // MODPLUG_TRACKER
 
 
-size_t DigiBoosterEcho::GetChunk(mpt::byte *(&data), bool)
-//--------------------------------------------------------
+IMixPlugin::ChunkData DigiBoosterEcho::GetChunk(bool)
+//---------------------------------------------------
 {
-	data = reinterpret_cast<mpt::byte *>(&chunk);
-	return sizeof(chunk);
+	auto data = reinterpret_cast<const mpt::byte *>(&m_chunk);
+	return ChunkData(data, sizeof(m_chunk));
 }
 
 
-void DigiBoosterEcho::SetChunk(size_t size, mpt::byte *data, bool)
-//----------------------------------------------------------------
+void DigiBoosterEcho::SetChunk(const ChunkData &chunk, bool)
+//----------------------------------------------------------
 {
-	if(size == sizeof(chunk) && !memcmp(data, "Echo", 4))
+	auto data = chunk.data();
+	if(chunk.size() == sizeof(chunk) && !memcmp(data, "Echo", 4))
 	{
-		memcpy(&chunk, data, size);
+		memcpy(&m_chunk, data, chunk.size());
 		RecalculateEchoParams();
 	}
 }
@@ -223,13 +224,13 @@ void DigiBoosterEcho::SetChunk(size_t size, mpt::byte *data, bool)
 void DigiBoosterEcho::RecalculateEchoParams()
 //-------------------------------------------
 {
-	m_delayTime = (chunk.param[kEchoDelay] * m_sampleRate + 250) / 500;
-	m_PMix = (chunk.param[kEchoMix]) * (1.0f / 256.0f);
-	m_NMix = (256 - chunk.param[kEchoMix]) * (1.0f / 256.0f);
-	m_PCrossPBack = (chunk.param[kEchoCross] * chunk.param[kEchoFeedback]) * (1.0f / 65536.0f);
-	m_PCrossNBack = (chunk.param[kEchoCross] * (256 - chunk.param[kEchoFeedback])) * (1.0f / 65536.0f);
-	m_NCrossPBack = ((chunk.param[kEchoCross] - 256) * chunk.param[kEchoFeedback]) * (1.0f / 65536.0f);
-	m_NCrossNBack = ((chunk.param[kEchoCross] - 256) * (chunk.param[kEchoFeedback] - 256)) * (1.0f / 65536.0f);
+	m_delayTime = (m_chunk.param[kEchoDelay] * m_sampleRate + 250) / 500;
+	m_PMix = (m_chunk.param[kEchoMix]) * (1.0f / 256.0f);
+	m_NMix = (256 - m_chunk.param[kEchoMix]) * (1.0f / 256.0f);
+	m_PCrossPBack = (m_chunk.param[kEchoCross] * m_chunk.param[kEchoFeedback]) * (1.0f / 65536.0f);
+	m_PCrossNBack = (m_chunk.param[kEchoCross] * (256 - m_chunk.param[kEchoFeedback])) * (1.0f / 65536.0f);
+	m_NCrossPBack = ((m_chunk.param[kEchoCross] - 256) * m_chunk.param[kEchoFeedback]) * (1.0f / 65536.0f);
+	m_NCrossNBack = ((m_chunk.param[kEchoCross] - 256) * (m_chunk.param[kEchoFeedback] - 256)) * (1.0f / 65536.0f);
 }
 
 OPENMPT_NAMESPACE_END

mptrack/VstPresets.cpp
@@ -97,7 +97,7 @@ VSTPresets::ErrorCode VSTPresets::LoadFile(FileReader &file, IMixPlugin &plugin)
 			if(chunkData)
 			{
 				file.ReadRaw(chunkData, chunkSize);
-				plugin.SetChunk(chunkSize, chunkData, false);
+				plugin.SetChunk(mpt::as_span(chunkData, chunkSize), false);
 				delete[] chunkData;
 			} else
 			{
@@ -148,7 +148,7 @@ VSTPresets::ErrorCode VSTPresets::LoadFile(FileReader &file, IMixPlugin &plugin)
 			if(chunkData)
 			{
 				file.ReadRaw(chunkData, chunkSize);
-				plugin.SetChunk(chunkSize, chunkData, true);
+				plugin.SetChunk(mpt::as_span(chunkData, chunkSize), true);
 				delete[] chunkData;
 			} else
 			{
@@ -193,12 +193,12 @@ bool VSTPresets::SaveFile(std::ostream &f, IMixPlugin &plugin, bool bank)
 
 		if(writeChunk)
 		{
-			mpt::byte *chunk = nullptr;
-			uint32 chunkSize = mpt::saturate_cast<uint32>(plugin.GetChunk(chunk, true));
-			if(chunkSize && chunk)
+			auto chunk = plugin.GetChunk(true);
+			uint32 chunkSize = mpt::saturate_cast<uint32>(chunk.size());
+			if(chunkSize)
 			{
 				mpt::IO::WriteIntBE(f, chunkSize);
-				mpt::IO::WriteRaw(f, chunk, chunkSize);
+				mpt::IO::WriteRaw(f, chunk.data(), chunkSize);
 			} else
 			{
 				// The plugin returned no chunk! Gracefully go back and save parameters instead...
@@ -251,12 +251,12 @@ void VSTPresets::SaveProgram(std::ostream &f, IMixPlugin &plugin)
 
 	if(writeChunk)
 	{
-		mpt::byte *chunk = nullptr;
-		uint32 chunkSize = mpt::saturate_cast<uint32>(plugin.GetChunk(chunk, false));
-		if(chunkSize && chunk)
+		auto chunk = plugin.GetChunk(false);
+		uint32 chunkSize = mpt::saturate_cast<uint32>(chunk.size());
+		if(chunkSize)
 		{
 			mpt::IO::WriteIntBE(f, chunkSize);
-			mpt::IO::WriteRaw(f, chunk, chunkSize);
+			mpt::IO::WriteRaw(f, chunk.data(), chunkSize);
 		} else
 		{
 			// The plugin returned no chunk! Gracefully go back and save parameters instead...

mptrack/Vstplug.cpp
@@ -1731,17 +1731,23 @@ void CVstPlugin::CacheParameterNames(int32 firstParam, int32 lastParam)
 }
 
 
-size_t CVstPlugin::GetChunk(mpt::byte *(&chunk), bool isBank)
-//-----------------------------------------------------------
+IMixPlugin::ChunkData CVstPlugin::GetChunk(bool isBank)
+//-----------------------------------------------------
 {
-	return Dispatch(effGetChunk, isBank ? 0 : 1, 0, &chunk, 0);
+	mpt::byte *chunk = nullptr;
+	auto size = Dispatch(effGetChunk, isBank ? 0 : 1, 0, &chunk, 0);
+	if(chunk == nullptr)
+	{
+		size = 0;
+	}
+	return ChunkData(chunk, size);
 }
 
 
-void CVstPlugin::SetChunk(size_t size, mpt::byte *chunk, bool isBank)
-//-------------------------------------------------------------------
+void CVstPlugin::SetChunk(const ChunkData &chunk, bool isBank)
+//------------------------------------------------------------
 {
-	Dispatch(effSetChunk, isBank ? 0 : 1, size, chunk, 0);
+	Dispatch(effSetChunk, isBank ? 0 : 1, chunk.size(), const_cast<mpt::byte *>(chunk.data()), 0);
 }
 
 

mptrack/Vstplug.h
@@ -77,8 +77,8 @@ public:
 
 	// Check if programs should be stored as chunks or parameters
 	virtual bool ProgramsAreChunks() const { return (m_Effect.flags & effFlagsProgramChunks) != 0; }
-	virtual size_t GetChunk(mpt::byte *(&chunk), bool isBank);
-	virtual void SetChunk(size_t size, mpt::byte *chunk, bool isBank);
+	virtual ChunkData GetChunk(bool isBank);
+	virtual void SetChunk(const ChunkData &chunk, bool isBank);
 	// If true, the plugin produces an output even if silence is being fed into it.
 	//virtual bool ShouldProcessSilence() { return IsInstrument() || ((m_Effect.flags & effFlagsNoSoundInStop) == 0 && Dispatch(effGetTailSize, 0, 0, nullptr, 0.0f) != 1); }
 	// Old JUCE versions set effFlagsNoSoundInStop even when the shouldn't (see various ValhallaDSP reverb plugins). While the user cannot change the plugin bypass setting manually yet, play safe with VST plugins and do not optimize.

plugins/MidiInOut/MidiInOut.cpp
@@ -67,13 +67,12 @@ uint32 MidiInOut::GetLatency() const
 void MidiInOut::SaveAllParameters()
 //---------------------------------
 {
-	mpt::byte *chunk = nullptr;
-	size_t size = GetChunk(chunk, false);
-	if(size == 0 || chunk == nullptr)
+	auto chunk = GetChunk(false);
+	if(chunk.size() == 0)
 		return;
 
 	m_pMixStruct->defaultProgram = -1;
-	m_pMixStruct->pluginData.assign(chunk, chunk + size);
+	m_pMixStruct->pluginData.assign(chunk.cbegin(), chunk.cend());
 }
 
 
@@ -81,7 +80,7 @@ void MidiInOut::RestoreAllParameters(int32 program)
 //-------------------------------------------------
 {
 	IMixPlugin::RestoreAllParameters(program);	// First plugin version didn't use chunks.
-	SetChunk(m_pMixStruct->pluginData.size(), m_pMixStruct->pluginData.data(), false);
+	SetChunk(mpt::as_span(m_pMixStruct->pluginData), false);
 }
 
 
@@ -92,8 +91,8 @@ enum ChunkFlags
 	kIgnoreTiming	= 0x04,	// Do not send timing and sequencing information
 };
 
-size_t MidiInOut::GetChunk(mpt::byte *(&chunk), bool /*isBank*/)
-//--------------------------------------------------------------
+IMixPlugin::ChunkData MidiInOut::GetChunk(bool /*isBank*/)
+//--------------------------------------------------------
 {
 	const std::string programName8 = mpt::ToCharset(mpt::CharsetUTF8, m_programName);
 
@@ -112,15 +111,14 @@ size_t MidiInOut::GetChunk(mpt::byte *(&chunk), bool /*isBank*/)
 	mpt::IO::WriteRaw(s, m_outputDevice.name.c_str(), m_outputDevice.name.size());
 	mpt::IO::WriteIntLE<uint64>(s, IEEE754binary64LE(m_latency).GetInt64());
 	m_chunkData = s.str();
-	chunk = const_cast<mpt::byte *>(mpt::byte_cast<const mpt::byte *>(m_chunkData.c_str()));
-	return m_chunkData.size();
+	return mpt::as_span(mpt::byte_cast<const mpt::byte *>(m_chunkData.data()), m_chunkData.size());
 }
 
 
-void MidiInOut::SetChunk(size_t size, mpt::byte *chunk, bool /*isBank*/)
-//----------------------------------------------------------------------
+void MidiInOut::SetChunk(const ChunkData &chunk, bool /*isBank*/)
+//---------------------------------------------------------------
 {
-	FileReader file(mpt::as_span(chunk, size));
+	FileReader file(chunk);
 	if(!file.CanRead(9 * sizeof(uint32))
 		|| !file.ReadMagic("fEvN")				// VST program chunk magic
 		|| file.ReadInt32LE() > GetVersion()	// Plugin version

plugins/MidiInOut/MidiInOut.h
@@ -161,8 +161,8 @@ public:
 	virtual int GetNumOutputChannels() const { return 0; }
 
 	virtual bool ProgramsAreChunks() const { return true; }
-	virtual size_t GetChunk(mpt::byte *(&chunk), bool isBank);
-	virtual void SetChunk(size_t size, mpt::byte *chunk, bool isBank);
+	virtual ChunkData GetChunk(bool isBank);
+	virtual void SetChunk(const ChunkData &chunk, bool isBank);
 
 protected:
 	// Open a device for input or output.

soundlib/plugins/DigiBoosterEcho.h
@@ -59,7 +59,7 @@ protected:
 	float m_NCrossPBack, m_NCrossNBack;
 
 	// Settings chunk for file I/O
-	PluginChunk chunk;
+	PluginChunk m_chunk;
 
 public:
 	static IMixPlugin* Create(VSTPluginLib &factory, CSoundFile &sndFile, SNDMIXPLUGIN *mixStruct);
@@ -111,8 +111,8 @@ public:
 	virtual int GetNumOutputChannels() const { return 2; }
 
 	virtual bool ProgramsAreChunks() const { return true; }
-	virtual size_t GetChunk(mpt::byte *(&data), bool);
-	virtual void SetChunk(size_t size, mpt::byte *data, bool);
+	virtual ChunkData GetChunk(bool);
+	virtual void SetChunk(const ChunkData &chunk, bool);
 
 protected:
 	void RecalculateEchoParams();

soundlib/plugins/PlugInterface.h
@@ -216,9 +216,10 @@ public:
 	virtual int GetNumInputChannels() const = 0;
 	virtual int GetNumOutputChannels() const = 0;
 
+	typedef mpt::const_byte_span ChunkData;
 	virtual bool ProgramsAreChunks() const { return false; }
-	virtual size_t GetChunk(mpt::byte *(&/*chunk*/), bool /*isBank*/) { return 0; }
-	virtual void SetChunk(size_t /*size*/, mpt::byte * /*chunk*/, bool /*isBank*/) { }
+	virtual ChunkData GetChunk(bool /*isBank*/) { return ChunkData(); }
+	virtual void SetChunk(const ChunkData &/*chunk*/, bool /*isBank*/) { }
 };
 
 

commit 68237d1d188ea3e61cef10c8a50d1baffdbcc901

Merged revision(s) 25193 from branches/OpenMPT-1.29: Merged revision(s) 25189 from trunk/OpenMPT: [Fix] Plugins: DigiBooster: Fix size check in SetChunk() to check the destination size instead of bogus sizeof(mpt::const_byte_span). Broken since r8363. Fixes <https://bugs.openmpt.org/view.php?id=1966>. ........ ........
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.28@25194 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/plugins/DigiBoosterEcho.cpp
@@ -211,7 +211,7 @@ IMixPlugin::ChunkData DigiBoosterEcho::GetChunk(bool)
 void DigiBoosterEcho::SetChunk(const ChunkData &chunk, bool)
 {
 	auto data = chunk.data();
-	if(chunk.size() == sizeof(chunk) && !memcmp(data, "Echo", 4))
+	if(chunk.size() == sizeof(m_chunk) && !memcmp(data, "Echo", 4))
 	{
 		memcpy(&m_chunk, data, chunk.size());
 		RecalculateEchoParams();