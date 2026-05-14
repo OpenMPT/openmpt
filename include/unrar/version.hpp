fda54ef4b7cdacd11fe46d3fb0b04baee157b32e
Johannes Schultz <sagamusix@openmpt.org>
2026-05-14 12:57:17 +0000

    [Var] Update to unrar 7.2.6.
    git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25319 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- include/unrar/version.hpp
+++ include/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     7
-#define RARVER_MINOR    20
+#define RARVER_MINOR    21
 #define RARVER_BETA      0
-#define RARVER_DAY       1
-#define RARVER_MONTH     2
+#define RARVER_DAY      27
+#define RARVER_MONTH     4
 #define RARVER_YEAR   2026
