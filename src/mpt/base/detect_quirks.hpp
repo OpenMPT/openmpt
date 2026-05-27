OpenMPT/openmpt

commit bf1f96389e0b6ca954ce6b9ae27d56623f168937

[Imp] mpt/base/detect_quirks.hpp: libc++ 20 apparently has std::to_chars for floating point now.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25345 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/base/detect_quirks.hpp
@@ -421,7 +421,7 @@
 
 
 
-#if (MPT_LIBCXX_MS && (MPT_MSVC_BEFORE(2019, 4) || !MPT_COMPILER_MSVC)) || MPT_LIBCXX_GNU_BEFORE(11) || MPT_LIBCXX_LLVM || MPT_LIBCXX_GENERIC
+#if (MPT_LIBCXX_MS && (MPT_MSVC_BEFORE(2019, 4) || !MPT_COMPILER_MSVC)) || MPT_LIBCXX_GNU_BEFORE(11) || MPT_LIBCXX_LLVM_BEFORE(200000) || MPT_LIBCXX_GENERIC
 #define MPT_LIBCXX_QUIRK_NO_TO_CHARS_FLOAT
 #endif
 

commit d8c9fedbbf0364ed39ecc3719780b7e678ee1e69

[Fix] mpt/base/detect_quirks.hpp: Set MPT_LIBCXX_QUIRK_NO_TO_CHARS_FLOAT on macOS < 13.3. Broken since r25345.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25347 56274372-70c3-4bfc-bfc3-4c3a0b034d27

src/mpt/base/detect_quirks.hpp
@@ -424,6 +424,15 @@
 #if (MPT_LIBCXX_MS && (MPT_MSVC_BEFORE(2019, 4) || !MPT_COMPILER_MSVC)) || MPT_LIBCXX_GNU_BEFORE(11) || MPT_LIBCXX_LLVM_BEFORE(200000) || MPT_LIBCXX_GENERIC
 #define MPT_LIBCXX_QUIRK_NO_TO_CHARS_FLOAT
 #endif
+#if MPT_OS_MACOSX_OR_IOS
+#if !defined(MAC_OS_X_VERSION_13_3)
+#define MPT_LIBCXX_QUIRK_NO_TO_CHARS_FLOAT
+#else
+#if (MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_13_3)
+#define MPT_LIBCXX_QUIRK_NO_TO_CHARS_FLOAT
+#endif
+#endif
+#endif
 
 
 