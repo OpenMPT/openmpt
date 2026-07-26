594b442f446b866cde8da4fd9fc9e1345e252378
Jörn Heusipp <manx@openmpt.org>
2026-07-26 16:45:50 +0000

    Merged revision(s) 25485 from branches/OpenMPT-1.32: Merged revision(s) 25483 from trunk/OpenMPT: [Fix] build: premake: libopenmpt does not require mpt/environment. ........ ........
    git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.31@25486 56274372-70c3-4bfc-bfc3-4c3a0b034d27

--- build/premake-xcode/mpt-libopenmpt.lua
+++ build/premake-xcode/mpt-libopenmpt.lua
@@ -43,6 +43,8 @@
 	excludes {
 		"../../src/mpt/crypto/**.cpp",
 		"../../src/mpt/crypto/**.hpp",
+		"../../src/mpt/environment/**.cpp",
+		"../../src/mpt/environment/**.hpp",
 		"../../src/mpt/fs/**.cpp",
 		"../../src/mpt/fs/**.hpp",
 		"../../src/mpt/json/**.cpp",
