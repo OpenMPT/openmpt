OpenMPT/openmpt

commit 08729d5b5a4f29f25d8ce917a02f1a0c0b7a460d

[Fix] Parameter Editor: Rows not visible in the pattern editor were not updated properly while modifying (https://bugs.openmpt.org/view.php?id=1982).
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25481 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/EffectVis.cpp
@@ -429,12 +429,12 @@ void CEffectVis::DrawNodes()
 	//erase
 	if ((ROWINDEX)m_nRowToErase < m_startRow || m_nParamToErase < 0)
 	{
-		m_dcNodes.FillSolidRect(&m_rcDraw, 0);
+		m_dcNodes.FillSolidRect(&m_rcDraw, RGB(0, 0, 0));
 	} else
 	{
 		int x = RowToScreenX(m_nRowToErase);
 		CRect r(x - nodeSizeHalf, m_rcDraw.top, x + nodeSizeHalf + 1, m_rcDraw.bottom);
-		m_dcNodes.FillSolidRect(&r, 0);
+		m_dcNodes.FillSolidRect(&r, RGB(0, 0, 0));
 	}
 
 	for (ROWINDEX row = m_startRow; row <= m_endRow; row++)
@@ -708,6 +708,9 @@ void CEffectVis::OnMouseMove(UINT nFlags, CPoint point)
 		m_nLastDrawnRow = row;
 		m_nLastDrawnY = point.y;
 	}
+
+	DrawNodes();
+
 	//update status bar
 	CString status;
 	CString effectName;
@@ -926,6 +929,7 @@ void CEffectVis::MakeChange(ROWINDEX row, int y)
 
 	m_ModDoc.SetModified();
 	m_ModDoc.UpdateAllViews(nullptr, RowHint(row), this);
+	InvalidateRow(row);  // Only invalidate, actual redrawing is triggered in OnMouseMove as we may modify multiple rows with one mouse event
 }
 
 void CEffectVis::SetPcNote(ROWINDEX row)

commit 313197f24850e7e57ddbad7a68916d86466e3b7c

Merged revision(s) 25491 from trunk/OpenMPT: [Fix] Parameter Editor: Fix crash introduced in r25481. ........
git-svn-id: https://source.openmpt.org/svn/openmpt/branches/OpenMPT-1.32@25492 56274372-70c3-4bfc-bfc3-4c3a0b034d27

mptrack/EffectVis.cpp
@@ -676,7 +676,8 @@ void CEffectVis::OnMouseMove(UINT nFlags, CPoint point)
 		m_nParamToErase = GetParam(m_nDragItem);
 
 		MakeChange(m_nDragItem, point.y);
-	} else if ((m_dwStatus & FXVSTATUS_LDRAGGING))
+		DrawNodes();
+	} else if(m_dwStatus & FXVSTATUS_LDRAGGING)
 	{
 		// Interpolate if we detect that rows have been skipped but the left mouse button was not released.
 		// This ensures we produce a smooth curve even when we are not notified of mouse movements at a high frequency (e.g. if CPU usage is high)
@@ -707,15 +708,13 @@ void CEffectVis::OnMouseMove(UINT nFlags, CPoint point)
 		// Remember last modified point in case we need to interpolate
 		m_nLastDrawnRow = row;
 		m_nLastDrawnY = point.y;
+		DrawNodes();
 	}
-
-	DrawNodes();
 
 	//update status bar
 	CString status;
 	CString effectName;
 	uint16 paramValue;
-
 
 	if (IsPcNote(row))
 	{