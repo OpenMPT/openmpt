OpenMPT/openmpt

commit feb0d34c69508e8fa3717552fd7c079fdfcd7c22

[Ref] CSoundFile::InitializeGlobals now takes the channel count in addition to the format to initialize to. With almost all supported formats, we can determine the correct channel count very early on, making it easier to initialize the correct number of channel headers, and allowing us to remove CSoundFile::InitializeChannels completely. In a second step, the fixed-size ChnSettings array and m_nChannels can be merged into a ChnSettings vector. [Reg] SymMOD: Require first chunk to be number of channels to simplify initialization. In practice, all SymMOD files are structured like that.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@20949 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_gt2.cpp
@@ -532,12 +532,10 @@ bool CSoundFile::ReadGTK(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 
 	// Globals
-	InitializeGlobals(MOD_TYPE_MPT);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_MPT, fileHeader.numChannels);
 	m_SongFlags.set(SONG_IMPORTED);
 	m_playBehaviour = GetDefaultPlaybackBehaviour(MOD_TYPE_IT);
 	m_playBehaviour.set(kApplyOffsetWithoutNote);
-	m_nChannels = fileHeader.numChannels;
 	SetupMODPanning(true);
 
 	m_modFormat.madeWithTracker = U_("Graoumf Tracker");
@@ -1182,9 +1180,24 @@ bool CSoundFile::ReadGT2(FileReader &file, ModLoadingFlags loadFlags)
 	if(loadFlags == onlyVerifyHeader)
 		return true;
 
+	std::vector<uint16be> pannedTracks;
+	file.ReadVector(pannedTracks, fileHeader.numPannedTracks);
+
+	ChunkReader chunkFile(file);
+	auto chunks = chunkFile.ReadChunksUntil<GT2Chunk>(1, GT2Chunk::idENDC);
+
+	if(auto chunk = chunks.GetChunk(GT2Chunk::idPATS); chunk.CanRead(2))
+	{
+		if(uint16 channels = chunk.ReadUint16BE(); channels >= 1 && channels <= MAX_BASECHANNELS)
+			InitializeGlobals(MOD_TYPE_MPT, channels);
+		else
+			return false;
+	} else
+	{
+		return false;
+	}
+
 	// Globals
-	InitializeGlobals(MOD_TYPE_MPT);
-	InitializeChannels();
 	m_SongFlags.set(SONG_IMPORTED | SONG_EXFILTERRANGE);
 	m_playBehaviour = GetDefaultPlaybackBehaviour(MOD_TYPE_IT);
 	m_playBehaviour.set(kFT2ST3OffsetOutOfRange, fileHeader.fileVersion >= 6);
@@ -1196,7 +1209,6 @@ bool CSoundFile::ReadGT2(FileReader &file, ModLoadingFlags loadFlags)
 	mptHistory.loadDate.year = fileHeader.year;
 	m_FileHistory.push_back(mptHistory);
 
-	m_nChannels = 32;
 	m_modFormat.madeWithTracker = mpt::ToUnicode(mpt::Charset::ASCII, mpt::String::ReadBuf(mpt::String::spacePadded, fileHeader.trackerName));
 	m_modFormat.formatName = (fileHeader.fileVersion <= 5 ? MPT_UFORMAT("Graoumf Tracker v{}") : MPT_UFORMAT("Graoumf Tracker 2 v{}"))(fileHeader.fileVersion);
 	m_modFormat.type = U_("gt2");
@@ -1205,35 +1217,20 @@ bool CSoundFile::ReadGT2(FileReader &file, ModLoadingFlags loadFlags)
 	m_songName = mpt::String::ReadBuf(mpt::String::spacePadded, fileHeader.songName);
 
 	m_nSamplePreAmp = 256;
-	m_nDefaultGlobalVolume = 384 / (3 + m_nChannels);  // See documentation on command 5xxx
+	m_nDefaultGlobalVolume = 384 / (3 + m_nChannels);  // See documentation on command 5xxx: Default linear master volume is 12288 / (3 + number of channels)
 
 	if(fileHeader.fileVersion <= 5)
 	{
 		Order().SetDefaultSpeed(std::max(fileHeader.speed.get(), uint16(1)));
 		Order().SetDefaultTempoInt(std::max(fileHeader.tempo.get(), uint16(1)));
 		m_nDefaultGlobalVolume = std::min(Util::muldivr_unsigned(fileHeader.masterVol, MAX_GLOBAL_VOLUME, 4095), uint32(MAX_GLOBAL_VOLUME));
-		uint16 tracks = fileHeader.numPannedTracks;
-		LimitMax(tracks, MAX_BASECHANNELS);
+		const CHANNELINDEX tracks = std::min(static_cast<CHANNELINDEX>(pannedTracks.size()), m_nChannels);
 		for(CHANNELINDEX chn = 0; chn < tracks; chn++)
 		{
-			ChnSettings[chn].nPan = std::min(static_cast<uint16>(Util::muldivr_unsigned(file.ReadUint16BE(), 256, 4095)), uint16(256));
+			ChnSettings[chn].nPan = std::min(static_cast<uint16>(Util::muldivr_unsigned(pannedTracks[chn], 256, 4095)), uint16(256));
 		}
 	}
 	file.Seek(fileHeader.headerSize);
-
-	ChunkReader chunkFile(file);
-	auto chunks = chunkFile.ReadChunksUntil<GT2Chunk>(1, GT2Chunk::idENDC);
-
-	if(auto chunk = chunks.GetChunk(GT2Chunk::idPATS); chunk.CanRead(2))
-	{
-		if(uint16 channels = chunk.ReadUint16BE(); channels >= 1 && channels <= MAX_BASECHANNELS)
-			m_nChannels = channels;
-		else
-			return false;
-	} else
-	{
-		return false;
-	}
 
 	if(auto chunk = chunks.GetChunk(GT2Chunk::idSONG); chunk.CanRead(2))
 	{

mptrack/Moddoc.cpp
@@ -643,14 +643,6 @@ void CModDoc::InitializeMod()
 		// Set up mix levels
 		m_SndFile.m_PlayState.m_nGlobalVolume = m_SndFile.m_nDefaultGlobalVolume = MAX_GLOBAL_VOLUME;
 		m_SndFile.m_nSamplePreAmp = m_SndFile.m_nVSTiVolume = 48;
-
-		for (CHANNELINDEX nChn = 0; nChn < MAX_BASECHANNELS; nChn++)
-		{
-			m_SndFile.ChnSettings[nChn].dwFlags.reset();
-			m_SndFile.ChnSettings[nChn].nVolume = 64;
-			m_SndFile.ChnSettings[nChn].nPan = 128;
-			m_SndFile.m_PlayState.Chn[nChn].nGlobalVol = 64;
-		}
 		// Setup LRRL panning scheme for MODs
 		m_SndFile.SetupMODPanning();
 	}
@@ -1178,9 +1170,9 @@ bool CModDoc::NoteOff(UINT note, bool fade, INSTRUMENTINDEX ins, CHANNELINDEX cu
 		{
 			PLUGINDEX plug = pIns->nMixPlug;      // First try intrument VST
 			if((!plug || plug > MAX_MIXPLUGINS)   // No good plug yet
-				&& currentChn < MAX_BASECHANNELS) // Chan OK
+				&& currentChn < m_SndFile.ChnSettings.size())
 			{
-				plug = m_SndFile.ChnSettings[currentChn].nMixPlugin;// Then try Channel VST
+				plug = m_SndFile.ChnSettings[currentChn].nMixPlugin;  // Then try Channel VST
 			}
 
 			if(plug && plug <= MAX_MIXPLUGINS)

mptrack/Undo.cpp
@@ -152,7 +152,7 @@ bool CPatternUndo::PrepareBuffer(undobuf_t &buffer, PATTERNINDEX pattern, CHANNE
 
 	if(storeChannelInfo)
 	{
-		undo.channelInfo.assign(std::begin(sndFile.ChnSettings) + firstChn, std::begin(sndFile.ChnSettings) + firstChn + numChns);
+		undo.channelInfo.assign(sndFile.ChnSettings.begin() + firstChn, sndFile.ChnSettings.begin() + firstChn + numChns);
 	}
 
 	buffer.push_back(std::move(undo));

mptrack/View_gen.cpp
@@ -371,7 +371,7 @@ void CViewGlobals::UpdateView(UpdateHint hint, CObject *pObject)
 		{
 			const CHANNELINDEX nChn = m_nActiveTab * CHANNELS_IN_TAB + ichn;
 			const BOOL bEnable = (nChn < sndFile.GetNumChannels()) ? TRUE : FALSE;
-			if(nChn < MAX_BASECHANNELS)
+			if(nChn < sndFile.GetNumChannels())
 			{
 				const auto &chnSettings = sndFile.ChnSettings[nChn];
 				// Text
@@ -605,7 +605,7 @@ void CViewGlobals::PopulateChannelPlugins(UpdateHint hint, const CObject *pObj)
 	const CSoundFile &sndFile = GetDocument()->GetSoundFile();
 	for(CHANNELINDEX ichn = 0; ichn < CHANNELS_IN_TAB; ichn++)
 	{
-		if(const CHANNELINDEX nChn = m_nActiveTab * CHANNELS_IN_TAB + ichn; nChn < MAX_BASECHANNELS)
+		if(const CHANNELINDEX nChn = m_nActiveTab * CHANNELS_IN_TAB + ichn; nChn < sndFile.GetNumChannels())
 		{
 			PLUGINDEX sel = sndFile.ChnSettings[nChn].nMixPlugin ? sndFile.ChnSettings[nChn].nMixPlugin - 1 : PLUGINDEX_INVALID;
 			m_CbnEffects[ichn].Update(PluginComboBox::Config{PluginComboBox::ShowNoPlugin}.Hint(hint, pObj).CurrentSelection(sel), sndFile);

soundlib/Load_667.cpp
@@ -77,11 +77,10 @@ bool CSoundFile::Read667(FileReader &file, ModLoadingFlags loadFlags)
 	if(loadFlags == onlyVerifyHeader)
 		return true;
 
-	InitializeGlobals(MOD_TYPE_S3M);
+	InitializeGlobals(MOD_TYPE_S3M, 18);
 	m_SongFlags.set(SONG_IMPORTED);
 	Order().SetDefaultTempoInt(150);
 	Order().SetDefaultSpeed(fileHeader.speed);
-	m_nChannels = 18;
 	m_nSamples = 64;
 
 	ReadOrderFromFile<uint8>(Order(), file, fileHeader.numOrders);
@@ -90,8 +89,6 @@ bool CSoundFile::Read667(FileReader &file, ModLoadingFlags loadFlags)
 		if(pat >= 128)
 			return false;
 	}
-
-	InitializeChannels();
 
 	for(SAMPLEINDEX smp = 1; smp <= m_nSamples; smp++)
 	{

soundlib/Load_669.cpp
@@ -135,12 +135,11 @@ bool CSoundFile::Read669(FileReader &file, ModLoadingFlags loadFlags)
 		return false;
 	}
 
-	InitializeGlobals(MOD_TYPE_669);
+	InitializeGlobals(MOD_TYPE_669, 8);
 	m_nMinPeriod = 28 << 2;
 	m_nMaxPeriod = 1712 << 3;
 	Order().SetDefaultTempoInt(78);
 	Order().SetDefaultSpeed(4);
-	m_nChannels = 8;
 	m_playBehaviour.set(kPeriodsAreHertz);
 	m_SongFlags.set(SONG_FASTPORTAS | SONG_AUTO_TONEPORTA);
 #ifdef MODPLUG_TRACKER
@@ -179,7 +178,6 @@ bool CSoundFile::Read669(FileReader &file, ModLoadingFlags loadFlags)
 	// Set up panning
 	for(CHANNELINDEX chn = 0; chn < 8; chn++)
 	{
-		ChnSettings[chn].Reset();
 		ChnSettings[chn].nPan = (chn & 1) ? 0xD0 : 0x30;
 	}
 

soundlib/Load_amf.cpp
@@ -125,10 +125,8 @@ bool CSoundFile::ReadAMF_Asylum(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	InitializeGlobals(MOD_TYPE_AMF0);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_AMF0, 8);
 	SetupMODPanning(true);
-	m_nChannels = 8;
 	Order().SetDefaultSpeed(fileHeader.defaultSpeed);
 	Order().SetDefaultTempoInt(fileHeader.defaultTempo);
 	m_nSamples = fileHeader.numSamples;
@@ -573,8 +571,7 @@ bool CSoundFile::ReadAMF_DSMI(FileReader &file, ModLoadingFlags loadFlags)
 	if(loadFlags == onlyVerifyHeader)
 		return true;
 
-	InitializeGlobals(MOD_TYPE_AMF);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_AMF, (fileSignature.version < 9) ? 4 : fileHeader.numChannels);
 
 	if(isDMF)
 	{
@@ -588,13 +585,11 @@ bool CSoundFile::ReadAMF_DSMI(FileReader &file, ModLoadingFlags loadFlags)
 	}
 	m_modFormat.charset = mpt::Charset::CP437;
 
-	m_nChannels = fileHeader.numChannels;
 	m_nSamples = fileHeader.numSamples;
 
 	if(fileSignature.version < 9)
 	{
 		// Old format revisions are fixed to 4 channels
-		m_nChannels = 4;
 		for(CHANNELINDEX chn = 0; chn < 4; chn++)
 		{
 			ChnSettings[chn].nPan = (chn & 1) ? 0xC0 : 0x40;

soundlib/Load_ams.cpp
@@ -400,11 +400,9 @@ bool CSoundFile::ReadAMS(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	InitializeGlobals(MOD_TYPE_AMS);
-	InitializeChannels();
+	InitializeGlobals(MOD_TYPE_AMS, (fileHeader.channelConfig & 0x1F) + 1);
 
 	m_SongFlags = SONG_ITCOMPATGXX | SONG_ITOLDEFFECTS;
-	m_nChannels = (fileHeader.channelConfig & 0x1F) + 1;
 	m_nSamples = fileHeader.numSamps;
 	SetupMODPanning(true);
 
@@ -774,12 +772,11 @@ bool CSoundFile::ReadAMS2(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 	}
 
-	InitializeGlobals(MOD_TYPE_AMS);
+	InitializeGlobals(MOD_TYPE_AMS, 32);
 
 	m_songName = std::move(songName);
 
 	m_nInstruments = fileHeader.numIns;
-	m_nChannels = 32;
 	SetupMODPanning(true);
 
 	m_modFormat.formatName = U_("Velvet Studio");
@@ -912,7 +909,6 @@ bool CSoundFile::ReadAMS2(FileReader &file, ModLoadingFlags loadFlags)
 	// Channel names
 	for(CHANNELINDEX chn = 0; chn < 32; chn++)
 	{
-		ChnSettings[chn].Reset();
 		file.ReadSizedString<uint8le, mpt::String::spacePadded>(ChnSettings[chn].szName);
 	}
 

commit 7737618e7eead6d0b60f55f43f0363d037cdba17

[Fix] GT2: Loading file versions >= 6 broke in r20949.
git-svn-id: https://source.openmpt.org/svn/openmpt/trunk/OpenMPT@25496 56274372-70c3-4bfc-bfc3-4c3a0b034d27

soundlib/Load_gt2.cpp
@@ -1181,7 +1181,9 @@ bool CSoundFile::ReadGT2(FileReader &file, ModLoadingFlags loadFlags)
 		return true;
 
 	std::vector<uint16be> pannedTracks;
-	file.ReadVector(pannedTracks, fileHeader.numPannedTracks);
+	if(fileHeader.fileVersion <= 5)
+		file.ReadVector(pannedTracks, fileHeader.numPannedTracks);
+	file.Seek(fileHeader.headerSize);
 
 	ChunkReader chunkFile(file);
 	auto chunks = chunkFile.ReadChunksUntil<GT2Chunk>(1, GT2Chunk::idENDC);
@@ -1230,7 +1232,6 @@ bool CSoundFile::ReadGT2(FileReader &file, ModLoadingFlags loadFlags)
 			ChnSettings[chn].nPan = std::min(static_cast<uint16>(Util::muldivr_unsigned(pannedTracks[chn], 256, 4095)), uint16(256));
 		}
 	}
-	file.Seek(fileHeader.headerSize);
 
 	if(auto chunk = chunks.GetChunk(GT2Chunk::idSONG); chunk.CanRead(2))
 	{